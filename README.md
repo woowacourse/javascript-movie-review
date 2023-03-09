965a5573336ed33905950e0e64c0f4f1c0df819a
Cron <hojoong.choi@woowahan.com>
2023-03-09 14:41:57 +0900

    feat: setup build configurations and templates

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-movie-review
+
+FE 5기 레벨1 영화관 미션
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotEnv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: "/",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new DotEnv(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|ts)$/i,
+        exclude: /node_modules/,
+        use: { loader: "ts-loader" },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
