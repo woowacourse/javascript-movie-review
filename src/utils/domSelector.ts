b5096d56ae166f7fdb9fbbf701902b1f792173c2
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-14 17:48:21 +0900

    feat: domSelector 구현

---
+++ src/utils/domSelector.ts
@@ -0,0 +1,15 @@
+const $ = <E extends Element>(selector: string): E => {
+  const element = document.querySelector(selector);
+
+  if (!element) {
+    throw new Error(`DOM에 ${selector} 요소가 존재하지 않습니다.`);
+  }
+
+  return <E>element;
+}
+
+const $$ = <E extends Element>(selector: string): NodeListOf<E> => {
+  return document.querySelectorAll(selector);
+}
+
+export { $, $$ };
