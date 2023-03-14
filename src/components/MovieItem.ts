c8d25a7d2bf5f172e50a756dda27a4b56c801e90
nlom0218 <nlom0218@naver.com>
2023-03-15 00:25:24 +0900

    feat: 영화 정보 전달하기
    Co-authored-by: ukkodeveloper <ukkodeveloper@users.noreply.github.com>

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -1,7 +1,11 @@
+import {Movie} from '../app';
+
 class MovieItem {
   private _node!: HTMLElement;
+  private movieData: Movie;
 
-  constructor() {
+  constructor(movieDate: Movie) {
+    this.movieData = movieDate;
     this.createTemplate();
   }
 
@@ -12,25 +16,25 @@ class MovieItem {
   createTemplate(): this {
     this._node = document.createElement('li');
 
-    this._node.insertAdjacentHTML(
-      'afterbegin',
-      `
-      <a>
-        <div class="item-card">
-          <img
-            class="item-thumbnail"
-            src="https://image.tmdb.org/t/p/w220_and_h330_face/cw6jBnTauNmEEIIXcoNEyoQItG7.jpg"
-            loading="lazy"
-            alt="앤트맨과 와스프: 퀀텀매니아"
-          />
-          <p class="item-title">앤트맨과 와스프: 퀀텀매니아</p>
-          <p class="item-score"><img src="./star_filled.png" alt="별점" /> 6.5</p>
-        </div>
+    this._node.innerHTML = `<a>
+      <div class="item-card">
+        <img
+          class="item-thumbnail"
+          src="https://image.tmdb.org/t/p/w220_and_h330_face/${this.movieData.backdrop_path}"
+          loading="lazy"
+          alt=${this.movieData.title}
+        />
+        <p class="item-title">${this.movieData.title}</p>
+        <p class="item-score"><img src="./star_filled.png" alt="별점" /> ${this.movieData.vote_average}</p>
+      </div>
       </a>
-      `
-    );
+      `;
 
     return this;
+  }
+
+  updateMovie(props: Movie) {
+    this.movieData = props;
   }
 }
 
