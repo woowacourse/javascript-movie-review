82760c2017b8b29597345165a5829f1d846f5e71
Dain Lee <feb.dain@gmail.com>
2023-03-14 16:35:13 +0900

    feat: 헤더 컴포넌트화

---
+++ src/components/Header.ts
@@ -0,0 +1,20 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+    <header>
+      <h1><img src="./logo.png" alt="MovieList 로고" /></h1>
+      <div class="search-box">
+        <input type="text" placeholder="검색" />
+        <button class="search-button">검색</button>
+      </div>
+    </header>
+    `;
+  }
+}
+
+export default Header;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "./css/reset.css";
+import "./css/common.css";
+import "./assets/logo.png";
+import "./assets/search_button.png";
+import "./assets/star_empty.png";
+import "./assets/star_filled.png";
+import Header from "./components/Header";
+
+customElements.define("movie-header", Header);
