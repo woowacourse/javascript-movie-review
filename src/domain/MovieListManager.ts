ba0ce9aa8eb767d9506a7fd706d365f76507d313
Q Kim <qoli0703@gmail.com>
2023-03-14 21:03:51 +0900

    feat: 검색 및 더보기 기능 구현

--- src/domain/MovieListManager.ts
+++ src/domain/MovieListManager.ts
@@ -12,6 +12,7 @@ class MovieListManager {
   private query: string = "";
   private list: Movie[] = [];
   private currentPage: number = 1;
+  private isLastPage = false;
 
   async fetchMovieList() {
     const url = this.query === "" 
@@ -21,9 +22,23 @@ class MovieListManager {
     await fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        this.list = [...data.results]
+        if (data["total_results"] === this.list.length) this.isLastPage = true;
+        this.list.push(...data.results)
       });
   }
+
+  async searchMovieList(movieName:string){
+    this.query = movieName;
+    this.currentPage = 1;
+    this.list = [];
+    await this.fetchMovieList();
+  }
+
+  async getMoreMovieList(){
+    this.currentPage += 1;
+    await this.fetchMovieList();
+  }
+  
 }
 
 export default MovieListManager;
