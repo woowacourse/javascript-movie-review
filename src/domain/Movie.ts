0e460827a1600ea06328c8a5a7d90a160fd0855e
Jungwoo <evencoding@gmail.com>
2023-03-14 17:56:57 +0900

    feat: 특정 query를 포함하는 영화 검색 기능  구현

--- src/domain/Movie.ts
+++ src/domain/Movie.ts
@@ -1,7 +1,7 @@
 class Movie {
   async getPopularMovies(page = 1) {
     const movieList = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${page}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=${page}`
     )
       .then(async (response) => {
         if (response.ok) return response.json();
@@ -15,7 +15,20 @@ class Movie {
     return movieList;
   }
 
-  async findMovies(query: string, page = 1) {}
+  async findMovies(query: string, page = 1) {
+    const foundedMovies = fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}&page=${page}`
+    )
+      .then(async (response) => {
+        if (response.ok) return response.json();
+        const errorMessage = await response.json().then((data) => data.errors);
+
+        throw new Error(errorMessage);
+      })
+      .catch((error) => alert(error));
+
+    return foundedMovies;
+  }
 }
 
 export default Movie;
