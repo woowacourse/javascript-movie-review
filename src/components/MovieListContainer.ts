f5ec77720373b2cdbb6514f518e85c3b5987767d
hae-on <solbi2004@naver.com>
2023-03-14 19:53:21 +0900

    feat: type에 따른 popular movie 데이터 전달

--- src/components/MovieListContainer.ts
+++ src/components/MovieListContainer.ts
@@ -1,15 +1,21 @@
-customElements.define(
-  "movie-list-container",
-  class MovieListContainer extends HTMLElement {
-    constructor() {
-      super();
-    }
-
-    render() {
-      this.innerHTML = /* html */ `
-          <h2>지금 인기 있는 영화</h2>
-          <button class="btn primary full-width">더 보기</button>
-        `;
-    }
+class MovieListContainer extends HTMLElement {
+  constructor() {
+    super();
   }
-);
+
+  render() {
+    const type = this.getAttribute("type");
+
+    this.innerHTML = /* html */ `
+        <h2>${type === "popular" ? "지금 인기 있는 영화" : "검색 결과"}</h2>
+        <movie-list class="item-list" />
+        <button class="btn primary full-width">더 보기</button>
+      `;
+  }
+}
+
+interface MovieListContainer {
+  "movie-list-container": typeof MovieListContainer;
+}
+
+customElements.define("movie-list-container", MovieListContainer);
