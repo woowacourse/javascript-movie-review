904e6c075f4fb9de1d43fdea25b0b09a51372809
Dain Lee <feb.dain@gmail.com>
2023-03-14 16:48:03 +0900

    feat: 영화 목록 컴포넌트화

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ import "./assets/search_button.png";
 import "./assets/star_empty.png";
 import "./assets/star_filled.png";
 import Header from "./components/Header";
+import MovieList from "./components/MovieList";
 
 customElements.define("movie-header", Header);
+customElements.define("movie-list", MovieList);
