1b48a8eb14512b8c1042c2c81c9d0a27c1b18e67
hae-on <solbi2004@naver.com>
2023-03-14 19:47:48 +0900

    feat: 이미지 연결

---
+++ images/index.ts
@@ -0,0 +1,6 @@
+import Logo from "./logo.png";
+import Search from "./search_button.png";
+import StartEmpty from "./star_empty.png";
+import StarFilled from "./star_filled.png";
+
+export { Logo, Search, StartEmpty, StarFilled };
