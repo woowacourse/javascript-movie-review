251f44e11af93003412f56dade8284c8c424077d
hae-on <solbi2004@naver.com>
2023-03-14 19:49:47 +0900

    feat: 데이터 받아서 move item 렌더링 하도록 구현

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -1,24 +1,28 @@
+import { StarFilled } from "../../images";
+
 customElements.define(
   "movie-item",
   class MovieItem extends HTMLElement {
     constructor() {
       super();
 
+      const posterPath = this.getAttribute("poster-path");
+      const title = this.getAttribute("title");
+      const voteAverage = this.getAttribute("vote_average");
+
       this.innerHTML = /* html */ `
-      <li>
         <a href="#">
           <div class="item-card">
             <img
               class="item-thumbnail"
-              src="https://image.tmdb.org/t/p/w220_and_h330_face/cw6jBnTauNmEEIIXcoNEyoQItG7.jpg"
+              src="https://image.tmdb.org/t/p/original/${posterPath}"
               loading="lazy"
-              alt="앤트맨과 와스프: 퀀텀매니아"
+              alt="${title}"
             />
-            <p class="item-title">앤트맨과 와스프: 퀀텀매니아</p>
-            <p class="item-score"><img src="./star_filled.png" alt="별점" /> 6.5</p>
+            <p class="item-title">${title}</p>
+            <p class="item-score"><img src="${StarFilled}" alt="별점" />${voteAverage}</p>
           </div>
         </a>
-      </li>
   `;
     }
   }
