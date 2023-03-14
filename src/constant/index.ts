8b5d02f362a6e455f029c104a7ae56691a67a69b
semnil5202 <semnil5202@gmail.com>
2023-03-14 17:21:21 +0900

    feat: PNG 파일 모듈화
    Co-authored-by: 황준승 <turtle601@users.noreply.github.com>

---
+++ src/constant/index.ts
@@ -0,0 +1,13 @@
+import logo from '../../templates/logo.png';
+import searchButton from '../../templates/search_button.png';
+import starEmpty from '../../templates/star_empty.png';
+import starFilled from '../../templates/star_filled.png';
+
+const MOVIE_APP_IMG_PATH = {
+  logo,
+  searchButton,
+  starEmpty,
+  starFilled,
+};
+
+export { MOVIE_APP_IMG_PATH };
