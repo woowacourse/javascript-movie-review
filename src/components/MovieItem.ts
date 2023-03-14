cbd6e3d0623a2e6e2692adb5d173b0b3c32a5aee
Dain Lee <feb.dain@gmail.com>
2023-03-14 17:14:14 +0900

    feat: 영화 아이템 컴포넌트화

---
+++ src/components/MovieItem.ts
@@ -0,0 +1,27 @@
+export default class MovieItem extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+    <li>
+      <a href="#">
+        <div class="item-card">
+          <img
+            class="item-thumbnail"
+            src="https://image.tmdb.org/t/p/w220_and_h330_face/cw6jBnTauNmEEIIXcoNEyoQItG7.jpg"
+            loading="lazy"
+            alt="앤트맨과 와스프: 퀀텀매니아"
+          />
+          <p class="item-title">앤트맨과 와스프: 퀀텀매니아</p>
+          <p class="item-score">
+            <img src="./star_filled.png" alt="별점" /> 6.5
+          </p>
+        </div>
+      </a>
+    </li>
+    `;
+  }
+}
--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -7,27 +7,16 @@ export default class MovieList extends HTMLElement {
   render() {
     this.innerHTML = `
       <section class="item-view">
-        <h2>지금 인기 있는 영화</h2>
-        <ul class="item-list">
-          <li>
-            <a href="#">
-              <div class="item-card">
-                <img
-                  class="item-thumbnail"
-                  src="https://image.tmdb.org/t/p/w220_and_h330_face/cw6jBnTauNmEEIIXcoNEyoQItG7.jpg"
-                  loading="lazy"
-                  alt="앤트맨과 와스프: 퀀텀매니아"
-                />
-                <p class="item-title">앤트맨과 와스프: 퀀텀매니아</p>
-                <p class="item-score">
-                  <img src="./star_filled.png" alt="별점" /> 6.5
-                </p>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <button class="btn primary full-width">더 보기</button>
-      </section>
+      <h2>지금 인기 있는 영화</h2>
+      <ul class="item-list">
+        <movie-item></movie-item>
+        <movie-item></movie-item>
+        <movie-item></movie-item>
+        <movie-item></movie-item>
+        <movie-item></movie-item>
+      </ul>
+      <button class="btn primary full-width">더 보기</button>
+    </section>
     `;
   }
 }
