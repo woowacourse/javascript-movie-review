c866b880cb0b537deb0f1dd2fddc240dd93e0718
Dain Lee <feb.dain@gmail.com>
2023-03-14 20:01:09 +0900

    feat: API에서 가져온 정보 렌더링

--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -9,23 +9,44 @@ export default class MovieList extends HTMLElement {
   }
 
   async connectedCallback() {
-    this.movies = await movieApi.fetchMovieInfo();
-    this.render();
+    const { page, results, total_pages, total_results } =
+      await movieApi.fetchMovieInfo();
+    this.movies = results;
+    this.renderMovies();
   }
 
-  render() {
+  renderMovies() {
     this.innerHTML = `
-      <section class="item-view">
+    <section class="item-view">
       <h2>지금 인기 있는 영화</h2>
       <ul class="item-list">
-        <movie-item></movie-item>
-        <movie-item></movie-item>
-        <movie-item></movie-item>
-        <movie-item></movie-item>
-        <movie-item></movie-item>
+        ${this.movies.map((movie) => this.renderMovie(movie)).join("")}
       </ul>
       <button class="btn primary full-width">더 보기</button>
     </section>
+    `;
+  }
+
+  renderMovie(movie: any) {
+    return `
+    <li>
+    <a href="#">
+      <div class="item-card">
+        <img
+          class="item-thumbnail"
+          src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+          loading="lazy"
+          alt="${movie.title}"
+        />
+        <p class="item-title">${movie.title}</p>
+        <p class="item-score">
+          <img src="./star_${
+            movie.vote_average > 0 ? "filled" : "empty"
+          }.png" alt="별점" /> ${movie.vote_average}
+        </p>
+      </div>
+    </a>
+  </li>
     `;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ import "./assets/star_empty.png";
 import "./assets/star_filled.png";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
-import MovieItem from "./components/MovieItem";
 
 customElements.define("movie-header", Header);
 customElements.define("movie-list", MovieList);
-customElements.define("movie-item", MovieItem);
