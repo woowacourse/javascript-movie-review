176c5541abc7b53cedaf64d038630093e223f945
TaeeunKim <xodms0309@naver.com>
2023-03-14 17:39:58 +0900

    feat: API 통신으로 받아온 영화 목록을 화면에 렌더링

--- src/components/MovieItem/index.ts
+++ src/components/MovieItem/index.ts
@@ -3,5 +3,13 @@ import template from './index.html';
 export class MovieItem extends HTMLElement {
   connectedCallback() {
     this.innerHTML = template;
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = template
+      .replace('{poster_path}', this.getAttribute('poster')!)
+      .replaceAll('{title}', this.getAttribute('title')!)
+      .replace('{vote_average}', this.getAttribute('vote')!);
   }
 }
--- src/components/MovieList/index.ts
+++ src/components/MovieList/index.ts
@@ -1,7 +1,23 @@
 import template from './index.html';
+import { fetchPopularMovies } from '../../fetch';
+import { Movie } from '../../types';
 
 export class MovieList extends HTMLElement {
   connectedCallback() {
     this.innerHTML = template;
+    this.getPopularMovieList();
+  }
+
+  async getPopularMovieList() {
+    const movieList = await fetchPopularMovies();
+    const $movieList = document.querySelector('.item-list');
+
+    movieList.results.map((movie: Movie) => {
+      const { title, poster_path, vote_average } = movie;
+      $movieList?.insertAdjacentHTML(
+        'beforeend',
+        `<movie-item title=${`"${title}"`} poster=${poster_path} vote=${vote_average}></movie-item>`,
+      );
+    });
   }
 }
