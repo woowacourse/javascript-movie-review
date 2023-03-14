9fb93d6143740af0387cf32fb48a7b61bfe62619
NaveOWO <pianowell96@gmail.com>
2023-03-14 21:01:02 +0900

    [ feat ] 영화 리스트 컴포넌트 생성

---
+++ src/components/MovieItem.ts
@@ -0,0 +1,22 @@
+import { MovieInfoType } from "../@types/movieType";
+
+export const MovieItem = (movieInfo: MovieInfoType) => {
+  const { title, poster_path, vote_average } = movieInfo;
+
+  return `
+    <li>
+      <a href="#">
+        <div class="item-card">
+          <img
+            class="item-thumbnail"
+            src="https://image.tmdb.org/t/p/w220_and_h330_face/${poster_path}"
+            loading="lazy"
+            alt="앤트맨과 와스프: 퀀텀매니아"
+          />
+          <p class="item-title">${title}</p>
+          <p class="item-score"><img src="./star_filled.png" alt="별점" /> ${vote_average}</p>
+        </div>
+      </a>
+    </li>
+    `;
+};
