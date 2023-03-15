2647bff233b50becfcc916aba42f937539fb586d
Q Kim <qoli0703@gmail.com>
2023-03-15 13:35:01 +0900

    feat: 헤더 컴포넌트 및 검색 기능 추가

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import { $ } from '../util/querySelector';
+
+class Header {
+  #element;
+  #manager;
+
+  constructor (manager, element) {
+    this.#element = element;
+    this.#manager = manager;
+  }
+
+  render () {
+    console.log(this.#element, this.#manager);
+    this.#element.innerHTML = `
+      <h1><img src="./logo.png" alt="MovieList 로고" /></h1>
+      <div class="search-box">
+        <input type="text" id="search-input" placeholder="검색" />
+        <button class="search-button">검색</button>
+      </div>
+    `;
+    this.#searchEvent();
+  }
+
+  #searchEvent () {
+    this.#element.addEventListener('click', async (e) => {
+      if (e.target.tagName === 'BUTTON') {
+        const searchData = $('#search-input').value;
+        await this.#manager.searchMovieList(searchData);
+        this.#element.dispatchEvent(new CustomEvent('searchFullfilled', { bubbles: true }));
+      }
+    });
+  }
+}
+
+export default Header;
