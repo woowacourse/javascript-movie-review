01ed1b5a5dfe443194ec3532e15e05b3b78a5953
semnil5202 <semnil5202@gmail.com>
2023-03-15 16:44:16 +0900

    refactor: URL에 page 파라미터 추가 및 변수명 변경
    Co-authored-by: 황준승 <turtle601@users.noreply.github.com>

--- src/domain/api/index.ts
+++ src/domain/api/index.ts
@@ -1,8 +1,8 @@
 const API_KEY = '61777263370291e01254d71031583c64';
-const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`;
 
-export const getApiPopularMovie = async () => {
-  const rootData = await fetch(url);
+export const getApiPopularMovie = async <T>(page: number): Promise<T> => {
+  const fetchingData = await fetch(`${url}&page=${page}`);
 
-  return rootData.ok ? rootData.json() : rootData.ok;
+  return fetchingData.json();
 };
