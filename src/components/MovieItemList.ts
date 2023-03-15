9b697a0327cdb7719984670a3777f3d2ed154822
NaveOWO <pianowell96@gmail.com>
2023-03-15 22:01:07 +0900

    [ feat ] movieItemList 변경

--- src/components/MovieItemList.ts
+++ src/components/MovieItemList.ts
@@ -1,15 +1,42 @@
+import { moreButton } from "./moreButton";
 import { MovieItem } from "./MovieItem";
 
 export default class MovieItemList {
   private _movieItems: any;
-  private _currentPage: number;
+  currentTab: any;
 
-  constructor(movieInfo: any) {
-    this._movieItems = movieInfo;
-    this._currentPage = 1;
+  constructor() {
+    //this._movieItems = movieInfo;
+    this.render();
+    // this.bindEvent();
   }
 
+  // async bindEvent(callback: any) {
+  //   document.querySelector(".primary")?.addEventListener("click", async () => {
+  //     await callback();
+  //   });
+  // }
+
   create() {
-    return `<ul class="item-list">${this._movieItems}</ul>`;
+    return `<ul class="item-list"></ul>
+    ${moreButton()}
+    `;
+  }
+
+  addMovies(n: any) {
+    const listContainer = document.createElement("li");
+
+    listContainer.innerHTML = n;
+    console.log(n);
+    document.querySelector(".item-list")?.appendChild(listContainer);
+  }
+
+  render() {
+    const container = document.createElement("div");
+    container.innerHTML = this.create();
+    console.log(container);
+    document
+      .querySelector(".item-view")
+      ?.insertAdjacentElement("afterend", container);
   }
 }
