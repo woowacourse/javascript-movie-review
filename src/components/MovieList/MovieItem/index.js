a301e5199d1c70b4c29c2984034940c7ca8fc0b4
woo-jk <201702042@o.cnu.ac.kr>
2023-03-15 13:13:49 +0900

    함수형 컴포넌트 코드 중간 저장

--- src/components/MovieList/MovieItem/index.js
+++ src/components/MovieList/MovieItem/index.js
@@ -1,19 +1,21 @@
 import "./index.css";
 import StarIcon from "../../../images/star_filled.png";
 
-const MovieItem = () => {
+const MovieItem = (movie) => {
+  const { title, posterSrc, voteAverage } = movie.getMovieData();
+
   return `
   <li>
     <a href="#">
       <div class="item-card">
         <img
           class="item-thumbnail"
-          src="https://image.tmdb.org/t/p/w220_and_h330_face/cw6jBnTauNmEEIIXcoNEyoQItG7.jpg"
+          src=${posterSrc}
           loading="lazy"
-          alt="앤트맨과 와스프: 퀀텀매니아"
+          alt=${title}
         />
-        <p class="item-title">앤트맨과 와스프: 퀀텀매니아</p>
-        <p class="item-score"><img src=${StarIcon} alt="별점" /> 6.5</p>
+        <p class="item-title">${title}</p>
+        <p class="item-score"><img src=${StarIcon} alt="별점" /> ${voteAverage}</p>
       </div>
     </a>
   </li>
--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -2,12 +2,50 @@ import "./index.css";
 import MovieItem from "./MovieItem";
 import SkeletonList from "./SkeletonList";
 
-const MovieList = () => {
+// const temp = async (type, pageNumber) => {
+//     const newHtml = awaitfetch(
+//       "https://api.themoviedb.org/3/movie/popular?api_key=ef7c54f21b65b1fe66b6cf70349fa55f&language=ko&page=1"
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         // return data.results.map((item) => item.title).join("<br>");
+//         return `
+//           <div>
+//             <p>{data.asd}</p>
+//             <p>{data.asd}</p>
+//           </div>
+//         `;
+//       });
+
+//     document.querySelector("#temp").innerHTML = newHTML;
+//   }
+
+//   return `<div id="temp">hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~hi~</div>`;
+// };
+
+const MovieList = (type, searchKeyword) => {
+  const template = () => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=ef7c54f21b65b1fe66b6cf70349fa55f&language=ko&page=1"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        document.querySelector("#temp").innerHTML = data.results
+          .map((item) => item.title)
+          .join("<br>");
+      });
+    return `<div id='temp' class='item-list'>${SkeletonList()}</div>`;
+  };
+
   return `
         <section class="item-view">
-          <h2>지금 인기 있는 영화</h2>
+          <h2>${
+            type === "popular"
+              ? "지금 인기있는 영화"
+              : `"${searchKeyword}" 검색결과`
+          }</h2>
           <ul class="item-list">
-          ${Array.from({ length: 20 }, () => MovieItem()).join("")}
+            ${template()}
           </ul>
           <button class="btn primary full-width">더 보기</button>
         </section>
