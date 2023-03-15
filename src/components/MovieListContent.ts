cc8650f34b0878d6bd27d73b26c059c0bedb2c4a
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-15 16:26:18 +0900

    refactor: MovieListContent component 구현

---
+++ src/components/MovieListContent.ts
@@ -0,0 +1,66 @@
+import MovieList from "../domain/MovieList";
+import MovieItem from "./MovieItem";
+import { Movie } from "../types/movie";
+import { $ } from "../utils/domSelector";
+import InvalidMessage from "./InvalidMessage";
+
+const MovieListContent = {
+  loadMovies: async (searchKey?: string) => {
+    try {
+      $<HTMLButtonElement>("#movie-list-title").style.display = "block";
+      $<HTMLButtonElement>("#more-button").style.display = "block";
+
+      if (searchKey) {
+        $<HTMLElement>(
+          "#movie-list-title"
+        ).textContent = `"${searchKey}" 검색 결과`;
+      }
+
+      MovieListContent.render();
+      const movies = await MovieList.getMovieData();
+
+      MovieListContent.render(movies);
+
+      if (movies.length < 20) {
+        $<HTMLButtonElement>("#more-button").style.display = "none";
+      }
+
+      if (searchKey && movies.length === 0) {
+        $<HTMLButtonElement>("#movie-list-title").style.display = "none";
+        InvalidMessage.renderNoSearchMessage(searchKey);
+        return;
+      }
+    } catch (error) {
+      InvalidMessage.renderErrorMessage();
+    }
+  },
+
+  render: (movies?: Movie[]) => {
+    const errorMessageElement = document.querySelector(".error-message");
+
+    if (errorMessageElement) {
+      $<HTMLElement>(".item-view").removeChild(errorMessageElement);
+    }
+
+    const template = `
+        ${
+          !movies
+            ? `
+        <li>
+          <a href="#">
+            <div class="item-card">
+              <div class="item-thumbnail skeleton"></div>
+              <div class="item-title skeleton"></div>
+              <div class="item-score skeleton"></div>
+            </div>
+          </a>
+          </li>`.repeat(20)
+            : movies.map((movie) => MovieItem.render(movie)).join("")
+        }`;
+
+    $<HTMLElement>(".item-list").replaceChildren();
+    $<HTMLElement>(".item-list").insertAdjacentHTML("beforeend", template);
+  },
+};
+
+export default MovieListContent;
