c3cf8518033006cbf72de3f4dbc461afb06e8940
TaeeunKim <xodms0309@naver.com>
2023-03-15 19:46:01 +0900

    refactor: 인기영화와 검색된 영화를 가져오는 api 요청을 한 함수로 합침

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,19 +1,8 @@
-const getAPIUrl = (params: string, page = 1) =>
-  `https://api.themoviedb.org/3${params}?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${page}`;
+const getAPIUrl = (params: string, page = 1, query = '') =>
+  `https://api.themoviedb.org/3${params}?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${page}&query=${query}`;
 
-export const fetchPopularMovies = async (page: number) => {
-  const API_URL = getAPIUrl('/movie/popular', page);
-  const res = await fetch(API_URL);
-  try {
-    const data = await res.json();
-    return data.results;
-  } catch (error) {
-    if (error instanceof Error) console.log(error.message);
-  }
-};
-
-export const fetchSearchMovies = async (page: number, query: string) => {
-  const API_URL = `${getAPIUrl('/search/movie', page)}&query=${query}`;
+export const fetchMovies = async (params: string, page = 1, query = '') => {
+  const API_URL = getAPIUrl(params, page, query);
   const res = await fetch(API_URL);
   try {
     const data = await res.json();
