3d6f9bcb9553cdd3037b708601281248f46bd171
semnil5202 <semnil5202@gmail.com>
2023-03-15 16:40:48 +0900

    feat: Observer 패턴 구현
    도메인 Movies가 Observable 클래스를 상속받는다.
    
    Co-authored-by: 황준승 <turtle601@users.noreply.github.com>

---
+++ src/domain/Observable.ts
@@ -0,0 +1,22 @@
+type OberserverType = {
+  movies: ((value?: Object) => void)[];
+};
+
+export default class Observable {
+  observer: OberserverType;
+
+  constructor() {
+    this.observer = {
+      movies: [],
+    };
+  }
+
+  subscribe(key: keyof OberserverType, method: (value?: Object) => void) {
+    this.observer[key].push(method);
+  }
+
+  notify(key: keyof OberserverType, newData?: Object) {
+    console.log('notify');
+    this.observer[key].forEach((method) => method(newData));
+  }
+}
