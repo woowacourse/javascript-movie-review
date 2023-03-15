4cc67789ede5ca9891d3c52df4209e5dffc77eeb
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-15 13:58:48 +0900

    refactor: render함수 수정

--- src/components/MovieListContainer.ts
+++ src/components/MovieListContainer.ts
@@ -1,43 +1,53 @@
 import MovieList from "../domain/MovieList";
+import MovieItem from "./MovieItem";
 import { Movie } from "../types/movie";
 import { $ } from "../utils/domSelector";
-import MovieItem from "./MovieItem";
 
 const MovieListContainer = {
-  render: (isSearch: boolean, movies: Movie[]) => {
-    return `
-      <section class="item-view">
-        <h2>${isSearch ? "검색 결과" : "지금 인기 있는 영화"}</h2>
-        <ul class="item-list">
-        ${movies.map((movie) => MovieItem.render(movie)).join("")}
-        </ul>
-        <button id="more-button" class="btn primary full-width">더 보기</button>
-      </section>`;
+  loadMovies: async (searchKey?: string) => {
+    try {
+      MovieListContainer.render();
+      const movies = await MovieList.getMovieData();
+      console.log(movies);
+      MovieListContainer.render(movies, searchKey);
+    } catch (error) {
+      console.log(error);
+    }
   },
 
-  renderSkeleton: (isSearch: boolean) => {
-    return `
+  render: (movies?: Movie[], searchKey?: string) => {
+    const template = `
       <section class="item-view">
-        <h2>${isSearch ? "검색 결과" : "지금 인기 있는 영화"}</h2>
+        <h2>${
+          searchKey ? `"${searchKey}" 검색 결과` : "지금 인기 있는 영화"
+        }</h2>
         <ul class="item-list">
-        ${`
-          <li>
-            <a href="#">
-              <div class="item-card">
-                <div class="item-thumbnail skeleton"></div>
-                <div class="item-title skeleton"></div>
-                <div class="item-score skeleton"></div>
-              </div>
-            </a>
-            </li>`.repeat(16)}
+        ${
+          !movies
+            ? `
+        <li>
+          <a href="#">
+            <div class="item-card">
+              <div class="item-thumbnail skeleton"></div>
+              <div class="item-title skeleton"></div>
+              <div class="item-score skeleton"></div>
+            </div>
+          </a>
+          </li>`.repeat(20)
+            : movies.map((movie) => MovieItem.render(movie)).join("")
+        }
         </ul>
         <button id="more-button" class="btn primary full-width">더 보기</button>
       </section>`;
+
+    $<HTMLElement>("main").replaceChildren();
+    $<HTMLElement>("main").insertAdjacentHTML("beforeend", template);
+    MovieListContainer.onClick();
   },
 
   onClick: () => {
     $<HTMLButtonElement>("#more-button").addEventListener("click", async () => {
-      const movies: Movie[] = await MovieList.fetchMovieData();
+      const movies: Movie[] = await MovieList.getMovieData();
       $<HTMLUListElement>(".item-list").insertAdjacentHTML(
         "beforeend",
         movies.map((movie) => MovieItem.render(movie)).join("")
