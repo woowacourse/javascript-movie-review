ad8793637f89866cfef2ff0fe334f6c24808f5d7
Jungwoo <evencoding@gmail.com>
2023-03-15 19:38:02 +0900

    refactor: fetch된 데이터에 대해 spread 연산자를 사용 및 적용

--- src/Store.js
+++ src/Store.js
@@ -4,32 +4,28 @@ const Store = {
   movie: new Movie(),
   movies: { results: [], nextPage: 1, query: '', category: 'popular' },
 
-  async getPopularMovies(page = 1) {
+  async getPopularMovies(curPage = 1) {
     if (this.category === 'search') {
       this.movies['nextPage'] = 1;
     }
 
-    const movieList = await this.movie.getPopularMovies(page);
-    console.log(movieList);
+    const { results, total_pages, page } = await this.movie.getPopularMovies(curPage);
 
-    this.movies['results'] =
-      page === 1 ? [...movieList.results] : [...this.movies.results, ...movieList.results];
-    this.movies['nextPage'] = movieList.total_pages === movieList.page ? -1 : movieList.page + 1;
+    this.movies['results'] = page === 1 ? [...results] : [...this.movies.results, ...results];
+    this.movies['nextPage'] = total_pages === page ? -1 : page + 1;
     this.movies['category'] = 'popular';
   },
 
-  async searchedMovies(query, page = 1) {
+  async searchedMovies(query, curPage = 1) {
     if (this.category === 'popular') {
       this.movies['nextPage'] = 1;
     }
 
-    const movieList = await this.movie.findMovies(query, page);
+    const { results, total_pages, page } = await this.movie.findMovies(query, curPage);
 
-    this.movies['results'] =
-      page === 1 ? [...movieList.results] : [...this.movies.results, ...movieList.results];
+    this.movies['results'] = page === 1 ? [...results] : [...this.movies.results, ...results];
     this.movies['query'] = query;
-    this.movies['nextPage'] = movieList.total_pages === movieList.page ? -1 : movieList.page + 1;
-
+    this.movies['nextPage'] = total_pages === page ? -1 : page + 1;
     this.movies['category'] = 'search';
   },
 };
