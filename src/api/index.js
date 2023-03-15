e59d8b13748d4dac9b6a8b51479dc91d51dd3930
bassyu <yrkdgus@gmail.com>
2023-03-15 17:26:27 +0900

    feat(api): 예외 발생 구현

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,21 @@
 const API_KEY = process.env.API_KEY;
 
 export const getPopularMovies = async (page) => {
-  const data = await fetch(
+  const response = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`
   );
 
-  return data.json();
+  if (!response.ok) throw new Error('인기 영화 불러오기 실패');
+
+  return response.json();
 };
 
 export const getSearchMovies = async (query, page) => {
-  const data = await fetch(
+  const response = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ko-KR&query=${query}&page=${page}`
   );
 
-  return data.json();
+  if (!response.ok) throw new Error('검색된 영화 불러오기 실패');
+
+  return response.json();
 };
