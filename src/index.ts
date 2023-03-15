66fc289c90cc2c7e71a3a7d58dd2f799eeb3dc70
inyeong-kang <tomatorkd@naver.com>
2023-03-15 14:12:33 +0900

    chore: tsconfig, webpack.config setting

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+//import { getMovies } from '../src/data/api';
+import './styles/reset.css';
+//import { CONSTANT_EXAMPLE } from './constants.ts';
+
+//const getMovies = require('../src/data/api.ts');
+//getMovies();
+//console.log(CONSTANT_EXAMPLE);
