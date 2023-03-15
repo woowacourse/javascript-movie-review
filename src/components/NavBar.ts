72f6e81914245724f5ecf1ea2d87475cb9174f81
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-15 13:59:04 +0900

    refactor: 엔터와 클릭 한번에 처리하는 함수 구현

--- src/components/NavBar.ts
+++ src/components/NavBar.ts
@@ -1,5 +1,6 @@
-import { Logo } from "../assets";
 import MovieList from "../domain/MovieList";
+import MovieListContainer from "./MovieListContainer";
+import { Logo } from "../assets";
 import { $ } from "../utils/domSelector";
 
 const NavBar = {
@@ -7,38 +8,26 @@ const NavBar = {
     return `
       <header>
         <h1><img src="${Logo}" alt="MovieList 로고" /></h1>
-        <div class="search-box">
+        <form class="search-box">
           <input id="search-input" type="text" placeholder="검색" />
           <button class="search-button">검색</button>
-        </div>
+        </form>
       </header>`;
   },
 
-  onPressEnter: () => {
-    $<HTMLInputElement>("#search-input").addEventListener(
-      "keydown",
-      async (event: KeyboardEvent) => {
-        if (event.key == "enter") {
-          const target = event.target as HTMLInputElement;
-          MovieList.initCurrentPage();
-          // MovieListContainer.renderSkeleton();
-          const movies = await MovieList.searchMovieData(target.value);
-          // MovieListContainer.renderSkeleton();
-        }
+  onSubmit: () => {
+    $<HTMLFormElement>(".search-box").addEventListener(
+      "submit",
+      async (event: Event) => {
+        event.preventDefault();
+        const target = event.target as HTMLFormElement;
+        const inputElement = target.elements[0] as HTMLInputElement;
+        MovieList.setSearchKey(inputElement.value);
+        MovieList.initCurrentPage();
+        MovieListContainer.loadMovies(inputElement.value);
       }
     );
   },
-
-  onClick: () => {
-    $<HTMLButtonElement>(".search-button").addEventListener(
-      "click",
-      (event: Event) => {
-        // domain 값 전달
-      }
-    );
-  },
-
-  // 검색 창 이벤트
 };
 
 export default NavBar;
