128f441127db1ec29a7b4bf386141035fa822c50
Q Kim <qoli0703@gmail.com>
2023-03-15 20:02:41 +0900

    feat: 엔터키 검색 가능 기능 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,26 +12,30 @@ class Header {
   render () {
     this.#element.innerHTML = `
       <h1><img src="./assets/logo.png" alt="MovieList 로고" /></h1>
-      <div class="search-box">
+      <form class="search-box">
         <input type="text" id="search-input" placeholder="검색" />
         <button class="search-button">검색</button>
-      </div>
+      </form>
     `;
     this.#searchEvent();
   }
 
   #searchEvent () {
-    this.#element.addEventListener('click', async (e) => {
-      if (e.target.tagName === 'BUTTON') {
-        const searchData = $('#search-input').value;
-        this.#element.dispatchEvent(new CustomEvent('searchPending', { bubbles: true }));
-        await this.#manager.searchMovieList(searchData);
-        this.#element.dispatchEvent(new CustomEvent('searchFullfilled', { bubbles: true }));
-      } else if (e.target.tagName === 'IMG') {
+    this.#element.addEventListener('click', async (event) => {
+      if (event.target.tagName === 'IMG') {
         this.#element.dispatchEvent(new CustomEvent('searchPending', { bubbles: true }));
         await this.#manager.searchMovieList('');
         this.#element.dispatchEvent(new CustomEvent('searchFullfilled', { bubbles: true }));
       }
+    });
+
+    this.#element.addEventListener('submit', async (event) => {
+      event.preventDefault();
+
+      const searchData = $('#search-input').value;
+      this.#element.dispatchEvent(new CustomEvent('searchPending', { bubbles: true }));
+      await this.#manager.searchMovieList(searchData);
+      this.#element.dispatchEvent(new CustomEvent('searchFullfilled', { bubbles: true }));
     });
   }
 }
