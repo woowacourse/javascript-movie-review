3768981f09ec777d0e47f02c0c1e5a4ff7ae6943
n0eyes <doll4586633@naver.com>
2023-03-15 16:29:36 +0900

    chore: file-loader 설치
    Co-authored-by: tkdrb12 <tkdrb15@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -34,11 +34,15 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', { loader: 'css-loader', options: { url: false } }],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
       },
     ],
   },
