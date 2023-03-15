693ad7f5e25fa9d3eb74a1328f5e380c8b3f8573
yeopto <gunyeop530@gmail.com>
2023-03-15 21:57:07 +0900

    feat: 헤더 로고를 눌렀을 때 인기있는 영화를 보여주는 로직을 작성하다
    Co-authored-by: Gilpop8663 <wolfye@naver.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import './Header.css';
 import Logo from '../image/logo.png';
+import { $ } from '../utils/common';
 
 class Header extends HTMLElement {
   connectedCallback() {
     this.render();
+    this.setLogoClickEvent();
   }
 
   render() {
     this.innerHTML = `
     <header>
-      <h1><img src="${Logo}" alt="MovieList 로고" /></h1>
+      <h1 id="logo"><img src="${Logo}" alt="MovieList 로고" /></h1>
       <search-input></search-input>
     </header>`;
+  }
+
+  setLogoClickEvent() {
+    $('#logo').addEventListener('click', () => {
+      $('movies-container').setSearchWord('');
+    });
   }
 }
 
