44979393836649ac5aff42e2a6b361c5527495dc
inyeong-kang <tomatorkd@naver.com>
2023-03-15 21:46:41 +0900

    feat: 이벤트 util 함수 구현

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,26 @@
-export const render = (template: any) => {
-  const bodyElem = $('#app') as HTMLElement;
-  bodyElem.innerHTML = template();
-};
-
 export const $ = (selector: string, parentNode: ParentNode | null = document) =>
   parentNode && parentNode.querySelector(selector);
 export const $$ = (selector: string, parentNode: ParentNode | null = document) =>
   parentNode && parentNode.querySelectorAll(selector);
+
+export const render = (template: string) => {
+  const bodyElem = $('#app') as HTMLElement;
+  bodyElem.innerHTML = template;
+  // bodyElem.innerHTML = template();
+  // setEvent.setAllevent();
+};
+
+export const event = {
+  events: [],
+
+  addEvent(element: string, event: 'click' | 'submit', callback: Function) {
+    // this.events.push({});
+  },
+  /*
+  setAllevent() {
+    this.events.forEach((event) => {
+      Element.addEven-- - ('event', callback);
+    });
+  },
+  */
+};
