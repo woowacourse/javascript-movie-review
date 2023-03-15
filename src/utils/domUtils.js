7790bd1bef388a682e32d724819661500a33d284
bassyu <yrkdgus@gmail.com>
2023-03-15 17:27:55 +0900

    refactor(domUtils): 필요없는 기능 삭제

--- src/utils/domUtils.js
+++ src/utils/domUtils.js
@@ -4,7 +4,3 @@ export const dispatchCustomEvent = ($target, eventType, data = null) => {
 };
 
 export const $ = (selectors) => document.querySelector(selectors);
-
-export const hideElement = (element) => element.classList.add('hide');
-
-export const showElement = (element) => element.classList.remove('hide');
