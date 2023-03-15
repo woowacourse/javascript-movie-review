e198f71914dee71a9d68d48d09598207ab587109
Jungwoo <evencoding@gmail.com>
2023-03-15 16:17:14 +0900

    feat: movieList에서 주입받은 데이터를 사용해 인터페이스 통일

--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -1,18 +1,20 @@
 import starFilled from '../../templates/star_filled.png';
 
-function movieItem({ title, rate, url }) {
+function movieItem({ title, vote_average, poster_path }) {
   return `
     <li>
         <a href="#">
         <div class="item-card">
             <img
             class="item-thumbnail"
-            src="${url}"
+            src="https://image.tmdb.org/t/p/w500/${poster_path}"
             loading="lazy"
             alt="${title}"
             />
             <p class="item-title">${title}</p>
-            <p class="item-score"><img src="${starFilled}" alt="별점" /> ${rate}</p>
+            <p class="item-score"><img src="${starFilled}" alt="별점" /> ${vote_average.toFixed(
+    1
+  )}</p>
         </div>
         </a>
     </li>
