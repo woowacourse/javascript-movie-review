91c3f9dc4ea02776d1fe3b4ed4ff3397c1b63099
TaeeunKim <xodms0309@naver.com>
2023-03-15 15:33:17 +0900

    feat: 검색어를 넣었을 때 그에 맞는 영화를 fetching하는 기능 구현

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,7 +1,18 @@
 import template from './index.html';
+import { fetchSearchMovie } from '../../fetch';
 
 export class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = template;
+    const $input = this.querySelector('input');
+    $input?.addEventListener('change', (e: Event) => {
+      if (!(e.currentTarget instanceof HTMLInputElement)) return;
+      const { value } = e.currentTarget;
+      this.searchMovie(value);
+    });
+  }
+
+  async searchMovie(value: string) {
+    const movies = await fetchSearchMovie(value);
   }
 }
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,8 +1,19 @@
-const getAPIUrl = (params: string, page: number) =>
+const getAPIUrl = (params: string, page = 1) =>
   `https://api.themoviedb.org/3${params}?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${page}`;
 
 export const fetchPopularMovies = async (page: number) => {
   const API_URL = getAPIUrl('/movie/popular', page);
+  const res = await fetch(API_URL);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message);
+  }
+};
+
+export const fetchSearchMovie = async (query: string) => {
+  const API_URL = `${getAPIUrl('/search/movie')}&query=${query}`;
   const res = await fetch(API_URL);
   try {
     const data = await res.json();
