de9932ec55ee9315cab38b5356203a4cf0d6bb0f
bassyu <yrkdgus@gmail.com>
2023-03-15 17:22:10 +0900

    feat(MovieListSection): 스켈레톤 UI 구현

--- src/components/MovieListSection.js
+++ src/components/MovieListSection.js
@@ -7,7 +7,24 @@ class MovieListSection extends HTMLElement {
       <section class="item-view">
         <h2 id="movie-list-title">지금 인기 있는 영화</h2>
         <ul class="item-list"></ul>
-        <ul class="item-list" id="skeleton-list"></ul>
+        <ul class="item-list" id="skeleton-list">
+        ${Array(20)
+          .fill()
+          .map(
+            () => /* html */ `
+            <li>
+              <a href="#">
+                <div class="item-card">
+                  <div class="item-thumbnail skeleton"></div>
+                  <div class="item-title skeleton"></div>
+                  <div class="item-score skeleton"></div>
+                </div>
+              </a>
+            </li>
+            `
+          )
+          .join('')}
+        </ul>
         <button id="load-more" class="btn primary full-width">더 보기</button>
       </section>
     `;
