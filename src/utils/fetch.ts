3d65c0cf5e707624196875cae60f0fdf9102c9f0
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-03-15 17:26:00 +0900

    refactor: base url 및 api
    Co-authored-by: Geun Taek Ahn <GEUNTAEK1013@users.noreply.github.com>

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,8 @@
+import { API, TMDB_BASE_URL } from "../constant/api";
+
 export const getPopularMovies = async (page: number) => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`,
+    `${TMDB_BASE_URL}${API.GET_POPULAR}?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`,
     {
       method: "GET",
     }
