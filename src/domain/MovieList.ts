9373075cbe2009823fa29d0e60619773887279c6
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-15 13:59:23 +0900

    refactor: domain에서 검색어를 관리하도록 수정

--- src/domain/MovieList.ts
+++ src/domain/MovieList.ts
@@ -1,8 +1,9 @@
-import { Movie, MovieDataType } from "../types/movie";
+import { MovieDataType } from "../types/movie";
 
 class MovieList {
   private static instance: MovieList;
   private currentPage: number = 1;
+  private searchKey: string = "";
 
   static getInstance() {
     if (!MovieList.instance) {
@@ -12,11 +13,13 @@ class MovieList {
     return MovieList.instance;
   }
 
-  constructor() {}
+  setSearchKey(newSearchKey: string) {
+    this.searchKey = newSearchKey;
+  }
 
-  async fetchMovieData() {
+  async fetchPopularMovieData() {
     const data = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=f34d1031242e2e85f709994a1cede895&language=en-US&page=${this.currentPage}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=f34d1031242e2e85f709994a1cede895&language=ko-KR&page=${this.currentPage}`
     );
     const movieData: MovieDataType = await data.json();
     this.currentPage = movieData.page + 1;
@@ -24,15 +27,20 @@ class MovieList {
     return [...movieData.results];
   }
 
-  async searchMovieData(searchKey: string) {
-    // 초기화
+  async searchMovieData() {
     const data = await fetch(
-      ` https://api.themoviedb.org/3/search/movie?api_key=f34d1031242e2e85f709994a1cede895&language=en-US&query=${searchKey}&page=${this.currentPage}&include_adult=false`
+      ` https://api.themoviedb.org/3/search/movie?api_key=f34d1031242e2e85f709994a1cede895&language=ko-KR&query=${this.searchKey}&page=${this.currentPage}&include_adult=false`
     );
     const movieData: MovieDataType = await data.json();
     this.currentPage = movieData.page + 1;
 
     return [...movieData.results];
+  }
+
+  async getMovieData() {
+    return this.searchKey !== ""
+      ? await this.searchMovieData()
+      : await this.fetchPopularMovieData();
   }
 
   initCurrentPage() {
