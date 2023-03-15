33e7c621baaff0bf9701e4ec414ff687b3d92c63
NaveOWO <pianowell96@gmail.com>
2023-03-15 14:56:30 +0900

    [ feat ] 키워드 검색결과 api 함수 선언

---
+++ src/api/keywordSearch.ts
@@ -0,0 +1,14 @@
+export const getSearchResult = () => {
+  let currentPage = 1;
+
+  return async function getCurrentResult(keyword: string) {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/keyword?api_key=${process.env.API_KEY}&query=${keyword}&page=${currentPage}`
+    );
+    const data = await response.json();
+    console.log(data?.results);
+    currentPage += 1;
+
+    return data.results;
+  };
+};
