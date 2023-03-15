de7085cf02f46d359e8a0fb8bc06e19a5f33b5b0
inyeong-kang <tomatorkd@naver.com>
2023-03-15 12:52:04 +0900

    chore: eslint prettier setting

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['airbnb', 'prettier'],
-  rules: {
-    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
-    'no-console': ['error'],
-  },
-};
