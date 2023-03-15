cad68f5b6ccb4a14d069d0d09a93088be054499e
inyeong-kang <tomatorkd@naver.com>
2023-03-15 20:21:41 +0900

    feat: 영화 포스터 이미지 url을 가공하는 함수 구현

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -1,6 +1,7 @@
 import star_filled from '../assets/star_filled.png';
 
 import { IMovie } from '../data/api';
+import { makeImagePath } from '../utils/makeImagePath';
 
 export function MovieItem({ poster_path, title, vote_average }: IMovie) {
   return `
@@ -9,9 +10,9 @@ export function MovieItem({ poster_path, title, vote_average }: IMovie) {
             <div class="item-card">
                 <img
                 class="item-thumbnail"
-                src=${poster_path}
+                src=${makeImagePath(poster_path)}
                 loading="lazy"
-                alt="${title}"
+                alt=${title}
                 />
                 <p class="item-title">${title}</p>
                 <p class="item-score"><img src="${star_filled}" alt="별점" />${vote_average}</p>
