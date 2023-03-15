b362beddc43a7a44a93ca5ab2d9177415f97f900
Dain Lee <feb.dain@gmail.com>
2023-03-15 15:44:55 +0900

    feat: 로고 클릭시 처음 화면으로 돌아가는 기능 구현

--- src/components/headerHandler.ts
+++ src/components/headerHandler.ts
@@ -6,7 +6,15 @@ export const onSubmitSearchBox = () => {
   executeEventListener($(".search-box"), "submit", async (event) => {
     const formData = new FormData(event.target as HTMLFormElement);
     const keyword = String(formData.get("search-bar"));
-    
+
     await movieApi.fetchSearchedMovieInfo(keyword);
   });
+};
+
+export const onClickLogo = () => {
+  executeEventListener(
+    $("#logo"),
+    "click",
+    async () => await movieApi.fetchPopularMovieInfo()
+  );
 };
