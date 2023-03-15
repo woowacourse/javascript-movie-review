4ca0700d99734e267e45d24e7a05e432a968038c
NaveOWO <pianowell96@gmail.com>
2023-03-15 22:01:18 +0900

    [ feat ] 도메인 로직 생성

--- src/domain/MovieData.ts
+++ src/domain/MovieData.ts
@@ -1,18 +1,39 @@
 import { MovieInfoType } from "../@types/movieType";
+import { MovieItem } from "../components/MovieItem";
+import MovieItemList from "../components/MovieItemList";
 
 class MovieData {
-  private _movieDatas: any;
-  constructor(movieDatas: any) {
-    this._movieDatas = movieDatas;
+  private _movieDatas: any; // any?
+  private _currentTab: string;
+
+  getData: any;
+
+  constructor() {
+    this._movieDatas;
+    this._currentTab = "all";
+    // this.getData = getData;
   }
 
-  convertMovieData() {
-    return this._movieDatas.map((item: MovieInfoType) => {
+  convertMovieData(movieItems: any) {
+    // any..?
+    return movieItems.map((item: MovieInfoType) => {
       const title = item?.title;
       const posterPath = item?.poster_path;
       const voteAverage = item?.vote_average;
       return { title, posterPath, voteAverage };
     });
+  }
+
+  async generateElement(getData: any) {
+    const movieItems = await getData();
+    const movieElement = await this.convertMovieData(movieItems)
+      .map((item: any) => {
+        return MovieItem(item);
+      })
+      .join("");
+    return movieElement;
+
+    // await movieItemList.bindEvent(this.generateElement(getData));
   }
 }
 
