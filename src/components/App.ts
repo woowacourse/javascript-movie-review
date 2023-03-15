7816f355d62cf0701d082b1408e445b40ea5b42e
inyeong-kang <tomatorkd@naver.com>
2023-03-15 21:48:21 +0900

    chore: 불필요한 모듈 삭제

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,11 +1,9 @@
 import { Header } from './Header';
 import { MainPage } from './MainPage';
-import { getMovies, getSearchMovie } from '../data/api';
-import { usePopularMovie, useSearchedMovie } from '../data/useMovie';
 
 export function App() {
   return `
-        ${Header()}
-        ${MainPage()}
+      ${Header()}
+      ${MainPage()}
     `;
 }
