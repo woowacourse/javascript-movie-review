ba65d7abaaf15dec2b465d61b48b61d76d766e1a
bassyu <yrkdgus@gmail.com>
2023-03-15 17:38:56 +0900

    feat(App): 원하는 api로 데이터 불러오는 기능 구현
    - 불러오는 중 스켈레톤 표시
    - 오류 발생시 메세지 알림

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ const App = {
   currentPage: 1,
   query: '',
 
+  async loadMovies(api, params) {
+    let newMovies = [];
+    show('#skeleton-list');
+
+    try {
+      const { results } = await api(...params);
+      newMovies = results;
+    } catch (error) {
+      alert(error.message);
+    }
+
+    hide('#skeleton-list');
+    return newMovies;
+  },
+
   refresh() {
     this.currentPage = 1;
     clearList();
