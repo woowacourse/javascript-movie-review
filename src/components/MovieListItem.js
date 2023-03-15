07f1d2dfc9d9ad7c73b4c6c98b1d6bcc519224ca
bassyu <yrkdgus@gmail.com>
2023-03-15 17:21:44 +0900

    feat(MovieListItem): 이미지 URL 없을 때 기본 이미지 적용

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,15 +1,21 @@
 import StarFilled from '../../image/star_filled.png';
+import DefaultPoster from '../../image/default_poster.png';
 
 class MovieListItem extends HTMLElement {
   constructor() {
     super();
+    const imgSrc = this.getAttribute('poster-path');
     this.innerHTML = /* html */ `
       <li>
         <a href="#">
           <div class="item-card">
             <img
               class="item-thumbnail"
-              src="https://image.tmdb.org/t/p/w220_and_h330_face${this.getAttribute('poster-path')}"
+              src=${
+                imgSrc === 'null'
+                  ? DefaultPoster
+                  : `https://image.tmdb.org/t/p/w220_and_h330_face${imgSrc}`
+              }
               loading="lazy"
               alt="${this.getAttribute('title')}"
             />
