39e80f90a70c29996f1e80ca9b8b8caa10483b8b
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-15 20:25:02 +0900

    feat: skeleton ui 랜더링 기능 구현

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -13,6 +13,8 @@ export default class MovieList {
   render() {
     this.$element.innerHTML = this.template();
 
+    this.toggleSkeletonList();
+
     return this;
   }
 
@@ -25,13 +27,36 @@ export default class MovieList {
       return html + movieCard;
     }, '');
 
+    this.toggleSkeletonList();
     this.$element.querySelector('.item-list').insertAdjacentHTML('beforeend', movieCardsHTML);
+  }
+
+  getSkeletonCardsHTML(count) {
+    const skeletonCardHTML = `
+    <li>
+      <a href="#">
+        <div class="item-card">
+          <div class="item-thumbnail skeleton"></div>
+          <div class="item-title skeleton"></div>
+          <div class="item-score skeleton"></div>
+        </div>
+      </a>
+    </li>`;
+
+    return skeletonCardHTML.repeat(count);
   }
 
   template() {
     return /* html */ ` 
     <h2>지금 인기 있는 영화</h2>
-    <ul class="item-list"></ul>
+    <ul class="item-list"></ul> 
+    <ul class="skeleton-item-list item-list hide">
+      ${this.getSkeletonCardsHTML(20)}
+    </ul>
     <button class="btn primary full-width">더 보기</button>`;
+  }
+
+  toggleSkeletonList() {
+    this.$element.querySelector('.skeleton-item-list').classList.toggle('hide');
   }
 }
