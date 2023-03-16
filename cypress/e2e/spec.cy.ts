62cb1777f13f596bd01f2a00009c98d8778284e0
hae-on <solbi2004@naver.com>
2023-03-16 15:00:16 +0900

    test: skeleton UI test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,9 +6,6 @@ api 주소를 이상하게 해서 한다
 요청 시간을 길게 준다 */
 
 /**
- * 페이지 끝에 도달한 경우에는 더보기 버튼을 화면에 출력하지 않는다.
- *  영화 목록 아이템에 대한 Skeleton UI를 구현한다.
-
 * 검색를 포함하고 있는 영화를 검색할 수 있다.
   - 엔터키를 눌러 검색할 수 있다.
   - 검색 버튼을 클릭하여 검색할 수 있다.
@@ -34,7 +31,18 @@ describe("정상 작동 기능 테스트", () => {
 
   it("더보기 버튼을 누르면 다음의 영화 목록을 불러 올 수 있다", () => {
     cy.get("#more-button").click();
-
+    cy.wait(2000);
     cy.get("movie-item").should("have.length", 40);
+  });
+
+  it("영화 목록 아이템에 대한 Skeleton UI를 구현한다", () => {
+    const apiKey = Cypress.env("CYPRESS_API_KEY");
+
+    cy.intercept(
+      "GET",
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR&page=1`
+    ).as("moviePopular");
+
+    cy.get(".skeleton").should("be.visible");
   });
 });
