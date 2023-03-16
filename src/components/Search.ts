e58dbc88e2930df1a6f52fa0261e3f936065c7d3
inyeong-kang <tomatorkd@naver.com>
2023-03-16 11:31:20 +0900

    feat: 검색 이벤트 핸들링 구현

--- src/components/Search.ts
+++ src/components/Search.ts
@@ -1,7 +1,16 @@
+import { getFormFields } from '../utils/formData';
+import { $, Event } from '../utils/index';
+
 export function Search() {
+  Event.addEvent('submit', '.search-box', (event) => {
+    event.preventDefault();
+    const formEl = $('.search-box') as HTMLFormElement;
+    const formData = getFormFields(formEl);
+  });
+
   return `
         <form class="search-box">
-            <input type="text" placeholder="검색" />
+            <input type="text" placeholder="검색" name="keyword"/>
             <button type="submit" class="search-button">검색</button>
         </form>
      `;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,61 @@
+interface IEvent {
+  parentSelector?: string;
+  targetSelector: string;
+  event: keyof HTMLElementEventMap;
+  callback: (this: Element, e: HTMLElementEventMap[IEvent['event']]) => void;
+}
+
 export const $ = (selector: string, parentNode: ParentNode | null = document) =>
   parentNode && parentNode.querySelector(selector);
+
 export const $$ = (selector: string, parentNode: ParentNode | null = document) =>
   parentNode && parentNode.querySelectorAll(selector);
+
+export const isTarget = (
+  target: EventTarget | null,
+  { targetSelector, parentSelector }: { targetSelector: string; parentSelector: string }
+) => {
+  const children = $$(targetSelector, $(parentSelector));
+  if (target instanceof Element && children)
+    return [...children].includes(target) || target.closest(targetSelector);
+
+  return false;
+};
 
 export const render = (template: string) => {
   const bodyElem = $('#app') as HTMLElement;
   bodyElem.innerHTML = template;
-  // bodyElem.innerHTML = template();
-  // setEvent.setAllevent();
+  Event.setAllEvents();
 };
 
-export const event = {
-  events: [],
+const events: IEvent[] = [];
 
-  addEvent(element: string, event: 'click' | 'submit', callback: Function) {
-    // this.events.push({});
+export const Event = {
+  addEvent(
+    event: IEvent['event'],
+    targetSelector: IEvent['targetSelector'],
+    callback: IEvent['callback'],
+    parentSelector?: IEvent['parentSelector']
+  ) {
+    events.push(
+      parentSelector
+        ? { event, targetSelector, callback, parentSelector }
+        : { event, targetSelector, callback }
+    );
   },
-  /*
-  setAllevent() {
-    this.events.forEach((event) => {
-      Element.addEven-- - ('event', callback);
+
+  setAllEvents() {
+    events.forEach(({ parentSelector, targetSelector, event, callback }) => {
+      if (parentSelector) {
+        $(parentSelector)?.addEventListener(event, (e) => {
+          const $parent = $(parentSelector);
+
+          if (isTarget(e.target, { targetSelector, parentSelector }) && $parent)
+            callback.call($parent, e);
+        });
+      } else {
+        $(targetSelector)?.addEventListener(event, callback);
+      }
     });
   },
-  */
 };
