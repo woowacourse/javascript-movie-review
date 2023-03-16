c467afd0352ebbc3f2cd8789c8ead75f6fabe4b8
NaveOWO <pianowell96@gmail.com>
2023-03-16 09:41:52 +0900

    [ style ] ui 변경

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -4,12 +4,12 @@ export const MovieItem = (movieInfo: any) => {
   const { title, posterPath, voteAverage } = movieInfo;
 
   return `
-
+<li>
       <a href="#">
         <div class="item-card">
           <img
             class="item-thumbnail"
-            src="https://image.tmdb.org/t/p/original${posterPath}"
+            src="https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}"
             loading="lazy"
             alt="앤트맨과 와스프: 퀀텀매니아"
           />
@@ -17,6 +17,6 @@ export const MovieItem = (movieInfo: any) => {
           <p class="item-score"><img src="./star_filled.png" alt="별점" /> ${voteAverage}</p>
         </div>
       </a>
-
+</li>
     `;
 };
