7256646779311138de6c35264bed043dc4b1e98b
hae-on <solbi2004@naver.com>
2023-03-16 16:47:28 +0900

    test: 비정상적인 api 호출 시 에러 이미지 출력 test

---
+++ cypress/e2e/error.cy.ts
@@ -0,0 +1,17 @@
+describe("오류 테스트", () => {
+  it("비정상적인 api 주소를 받아왔을 때 에러 이미지 출력", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: /^https:\/\/api.themoviedb.org\/3\/movie\/popular*/,
+      },
+      { statusCode: 404 }
+    ).as("getPopularMovies");
+
+    cy.visit("http://localhost:8080/");
+
+    cy.wait("@getPopularMovies").then((interception) => {
+      cy.get(".error-image").should("be.visible");
+    });
+  });
+});
