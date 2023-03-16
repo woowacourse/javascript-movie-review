32e06325fc596589214eee4aaeb8c531b4381824
woo-jk <201702042@o.cnu.ac.kr>
2023-03-16 15:59:15 +0900

    test: e2e 인기순 데이터 fetching 테스트

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -10,4 +10,10 @@ describe("template spec", () => {
 
     cy.get(".more").should("exist");
   });
+
+  it("처음 사이트를 방문하면 인기순 영화 데이터 20개가 존재해야 한다.", () => {
+    cy.get(".search-title").should("contain", "지금 인기있는 영화");
+
+    cy.get(".item-card:not(.skeleton)").should("have.length", 20);
+  });
 });
