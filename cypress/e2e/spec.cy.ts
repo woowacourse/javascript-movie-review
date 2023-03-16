5c92be87d6e0c70bf38e01a084d573a5432d5588
Leejin-Yang <ejin1996@gmail.com>
2023-03-16 15:37:27 +0900

    test: e2e 테스트 작성
    Co-authored-by: d0dam <smallkdb@gmail.com>

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,73 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:8080/');
+import type { Movie } from '../../src/types/movie';
+
+beforeEach(() => {
+  cy.intercept(
+    {
+      method: 'GET',
+      url: /^https:\/\/api.themoviedb.org\/3\/movie\/popular*/,
+    },
+    { fixture: 'movie-popular-mock.json' }
+  ).as('getPopularMovies');
+
+  cy.visit('http://localhost:8080/');
+});
+
+describe('메인 화면과 더보기 버튼 테스트', () => {
+  it('앱을 실행하면 지금 인기있는 영화 리스트를 보여준다.', () => {
+    cy.wait('@getPopularMovies').then((interception) => {
+      const movieItems = interception.response?.body.results;
+      expect(movieItems.length).to.equal(20);
+    });
+
+    cy.get('.item-list').children().should('have.length', 20);
+  });
+
+  it('더보기 버튼을 클릭하면 영화 리스트를 더 불러와 보여준다.', () => {
+    cy.get('#load-more-button').click();
+
+    cy.wait('@getPopularMovies').then((interception) => {
+      const movieItems = interception.response?.body.results;
+      expect(movieItems.length).to.equal(20);
+    });
+    cy.get('.item-list').children().should('have.length', 40);
+  });
+});
+
+describe('영화 검색 테스트', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api.themoviedb.org\/3\/search\/movie*/,
+      },
+      { fixture: 'movie-search-mock.json' }
+    ).as('getSearchedMovies');
+
+    cy.get('#movie-search-form input').type('해리포터');
+    cy.get('#movie-search-form').submit();
+  });
+
+  it('"해리포터"를 검색하면 해리포터 문자열이 포함된 영화 리스트를 보여준다.', () => {
+    cy.wait('@getSearchedMovies').then((interception) => {
+      const movieItems: Movie[] = interception.response?.body.results;
+      expect(movieItems.length).to.equal(8);
+
+      movieItems.forEach((item) => {
+        expect(item.title).to.contain('해리 포터');
+      });
+    });
+
+    cy.get('.item-list').children().should('have.length', 8);
+  });
+
+  it('영화 검색 후 헤더 타이틀 버튼을 클릭하면 지금 인기있는 영화 목록을 보여준다.', () => {
+    cy.get('.page-title-button').click();
+
+    cy.wait('@getPopularMovies').then((interception) => {
+      const movieItems = interception.response?.body.results;
+      expect(movieItems.length).to.equal(20);
+    });
+
+    cy.get('.item-list').children().should('have.length', 20);
   });
 });
