8d76e8bc88343ebe5868ab2c2f85210ec675b972
ashleysyheo <ashleysyheo@gmail.com>
2023-03-16 17:02:41 +0900

    refactor: MovieListContainer 컴포넌트 수정

--- src/components/MovieListContainer.ts
+++ src/components/MovieListContainer.ts
@@ -2,7 +2,7 @@ import MovieList from '../domain/MovieList';
 import MovieItem from './MovieItem';
 import { Movie } from '../types/movie';
 import { $ } from '../utils/domSelector';
-import { MOVIE_MAX_COUNT } from '../constants';
+import MovieListContent from './MovieListContent';
 
 const MovieListContainer = {
   template() {
@@ -11,37 +11,55 @@ const MovieListContainer = {
         <h2 id="movie-list-title">지금 인기 있는 영화</h2>
         <ul class="item-list"></ul>
         <button id="more-button" class="btn primary full-width">더 보기</button>
+        <div class="error-message hide"></div>
       </section>
     `;
   },
 
   onClick: () => {
     $<HTMLButtonElement>('#more-button').addEventListener('click', async () => {
-      const movies: Movie[] = await MovieList.getMovieData();
-
-      $<HTMLUListElement>('.item-list').insertAdjacentHTML(
-        'beforeend',
-        movies.map((movie) => MovieItem.render(movie)).join('')
-      );
-
-      if (movies.length < MOVIE_MAX_COUNT) {
-        $<HTMLButtonElement>('#more-button').style.display = 'none';
-      }
+      MovieListContent.loadMoreMovies();
     });
-  },
-
-  show: () => {
-    $<HTMLHeadingElement>('#movie-list-title').style.display = 'block';
-    $<HTMLButtonElement>('#more-button').style.display = 'block';
-  },
-
-  hideTitle: () => {
-    $<HTMLHeadingElement>('#movie-list-title').style.display = 'none';
-  },
-
-  hideButton: () => {
-    $<HTMLButtonElement>('#more-button').style.display = 'none';
   },
 };
 
 export default MovieListContainer;
+
+// import MovieList from '../domain/MovieList';
+// import MovieItem from './MovieItem';
+// import { $ } from '../utils/domSelector';
+// import MovieListContent from './MovieListContent';
+
+// const MovieListContainer = {
+//   template() {
+//     return `
+//       <section class="item-view">
+//         <h2 id="movie-list-title">지금 인기 있는 영화</h2>
+//         <ul class="item-list"></ul>
+//         <button id="more-button" class="btn primary full-width">더 보기</button>
+//         <div class="error-message hide"></div>
+//       </section>
+//     `;
+//   },
+
+//   onClick: () => {
+//     $<HTMLButtonElement>('#more-button').addEventListener('click', async () => {
+//       MovieListContent.loadMoreMovies();
+//     });
+//   },
+
+//   show: () => {
+//     $<HTMLHeadingElement>('#movie-list-title').style.display = 'block';
+//     $<HTMLButtonElement>('#more-button').style.display = 'block';
+//   },
+
+//   hideTitle: () => {
+//     $<HTMLHeadingElement>('#movie-list-title').style.display = 'none';
+//   },
+
+//   hideButton: () => {
+//     $<HTMLButtonElement>('#more-button').style.display = 'none';
+//   },
+// };
+
+// export default MovieListContainer;
