6169b7260f443990abe5e418fe62a9e709f9364c
nlom0218 <nlom0218@naver.com>
2023-03-16 18:35:21 +0900

    build: 웹팩 파일 수정

--- src/domain/getAPI.ts
+++ src/domain/getAPI.ts
@@ -1,6 +1,6 @@
 const getAPI = {
   popularMovie: (page: number) => {
-    return `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KRS&page=${page}`;
+    return `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`;
   },
   searchMovie: (keyword: string, page: number) =>
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${keyword}&page=${page}`,
