7b30651aae6996ddce2d910fb0be6f356b9c0a28
NaveOWO <pianowell96@gmail.com>
2023-03-16 15:15:01 +0900

    [ feat ] api 선언부 try&catch 추가

--- src/api/movieList.ts
+++ src/api/movieList.ts
@@ -2,12 +2,16 @@ export const getPopularMovie = () => {
   let currentPage = 1;
 
   return async function getCurrentMovies() {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&page=${currentPage}`
-    );
-    const data = await response.json();
-    currentPage += 1;
-
-    return data.results;
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&page=${currentPage}`
+      );
+      const data = await response.json();
+      currentPage += 1;
+      const a = { data, currentPage };
+      return a;
+    } catch (e) {
+      console.log(e);
+    }
   };
 };
