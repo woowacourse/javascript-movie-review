4b35a1bdb3460e511de0954e6892fc5c11612148
bassyu <yrkdgus@gmail.com>
2023-03-16 13:43:32 +0900

    refactor(api): base url 상수화

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 const API_KEY = process.env.API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const getPopularMovies = async (page) => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`
   );
 
   if (!response.ok) throw new Error('인기 영화 불러오기 실패');
@@ -12,7 +13,7 @@ export const getPopularMovies = async (page) => {
 
 export const getSearchMovies = async (query, page) => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ko-KR&query=${query}&page=${page}`
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&language=ko-KR&query=${query}&page=${page}`
   );
 
   if (!response.ok) throw new Error('검색된 영화 불러오기 실패');
