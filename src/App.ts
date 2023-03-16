171f757d220269afba0f5e903d81e4f2486941ca
NaveOWO <pianowell96@gmail.com>
2023-03-16 11:15:05 +0900

    [ feat ] search 컴포넌트 커스텀 이벤트 생성

--- src/App.ts
+++ src/App.ts
@@ -4,10 +4,19 @@ import { getPopularMovie } from "./api/movieList";
 import { moreButton } from "./components/moreButton";
 import { MovieItem } from "./components/MovieItem";
 import MovieItemList from "./components/MovieItemList";
+import SearchBox from "./components/SearchBox";
 import MovieData from "./domain/MovieData";
 
 export const App = async () => {
   let currentTab = "ALL";
+  const searchBox = new SearchBox();
+  console.log(document.querySelector(".search-input"));
+  document
+    .querySelector(".search-input")!
+    .addEventListener("searchButtonClicked", (e: unknown) => {
+      if (!(e instanceof CustomEvent)) return;
+      console.log(e.detail.query);
+    });
 
   const getData = getPopularMovie(); // 최초 데이타
 
