b078718e0fb49cb72881139209c7c52f84fa4349
chex <chex417@naver.com>
2023-03-16 15:01:16 +0900

    feat: 로고 이미지 클릭 시 첫 화면으로 이동하는 기능 추가

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -5,7 +5,7 @@ class Header {
 
   render() {
     const template = `      
-      <h1><img src="./assets/logo.png" alt="MovieList 로고" /></h1>
+      <h1><a href="./"><img src="./assets/logo.png" alt="MovieList 로고" /></a></h1>
       <div class="search-box">
         <input type="text" id="search-input" placeholder="Search" />
         <button class="search-button" id="search-button">검색</button>
