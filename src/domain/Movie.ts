c25ce982ba1aa983efd1af78fe2351d2a2e52e6a
Jungwoo <evencoding@gmail.com>
2023-03-16 14:11:13 +0900

    refactor: fetch 데이터의 반환값 변경에 따른 반환 데이터 인터페이스 변경

--- src/domain/Movie.ts
+++ src/domain/Movie.ts
@@ -1,25 +1,29 @@
 import { fetchData } from '../http';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+interface IMovieProps {
+  curPage: number;
+}
+
+interface IFindMovieProps extends IMovieProps {
+  query: string;
+}
+
 class Movie {
-  async getPopularMovies(curPage = 1) {
+  async getPopularMovies({ curPage = 1 }: IMovieProps) {
     const movieList = await fetchData(
-      `${BASE_URL}/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=${curPage}`
+      `${BASE_URL}/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${curPage}`
     );
 
-    const { results, total_pages, page } = movieList;
-
-    return { results, total_pages, page };
+    return movieList;
   }
 
-  async findMovies(query: string, curPage = 1) {
+  async findMovies({ query, curPage = 1 }: IFindMovieProps) {
     const foundedMovies = await fetchData(
-      `${BASE_URL}/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}&page=${curPage}`
+      `${BASE_URL}/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&query=${query}&page=${curPage}`
     );
 
-    const { results, total_pages, page } = foundedMovies;
-
-    return { results, total_pages, page };
+    return foundedMovies;
   }
 }
 
