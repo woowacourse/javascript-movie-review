809e57a5e05d03a4e4029843b218c0be47e48a72
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-16 16:02:25 +0900

    feat: 로고 클릭 시 초기 화면 렌더 기능 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ export default class Header {
 
   template() {
     return `    
-    <h1><img src=${logo} alt="MovieList 로고"/></h1>
+    <h1><img class="main-logo" src=${logo} alt="MovieList 로고"/></h1>
     <form class="search-box">
       <input name='query'type="text" placeholder="검색" />
       <button class="search-button">검색</button>
@@ -27,6 +27,7 @@ export default class Header {
 
   setEvent() {
     this.$element.querySelector('.search-box').addEventListener('submit', this.onSubmitSerachForm.bind(this));
+    this.$element.querySelector('.main-logo').addEventListener('click', this.onClickMainLogo.bind(this));
   }
 
   onSubmitSerachForm(e) {
@@ -35,5 +36,10 @@ export default class Header {
     const queryInput = e.target.elements['query'].value;
 
     this.#renderMovieListByOption('search', queryInput);
+  }
+
+  onClickMainLogo() {
+    this.#renderMovieListByOption('popular');
+    this.$element.querySelector('.search-box').reset();
   }
 }
