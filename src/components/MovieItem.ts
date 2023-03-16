0b4d07cd692972e700b8621168f97cd8b7051a89
NaveOWO <pianowell96@gmail.com>
2023-03-16 15:15:14 +0900

    [ style ] 스켈레톤 ui추가

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -8,13 +8,13 @@ export const MovieItem = (movieInfo: any) => {
       <a href="#">
         <div class="item-card">
           <img
-            class="item-thumbnail"
+            class="item-thumbnail skeleton "
             src="https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}"
             loading="lazy"
             alt="앤트맨과 와스프: 퀀텀매니아"
           />
-          <p class="item-title">${title}</p>
-          <p class="item-score"><img src="./star_filled.png" alt="별점" /> ${voteAverage}</p>
+          <p class="item-title ">${title}</p>
+          <p class="item-score "><img src="./star_filled.png" alt="별점" /> ${voteAverage}</p>
         </div>
       </a>
 </li>
