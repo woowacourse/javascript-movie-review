0f6f81a79d55fe8dc1ca811ba646f4764c0bb6ac
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-17 17:09:24 +0900

    refactor: 요청 쿼리 간단화

--- src/App.ts
+++ src/App.ts
@@ -8,73 +8,47 @@ class App {
   #movieList = new MovieList();
   #movieFetcher = new MovieFetcher();
   #loadMoreButton = new LoadMoreButton();
-  #requestListType = 'popularity';
   #searchKeyword = '';
 
   constructor() {
     this.#header.render();
     this.#movieList.renderListTitle('Popular movies');
-    this.fetchAndUpdateMovieList('popularity', 'overwrite');
+    this.fetchAndUpdateMovieList('overwrite');
     this.#loadMoreButton.render('Load More');
 
     this.#loadMoreButton.addClickEventHandler(this.onClickLoadMoreButton);
     this.#header.addClickEventHandler(this.onClickSearchButton);
   }
 
-  async fetchAndUpdateMovieList(requestListType: string, updateType: string, keyword: string = '') {
-    if (updateType === 'overwrite') this.#movieFetcher.resetPage();
+  async fetchAndUpdateMovieList(updateMode: string, keyword: string = '') {
+    if (updateMode === 'overwrite') this.#movieFetcher.resetPage();
 
     this.#movieList.renderSkeletonItems();
 
-    const { result, movieList } =
-      requestListType === 'popularity'
-        ? await this.#movieFetcher.fetchMovieInfoByPopularity()
-        : await this.#movieFetcher.fetchMovieInfoByKeyword(keyword);
+    const { result, status, movies } = await this.#movieFetcher.fetchMovies(keyword);
 
     this.#movieList.removeSkeletonItems();
 
-    if (result === 'PAGE_ERROR') {
-      alert('페이지 에러');
+    // 임시 오류 메시지
+    if (result !== 'OK') {
+      console.log('Error!', status, movies);
     }
 
-    if (result === 'CLIENT_ERROR') {
-      alert('잘못된 요청입니다. 잠시 후 다시 시도해 주세요.');
-    }
+    if (!movies) return;
 
-    if (result === 'SYSTEM_CRASHED') {
-      alert(
-        '죄송합니다. 알 수 없는 오류로 인해 영화 정보를 가져오는 데 실패하였습니다. 페이지 새로고침 후 다시 시도해 주세요.',
-      );
-      return;
-    }
-
-    if (result === 'SERVER_ERROR') {
-      alert(
-        '죄송합니다. 서버에 문제가 있어 현재 영화 정보를 가져올 수 없습니다. 잠시 후 다시 시도해 주세요.',
-      );
-      return;
-    }
-
-    if (result === 'EMPTY_LIST') {
-      alert('검색 결과가 없습니다!');
-    }
-
-    if (!movieList) return;
-
-    updateType === 'overwrite'
-      ? this.#movieList.renderContents(movieList)
-      : this.#movieList.renderNextContents(movieList);
+    updateMode === 'overwrite'
+      ? this.#movieList.renderContents(movies)
+      : this.#movieList.renderNextContents(movies);
   }
 
   onClickLoadMoreButton = () => {
-    this.fetchAndUpdateMovieList(this.#requestListType, 'append', this.#searchKeyword);
+    this.fetchAndUpdateMovieList('append', this.#searchKeyword);
   };
 
   onClickSearchButton = (keyword: string) => {
     this.#movieList.setTitle(`Search Results of "${keyword}"`);
-    this.#requestListType = 'keyword';
-    this.#searchKeyword = keyword;
-    this.fetchAndUpdateMovieList(this.#requestListType, 'overwrite', this.#searchKeyword);
+    this.#movieFetcher.setRequestMode('search');
+    this.fetchAndUpdateMovieList('overwrite', keyword);
   };
 }
 
--- src/domains/MovieFetcher.ts
+++ src/domains/MovieFetcher.ts
@@ -1,81 +1,72 @@
-import { MovieType, APIMovieType, ResponseType } from '../types';
+import { APIMovieType, ResponseType } from '../types';
+
+const API_URL = {
+  popularity: (pageNo: number) =>
+    `https://wzrabbit-movie-review.netlify.app/.netlify/functions/popularMovies?page=${pageNo}`,
+  search: (pageNo: number, keyword: string) =>
+    `https://wzrabbit-movie-review.netlify.app/.netlify/functions/searchMovies?page=${pageNo}&query=${keyword}`,
+};
+
+const ERROR_MESSAGE: Record<number, string> = {
+  422: '너무 많은 페이지를 불러왔습니다.',
+};
+
+const DEFAULT_ERROR_MESSAGE = '죄송합니다. 문제가 발생하여 영화 정보를 가져오지 못 했습니다.';
 
 class MovieFetcher {
   #currentPage = 1;
+  #requestMode = 'popularity';
+  #previousKeyword = '';
 
   resetPage() {
     this.#currentPage = 1;
   }
 
-  async fetchMovieInfoByPopularity(): Promise<ResponseType> {
-    try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${
-          process.env.API_KEY
-        }&language=en-US&page=${this.#currentPage}`,
-      );
-
-      if (!response.ok) {
-        if (response.status === 422) return { result: 'PAGE_ERROR' };
-        if (response.status >= 400 && response.status <= 499) return { result: 'CLIENT_ERROR' };
-        if (response.status >= 500 && response.status <= 599) return { result: 'SERVER_ERROR' };
-        return { result: '' };
-      }
-
-      const responseText = await response.text();
-      const parsedResponseText = JSON.parse(responseText);
-
-      const movieList = parsedResponseText.results.map((currentResult: APIMovieType) => ({
-        title: currentResult.title,
-        posterPath: currentResult.poster_path,
-        voteAverage: currentResult.vote_average,
-      }));
-
-      if (movieList.length === 0) {
-        return { result: 'EMPTY_LIST' };
-      }
-
-      this.#currentPage += 1;
-
-      return { result: 'OK', movieList };
-    } catch (error) {
-      return { result: 'SYSTEM_CRASHED' };
-    }
+  setRequestMode(mode: string) {
+    this.#requestMode = mode;
   }
 
-  async fetchMovieInfoByKeyword(keyword: string): Promise<ResponseType> {
+  /**
+   * 정해져 있는 페이지와 검색 모드를 토대로 API 서버와의 통신을 통해 영화 정보를 받아와, 그 결과를 돌려줍니다.
+   * 만약 검색 모드임에도 검색어가 비어 있는 경우에는, 이전 검색어를 그대로 사용하여 정보를 받아옵니다.
+   *
+   * @param keyword - 검색 모드 시 사용할 검색어. 검색 모드가 아닐 경우 무시됩니다.
+   */
+  async fetchMovies(keyword: string = ''): Promise<ResponseType> {
+    console.log('ok start');
     try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${
-          process.env.API_KEY
-        }&language=en-US&query=${keyword}&page=${this.#currentPage}&include_adult=false`,
-      );
+      if (keyword !== '') this.#previousKeyword = keyword;
+      const finalKeyword = keyword === '' ? this.#previousKeyword : keyword;
+
+      console.log('keyword is now', finalKeyword);
+
+      const response =
+        this.#requestMode === 'popularity'
+          ? await fetch(API_URL.popularity(this.#currentPage))
+          : await fetch(API_URL.search(this.#currentPage, finalKeyword));
 
       if (!response.ok) {
-        if (response.status === 422) return { result: 'PAGE_ERROR' };
-        if (response.status >= 400 && response.status <= 499) return { result: 'CLIENT_ERROR' };
-        if (response.status >= 500 && response.status <= 599) return { result: 'SERVER_ERROR' };
-        return { result: '' };
+        return {
+          result: ERROR_MESSAGE[response.status] || DEFAULT_ERROR_MESSAGE,
+          status: response.status,
+        };
       }
 
-      const responseText = await response.text();
-      const parsedResponseText = JSON.parse(responseText);
-
-      const movieList = parsedResponseText.results.map((currentResult: APIMovieType) => ({
-        title: currentResult.title,
-        posterPath: currentResult.poster_path,
-        voteAverage: currentResult.vote_average,
+      const responseText = JSON.parse(await response.text());
+      const movies = responseText.results.map((movie: APIMovieType) => ({
+        title: movie.title,
+        posterPath: movie.poster_path,
+        voteAverage: movie.vote_average,
       }));
 
-      if (movieList.length === 0) {
-        return { result: 'EMPTY_LIST' };
+      if (movies.length === 0) {
+        return { result: 'NO_MORE_MOVIES' };
       }
 
       this.#currentPage += 1;
-
-      return { result: 'OK', movieList };
-    } catch (error) {
-      return { result: 'SYSTEM_CRASHED' };
+      return { result: 'OK', movies: movies };
+    } catch {
+      return { result: 'FETCH_CRASHED' };
     }
   }
 }
