1435cdae5961b6e5a058fc63260e333827dda85c
ashleysyheo <ashleysyheo@gmail.com>
2023-03-17 16:56:02 +0900

    test: e2e 테스트 추가 및 수정

--- cypress/e2e/movieReview.cy.js
+++ cypress/e2e/movieReview.cy.js
@@ -37,7 +37,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     cy.visit(TEST_URL);
   });
 
-  it('1. 웹 페이지에 처음 방문하면 지금 인기 있는 영화 목록 데이터가 렌더링되기 전에 skeleton을 볼 수 있다..', () => {
+  it('웹 페이지에 처음 방문하면 지금 인기 있는 영화 목록 데이터가 렌더링되기 전에 skeleton을 볼 수 있다.', () => {
     cy.intercept(
       `${API_BASE_URL}movie/popular?api_key=${apiKey}&language=ko-KR&page=1`,
       (request) => {
@@ -62,7 +62,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     cy.get('.skeleton').should('be.visible');
   });
 
-  it('2. 웹 페이지에 처음 방문하면 지금 인기 있는 영화 목록을 볼 수 있다.', () => {
+  it('웹 페이지에 처음 방문하면 지금 인기 있는 영화 목록을 볼 수 있다.', () => {
     cy.get('.item-card').should('have.length', MOVIE_MAX_COUNT);
 
     cy.fixture('popularMoviesPage1.json').then((expectedData) => {
@@ -72,7 +72,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     });
   });
 
-  it('3. 지금 인기 있는 영화 목록에서 더보기 버튼을 누르면 영화를 더 볼 수 있다.', () => {
+  it('지금 인기 있는 영화 목록에서 더보기 버튼을 누르면 영화를 더 볼 수 있다.', () => {
     cy.get('.item-card').should('have.length', MOVIE_MAX_COUNT);
     cy.get('#more-button').click({ force: true });
 
@@ -87,7 +87,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     });
   });
 
-  it('4. 검색시 검색 결과가 있으면 검색 결과 목록을 볼 수 있다.', () => {
+  it('검색시 검색 결과가 있으면 검색 결과 목록을 볼 수 있다.', () => {
     cy.get('#search-input').type('외계인', { force: true });
     cy.get('#search-button').click({ force: true });
 
@@ -100,7 +100,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     });
   });
 
-  it('5. 검색 결과 목록 더보기 버튼을 누르면 검색 결과의 영화를 더 볼 수 있다.', () => {
+  it('검색 결과 목록 더보기 버튼을 누르면 검색 결과의 영화를 더 볼 수 있다.', () => {
     cy.get('#search-input').type('외계인', { force: true });
     cy.get('#search-button').click({ force: true });
 
@@ -120,7 +120,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     });
   });
 
-  it('6. 페이지 끝에 도달한 경우에는 더보기 버튼이 화면에 더이상 보이지 않는다.', () => {
+  it('페이지 끝에 도달한 경우에는 더보기 버튼이 화면에 더이상 보이지 않는다.', () => {
     cy.get('#search-input').type('외계인', { force: true });
     cy.get('#search-button').click({ force: true });
 
@@ -133,7 +133,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     cy.get('#more-button').should('not.be.visible');
   });
 
-  it('6. 검색시 검색 결과가 없으면 결과 없음 메세지가 화면에 출력된다.', () => {
+  it('검색시 검색 결과가 없으면 결과 없음 메세지가 화면에 출력된다.', () => {
     cy.get('#search-input').type('외계인입니다요', { force: true });
     cy.get('#search-button').click({ force: true });
 
@@ -145,7 +145,7 @@ describe('영화 리뷰 e2e 테스트', () => {
     );
   });
 
-  it('7. HTTP 에러 발생시 에러 메세지가 화면에 출력된다.', () => {
+  it('HTTP 에러 발생시 에러 메세지가 화면에 출력된다.', () => {
     cy.intercept('GET', `${API_BASE_URL}movie/popular?api_key=${apiKey}&language=ko-KR&page=1`, {
       statusCode: 400,
     }).as('HTTPError');
@@ -154,5 +154,16 @@ describe('영화 리뷰 e2e 테스트', () => {
     cy.wait('@HTTPError');
 
     cy.get('.error-message').should('contain', '요청하신 작업을 할 수 없습니다.');
+  });
+
+  it('HTTP 에러 발생시 에러 메세지가 화면에 출력된다.', () => {
+    cy.intercept('GET', `${API_BASE_URL}movie/popular?api_key=${apiKey}&language=ko-KR&page=1`, {
+      statusCode: 500,
+    }).as('HTTPError');
+
+    cy.visit(TEST_URL);
+    cy.wait('@HTTPError');
+
+    cy.get('.error-message').should('contain', '서비스 이용에 불편을 드려 죄송합니다.');
   });
 });
