1bb3094c748b4b62ed4700a5dd3a5106f215577a
ashleysyheo <ashleysyheo@gmail.com>
2023-03-18 01:43:33 +0900

    test: MovieList 테스트 코드 수정

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -15,20 +15,20 @@ describe('MovieList 클래스 테스트', () => {
   test('지금 인기 있은 영화 목록 데이터를 받아온 후 필요한 정보들만 담긴 객체들이 있는 배열을 반환한다.', async () => {
     fetchPopularMovieData.mockResolvedValueOnce(POPULAR_MOVIE_DATA.PAGE1);
 
-    const { movies, searchKey } = await MovieList.getMovieData();
+    const { movies, searchQuery } = await MovieList.getMovieData();
 
     expect(movies).toEqual(POPULAR_MOVIE_MAPPED_DATA.PAGE1);
-    expect(searchKey).toBe('');
+    expect(searchQuery).toBe('');
     expect(fetchPopularMovieData).toHaveBeenCalledWith(1);
   });
 
   test('데이터 재요청 시 지금 인기 있은 영화 목록 다음 페이지의 데이터를 반환한다.', async () => {
     fetchPopularMovieData.mockResolvedValueOnce(POPULAR_MOVIE_DATA.PAGE2);
 
-    const { movies, searchKey } = await MovieList.getMovieData();
+    const { movies, searchQuery } = await MovieList.getMovieData();
 
     expect(movies).toEqual(POPULAR_MOVIE_MAPPED_DATA.PAGE2);
-    expect(searchKey).toBe('');
+    expect(searchQuery).toBe('');
     expect(fetchPopularMovieData).toHaveBeenCalledWith(2);
   });
 
@@ -37,20 +37,20 @@ describe('MovieList 클래스 테스트', () => {
 
     MovieList.init('외계인');
 
-    const { movies, searchKey } = await MovieList.getMovieData();
+    const { movies, searchQuery } = await MovieList.getMovieData();
 
     expect(movies).toEqual(SEARCHED_MOVIE_MAPPED_DATA.PAGE1);
-    expect(searchKey).toBe('외계인');
+    expect(searchQuery).toBe('외계인');
     expect(fetchSearchedMovieData).toHaveBeenCalledWith('외계인', 1);
   });
 
   test('검색어가 있을 때 데이터 재요청 시 검색어의 다음 페이지의 데이터를 반환한다.', async () => {
     fetchSearchedMovieData.mockResolvedValueOnce(SEARCHED_MOVIE_DATA.PAGE2);
 
-    const { movies, searchKey } = await MovieList.getMovieData();
+    const { movies, searchQuery } = await MovieList.getMovieData();
 
     expect(movies).toEqual(SEARCHED_MOVIE_MAPPED_DATA.PAGE2);
-    expect(searchKey).toBe('외계인');
+    expect(searchQuery).toBe('외계인');
     expect(fetchSearchedMovieData).toHaveBeenCalledWith('외계인', 2);
   });
 
