20e54193446eaa421922ceffcb60542b3e0cad7c
regularPark <srk99279@naver.com>
2023-03-18 18:59:34 +0900

    refactor: depth 통일 및 불필요한 파일 삭제

--- src/components/Skeleton.ts
+++ src/components/Skeleton.ts
@@ -1,13 +1,14 @@
 class Skeleton {
-  private template = `<li class="skeleton">
-  <a href="#">
-    <div class="item-card">
-      <div class="item-thumbnail skeleton"></div>
-      <div class="item-title skeleton"></div>
-      <div class="item-score skeleton"></div>
-    </div>
-  </a>
-</li>`;
+  private template = `
+  <li class="skeleton">
+    <a href="#">
+      <div class="item-card">
+        <div class="item-thumbnail skeleton"></div>
+        <div class="item-title skeleton"></div>
+        <div class="item-score skeleton"></div>
+      </div>
+    </a>
+  </li>`;
 
   render() {
     return this.template;
