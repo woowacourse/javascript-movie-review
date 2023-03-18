d248c1b3e0711b222abf4c975e12694f96c0b7c2
hae-on <solbi2004@naver.com>
2023-03-18 18:16:23 +0900

    refactor: fetch 분리와 에러 핸들링 추가

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,25 +1,26 @@
 import { FetchUrl } from "./utils/constants";
 
-export const mostPopular = async (pageNumber: number) => {
-  const response = await fetch(`${FetchUrl.POPULAR_URL}${pageNumber}`);
-
-  if (response.ok) {
-    const result = await response.json();
-
-    return result;
+const fetchData = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const result = await response.json();
+      return result;
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    }
+    return null;
   }
-  return null;
+};
+
+export const mostPopular = async (pageNumber: number) => {
+  const url = `${FetchUrl.POPULAR_URL}${pageNumber}`;
+  return await fetchData(url);
 };
 
 export const search = async (query: string, pageNumber: number) => {
-  const response = await fetch(
-    `${FetchUrl.SEARCH_URL}${query}&page=${pageNumber}&include_adult=false`
-  );
-
-  if (response.ok) {
-    const result = await response.json();
-
-    return result;
-  }
-  return null;
+  const url = `${FetchUrl.SEARCH_URL}${query}&page=${pageNumber}&include_adult=false`;
+  return await fetchData(url);
 };
