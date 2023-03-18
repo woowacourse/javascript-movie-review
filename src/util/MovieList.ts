5bcd7a9c4bfac7070e70a536513359d2fe9e07f6
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-03-18 20:48:32 +0900

    refactor: transformMovieItemType 메서드 로직 변경

--- src/util/MovieList.ts
+++ src/util/MovieList.ts
@@ -3,14 +3,7 @@ import { MovieElementData, MovieItem } from "../abstracts/type";
 const transformMovieItemsType = (
   movieItems: MovieItem[]
 ): MovieElementData[] => {
-  return movieItems.map(
-    ({ poster_path, id, title, vote_average }: MovieItem) => ({
-      poster_path,
-      id,
-      title,
-      vote_average,
-    })
-  );
+  return movieItems;
 };
 
 export default transformMovieItemsType;
