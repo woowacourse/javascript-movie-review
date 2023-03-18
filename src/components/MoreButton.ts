ff5b3689908c298736efe18776e345179a6d6e64
hozzijeong <ghwls0905@naver.com>
2023-03-18 20:23:42 +0900

    refactor: MoreButton.js -> ts로 변환

--- src/components/MoreButton.ts
+++ src/components/MoreButton.ts
@@ -3,7 +3,7 @@ import { Store } from '../Store';
 class MoreButton {
   $button = document.createElement('button');
 
-  constructor($target) {
+  constructor($target: HTMLElement) {
     this.$button.className = 'btn primary full-width more-button';
 
     this.render($target);
@@ -11,12 +11,14 @@ class MoreButton {
     this.$button.addEventListener('click', this.onClickMoreButton);
   }
 
-  render($target) {
+  render($target: HTMLElement) {
     this.$button.innerText = '더 보기';
     $target.insertAdjacentElement('beforeend', this.$button);
   }
 
   onClickMoreButton() {
+    if (!Store.movieStates) return;
+
     const { nextPage, category, query } = Store.movieStates.getMovieStates();
 
     if (category === 'popular') {
