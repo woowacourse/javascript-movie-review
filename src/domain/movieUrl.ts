07304e1746421343019d177fe6f84062e2fd4fd7
Dahye <06robin11@gmail.com>
2023-03-19 22:02:09 +0900

    refactor(url): baseurl 상수로 빼줌

--- src/domain/movieUrl.ts
+++ src/domain/movieUrl.ts
@@ -1,9 +1,10 @@
 const API_KEY = process.env.API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const popularMovieUrl = (page: number) => {
-  return `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}&include_adult=false`;
+  return `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}&include_adult=false`;
 };
 
 export const searchMovieUrl = (query: string, page: number) => {
-  return `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ko-KR&query=${query}&page=${page}&include_adult=false`;
+  return `${BASE_URL}/search/movie?api_key=${API_KEY}&language=ko-KR&query=${query}&page=${page}&include_adult=false`;
 };
