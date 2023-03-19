468dadecd4ee8cc4ce0a9b8e44e7c70835e6eb9b
Patrick <kuyt3122@naver.com>
2023-03-19 17:32:28 +0900

    refactor: api호출 로직과 UI로직 분리

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,4 +1,5 @@
 import { FailedResponse } from './types';
+import { ErrorAlert } from './UI/errorAlert';
 
 export const getAPIUrl = (params: string, page = 1, query = '') =>
   `https://api.themoviedb.org/3${params}?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${page}&query=${query}`;
@@ -14,6 +15,6 @@ export const fetchMovies = async (params: string, page = 1, query = '') => {
     const data = await res.json();
     return { movies: data.results, total_pages: data.total_pages };
   } catch (error) {
-    if (error instanceof Error) alert(error.message);
+    if (error instanceof Error) ErrorAlert(error.message);
   }
 };
