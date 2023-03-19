dfc23cb5cbc7d6d5dd801c5cce9cdf7321c0d62d
dladncks1217 <dlaxodud1217@gmail.com>
2023-03-19 19:48:36 +0900

    refactor: private 변수 선언 방식 변경

--- src/domain/MovieListManager.ts
+++ src/domain/MovieListManager.ts
@@ -8,61 +8,62 @@ const getSearchMovieUrl = (query: string, page = 1) =>
   `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${query}&page=${page}&include_adult=false`;
 
 class MovieListManager {
-  private query: string = '';
-  private list: Movie[] = [];
-  private currentPage: number = 1;
-  private lastPage = false;
-  private storage: Storage;
+  #query: string = '';
+  #list: Movie[] = [];
+  #currentPage: number = 1;
+  #lastPage = false;
+  #storage: Storage;
 
   constructor(storage: Storage) {
-    this.storage = storage;
-    this.query = this.storage.getItem('query');
+    this.#storage = storage;
+    this.#query = this.#storage.getItem('query');
   }
 
   getCurrentPage() {
-    return this.currentPage;
+    return this.#currentPage;
   }
 
   getMovieList() {
-    return this.list.map((movie) => ({ ...movie }));
+    return this.#list.map((movie) => ({ ...movie }));
   }
 
   getQuery() {
-    return this.query;
+    return this.#query;
   }
 
   isLastPage() {
-    return this.lastPage;
+    return this.#lastPage;
   }
 
   async fetchMovieList() {
-    this.storage.setItem('query', this.query);
+    this.#storage.setItem('query', this.#query);
 
     const url =
-      this.query === ''
-        ? getPopularMovieRequestUrl(this.currentPage)
-        : getSearchMovieUrl(this.query, this.currentPage);
+      this.#query === ''
+        ? getPopularMovieRequestUrl(this.#currentPage)
+        : getSearchMovieUrl(this.#query, this.#currentPage);
     if (navigator.onLine) {
       await fetch(url)
         .then((res) => res.json())
         .then((data) => {
-          this.list.push(...data.results);
-          if (data['total_results'] === this.list.length) this.lastPage = true;
-          else this.lastPage = false;
+          this.#list.push(...data.results);
+          if (data['total_results'] === this.#list.length)
+            this.#lastPage = true;
+          else this.#lastPage = false;
         })
         .catch(() => alert('정보 요청에 실패했습니다.'));
     }
   }
 
   async searchMovieList(movieName: string) {
-    this.query = movieName;
-    this.currentPage = 1;
-    this.list = [];
+    this.#query = movieName;
+    this.#currentPage = 1;
+    this.#list = [];
     await this.fetchMovieList();
   }
 
   async getMoreMovieList() {
-    this.currentPage += 1;
+    this.#currentPage += 1;
     await this.fetchMovieList();
   }
 }
