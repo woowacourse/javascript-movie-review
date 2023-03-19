cf4966034a4df393b23d3c4ea9d9b4608125f428
Dain Lee <feb.dain@gmail.com>
2023-03-19 20:08:10 +0900

    style: css 수정 및 a 태그 삭제

--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -39,26 +39,24 @@ export default class MovieList extends HTMLElement {
   renderMovie(movie: Movie) {
     return `
     <li>
-      <a href="#">
-        <div class="item-card">
-          <img
-            class="item-thumbnail"
-            src="https://image.tmdb.org/t/p/w500/${movie.poster}"
-            onerror="
-              this.style.border='1px solid #e2e2e2';
-              this.src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg';
-            "
-            loading="lazy"
-            alt="${movie.title}"
-          />
-          <p class="item-title">${movie.title}</p>
-          <p class="item-score">
-            <img src="./assets/star_${
-              movie.ratings > 0 ? "filled" : "empty"
-            }.png" alt="별점" /> ${movie.ratings.toFixed(1)}
-          </p>
-        </div>
-      </a>
+      <div class="item-card">
+        <img
+          class="item-thumbnail"
+          src="https://image.tmdb.org/t/p/w500/${movie.poster}"
+          onerror="
+            this.style.border='1px solid #e2e2e2';
+            this.src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg';
+          "
+          loading="lazy"
+          alt="${movie.title}"
+        />
+        <p class="item-title">${movie.title}</p>
+        <p class="item-score">
+          <img src="./assets/star_${
+            movie.ratings > 0 ? "filled" : "empty"
+          }.png" alt="별점" /> ${movie.ratings.toFixed(1)}
+        </p>
+      </div>
     </li>
     `;
   }
--- src/components/Skeleton.ts
+++ src/components/Skeleton.ts
@@ -7,13 +7,11 @@ export default class Skeleton extends HTMLElement {
   render() {
     this.innerHTML = `
       <li class="movie-skeleton">
-        <a href="#">
-          <div class="item-card">
-            <div class="item-thumbnail skeleton"></div>
-            <div class="item-title skeleton"></div>
-            <div class="item-score skeleton"></div>
-          </div>
-        </a>
+        <div class="item-card">
+          <div class="item-thumbnail skeleton"></div>
+          <div class="item-title skeleton"></div>
+          <div class="item-score skeleton"></div>
+        </div>
       </li>
     `;
   }
