29290505bb29137f96182060bbc11ecdcfdf52de
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-20 00:45:11 +0900

    refactor(movieitem): 스켈레톤을 덮어씌우도록 랜더링 방식 변경
    - 탬플릿에서 클래스 형태의 컴포넌트로 변경

--- src/components/movieItem.ts
+++ src/components/movieItem.ts
@@ -1,25 +1,52 @@
 import { MovieType } from '../types';
+import { $, $$ } from '../utils/domSelector';
 
-const movieItem = ({ title, posterPath, voteAverage }: MovieType) => {
-  const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
-  const originalImageUrl = `${IMAGE_BASE_URL}${posterPath}`;
-  const alternativeImageUrl = '../../assets/no_image.png';
-  const imageUrl = posterPath ? originalImageUrl : alternativeImageUrl;
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
+const NO_IMAGE_URL = '../../assets/no_image.png';
 
-  return `
-    <li>
+type MovieItemConstructorType = {
+  skeleton: Element;
+  movieInfo: MovieType;
+};
+
+class MovieItem {
+  #skeleton: Element;
+  #item!: HTMLElement;
+
+  constructor({ skeleton, movieInfo }: MovieItemConstructorType) {
+    this.#skeleton = skeleton;
+    this.#createElement(movieInfo);
+    this.#replaceSkeletonWhenLoaded();
+  }
+
+  render() {
+    $('.item-list').appendChild(this.#item);
+  }
+
+  #createElement({ title, posterPath, voteAverage }: MovieType) {
+    const imageUrl = posterPath ? `${IMAGE_BASE_URL}${posterPath}` : NO_IMAGE_URL;
+
+    this.#item = document.createElement('li');
+    this.#item.innerHTML = `
       <a href="#">
         <div class="item-card">
           <img
             class="item-thumbnail"
             src="${imageUrl}"
-            loading="lazy"
-            alt="${title}" />
-          <p class="item-title">${title}</p>
+            alt="${title}"/>
+          <p class="item-title ">${title}</p>
           <p class="item-score"><img src="../../assets/star_filled.png" alt="별점" /> ${voteAverage}</p>
         </div>
-      </a>
-    </li>`;
-};
+      </a>`;
+  }
 
-export default movieItem;
+  #replaceSkeletonWhenLoaded() {
+    const $imageElement = $('img', this.#item);
+
+    $imageElement.addEventListener('load', () => {
+      $('.item-list').replaceChild(this.#item, this.#skeleton);
+    });
+  }
+}
+
+export default MovieItem;
