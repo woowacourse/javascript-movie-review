cd0a0d8f22c07085eb84b098f3ce90a99e32da98
dladncks1217 <dlaxodud1217@gmail.com>
2023-03-19 19:14:41 +0900

    refactor: hasOwnProperty -> hasOwn 메서드 사용으로 수정

--- src/util/EventBus.js
+++ src/util/EventBus.js
@@ -5,14 +5,14 @@ const notify = (event) => {
 };
 
 const EventBus = {
-  subscribe (eventName, callback) {
-    if (!Object.prototype.hasOwnProperty.call(eventManager, eventName)) {
+  subscribe(eventName, callback) {
+    if (!Object.hasOwn(eventManager, eventName)) {
       eventManager[eventName] = [];
       document.addEventListener(eventName, notify);
     }
 
     eventManager[eventName].push(callback);
-  }
+  },
 };
 
 export default EventBus;
