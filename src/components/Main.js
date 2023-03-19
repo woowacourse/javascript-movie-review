30c18b3c0c57fbe8de44eba3a3f13c6d226316dc
dladncks1217 <dlaxodud1217@gmail.com>
2023-03-19 15:11:52 +0900

    refactor: renderSkeleton 메서드 추상화

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ class Main {
   #element;
   #manager;
 
-  constructor (element, manager) {
+  constructor(element, manager) {
     this.#element = element;
     this.#manager = manager;
 
@@ -20,26 +20,27 @@ class Main {
     `;
   }
 
-  renderSkeleton () {
+  renderSkeleton(element) {
     const query = this.#manager.getQuery();
     const skeleton = new MovieSkeleton();
 
-    $('h2', this.#element).innerHTML = query === '' ? '지금 인기 있는 영화' : `"${query}" 검색 결과`;
-    $('ul', this.#element).appendChild(document.createElement('div'));
+    $('h2', element).innerHTML =
+      query === '' ? '지금 인기 있는 영화' : `"${query}" 검색 결과`;
+    $('ul', element).appendChild(document.createElement('div'));
 
-    const skeletonElement = $('ul', this.#element).lastElementChild;
+    const skeletonElement = $('ul', element).lastElementChild;
     skeletonElement.outerHTML = skeleton.template().repeat(SKELETON_ITEM_COUNT);
   }
 
-  async render () {
+  async render() {
     const movie = new Movie();
     const query = this.#manager.getQuery();
 
     if (query === '' && !this.#manager.getMovieList().length) {
-      this.renderSkeleton();
+      this.renderSkeleton(this.#element);
       await this.#manager.searchMovieList('');
     } else if (this.#manager.getCurrentPage() === 1) {
-      this.renderSkeleton();
+      this.renderSkeleton(this.#element);
       await this.#manager.searchMovieList(query);
     }
 
@@ -48,22 +49,25 @@ class Main {
     <ul class="item-list">
     ${
       this.#manager.getMovieList().length
-      ? this.#manager.getMovieList().map((movieInfo) => movie.template(movieInfo)).join('\n')
-      : '<p>검색 결과가 없습니다.</p>'
+        ? this.#manager
+            .getMovieList()
+            .map((movieInfo) => movie.template(movieInfo))
+            .join('\n')
+        : '<p>검색 결과가 없습니다.</p>'
     }
     </ul>
     ${
       this.#manager.isLastPage()
-      ? ''
-      : '<button class="btn primary full-width">더 보기</button>'
+        ? ''
+        : '<button class="btn primary full-width">더 보기</button>'
     }
     `;
   }
 
-  #requestMovieListEvent () {
+  #requestMovieListEvent() {
     this.#element.addEventListener('click', async (e) => {
       if (e.target.tagName === 'BUTTON') {
-        this.renderSkeleton();
+        this.renderSkeleton(this.#element);
         await this.#manager.getMoreMovieList();
         this.render();
       }
