de8a2b4f7d06fa89f78efe40710c517cba8b9015
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-20 00:46:46 +0900

    feat(loadmorebutton): 에러 메시지 표시 기능 추가, 버튼 비활성화/활성화 기능 추가

--- src/components/LoadMoreButton.ts
+++ src/components/LoadMoreButton.ts
@@ -1,19 +1,36 @@
 import { $ } from '../utils/domSelector';
 
 class LoadMoreButton {
-  #element = document.createElement('button');
+  #element = document.createElement('div');
+  #isDisabled = false;
 
   render(name: string) {
     const parentElement = $('.item-view');
 
-    this.#element.textContent = name;
-    this.#element.classList.add('btn', 'primary', 'full-width');
+    this.#element.classList.add('show-more-button-wrapper');
+    this.#element.innerHTML = `
+      <button class="btn primary full-width">${name}</button>
+      <span class="button-error-message"></span>`;
 
     parentElement.appendChild(this.#element);
   }
 
+  disableButtonWithErrorMessage(message: string) {
+    this.#isDisabled = true;
+    $('.btn', this.#element).setAttribute('disabled', '');
+    $('.button-error-message', this.#element).innerText = message;
+  }
+
+  enableButton() {
+    this.#isDisabled = false;
+    $('.btn', this.#element).removeAttribute('disabled');
+    $('.button-error-message', this.#element).innerText = '';
+  }
+
   addClickEventHandler(onClickLoadMoreButton: CallableFunction) {
     this.#element.addEventListener('click', () => {
+      if (this.#isDisabled) return;
+
       onClickLoadMoreButton();
     });
   }
