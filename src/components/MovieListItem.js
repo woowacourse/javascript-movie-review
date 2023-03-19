3cd9e72514d64074fd806c4f585cb15e2afc1941
bassyu <yrkdgus@gmail.com>
2023-03-19 22:09:33 +0900

    refactor(MovieListItem): render 함수 분리

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -4,25 +4,32 @@ import DefaultPoster from '../../image/default_poster.png';
 class MovieListItem extends HTMLElement {
   constructor() {
     super();
-    const imgSrc = this.getAttribute('poster-path');
+    this.render();
+  }
+
+  render() {
+    const posterPath = this.getAttribute('poster-path');
     this.innerHTML = /* html */ `
       <li>
         <a href="#">
           <div class="item-card">
             <img
-              class="item-thumbnail"
+              class="item-thumbnail skeleton"
               src=${
-                imgSrc === 'null'
+                posterPath === 'null'
                   ? DefaultPoster
-                  : `https://image.tmdb.org/t/p/w220_and_h330_face${imgSrc}`
+                  : `https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`
               }
               loading="lazy"
               alt="${this.getAttribute('title')}"
             />
-            <p class="item-title">${this.getAttribute('title')}</p>
-            <p class="item-score"><img src="${StarFilled}" alt="별점" /> ${this.getAttribute(
-      'vote-average'
-    )}</p>
+            <p class="item-title">
+              ${this.getAttribute('title')}
+            </p>
+            <p class="item-score">
+              <img src="${StarFilled}" alt="별점" /> 
+              ${this.getAttribute('vote-average')}
+            </p>
           </div>
         </a>
       </li>
