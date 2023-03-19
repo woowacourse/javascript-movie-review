11a3cf517a439eb2c54ea6670a30102444be93c7
nangkyeonglim <nangkyeonglim@gmail.com>
2023-03-19 23:04:18 +0900

    refactor: fetch의 default method 생략

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,9 +1,6 @@
 export const mostPopular = async (pageNumber: number) => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${pageNumber}`,
-    {
-      method: "GET",
-    }
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${pageNumber}`
   );
 
   if (response.ok) {
@@ -16,10 +13,7 @@ export const mostPopular = async (pageNumber: number) => {
 
 export const search = async (query: string, pageNumber: number) => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${query}&page=${pageNumber}&include_adult=false`,
-    {
-      method: "GET",
-    }
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${query}&page=${pageNumber}&include_adult=false`
   );
 
   if (response.ok) {
