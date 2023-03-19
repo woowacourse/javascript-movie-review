1acda7f04b9f04ffd9bf7bc48b4663f514d62963
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-19 09:10:35 +0900

    refactor: MovieCard 메서드 분리

--- src/components/MovieCard.ts
+++ src/components/MovieCard.ts
@@ -2,69 +2,75 @@ import NULL_IMAGE from '../const/nullImage';
 import { MovieItem } from '../domain/processMovieData';
 
 class MovieCard {
-  private _node!: HTMLElement;
-  private movieData: MovieItem;
+  readonly node: HTMLElement;
+  private thumbnail!: HTMLImageElement;
+  private thumbnailSkeleton!: HTMLDivElement;
 
-  constructor(movieDate: MovieItem) {
-    this.movieData = movieDate;
-    this.createTemplate();
-    this.setEvent();
+  private movieItemDetails: MovieItem;
+
+  constructor(movieItemDetails: MovieItem) {
+    // li 태그를 부모 태그로.
+    this.node = document.createElement('li');
+
+    // 상태 초기화
+    this.movieItemDetails = movieItemDetails;
+
+    // dom 생성 후 이벤트 추가
+    this.composeNode().setElements().addEvents();
   }
 
-  get node(): HTMLElement {
-    return this._node;
-  }
-
-  createTemplate(): this {
-    this._node = document.createElement('li');
-
-    console.log(this.movieData.posterPath);
-
-    this._node.innerHTML = `<a>
+  composeNode(): this {
+    this.node.innerHTML = `<a>
         <div class="item-card">
           <div class="item-thumbnail skeleton"></div>
           <img
             id="item-thumbnail"
             class="item-thumbnail hidden"
-            src="${this.movieData.posterPath}"
-            alt=${this.movieData.title}
+            src="${this.movieItemDetails.posterPath}"
+            alt=${this.movieItemDetails.title}
           />
-          <p class="item-title">${this.movieData.title}</p>
+          <p class="item-title">${this.movieItemDetails.title}</p>
           <div class="item-score">
             <img src="./star_filled.png" alt="별점" />
-            <p>${this.movieData.voteAverage}</p>
+            <p>${this.movieItemDetails.voteAverage}</p>
           </div>
         </div>
       </a>
     `;
+    return this;
+  }
+
+  setElements(): this {
+    const thumbnail = this.node.querySelector<HTMLImageElement>('#item-thumbnail');
+    const thumbnailSkeleton = this.node.querySelector<HTMLDivElement>('.skeleton');
+
+    // type guard
+    if (!thumbnail || !thumbnailSkeleton) {
+      return this;
+    }
+
+    this.thumbnail = thumbnail;
+    this.thumbnailSkeleton = thumbnailSkeleton;
 
     return this;
   }
 
-  updateMovie(props: MovieItem) {
-    this.movieData = props;
+  addEvents(): this {
+    this.thumbnail.addEventListener('load', this.#handleLoadImage.bind(this));
+    this.thumbnail.addEventListener('error', this.#handleNullImage.bind(this));
+
+    return this;
   }
 
-  completeLoadImage(thumbnail: HTMLImageElement, thumbnailSkeleton: HTMLDivElement) {
-    thumbnailSkeleton.remove();
-    thumbnail.classList.remove('hidden');
+  #handleLoadImage() {
+    if (!this.thumbnail.complete) return;
+
+    this.thumbnailSkeleton.remove();
+    this.thumbnail.classList.remove('hidden');
   }
 
-  setEvent() {
-    const thumbnail = this._node.querySelector<HTMLImageElement>('#item-thumbnail');
-    const thumbnailSkeleton = this._node.querySelector<HTMLDivElement>('.skeleton');
-
-    if (!thumbnail || !thumbnailSkeleton) return;
-
-    thumbnail.addEventListener('load', () => {
-      if (!thumbnail.complete) return;
-
-      this.completeLoadImage(thumbnail, thumbnailSkeleton);
-    });
-
-    thumbnail.addEventListener('error', () => {
-      thumbnail.src = NULL_IMAGE;
-    });
+  #handleNullImage() {
+    this.thumbnail.src = NULL_IMAGE;
   }
 }
 
