32305bce8a2e1a02ee0ef82f0f9ae607ee9615b0
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-19 21:18:17 +0900

    fix: 웹팩 설정을 통한 웹팩 배포 버그 수정

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -4,7 +4,7 @@ export default class Header extends HTMLElement {
   constructor() {
     super();
     this.render();
-    
+
     onSubmitSearchBox();
     onClickLogo();
   }
@@ -12,7 +12,7 @@ export default class Header extends HTMLElement {
   render() {
     this.innerHTML = `
     <header>
-      <h1><img id="logo" src="../assets/logo.png" alt="MovieList 로고" /></h1>
+      <h1><img id="logo" src="./assets/logo.png" alt="MovieList 로고" /></h1>
       <form id="search-bar" class="search-box">
         <input type="text" placeholder="검색" name="search-bar"/>
         <button class="search-button">검색</button>
