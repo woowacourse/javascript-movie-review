4bd5f9276c3733ece97660abc61025920876c343
nlom0218 <nlom0218@naver.com>
2023-03-19 14:35:35 +0900

    refactor: promt 창 지우기

--- src/domain/getAPI.ts
+++ src/domain/getAPI.ts
@@ -1,11 +1,9 @@
-const API_KEY = process.env.API_KEY ?? prompt('API KEY를 입력해주세요');
-
 const getAPI = {
   popularMovie: (page: number) => {
-    return `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`;
+    return `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`;
   },
   searchMovie: (keyword: string, page: number) =>
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ko-KR&query=${keyword}&page=${page}`,
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${keyword}&page=${page}`,
 };
 
 export default getAPI;
