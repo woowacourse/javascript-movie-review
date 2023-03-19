b241b090a1071e9a117a1ceea1bcdf78f3c12ffd
dladncks1217 <dlaxodud1217@gmail.com>
2023-03-19 19:39:05 +0900

    refactor: 네트워크 통신 검증 로직 추가

--- src/domain/MovieListManager.ts
+++ src/domain/MovieListManager.ts
@@ -1,16 +1,14 @@
-import { Movie } from "../type/movie";
-import Storage from "../type/Storage";
+import { Movie } from '../type/movie';
+import Storage from '../type/Storage';
 
-const getPopularMovieRequestUrl = (page = 1) => (
-  `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`
-);
+const getPopularMovieRequestUrl = (page = 1) =>
+  `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`;
 
-const getSearchMovieUrl = (query: string, page = 1) => (
-  `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${query}&page=${page}&include_adult=false`
-);
+const getSearchMovieUrl = (query: string, page = 1) =>
+  `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${query}&page=${page}&include_adult=false`;
 
 class MovieListManager {
-  private query: string = "";
+  private query: string = '';
   private list: Movie[] = [];
   private currentPage: number = 1;
   private lastPage = false;
@@ -21,13 +19,13 @@ class MovieListManager {
     this.query = this.storage.getItem('query');
   }
 
-  getCurrentPage(){
+  getCurrentPage() {
     return this.currentPage;
   }
 
   getMovieList() {
     return this.list.map((movie) => ({ ...movie }));
-  }   
+  }
 
   getQuery() {
     return this.query;
@@ -40,28 +38,30 @@ class MovieListManager {
   async fetchMovieList() {
     this.storage.setItem('query', this.query);
 
-    const url = this.query === "" 
-      ? getPopularMovieRequestUrl(this.currentPage)
-      : getSearchMovieUrl(this.query, this.currentPage);
-
-    await fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        this.list.push(...data.results);
-        if (data["total_results"] === this.list.length) this.lastPage = true;
-        else this.lastPage = false;
-      })
-      .catch(() => alert('정보 요청에 실패했습니다.'));
+    const url =
+      this.query === ''
+        ? getPopularMovieRequestUrl(this.currentPage)
+        : getSearchMovieUrl(this.query, this.currentPage);
+    if (navigator.onLine) {
+      await fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          this.list.push(...data.results);
+          if (data['total_results'] === this.list.length) this.lastPage = true;
+          else this.lastPage = false;
+        })
+        .catch(() => alert('정보 요청에 실패했습니다.'));
+    }
   }
 
-  async searchMovieList(movieName: string){
+  async searchMovieList(movieName: string) {
     this.query = movieName;
     this.currentPage = 1;
     this.list = [];
     await this.fetchMovieList();
   }
 
-  async getMoreMovieList () {
+  async getMoreMovieList() {
     this.currentPage += 1;
     await this.fetchMovieList();
   }
