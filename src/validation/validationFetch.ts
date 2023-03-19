1f877c456ac11ca52ebd9d309d67281cc0230d28
semnil5202 <semnil5202@gmail.com>
2023-03-20 04:12:10 +0900

    refactor: API Status 예외 처리 로직 분리

---
+++ src/validation/validationFetch.ts
@@ -0,0 +1,11 @@
+export default function validateStatus(status: number) {
+  if (status >= 500) {
+    window.alert('서버에서 영화 정보를 가져올 수 없습니다.');
+    return true;
+  } else if (status >= 400) {
+    window.alert('페이지 정보를 가져올 수 없습니다.');
+    return true;
+  }
+
+  return false;
+}
