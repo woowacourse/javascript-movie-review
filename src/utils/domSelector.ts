c329a1b5828e146d29161631551f6c46b253f21a
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-20 00:47:19 +0900

    refactor(domselector): HTMLElement을 항상 반환하도록 개선

--- src/utils/domSelector.ts
+++ src/utils/domSelector.ts
@@ -1,12 +1,21 @@
-const $ = <E extends Element>(selector: string): E => {
-  const $HTMLElement = document.querySelector(selector);
-  if (!$HTMLElement) throw new Error(`Dom Error`);
+const ERROR_ELEMENT_NOT_FOUND = '요소를 불러오는 데 실패했습니다. 페이지를 새로고침 해 주세요.';
 
-  return <E>$HTMLElement;
+const $ = (selector: string, target?: HTMLElement): HTMLElement => {
+  const $selectedElement = target
+    ? target.querySelector(selector)
+    : document.querySelector(selector);
+
+  if (!($selectedElement instanceof HTMLElement)) throw new Error(ERROR_ELEMENT_NOT_FOUND);
+
+  return $selectedElement;
 };
 
-const $$ = <E extends Element>(selector: string): NodeListOf<E> => {
-  return document.querySelectorAll(selector);
+const $$ = (selector: string, target?: HTMLElement): NodeListOf<Element> => {
+  const $selectedElements = target
+    ? target.querySelectorAll(selector)
+    : document.querySelectorAll(selector);
+
+  return $selectedElements;
 };
 
 export { $, $$ };
