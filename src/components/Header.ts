93f914877cfb6613403c170b9c3ecc9768e084c4
inyeong-kang <tomatorkd@naver.com>
2023-03-21 02:16:27 +0900

    feat: 홈 로고를 클릭하르면 메인 홈 주소로 이동하는 기능 구현

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,10 +1,17 @@
-import { Search } from './Search';
 import logo from '../assets/logo.png';
 
+import { $, Event } from '../utils/index';
+
+import { Search } from './Search';
+
 export function Header() {
+  Event.addEvent('click', '#home-logo', async () => {
+    window.location.href = '/';
+  });
+
   return `
     <header>
-        <h1><img src='${logo}' alt="MovieList 로고" /></h1>
+        <h1><img src='${logo}' alt="MovieList 로고" id="home-logo"/></h1>
         ${Search()}
     </header>
    `;
