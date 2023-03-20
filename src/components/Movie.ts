edff323431b71df0eaf9a7677f6b506f79b6c047
Gilpop8663 <wolfye@naver.com>
2023-03-20 20:09:36 +0900

    refactor: 평점이 소수점 1자리까지 나오게 구현

--- src/components/Movie.ts
+++ src/components/Movie.ts
@@ -7,6 +7,8 @@ class Movie extends HTMLElement {
   }
 
   render() {
+    const SLICE_START = 0;
+    const SLICE_END = 3;
     const imgUrl = this.getAttribute('imgUrl');
     const title = this.getAttribute('title');
     const score = this.getAttribute('score');
@@ -18,7 +20,7 @@ class Movie extends HTMLElement {
           <movie-image imgUrl="${imgUrl}" title="${title}" width="200"></movie-image>
           <p class="item-title">${title}</p>
           <p class="item-score">
-            ${score}
+            ${score?.slice(SLICE_START, SLICE_END)}
             <img src="${STAR_FILLED}" alt="별점" />
           </p>
         </div>
