00ffe778f3d504f642e332b7d76a758604096293
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-20 21:46:31 +0900

    remove: 사용하지 않는 유틸 제거

--- src/utils/handleError.ts
+++
@@ -1,9 +0,0 @@
-export const tryCatchWrapper = async (doFunction: () => void, handleError: (errorMessage: string) => void) => {
-  try {
-    await doFunction();
-  } catch (error) {
-    if (error instanceof Error) {
-      handleError(error.message);
-    }
-  }
-};
