4104ce4ea3bfd35d5f1fdfb703608419d42d7a7c
bassyu <yrkdgus@gmail.com>
2023-03-20 22:30:50 +0900

    feat(ErrorPage): 에러 메세지 수정

--- src/components/ErrorPage.ts
+++ src/components/ErrorPage.ts
@@ -7,7 +7,7 @@ class ErrorPage extends HTMLElement {
   render() {
     this.innerHTML = /* html */ `
       <section class="error-view">
-        <h1>페이지를 가져오다가 놓쳤어요 😢</h1>
+        <h1>페이지를 가져오지 못했어요 😢</h1>
         <p>네트워크 연결을 확인해주세요</p>
         <p>서버 문제일 수 있으니 나중에 다시 방문해주세요</p>
       </section>
