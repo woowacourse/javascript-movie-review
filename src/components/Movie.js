ea915040589a237c58e67f20843203e9ebb5adab
Q Kim <qoli0703@gmail.com>
2023-03-20 12:44:53 +0900

    refactor: 불필요한 <a> 태그 제거

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,18 +2,16 @@ class Movie {
   template ({ poster_path, title, vote_average }) {
     return `
     <li class="movie-info">
-      <a>
-        <div class="item-card">
-          <img
-            class="item-thumbnail"
-            src="https://image.tmdb.org/t/p/w500${poster_path}"
-            loading="lazy"
-            alt="${title}"
-          />
-          <p class="item-title">${title}</p>
-          <p class="item-score"><img src="./assets/star_filled.png" alt="별점" /> ${vote_average}</p>
-        </div>
-      </a>
+      <div class="item-card">
+        <img
+          class="item-thumbnail"
+          src="https://image.tmdb.org/t/p/w500${poster_path}"
+          loading="lazy"
+          alt="${title}"
+        />
+        <p class="item-title">${title}</p>
+        <p class="item-score"><img src="./assets/star_filled.png" alt="별점" /> ${vote_average}</p>
+      </div>
     </li>
      `;
   }
--- src/components/MovieSkeleton.js
+++ src/components/MovieSkeleton.js
@@ -2,13 +2,11 @@ class MovieSkeleton {
   template () {
     return `
     <li class="skeleton">
-      <a href="#">
-        <div class="item-card">
-          <div class="item-thumbnail skeleton"></div>
-          <div class="item-title skeleton"></div>
-          <div class="item-score skeleton"></div>
-        </div>
-      </a>
+      <div class="item-card">
+        <div class="item-thumbnail skeleton"></div>
+        <div class="item-title skeleton"></div>
+        <div class="item-score skeleton"></div>
+      </div>
     </li>
     `;
   }
