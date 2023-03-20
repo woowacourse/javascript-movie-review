830573c3011c8131c11e6cc662be3892c8d4ef53
chsua <csj1919@naver.com>
2023-03-20 20:52:03 +0900

    test: validation test 생성

--- src/CONSTANT.ts
+++ src/CONSTANT.ts
@@ -1,5 +1,5 @@
 export const ERROR_MESSAGE = {
-  MIN_INPUT_KEYWORD: '1자 이상으로 입력해주세요.',
+  MIN_INPUT_KEYWORD: '1자 이상 입력해주세요.',
   MAX_INPUT_KEYWORD: '10자 이하로 입력해주세요.',
   HTTP_400: '잘못된 요청입니다. 확인해주세요.😥',
   HTTP_500: '현재 페이지를 실행할 수 없습니다.\n 잠시후 다시 시도해주세요.😥',
