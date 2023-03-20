2e1d6e576b40c3521655bd4ed120e86a6405a952
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-21 02:25:30 +0900

    feat(type): API 타입 제거, 응답 타입 수정, 컴포넌트 타입 추가

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,17 +1,26 @@
-export interface MovieType {
+import Header from '../components/Header';
+import MovieList from '../components/MovieList';
+import MovieFetcher from '../domains/MovieFetcher';
+import LoadMoreButton from '../components/LoadMoreButton';
+
+type MovieType = {
   title: string;
   posterPath: string;
   voteAverage: number;
-}
+};
 
-export interface APIMovieType {
-  title: string;
-  poster_path: string;
-  vote_average: number;
-}
-
-export interface ResponseType {
+type MovieFetchResponseType = {
   result: string;
   status?: number;
-  movieList?: MovieType[];
-}
+  errorMessage?: string;
+  movies?: MovieType[];
+};
+
+type ViewBundleType = {
+  header: Header;
+  movieList: MovieList;
+  movieFetcher: MovieFetcher;
+  loadMoreButton: LoadMoreButton;
+};
+
+export { MovieType, MovieFetchResponseType, ViewBundleType };
