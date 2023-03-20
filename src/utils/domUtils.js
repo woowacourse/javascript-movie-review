531e77f777575ea22b519eb10ae4d658b1d8516f
Suyoung <jsy8492@gmail.com>
2023-03-20 14:41:13 +0900

    refactor: dom 유틸 함수 관련 파일을 TS로 마이그레이션

--- src/utils/domUtils.js
+++
@@ -1,6 +0,0 @@
-export const dispatchCustomEvent = ($target, eventType, data = null) => {
-  const customEvent = new CustomEvent(eventType, { bubbles: true, detail: data });
-  $target.dispatchEvent(customEvent);
-};
-
-export const $ = (selectors) => document.querySelector(selectors);
