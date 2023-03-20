08a2a34f54191a8adb606f782208de6d94bd89c0
minjaeKim <smallkdb@gmail.com>
2023-03-20 20:30:27 +0900

    refactor: api 유틸에 에러 처리 추가

--- src/api/getPopularMovies.ts
+++ src/api/getPopularMovies.ts
@@ -1,11 +1,16 @@
 import { BASE_URL } from '../constants';
+import { ERROR_MESSAGE } from '../constants/index';
 import type { MovieAPIResponse } from '../types/movie';
 
 const getPopularMovies = async (page: number = 1) => {
   const response = await fetch(`${BASE_URL}movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${page}`);
-  const data: MovieAPIResponse = await response.json();
 
-  return data;
+  if (response.ok) {
+    const data: MovieAPIResponse = await response?.json();
+    return data;
+  } else {
+    throw new Error(ERROR_MESSAGE.DATA_LOAD);
+  }
 };
 
 export default getPopularMovies;
