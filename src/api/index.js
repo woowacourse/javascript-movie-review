9990b9f8823c0a7a76d1a8454a05364ab0b72b92
bassyu <yrkdgus@gmail.com>
2023-03-20 17:25:23 +0900

    refactor(api): throw Error 삭제
    - ErrorPage 랜더링으로 에러 핸들링 통일

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,6 @@ export const getPopularMovies = async (page) => {
     `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`
   );
 
-  if (!response.ok) throw new Error('인기 영화 불러오기 실패');
-
   return response.json();
 };
 
@@ -15,8 +13,6 @@ export const getSearchMovies = async (query, page) => {
   const response = await fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&language=ko-KR&query=${query}&page=${page}`
   );
-
-  if (!response.ok) throw new Error('검색된 영화 불러오기 실패');
 
   return response.json();
 };
