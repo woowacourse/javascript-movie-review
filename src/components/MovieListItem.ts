80e2e8827333c820edf63b86f0b94e28e1b166ce
solo5star <solo_5@naver.com>
2023-03-20 14:45:53 +0900

    style: 잘못된 indent 수정

--- src/components/MovieListItem.ts
+++ src/components/MovieListItem.ts
@@ -1,19 +1,20 @@
 import { Movie } from '../movies.type';
 
 class MovieListItem {
-  private template = (movie: Movie) => `<li>
-    <a href="#">
-      <div class="item-card">
-        <img
-          class="item-thumbnail"
-          src="https://image.tmdb.org/t/p/w220_and_h330_face${movie.poster_path}"
-          alt="${movie.title}"
-        />
-        <p class="item-title">${movie.title}</p>
-        <p class="item-score"><img src="assets/star_filled.png" alt="별점" /> ${movie.vote_average}</p>
-      </div>
-    </a>
-  </li>`;
+  private template = (movie: Movie) => `
+    <li>
+      <a href="#">
+        <div class="item-card">
+          <img
+            class="item-thumbnail"
+            src="https://image.tmdb.org/t/p/w220_and_h330_face${movie.poster_path}"
+            alt="${movie.title}"
+          />
+          <p class="item-title">${movie.title}</p>
+          <p class="item-score"><img src="assets/star_filled.png" alt="별점" /> ${movie.vote_average}</p>
+        </div>
+      </a>
+    </li>`;
 
   render(movie: Movie) {
     return this.template(movie);
