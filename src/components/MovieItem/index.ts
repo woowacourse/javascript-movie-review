4411cda02302bf1acf029005309161e5ae2e11a7
TaeeunKim <xodms0309@naver.com>
2023-03-20 17:23:33 +0900

    feat: 각 이미지가 로딩되었을 때 skeleton이 사라지게 구현

--- src/components/MovieItem/index.ts
+++ src/components/MovieItem/index.ts
@@ -1,10 +1,12 @@
 import './index.css';
 import template from './index.html';
+import { $ } from '../../utils/dom';
 
 export class MovieItem extends HTMLElement {
   connectedCallback() {
     this.innerHTML = template;
     this.render();
+    this.skeletonHandler();
   }
 
   render() {
@@ -12,5 +14,18 @@ export class MovieItem extends HTMLElement {
       .replace('{poster_path}', this.getAttribute('poster')!)
       .replaceAll('{title}', this.getAttribute('title')!)
       .replace('{vote_average}', this.getAttribute('vote')!);
+  }
+
+  skeletonHandler() {
+    const image = $<HTMLImageElement>('img', this);
+    image.addEventListener('load', () => {
+      image.classList.remove('hidden');
+      this.removeSkeleton();
+    });
+  }
+
+  removeSkeleton() {
+    const skeleton = $<HTMLDivElement>('.skeleton', this);
+    skeleton.remove();
   }
 }
