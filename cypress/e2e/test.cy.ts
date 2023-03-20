40c2356a741f09112ec9106a44ae218d5901b7f3
yogjin <youngjin988@gmail.com>
2023-03-20 15:56:32 +0900

    test: 각 테스트 수행 전에 api intercept 하고 웹페이지 visit 하도록 수정, url regex 수정

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,26 +1,19 @@
-describe('E2E 테스트입니다.', () => {
-  it('초기에 영화(목 데이터) 20개를 렌더링하고, 더보기를 클릭하면 다음 페이지 20개를 불러온다.', () => {
+describe('E2E 테스트', () => {
+  beforeEach(() => {
     cy.intercept(
       {
         method: 'GET',
-        url: /^https:\/\/api.themoviedb.org\/3\/movie\/popular$/,
+        url: /^https:\/\/api.themoviedb.org\/3\/movie\/popular?.*/,
       },
       {
         fixture: 'movie-20.json',
       },
     ).as('getPopularMovies');
 
-    cy.visit('http://localhost:8080/');
-
-    cy.get('#js-more-movie-button').click();
-    cy.get('.item-card').should('have.length', 40);
-  });
-
-  it('검색을 하면 검색된 영화 목록을 불러오고, 더 이상 불러올 영화가 없으면 더보기 버튼이 사라진다.', () => {
     cy.intercept(
       {
         method: 'GET',
-        url: /^https:\/\/api.themoviedb.org\/3\/search\/movie$/,
+        url: /^https:\/\/api.themoviedb.org\/3\/search\/movie?.*/,
       },
       {
         fixture: 'movie-20.json',
@@ -28,21 +21,34 @@ describe('E2E 테스트입니다.', () => {
     ).as('searchMovies');
 
     cy.visit('http://localhost:8080/');
+  });
 
+  it('첫 화면에서 영화(목 데이터) 20개를 렌더링한다.', () => {
+    cy.get('.item-card').should('have.length', 20);
+  });
+
+  it('더보기를 클릭하면 다음 페이지 20개를 렌더링한다.', () => {
+    cy.get('#js-more-movie-button').click();
+    cy.get('.item-card').should('have.length', 40);
+  });
+
+  it('검색을 하면 검색된 영화 목록 20개를 렌더링한다.', () => {
     cy.get('#js-search-input').type('검색 키워드');
     cy.get('.search-button').click();
 
     cy.get('.item-card').should('have.length', 20);
+  });
 
+  it('더 이상 불러올 영화가 없으면 더보기 버튼이 사라진다.', () => {
     cy.intercept(
       {
         method: 'GET',
-        url: /^https:\/\/api.themoviedb.org\/3\/search\/movie$/,
+        url: /^https:\/\/api.themoviedb.org\/3\/movie\/popular\?.*/,
       },
       {
         fixture: 'movie-10.json',
       },
-    ).as('searchMovies');
+    ).as('getPopularMovies');
 
     cy.get('#js-more-movie-button').click();
     cy.get('.item-card').should('have.length', 30);
