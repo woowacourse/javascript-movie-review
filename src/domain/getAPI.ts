e5df899b6a5f4612ba9072f5afba781782cfa15f
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-20 20:38:33 +0900

    refactor: 사용하지 않는 파일 삭제

--- src/domain/getAPI.ts
+++
@@ -1,9 +0,0 @@
-const getAPI = {
-  popularMovie: (page: number) => {
-    return `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KRS&page=${page}`;
-  },
-  searchMovie: (keyword: string, page: number) =>
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${keyword}&page=${page}`,
-};
-
-export default getAPI;
