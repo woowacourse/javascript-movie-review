c4575d35470dc0451573e1c5cc9229d4069be283
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-20 20:46:47 +0900

    refactor: Skeleton 상수화 분리

--- src/constant/variables.ts
+++ src/constant/variables.ts
@@ -2,3 +2,15 @@ export const LIST_STATE = {
   POPULAR: "popular",
   SEARCHED: "searched",
 };
+
+export const SKELETON = {
+  LIST: ` <li>
+           <a href="#">
+             <div class="item-card">
+             <div class="item-thumbnail skeleton"></div>
+             <div class="item-title skeleton"></div>
+             <div class="item-score skeleton"></div>
+             </div>
+           </a>
+         </li>`,
+};
