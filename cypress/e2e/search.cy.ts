2aac31b5572ed9e283e002be9bfc26f81e3e4e7a
cruelladevil <dev.timetravel@gmail.com>
2023-03-20 17:00:42 +0900

    test: e2e 테스트 분리

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,43 @@
+describe('영화 검색에 대한 테스트입니다.', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api.themoviedb.org\/3\/search\/movie\?.*/,
+      },
+      {
+        fixture: 'movie-20.json',
+      },
+    ).as('searchMovies');
+
+    cy.visit('http://localhost:8080/');
+
+    cy.get('#js-search-input').type('검색 키워드');
+    cy.get('.search-button').click();
+  });
+
+  it('검색을 하면 검색된 영화 목록을 불러온다.', () => {
+    cy.get('.item-card').should('have.length', 20);
+  });
+
+  it('더보기를 클릭하면 다음 페이지의 영화 20개를 불러온다.', () => {
+    cy.get('#js-more-movie-button').click();
+    cy.get('.item-card').should('have.length', 40);
+  });
+
+  it('더 이상 불러올 영화가 없으면 더보기 버튼이 사라진다.', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api.themoviedb.org\/3\/search\/movie\?.*/,
+      },
+      {
+        fixture: 'movie-10.json',
+      },
+    ).as('searchMovies');
+
+    cy.get('#js-more-movie-button').click();
+    cy.get('.item-card').should('have.length', 30);
+    cy.get('#js-more-movie-button').should('not.be.visible');
+  });
+});
