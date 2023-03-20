d8c2792c04df069dc4d031c32648bf7a5c2212f8
solo5star <solo_5@naver.com>
2023-03-20 15:22:04 +0900

    feat: `client` get, post, put 지원

--- src/client.ts
+++ src/client.ts
@@ -17,22 +17,32 @@ class Client {
 
   private static readonly BASE_URL = 'https://api.themoviedb.org/3';
 
-  async fetch(path: string): Promise<TMDBResponse> {
-    const res = await fetch(`${Client.BASE_URL}${path}&api_key=${Client.API_KEY}`);
+  async fetch(path: string, init?: RequestInit): Promise<TMDBResponse> {
+    const res = await fetch(`${Client.BASE_URL}${path}&api_key=${Client.API_KEY}`, init);
     if (!res.ok) throw await res.json();
 
     const response: TMDBResponse = await res.json();
     return response;
   }
 
-  async getPopularMovies(page = 1): Promise<TMDBResponse> {
-    return this.fetch(`/movie/popular?language=ko-KR&page=${page}`);
+  get(path: string, init?: Omit<RequestInit, 'method'>) {
+    return this.fetch(path, { method: 'GET', ...init });
   }
 
-  async getSearchMovies(query: string, page = 1): Promise<TMDBResponse> {
-    return this.fetch(
-      `/search/movie?language=ko-KR&query=${query}&page=${page}&include_adult=false`,
-    );
+  post(path: string, init?: Omit<RequestInit, 'method'>) {
+    return this.fetch(path, { method: 'POST', ...init });
+  }
+
+  put(path: string, init?: Omit<RequestInit, 'method'>) {
+    return this.fetch(path, { method: 'PUT', ...init });
+  }
+
+  getPopularMovies(page = 1): Promise<TMDBResponse> {
+    return this.get(`/movie/popular?language=ko-KR&page=${page}`);
+  }
+
+  getSearchMovies(query: string, page = 1): Promise<TMDBResponse> {
+    return this.get(`/search/movie?language=ko-KR&query=${query}&page=${page}&include_adult=false`);
   }
 }
 
