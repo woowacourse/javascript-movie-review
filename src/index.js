fd08eb35ce723fdf1dd0bd6dce5565da3c27ba7e
bassyu <yrkdgus@gmail.com>
2023-03-20 17:10:04 +0900

    feat(index.js): ErrorPage, SkeletonListItem 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import './css/app.css';
 import './css/util.css';
 
 import MovieHeader from './components/MovieHeader';
-import MovieListSection from './components/MovieListSection';
+import MoviePage from './components/MoviePage';
 import MovieListItem from './components/MovieListItem';
+import SkeletonListItem from './components/SkeletonListItem';
+import ErrorPage from './components/ErrorPage';
+
 import App from './App';
 
 customElements.define('movie-header', MovieHeader);
-customElements.define('movie-list-section', MovieListSection);
+customElements.define('movie-page', MoviePage);
 customElements.define('movie-list-item', MovieListItem);
+customElements.define('skeleton-list-item', SkeletonListItem);
+customElements.define('error-page', ErrorPage);
+
 App.init();
