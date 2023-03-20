736e593deea0c1c25802ce926f061bace330849f
inyeong-kang <tomatorkd@naver.com>
2023-03-21 01:53:56 +0900

    refactor: 컴포넌트 이름 변경 PageHeader > PageTitle

--- src/components/MainPage.ts
+++ src/components/MainPage.ts
@@ -1,11 +1,11 @@
 import { MovieList } from './MovieList';
-import { PageHeader } from './PageHeader';
+import { PageTitle } from './PageTitle';
 import { ViewMoreButton } from './ViewMoreButton';
 
 export function MainPage() {
   return `
     <section class="item-view">
-        ${PageHeader()}
+        ${PageTitle()}
         ${MovieList()}
         ${ViewMoreButton()}
     </section>
