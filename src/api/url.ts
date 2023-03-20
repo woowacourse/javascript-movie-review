99fde4cd7779bcd5ea95fb9596c9ed0e0bbb812a
nangkyeonglim <nangkyeonglim@gmail.com>
2023-03-20 15:18:08 +0900

    refactor: API URL 분리

---
+++ src/api/url.ts
@@ -0,0 +1,6 @@
+const API_KEY = process.env.API_KEY;
+
+export const URL = {
+  GET_POPULAR_MOVIES: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR`,
+  GET_SEARCH_MOVIES: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ko-KR&include_adult=false`,
+};
