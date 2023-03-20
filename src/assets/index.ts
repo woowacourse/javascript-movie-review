526c545e24846ba9997ac69e991ed585aaccc881
ashleysyheo <ashleysyheo@gmail.com>
2023-03-20 23:13:35 +0900

    feat: close button 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,6 @@ import Logo from './logo.png';
 import SearchButton from './search_button.png';
 import EmptyStar from './star_empty.png';
 import FilledStar from './star_filled.png';
+import CloseButton from './close_button.svg';
 
-export { Logo, SearchButton, EmptyStar, FilledStar };
+export { Logo, SearchButton, EmptyStar, FilledStar, CloseButton };
