041c80a4b9e5dd97ae5e82e851b499ddd9897e8a
NaveOWO <pianowell96@gmail.com>
2023-03-21 17:26:49 +0900

    [ test ] 테스트명 수정

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,57 +1,55 @@
-import { getPopularMovie } from "../../src/api/movieList";
-
-describe("template spec", () => {
+describe("첫 화면에 위치한 elements 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:8080/");
   });
 
-  it("display", () => {
+  it("첫 화면이 로드되면 20개의 데이터가 존재한다.", () => {
     cy.get(".item-card").should("have.length", 20);
   });
 
-  it("moreButton", () => {
+  it("데이터 20개가 있고, 더보기버튼을 눌렀을 때 데이터의 개수는 40개가 된다.", () => {
     cy.get(".item-card").should("have.length", 20);
     cy.get(".primary").click();
     cy.get(".item-card").should("have.length", 40);
   });
 
-  it("redering test", () => {
+  it("Porter라는 키워드를 입력했을 때 해당 데이터 20개가 보여진다.t", () => {
     cy.get(".search-input").type("porter");
     cy.get(".search-input").trigger("keyup", { keyCode: 13 });
     cy.get(".item-card").should("have.length", 20);
   });
 });
 
-describe("api test", () => {
-  it("popular movie api", () => {
+describe("api 통신 테스트", () => {
+  it("popular movie를 불러오는 함수를 실행하면 해당 경로의 데이터가 20개씩 불러와진다.", () => {
     cy.intercept(
       {
         method: "GET",
         url: /^https:\/\/api.themoviedb.org\/3\/movie\/popular*/,
       },
       { fixture: "movie-popular.json" }
-    ).as("getPopularMovies");
+    ).as("getMovieData");
 
     cy.visit("http://localhost:8080/");
 
-    cy.wait("@getPopularMovies").then((interception) => {
+    cy.wait("@getMovieData").then((interception) => {
       const movieItems = interception.response?.body.results;
       expect(movieItems.length).to.equal(20);
     });
   });
 
-  it("search movie api", () => {
+  it("search movie를 불러오는 함수를 실행하면 해당 데이터가 20개 불러와진다.", () => {
     cy.intercept(
       {
         method: "GET",
         url: /^https:\/\/api.themoviedb.org\/3\/movie\/popular*/,
       },
       { fixture: "movie-popular.json" }
-    ).as("getPopularMovies");
+    ).as("getMovieData");
 
     cy.visit("http://localhost:8080/");
 
-    cy.wait("@getPopularMovies").then((interception) => {
+    cy.wait("@getMovieData").then((interception) => {
       const movieItems = interception.response?.body.results;
       expect(movieItems.length).to.equal(20);
     });
