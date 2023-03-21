b57ea9bfb781645fbbb89ff939c97859bf78eaab
n0eyes <doll4586633@naver.com>
2023-03-21 14:22:14 +0900

    feat: 검색 결과가 없는 경우 사용성 개선
    - 더 보기 버튼 제거
    - 검색 결과가 없습니다 문구 노출

--- src/components/MovieChart/index.ts
+++ src/components/MovieChart/index.ts
@@ -2,12 +2,12 @@ import { assemble, Event, useEffect } from '../../core';
 import { $, getElement } from './../../utils/common/domHelper';
 import { MovieListComponent, SkeletonMovieListComponent } from './action';
 import { useMovieChart } from '../../hooks/useMovieChart';
-import { LAST_PAGE } from '../../constants';
+import { LAST_PAGE, NO_RESULT } from '../../constants';
 
 interface MovieChart {
   keyword: string;
 }
-let page: number;
+
 const MovieChart = assemble<MovieChart>(({ keyword }) => {
   const { chartInfo, movieList, isLoading, fetchMore } = useMovieChart(keyword);
 
@@ -19,17 +19,25 @@ const MovieChart = assemble<MovieChart>(({ keyword }) => {
       },
     },
   ];
-
   const $template = getElement(`
     <main>
       <section class="item-view">
         <h2>지금 인기 있는 영화</h2>
-        <fragment id='MovieList'>
-          ${chartInfo ? MovieListComponent({ movieList }) : ''}
-        </fragment>
-        <fragment id='SkeletonList'>
-          ${isLoading ? SkeletonMovieListComponent() : ''}
-        </fragment>
+        <div class="item-list-layout">
+          ${
+            isLoading
+              ? `
+              <fragment id='SkeletonList'>
+                ${SkeletonMovieListComponent()}
+              </fragment>
+            `
+              : `
+              <fragment id='MovieList'>
+                ${movieList.length ? MovieListComponent({ movieList }) : NO_RESULT}
+              </fragment>
+            `
+          }
+        </div>
         ${
           chartInfo?.page !== LAST_PAGE
             ? `<button class="btn primary full-width">
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,5 @@ export const PAGE = 1;
 export const LAST_PAGE = 500;
 export const PAGINATION = 1;
 export const DUMMY_AMOUNT = 20;
+
+export const NO_RESULT = '검색 결과가 없습니다';
