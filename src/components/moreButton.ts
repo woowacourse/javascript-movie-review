17b834f7140afe247499f127e1419bd34a23ad0f
NaveOWO <pianowell96@gmail.com>
2023-03-21 13:49:44 +0900

    [ feat ] 클래스로 변경 & 메서드 추가

--- src/components/moreButton.ts
+++ src/components/moreButton.ts
@@ -3,3 +3,40 @@ export const moreButton = () => {
     <button class="btn primary full-width">더 보기</button>
     `;
 };
+
+export class MoreButton {
+  constructor() {
+    this.render();
+    this.hadleEvent();
+  }
+
+  create() {
+    return `
+    <button class="btn primary full-width">더 보기</button>
+    `;
+  }
+
+  render() {
+    document
+      .querySelector("main")
+      ?.insertAdjacentHTML("afterend", this.create());
+  }
+
+  hadleEvent() {
+    const moreButton = document.querySelector(".primary");
+    moreButton?.addEventListener("click", this.onClick);
+  }
+
+  onClick() {
+    const event = new CustomEvent("clickMoreButton");
+    document.querySelector(".primary")?.dispatchEvent(event);
+  }
+
+  hide() {
+    document.querySelector(".primary")?.classList.add("hidden");
+  }
+
+  show() {
+    document.querySelector(".primary")?.classList.remove("hidden");
+  }
+}
