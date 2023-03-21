48dad9a06c178be48d39f7bda658a5fbff1007d9
chex <chex1004@gmail.com>
2023-03-21 09:43:20 +0900

    refactor: 검색창 input type을 search로 변경

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -7,7 +7,7 @@ class Header {
     const template = `      
       <h1><a href="./"><img src="./assets/logo.png" alt="MovieList 로고" /></a></h1>
       <form class="search-box" id="search-form">
-        <input type="text" id="search-input" name="keyword" placeholder="Search" />
+        <input type="search" id="search-input" name="keyword" placeholder="Search" />
         <button type="submit" class="search-button" id="search-button">검색</button>
       </form>`;
 
