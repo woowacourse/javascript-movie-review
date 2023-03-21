c02237f193a055f8c2ed85b5a49a448e06149c32
semnil5202 <semnil5202@gmail.com>
2023-03-21 15:03:57 +0900

    refactor: 오류 메세지 상수화

--- src/validation/alertFetchStatus.ts
+++ src/validation/alertFetchStatus.ts
@@ -1,9 +1,11 @@
+import { ERROR_MESSAGE } from '../constant/index';
+
 export default function alertFetchStatus(status: number) {
   if (status >= 500) {
-    window.alert('서버에서 영화 정보를 가져올 수 없습니다.');
+    window.alert(ERROR_MESSAGE.noResponse);
     return true;
   } else if (status >= 400) {
-    window.alert('페이지 정보를 가져올 수 없습니다.');
+    window.alert(ERROR_MESSAGE.wrongRequset);
     return true;
   }
 
