a9e9a4b54efc0d8b2a27ef0cb4ff113a33dae54d
NaveOWO <pianowell96@gmail.com>
2023-03-21 13:49:27 +0900

    [ style ] 변수명 변경

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -1,7 +1,7 @@
-import { CoreInfoType } from "../@types/movieType";
+import { CoreInfoType, MovieInfoType } from "../@types/movieType";
 
-export const MovieItem = (movieInfo: CoreInfoType) => {
-  const { title, posterPath, voteAverage } = movieInfo;
+export const MovieItem = (movieInfo: MovieInfoType) => {
+  const { title, poster_path, vote_average } = movieInfo;
 
   return `
 <li>
@@ -9,12 +9,12 @@ export const MovieItem = (movieInfo: CoreInfoType) => {
         <div class="item-card">
           <img
             class="item-thumbnail skeleton "
-            src="https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}"
+            src="https://image.tmdb.org/t/p/w220_and_h330_face${poster_path}"
             loading="lazy"
             alt="앤트맨과 와스프: 퀀텀매니아"
           />
           <p class="item-title ">${title}</p>
-          <p class="item-score "><img src="./star_filled.png" alt="별점" /> ${voteAverage}</p>
+          <p class="item-score "><img src="./star_filled.png" alt="별점" /> ${vote_average}</p>
         </div>
       </a>
 </li>
