468b562b39c03e4176230573a91297b6e3a5f502
afds4567 <afds4567@gmail.com>
2023-03-21 15:59:08 +0900

    [ style ] 이미지 없는 경우 대체 이미지 출력 기능

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -3,20 +3,28 @@ import { CoreInfoType } from "../@types/movieType";
 export const MovieItem = (movieInfo: CoreInfoType) => {
   const { title, posterPath, voteAverage } = movieInfo;
 
+  const thumbnail = posterPath
+    ? `
+    <img
+      class="item-thumbnail skeleton"
+      src="https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}"
+      loading="lazy"
+      alt="${title}"
+    />`
+    : `
+    <div class="item-thumbnail empty">
+      <span>No Image</span>
+    </div>`;
+
   return `
-<li>
+    <li>
       <a href="#">
         <div class="item-card">
-          <img
-            class="item-thumbnail skeleton "
-            src="https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}"
-            loading="lazy"
-            alt="${title}"
-          />
+          ${thumbnail}
           <p class="item-title ">${title}</p>
           <p class="item-score "><img src="./star_filled.png" alt="별점" /> ${voteAverage}</p>
         </div>
       </a>
-</li>
-    `;
+    </li>
+  `;
 };
