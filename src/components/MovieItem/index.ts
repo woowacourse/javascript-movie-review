80680163a74f2c553fa38fb26d1df04df15d4083
Patrick <kuyt3122@naver.com>
2023-03-21 14:20:00 +0900

    refactor: skeleton 동적으로 가져오도록 수정

--- src/components/MovieItem/index.ts
+++ src/components/MovieItem/index.ts
@@ -4,6 +4,7 @@ export class MovieItem extends HTMLElement {
   connectedCallback() {
     this.innerHTML = template;
     this.#render();
+    this.imageSkeletonHandler();
   }
 
   #render() {
@@ -11,5 +12,15 @@ export class MovieItem extends HTMLElement {
       .replace('{poster_path}', this.getAttribute('poster') ?? '알 수 없음')
       .replaceAll('{title}', this.getAttribute('title') ?? '알 수 없음')
       .replace('{vote_average}', this.getAttribute('vote') ?? '알 수 없음');
+  }
+
+  imageSkeletonHandler() {
+    const a = this.querySelector('img');
+    const b = this.querySelector('.skeleton');
+
+    a?.addEventListener('load', () => {
+      a.classList.remove('hidden');
+      b?.remove();
+    });
   }
 }
