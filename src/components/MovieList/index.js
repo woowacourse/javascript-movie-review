51c1db2b3e48eef19d48baf9cb9c82f5bc57182d
sh981013s <sh981013s@gmail.com>
2023-03-21 19:37:52 +0900

    fix: rebase conflict resolved

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,12 +1,13 @@
 import "./index.css";
 import MovieItem from "./MovieItem";
+import SkeletonList from "./SkeletonList";
 
 const MovieList = () => {
   return `
         <section class="item-view">
           <h2>지금 인기 있는 영화</h2>
           <ul class="item-list">
-            ${Array.from({ length: 20 }, () => MovieItem()).join("")}
+          ${Array.from({ length: 20 }, () => MovieItem()).join("")}
           </ul>
           <button class="btn primary full-width">더 보기</button>
         </section>
