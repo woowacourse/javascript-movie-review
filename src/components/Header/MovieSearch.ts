06ac4db095fb8612856fce84b5e324dc05405a91
Leejin-Yang <ejin1996@gmail.com>
2023-03-21 13:39:16 +0900

    refactor: 영화 검색 input 타입 text -> search

--- src/components/Header/MovieSearch.ts
+++ src/components/Header/MovieSearch.ts
@@ -9,7 +9,7 @@ const MovieSearch = {
     return `
       <div class="search-box">
         <form id=${ID.MOVIE_SEARCH_FORM}>
-          <input type="text" name="search-query" placeholder="검색" />
+          <input type="search" name="search-query" placeholder="검색" />
           <button class="search-button">검색</button>
         </form>
       </div>
