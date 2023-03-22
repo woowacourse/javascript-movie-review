1ab29922a073cbe5c609f7de2802fe9a89bfde09
Leejin-Yang <ejin1996@gmail.com>
2023-03-23 01:37:16 +0900

    refactor: dom 유틸 함수 에러 메세지 수정

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -2,7 +2,7 @@ export const $ = <T extends Element = Element>(selector: string) => {
   const element = document.querySelector<T>(selector);
 
   if (element === null) {
-    throw new Error('element not found');
+    throw new Error(`${selector} not found`);
   }
 
   return element;
