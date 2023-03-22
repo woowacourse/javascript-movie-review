894d86b0bbc32d426ac1a0e21b45762110b99f65
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-22 15:48:09 +0900

    refactor: 에러 메시지 추가

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -9,7 +9,11 @@ export const getPopularMovies = async (page: number) => {
   );
 
   if (response.ok) return response.json();
-  alert("오류 발생");
+  else {
+    const errorMessage = `인기 영화 가져오기, 페이지: ${page}\n\n서버에서 오류가 발생했습니다.`;
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
 };
 
 export const getSearchedMovies = async (movieName: string, page: number) => {
@@ -21,5 +25,9 @@ export const getSearchedMovies = async (movieName: string, page: number) => {
   );
 
   if (response.ok) return response.json();
-  alert("오류 발생");
+  else {
+    const errorMessage = `검색어: ${movieName}, 페이지: ${page}\n\n서버에서 오류가 발생했습니다.`;
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
 };
