059674287e52716c3c028edbc58e5d5e64b773b9
turtle601 <turtle601@naver.com>
2023-03-22 10:39:52 +0900

    refactor(Component): useState 관련 로직 변경

--- src/components/core/Component.ts
+++ src/components/core/Component.ts
@@ -1,45 +1,20 @@
 // store
-import { useObserverState } from '../atoms/useObserverState';
-import { StateType } from '../atoms/Store';
 import { store } from '../atoms/Observer';
 
-// components
-import Header from '../Header';
-import Title from '../Title';
-import SeeMore from '../SeeMore';
-import MovieListContainer from '../MovieList/MovieListContainer';
-
-type ComponentType = {
-  header?: Header;
-  movieListContainer?: MovieListContainer;
-  title?: Title;
-  seeMore?: SeeMore;
-};
+// hook
+import { useStateHook } from '../hooks/useState';
 
 export default abstract class Component {
-  component: ComponentType = {};
+  state;
+
+  constructor() {
+    this.state = this.useState();
+  }
 
   useState() {
     const proxyState = store(this.render.bind(this));
-    const stateKey: (keyof StateType)[] = [
-      'movies',
-      'popularPage',
-      'searchPage',
-      'isSearched',
-      'isEnd',
-      'query',
-      'isLoading',
-    ];
-    stateKey.forEach((value) => useObserverState(proxyState).getValue(value));
-    return useObserverState(proxyState);
+    return useStateHook(proxyState);
   }
 
-  setComponentInstance<T extends keyof ComponentType>(
-    key: T,
-    instance: ComponentType[T]
-  ) {
-    this.component[key] = instance;
-  }
-
-  abstract render(): void;
+  render() {}
 }
