2ef2df12a71c37e88af274e1deb9519b4ffcfefc
semnil5202 <semnil5202@gmail.com>
2023-03-23 01:16:43 +0900

    refactor: 변수명 수정 (url -> popularUrl)

--- src/domain/api/index.ts
+++ src/domain/api/index.ts
@@ -1,14 +1,13 @@
 import { FetchingMovieType } from '../../type/movie';
 
 const API_KEY = process.env.API_KEY;
-
-const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`;
+const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`;
 const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US`;
 
 export const getApiPopularMovie = async (
   page: number
 ): Promise<FetchingMovieType> => {
-  const fetchingData = await fetch(`${url}&page=${page}`);
+  const fetchingData = await fetch(`${popularUrl}&page=${page}`);
 
   const status = fetchingData.status;
   const movieList = await fetchingData.json();
