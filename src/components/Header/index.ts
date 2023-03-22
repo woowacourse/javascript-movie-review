b3b9fcf934afdeb22a2395f3d7fdcb987763288e
Leejin-Yang <ejin1996@gmail.com>
2023-03-22 11:26:08 +0900

    refactor: 페이지 제목 button -> a 태그로 수정

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,20 +1,19 @@
 import './Header.style.css';
+
 import MovieSearch from './MovieSearch';
 import { logoImage } from '../../assets/images';
 import type Movies from '../../domain/Movies';
-import MovieCardSection from '../MovieCardSection';
-import MovieCardList from '../MovieCardSection/MovieCardList';
-import LoadMoreButton from '../MovieCardSection/LoadMoreButton';
-import { CLASS } from '../../constants/selector';
 import Tooltip from '../common/Tooltip';
-import { isCustomErrorMessage } from '../../constants/message';
-import { $ } from '../../utils/dom';
 
 const Header = {
   template() {
     return `
       <header>
-        <h1><button type="button" class=${CLASS.PAGE_TITLE_BUTTON}><img src=${logoImage} alt="MovieList 로고" /></button></h1>
+        <h1>
+          <a href="./">
+            <img src=${logoImage} alt="MovieList 로고" />
+          </a>
+        </h1>
         ${MovieSearch.template()}
         <div class="search-tooltip hide">
           ${Tooltip.template('올바른 검색값을 입력해주세요.')}
@@ -25,23 +24,6 @@ const Header = {
 
   setEvent(movies: Movies) {
     MovieSearch.setEvent(movies);
-
-    const pageTitleButton = $<HTMLButtonElement>(`.${CLASS.PAGE_TITLE_BUTTON}`);
-
-    pageTitleButton.addEventListener('click', async () => {
-      MovieCardSection.render();
-
-      try {
-        const results = await movies.init();
-
-        MovieCardList.paint(results);
-        LoadMoreButton.handleVisibility(movies.isLastPage());
-      } catch (error) {
-        if (isCustomErrorMessage(error)) {
-          MovieCardSection.renderErrorMessage(error);
-        }
-      }
-    });
   },
 };
 
