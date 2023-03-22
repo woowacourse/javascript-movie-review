77cdac73a43d0d1555ed1971ba8a389caac733de
turtle601 <turtle601@naver.com>
2023-03-22 10:46:16 +0900

    refactor(App): 컴포넌트 인스턴스 생성 후 렌더링

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,19 +1,33 @@
 // components
 import Header from './Header';
 import Title from './Title';
+import MoviePopularList from './MovieList/MoviePopularList';
+import MovieSearchList from './MovieList/MovieSearchList';
 import MovieListContainer from './MovieList/MovieListContainer';
 import SeeMore from './SeeMore';
-import Component from './core/Component';
+import Skeleton from './MovieList/Skeleton';
 
 import { $ } from '../utils/domHelper';
 
-export default class App extends Component {
+type ComponentType = {
+  header?: Header;
+  movieListContainer?: MovieListContainer;
+  popularMovieList?: MoviePopularList;
+  searchMovieList?: MovieSearchList;
+  title?: Title;
+  seeMore?: SeeMore;
+  skeleton?: Skeleton;
+};
+
+export default class App {
   $target;
 
-  constructor($target: HTMLElement) {
-    super();
+  components: ComponentType;
 
+  constructor($target: HTMLElement) {
     this.$target = $target;
+
+    this.components = {};
   }
 
   template() {
@@ -23,6 +37,7 @@ export default class App extends Component {
         <section class="item-view">
         <h2 class="movie-list-title"></h2>
         <ul class="item-list movie-container"></ul>
+        <ul class="item-list skeleton-container"></ul>
         <button class="btn primary full-width"></button>
         </section>
       </main>
@@ -35,22 +50,47 @@ export default class App extends Component {
   }
 
   makeComponent() {
-    this.setComponentInstance('header', new Header($('header')));
-    this.setComponentInstance(
-      'movieListContainer',
-      new MovieListContainer($('.item-list'))
+    this.components.title = new Title($('.movie-list-title'));
+
+    this.components.popularMovieList = new MoviePopularList(
+      $('.movie-container')
     );
-    this.setComponentInstance('title', new Title($('.movie-list-title')));
-    this.setComponentInstance('seeMore', new SeeMore($('.btn')));
+
+    this.components.searchMovieList = new MovieSearchList(
+      $('.movie-container')
+    );
+
+    this.components.movieListContainer = new MovieListContainer({
+      $target: $('.movie-container'),
+      components: {
+        popular: this.components.popularMovieList,
+        search: this.components.searchMovieList,
+      },
+    });
+
+    this.components.header = new Header({
+      $target: $('header'),
+      components: { search: this.components.searchMovieList },
+    });
+
+    this.components.seeMore = new SeeMore({
+      $target: $('.btn'),
+      components: { movieList: this.components.movieListContainer },
+    });
+
+    this.components.skeleton = new Skeleton($('.skeleton-container'));
   }
 
   mounted() {
     this.makeComponent();
 
-    const { header, movieListContainer, seeMore } = this.component;
+    const { header, movieListContainer, title, seeMore, skeleton } =
+      this.components;
 
-    header?.setEvent();
+    header?.render().setEvent();
+    title?.render();
     movieListContainer?.fetchData();
-    seeMore?.setEvent(() => movieListContainer?.fetchData());
+    seeMore?.render().setEvent();
+    skeleton?.render();
   }
 }
