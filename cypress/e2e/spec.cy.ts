1d682b2a688adc027916ce471ed25b2f3f3c8244
Leejin-Yang <ejin1996@gmail.com>
2023-03-22 20:06:59 +0900

    test: 마지막 페이지인 경우 더보기 버튼 hide 테스트 작성

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 import { CLASS, ID } from '../../src/constants/selector';
-import type { RawMovie } from '../../src/types/movie';
+import type { MovieAPIResponse } from '../../src/types/movie';
 
 beforeEach(() => {
   cy.intercept(
@@ -13,7 +13,7 @@ beforeEach(() => {
   cy.visit('http://localhost:8080/');
 });
 
-describe('메인 화면과 더보기 버튼 테스트', () => {
+describe('메인 화면 테스트', () => {
   it('앱을 실행하면 지금 인기있는 영화 리스트 20개를 보여준다.', () => {
     cy.wait('@getPopularMovies').then((interception) => {
       const movieItems = interception.response?.body.results;
@@ -22,17 +22,6 @@ describe('메인 화면과 더보기 버튼 테스트', () => {
 
     cy.get(`.${CLASS.ITEM_VIEW} > h2`).should('have.text', '지금 인기있는 영화');
     cy.get(`.${CLASS.ITEM_LIST}`).children().should('have.length', 20);
-  });
-
-  it('더보기 버튼을 클릭하면 영화 리스트를 더 불러와 보여준다.', () => {
-    cy.get(`#${ID.LOAD_MORE_BUTTON}`).click();
-
-    cy.wait('@getPopularMovies').then((interception) => {
-      const movieItems = interception.response?.body.results;
-      expect(movieItems.length).to.equal(20);
-    });
-
-    cy.get(`.${CLASS.ITEM_LIST}`).children().should('have.length', 40);
   });
 });
 
@@ -65,5 +54,38 @@ describe('영화 검색 테스트', () => {
     cy.url().should('eq', 'http://localhost:8080/');
     cy.get(`.${CLASS.ITEM_VIEW} > h2`).should('have.text', '지금 인기있는 영화');
     cy.get(`.${CLASS.ITEM_LIST}`).children().should('have.length', 20);
+  });
+});
+
+describe('더보기 버튼 테스트', () => {
+  it('더보기 버튼을 클릭하면 영화 리스트를 더 불러와 보여준다.', () => {
+    cy.get(`#${ID.LOAD_MORE_BUTTON}`).click();
+
+    cy.wait('@getPopularMovies').then((interception) => {
+      const movieItems = interception.response?.body.results;
+      expect(movieItems.length).to.equal(20);
+    });
+
+    cy.get(`.${CLASS.ITEM_LIST}`).children().should('have.length', 40);
+  });
+
+  it('불러온 페이지가 마지막 페이지면 더보기 버튼을 숨긴다.', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api.themoviedb.org\/3\/search\/movie*/,
+      },
+      { fixture: 'movie-search-mock.json' }
+    ).as('getSearchedMovies');
+
+    cy.get(`#${ID.MOVIE_SEARCH_FORM} input`).type('해리포터');
+    cy.get(`#${ID.MOVIE_SEARCH_FORM}`).submit();
+
+    cy.wait('@getSearchedMovies').then((interception) => {
+      const movieItems: MovieAPIResponse = interception.response?.body;
+      expect(movieItems.total_pages).to.equal(1);
+    });
+
+    cy.get(`#${ID.LOAD_MORE_BUTTON}`).should('not.be.visible');
   });
 });
