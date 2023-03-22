5077fefee81d1a1d31fbd9d6bb57476e895ab155
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-03-22 16:49:05 +0900

    refactor: movieApi async/await로 통일

--- src/domains/movieApi.ts
+++ src/domains/movieApi.ts
@@ -1,25 +1,26 @@
-import { FetchMovieListProps } from '../types/movieApi';
+import { MovieResult } from '../types/movieApi';
 
-const getPopularMovieRequestUrl = (page: number = 1) =>
-  `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`;
+const getPopularMovieRequestUrl = (page: number) => {
+  return `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`;
+};
 
-const getSearchMovieRequestUrl = (query: FormDataEntryValue, page: number = 1) =>
-  `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${query}&page=${page}&include_adult=false`;
+const getSearchMovieRequestUrl = (query: FormDataEntryValue, page: number) => {
+  return `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&query=${query}&page=${page}&include_adult=false`;
+};
 
 export const movieApi = {
   async searchMovieList(query: FormDataEntryValue, page: number) {
-    return await this.fetchMovieList({ query, currentPage: page });
+    return await this.fetchMovieList(query, page);
   },
 
   async getMoreMovieList(currentPage: number) {
-    return await this.fetchMovieList({ query: '', currentPage: currentPage + 1 }).then(data => data);
+    return await this.fetchMovieList('', currentPage + 1);
   },
 
-  async fetchMovieList({ query, currentPage }: FetchMovieListProps) {
+  async fetchMovieList(query: FormDataEntryValue, currentPage: number): Promise<MovieResult[]> {
     const requestUrl = query ? getSearchMovieRequestUrl(query, currentPage) : getPopularMovieRequestUrl(currentPage);
+    const result = await fetch(requestUrl);
 
-    return await fetch(requestUrl)
-      .then(result => result.json())
-      .then(data => data);
+    return await result.json();
   },
 };
--- src/types/movieApi.ts
+++ src/types/movieApi.ts
@@ -1,4 +1,23 @@
-export interface FetchMovieListProps {
-  query: FormDataEntryValue;
-  currentPage: number;
+export interface MovieRoot {
+  page: number;
+  results: MovieResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieResult {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
