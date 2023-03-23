99cc4bae3804108eb8ed6088735ccf6754fbceed
Dain Lee <feb.dain@gmail.com>
2023-03-23 13:51:27 +0900

    design: 대체 이미지 배경 색상 지정 및 주요 색상 변수로 사용

--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -46,6 +46,7 @@ export default class MovieList extends HTMLElement {
           src="https://image.tmdb.org/t/p/w500/${movie.poster}"
           onerror="
             this.style.border='1px solid #e2e2e2';
+            this.style.background='var(--background-color)';
             this.src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg';
           "
           loading="lazy"
