f9147d8040b925cd0c0e9ab8053574706e222bfb
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-03-23 16:42:35 +0900

    build: webpack.yml API_KEY 추가

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     historyApiFallback: true,
   },
   output: {
-    // publicPath: '/',
+    publicPath: '/javascript-movie-review',
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
