98c85a6984a600136a6887d5fab4cabeffd45ddf
bassyu <yrkdgus@gmail.com>
2023-03-23 19:45:00 +0900

    feat(index.ts): 추가한 컴포넌트, CSS 적용

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import './css/reset.css';
 import './css/app.css';
 import './css/util.css';
+import './css/responsive.css';
 
 import MovieHeader from './components/MovieHeader';
 import MoviePage from './components/MoviePage';
 import MovieListItem from './components/MovieListItem';
 import SkeletonListItem from './components/SkeletonListItem';
 import ErrorPage from './components/ErrorPage';
-
-import App from './App';
+import MovieDetailBox from './components/MovieDetailBox';
+import VoteArea from './components/VoteArea';
 
 customElements.define('movie-header', MovieHeader);
 customElements.define('movie-page', MoviePage);
 customElements.define('movie-list-item', MovieListItem);
 customElements.define('skeleton-list-item', SkeletonListItem);
 customElements.define('error-page', ErrorPage);
+customElements.define('movie-detail-box', MovieDetailBox);
+customElements.define('vote-area', VoteArea);
+
+import App from './App';
 
 new App();
