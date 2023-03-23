fa8651ff91c742767cb424f2c9dc358e4002cb68
bassyu <yrkdgus@gmail.com>
2023-03-23 19:56:23 +0900

    feat(dom): 모달, 별점 랜더링 구현
    - 별점만 바꿀 때를 위해 구분해서 구현

--- src/dom.ts
+++ src/dom.ts
@@ -8,18 +8,18 @@ export const show = (selectors: string) => $(selectors)?.classList.remove('hide'
 export const renderMovieListItem = (movies: Movie[]) => {
   $('.item-list')?.insertAdjacentHTML(
     'beforeend',
-    /* html */ `${movies
+    movies
       .map(
-        ({ title, posterPath, voteAverage }) => /* html */ `
-            <movie-list-item 
-              title="${title}" 
-              poster-path="${posterPath}" 
-              vote-average="${voteAverage}"
-            ></movie-list-item>
-          `
+        ({ id, title, posterPath, voteAverage }) => /* html */ `
+          <movie-list-item 
+            id="${id}"
+            title="${title}" 
+            poster-path="${posterPath}" 
+            vote-average="${voteAverage}"
+          ></movie-list-item>
+        `
       )
-      .join('')}
-    `
+      .join('')
   );
 };
 
@@ -34,6 +34,31 @@ export const renderErrorPage = () => {
 };
 
 export const resetSearchBox = () => {
-  const searchBox = $('.search-box');
-  if (searchBox instanceof HTMLFormElement) searchBox.reset();
+  const $searchBox = $('.search-box');
+  if ($searchBox instanceof HTMLFormElement) $searchBox.reset();
+};
+
+export const renderMovieDetailBox = (movie: Movie, myVote: string) => {
+  const $modal = $('.modal');
+  if (!$modal) return;
+
+  const { id, title, posterPath, voteAverage, genreString, overview } = movie;
+  $modal.innerHTML = /* html */ `
+    <movie-detail-box
+      id="${id}"
+      title="${title}" 
+      poster-path="${posterPath}" 
+      vote-average="${voteAverage}" 
+      genreString="${genreString}" 
+      overview="${overview}"
+      my-vote="${myVote}"
+    ></movie-detail-box>
+  `;
+};
+
+export const renderMyVoteArea = (id: string, myVote: string) => {
+  const $voteArea = $('.vote-area');
+  if (!$voteArea) return;
+
+  $voteArea.outerHTML = /* html */ `<vote-area id="${id}" my-vote="${myVote}"></vote-area>`;
 };
