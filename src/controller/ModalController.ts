5ee221643dd45d74c033e6fc608a7d3d7189b163
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-23 18:08:20 +0900

    feat(modalcontroller): 모달을 생성하고, 모달 표시 및 모달의 별점요청을 감지하는 기능 구현

--- src/controller/ModalController.ts
+++ src/controller/ModalController.ts
@@ -1,18 +1,36 @@
 import MovieStorage from '../domains/MovieStorage';
+import MovieInfoModal from '../components/MovieInfoModal';
 import EventBus from '../EventBus';
+import ratingLocalStorage from '../domains/RatingLocalStorage';
+import { $ } from '../utils/domSelector';
 
 class ModalController {
+  private movieInfoModal: MovieInfoModal;
+
   constructor() {
     EventBus.setEvent('openInfoModal', this.openInfoModalWithMovieId);
-    console.log('set event');
+    EventBus.setEvent('setUserRating', this.setUserMovieRating);
+    this.movieInfoModal = new MovieInfoModal({
+      parentElement: $('#app'),
+      onRatingRequestEvent: this.getUserMovieRating,
+    });
   }
 
-  openInfoModalWithMovieId(movieId: number) {
+  openInfoModalWithMovieId = (movieId: number) => {
     const movieInfo = MovieStorage.getMovieById(movieId);
-    alert('영화 정보:' + JSON.stringify(movieInfo));
 
-    console.log(`${movieId} 번 영화의 정보입니다.`);
-    console.log(movieInfo);
+    if (!(movieInfo instanceof Error)) {
+      this.movieInfoModal.openInfoModalWithInfo(movieInfo);
+    }
+  };
+
+  setUserMovieRating(movieId: number, rating: number) {
+    ratingLocalStorage.setMovieRating(movieId, rating);
+  }
+
+  getUserMovieRating(movieId: number) {
+    console.log('ok we will give rating');
+    return ratingLocalStorage.getMovieRating(movieId);
   }
 }
 
