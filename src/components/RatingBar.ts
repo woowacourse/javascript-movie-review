c8b275cd129502d0078be91215d3a2e5a73515d0
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-23 18:05:59 +0900

    feat(ratingbar): 별점 컴포넌트 구현
    - 별점을 표시
    - 별점이 변경되면, 별점 설명(alias) 변경
    - 별점이 변경되면, 영화 ID에 해당하는 로컬 스토리지 데이터 변경 요청

---
+++ src/components/RatingBar.ts
@@ -0,0 +1,78 @@
+import { $, $$ } from '../utils/domSelector';
+import EventBus from '../EventBus';
+
+type RatingBarType = {
+  parentElement: HTMLElement;
+  id: number;
+  startRating: number;
+  ratingInfos: { rating: number; alias: string }[];
+};
+
+class RatingBar {
+  private $parentElement;
+  private id;
+  private $element = document.createElement('div');
+  private startRating;
+  private ratingInfos;
+
+  constructor({ parentElement, id, startRating, ratingInfos }: RatingBarType) {
+    this.$parentElement = parentElement;
+    this.id = id;
+    this.startRating = startRating;
+    this.ratingInfos = ratingInfos;
+
+    this.render();
+    this.changeAliasIfRatingChanged();
+  }
+
+  private render() {
+    this.$element.classList.add('user-rating-bar-wrapper');
+    this.$element.innerHTML = `
+      <div class="rating-title">내 별점</div>
+      <div class="user-rating-bar">${this.getStarButtonsTemplate()}</div>
+      <div class="modal-text user-rating-score">${this.startRating}</div>
+      <div class="modal-text user-rating-alias">${this.getAliasByRating(this.startRating)}</div>
+    `;
+
+    this.$parentElement.appendChild(this.$element);
+  }
+
+  private getStarButtonsTemplate() {
+    return this.ratingInfos
+      .reverse()
+      .map(
+        ({ rating }) =>
+          `<input type="radio" name="rating" value="${rating}" ${
+            this.startRating === rating ? 'checked' : ''
+          }/>`,
+      )
+      .join('');
+  }
+
+  private getAliasByRating(searchRating: number) {
+    const searchedInfo = this.ratingInfos.find(({ rating }) => rating === searchRating);
+
+    return searchedInfo ? searchedInfo.alias : '정보 없음';
+  }
+
+  private changeAliasIfRatingChanged() {
+    const $ratingButtons = $$('.user-rating-bar input[type="radio"][name="rating"]', this.$element);
+    const $ratingScore = $('.user-rating-score', this.$element);
+    const $ratingAlias = $('.user-rating-alias', this.$element);
+
+    $ratingButtons.forEach((radio) => {
+      if (radio instanceof HTMLInputElement) {
+        radio.addEventListener('change', () => {
+          const rating = radio.value;
+
+          $ratingScore.innerText = rating;
+          $ratingAlias.innerHTML = this.getAliasByRating(Number(rating));
+
+          EventBus.triggerEvent('setUserRating', [this.id, Number(rating)]);
+        });
+      }
+    });
+  }
+}
+
+export default RatingBar;
