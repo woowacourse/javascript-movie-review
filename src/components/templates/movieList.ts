e26c7afb9a42b74f64faaa13bcab1a899b71f310
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-03-23 15:32:35 +0900

    perf: 검색 오류 화면 텍스트 추가

--- src/components/templates/movieList.ts
+++ src/components/templates/movieList.ts
@@ -29,7 +29,12 @@ export const generateMovieListTemplate = (movieList: MovieResult[]) => {
   if (!movieItemTemplateList) {
     /* html */
     return `
-			<h2 class="empty-movie-message">찾을 수 없는 영화 이름입니다 🥲</h2>
+			<div class="empty-movie-list-container">
+				<h3>찾을 수 없는 영화 이름입니다 🥲</h3>
+				<p>단어의 철자가 정확한지 확인해 보세요.</p>
+				<p>검색어의 단어 수를 줄이거나, 보다 일반적인 검색어로 다시 검색해 보세요.</p>
+				<p>두 단어 이상의 검색어인 경우, 띄어쓰기를 확인해보세요.</p>
+			</div>
 		`;
   }
 
