a74b2bfc04cf09580d25ea36331544546195eb4d
n0eyes <doll4586633@naver.com>
2023-03-23 22:49:59 +0900

    feat: localStorage 유틸 함수 추가

---
+++ src/utils/common/localStorage.ts
@@ -0,0 +1,11 @@
+export function addData(data: unknown, id: string = 'mock') {
+  localStorage.setItem(id, JSON.stringify(data));
+}
+
+export function getData(id: string = 'my_votes', defaultData?: unknown) {
+  const data = localStorage.getItem(id);
+
+  if (!data) return defaultData;
+
+  return JSON.parse(data);
+}
