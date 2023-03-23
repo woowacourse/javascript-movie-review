8d246ff7be9143eb90bf202a39ff83ee8a81f6d2
bassyu <yrkdgus@gmail.com>
2023-03-23 19:26:45 +0900

    feat(MovieDetailBox): 영화 디테일 컴포넌트 구현
    - 닫기 버튼, 특정 키를 누르면 close 실행
    - 내 별점 컴포넌트는 따로 분리

---
+++ src/components/MovieDetailBox.ts
@@ -0,0 +1,67 @@
+import DefaultPoster from '../../assets/default_poster.png';
+import { STAR_FILLED } from '../../assets/svg';
+import { hide } from '../dom';
+
+class MovieDetailBox extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  render() {
+    const id = this.getAttribute('id');
+    const posterPath = this.getAttribute('poster-path');
+
+    this.innerHTML = /* html */ `
+      <div class="box" id="${id}">
+        <section class="head">
+          <h1>${this.getAttribute('title')}</h1>
+          <button id="close">✕</button>
+        </section>
+        <section class="body">
+          <img 
+            class="poster" 
+            src=${
+              posterPath === 'null'
+                ? DefaultPoster
+                : `https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`
+            }
+          />
+          <div class="detail">
+            <div class="description">
+              <p>
+                ${this.getAttribute('genreString')}
+                <span>${STAR_FILLED}</span>
+                ${this.getAttribute('vote-average')}
+              </p>
+              <p>
+                ${this.getAttribute('overview')}
+              </p>
+            </div>
+            <vote-area 
+              id="${id}" 
+              my-vote="${this.getAttribute('my-vote')}"
+            ></vote-area>
+          </div>
+        </section>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    document.addEventListener('keydown', this.onKeyDown);
+    this.querySelector('#close')?.addEventListener('click', this.close);
+  }
+
+  onKeyDown = (e: Event) => {
+    const closeKeys = ['Escape', 'Backspace'];
+    if (e instanceof KeyboardEvent && closeKeys.includes(e.key)) this.close();
+  };
+
+  close = () => {
+    hide('.modal');
+    document.body.classList.remove('fix');
+  };
+}
+
+export default MovieDetailBox;
