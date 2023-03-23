f894c2e56c267914c06993df1d483bdc11c25a75
bassyu <yrkdgus@gmail.com>
2023-03-23 19:39:33 +0900

    feat(constants): 별점 메세지 상수 추가

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,12 @@ export const MAX_MOVIES_PER_PAGE = 20;
 export const POPULAR_TITLE = '지금 인기 있는 영화';
 
 export const MAXIMUM_PAGE = 500;
+
+export const VOTE_MESSAGE: Record<number, string> = {
+  0: '',
+  2: '2 최악이에요',
+  4: '4 별로예요',
+  6: '6 보통이에요',
+  8: '8 재미있어요',
+  10: '10 명작이에요',
+};
