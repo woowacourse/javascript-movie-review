ae8f0872e7180ec9215d4cab387709bbc5c60ba9
NaveOWO <pianowell96@gmail.com>
2023-03-23 21:21:52 +0900

    [ feat ] 클릭했을 때 별점이 업데이트 되는 기능 구현

--- src/components/StarInput.ts
+++ src/components/StarInput.ts
@@ -4,10 +4,11 @@ export class StarInput {
   private _id: number;
   private _score: number;
 
-  constructor(id: number, score: number) {
+  constructor(id: number) {
     this._id = id;
-    this._score = score;
+    this._score = -1;
     this.render();
+    this.handleEvent();
   }
 
   create() {
@@ -15,8 +16,7 @@ export class StarInput {
             <section class="score" id=${this._id}>
                 <strong class="my-score">내 별점</strong>
                 <div class="score__img-wrapper">
-                ${this.getCountedStar(this._score, "filled")}
-                ${this.getCountedStar(5 - this._score, "empty")}
+                ${this.getCountedStar()}
                 </div>
                 <strong class="score__count">6</strong>
                 <strong class="score__comment">보통이에요</strong>
@@ -24,10 +24,12 @@ export class StarInput {
         `;
   }
 
-  getCountedStar(starCount: number, starCategory: String) {
-    return Array.from({ length: starCount })
-      .map(() => {
-        return `<img src="/star_${starCategory}.png" />`;
+  getCountedStar() {
+    return Array.from({ length: 5 })
+      .map((_, idx) => {
+        return idx <= this._score
+          ? `<img src="/star_filled.png" class="star-img" id=${idx} />`
+          : `<img src="/star_empty.png" class="star-img" id=${idx} />`;
       })
       .join("");
   }
@@ -35,5 +37,32 @@ export class StarInput {
   render() {
     const modal = $(".modal__info") as HTMLElement;
     modal.insertAdjacentHTML("beforeend", this.create());
+  }
+
+  rerenderStarImages() {
+    const imageWrapper = $(".score__img-wrapper") as HTMLElement;
+    imageWrapper.innerHTML = "";
+    imageWrapper.innerHTML = this.getCountedStar();
+  }
+
+  updateScore(score: number) {
+    this._score = score;
+  }
+
+  updateStarImage(targetElement: HTMLElement) {
+    Number(targetElement.id) > this._score
+      ? this.updateScore(Number(targetElement.id))
+      : this.updateScore(Number(targetElement.id) - 1);
+    this.rerenderStarImages();
+  }
+
+  handleEvent() {
+    const starBox = $(".score__img-wrapper") as HTMLElement;
+    starBox.addEventListener("click", (e) => {
+      const target = e.target as HTMLElement;
+      if (target.className === "star-img") {
+        this.updateStarImage(target);
+      }
+    });
   }
 }
