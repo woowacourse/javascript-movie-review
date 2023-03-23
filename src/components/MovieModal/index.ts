0329cadf859eff50d495f666c3d352bb047e8d41
TaeeunKim <xodms0309@naver.com>
2023-03-23 16:30:57 +0900

    feat: 사용자가 별점을 남겼을 때 점수를 알려주는 기능 구현

--- src/components/MovieModal/index.ts
+++ src/components/MovieModal/index.ts
@@ -1,4 +1,6 @@
+import './index.css';
 import template from './index.html';
+import { $ } from '../../utils/dom';
 
 class MovieModal extends HTMLElement {
   constructor() {
@@ -7,6 +9,13 @@ class MovieModal extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = template;
+    const ratingWrapper = $('.rating-item-wrapper', this);
+    ratingWrapper.addEventListener('click', (e: Event) => {
+      if (!(e.target instanceof HTMLInputElement)) return;
+      const rating = Number(e.target.value);
+      const mark = $(`label[for=star${rating}`, this).getAttribute('title');
+      $('.my-rating', this).textContent = `${rating * 2}점 - ${mark}`;
+    });
   }
 }
 export default MovieModal;
