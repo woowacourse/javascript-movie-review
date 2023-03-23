56dfbd4147deecbafb0cf03419b47938a91bce28
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-23 18:12:56 +0900

    refactor(movieitem): 이미지 경로를 가져올 때, 상수를 이용하도록 변경 (상수 분리)

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -1,8 +1,6 @@
 import { MovieType } from '../types';
 import { $ } from '../utils/domSelector';
-
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
-const NO_IMAGE_URL = '../../assets/no_image.png';
+import { IMAGE_URL } from '../constants';
 
 type MovieItemConstructorType = {
   parentElement: HTMLElement;
@@ -23,14 +21,12 @@ class MovieItem {
   }
 
   private createElement({ title, posterPath, id, voteAverage }: MovieType) {
-    const imageUrl = posterPath ? `${IMAGE_BASE_URL}${posterPath}` : NO_IMAGE_URL;
-
     this.$item = document.createElement('a');
     this.$item.innerHTML = `
       <div class="item-card" movie-id="${id}">
         <img
           class="item-thumbnail"
-          src="${imageUrl}"
+          src="${IMAGE_URL(posterPath)}"
           alt="${title}"/>
         <p class="item-title">${title}</p>
         <p class="item-score"><img src="./assets/star_filled.png" alt="별점" /> ${voteAverage}</p>
