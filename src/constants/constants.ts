fa33fb01648a0dceb841473ad808bd0d9b6b0790
inyeong-kang <tomatorkd@naver.com>
2023-03-23 16:43:59 +0900

    feat: 에러 메시지 작성 및 오류 발생한 경우에 띄울 페이지 구현

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -10,4 +10,7 @@ export const MAX_KEYWORD_LENGTH = 150;
 export const ERROR_MESSAGE = Object.freeze({
   EMPTY: '아무것도 입력하지 않았습니다.',
   KEYWORD_LENGTH: `검색 키워드는 ${MAX_KEYWORD_LENGTH}자 이하로 입력해주세요.`,
+  CLINET_404: '요청하신 리소스는 찾을 수 없습니다. (404 Not Found)',
+  CLINET_401: '유효한 API key가 필요합니다. (Invalid API key: You must be granted a valid key)',
+  SERVER: '서버에 문제가 발생했습니다.',
 });
