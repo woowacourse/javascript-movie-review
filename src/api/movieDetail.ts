fb208bd66eb8c8605c195a1e18500dca66caa30c
Suyoung <jsy8492@gmail.com>
2023-03-23 10:43:57 +0900

    feat: 영화 상세 정보 불러오기 api 요청을 위한 타입 및 함수 작성

---
+++ src/api/movieDetail.ts
@@ -0,0 +1,57 @@
+import { request } from './index';
+
+export interface MovieDetailResponse {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: any;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: any;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path?: string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  iso_639_1: string;
+  name: string;
+}
+
+export const fetchMovieDetail = async (id: number): Promise<MovieDetailResponse> => {
+  const movieDetail = await request<MovieDetailResponse>(`movie/${id}`);
+
+  return movieDetail;
+};
