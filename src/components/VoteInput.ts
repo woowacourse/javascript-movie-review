0e179f499d45962af50185079037a7eb4c1791df
solo5star <solo_5@naver.com>
2023-03-23 20:04:49 +0900

    feat: VoteInput을 radio 기반으로 변경

--- src/components/VoteInput.ts
+++ src/components/VoteInput.ts
@@ -12,19 +12,19 @@ export class VoteInput {
   constructor({ value }: VoteInputProps = {}) {
     this.$root.classList.add('vote');
     this.$root.innerHTML = `
-      <button type="button" data-value="2" class="star"></button>
-      <button type="button" data-value="4" class="star"></button>
-      <button type="button" data-value="6" class="star"></button>
-      <button type="button" data-value="8" class="star"></button>
-      <button type="button" data-value="10" class="star"></button>
+      <label class="star"><input name="vote" type="radio" value="2" data-text="최악이에요"></label>
+      <label class="star"><input name="vote" type="radio" value="4" data-text="별로에요"></label>
+      <label class="star"><input name="vote" type="radio" value="6" data-text="보통이에요"></label>
+      <label class="star"><input name="vote" type="radio" value="8" data-text="재미있어요"></label>
+      <label class="star"><input name="vote" type="radio" value="10" data-text="명작이에요"></label>
     `.trim();
 
-    if (value) this.$(`.star[data-value="${value}"]?`)?.classList.add('active');
+    if (value) this.$(`.star:has([value="${value}"])?`)?.classList.add('active');
 
     this.$root.addEventListener('click', (event) => {
-      if (event.target instanceof HTMLButtonElement) {
+      if (event.target instanceof HTMLInputElement) {
         this.$('.active?')?.classList.remove('active');
-        event.target.classList.add('active');
+        event.target.closest('label')!.classList.add('active');
       }
     });
   }
