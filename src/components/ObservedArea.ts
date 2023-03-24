eb277051aa951bf544d56faafdd784c2c53a928f
nangkyeonglim <nangkyeonglim@gmail.com>
2023-03-24 21:35:09 +0900

    feat: 무한 스크롤 observed area 구현

---
+++ src/components/ObservedArea.ts
@@ -0,0 +1,48 @@
+import { dispatchCustomEvent, $ } from "../utils/dom";
+
+class ObservedArea extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+    this.addEvent();
+  }
+
+  render() {
+    this.innerHTML = `<div class="scroll-area"></div>`;
+  }
+
+  addEvent() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.handleIntersect();
+          }
+        });
+      },
+      {
+        threshold: 0.9,
+      }
+    );
+    observer.observe(document.querySelector(".scroll-area") as HTMLElement);
+  }
+
+  handleIntersect() {
+    const container = <HTMLElement>$("movie-list-container");
+    dispatchCustomEvent(container, {
+      eventType: "fetchMovieData",
+      data: container.getAttribute("type"),
+    });
+  }
+}
+
+interface ObservedArea {
+  "observed-area": typeof ObservedArea;
+}
+
+customElements.define("observed-area", ObservedArea);
+
+export default ObservedArea;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import "../templates/reset.css";
 import "../templates/common.css";
 
-import MovieHeader from "../src/components/MovieHeader";
-import SearchBox from "../src/components/SearchBox";
-import MovieItem from "../src/components/MovieItem";
-import MovieList from "../src/components/MovieList";
-import MovieListContainer from "../src/components/MovieListContainer";
-import CustomModal from "../src/components/CustomModal";
-import MovieDetail from "../src/components/MovieDetail";
+import MovieHeader from "./components/MovieHeader";
+import SearchBox from "./components/SearchBox";
+import MovieItem from "./components/MovieItem";
+import MovieList from "./components/MovieList";
+import MovieListContainer from "./components/MovieListContainer";
+import CustomModal from "./components/CustomModal";
+import MovieDetail from "./components/MovieDetail";
 import MovieScore from "./components/MovieScore";
+import ObservedArea from "./components/ObservedArea";
 
 import movieApp from "./movieApp";
 
