7abc8651c5d00fd220102190f8f256f681ff6c19
solo5star <solo_5@naver.com>
2023-03-24 21:57:22 +0900

    feat: Toast stacking 지원, 디자인 수정, duration 파라미터 추가

--- src/components/Toast.ts
+++ src/components/Toast.ts
@@ -1,26 +1,35 @@
 import { $ } from '../utils/selector';
 
+export type ToastProps = {
+  message: string;
+  duration?: number;
+};
+
 export class Toast {
   private readonly $root = document.createElement('div');
 
-  constructor(private readonly message: string) {
-    this.$root.innerText = this.message;
-    this.$root.classList.add('toast');
+  constructor({ message, duration = 4000 }: ToastProps) {
+    this.$root.innerText = message;
+    this.$root.classList.add('toast', 'fade');
 
-    setTimeout(() => {
-      this.$root.classList.add('fade-out');
+    this.$root.addEventListener('animationend', () => {
+      this.$root.classList.remove('fade');
+
       setTimeout(() => {
-        this.$root.remove();
-      }, 1000);
-    }, 5000);
+        this.$root.classList.add('fade', 'dispose');
+        this.$root.addEventListener('animationend', () => {
+          this.$root.remove();
+        });
+      }, duration);
+    });
   }
 
   getRoot() {
     return this.$root;
   }
 
-  static create(message: string) {
-    const toast = new Toast(message);
+  static create(message: string, duration?: number) {
+    const toast = new Toast({ message, duration });
     $('#toast-container').append(toast.getRoot());
     return toast;
   }
