71319021ae4fbdaedb66d49623e8765a9d1afeff
turtle601 <turtle601@naver.com>
2023-03-24 17:18:42 +0900

    feat(cache): 스크롤된 page 정보(search, popular)가 저장된 cacheHook 구현

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,3 +1,5 @@
+import { MovieApiType } from '../type/movie';
+
 type CacheType = {
   popularPage: Set<number>;
   searchPage: Set<number>;
@@ -6,4 +8,30 @@ type CacheType = {
 export const cache: CacheType = {
   popularPage: new Set(),
   searchPage: new Set(),
+};
+
+export const cacheHook = {
+  popular: {
+    reset: () => {
+      cache.popularPage = new Set();
+    },
+
+    store: (page: number) => {
+      cache.popularPage.add(page);
+    },
+
+    has: (page: number) => {
+      cache.popularPage.has(page);
+    },
+  },
+
+  search: {
+    reset: () => {
+      cache.searchPage = new Set();
+    },
+
+    store: (page: number) => {
+      cache.searchPage.add(page);
+    },
+  },
 };
