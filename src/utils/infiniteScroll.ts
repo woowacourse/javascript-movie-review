e28f4b4a81af4a941e19b88deb52cd3563311511
NaveOWO <pianowell96@gmail.com>
2023-03-25 03:36:47 +0900

    [ feat ] 무한스크롤 함수 생성

---
+++ src/utils/infiniteScroll.ts
@@ -0,0 +1,25 @@
+import { $$ } from "./selector";
+
+export const loadDataByInfiniteScroll = async (
+  target: HTMLElement,
+  renderMovies: any
+) => {
+  const callback = async (
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) => {
+    const observerTarget = entries[0];
+
+    if (!observerTarget.isIntersecting) return;
+    if (observerTarget.isIntersecting) {
+      observer.unobserve(target);
+    }
+
+    $$(".item-card").length >= 20 && (await renderMovies());
+    observer.observe(target);
+  };
+
+  const observer = new IntersectionObserver(callback);
+  observer.observe(target);
+  return observer;
+};
