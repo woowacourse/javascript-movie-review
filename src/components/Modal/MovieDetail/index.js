2496cbb45133a756f3109736296c43db80259b52
woo-jk <201702042@o.cnu.ac.kr>
2023-03-24 17:19:00 +0900

    feat: 영화 상세정보 모달창 퍼블리싱

--- src/components/Modal/MovieDetail/index.js
+++ src/components/Modal/MovieDetail/index.js
@@ -1,16 +1,66 @@
+import "./index.css";
+import { fetchMovieDetail } from "../../../apis";
+import MovieDetail from "../../../domain/MovieDetail";
+import StarIcon from "../../../images/star_filled.png";
+import NotFoundImageIcon from "../../../images/not_found_image.png";
+import { imageUrl } from "../../../constants/urls";
+
 const MovieDetailModal = {
-  template(id) {
+  template(movieDetail) {
+    const { title, posterSrc, voteAverage, genres, overview, myVote } =
+      movieDetail.getMovieDetailInfo();
+
     return `
     <div class="movie-detail">
-     ${id}
+      <div class="detail-header">
+        <div class="detail-title">${title}</div>
+        <button class="close-button" />
+      </div>
+      <div class="detail-body">
+        <img
+        class="detail-image"
+        src=${posterSrc ? this.convertToImgUrl(posterSrc) : NotFoundImageIcon}
+        alt=${title}
+        />
+        <div class="detail-contents">
+          <p class="detail-genre-score">${genres.map(
+            (genre) => genre.name
+          )} <img class="detail-star" src=${StarIcon} alt="별점" /> ${voteAverage}</p>
+          <p class="detail-overview">${overview || "영화에 대한 줄거리가 존재하지 않습니다."}</p>
+          <div class="my-vote-container">
+            <span class="my-vote-title">내 별점</span>
+            <div class="my-vote-star-container">
+              <img class="my-vote-star" src=${StarIcon} alt="별점" />
+              <img class="my-vote-star" src=${StarIcon} alt="별점" />
+              <img class="my-vote-star" src=${StarIcon} alt="별점" />
+              <img class="my-vote-star" src=${StarIcon} alt="별점" />
+              <img class="my-vote-star" src=${StarIcon} alt="별점" />
+            </div>
+            <span class="my-vote-score">10</span>
+            <span class="my-vote-desc">명작이에요</span>
+          </div>
+        </div>
+      </div>
     </div>
     `;
   },
 
-  render(id) {
+  async render(id) {
     const $modalContainer = document.querySelector(".modal-container");
+    const movieDetail = await this.getMovieDetail(id);
+    $modalContainer.innerHTML = this.template(movieDetail);
+  },
 
-    $modalContainer.innerHTML = this.template(id);
+  async getMovieDetail(id) {
+    const movieDetailData = await fetchMovieDetail(id);
+    const myVote = 5;
+    const movieDetail = new MovieDetail(movieDetailData, myVote);
+
+    return movieDetail;
+  },
+
+  convertToImgUrl(imgSrc) {
+    return `${imageUrl}${imgSrc}`;
   },
 };
 
