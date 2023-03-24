c0b0c76e258f48fc780d9bfda37e284e1c448bed
TaeeunKim <xodms0309@naver.com>
2023-03-24 13:33:49 +0900

    feat: 영화 상세 정보 api를 불러오는 함수 구현

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,4 +1,5 @@
 import {
+  DetailMovie,
   FailedResponse,
   Movie,
   MovieDataResponse,
@@ -8,8 +9,10 @@ import {
 } from './types';
 import { handleError } from './utils/handleError';
 
-export const getAPIUrl = (params: string, options: Options) =>
-  `https://api.themoviedb.org/3${params}?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${options.page}&query=${options.query}`;
+export const getAPIUrl = (params: string, options?: Options) =>
+  `https://api.themoviedb.org/3${params}?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${
+    options?.page || 1
+  }&query=${options?.query}`;
 
 export const fetchMovies = async (params: string, options: Options): Promise<MovieList> => {
   const API_URL = getAPIUrl(params, options);
@@ -25,12 +28,44 @@ export const fetchMovies = async (params: string, options: Options): Promise<Mov
 
     const data: MovieDataResponse = await res.json();
     const movies: Movie[] = data.results.map((result: MovieResults) => ({
+      id: result.id,
       title: result.title,
       posterPath: result.poster_path,
       voteAverage: result.vote_average,
     }));
 
     return { movies: movies, totalPages: data.total_pages };
+  } catch (error) {
+    if (error instanceof Error) {
+      handleError(error.message);
+    }
+    throw new Error();
+  }
+};
+
+export const fetchMovie = async (params: string) => {
+  const API_URL = getAPIUrl(params);
+  try {
+    if (!navigator.onLine) {
+      throw new Error('네트워크 오프라인이 감지되었습니다');
+    }
+    const res = await fetch(API_URL);
+    if (!res.ok) {
+      const error: FailedResponse = await res.json();
+      throw new Error(error.status_message);
+    }
+
+    const data = await res.json();
+    const movie: DetailMovie = {
+      id: data.id,
+      title: data.title,
+      voteAverage: data.vote_average,
+      genres: data.genres,
+      overview: data.overview,
+      posterPath: data.poster_path,
+    };
+
+    return movie;
   } catch (error) {
     if (error instanceof Error) {
       handleError(error.message);
