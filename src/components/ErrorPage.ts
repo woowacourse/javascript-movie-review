25122cca33fd718f01ce913d54fcde2135796544
bassyu <yrkdgus@gmail.com>
2023-03-24 16:14:02 +0900

    feat(ErrorPage): 기본 메세지 추가

--- src/components/ErrorPage.ts
+++ src/components/ErrorPage.ts
@@ -8,10 +8,11 @@ class ErrorPage extends HTMLElement {
 
   render() {
     const statusCode = Number(this.getAttribute('status-code'));
+    const statusMessages = STATUS_MESSAGES_MAP[statusCode] ?? ['나중에 다시 시도해주세요'];
     this.innerHTML = /* html */ `
       <section class="error-view">
         <h1>페이지를 가져오지 못했어요 😢</h1>
-        ${STATUS_MESSAGES_MAP[statusCode].map((message) => /* html */ `<p>${message}</p>`).join('')}
+        ${statusMessages.map((message) => /* html */ `<p>${message}</p>`).join('')}
       </section>
     `;
   }
