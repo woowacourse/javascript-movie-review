b320f4f13bd6bfc52f36c155f6994e9b6258345e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-24 21:19:22 +0900

    feat(storage): 로컬스토리지 유틸 함수 구현

---
+++ src/utils/storage.ts
@@ -0,0 +1,15 @@
+const storage = {
+  setLocalStorage<T>(key: string, data: T) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
+
+  getLocalStorage(key: string) {
+    const data = localStorage.getItem(key);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+  },
+};
+
+export default storage;
