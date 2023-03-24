393bbe9679d9e22d0fc9ac2b2182e0d7bbf4e78c
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-03-24 15:51:36 +0900

    feat: 모달 스켈레톤 ui 구현 2

--- src/components/modal/MovieModalComponent.js
+++ src/components/modal/MovieModalComponent.js
@@ -2,29 +2,114 @@ import CustomAsyncComponent from "../../abstracts/CustomAsyncComponent";
 import SelectVoteScoreComponent from "./SelectVoteScoreComponent";
 import StarFilledImg from "../../../templates/star_filled.png";
 import ModalCloseButtonImg from "../../../templates/close.png";
-import { navigate } from "../../util/Router";
+import { fetchMovieDetails } from "../../util/Api";
 
 export default class MovieModalComponent extends CustomAsyncComponent {
+  #state = {};
+  constructor() {
+    super();
+    this.state = {
+      detail: {
+        id: -1,
+        title: "",
+        poster_path: "",
+        genres: "",
+        vote_average: 0,
+        overview: "",
+      },
+    };
+  }
+
+  addSkeleton() {
+    this.imgSkeleton = document.createElement("div");
+    this.imgSkeleton.className = "modal-item-thumbnail skeleton";
+    this.querySelector(".movie-modal-left").prepend(this.imgSkeleton);
+
+    this.movieImg = this.querySelector(".movie-modal-img");
+    this.movieImg.classList.add("fadeout");
+
+    this.titleElement = this.querySelector(".movie-modal-title");
+    this.titleElement.classList.add("skeleton");
+
+    this.genreElement = this.querySelector(".movie-modal-info-head > p");
+    this.genreElement.classList.add("skeleton");
+
+    this.overviewElement = this.querySelector(".movie-modal-description");
+    this.overviewElement.classList.add("skeleton");
+
+    this.starElement = this.querySelector("#vote-star");
+    this.starElement.classList.add("fadeout");
+
+    this.voteAverageElement = this.querySelector(".movie-modal-average-vote");
+    this.voteAverageElement.classList.add("skeleton");
+
+    this.voteSkeleton = document.createElement("div");
+    this.voteSkeleton.className = "select-vote-score skeleton";
+    this.querySelector(".movie-modal-vote").prepend(this.voteSkeleton);
+
+    this.originVoteElement = this.querySelector("select-vote-score");
+    this.originVoteElement.classList.add("fadeout");
+  }
+
+  deleteSkelton() {
+    this.imgSkeleton.remove();
+    this.movieImg.classList.remove("fadeout");
+    this.titleElement.classList.remove("skeleton");
+    this.genreElement.classList.remove("skeleton");
+    this.overviewElement.classList.remove("skeleton");
+    this.voteAverageElement.classList.remove("skeleton");
+    this.starElement.classList.remove("fadeout");
+    this.voteSkeleton.remove();
+    this.originVoteElement.classList.remove("fadeout");
+  }
+
   handleEvent() {
     this.querySelector(".movie-modal-close").addEventListener("click", () => {
-      navigate("/");
+      history.back();
     });
 
-    this.addEventListener("keyup", async (e) => {
+    document.addEventListener("keyup", (e) => {
       if (e.key === "Backspace") {
         e.preventDefault();
-        navigate("/");
+        history.back();
+      }
+    });
+
+    this.addSkeleton();
+    this.movieImg.addEventListener("load", () => {
+      if (this.movieImg.complete) {
+        this.deleteSkelton();
       }
     });
   }
 
+  async loadMovieDetail(id) {
+    const detailData = await fetchMovieDetails(id);
+    Object.entries(detailData).forEach(async ([key, value]) => {
+      this.state.detail[key] = String(value);
+    });
+    this.rerender();
+  }
+
+  async rerender() {
+    this.titleElement.textContent = this.state.detail.title;
+    this.movieImg.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w500${this.state.detail.poster_path}`
+    );
+    this.voteAverageElement.lastChild.textContent =
+      this.state.detail.vote_average;
+    this.genreElement.textContent = this.state.detail.genres;
+    this.overviewElement.textContent = this.state.detail.overview;
+    this.originVoteElement.setAttribute(
+      "movie-id",
+      String(this.state.detail.id)
+    );
+  }
+
   async template() {
-    const id = this.getAttribute("id");
-    const title = this.getAttribute("title");
-    const moviePoster = this.getAttribute("poster_path");
-    const genreNames = this.getAttribute("genres");
-    const averageVote = this.getAttribute("vote_average");
-    const description = this.getAttribute("overview");
+    const { id, title, poster_path, genres, vote_average, overview } =
+      this.state.detail;
 
     return /*html*/ `
         <div class="movie-modal-wrapper">
@@ -34,16 +119,16 @@ export default class MovieModalComponent extends CustomAsyncComponent {
             </div>
             <div class="movie-modal-contents">
                 <div class="movie-modal-left">
-                    <img class="movie-modal-img" src="https://image.tmdb.org/t/p/w500${moviePoster}" alt="Movie poster" />
+                    <img class="movie-modal-img" src="" alt="Movie poster" />
                 </div>
                 <div class="movie-modal-right">
                     <div class="movie-modal-information">
                         <div class="movie-modal-info-head">
-                            <p>${genreNames}</p> 
-                            <div class="movie-modal-average-vote"><img src=${StarFilledImg} alt="별점" /> ${averageVote}</div>
+                            <p>${genres}</p> 
+                            <div class="movie-modal-average-vote"><img id="vote-star" src=${StarFilledImg} alt="별점" /> <p>${vote_average}</p></div>
                         </div>
                         <div class="movie-modal-description">
-                            <p>${description}</p>
+                            <p>${overview}</p>
                         </div>
                     </div>
                     <div class="movie-modal-vote">
