0427e0bab2d92d9b274f028eb5963dc05c98cddc
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-25 23:02:14 +0900

    refactor(storage): 데이터가 없는 경우 return타입 명시

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -6,9 +6,9 @@ const storage = {
   getLocalStorage(key: string) {
     const data = localStorage.getItem(key);
 
-    if (data) {
-      return JSON.parse(data);
-    }
+    if (!data) return;
+
+    return JSON.parse(data);
   },
 };
 
