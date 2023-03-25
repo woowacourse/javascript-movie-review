0e2895d10960c6295bcb28d73e2ca23573b44b11
jeongwusi <jeongwusi@gmail.com>
2023-03-25 20:10:16 +0900

    ✨ feat: 검색창 컴포넌트 생성

--- src/assets/images.ts
+++
@@ -1,9 +0,0 @@
-import './logo.png';
-import './search_button.png';
-import './star_filled.png';
-
-export const IMAGES = {
-  LOGO: 'static/media/logo.png',
-  SEARCH_BUTTON: 'static/media/search_button.png',
-  STAR_FILLED: 'static/media/star_filled.png',
-};
--- src/components/templates/showMoreButton.ts
+++
@@ -1,5 +0,0 @@
-export const showMoreButtonTemplate = () =>
-  /* html */
-  `
-  <button class="btn primary full-width" id="moreButton">더 보기</button>
-	`;
