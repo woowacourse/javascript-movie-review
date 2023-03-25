4108e87376ef579ffeaa506db7124e87fa62a414
n0eyes <doll4586633@naver.com>
2023-03-25 23:28:16 +0900

    feat: useMovieOverviewModal hook 추가

---
+++ src/hooks/useMovieOverviewModal.ts
@@ -0,0 +1,50 @@
+import { fetchMovieGenres, waitFor } from '../apis';
+import { RemoteMovieGenre } from '../apis/movieChart.type';
+import { MovieOverview } from '../components/MovieOverviewModal';
+import { GENRES } from '../constants/movieChart';
+import { useEffect, useState } from '../core';
+import { Movie } from '../domain/Movie';
+import { useModal } from './useModal';
+
+function useMovieOverviewModal(initial: boolean) {
+  const [isOpen, open, close] = useModal(initial);
+  const [genres, setGenres] = useState<RemoteMovieGenre[]>([]);
+  const [focusedMovie, setFocusMovie] = useState<MovieOverview | null>(null);
+
+  const openModal = (movie: Movie) => {
+    focusMovie(movie);
+    open();
+  };
+
+  const closeModal = () => {
+    setFocusMovie(null);
+    close();
+  };
+
+  const parseGenres = (genre_ids: number[]) => {
+    return genres.filter(({ id }) => genre_ids.includes(id)).map(({ name }) => GENRES[name]);
+  };
+
+  const focusMovie = (movie: Movie) => {
+    const {
+      movieInfo: { genre_ids, my_vote, overview, title, poster_path, vote_average },
+    } = movie;
+
+    setFocusMovie({ genres: parseGenres(genre_ids), my_vote, overview, title, poster_path, vote_average });
+  };
+
+  const fetchGenres = async () => {
+    const [data, error] = await waitFor(fetchMovieGenres());
+    if (error) throw new Error(JSON.stringify(error));
+
+    setGenres(data.genres);
+  };
+
+  useEffect(() => {
+    fetchGenres();
+  }, []);
+
+  return { isOpen, focusedMovie, openModal, closeModal };
+}
+
+export { useMovieOverviewModal };
