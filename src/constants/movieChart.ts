c9ef0049bd82deaa4514489aa1cfc3b3777eb16e
n0eyes <doll4586633@naver.com>
2023-03-25 23:27:57 +0900

    feat: 장르, 평점 메세지 상수 추가

---
+++ src/constants/movieChart.ts
@@ -0,0 +1,37 @@
+export const INITIAL_PAGE = 1;
+export const PAGE = 1;
+export const LAST_PAGE = 500;
+export const PAGINATION = 1;
+export const DUMMY_AMOUNT = 20;
+
+export const NO_RESULT = '검색 결과가 없습니다';
+
+export const GENRES = {
+  Action: '액션',
+  Adventure: '모험',
+  Animation: '만화',
+  Comedy: '코미디',
+  Crime: '범죄',
+  Documentary: '다큐',
+  Drama: '드라마',
+  Family: '가족',
+  Fantasy: '판타지',
+  History: '역사',
+  Horror: '호러',
+  Music: '음악',
+  Mystery: '미스테리',
+  ['Science Fiction']: 'SF',
+  ['TV Movie']: 'TV 영화',
+  Thriller: '스릴러',
+  War: '전쟁',
+  Western: '서부',
+} as const;
+
+export const MY_VOTE = {
+  0: '별점을 선택하세요',
+  2: '아주 별로예요',
+  4: '별로예요',
+  6: '보통이에요',
+  8: '재밌어요',
+  10: '아주 재밌어요',
+};
