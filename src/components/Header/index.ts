3df26ee150503549dd2e1547a83cd8e35d328954
n0eyes <doll4586633@naver.com>
2023-03-26 06:11:38 +0900

    css: Header 반응형 UI 적용

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,6 +1,7 @@
 import { assemble, Event, useState } from '../../core';
 import { getFormFields, isFormElement } from '../../utils/common/formData';
-import { getElement } from './../../utils/common/domHelper';
+import { isMobile } from '../../utils/common/responsive';
+import { getClosest, getElement, isHTMLElement } from './../../utils/common/domHelper';
 import { $ } from './../../utils/common/domHelper';
 
 export interface HeaderProps {
@@ -19,6 +20,16 @@ const Header = assemble<HeaderProps>(({ handleKeyword }) => {
 
           setKeyword(fields['keyword']);
           handleKeyword(fields['keyword']);
+          $('.search-input')?.classList.add('hidden');
+        }
+      },
+    },
+    {
+      event: 'click',
+      callback(e) {
+        if (getClosest(e.target, '.search-box')) {
+          $('.search-input')?.classList.remove('hidden');
+          ($('.search-input') as HTMLInputElement).focus();
         }
       },
     },
@@ -29,8 +40,8 @@ const Header = assemble<HeaderProps>(({ handleKeyword }) => {
       <h1><a href="./"><img src="./logo.png" alt="MovieList 로고" /></a></h1>
       <div class="search-box">
         <form class="search-form">
-          <input type="text" name="keyword" placeholder="검색" value="${keyword}" />
-          <button class="search-button">검색</button>
+          <input class='search-input hidden' type="text" name="keyword" placeholder="검색" value="${keyword}" />
+          <button type=${isMobile ? 'button' : 'submit'} class="search-button">검색</button>
         </form>
       </div>
     </header>
