db4ad80c5f1a9045e5a80808254e61a5331cb920
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-25 20:03:47 +0900

    feat: app 시작전 로컬스토리지 초기화 로직 추가

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import App from './App';
 
+import { storage } from './utils';
+
+import { PERSONAL_VOTE_KEY } from './constants/storageKey';
+
 import './css/reset.css';
 import './css/common.css';
 import './css/modal.css';
 import './css/detailMovieCard.css';
+
+if (!storage.getLocalStorage(PERSONAL_VOTE_KEY)) {
+  storage.setLocalStorage(PERSONAL_VOTE_KEY, []);
+}
 
 new App();
