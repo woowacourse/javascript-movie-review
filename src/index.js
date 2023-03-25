65f6f12b28fa29ef75a299c2cf5435ef2fcd9141
NaveOWO <pianowell96@gmail.com>
2023-03-26 03:12:56 +0900

    [ style ] 이미지 삽입

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "../templates/logo.png";
 import "../templates/star_filled.png";
+import "../templates/star_empty.png";
+import "../templates/search_button.png";
+import "../templates/bear.png"
+import "../templates/ch-logo.png"
 import { App } from "./App";
 import "../templates/reset.css";
 import "../templates/common.css";
