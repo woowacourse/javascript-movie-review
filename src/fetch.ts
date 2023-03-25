d60dc82e5d328d2f2ecf92692772d12c9ef99980
TaeeunKim <xodms0309@naver.com>
2023-03-25 17:49:33 +0900

    refactor: fetch를 하는 공통 request함수 생성

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,75 +1,37 @@
-import {
-  DetailMovie,
-  FailedResponse,
-  Movie,
-  MovieDataResponse,
-  MovieList,
-  MovieResults,
-  Options,
-} from './types';
-import { handleError } from './utils/handleError';
+import { DetailMovie, Movie, MovieDataResponse, MovieList, MovieResults, Options } from './types';
+import { request } from './utils/request';
 
 export const getAPIUrl = (params: string, options?: Options) =>
   `https://api.themoviedb.org/3${params}?api_key=${process.env.MOVIE_API_KEY}&language=ko-KR&page=${
     options?.page || 1
-  }&query=${options?.query}`;
+  }&query=${options?.query || ''}`;
 
 export const fetchMovies = async (params: string, options: Options): Promise<MovieList> => {
   const API_URL = getAPIUrl(params, options);
-  try {
-    if (!navigator.onLine) {
-      throw new Error('네트워크 오프라인이 감지되었습니다');
-    }
-    const res = await fetch(API_URL);
-    if (!res.ok) {
-      const error: FailedResponse = await res.json();
-      throw new Error(error.status_message);
-    }
 
-    const data: MovieDataResponse = await res.json();
-    const movies: readonly Movie[] = data.results.map((result: MovieResults) => ({
-      id: result.id,
-      title: result.title,
-      posterPath: result.poster_path,
-      voteAverage: result.vote_average,
-    }));
+  const data: MovieDataResponse = await request(API_URL);
+  const movies: readonly Movie[] = data.results.map((result: MovieResults) => ({
+    id: result.id,
+    title: result.title,
+    posterPath: result.poster_path,
+    voteAverage: result.vote_average,
+  }));
 
-    return { movies: movies, totalPages: data.total_pages };
-  } catch (error) {
-    if (error instanceof Error) {
-      handleError(error.message);
-    }
-    throw new Error();
-  }
+  return { movies: movies, totalPages: data.total_pages };
 };
 
 export const fetchMovie = async (params: string) => {
   const API_URL = getAPIUrl(params);
-  try {
-    if (!navigator.onLine) {
-      throw new Error('네트워크 오프라인이 감지되었습니다');
-    }
-    const res = await fetch(API_URL);
-    if (!res.ok) {
-      const error: FailedResponse = await res.json();
-      throw new Error(error.status_message);
-    }
 
-    const data = await res.json();
-    const movie: DetailMovie = {
-      id: data.id,
-      title: data.title,
-      voteAverage: data.vote_average,
-      genres: data.genres,
-      overview: data.overview,
-      posterPath: data.poster_path,
-    };
+  const data = await request(API_URL);
+  const movie: DetailMovie = {
+    id: data.id,
+    title: data.title,
+    voteAverage: data.vote_average,
+    genres: data.genres,
+    overview: data.overview,
+    posterPath: data.poster_path,
+  };
 
-    return movie;
-  } catch (error) {
-    if (error instanceof Error) {
-      handleError(error.message);
-    }
-    throw new Error();
-  }
+  return movie;
 };
