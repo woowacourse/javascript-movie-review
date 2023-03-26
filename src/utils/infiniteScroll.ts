a0204706eeb2d0350aaba49e74f57e79d555a5c6
turtle601 <turtle601@naver.com>
2023-03-27 00:38:12 +0900

    feat(delay): 연속된 fetch 요청에 대비하여 스로틀 적용

--- src/utils/infiniteScroll.ts
+++ src/utils/infiniteScroll.ts
@@ -1,3 +1,5 @@
+import { delayHelper } from './delayHelper';
+
 const options = {
   threshold: 1, // 50%가 viewport에 들어와 있어야 callback 실행
 };
@@ -6,7 +8,7 @@ export const scrollHook = (fn: VoidFunction) => {
   return new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        fn();
+        delayHelper().throttle(() => fn(), 900)();
       }
     });
   }, options);
