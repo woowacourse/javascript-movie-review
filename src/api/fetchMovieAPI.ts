f091823dcceee373c7c353fa31b83333a7ae4946
ashleysyheo <ashleysyheo@gmail.com>
2023-03-27 00:37:27 +0900

    refactor: NodeJS 네임스페이스 만들어서 process.env.API_KEY 타입 추가

--- src/api/fetchMovieAPI.ts
+++ src/api/fetchMovieAPI.ts
@@ -2,9 +2,17 @@ import { MovieData, MovieDataResult, MovieGenreData } from '../types/movie';
 import { generateApiUrl } from './generateApiUrl';
 import { fetchAPI } from './fetchAPI';
 
+function checkAPIKey() {
+  if (!process.env.API_KEY) {
+    throw new Error('API_KEY 환경 변수가 없습니다.');
+  }
+}
+
 async function fetchPopularMovieData(currentPage: number): Promise<MovieDataResult[]> {
+  checkAPIKey();
+
   const apiUrl = generateApiUrl('movie/popular', {
-    api_key: process.env.API_KEY as string,
+    api_key: process.env.API_KEY,
     language: 'ko-KR',
     page: currentPage,
   });
@@ -17,8 +25,10 @@ async function fetchSearchedMovieData(
   searchQuery: string,
   currentPage: number
 ): Promise<MovieDataResult[]> {
+  checkAPIKey();
+
   const apiUrl = generateApiUrl('search/movie', {
-    api_key: process.env.API_KEY as string,
+    api_key: process.env.API_KEY,
     language: 'ko-KR',
     query: searchQuery,
     page: currentPage,
@@ -30,8 +40,10 @@ async function fetchSearchedMovieData(
 }
 
 async function fetchMovieGenreData() {
+  checkAPIKey();
+
   const apiUrl = generateApiUrl('genre/movie/list', {
-    api_key: process.env.API_KEY as string,
+    api_key: process.env.API_KEY,
     language: 'ko-KR',
   });
   const movieGenreData: MovieGenreData = await fetchAPI(apiUrl);
