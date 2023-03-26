6eea3604205ff74c0f52c789bb99c0405630d114
chsua <csj1919@naver.com>
2023-03-26 16:01:37 +0900

    feat: 모달창 컴포넌트 생성

--- src/components/MovieInfoModal.ts
+++ src/components/MovieInfoModal.ts
@@ -2,44 +2,41 @@ import star_filled from '../assets/star_filled.png';
 import star_empty from '../assets/star_empty.png';
 import { makePosterImagePath } from '../utils/makePosterImagePath';
 import { MovieInterface, ScoreType } from '../utils/type';
-import { Event } from '../utils/index';
+import { $, Event } from '../utils/index';
+import { UserScoreStar } from './UserScoreStar';
+import { GENRE, USER_SCORE_TEXT } from '../CONSTANT';
 
 export function MovieInfoModal() {
-  //   { poster_path, title, vote_average }: MovieInterface,
-  //   userScore: ScoreType
   return `
-  <dialog>
-    <div class="modal-movie-name">
+  <dialog class="modal-movie-info">
+    <div class="modal-movie-header">
+        <p class="modal-movie-name"> </p>
         <button class="modal-close">X</button>
     </div>
     <div class="modal-movie-detail">
-        <img src="" class="modal-movie-poster" alt="">
-        <div class="modal-movie-detail-text">
-            <div class="modal-movie-category">장르/ 
-                <img src="${star_empty}" alt="별점" /></p>
-            </div>
-            <P class="modal-movie-story">줄거리</p>
-            <div class="modal-movie-category-star">사용자 별점</div>
-        </div>
+        
     </div>
   </dialog>
      `;
 }
 
-// <dialog>
-// <div class="modal-movie-name">${title}
-//     <button class="modal-close">X</button>
-// </div>
-// <div class="modal-movie-detail">
-//     <img src=${makePosterImagePath(poster_path)} class="modal-movie-poster" alt="${title}">
-//     <div class="modal-movie-detail-text">
-//         <div class="modal-movie-category">장르/
-//             <img src="${
-//               vote_average === 0 ? star_empty : star_filled
-//             }" alt="별점" />${vote_average}</p>
-//         </div>
-//         <P class="modal-movie-story">줄거리</p>
-//         <div class="modal-movie-category-star">사용자 별점</div>
-//     </div>
-// </div>
-// </dialog>
+export function printMovieDetail(movie: MovieInterface, targetUSerScore: ScoreType) {
+  const { poster_path, genre_ids, vote_average, overview, id } = movie;
+  const scoreText = targetUSerScore ? targetUSerScore + USER_SCORE_TEXT[targetUSerScore] : '';
+  console.log(genre_ids);
+  return `
+    <img src="${makePosterImagePath(poster_path)}" class="modal-movie-poster" alt="poster">
+    <div class="modal-movie-detail-text">
+        <div class="modal-movie-category">
+          ${genre_ids.map((id) => GENRE[id]).join(', ')}
+          <img src="${vote_average === 0 ? star_empty : star_filled}" alt="별점" />
+          ${vote_average}
+        </div>
+        <P class="modal-movie-story">${
+          overview === '' ? '해당 영화의 줄거리 정보가 없습니다.' : overview
+        }</p>
+        <div class="modal-movie-star ${id}"> 내 별점 
+          <div class="modal-movie-star">${UserScoreStar(targetUSerScore)}</div>
+          ${scoreText}</div>
+    </div>`;
+}
