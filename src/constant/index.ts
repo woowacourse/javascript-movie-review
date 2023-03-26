d312cf890618872f3b2b24ad7d662f47f1b77e5a
turtle601 <turtle601@naver.com>
2023-03-27 00:38:53 +0900

    refactor(constant): voteMessage 상수 변경

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -14,11 +14,11 @@ const MOVIE_LOCAL_STORAGE_KEY = 'MOVIE_APP';
 
 const VoteMessage = Object.freeze([
   '본인의 점수를 매겨주세요!!',
-  '2점: 최악이예요',
-  '4점: 별로예요',
-  '6점: 보통이에요',
-  '8점: 재미있어요',
-  '10점: 명작이에요',
+  '점: 최악이예요',
+  '점: 별로예요',
+  '점: 보통이에요',
+  '점: 재미있어요',
+  '점: 명작이에요',
 ]);
 
 export { MOVIE_APP_IMG_PATH, MOVIE_LOCAL_STORAGE_KEY, VoteMessage };
