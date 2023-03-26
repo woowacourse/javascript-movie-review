a4b308a28bb0ecec6677b2ef8c73f0462062908a
chsua <csj1919@naver.com>
2023-03-26 21:34:53 +0900

    feat: 영화 별점 영역 이벤트 무한발생/삭제 방지 위해 최초 1회 랜더링으로 수정

--- src/components/UserScoreStar.ts
+++ src/components/UserScoreStar.ts
@@ -3,19 +3,18 @@ import star_empty from '../assets/star_empty.png';
 import { ScoreType } from '../utils/type';
 import { $, Event } from '../utils/index';
 import { USER_SCORE_TEXT } from '../CONSTANT';
+import MovieData from '../data/MovieData';
 
 export function scoreStars() {
-  //계속 hover되는 문제점 발생
   Event.addEvent('mouseover', '.modal-movie-star', (event) => {
     const target = event.target as HTMLElement;
     const targetImgElem = target.closest('img') as HTMLImageElement;
-    if (!targetImgElem) return;
+    if (targetImgElem === null) return;
 
     const starNumber = (Number(targetImgElem.id.split('-')[2]) * 2) as ScoreType;
-    console.log(starNumber);
 
     const starArea = $('.modal-movie-star-area') as HTMLElement;
-    starArea.children[0].innerHTML = showStar(starNumber);
+    showStar(starNumber);
 
     const scoreText = starNumber ? starNumber + ' ' + USER_SCORE_TEXT[starNumber] : '';
     starArea.children[1].textContent = `${scoreText}`;
@@ -24,34 +23,37 @@ export function scoreStars() {
   Event.addEvent('click', '.modal-movie-star', (event) => {
     const target = event.target as HTMLElement;
     const targetImgElem = target.closest('img') as HTMLImageElement;
-    if (targetImgElem) console.log(targetImgElem.id);
+    if (targetImgElem === null) return;
+
+    const starNumber = (Number(targetImgElem.id.split('-')[2]) * 2) as ScoreType;
+    const targetId = Number(targetImgElem.parentElement?.parentElement?.classList[1]);
+
+    MovieData.giveUserScore(targetId, starNumber);
   });
 
   return `
     <div class="modal-movie-star">
-
-    </div>`;
+      <img id="star-index-1" src="${star_empty}" alt="빈 별" />
+      <img id="star-index-2" src="${star_empty}" alt="빈 별" />
+      <img id="star-index-3" src="${star_empty}" alt="빈 별" />
+      <img id="star-index-4" src="${star_empty}" alt="빈 별" />
+      <img id="star-index-5" src="${star_empty}" alt="빈 별" />
+    </div>
+    `;
 }
 
 function transStarColor() {}
 
 export function showStar(score: ScoreType) {
-  const scoreNumber = score === undefined ? 0 : score;
-  const starArray = new Array(5)
-    .fill(null)
-    .map((_, index) => (index < scoreNumber / 2 ? 'filled' : 'empty'));
-
-  return `
-    ${starTemplate(starArray)}`;
-}
-
-function starTemplate(starArray: ('filled' | 'empty')[]) {
-  return starArray
-    .map((state, index) => {
-      return `<img id="star-index-${index + 1}" src="${
-        state === 'empty' ? star_empty : star_filled
-      }" alt="${state === 'empty' ? '빈 별' : '채워진 별'}" />
-  `;
-    })
-    .join('');
+  if (score === undefined) return;
+  const stars = $('.modal-movie-star') as HTMLElement;
+  for (let i = 0; i <= 4; i++) {
+    if (i < score / 2) {
+      stars.children[i].setAttribute('src', star_filled);
+      stars.children[i].setAttribute('alt', '채워진 별');
+    } else {
+      stars.children[i].setAttribute('src', star_empty);
+      stars.children[i].setAttribute('alt', '빈 별');
+    }
+  }
 }
