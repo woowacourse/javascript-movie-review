414aac05042b5f23cecf29f79fc96d2f260f4ba5
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-26 16:22:24 +0900

    feat: 영화 별점 별 코멘트 상수화

---
+++ src/constants/ratingComments.ts
@@ -0,0 +1,10 @@
+const COMMENT_BY_RATING: { [score: number]: string } = {
+  0: '평가해주세요',
+  2: '최악이예요',
+  4: '별로예요',
+  6: '보통이에요',
+  8: '재미있어요',
+  10: '명작이에요',
+};
+
+export default COMMENT_BY_RATING;
