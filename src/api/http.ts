e8d6d66da0ca51a7fca836d9943e520dd40784ec
hozzijeong <ghwls0905@naver.com>
2023-03-26 11:23:35 +0900

    refactor: network 에러 발생시 throw 위치 변경

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,13 +1,13 @@
 import { MESSAGE, statusCodeToErrorMessage } from './statusCode';
 
 export const fetchData = async <T>(url: string): Promise<T> => {
-  const response = await fetch(url);
-
-  const data = await response.json();
-
   if (!navigator.onLine) {
     throw new Error(MESSAGE.OFFLINE);
   }
+
+  const response = await fetch(url);
+
+  const data = await response.json();
 
   if (!response.ok) {
     throw new Error(statusCodeToErrorMessage(data.success_code));
