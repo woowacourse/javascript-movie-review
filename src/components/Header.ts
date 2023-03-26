d75bee3802a62b08c9f5a30e1859e5a59905c894
guridaek <xylitol79@gmail.com>
2023-03-26 13:51:58 +0900

    feat: 검색 창 반응형(모바일) 적용

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,6 +1,8 @@
 import logo from '../assets/logo.png';
 import { Component } from '../type/Component';
 
+const MOBILE_WIDTH = 768;
+
 export default class Header implements Component {
   #renderer;
   $element;
@@ -20,6 +22,7 @@ export default class Header implements Component {
 
   render() {
     this.$element.innerHTML = this.template();
+    if (window.outerWidth <= MOBILE_WIDTH) this.setEventForMobile();
     this.setEvent();
   }
 
@@ -27,27 +30,59 @@ export default class Header implements Component {
     return `    
     <h1><img class="main-logo" src=${logo} alt="MovieList 로고"/></h1>
     <form class="search-box">
-      <input name='query'type="text" placeholder="검색" />
+      <input name='query'type="text" placeholder="검색" required/>
       <button class="search-button">검색</button>
     </form>`;
   }
 
   setEvent() {
-    this.$element.querySelector('.search-box')?.addEventListener('submit', this.onSubmitSerachForm.bind(this));
-    this.$element.querySelector('.main-logo')?.addEventListener('click', this.onClickMainLogo.bind(this));
+    (<HTMLFormElement>this.$element.querySelector('.search-box')).addEventListener(
+      'submit',
+      this.onSubmitSerachForm.bind(this)
+    );
+    (<HTMLImageElement>this.$element.querySelector('.main-logo')).addEventListener(
+      'click',
+      this.onClickMainLogo.bind(this)
+    );
+  }
+
+  setEventForMobile() {
+    this.hideSearchInput();
+    
+    (<HTMLButtonElement>this.$element.querySelector('.search-button')).addEventListener(
+      'click',
+      this.onClickSearchButton.bind(this),
+      { once: true }
+    );
   }
 
   onSubmitSerachForm(e: Event) {
     e.preventDefault();
 
     const $form = <HTMLFormElement>e.target;
-    const $query = <HTMLInputElement>$form.querySelector('input[name="query"]');
+    const $input = <HTMLInputElement>$form.querySelector('input[name="query"]');
 
-    this.#renderer.searchedMovieList($query.value);
+    this.#renderer.searchedMovieList($input.value);
+  }
+
+  onClickSearchButton(e: Event) {
+    e.preventDefault();
+
+    this.showSearchInput();
   }
 
   onClickMainLogo() {
+    this.render();
     this.#renderer.popularMovieList();
-    (<HTMLFormElement>this.$element.querySelector('.search-box')).reset();
+  }
+
+  hideSearchInput() {
+    const $input = <HTMLInputElement>this.$element.querySelector('input[name="query"]');
+    $input.classList.add('hide');
+  }
+
+  showSearchInput() {
+    const $input = <HTMLInputElement>this.$element.querySelector('input[name="query"]');
+    $input.classList.remove('hide');
   }
 }
