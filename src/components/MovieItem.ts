33f38480d528cc546abf9af32fff1a42add4f9ef
chsua <csj1919@naver.com>
2023-03-26 16:07:01 +0900

    feat: movie 객체의 평점 속성이 0인 경우 빈 별 보여주기 기능 구현

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -1,11 +1,12 @@
 import star_filled from '../assets/star_filled.png';
+import star_empty from '../assets/star_empty.png';
 import { MovieInterface } from '../utils/type';
 import { makePosterImagePath } from '../utils/makePosterImagePath';
 
-export function MovieItem({ poster_path, title, vote_average }: MovieInterface) {
+export function MovieItem({ id, poster_path, title, vote_average }: MovieInterface) {
   return `
-  <li>
-        <a href="#">
+  <li id=${id}>
+        <a href="#$">
             <div class="item-card">
                 <img
                 class="item-thumbnail movie-poster-background"
@@ -14,7 +15,9 @@ export function MovieItem({ poster_path, title, vote_average }: MovieInterface) 
                 alt=${title}
                 />
                 <p class="item-title">${title}</p>
-                <p class="item-score"><img src="${star_filled}" alt="별점" />${vote_average}</p>
+                <p class="item-score"><img src="${
+                  vote_average === 0 ? star_empty : star_filled
+                }" alt="별점" />${vote_average}</p>
             </div>
         </a>
     </li>`;
