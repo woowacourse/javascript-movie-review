50d8cb558ad834505d1e3dd6d1e7c6cdcba3135a
chsua <csj1919@naver.com>
2023-03-26 16:05:35 +0900

    feat: index에 최초 모달창 템플릿 생성

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import { MovieInterface } from '../utils/type';
-
 interface IEvent {
   targetSelector: string;
   event: keyof HTMLElementEventMap;
@@ -13,6 +11,9 @@ export const render = (template: string) => {
   const bodyElem = $('#app') as HTMLElement;
   bodyElem.innerHTML = template;
   Event.setAllEvents();
+  const dialogElem = $('dialog') as HTMLDialogElement;
+  // dialogElem.showModal();
+  // dialogElem.close();
 };
 
 const events: IEvent[] = [];
@@ -31,10 +32,3 @@ export const Event = {
     });
   },
 };
-
-export function toggleMoreButton(result: MovieInterface[]) {
-  const moreButton = $('.view-more-button') as HTMLElement;
-
-  if (result.length >= 20 && result.length > 0) return (moreButton.style.display = 'inline-block');
-  return (moreButton.style.display = 'none');
-}
