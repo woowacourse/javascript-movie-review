725065a59ef83a321ec70b83974aecb3711a4d28
sh981013s <sh981013s@gmail.com>
2023-03-27 09:34:37 +0900

    test: 테스트 파일 분리

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,4 +1,7 @@
-describe("영화 리뷰 애플리케이션 popular 영화 테스트", () => {
+import "./popularMovies.spec";
+import "./movieDetailModal.spec";
+
+describe("영화 리뷰 애플리케이션 초기 렌더링 테스트", () => {
   beforeEach(() => {
     cy.intercept(
       {
@@ -27,91 +30,5 @@ describe("영화 리뷰 애플리케이션 popular 영화 테스트", () => {
 
   it("영화 데이터를 받아오기 전까지 스켈레톤 UI를 렌더링 해야한다.", () => {
     cy.get(".skeleton").should("exist");
-  });
-
-  it("하단부로 내리면 누르면 영화 데이터 20개를 추가로 받아서 렌더링 해야 한다.", () => {
-    cy.get(".item-card:not(.skeleton)").should("have.length", 20);
-
-    cy.document().then((document) => {
-      document.documentElement.scrollTop = document.documentElement.scrollHeight;
-    });
-
-    cy.wait(1000);
-    cy.get(".item-card:not(.skeleton)").should("have.length", 40);
-  });
-
-  it("검색창에 '해리포터'를 검색하면 해리포터 시리즈 영화 8개가 존재해야 한다.", () => {
-    cy.get(".search-input").type("해리포터");
-    cy.get(".search-box").submit();
-
-    cy.get(".item-card:not(.skeleton)").should("have.length", 8);
-  });
-
-  it("검색창에 '해리포터'를 검색하면 상단에 '해리포터 검색결과' 문구가 존재해야 한다.", () => {
-    cy.get(".search-input").type("해리포터");
-    cy.get(".search-box").submit();
-
-    cy.get(".search-title").should("contain", '"해리포터" 검색결과');
-  });
-
-  it("검색 되지 않는 영화를 검색 할 경우 검색 결과가 없다는 메시지가 출력되어야 한다.", () => {
-    cy.get(".search-input").type("해리포터10");
-    cy.get(".search-box").submit();
-
-    cy.get(".search-title").should("contain", '"해리포터10"에 대한 검색 결과가 없습니다 :(');
-  });
-
-  it("영화를 눌렀을 때 모달이 보여야 한다.", () => {
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get(".modal-container").should("be.visible");
-  });
-
-  it("모달의 우측 상단에 위치한 닫기 버튼을 눌렀을 때 모달이 닫혀야 한다.", () => {
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get(".close-button").click();
-    cy.get(".modal-container").should("not.be.visible");
-  });
-
-  it("모달이 열려있을 때 바깥 영역을 누르면 모달이 닫혀야 한다.", () => {
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get("body").click(0, 0);
-    cy.get(".modal-container").should("not.be.visible");
-  });
-
-  it("모달이 열려있을 때 별점을 등록 가능해야 한다.", () => {
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get(".user-rate-container img").first().click();
-    cy.get(".score").should("have.text", "2");
-    cy.get(".score-comment").should("have.text", "최악이예요");
-  });
-
-  it("모달이 열려있을 때 별점을 등록 후 다시 진입 시 남긴 별점이 있어야 한다..", () => {
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get(".user-rate-container img").first().click();
-    cy.get(".score").should("have.text", "2");
-    cy.get(".score-comment").should("have.text", "최악이예요");
-    cy.get("body").click(0, 0);
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get(".score").should("have.text", "2");
-    cy.get(".score-comment").should("have.text", "최악이예요");
-  });
-
-  it("등록한 별점과 같은 별점을 누르면 별점 등록이 초기화 되어야 한다.", () => {
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get(".user-rate-container img").first().click();
-    cy.get(".score").should("have.text", "2");
-    cy.get(".score-comment").should("have.text", "최악이예요");
-    cy.get(".user-rate-container img").first().click();
-    cy.get(".score-comment").should("not.have.text", "최악이예요");
-  });
-
-  it("등록한 별점과 다른 별점을 누르면 별점이 바뀌어야 한다.", () => {
-    cy.get(".item-card").first().scrollIntoView().click({ force: true });
-    cy.get(".user-rate-container img").first().click();
-    cy.get(".score").should("have.text", "2");
-    cy.get(".score-comment").should("have.text", "최악이예요");
-    cy.get(".user-rate-container img").eq(3).click();
-    cy.get(".score").should("have.text", "8");
-    cy.get(".score-comment").should("have.text", "재미있어요");
   });
 });
