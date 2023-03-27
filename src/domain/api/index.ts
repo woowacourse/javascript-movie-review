94ac45503f67f21b9aaddb04667c0d20ffc7a36d
semnil5202 <semnil5202@gmail.com>
2023-03-27 17:12:45 +0900

    refactor: 각각의 Url대신 Base Url로 변경

--- src/domain/api/index.ts
+++ src/domain/api/index.ts
@@ -1,14 +1,14 @@
 import { FetchingDetailOfMovieType, FetchingMovieType } from '../../type/movie';
 
 const API_KEY = process.env.API_KEY;
-const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=KO`;
-const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=KO`;
-const detailUrl = `https://api.themoviedb.org/3/movie`;
+const BASE_URL = `https://api.themoviedb.org/3/`;
 
 export const getApiPopularMovie = async (
   page: number
 ): Promise<FetchingMovieType> => {
-  const fetchingData = await fetch(`${popularUrl}&page=${page}`);
+  const fetchingData = await fetch(
+    `${BASE_URL}movie/popular?api_key=${API_KEY}&language=KO&page=${page}`
+  );
 
   const status = fetchingData.status;
   const movieList = await fetchingData.json();
@@ -21,7 +21,7 @@ export const getApiSearchMovie = async (
   page: number
 ): Promise<FetchingMovieType> => {
   const fetchingData = await fetch(
-    `${searchUrl}s&query=${query}&page=${page}&include_adult=false`
+    `${BASE_URL}search/movie?api_key=${API_KEY}&language=KO&query=${query}&page=${page}&include_adult=false`
   );
 
   const status = fetchingData.status;
@@ -34,7 +34,7 @@ export const getApiDetailMovie = async (
   id: number
 ): Promise<FetchingDetailOfMovieType> => {
   const fetchingData = await fetch(
-    `${detailUrl}/${id}?api_key=${API_KEY}&language=KO`
+    `${BASE_URL}movie/${id}?api_key=${API_KEY}&language=KO`
   );
 
   const status = fetchingData.status;
