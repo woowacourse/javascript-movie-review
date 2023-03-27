f49f42cd45b5823b04cae54376d17f81930665bc
hozzijeong <ghwls0905@naver.com>
2023-03-27 14:56:56 +0900

    refactor: Object.freeze -> as const 로 변경

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
 export const TMDB_MOVIE_BASE_URL = 'https://api.themoviedb.org/3';
 
-export const STAR_DESCRIPTION: { [key: number]: string } = Object.freeze({
+export const STAR_DESCRIPTION: { [key: number]: string } = {
   0: '별점이 없습니다.',
   2: '최악이예요.',
   4: '별로예요.',
   6: '보통이에요.',
   8: '재미있어요.',
   10: '명작이에요.',
-});
+} as const;
