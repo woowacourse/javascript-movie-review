135d7e995e9d33ab310e2304fd4bd5243573adf2
woo-jk <201702042@o.cnu.ac.kr>
2023-03-27 20:46:47 +0900

    fix: 스켈레톤 ui 모바일 화면에서 레이아웃 깨지는 문제 해결

--- src/components/MovieList/SkeletonList/index.js
+++ src/components/MovieList/SkeletonList/index.js
@@ -14,13 +14,11 @@ class SkeletonList {
       { length: 20 },
       () => `
     <li>
-    <a href="#">
       <div class="item-card skeleton">
         <div class="item-thumbnail skeleton"></div>
         <div class="item-title skeleton"></div>
         <div class="item-score skeleton"></div>
       </div>
-    </a>
     </li>`
     ).join("");
   }
