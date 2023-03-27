6e993d686d0ba41a8f094a49b94f49a12ede7e5b
Suyoung <jsy8492@gmail.com>
2023-03-27 15:57:09 +0900

    feat: 영화 상세 정보를 fetch하여 모달에 보여주는 기능 구현

--- src/components/MovieListSection.ts
+++ src/components/MovieListSection.ts
@@ -2,9 +2,9 @@ import MovieDetailModal from './MovieDetailModal';
 
 import CustomModal from './common/CustomModal';
 
-import { SKELETON_TEMPLATE } from '../constants';
-
 import { $, dispatchCustomEvent } from '../utils/domUtils';
+
+import { fetchMovieDetail } from '../domain/remotes/movieDetail';
 
 class MovieListSection extends HTMLElement {
   constructor() {
@@ -13,9 +13,6 @@ class MovieListSection extends HTMLElement {
       <section class="item-view">
         <h2 id="movie-list-title">지금 인기 있는 영화</h2>
         <ul class="item-list"></ul>
-        <ul class="item-list" id="skeleton-list">
-        ${SKELETON_TEMPLATE}
-        </ul>
         <div id="scroll-observer"></div>
       </section>
     `;
@@ -55,8 +52,8 @@ class MovieListSection extends HTMLElement {
   async fetchMovieDetail(movieId: number) {
     const $movieDetailModal = $('movie-detail-modal') as MovieDetailModal;
     $movieDetailModal.showSkeleton();
-    // const movieDetail = await fetchMovieDetail(movieId);
-    // $movieDetailModal.render(movieDetail);
+    const movieDetail = await fetchMovieDetail(movieId);
+    $movieDetailModal.render(movieDetail);
   }
 }
 
