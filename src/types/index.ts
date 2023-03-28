d5f12556619c6cd2fa742d31c212ba8439b352e7
hozzijeong <ghwls0905@naver.com>
2023-03-28 23:43:22 +0900

    feat: event brocker 코드 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,5 @@
 export type MovieListCategory = 'popular' | 'search';
+
+export type UpdateMovieListEvent = CustomEvent<{ keyword: string }>;
+export type AppendMovieListEvent = CustomEvent<{ keyword: string }>;
+export type DetailMovieEvent = CustomEvent<{ movieId: string }>;
