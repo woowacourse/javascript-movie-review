4146db718a48a791ccb47b35e452e8488c0e95a5
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-03-29 00:50:44 +0900

    refactor: 점수 매기기 박스 로직 수정

--- src/components/RatingBox.ts
+++ src/components/RatingBox.ts
@@ -20,24 +20,10 @@ export default class RatingBox extends HTMLElement {
   render() {
     const { myRating } = this.#state;
 
-    this.innerHTML = `
+    this.innerHTML = /*html*/ `
       <div class="rating-box flex align-center">
         <p>내 별점</p>
-        <img class="star star-${
-          myRating >= 2 ? "filled" : "empty"
-        } mr-4" alt="별점" />
-        <img class="star star-${
-          myRating >= 4 ? "filled" : "empty"
-        } mr-4" alt="별점" />
-        <img class="star star-${
-          myRating >= 6 ? "filled" : "empty"
-        } mr-4" alt="별점" />
-        <img class="star star-${
-          myRating >= 8 ? "filled" : "empty"
-        } mr-4" alt="별점" />
-        <img class="star star-${
-          myRating >= 10 ? "filled" : "empty"
-        } mr-4" alt="별점" />
+        <div class="star-box">${this.starBoxTemplate()}</div>
         <h4 class="rating-count">${myRating}</h4>
         <h4 class="comment">${COMMENT[myRating]}</h4>
       </div>
@@ -45,27 +31,57 @@ export default class RatingBox extends HTMLElement {
   }
 
   setEvent() {
-    const $stars = this.querySelectorAll(".star");
+    const $starBox = this.querySelector(".star-box");
 
-    $stars?.forEach((star, index) => {
-      star.addEventListener("click", () => {
-        this.setState({
-          myRating: (index + 1) * SINGLE_STAR_SCORE,
-        });
-        this.setEvent();
-        this.dispatchEvent(
-          new CustomEvent("set-my-rating", {
-            bubbles: true,
-            detail: { movieId: this.movieId, myRating: this.#state.myRating },
-          })
-        );
+    $starBox?.addEventListener("click", (event) => {
+      const $stars = this.querySelectorAll(".star");
+      $stars?.forEach((star, index) => {
+        if (event.target === star)
+          this.setState({
+            myRating: (index + 1) * SINGLE_STAR_SCORE,
+          });
       });
+      this.rerenderStarBox();
+      this.dispatchEvent(
+        new CustomEvent("set-my-rating", {
+          bubbles: true,
+          detail: { movieId: this.movieId, myRating: this.#state.myRating },
+        })
+      );
     });
   }
 
   setState(newState: object) {
     this.#state = { ...this.#state, ...newState };
-    this.render();
+  }
+
+  rerenderStarBox() {
+    const { myRating } = this.#state;
+
+    const $starBox = this.querySelector(".star-box") as HTMLDivElement;
+    $starBox.innerHTML = this.starBoxTemplate();
+
+    const $ratingCount = this.querySelector(
+      ".rating-count"
+    ) as HTMLHeadingElement;
+    $ratingCount.innerText = String(myRating);
+
+    const $comment = this.querySelector(".comment") as HTMLHeadingElement;
+    $comment.innerText = COMMENT[myRating];
+  }
+
+  starBoxTemplate() {
+    const { myRating } = this.#state;
+
+    return /*html*/ `          
+      <img class="star star-${myRating >= 2 ? "filled" : "empty"}" alt="별점" />
+      <img class="star star-${myRating >= 4 ? "filled" : "empty"}" alt="별점" />
+      <img class="star star-${myRating >= 6 ? "filled" : "empty"}" alt="별점" />
+      <img class="star star-${myRating >= 8 ? "filled" : "empty"}" alt="별점" />
+      <img class="star star-${
+        myRating >= 10 ? "filled" : "empty"
+      }" alt="별점" />
+`;
   }
 }
 
