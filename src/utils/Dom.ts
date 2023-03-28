7563e0e99ae6f61b38f5e3713cc30ddb5d0f8870
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-03-29 01:01:27 +0900

    refactor: 불필요한 유틸 함수 제거

--- src/utils/Dom.ts
+++ src/utils/Dom.ts
@@ -1,7 +1,2 @@
 export const $ = (selector: string): HTMLElement | null =>
   document.querySelector(selector);
-
-export const $$ = (
-  selector: string,
-  element: HTMLElement
-): HTMLElement | null => document.querySelector(selector);
