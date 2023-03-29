7946f731d7a6a29e1cae894bb28520867459e635
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-03-29 17:42:36 +0900

    refactor: typeGuards 키 검사 선언형으로 수정

--- src/constants/movieApi.ts
+++ src/constants/movieApi.ts
@@ -9,3 +9,33 @@ export const requestUrl = Object.freeze({
     return `https://ornate-swan-ce5a5e.netlify.app/tmdb/movie/${id}?language=ko-KR`;
   },
 });
+
+export const movieRootKeys = Object.freeze(['page', 'results', 'total_pages', 'total_results']);
+
+export const movieDetailRootKeys = Object.freeze([
+  'adult',
+  'backdrop_path',
+  'belongs_to_collection',
+  'budget',
+  'genres',
+  'homepage',
+  'id',
+  'imdb_id',
+  'original_language',
+  'original_title',
+  'overview',
+  'popularity',
+  'poster_path',
+  'production_companies',
+  'production_countries',
+  'release_date',
+  'revenue',
+  'runtime',
+  'spoken_languages',
+  'status',
+  'tagline',
+  'title',
+  'video',
+  'vote_average',
+  'vote_count',
+]);
--- src/types/typeGuards.ts
+++ src/types/typeGuards.ts
@@ -1,39 +1,22 @@
+import { movieDetailRootKeys, movieRootKeys } from '../constants/movieApi';
 import { MovieDetailRoot, MovieRoot } from './movieApi';
 
 export const isMovieRoot = (root: unknown): root is MovieRoot => {
   const value = root as MovieRoot;
 
-  return 'page' in value && 'results' in value && 'total_pages' in value && 'total_results' in value;
+  if (!root || typeof root !== 'object') {
+    return false;
+  }
+
+  return movieRootKeys.every(key => key in value);
 };
 
 export const isMovieDetailRoot = (root: unknown): root is MovieDetailRoot => {
   const value = root as MovieDetailRoot;
 
-  return (
-    'adult' in value &&
-    'backdrop_path' in value &&
-    'belongs_to_collection' in value &&
-    'budget' in value &&
-    'genres' in value &&
-    'homepage' in value &&
-    'id' in value &&
-    'imdb_id' in value &&
-    'original_language' in value &&
-    'original_title' in value &&
-    'overview' in value &&
-    'popularity' in value &&
-    'poster_path' in value &&
-    'production_companies' in value &&
-    'production_countries' in value &&
-    'release_date' in value &&
-    'revenue' in value &&
-    'runtime' in value &&
-    'spoken_languages' in value &&
-    'status' in value &&
-    'tagline' in value &&
-    'title' in value &&
-    'video' in value &&
-    'vote_average' in value &&
-    'vote_count' in value
-  );
+  if (!root || typeof root !== 'object') {
+    return false;
+  }
+
+  return movieDetailRootKeys.every(key => key in value);
 };
