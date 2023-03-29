76f73241f766b643c3ca0b010b6b6a37f46322a8
Gilpop8663 <wolfye@naver.com>
2023-03-29 17:32:44 +0900

    refactor: 도메인 로직 분리 및 함수명 변경

--- src/domain/userMovieScore.ts
+++ src/domain/userMovieScore.ts
@@ -3,39 +3,68 @@ import { MovieScoreInfo } from '../types/type';
 const MOVIE_SCORE_KEY = 'movieScore';
 
 const userMovieScore = {
-  getLocalStorage: (): MovieScoreInfo[] => {
+  getUserData: (): MovieScoreInfo[] => {
     return JSON.parse(localStorage.getItem(MOVIE_SCORE_KEY) || '[]');
   },
 
   getScore: (id: number): number => {
-    const movieScore: MovieScoreInfo[] = userMovieScore.getLocalStorage();
+    const EMPTY_SCORE = 0;
+    const movieScore: MovieScoreInfo[] = userMovieScore.getUserData();
     const movie = movieScore.find((item: MovieScoreInfo) => item.id === id);
 
     if (!movie) {
-      return 0;
+      return EMPTY_SCORE;
     }
 
     return movie.score;
   },
 
-  setLocalStorage: (userScore: MovieScoreInfo): void => {
-    const movieScore: MovieScoreInfo[] = userMovieScore.getLocalStorage();
+  setUserData: (userScore: MovieScoreInfo): void => {
+    const movieScore: MovieScoreInfo[] = userMovieScore.getUserData();
 
-    const findIndex = movieScore.findIndex((item: MovieScoreInfo) => item.id === userScore.id);
+    const movieTargetIndex = movieScore.findIndex((item: MovieScoreInfo) => item.id === userScore.id);
 
-    if (findIndex === -1) {
-      const updatedMovieScore = [...movieScore, userScore];
-      localStorage.setItem(MOVIE_SCORE_KEY, JSON.stringify(updatedMovieScore));
+    if (movieTargetIndex === -1) {
+      userMovieScore.create({ movieScore, userScore });
+      return;
+    }
+    if (userScore.score === movieScore[movieTargetIndex].score) {
+      userMovieScore.delete({ movieScore, movieTargetIndex });
       return;
     }
 
+    userMovieScore.update({ movieScore, userScore, movieTargetIndex });
+  },
+
+  create: ({ movieScore, userScore }: { movieScore: MovieScoreInfo[]; userScore: MovieScoreInfo }) => {
+    const updatedMovieScore = [...movieScore, userScore];
+
+    localStorage.setItem(MOVIE_SCORE_KEY, JSON.stringify(updatedMovieScore));
+  },
+
+  delete: ({ movieScore, movieTargetIndex }: { movieScore: MovieScoreInfo[]; movieTargetIndex: number }): void => {
     const updatedMovieScore = movieScore;
-    updatedMovieScore.splice(findIndex, 1, userScore);
+
+    updatedMovieScore.splice(movieTargetIndex, 1);
+    localStorage.setItem(MOVIE_SCORE_KEY, JSON.stringify(updatedMovieScore));
+  },
+
+  update: ({
+    movieScore,
+    movieTargetIndex,
+    userScore,
+  }: {
+    movieScore: MovieScoreInfo[];
+    movieTargetIndex: number;
+    userScore: MovieScoreInfo;
+  }): void => {
+    const updatedMovieScore = movieScore;
+    updatedMovieScore.splice(movieTargetIndex, 1, userScore);
     localStorage.setItem(MOVIE_SCORE_KEY, JSON.stringify(updatedMovieScore));
   },
 
   getIsReviewed: (id: string): boolean => {
-    const movieScore: MovieScoreInfo[] = JSON.parse(localStorage.getItem(MOVIE_SCORE_KEY) || '[]');
+    const movieScore: MovieScoreInfo[] = userMovieScore.getUserData();
 
     return movieScore.some(movie => movie.id === Number(id));
   },
