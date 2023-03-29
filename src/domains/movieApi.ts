7e57439dc87fede7f6d97ed6a7b3d6058f3eb252
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-03-30 04:11:11 +0900

    fix: api 통신 간의 에러 처리

--- src/domains/movieApi.ts
+++ src/domains/movieApi.ts
@@ -1,18 +1,24 @@
 import { requestUrl } from '../constants/movieApi';
 import { MovieDetailRoot, MovieRoot } from '../types/movieApi';
 
-const fetchMovieList = async (requestUrl: string): Promise<MovieRoot | MovieDetailRoot> => {
-  return await (await fetch(requestUrl)).json();
+const fetchMovieList = async (requestUrl: string): Promise<MovieRoot | MovieDetailRoot | null> => {
+  try {
+    return await (await fetch(requestUrl)).json();
+  } catch (error) {
+    console.error(error);
+
+    return null;
+  }
 };
 
-export const getPopularMovieList = async (): Promise<MovieRoot | MovieDetailRoot> => {
-  return await fetchMovieList(requestUrl.getPopularMovie(1));
+export const getPopularMovieList = async (): Promise<MovieRoot | MovieDetailRoot | null> => {
+  return fetchMovieList(requestUrl.getPopularMovie(1));
 };
 
 export const searchMovieList = async (
   query: FormDataEntryValue,
   page: number
-): Promise<MovieRoot | MovieDetailRoot> => {
+): Promise<MovieRoot | MovieDetailRoot | null> => {
   const url = query ? requestUrl.getSearchMovie(query, page) : requestUrl.getPopularMovie(page);
 
   return await fetchMovieList(url);
@@ -21,12 +27,12 @@ export const searchMovieList = async (
 export const getMoreMovieList = async (
   query: FormDataEntryValue,
   nextPage: number
-): Promise<MovieRoot | MovieDetailRoot> => {
+): Promise<MovieRoot | MovieDetailRoot | null> => {
   const url = query ? requestUrl.getSearchMovie(query, nextPage) : requestUrl.getPopularMovie(nextPage);
 
   return await fetchMovieList(url);
 };
 
-export const getMovieDetails = async (id: string): Promise<MovieRoot | MovieDetailRoot> => {
+export const getMovieDetails = async (id: string): Promise<MovieRoot | MovieDetailRoot | null> => {
   return await fetchMovieList(requestUrl.getMovieDetail(id));
 };
