8861cade58c03804ad850a4106695d5f1b0ef83c
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-30 02:57:19 +0900

    test: 모달창 및 모달  별점 테스트 추가

--- cypress/e2e/popularMovieTest.cy.js
+++ cypress/e2e/popularMovieTest.cy.js
@@ -38,19 +38,32 @@ describe('popular movie test', () => {
     });
   });
 
-  it('더보기 버튼 클릭 시 20개의 스켈레톤이 발생한다.', () => {
-    cy.get('.btn').click();
+  it('스크롤을 맨 밑으로 내리면 20개의 스켈레톤이 발생한다.', () => {
+    cy.scrollTo('bottom', { duration: 3000 });
 
     cy.wait('@moviePopular').then(interception => {
       cy.get('.item-thumbnail.skeleton').should('have.length', 20);
     });
   });
 
-  it('더보기 버튼 클릭 시 40개의 아이템이 로딩된다..', () => {
-    cy.get('.btn').click();
+  it('스크롤을 맨 밑으로 내리면 총 40개의 아이템이 로딩된다.', () => {
+    cy.scrollTo('bottom', { duration: 3000 });
 
     cy.wait('@moviePopular').then(interception => {
       cy.get('.movie-list .item-card').should('have.length', 40);
     });
+  });
+
+  it('영화 아이템 클릭하면 이미지가 렌더링된다.', () => {
+    cy.wait(1000);
+    cy.get('.item-thumbnail').first().click();
+    cy.get('.modal-image').should('be.visible');
+  });
+
+  it('모달 창에서 왼쪽 첫 번째 별 클릭 시에 "최악이예요" 문구가 출력된다.', () => {
+    cy.wait(1000);
+    cy.get('.item-thumbnail').first().click();
+    cy.get('.rating-stars>img').first().click();
+    cy.get('.rating-word').should('have.text', '최악이예요');
   });
 });
