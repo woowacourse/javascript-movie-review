24fc9fc8cacdb0dace76fe7d5e63955b30c8f5e1
JIWON <afds4567@gmail.com>
2023-03-30 16:49:28 +0900

    [ fix ] 이미지 로드 에러 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import "../templates/logo.png";
-import "../templates/star_filled.png";
+import "./img/logo.png";
+import "./img/star_filled.png";
+import "./img/star_empty.png";
+import "./img/search_button.png";
+
 import { App } from "./App";
 import "../templates/reset.css";
 import "../templates/common.css";
