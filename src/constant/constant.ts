ee45555835047e159fdf5746d753b75b7678b85d
JIWON <afds4567@gmail.com>
2023-03-30 15:12:31 +0900

    [ refactor ] 상수화

---
+++ src/constant/constant.ts
@@ -0,0 +1,24 @@
+export const MOVIE_DATA = {
+  INITIAL_PAGE: 1
+}
+
+export const CURRENT_TAB = {
+  POPULAR: "POPULAR",
+  SEARCH: "SEARCH"
+}
+
+
+export const URL = {
+  BASE_URL: "https://api.themoviedb.org/3"
+}
+
+export const TOTAL_STAR_CNT = 5;
+
+export const SCORE_COMMENT: { [key: number]: string } = {
+  0: "별점을 입력해주세요",
+  2: "최악이예요",
+  4: "별로예요",
+  6: "보통이에요",
+  8: "재미있어요",
+  10: "명작이에요",
+};
