5822ebb133a724743de530b726da1caee8678de6
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-30 23:06:50 +0900

    refactor(domselector): 요소가 지정되지 않으면 자동으로 document를 사용하도록 개선

--- src/utils/domSelector.ts
+++ src/utils/domSelector.ts
@@ -1,9 +1,7 @@
 const ERROR_ELEMENT_NOT_FOUND = '요소를 불러오는 데 실패했습니다. 페이지를 새로고침 해 주세요.';
 
-const $ = (selector: string, target?: HTMLElement): HTMLElement => {
-  const $selectedElement = target
-    ? target.querySelector(selector)
-    : document.querySelector(selector);
+const $ = (selector: string, target: HTMLElement | Document = document): HTMLElement => {
+  const $selectedElement = target.querySelector(selector);
 
   if (!($selectedElement instanceof HTMLElement))
     throw new Error(`${ERROR_ELEMENT_NOT_FOUND} - "${selector}"`);
@@ -11,12 +9,8 @@ const $ = (selector: string, target?: HTMLElement): HTMLElement => {
   return $selectedElement;
 };
 
-const $$ = (selector: string, target?: HTMLElement): NodeListOf<Element> => {
-  const $selectedElements = target
-    ? target.querySelectorAll(selector)
-    : document.querySelectorAll(selector);
-
-  return $selectedElements;
+const $$ = (selector: string, target: HTMLElement | Document = document): NodeListOf<Element> => {
+  return target.querySelectorAll(selector);
 };
 
 export { $, $$ };
