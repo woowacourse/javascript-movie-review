67639299ed9b9ca8c37597a05b27f2b0ab8a4e62
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-31 09:23:15 +0900

    feat: 영화/장르 커스텀 타입가드 함수 구현

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -44,6 +44,49 @@ type ViewBundleType = {
   footerMessage: FooterMessage;
 };
 
+const isMoviesType = (movies: unknown): movies is MovieType[] => {
+  if (!Array.isArray(movies)) {
+    return false;
+  }
+
+  const isMovies = movies.every((movie) => {
+    if (!movie) {
+      return false;
+    }
+
+    const isStringAllValid = ['title', 'posterPath', 'overview'].every(
+      (property) => typeof movie[property] === 'string',
+    );
+
+    const isNumberAllValid = ['voteAverage', 'id'].every(
+      (property) => typeof movie[property] === 'number',
+    );
+
+    const isArrayAllValid =
+      movie.genreIds && movie.genreIds.every((genreId) => typeof genreId === 'number');
+
+    return isStringAllValid && isNumberAllValid && isArrayAllValid;
+  });
+
+  return isMovies;
+};
+
+const isGenresType = (genres: unknown): genre is GenreType[] => {
+  if (!Array.isArray(genres)) {
+    return false;
+  }
+
+  const isGenres = genres.every((genre) => {
+    if (!genre) {
+      return false;
+    }
+
+    return typeof genre.id === 'number' && typeof genre.name === 'string';
+  });
+
+  return isGenres;
+};
+
 export {
   MovieType,
   StorageMovieType,
