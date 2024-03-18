a86dc4d7e2dd288dd4d6bef47adf31b37eefb857
Cron <hojoong.choi@woowahan.com>
2024-03-19 00:17:48 +0900

    test: API 테스트

--- cypress/e2e/movie-list-api.cy.js
+++ cypress/e2e/movie-list-api.cy.js
@@ -1,11 +1,24 @@
+import { POPULAR_MOVIES_URL } from "../../src/constants";
+
 describe("API 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:8080");
   });
 
-  // request: https://docs.cypress.io/api/commands/request
-  // 환경변수 세팅: https://docs.cypress.io/guides/guides/environment-variables
+  // https://docs.cypress.io/api/commands/request
   it("영화 목록 API를 호출하면 한 번에 20개씩 목록에 나열되어야 한다", () => {
-    // TODO:
+    const popularMovieUrl =
+      POPULAR_MOVIES_URL +
+      "?" +
+      new URLSearchParams({
+        api_key: Cypress.env("API_KEY"),
+        language: "ko-KR",
+        page: 1,
+      });
+
+    cy.request("GET", popularMovieUrl).as("popularMovies");
+
+    cy.get("@popularMovies").its("status").should("eq", 200);
+    cy.get("@popularMovies").its("body.results").should("have.length", 20);
   });
 });
