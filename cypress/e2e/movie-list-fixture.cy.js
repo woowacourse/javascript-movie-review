97b18275db93173e0ea19d95886ab28eaca1c73e
Cron <hojoong.choi@woowahan.com>
2024-03-19 00:42:41 +0900

    test: fixture를 이용한 테스트(2)

--- cypress/e2e/movie-list-fixture.cy.js
+++ cypress/e2e/movie-list-fixture.cy.js
@@ -19,6 +19,10 @@ describe("Fixture를 이용한 테스트", () => {
       // interception으로 fixture가 잘 불러와졌는지 확인하는 코드
       const popularMovies = interception.response.body.results;
       expect(popularMovies.length).to.equal(20);
+
+      // 제대로 렌더링이 되었는지 테스트하는 코드
+      const popularMovieItems = cy.get(".item-list > li");
+      expect(popularMovieItems.should("have.length", 20));
     });
   });
 });
