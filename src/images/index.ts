9704245275507060d45966f89f0be58daf2a20e4
hanyoung <phanyoung@naver.com>
2024-03-19 19:57:05 +0900

    desing(images): searchButton, starEmpty, starFilled 이미지 추가
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,7 +1,13 @@
 import LOGO from "./logo.png";
+import SEARCH_BUTTON from "./search_button.png";
+import STAR_EMPTY from "./star_empty.png";
+import STAR_FILLED from "./star_filled.png";
 
 const IMAGES = {
   logo: LOGO,
+  searchButton: SEARCH_BUTTON,
+  starEmpty: STAR_EMPTY,
+  starFilled: STAR_FILLED,
 };
 
 export default IMAGES;
