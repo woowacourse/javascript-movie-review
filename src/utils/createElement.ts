3f39e5da987236046f873553e682b5307531f64b
brgndy <brgndy96@gmail.com>
2024-03-20 08:12:35 +0900

    refactor: createElement 속성 타입가드
    Co-authored-by: river <0jenn0@users.noreply.github.com>

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -1,14 +1,19 @@
-import { ElementOption, ElemetTag } from '../types/element';
+type ElementTag = keyof HTMLElementTagNameMap;
 
-const createElement = (tag: ElemetTag, props: ElementOption = {}) => {
-  const targetTag = document.createElement(tag);
+interface ElementOption<T extends HTMLElement> {
+  [prop: string]: any;
+}
+
+function createElement<T extends HTMLElement>(tag: ElementTag, props: ElementOption<T> = {}): T {
+  const element = document.createElement(tag) as T;
   Object.entries(props).forEach(([key, value]) => {
-    if (value) {
-      targetTag.setAttribute(key, value);
+    if (key in element) {
+      (element[key as keyof T] as unknown) = value;
+    } else {
+      element.setAttribute(key, value);
     }
   });
-
-  return targetTag;
-};
+  return element;
+}
 
 export default createElement;
