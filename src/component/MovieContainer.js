60b361ae73c0ec19a2cf1f41e78a1ff0ebde56ff
pakxe <pigkill40@naver.com>
2024-03-19 20:40:57 +0900

    feat: 영화 컨테이너 컴포넌트 구현 완료
    Co-authored-by: Seongjin Hong <mail@seongjin.me>

--- src/component/MovieContainer.js
+++ src/component/MovieContainer.js
@@ -1,121 +1,70 @@
-import { $ } from '../util/selector.js';
-import createButton from './Button.js';
-import { injectMovieDataToItem } from './MovieItem.js';
-import { createSkeletonMovieList } from './MovieList.js';
+import itemScoreIconPath from '../asset/star_filled.png';
 
-class MovieContainer {
-  #movieListContainer;
-  #sectionTitle;
-  #skeletonList;
-  #moreButton;
-  #handleMoreButton;
+function createMovieContainer({ title, movieList, hasNextPage }) {
+  const section = document.createElement('section');
+  section.classList.add('item-view');
 
-  constructor({ title, handleMoreButton }) {
-    this.#movieListContainer = $('ul.item-list');
-    this.#sectionTitle = $('.item-view > h2');
-    this.#sectionTitle.textContent = title;
-    this.#handleMoreButton = handleMoreButton;
-    this.#moreButton = createButton(
-      {
-        type: 'button',
-        id: 'more-button',
-        size: 'full-width',
-        variant: 'primary',
-        textContent: '더 보기',
-      },
-      {
-        type: 'click',
-        callbackFunction: () => this.initHandleClickMoreButton(),
-      },
-    );
-    this.toggleMoreButtonVisibility(false);
+  const titleHeader = document.createElement('h2');
+  titleHeader.textContent = title;
 
-    $('section').append(this.#moreButton);
+  section.append(titleHeader, createMovieList(movieList));
+
+  if (hasNextPage) {
+    section.append(createMoreButton());
   }
 
-  createSkeletonList() {
-    const skeletonMovieList = createSkeletonMovieList();
-
-    skeletonMovieList.forEach((skeletonMovie) => {
-      this.#movieListContainer.appendChild(skeletonMovie);
-    });
-
-    this.#skeletonList = skeletonMovieList;
-    this.toggleMoreButtonVisibility(false);
-    this.removeRetryButton();
-  }
-
-  createEmptySearchResult() {
-    const emptySearchResult = document.createElement('h3');
-    emptySearchResult.classList.add('empty-search-result');
-    emptySearchResult.textContent = '검색 결과가 없습니다.';
-    return emptySearchResult;
-  }
-
-  removeSkeleton() {
-    this.#skeletonList.forEach((skeleton) => {
-      skeleton.remove();
-    });
-  }
-
-  setEmptySearchResult(listLength) {
-    if (listLength !== 0) {
-      $('h3.empty-search-result')?.remove();
-    }
-
-    if (listLength === 0 && !$('h3.empty-search-result')) {
-      $('.item-view').insertBefore(this.createEmptySearchResult(), this.#movieListContainer);
-    }
-  }
-
-  fillMovieDataToSkeletonList({ movieList, hasNextPage }) {
-    this.setEmptySearchResult(movieList.length);
-
-    this.#skeletonList.forEach((item, i) => {
-      if (i >= movieList.length) return item.remove();
-      injectMovieDataToItem({ item, movie: movieList[i].data });
-    });
-
-    this.toggleMoreButtonVisibility(hasNextPage);
-    this.#skeletonList = [];
-  }
-
-  toggleMoreButtonVisibility(hasNextPage) {
-    this.#moreButton.style.visibility = hasNextPage ? 'visible' : 'hidden';
-  }
-
-  clearMovieList() {
-    this.#movieListContainer.replaceChildren();
-  }
-
-  createRetryButton(handleRetryButton) {
-    const retryButton = createButton(
-      {
-        type: 'button',
-        id: 'retry-button',
-        size: 'full-width',
-        variant: 'primary',
-        textContent: '다시 불러오기 ↻',
-      },
-      {
-        type: 'click',
-        callbackFunction: () => handleRetryButton(),
-      },
-    );
-    $('.item-view').insertBefore(retryButton, $('ul.item-list'));
-  }
-
-  removeRetryButton() {
-    $('#retry-button')?.remove();
-  }
-
-  async initHandleClickMoreButton() {
-    await this.#handleMoreButton();
-  }
-
-  setTitle(title) {
-    this.#sectionTitle.textContent = title;
-  }
+  return section;
 }
 
-export default MovieContainer;
+function createMovieList(movieList) {
+  console.log(movieList);
+  const movieListContainer = document.createElement('ul');
+  movieListContainer.classList.add('item-list');
+
+  const movieItems = movieList.map((movie) => {
+    const movieData = movie.data;
+
+    const aLink = document.createElement('a');
+    aLink.href = '#';
+
+    const itemCard = document.createElement('div');
+    itemCard.classList.add('item-card');
+
+    const itemThumbnail = document.createElement('img');
+    itemThumbnail.classList.add('item-thumbnail');
+    itemThumbnail.src = movieData.posterPath;
+    itemThumbnail.loading = 'lazy';
+    itemThumbnail.alt = movieData.title;
+
+    const itemTitle = document.createElement('p');
+    itemTitle.classList.add('item-title');
+    itemTitle.textContent = movieData.title;
+
+    const itemScore = document.createElement('p');
+    itemScore.classList.add('item-score');
+    const itemScoreIcon = document.createElement('img');
+    itemScoreIcon.src = itemScoreIconPath;
+    itemScoreIcon.alt = '별점';
+
+    itemScore.append(itemScoreIcon, movieData.voteAverage);
+    itemCard.append(itemThumbnail, itemTitle, itemScore);
+    aLink.append(itemCard);
+
+    return aLink;
+  });
+
+  movieItems.forEach((item) => movieListContainer.append(item));
+
+  // movieListContainer.append([...movieItems]);
+  return movieListContainer;
+}
+
+function createMoreButton() {
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.classList.add('btn', 'primary', 'full-width');
+  button.textContent = '더 보기';
+  return button;
+}
+
+export default createMovieContainer;
