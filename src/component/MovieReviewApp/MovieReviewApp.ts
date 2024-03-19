ce13d13f2fd4b11020aaa6b62078e48f8900cbc5
ImxYJL <allensain14@gmail.com>
2024-03-19 22:05:33 +0900

    feat: 최상위 컴포넌트인 MovieReviewApp 뼈대 구현 및 인기순 영화 목록 조회 기능 구현
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

--- src/component/MovieReviewApp/MovieReviewApp.ts
+++ src/component/MovieReviewApp/MovieReviewApp.ts
@@ -1,15 +1,35 @@
 import MovieReviewHeader from "../MovieReviewHeader/MovieReviewHeader";
 import MovieSearchInput from "../MovieSearchInput/MovieSearchInput";
+import MovieList from "../MovieList/MovieList";
+
+import { $ } from "../../utility/dom";
 
 class MovieReviewApp {
-  render() {
-    const headerElement = document.querySelector("header");
+  #renderMovieReviewHeader() {
+    const headerElement = $("header");
+
     const movieSearchInput = new MovieSearchInput();
 
     if (headerElement) {
       headerElement.appendChild(MovieReviewHeader.createHeader());
       headerElement.appendChild(movieSearchInput.createSearchBox());
     }
+  }
+
+  #renderMovieReviewMain() {
+    const mainElement = $("main");
+
+    const movieList = new MovieList();
+    movieList.createMovieItems();
+
+    if (mainElement) {
+      mainElement.appendChild(movieList.createMovieList());
+    }
+  }
+
+  render() {
+    this.#renderMovieReviewHeader();
+    this.#renderMovieReviewMain();
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import MovieReviewApp from "./component/MovieReviewApp/MovieReviewApp.ts";
-import starFilledImg from "./image/star_filled.png";
+
 import "./common.style.css";
+import "./reset.style.css";
+
 import "./component/MovieReviewHeader/movieReviewHeader.style.css";
 import "./component/MovieSearchInput/movieSearchInput.style.css";
+import "./component/MovieList/movieList.style.css";
+import "./component/MovieItem/movieItem.style.css";
+import "./component/MoreMoviesButton/moreMoviesButton.style.css";
 
 const movieReviewApp = new MovieReviewApp();
 
