04e4d97ecee065c85af26730a57893000776c91d
ImxYJL <allensain14@gmail.com>
2024-03-19 22:02:44 +0900

    feat: MovieList 레이아웃 구현
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

---
+++ src/component/MovieList/MovieList.ts
@@ -0,0 +1,38 @@
+import MovieItem from "../MovieItem/MovieItem";
+import { getMovieData } from "../../api/getMovieData";
+import { getPopularMoviesData } from "../../api/getPopularMoviesData";
+import { $, createElement } from "../../utility/dom";
+
+class MovieList {
+  #pageCount = "1";
+
+  async createMovieItems() {
+    const data = await getPopularMoviesData(this.#pageCount);
+    const ul = $(".item-list");
+
+    data.forEach(({ title, poster_path, vote_average }: IMovieItemData) => {
+      const movieItem = new MovieItem({ title, poster_path, vote_average });
+      ul?.appendChild(movieItem.createMovieItem());
+    });
+  }
+
+  createMovieList() {
+    const movieListSection = createElement("section", {
+      class: "item-view",
+    });
+
+    const movieListTitle = createElement("h2");
+    movieListTitle.textContent = "지금 인기 있는 영화";
+
+    const movieListUl = createElement("ul", {
+      class: "item-list",
+    });
+
+    movieListSection.appendChild(movieListTitle);
+    movieListSection.appendChild(movieListUl);
+
+    return movieListSection;
+  }
+}
+
+export default MovieList;
