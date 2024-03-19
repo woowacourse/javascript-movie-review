6bb4e775fc6361a0a4e6ca661532ed02872b1b1f
pakxe <pigkill40@naver.com>
2024-03-19 18:03:45 +0900

    refactor: 반복되는 데이터 가공 로직을 함수로 분리해 재사용하도록 수정

--- src/domain/MovieService.ts
+++ src/domain/MovieService.ts
@@ -7,18 +7,42 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
 const MOVIE_SEARCH_URL = `${BASE_URL}/search/movie`;
 
+interface MovieListData {
+  total_pages: number;
+  results: { id: number; title: string; poster_path: string; vote_average: number; overview: string }[];
+}
+
+interface MoviePageData extends MovieListData {
+  pageNumber: number;
+}
+
 class MovieService {
   private currentPageNumber: number = 1;
   private currentSearchPageNumber: number = 1;
 
   async fetchPopularMovieList(pageNumber: number) {
     // TODO: ! 지우기
-    const { total_pages, results } = await fetchDataFromUrl(POPULAR_MOVIES_URL, {
+    const { total_pages, results }: MovieListData = await fetchDataFromUrl(POPULAR_MOVIES_URL, {
       api_key: process.env.API_KEY!,
       language: 'ko-KR',
       page: pageNumber.toString(),
     });
 
+    return this.createMoviePageData({ total_pages, results, pageNumber: this.currentPageNumber });
+  }
+
+  async fetchSearchResult(searchKeyword: string) {
+    const { total_pages, results } = await fetchDataFromUrl(MOVIE_SEARCH_URL, {
+      api_key: process.env.API_KEY!,
+      language: 'ko-KR',
+      query: searchKeyword,
+      page: this.currentSearchPageNumber.toString(),
+    });
+
+    return this.createMoviePageData({ total_pages, results, pageNumber: this.currentSearchPageNumber });
+  }
+
+  createMoviePageData({ total_pages, results, pageNumber }: MoviePageData) {
     // TODO: 타입 바꾸기
     const movieList: Movie[] = results.map(
       (result: { id: number; title: string; poster_path: string; vote_average: number; overview: string }) =>
@@ -32,31 +56,6 @@ class MovieService {
 
     return {
       hasNextPage: total_pages > pageNumber,
-      movieList,
-    };
-  }
-
-  async fetchSearchResult(searchKeyword: string) {
-    const { total_pages, results } = await fetchDataFromUrl(MOVIE_SEARCH_URL, {
-      api_key: process.env.API_KEY!,
-      language: 'ko-KR',
-      query: searchKeyword,
-      page: this.currentSearchPageNumber.toString(),
-    });
-
-    // TODO: 타입 바꾸기
-    const movieList: Movie[] = results.map(
-      (result: { id: number; title: string; poster_path: string; vote_average: number }) =>
-        new Movie({
-          id: result.id,
-          title: result.title,
-          posterPath: result.poster_path,
-          voteAverage: result.vote_average,
-        }),
-    );
-
-    return {
-      hasNextPage: total_pages > this.currentSearchPageNumber,
       movieList,
     };
   }
