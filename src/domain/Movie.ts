64b5c2a9031ea44c87ee23f3411461261fffbcc9
pakxe <pigkill40@naver.com>
2024-03-19 15:17:47 +0900

    feat: 영화 객체 구현

--- src/domain/Movie.ts
+++ src/domain/Movie.ts
@@ -1,14 +1,15 @@
-import { MovieData } from '../interface/MovieData';
-import convertToPosterPath from '../util/convertToPosterPath';
+interface MovieData {
+  id: number;
+  title: string;
+  posterPath: string;
+  voteAverage: number;
+}
 
 export default class Movie {
   private movie: MovieData;
 
   constructor(movie: MovieData) {
-    this.movie = {
-      ...movie,
-      posterPath: convertToPosterPath({ relativePath: movie.posterPath, width: 200 }),
-    };
+    this.movie = movie;
   }
 
   get data(): MovieData {
