0c9a1880e19066400a980153620cc9d67a766c96
pakxe <pigkill40@naver.com>
2024-03-19 15:33:12 +0900

    test: 절대 경로 이미지와 일치하는지 테스트하도록 수정

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,4 +1,5 @@
 import Movie from '../src/domain/Movie.ts';
+import convertToPosterUrl from '../src/util/convertToPosterUrl.js';
 
 describe('영화 객체 테스트', () => {
   it('영화 데이터를 주입하면 올바르게 반환한다.', () => {
@@ -9,8 +10,9 @@ describe('영화 객체 테스트', () => {
       voteAverage: 6.913,
     };
 
+    const absolutePosterUrl = convertToPosterUrl({ relativePath: movieData.posterPath, width: 200 });
     const movie = new Movie(movieData);
 
-    expect(movie.data).toEqual(movieData);
+    expect(movie.data).toEqual({ ...movieData, posterPath: absolutePosterUrl });
   });
 });
