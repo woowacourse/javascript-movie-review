cdf6d7f773def05d2da6a9321afabc158a2e939e
pakxe <pigkill40@naver.com>
2024-03-19 20:12:19 +0900

    feat: 헤더 컴포넌트 구현
    Co-authored-by: Seongjin Hong <mail@seongjin.me>

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,28 +1,15 @@
 import { $ } from '../util/selector.js';
 import LogoImagePath from '../asset/logo.png';
 
-const SearchButtonClickEvent = new Event('clickSearchButton');
-const LogoClickEvent = new Event('logoClickEvent');
-
 function createHeader() {
   const header = renderHeader();
   $('#app').prepend(header);
-
-  header.querySelector('form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    event.target.dispatchEvent(SearchButtonClickEvent);
-  });
-
-  header.querySelector('.logo').addEventListener('click', (event) => {
-    event.target.dispatchEvent(LogoClickEvent);
-  });
 }
 
 function renderHeader() {
   const header = document.createElement('header');
 
   const logo = document.createElement('img');
-  logo.classList.add('logo');
   logo.alt = 'MovieList 로고';
   logo.src = LogoImagePath;
 
@@ -35,7 +22,7 @@ function renderHeader() {
 }
 
 function renderSearchBox() {
-  const searchBox = document.createElement('form');
+  const searchBox = document.createElement('div');
 
   const input = document.createElement('input');
   input.placeholder = '검색';
@@ -43,7 +30,6 @@ function renderSearchBox() {
 
   const searchButton = document.createElement('button');
   searchButton.classList.add('search-button');
-  searchButton.type = 'submit';
   searchButton.innerText = '검색';
 
   searchBox.append(input, searchButton);
