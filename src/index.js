7db9de5ff3eab10351328bdd07322eac04f501b9
jinsil <jinsil378900@gmail.com>
2024-03-19 19:53:19 +0900

    fix: css 순서 변경
    Co-authored-by: jaeml06 <jaeml0630@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import './css/reset.css';
+import './css/common.css';
+
 import './images/logo.png';
 import './images/search_button.png';
 import './images/star_empty.png';
 import './images/star_filled.png';
-
-import './css/common.css';
-import './css/reset.css';
 
 import AppController from './AppController';
 
