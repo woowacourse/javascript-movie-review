157dec85ac87b8c67c145ef9ee18dae1721518fb
이태훈 <rhymint@gmail.com>
2024-03-19 21:21:37 +0900

    design: component들의 css 적용
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import './reset.css';
+import './app.css';
 
-import Header from './components/Header';
-import MovieItems from './components/MovieItems';
-import SearchField from './components/SearchField';
+import Header from './components/Header/Header';
+import MovieItems from './components/MovieItems/MovieItems';
 
 const body = document.querySelector('body');
 
 body?.appendChild(Header.createElements());
-body?.appendChild(SearchField.createElements());
 body?.appendChild(MovieItems.createElements());
