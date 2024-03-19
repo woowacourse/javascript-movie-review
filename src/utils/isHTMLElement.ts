85f7977e8c3cc705928ad0996f65fc49fd42f531
brgndy <brgndy96@gmail.com>
2024-03-19 16:27:50 +0900

    refactor: HTMLElement 타입가드 유틸함수 인자 타입 변경
    Co-authored-by: 0jenn0 <jenn0.6n@gmail.com>

--- src/utils/isHTMLElement.ts
+++ src/utils/isHTMLElement.ts
@@ -1,2 +1,2 @@
-const isHTMLElement = (element: any): element is HTMLElement => element instanceof HTMLElement;
+const isHTMLElement = (element: HTMLElement | null): element is HTMLElement => element instanceof HTMLElement;
 export default isHTMLElement;
