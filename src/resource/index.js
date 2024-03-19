a2afcbde73a684bb1ae5f63fd9164bcffe4d8a65
GimGayeon <kky1373@naver.com>
2024-03-19 15:44:48 +0900

    feat: 초기 템플릿 구성
    Co-Authored-By: Largopie <jae990320@kakao.com>

---
+++ src/resource/index.js
@@ -0,0 +1,7 @@
+import '../../templates/common.css';
+import '../../templates/reset.css';
+
+export { default as SEARCH_BUTTON } from '../../templates/search_button.png';
+export { default as LOGO } from '../../templates/logo.png';
+export { default as STAR_EMPTY } from '../../templates/star_empty.png';
+export { default as STAR_FILLED } from '../../templates/star_filled.png';
