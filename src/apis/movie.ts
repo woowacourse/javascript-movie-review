d7c3de88c9b5829c994741a0cec0ce219ce67954
rbgksqkr <rbgks1937@naver.com>
2024-03-20 15:05:18 +0900

    feat(movie API): response를 파싱하는 로직 구현
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/apis/movie.ts
+++ src/apis/movie.ts
@@ -1,18 +1,36 @@
-const BASE_URL = 'https://api.themoviedb.org';
+import { genre } from '../constants/movie';
+import { IMovie, IMovieResponse } from '../types/movie';
 
-export const getPopularMovies = async (page: number) => {
-  const response = await fetch(BASE_URL + `/3/movie/popular?language=en-US&page=${page}`, {
+const BASE_URL = 'https://api.themoviedb.org';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
+
+export const getPopularMovies = async (page: number): Promise<IMovie[]> => {
+  const response = await fetch(BASE_URL + `/3/movie/popular?language=ko-KR&page=${page}`, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${process.env.API_KEY}`,
       accept: 'application/json',
     },
   });
-  const data = await response.json();
-  return data.results;
+  const { results } = await response.json();
+
+  return results.map(parseMovieResponse);
 };
 
-export const searchMoviesBytitle = async (title: string, page: number) => {
+const parseMovieResponse = (movieResponse: IMovieResponse): IMovie => {
+  const { id, title, poster_path, vote_average, genre_ids, overview } = movieResponse;
+  const movie: IMovie = {
+    id,
+    title,
+    imageSrc: `${IMAGE_BASE_URL}/original/${poster_path}`,
+    score: vote_average,
+    genre: genre_ids.map(genre_id => genre[genre_id]),
+    description: overview,
+  };
+  return movie;
+};
+
+export const searchMoviesByTitle = async (title: string, page: number) => {
   const response = await fetch(
     BASE_URL + `/3/search/movie?query=${title}&include_adult=false&language=en-US&page=${page}`,
     {
