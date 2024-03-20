b8278853f4e92ad8f0bc309a8ca995ef22125216
pakxe <pigkill40@naver.com>
2024-03-20 17:59:10 +0900

    feat: 스켈레톤 아이템 제작과 이 아이템에 데이터를 주입하는 함수 구현

--- src/component/MovieItem.js
+++ src/component/MovieItem.js
@@ -1,35 +1,5 @@
 import itemScoreIconPath from '../asset/star_filled.png';
-
-function createMovieItem(movie) {
-  const aLink = document.createElement('a');
-  aLink.href = '#';
-
-  const itemCard = document.createElement('div');
-  itemCard.classList.add('item-card');
-
-  const itemThumbnail = document.createElement('img');
-  itemThumbnail.classList.add('item-thumbnail');
-
-  itemThumbnail.src = movie.posterPath;
-  itemThumbnail.loading = 'lazy';
-  itemThumbnail.alt = movie.title;
-
-  const itemTitle = document.createElement('p');
-  itemTitle.classList.add('item-title');
-  itemTitle.textContent = movie.title;
-
-  const itemScore = document.createElement('p');
-  itemScore.classList.add('item-score');
-  const itemScoreIcon = document.createElement('img');
-  itemScoreIcon.src = itemScoreIconPath;
-  itemScoreIcon.alt = '별점';
-
-  itemScore.append(itemScoreIcon, movie.voteAverage);
-  itemCard.append(itemThumbnail, itemTitle, itemScore);
-  aLink.append(itemCard);
-
-  return aLink;
-}
+import { $ } from '../util/selector.js';
 
 function createSkeletonMovieItem() {
   const aLink = document.createElement('a');
@@ -41,13 +11,8 @@ function createSkeletonMovieItem() {
   const itemThumbnail = document.createElement('img');
   itemThumbnail.classList.add('item-thumbnail', 'skeleton');
 
-  // itemThumbnail.src = movie.posterPath;
-  // itemThumbnail.loading = 'lazy';
-  // itemThumbnail.alt = movie.title;
-
   const itemTitle = document.createElement('p');
   itemTitle.classList.add('item-title', 'skeleton');
-  // itemTitle.textContent = movie.title;
 
   const itemScore = document.createElement('p');
   itemScore.classList.add('item-score', 'skeleton');
@@ -58,4 +23,29 @@ function createSkeletonMovieItem() {
   return aLink;
 }
 
-export { createMovieItem, createSkeletonMovieItem };
+function injectMovieDataToItem({ item, movie }) {
+  const $itemThumbnail = item.querySelector('.item-thumbnail');
+  const $itemTitle = item.querySelector('.item-title');
+  const $itemScore = item.querySelector('.item-score');
+
+  $itemThumbnail.onload = () => {
+    $itemThumbnail.classList.remove('skeleton');
+    $itemTitle.classList.remove('skeleton');
+    $itemScore.classList.remove('skeleton');
+
+    $itemThumbnail.loading = 'lazy';
+    $itemThumbnail.alt = movie.title;
+    $itemTitle.textContent = movie.title;
+
+    const $itemScoreIcon = document.createElement('img');
+
+    $itemScoreIcon.src = itemScoreIconPath;
+    $itemScoreIcon.alt = '별점';
+
+    $itemScore.append($itemScoreIcon, movie.voteAverage);
+  };
+
+  $itemThumbnail.src = movie.posterPath;
+}
+
+export { createSkeletonMovieItem, injectMovieDataToItem };
