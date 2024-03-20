254fb10fe351a6964de91649256bb1fce51983bf
greetings1012 <ahnqwerty@gmail.com>
2024-03-20 15:59:55 +0900

    feat: css, png 파일 import

---
+++ src/resources.js
@@ -0,0 +1,8 @@
+import './css/common.css';
+
+import logo from './assets/image/logo.png';
+import searchButton from './assets/image/search_button.png';
+import starEmpty from './assets/image/star_empty.png';
+import starFilled from './assets/image/star_filled.png';
+
+export { logo, searchButton, starEmpty, starFilled };
