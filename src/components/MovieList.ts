ca87d5bc49b3d7cdc41f07e5d63e48e339ec21c6
hanyoung <phanyoung@naver.com>
2024-03-20 15:54:21 +0900

    refactor(MovieList): MovieState 제거하고 영화 목록을 movies 멤버변수로 관리
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -1,23 +1,20 @@
 import EventComponent from "./abstract/EventComponent";
 import QueryState from "../states/QueryState";
-import { composeMovieItem } from "./templates/composeMovieItem";
-import MoviesState from "../states/MoviesState";
+import { Movie, composeMovieItem } from "./templates/composeMovieItem";
 import { getPopularMovieList } from "../apis/movieList";
 
 interface MovieListProps {
   targetId: string;
   queryState: QueryState;
-  moviesState: MoviesState;
 }
 
 export default class MovieList extends EventComponent {
   private queryState: QueryState;
-  private moviesState: MoviesState;
+  private movies: Movie[] = [];
 
-  constructor({ targetId, queryState, moviesState }: MovieListProps) {
+  constructor({ targetId, queryState }: MovieListProps) {
     super({ targetId });
     this.queryState = queryState;
-    this.moviesState = moviesState;
   }
 
   getTemplate(): string {
@@ -35,11 +32,13 @@ export default class MovieList extends EventComponent {
   async fetchInitialMovies() {
     const movies = await getPopularMovieList();
 
-    this.moviesState.set(movies);
+    this.movies = movies;
+
+    this.init();
   }
 
   private generateMovieItemsTemplate() {
-    const movies = this.moviesState.get();
+    const movies = this.movies;
 
     const movieItemsTemplate = movies.reduce((movieListTemplate, movieInfo) => {
       return movieListTemplate + composeMovieItem(movieInfo);
--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ import "./styles/common.css";
 import MovieHeader from "./components/MovieHeader";
 import QueryState from "./states/QueryState";
 import MovieList from "./components/MovieList";
-import MoviesState from "./states/MoviesState";
 
 const queryState = new QueryState();
-const moviesState = new MoviesState();
 
 const movieHeader = new MovieHeader({ targetId: "movie-header", queryState });
 const movieList = new MovieList({
   targetId: "movie-list",
   queryState,
-  moviesState,
 });
 movieList.fetchInitialMovies();
 
 queryState.addObserver(movieList);
-moviesState.addObserver(movieList);
 
 movieHeader.init();
 movieList.init();
