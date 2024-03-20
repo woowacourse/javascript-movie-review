5ab6ae0cd7ddd1bdda8037d3cbc317972e99a27f
pakxe <pigkill40@naver.com>
2024-03-20 17:56:02 +0900

    rename: 폴더명 변경

--- src/util/convertToPosterPath.js
+++ src/util/convertToPosterPath.js
@@ -1,4 +1,4 @@
-import { IMAGE_BASE_URL } from '../constants/config.js';
+import { IMAGE_BASE_URL } from '../constant/config.js';
 
 export default function convertToPosterPath({ relativePath, width }) {
   return `${IMAGE_BASE_URL}${width}${relativePath}`;
