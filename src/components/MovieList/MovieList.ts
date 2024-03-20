abb83336b89533198a343f721048e8362b061266
anttiey <hanuuny@gmail.com>
2024-03-20 14:09:42 +0900

    feat(step 2): 영화 목록 더보기 기능 구현
    1. 더보기 버튼을 누르면 새로 데이터를 가져와 렌더링하는 handleMoreButtonClick 함수 구현
    
    2. MovieList에 영화 목록 데이터 props를 전달하는 방식으로 변경
    
    Co-authored-by: jinyoung234 <sm099@naver.com>

--- src/components/MovieList/MovieList.ts
+++ src/components/MovieList/MovieList.ts
@@ -1,12 +1,14 @@
-import { BaseResponse } from '../../apis/common/apiSchema.type';
-import MovieAPI from '../../apis/movie/movie';
-import { createElement } from '../../utils/dom/createElement/createElement';
 import Component from '../Component/Component';
 import MovieListCard from '../MovieListCard/MovieListCard';
+import { createElement } from '../../utils/dom/createElement/createElement';
 import { MovieListCardProps } from '../MovieListCard/MovieListCard.type';
 //TODO: 현재는 Props 타입으로 되어있지만 도메인 타입으로 수정해야 할듯!
 
-class MovieList extends Component {
+interface MovieListProps {
+  movieItemDetails: MovieListCardProps[];
+}
+
+class MovieList extends Component<MovieListProps> {
   protected render() {
     this.$element.append(this.createComponent());
   }
@@ -14,19 +16,12 @@ class MovieList extends Component {
   protected createComponent() {
     const $movieItemList = createElement({ tagName: 'ul', attributeOptions: { class: 'item-list' } });
 
-    //TODO: page를 동적으로 받아야하는 것 + API 레이어 분리
-    MovieAPI.fetchMovieDetails<BaseResponse<MovieListCardProps[]>>(1)
-      .then((data) => {
-        if (data) {
-          data.results.forEach((props) => {
-            const $li = createElement({ tagName: 'li' });
-            new MovieListCard($li, props);
+    this.props?.movieItemDetails.forEach((movieItemDetail) => {
+      const $li = createElement({ tagName: 'li' });
+      new MovieListCard($li, movieItemDetail);
 
-            $movieItemList.appendChild($li);
-          });
-        }
-      })
-      .catch(() => {});
+      $movieItemList.appendChild($li);
+    });
 
     return $movieItemList;
   }
--- src/components/MovieReviewBody/MovieReviewBody.ts
+++ src/components/MovieReviewBody/MovieReviewBody.ts
@@ -1,10 +1,47 @@
 import { createElement } from '../../utils/dom/createElement/createElement';
 import Component from '../Component/Component';
 import MovieList from '../MovieList/MovieList';
+import MovieAPI from '../../apis/movie/movie';
+import { BaseResponse } from '../../apis/common/apiSchema.type';
+import { MovieListCardProps } from '../MovieListCard/MovieListCard.type';
+import { querySelector } from '../../utils/dom/selector';
+import { on } from '../../utils/dom/eventListener/eventListener';
 
 class MovieReviewBody extends Component {
+  private state: { page: number } = {
+    page: 1,
+  };
+
   protected render() {
     this.$element.append(this.createComponent());
+  }
+
+  private handleMoreButtonClick() {
+    this.state.page += 1;
+
+    MovieAPI.fetchMovieDetails<BaseResponse<MovieListCardProps[]>>(this.state.page)
+      .then((data) => {
+        if (data?.results) {
+          const $movieListContainer = querySelector<HTMLDivElement>('#movie-list-container');
+          new MovieList($movieListContainer, { movieItemDetails: data?.results ?? [] });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    if (this.state.page === 5) {
+      const $button = querySelector<HTMLButtonElement>('#more-button', this.$element);
+      $button.remove();
+    }
+  }
+
+  protected setEvent(): void {
+    on({
+      target: querySelector<HTMLButtonElement>('#more-button', this.$element),
+      eventName: 'click',
+      eventHandler: this.handleMoreButtonClick.bind(this),
+    });
   }
 
   protected createComponent() {
@@ -14,13 +51,23 @@ class MovieReviewBody extends Component {
     $section.appendChild($movieTitle);
 
     const $movieListContainer = createElement({ tagName: 'div', attributeOptions: { id: 'movie-list-container' } });
-    new MovieList($movieListContainer);
+
+    MovieAPI.fetchMovieDetails<BaseResponse<MovieListCardProps[]>>(1)
+      .then((data) => {
+        // 스켈레톤 지우고
+        // ex) ~
+        new MovieList($movieListContainer, { movieItemDetails: data?.results ?? [] });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
     $section.appendChild($movieListContainer);
 
     const $button = createElement({
       tagName: 'button',
       text: '더보기',
-      attributeOptions: { class: 'btn primary full-width' },
+      attributeOptions: { id: 'more-button', class: 'btn primary full-width' },
     });
     $section.appendChild($button);
 
