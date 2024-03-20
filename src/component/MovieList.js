19a0e77c9cd0d0c4553bf4eec227225070ee051e
pakxe <pigkill40@naver.com>
2024-03-20 16:36:58 +0900

    fix: 데이터를 받아서 주입하도록 수정

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -6,7 +6,7 @@ function createMovieList(movieList) {
   const list = movieList || new Array(MAX_SINGLE_REQUEST_MOVIE_COUNT).fill();
 
   return list.map((movie) => {
-    const movieItem = movie ? createMovieItem(movie) : createSkeletonMovieItem();
+    const movieItem = movie ? createMovieItem(movie.data) : createSkeletonMovieItem();
 
     const li = document.createElement('li');
     li.append(movieItem);
