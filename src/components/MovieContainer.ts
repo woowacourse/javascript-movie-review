e3d3968b254037f1e152f829c6fb660fc0407e9d
GimGayeon <kky1373@naver.com>
2024-03-20 13:59:21 +0900

    feat(MovieContainer): 영화 정보 불러오는 클래스 구현
    Co-Authored-By: Largopie <jae990320@kakao.com>

--- src/components/MovieContainer.ts
+++ src/components/MovieContainer.ts
@@ -1,25 +1,48 @@
+import { MovieInfo } from '../api/api-type';
+import { getPopularMovieList } from '../api/popularMovieList';
 import MovieItem from './MovieItem';
 
-const MovieContainer = () => {
-  const section = document.createElement('section');
+class MovieContainer {
+  #page;
 
-  const h2 = document.createElement('h2');
-  const movieList = document.createElement('ul');
-  const button = document.createElement('button');
+  constructor() {
+    this.#page = 1;
+  }
 
-  movieList.classList.add('item-list');
-  button.classList.add('btn', 'primary', 'full-width');
+  async getMovies(page: number) {
+    const movieData = await getPopularMovieList(page);
+    return movieData;
+  }
 
-  button.textContent = '더 보기';
-  h2.textContent = '지금 인기 있는 영화';
+  createMovieItems(data: MovieInfo[]): HTMLElement[] {
+    return data.map((prop) => MovieItem(prop));
+  }
 
-  section.appendChild(h2);
-  section.appendChild(movieList);
-  section.appendChild(button);
+  async getTemplate() {
+    const section = document.createElement('section');
 
-  movieList.appendChild(MovieItem());
+    const h2 = document.createElement('h2');
+    const movieList = document.createElement('ul');
+    const button = document.createElement('button');
 
-  return section;
-};
+    section.classList.add('item-view');
+    movieList.classList.add('item-list');
+    button.classList.add('btn', 'primary', 'full-width');
+
+    button.textContent = '더 보기';
+    h2.textContent = '지금 인기 있는 영화';
+
+    section.appendChild(h2);
+    section.appendChild(movieList);
+    section.appendChild(button);
+
+    const movieData = await this.getMovies(this.#page);
+
+    this.createMovieItems(movieData).forEach((movieItem) => {
+      movieList.appendChild(movieItem);
+    });
+    return section;
+  }
+}
 
 export default MovieContainer;
