cf4c801d1ac2fdc919b29231cd15125e4dcdc8ab
GimGayeon <kky1373@naver.com>
2024-03-20 18:24:30 +0900

    feat(MovieContainer): 더보기 버튼 기능 구현
    Co-Authored-By: Largopie <jae990320@kakao.com>

--- src/components/MovieContainer.ts
+++ src/components/MovieContainer.ts
@@ -1,6 +1,6 @@
 import { MovieInfo } from '../api/api-type';
 import { getPopularMovieList } from '../api/popularMovieList';
-import SkeletonItem from './\bSkeletonItem';
+import SkeletonItem from './SkeletonItem';
 import MovieItem from './MovieItem';
 
 class MovieContainer {
@@ -9,12 +9,53 @@ class MovieContainer {
   constructor(element: HTMLElement) {
     this.#page = 1;
     this.#getTemplate(element);
+    this.#setEvent();
     this.renderMovies();
+  }
+
+  #getTemplate(element: HTMLElement) {
+    const section = document.createElement('section');
+
+    const h2 = document.createElement('h2');
+    const movieList = document.createElement('ul');
+    const button = document.createElement('button');
+
+    section.classList.add('item-view');
+    movieList.classList.add('item-list');
+    button.classList.add('btn', 'primary', 'full-width');
+
+    button.textContent = '더 보기';
+    h2.textContent = '지금 인기 있는 영화';
+
+    section.appendChild(h2);
+    section.appendChild(movieList);
+    section.appendChild(button);
+
+    element.appendChild(section);
+  }
+
+  #setEvent() {
+    document.querySelector('.btn')?.addEventListener('click', () => {
+      this.renderMovies();
+    });
   }
 
   async renderMovies() {
     this.#inputSkeleton();
     await this.#inputMovies();
+  }
+
+  #inputSkeleton() {
+    const ul = document.querySelector('.item-list');
+    if (!(ul instanceof HTMLElement)) return;
+
+    Array.from({ length: 20 }).forEach(() => ul.insertAdjacentElement('beforeend', SkeletonItem()));
+  }
+
+  #removeSkeleton() {
+    const skeletonItem = document.querySelector('.skeleton-item');
+
+    skeletonItem?.remove();
   }
 
   async #getMovies(page: number) {
@@ -37,43 +78,8 @@ class MovieContainer {
         ul.appendChild(movieItem);
         this.#removeSkeleton();
       });
+      this.#page += 1;
     }
-  }
-
-  #inputSkeleton() {
-    const ul = document.querySelector('.item-list');
-    if (!(ul instanceof HTMLElement)) return;
-
-    Array.from({ length: 20 }).forEach(() =>
-      ul.insertAdjacentElement('afterbegin', SkeletonItem()),
-    );
-  }
-
-  #removeSkeleton() {
-    const skeletonItem = document.querySelector('.skeleton-item');
-
-    skeletonItem?.remove();
-  }
-
-  #getTemplate(element: HTMLElement) {
-    const section = document.createElement('section');
-
-    const h2 = document.createElement('h2');
-    const movieList = document.createElement('ul');
-    const button = document.createElement('button');
-
-    section.classList.add('item-view');
-    movieList.classList.add('item-list');
-    button.classList.add('btn', 'primary', 'full-width');
-
-    button.textContent = '더 보기';
-    h2.textContent = '지금 인기 있는 영화';
-
-    section.appendChild(h2);
-    section.appendChild(movieList);
-    section.appendChild(button);
-
-    element.appendChild(section);
   }
 }
 
