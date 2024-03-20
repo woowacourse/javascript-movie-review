96c7d457224f2c866c3df43265ee5accd85faee9
pakxe <pigkill40@naver.com>
2024-03-20 18:00:27 +0900

    feat: 프로그램 진입점 작성

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './asset/logo.png';
+import './asset/search_button.png';
+import './asset/star_empty.png';
+import './asset/star_filled.png';
+
+import './component/reset.css';
+import './component/common.css';
+
+import { App } from './controller/controller.js';
+
+const app = new App();
+app.init();
