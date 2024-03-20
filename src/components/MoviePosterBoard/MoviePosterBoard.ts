789885fca5dbe7eb8235433dddcc5d13985f2245
ss0526100 <ss0526100@gmail.com>
2024-03-20 13:20:45 +0900

    feat: moviePosterBoard 구현

---
+++ src/components/MoviePosterBoard/MoviePosterBoard.ts
@@ -0,0 +1,32 @@
+import "./style.css";
+
+import createMoviePoster, { MovieInfo } from "../moviePoster/createMoviePoster";
+
+import createElement from "../../utils/createElement";
+
+class MoviePosterBoard {
+  element;
+  #ul: HTMLElement;
+
+  constructor(description: string) {
+    this.element = this.#createBasicElement(description);
+    this.#ul = createElement("ul", { class: "item-list" });
+    this.element.append(this.#ul);
+  }
+
+  addMoviePoster(movieInfos: MovieInfo[]) {
+    const newMoviePosters = movieInfos.map(createMoviePoster);
+
+    this.#ul.append(...newMoviePosters);
+  }
+
+  #createBasicElement(description: string) {
+    const section = createElement("section", { class: "item-view" });
+    const h2 = createElement("h2", {}, description);
+    section.append(h2);
+
+    return section;
+  }
+}
+
+export default MoviePosterBoard;
