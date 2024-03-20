782a904b0e3542ba03b2e3ac9508ec4b5bd0fcca
hanyoung <phanyoung@naver.com>
2024-03-20 15:38:54 +0900

    feat(index.js): MovieList 컴포넌트 및 MoviesState 추가
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import "./styles/common.css";
 import MovieHeader from "./components/MovieHeader";
 import QueryState from "./states/QueryState";
 import MovieList from "./components/MovieList";
+import MoviesState from "./states/MoviesState";
 
 const queryState = new QueryState();
+const moviesState = new MoviesState();
+
 const movieHeader = new MovieHeader({ targetId: "movie-header", queryState });
-const movieList = new MovieList({ targetId: "movie-list", queryState });
+const movieList = new MovieList({
+  targetId: "movie-list",
+  queryState,
+  moviesState,
+});
+movieList.fetchInitialMovies();
+
+queryState.addObserver(movieList);
+moviesState.addObserver(movieList);
 
 movieHeader.init();
 movieList.init();
