69a103ad494a3a02af279bdde65fae24549271af
pakxe <pigkill40@naver.com>
2024-03-20 17:59:54 +0900

    feat: 스켈레톤 아이템 리스트를 반환하도록해 인자 제거

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -2,8 +2,8 @@ import { createMovieItem, createSkeletonMovieItem } from './MovieItem.js';
 
 const MAX_SINGLE_REQUEST_MOVIE_COUNT = 20;
 
-function createSkeletonMovieList(movieList) {
-  const list = movieList || new Array(MAX_SINGLE_REQUEST_MOVIE_COUNT).fill();
+function createSkeletonMovieList() {
+  const list = new Array(MAX_SINGLE_REQUEST_MOVIE_COUNT).fill();
 
   return list.map((movie) => {
     const movieItem = movie ? createMovieItem(movie.data) : createSkeletonMovieItem();
