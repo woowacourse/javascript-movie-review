a48391e79bc510fbbb8ffb16be14b3ad451be0c7
ImxYJL <allensain14@gmail.com>
2024-03-20 13:38:58 +0900

    refactor: 로고 이미지를 감싸는 h1 태그 이름을 logoWrapper로 수정
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

--- src/component/MovieReviewHeader/MovieReviewHeader.ts
+++ src/component/MovieReviewHeader/MovieReviewHeader.ts
@@ -3,17 +3,19 @@ import logoImg from "../../image/logo.png";
 
 const MovieReviewHeader = {
   createHeader() {
-    const h1 = createElement("h1");
+    const logoWrapper = createElement("h1");
     const img = createElement("img", {
       class: "",
       src: `${logoImg}`,
       alt: "MovieList 로고",
     });
 
-    h1.appendChild(img);
+    logoWrapper.appendChild(img);
 
-    return h1;
+    return logoWrapper;
   },
+
+  
 };
 
 export default MovieReviewHeader;
