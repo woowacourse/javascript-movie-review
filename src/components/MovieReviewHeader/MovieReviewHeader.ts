8cd3fbf14ebdbc8bec50a872aa0eae747acbdedc
손진영 <sm099@naver.com>
2024-03-20 19:54:36 +0900

    feat: 조건부 ErrorToast 렌더링 구현
    사용자가 검색 창에 빈 값 입력시 ErrorToast를 1초만 보여주는 기능 구현
    
    Co-authored-by: anttiey <hanuuny@gmail.com>

--- src/components/MovieReviewHeader/MovieReviewHeader.ts
+++ src/components/MovieReviewHeader/MovieReviewHeader.ts
@@ -24,16 +24,17 @@ class MovieReviewHeader extends Component<MovieReviewHeaderProps> {
     event.preventDefault();
 
     const $searchForm = querySelector<HTMLFormElement>('#search-form', this.$element);
-    const $searchInput = querySelector<HTMLInputElement>('#search-input', this.$element);
 
+    const $searchInput = querySelector<HTMLInputElement>('#search-input', this.$element);
     const movieName = $searchInput.value;
 
-    const $section = querySelector<HTMLElement>('.item-view');
-    $section.remove();
+    if (movieName === '') {
+      this.renderErrorToast($searchForm, '아무것도 입력하지 않았습니다. 다시 입력해주세요.');
+      $searchInput.focus();
+      return;
+    }
 
-    const $main = querySelector<HTMLElement>('main');
-    new MovieReviewBody($main, { movieType: movieName });
-
+    this.renderMovieReviewBody(movieName);
     $searchForm.reset();
   }
 
@@ -51,6 +52,31 @@ class MovieReviewHeader extends Component<MovieReviewHeaderProps> {
     `;
 
     return $header;
+  }
+
+  private renderErrorToast($searchForm: HTMLElement, errorText: string) {
+    const errorToast = createElement({
+      tagName: 'div',
+      text: errorText,
+      attributeOptions: { class: 'toast' },
+    });
+
+    $searchForm.insertAdjacentElement('afterbegin', errorToast);
+
+    setTimeout(() => {
+      errorToast.classList.add('remove-toast');
+      setTimeout(() => {
+        $searchForm.removeChild(errorToast);
+      }, 1000);
+    }, 500);
+  }
+
+  private renderMovieReviewBody(movieName: string) {
+    const $section = querySelector<HTMLElement>('.item-view');
+    $section.remove();
+
+    const $main = querySelector<HTMLElement>('main');
+    new MovieReviewBody($main, { movieType: movieName });
   }
 }
 
