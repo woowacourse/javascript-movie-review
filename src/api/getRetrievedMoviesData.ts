bc5c5054fb994281d58eeb6233e47cba49a6eac8
ImxYJL <allensain14@gmail.com>
2024-03-20 17:28:14 +0900

    feat: 영화 검색 기능 구현
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>

---
+++ src/api/getRetrievedMoviesData.ts
@@ -0,0 +1,35 @@
+import { getMovieData } from "./getMovieData";
+
+const API_KEY = process.env.API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+const MOVIE_SEARCH_URL = `${BASE_URL}/search/movie`;
+
+export const getRetrievedMoviesData = async (
+  currentPage: string,
+  title: string
+) => {
+  if (typeof API_KEY === "string") {
+    const params = {
+      api_key: API_KEY,
+      language: "ko-KR",
+      page: currentPage,
+      query: `${title}`,
+    };
+
+    const searchMovieUrl = `${MOVIE_SEARCH_URL}?${new URLSearchParams(
+      params
+    ).toString()}`;
+
+    try {
+      const retrievedMovies = await getMovieData(searchMovieUrl);
+      if (retrievedMovies && retrievedMovies.results) {
+        return retrievedMovies.results;
+      } else {
+        throw new Error("영화 검색에 실패했습니다.");
+      }
+    } catch (error) {
+      console.error("영화 정보를 검색하는 중 오류 발생:", error);
+      return [];
+    }
+  }
+};
--- src/component/MovieList/MovieList.ts
+++ src/component/MovieList/MovieList.ts
@@ -1,23 +1,63 @@
 import MovieItem from "../MovieItem/MovieItem";
 import MoreMoviesButton from "../MoreMoviesButton/MoreMoviesButton";
-import { getMovieData } from "../../api/getMovieData";
+import { getRetrievedMoviesData } from "../../api/getRetrievedMoviesData";
 import { getPopularMoviesData } from "../../api/getPopularMoviesData";
 import { $, createElement } from "../../utility/dom";
 
 const MAX_PAGE_COUNT = 50;
-
 class MovieList {
+  #type: string;
+  #title: string;
   #movieListSection;
   #pageCount = 1;
 
   constructor() {
+    this.#title = "";
     this.#movieListSection = $(".item-view") as Element;
+    this.#type = "popular";
 
-    this.renderMovieList();
+    this.renderPopularMovieList();
     this.createMovieList();
+    this.retrievedTitleHandler();
   }
 
-  async createMovieItems() {
+  // searchMoviesByNameHandler()
+  retrievedTitleHandler() {
+    const formEl = $(".search-box");
+
+    formEl?.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      this.#type = "search";
+      this.#movieListSection.innerHTML = "";
+      this.#pageCount = 1;
+
+      const titleInput = (
+        formEl.querySelector("input[type='text']") as HTMLInputElement
+      ).value;
+
+      this.renderRetrievedMovieList(titleInput);
+      this.crateRetrievedMovieItems(titleInput);
+    });
+  }
+
+  async crateRetrievedMovieItems(titleInput: string) {
+    const ul = $("ul");
+
+    const data = await getRetrievedMoviesData(
+      this.#pageCount.toString(),
+      titleInput
+    );
+
+    data.forEach(({ title, poster_path, vote_average }: IMovieItemData) => {
+      const movieItem = new MovieItem({ title, poster_path, vote_average });
+      ul?.appendChild(movieItem.createMovieItem());
+    });
+
+    this.#pageCount += 1;
+  }
+
+  async createPopularMovieItems() {
     const ul = $(".item-list");
     const data = await getPopularMoviesData(this.#pageCount.toString());
 
@@ -33,7 +73,8 @@ class MovieList {
     if (this.#pageCount === MAX_PAGE_COUNT) this.renderMaxPageInfo();
     if (this.#pageCount > MAX_PAGE_COUNT) return;
 
-    this.createMovieItems();
+    if (this.#type === "popular") this.createPopularMovieItems();
+    if (this.#type === "search") this.crateRetrievedMovieItems(this.#title);
   }
 
   renderMaxPageInfo() {
@@ -52,7 +93,24 @@ class MovieList {
     return maxPageInfoElement;
   }
 
-  renderMovieList() {
+  renderRetrievedMovieList(titleInput: string) {
+    const movieListTitle = createElement("h2");
+    movieListTitle.textContent = `"${titleInput}" 검색 결과`;
+
+    const retrievedMovieListUl = createElement("ul", {
+      class: "item-list",
+    });
+
+    const moreMoviesButton = MoreMoviesButton.createMoreMoviesButton();
+
+    this.#movieListSection.appendChild(movieListTitle);
+    this.#movieListSection.appendChild(retrievedMovieListUl);
+    this.#movieListSection.appendChild(moreMoviesButton);
+
+    moreMoviesButton.addEventListener("click", () => this.createMovieList());
+  }
+
+  renderPopularMovieList() {
     const movieListTitle = createElement("h2");
     movieListTitle.textContent = "지금 인기 있는 영화";
 
