80e1ba64fe7fcd1323cc9716d75be9fb9c88db0b
brgndy <brgndy96@gmail.com>
2024-03-20 13:28:51 +0900

    refactor: createElement 함수 라인 줄이기
    Co-authored-by: 0jenn0 <jenn0.6n@gmail.com>

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -7,11 +7,8 @@ interface ElementOption<T extends HTMLElement> {
 function createElement<T extends HTMLElement>(tag: ElementTag, props: ElementOption<T> = {}): T {
   const element = document.createElement(tag) as T;
   Object.entries(props).forEach(([key, value]) => {
-    if (key in element) {
-      (element[key as keyof T] as unknown) = value;
-    } else {
-      element.setAttribute(key, value);
-    }
+    if (key in element) (element[key as keyof T] as unknown) = value;
+    else element.setAttribute(key, value);
   });
   return element;
 }
