cff438d146d991fc30ac1ce4883695882cffeb32
rbgksqkr <rbgks1937@naver.com>
2024-03-20 15:11:16 +0900

    design: 스크롤 제거
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/components/App.ts
+++ src/components/App.ts
@@ -15,7 +15,7 @@ class App {
   template() {
     return `
     <main>
-      <section class="movie-section">
+      <section class="item-view">
         <h2>지금 인기 있는 영화</h2>
       </section>
     </main>
@@ -26,7 +26,7 @@ class App {
     (async () => {
       const movies = await getPopularMovies(1);
       const movieListContainer = new MovieListContainer(movies);
-      const $section = dom.getElement<HTMLElement>(this.$target, '.movie-section');
+      const $section = dom.getElement<HTMLElement>(this.$target, '.item-view');
       $section.appendChild(movieListContainer.$target);
     })();
 
