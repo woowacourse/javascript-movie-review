c699c9298f08720212f449cb6f633993ade0d668
hain-tain <hain7ce@gmail.com>
2024-03-20 13:22:16 +0900

    feat(createElement): html 태그를 생성해주는 util 함수 구현
    Co-authored-by: cys4585 <cys4585@users.noreply.github.com>

---
+++ src/components/utils/createElement.ts
@@ -0,0 +1,29 @@
+interface Props {
+  tagName: string;
+  attribute?: Record<string, string>;
+  addEventListener?: Record<string, (e: Event) => void>;
+  children?: HTMLElement[] | string[];
+}
+
+const createElement = ({
+  tagName,
+  attribute = {},
+  addEventListener = {},
+  children = [],
+}: Props) => {
+  const $element = document.createElement(tagName);
+
+  Object.entries(attribute).forEach(([key, value]) => {
+    $element.setAttribute(key, value);
+  });
+
+  Object.entries(addEventListener).forEach(([eventType, listener]) => {
+    $element.addEventListener(eventType, listener);
+  });
+
+  $element.append(...children);
+
+  return $element;
+};
+
+export default createElement;
