f9bb8a85f9aa01526965371be26fd6db4c0c1efa
pakxe <pigkill40@naver.com>
2024-03-20 18:07:08 +0900

    fix: 리펙토링하며 제거한 함수 호출부 삭제

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -1,12 +1,12 @@
-import { createMovieItem, createSkeletonMovieItem } from './MovieItem.js';
+import { createSkeletonMovieItem } from './MovieItem.js';
 
 const MAX_SINGLE_REQUEST_MOVIE_COUNT = 20;
 
 function createSkeletonMovieList() {
   const list = new Array(MAX_SINGLE_REQUEST_MOVIE_COUNT).fill();
 
-  return list.map((movie) => {
-    const movieItem = movie ? createMovieItem(movie.data) : createSkeletonMovieItem();
+  return list.map(() => {
+    const movieItem = createSkeletonMovieItem();
 
     const li = document.createElement('li');
     li.append(movieItem);
