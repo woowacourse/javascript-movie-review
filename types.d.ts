272801fd5fd6d9cc0fe463a3fde6d9bbe68e2e89
Jeongwoo Park <wahoo9040@naver.com>
2024-03-20 13:39:14 +0900

    chore: 이미지 파일 관련 수정
    Co-Authored-By: 월하 <52562061+vi-wolhwa@users.noreply.github.com>

--- types.d.ts
+++ types.d.ts
@@ -4,4 +4,11 @@ declare global {
       TMDB_API_KEY: string;
     }
   }
+  declare module '*.png';
+  declare module '*.jpg';
+  declare module '*.jpeg';
+  declare module '*.gif';
+  declare module '*.svg';
 }
+
+export {};
