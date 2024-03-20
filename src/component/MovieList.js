18f6f5bdc5c262d88026229edf5efa3d760e9a09
pakxe <pigkill40@naver.com>
2024-03-20 14:48:27 +0900

    feat: 영화 목록 컴포넌트 구현

---
+++ src/component/MovieList.js
@@ -0,0 +1,18 @@
+import { createMovieItem, createSkeletonMovieItem } from './MovieItem.js';
+
+const MAX_SINGLE_REQUEST_MOVIE_COUNT = 20;
+
+function createMovieList(movieList) {
+  const list = movieList || new Array(MAX_SINGLE_REQUEST_MOVIE_COUNT).fill();
+
+  return list.map((movie) => {
+    const movieItem = movie ? createMovieItem(movie) : createSkeletonMovieItem();
+
+    const li = document.createElement('li');
+    li.append(movieItem);
+
+    return li;
+  });
+}
+
+export { createMovieList };
