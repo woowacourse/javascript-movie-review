af7ba34eed4d1bb320c69d7f62268147eba6702c
greetings1012 <ahnqwerty@gmail.com>
2024-03-20 16:25:57 +0900

    feat(Button): 버튼 컴포넌트 구현

---
+++ src/components/Button/Button.ts
@@ -0,0 +1,14 @@
+import IButton from '../../interfaces/IButton';
+
+const Button = {
+  create(buttonInformation: IButton): HTMLElement {
+    const button = document.createElement('button');
+
+    button.textContent = `${buttonInformation.innerText}`;
+    buttonInformation.classes.forEach((className) => button.classList.add(className));
+
+    return button;
+  },
+};
+
+export default Button;
