4e5b4cc57a59f4bc231a12ab153035d8f8ad7922
brgndy <brgndy96@gmail.com>
2024-03-21 08:17:30 +0900

    refactor: 상수 추가
    Co-authored-by: 0jenn0 <jenn0.6n@gmail.com>

--- src/constants/api/api.ts
+++ src/constants/api/api.ts
@@ -1,7 +1,11 @@
 export const BASE_URL = 'https://api.themoviedb.org/3/';
 
+export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
+
+export const BASE_IMAGE_WIDTH = 'w500';
+
 export const API_ENDPOINT = Object.freeze({
-  POPULAR: (pageNumber: number = 1) => `${BASE_URL}movie/popular?page=${pageNumber}`,
+  POPULAR: (pageNumber: number = 1) => `${BASE_URL}movie/popular?language=ko-kr&page=${pageNumber}`,
 });
 
 export const API_OPTION = {
