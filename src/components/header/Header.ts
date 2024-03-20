5ff4f6a8ab9216b401433c47fcfba6ac2c1607a5
rbgksqkr <rbgks1937@naver.com>
2024-03-20 18:00:40 +0900

    design(Header): 검색 버튼 스타일링 추가
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -20,18 +20,20 @@ class Header {
   template() {
     return /*html*/ `
       <h1>
-        <img src="./images/logo.png" alt="MovieList 로고" />
+        <a href="/">
+          <img src="./images/logo.png" alt="MovieList 로고" />
+        </a>
       </h1>
       <form class="search-box">
         <input id="search-input" type="text" placeholder="검색" />
-        <button class="search-button">검색</button>
+        <button class="search-button"><img width="14px" src='./images/search_button.png' /></button>
       </form>
 `;
   }
 
   render() {
     this.$target.innerHTML += this.template();
-    const $image = dom.getElement<HTMLImageElement>(this.$target, 'h1 > img');
+    const $image = dom.getElement<HTMLImageElement>(this.$target, 'h1 > a > img');
     $image.setAttribute('src', this.#imageSrc);
   }
 }
