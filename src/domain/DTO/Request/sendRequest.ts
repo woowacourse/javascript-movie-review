d792ac086a108aba6cae3050d2772f5048d3ff63
greetings1012 <ahnqwerty@gmail.com>
2024-03-21 10:46:34 +0900

    feat(sendRequest): fetchPopularMovies, fetchSearchMovies 메서드 기능 구현

--- src/domain/DTO/Request/sendRequest.ts
+++ src/domain/DTO/Request/sendRequest.ts
@@ -1,8 +1,38 @@
-const fetchData = (url: string, options: object) => {
-  fetch(url, options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-};
+import { POPULAR_MOVIES_URL, MOVIE_SEARCH_URL } from '../../../constants/DTO';
 
-export default fetchData;
+async function fetchPopularMovies(page: number) {
+  const KEY = process.env.API_KEY;
+  const popularMovieUrl =
+    POPULAR_MOVIES_URL +
+    '?' +
+    new URLSearchParams({
+      api_key: KEY as string,
+      language: 'ko-KR',
+      page: `${page}`,
+    });
+
+  const response = await fetch(popularMovieUrl);
+  const popularMovies = await response.json();
+  console.log(popularMovies);
+  return popularMovies;
+}
+
+async function fetchSearchMovies(page: number, userInput: string) {
+  const KEY = process.env.API_KEY;
+  const popularMovieUrl =
+    MOVIE_SEARCH_URL +
+    '?' +
+    new URLSearchParams({
+      api_key: KEY as string,
+      query: userInput,
+      language: 'ko-KR',
+      page: `${page}`,
+    });
+
+  const response = await fetch(popularMovieUrl);
+  const popularMovies = await response.json();
+  console.log(popularMovies);
+  return popularMovies;
+}
+
+export { fetchPopularMovies, fetchSearchMovies };
