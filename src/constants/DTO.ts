1e165486287edcb627fbc29e12c1ea04ee0b5e2b
greetings1012 <ahnqwerty@gmail.com>
2024-03-21 10:45:34 +0900

    feat(DTO): API 통신을 위한 URL 선언

---
+++ src/constants/DTO.ts
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
+const MOVIE_SEARCH_URL = `${BASE_URL}/search/movie`;
+
+export { POPULAR_MOVIES_URL, MOVIE_SEARCH_URL };
