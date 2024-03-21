30902eae73167a013197c600e8b170ce9d2459d7
pakxe <pigkill40@naver.com>
2024-03-21 13:40:55 +0900

    feat: 타입 단언 제거

--- src/domain/MovieService.ts
+++ src/domain/MovieService.ts
@@ -1,9 +1,7 @@
+import { MOVIE_SEARCH_URL, POPULAR_MOVIES_URL } from '../constant/config';
 import fetchDataFromUrl from '../util/fetchDataFromUrl';
 import Movie from './Movie';
-
-const BASE_URL = 'https://api.themoviedb.org/3';
-const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
-const MOVIE_SEARCH_URL = `${BASE_URL}/search/movie`;
+import getEnvVariable from './getEnvVariable';
 
 interface MovieListData {
   total_pages: number;
@@ -16,11 +14,10 @@ interface MoviePageData extends MovieListData {
 
 class MovieService {
   async fetchPopularMovieList(pageNumber: number) {
-    // TODO: ! 지우기
-    const { total_pages, results }: MovieListData = await fetchDataFromUrl(POPULAR_MOVIES_URL, {
-      api_key: process.env.API_KEY!,
+    const { total_pages, results } = await fetchDataFromUrl(POPULAR_MOVIES_URL, {
+      api_key: getEnvVariable('API_KEY'),
       language: 'ko-KR',
-      page: pageNumber.toString(),
+      page: pageNumber,
     });
 
     return this.createMoviePageData({ total_pages, results, pageNumber });
@@ -28,7 +25,7 @@ class MovieService {
 
   async fetchSearchResult({ query, pageNumber }: { query: string; pageNumber: number }) {
     const { total_pages, results } = await fetchDataFromUrl(MOVIE_SEARCH_URL, {
-      api_key: process.env.API_KEY!,
+      api_key: getEnvVariable('API_KEY'),
       language: 'ko-KR',
       query,
       page: pageNumber,
@@ -38,9 +35,8 @@ class MovieService {
   }
 
   createMoviePageData({ total_pages, results, pageNumber }: MoviePageData) {
-    // TODO: 타입 바꾸기
     const movieList: Movie[] = results.map(
-      (result: { id: number; title: string; poster_path: string; vote_average: number; overview: string }) =>
+      (result) =>
         new Movie({
           id: result.id,
           title: result.title,
