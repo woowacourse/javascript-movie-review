b0f61ef851ce4d44deb58032bc8049826e7d8758
chysis <chysiss@naver.com>
2024-03-21 10:46:16 +0900

    feat: 유틸 함수 분리
    Co-authored-by: ooherin <555ohr@naver.com>

---
+++ src/utils/formatList.ts
@@ -0,0 +1,12 @@
+import { MovieAPIReturnType } from './../domain/services/APIService';
+
+export const formatMovieList = (result: MovieAPIReturnType) => {
+  const formattedMovieList = result.results.map(movie => ({
+    id: movie.id,
+    title: movie.title,
+    posterPath: movie.poster_path,
+    voteAverage: movie.vote_average,
+  }));
+
+  return formattedMovieList;
+};
