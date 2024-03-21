99a7a53e553a22c4ca9c698ad8ed89a62149b600
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-21 13:16:47 +0900

    feat: 이미지 없을 때 `no Img` 사진으로 대체
    Co-Authored-By: badahertz52 <badahertz52@gmail.com>

--- src/components/MovieImg.ts
+++ src/components/MovieImg.ts
@@ -1,13 +1,17 @@
+import noImg from "../../templates/no_image.svg";
 import { IMAGE_URL } from "../config";
 import { Movie } from "../type/movie";
 import { createElementWithAttribute } from "../utils";
 
 const POSTER_STIZE = "w500";
 
+const imgSrc = (path: string | null) =>
+  path === null ? noImg : IMAGE_URL + POSTER_STIZE + path;
+
 const MovieImg = (movie: Movie) =>
   createElementWithAttribute("img", {
     class: "item-thumbnail",
-    src: IMAGE_URL + POSTER_STIZE + movie.poster_path,
+    src: imgSrc(movie.poster_path),
     loading: "lazy",
     alt: movie.title,
   });
