4831b241bc384eaeb90216eba1e8a872bd98372f
jinsil <jinsil378900@gmail.com>
2024-03-21 13:11:24 +0900

    refactor: 변수명 변경
    Co-authored-by: jaeml06 <jaeml0630@gmail.com>

--- src/AppController.js
+++ src/AppController.js
@@ -3,21 +3,20 @@ import { showSkeleton, updateCard } from "./components/movieCard/movieCard";
 import { MovieListWrapper } from "./components/movieListWrapper/MovieListWrapper";
 
 class AppController {
-  #currentPage;
+  #currentView ;
   
   constructor() {
-    this.#currentPage = new MovieListWrapper('지금 인기 있는 영화', 'popular');
+    this.#currentView  = new MovieListWrapper('지금 인기 있는 영화', 'popular');
   }
   
   async start() {
     const app = document.querySelector('#app');
-    
     const header = createHeader((inputValue) => {
-      this.#currentPage = new MovieListWrapper(`"${inputValue}" 검색 결과`, 'search', inputValue)
-      this.#currentPage.create();
+      this.#currentView  = new MovieListWrapper(`"${inputValue}" 검색 결과`, 'search', inputValue)
+      this.#currentView .create();
     });
     app.prepend(header);
-    this.#currentPage.create();
+    this.#currentView .create();
   }
 };
 
