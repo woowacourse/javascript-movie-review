1709458b092610dd9748ea5232bc989c5e2095a1
greetings1012 <ahnqwerty@gmail.com>
2024-03-21 17:08:11 +0900

    style(index.js): 필요없는 주석 제거

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import './resources.js';
 import MovieHeader from './components/MovieHeader/MovieHeader';
 import ItemView from './components/ItemView/ItemView';
-// import { AUTHENTICATION_OPTION } from './constants/DTO';
-// import authenticateRequest from './domain/DTO/Request/AuthenticationRequest';
 
 const init = () => {
-  // authenticateRequest(AUTHENTICATION_OPTION);
   MovieHeader.create();
   const item = new ItemView();
 };
