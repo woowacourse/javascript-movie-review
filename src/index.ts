e415cf14c29de4dc9faadf555dcc9f4699659802
hwinkr <dnddl8280@naver.com>
2024-03-21 16:54:01 +0900

    chore(App): 사용하지 않는 asset 파일 제거

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import "../templates/reset.css";
 import "../templates/common.css";
-import "../templates/star_empty.png";
-import "../templates/star_filled.png";
-import "../templates/logo.png";
-import "../templates/search_button.png";
 
 import { $ } from "./utils/dom";
 import App from "./App";
