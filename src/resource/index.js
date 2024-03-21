b69cd041fdc4c369de4c0629f0513e5a522d9789
GimGayeon <kky1373@naver.com>
2024-03-21 17:21:13 +0900

    chore: 에러 이미지 추가
    Co-Authored-By: Largopie <jae990320@kakao.com>

--- src/resource/index.js
+++ src/resource/index.js
@@ -7,3 +7,4 @@ export { default as STAR_EMPTY } from '../../templates/star_empty.png';
 export { default as STAR_FILLED } from '../../templates/star_filled.png';
 export { default as NO_IMAGE } from '../../templates/no_image.png';
 export { default as NO_SEARCH } from '../../templates/no_search_result.png';
+export { default as ERROR } from '../../templates/error_image.png';
