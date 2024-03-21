a0955da1a6f677b50798323da8f0c5a283b6d066
hanyoung <phanyoung@naver.com>
2024-03-21 10:59:49 +0900

    feat(SkeletonUI): SkeletonUI 클래스 생성
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

---
+++ src/components/SkeletonUI.ts
@@ -0,0 +1,25 @@
+import { $ } from "../utils/dom";
+import { HTMLTemplate } from "./abstract/BaseComponent";
+
+export default class SkeletonUI {
+  private template: HTMLTemplate;
+
+  constructor(template: HTMLTemplate) {
+    this.template = template;
+  }
+
+  render(targetId: string) {
+    const element = $(targetId);
+
+    if (!(element instanceof HTMLElement)) {
+      return;
+    }
+
+    console.log(this.template);
+    element.innerHTML = this.template;
+  }
+
+  insert(targetId: string, position: InsertPosition = "beforeend") {
+    $(targetId)?.insertAdjacentHTML(position, this.template);
+  }
+}
