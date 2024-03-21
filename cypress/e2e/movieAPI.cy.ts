11532ab21bd0142f352b65f4b136214504026377
jayming66 <mjw8941@gmail.com>
2024-03-21 16:27:39 +0900

    test: TMDB에서 영화 검색 GET요청 API 테스트
    Co-Authored-By: 00kang <70834044+00kang@users.noreply.github.com>

--- cypress/e2e/movieAPI.cy.ts
+++ cypress/e2e/movieAPI.cy.ts
@@ -3,21 +3,31 @@ describe('movieAPI 테스트', () => {
   it('TMDB API에 popular GET 요청을 하면 한번에 20개의 목록을 받아온다', () => {
     cy.visit('http://localhost:8080/#');
 
-    const POPULAR_MOVIES_URL = 'https://api.themoviedb.org/3/movie/popular';
-    const popularMovieUrl =
-      POPULAR_MOVIES_URL +
-      '?' +
-      new URLSearchParams({
-        api_key: Cypress.env('API_KEY'),
-        language: 'ko-KR',
-        page: 1,
-      });
+    const popularMovieUrl = `${Cypress.env('POPULAR_MOVIES_URL')}?${new URLSearchParams({
+      api_key: Cypress.env('API_KEY'),
+      language: 'ko-KR',
+      page: '1',
+    })}`;
 
     cy.request('GET', popularMovieUrl).as('popularMovies');
 
     cy.get('@popularMovies').its('status').should('eq', 200);
     cy.get('@popularMovies').its('body.results').should('have.length', 20);
-    cy.get('@popularMovies').its('status').should('not.eq', 400);
-    cy.get('@popularMovies').its('body.results').should('not.have.length', 21);
+  });
+
+  it('TMDB API에 search GET 요청을 하면 검색한 영화에 대한 정보를 한번에 최대 20개의 목록을 받아온다.', () => {
+    cy.visit('http://localhost:8080/#');
+
+    const popularMovieUrl = `${Cypress.env('SEARCH_MOVIES_URL')}?${new URLSearchParams({
+      api_key: Cypress.env('API_KEY'),
+      language: 'ko-KR',
+      page: '1',
+      query: '해리',
+    })}`;
+
+    cy.request('GET', popularMovieUrl).as('popularMovies');
+
+    cy.get('@popularMovies').its('status').should('eq', 200);
+    cy.get('@popularMovies').its('body.results').should('have.length', 20);
   });
 });
