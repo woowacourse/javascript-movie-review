8258f3e8062829bddc7af0375b623e77914ea37d
pakxe <pigkill40@naver.com>
2024-03-21 13:36:10 +0900

    chore: 재요청 이미지 import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './asset/logo.png';
 import './asset/search_button.png';
 import './asset/star_empty.png';
 import './asset/star_filled.png';
+import './asset/retry.svg';
 
 import './component/toast/toast.css';
 import './component/reset.css';
