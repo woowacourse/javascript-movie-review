f4332121a74888c50a3279bea171aa995e08edf7
Jeongwoo Park <wahoo9040@naver.com>
2024-03-21 16:59:55 +0900

    test: cypress 커맨드 설정
    Co-Authored-By: 월하 <52562061+vi-wolhwa@users.noreply.github.com>

--- cypress/e2e/movie-list-api.cy.ts
+++ cypress/e2e/movie-list-api.cy.ts
@@ -1,6 +1,6 @@
 describe('API 테스트', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
   });
 
   it('영화 목록 API를 호출하면 한 번에 20개씩 목록에 나열되어야 한다', () => {
--- cypress/e2e/movie-search.cy.ts
+++ cypress/e2e/movie-search.cy.ts
@@ -1,16 +1,10 @@
 describe('검색 테스트', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
   });
 
   it('검색 결과가 20개 보다 작으면 더보기 버튼은 사라진다.', () => {
-    cy.intercept(
-      {
-        method: 'GET',
-        url: /^https:\/\/api\.themoviedb\.org\/3\/search\/movie*/,
-      },
-      { fixture: 'movie-search.json' },
-    );
+    cy.interceptMovieSearchAPI();
     cy.get('.search-button').click();
     expect(cy.get('#see-more-button').should('have.class', 'hidden'));
   });
