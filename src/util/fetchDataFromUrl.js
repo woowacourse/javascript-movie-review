ed09cae43d2ef44134c7c2c3dba6ef6d0d01b27f
pakxe <pigkill40@naver.com>
2024-03-21 13:41:46 +0900

    rename: ts 로 파일 확장자 변경

--- src/util/fetchDataFromUrl.js
+++
@@ -1,6 +0,0 @@
-export default async function fetchDataFromUrl(url, query) {
-  const queryUrl = url + '?' + new URLSearchParams(query);
-  const response = await fetch(queryUrl);
-  const { total_pages, results } = await response.json();
-  return { total_pages, results };
-}
