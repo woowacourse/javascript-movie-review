cb9c500278c7b2ea118eceefcda087e8389a7979
pakxe <pigkill40@naver.com>
2024-03-21 16:11:33 +0900

    chore: 영화 이미지가 없는 경우 띄울 빈 이미지 import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './asset/search_button.png';
 import './asset/star_empty.png';
 import './asset/star_filled.png';
 import './asset/retry.svg';
+import './asset/poster_not_found.jpg';
 
 import './component/toast/toast.css';
 import './component/reset.css';
