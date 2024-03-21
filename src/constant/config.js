a32807394dffb7cb5302335d1e1cb1355d84215b
pakxe <pigkill40@naver.com>
2024-03-21 13:40:42 +0900

    feat: api 요청에 필요한 엔드포인트 상수화

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,2 +1,5 @@
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w';
+
+export const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
+export const MOVIE_SEARCH_URL = `${BASE_URL}/search/movie`;
