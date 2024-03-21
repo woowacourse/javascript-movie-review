f8846a35bdacdc06d67f56dc9d531b1751e3def7
pakxe <pigkill40@naver.com>
2024-03-21 13:34:41 +0900

    test: 서버 에러가 발생하는 경우 테스트 작성

--- cypress/e2e/api-error-test.cy.js
+++ cypress/e2e/api-error-test.cy.js
@@ -1,5 +1,3 @@
-import ERROR_MESSAGE from '../../src/constant/errorMessage.ts';
-
 describe('API에서 에러가 발생하는 경우 테스트', () => {
   it('서버 에러가 발생하면 toast를 띄운다', () => {
     cy.intercept(
@@ -11,10 +9,11 @@ describe('API에서 에러가 발생하는 경우 테스트', () => {
         statusCode: 500,
         body: { message: 'Internal Server Error' },
       },
-    );
+    ).as('getPopularMovies');
 
-    cy.visitHome();
-    cy.verifyToastExists(ERROR_MESSAGE.SERVER_ERROR);
+    cy.visit('/');
+
+    cy.contains('다시 요청해주세요.').should('exist');
   });
 
   it('서버 에러가 발생하면 재요청 버튼을 눌러 다시 api를 불러올 수 있다.', () => {
@@ -39,36 +38,13 @@ describe('API에서 에러가 발생하는 경우 테스트', () => {
           });
         }
       },
-    );
+    ).as('getPopularMovies');
 
-    cy.visitHome();
-    cy.verifyToastExists(ERROR_MESSAGE.SERVER_ERROR);
+    cy.visit('/');
+    cy.contains('다시 요청해주세요.').should('exist');
 
-    cy.get('#retry-button').click();
-    cy.verifyToastNotExists();
+    cy.get('.retry-button').click();
+    cy.contains('다시 요청해주세요.').should('not.exist');
     cy.get('.skeleton').should('not.exist');
-  });
-
-  it('5번 초과로 비동기 에러가 발생하면 더 이상 요청할 수 없게 제한한다.', () => {
-    cy.intercept(
-      {
-        method: 'GET',
-        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular*/,
-      },
-      {
-        statusCode: 500,
-        body: { message: 'Internal Server Error' },
-      },
-    ).as('fetchMovies');
-
-    cy.visitHome();
-
-    Array.from({ length: 4 }).forEach(() => {
-      cy.get('#retry-button').click();
-      cy.wait('@fetchMovies');
-    });
-
-    cy.get('#retry-button').click();
-    cy.verifyToastExists(ERROR_MESSAGE.RETRY_LIMIT_EXCEEDED);
   });
 });
