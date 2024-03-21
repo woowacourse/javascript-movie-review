1285c24e3c14156475d70db2e33fd04e56f3d22c
greetings1012 <ahnqwerty@gmail.com>
2024-03-21 15:23:20 +0900

    feat(SearchValidator): isInputEmpty 구현

--- src/domain/Validator/SearchValidator.ts
+++ src/domain/Validator/SearchValidator.ts
@@ -1,10 +1,14 @@
 const SearchValidator = {
   validate(searchInput: string) {
+    this.isInputEmpty(searchInput);
     this.isInputSpace(searchInput);
   },
 
+  isInputEmpty(searchInput: string) {
+    if (searchInput === '') throw new Error(`공백을 제외하고 1글자 이상 입력하세요.`);
+  },
+
   isInputSpace(searchInput: string) {
-    console.log(searchInput.split(' ').join(' '));
     if (searchInput.replace(/\s+/g, '') === '') {
       throw new Error(`공백을 제외하고 1글자 이상 입력하세요.`);
     }
