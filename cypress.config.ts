fff2407dd0faf04359815d9021e5af9ea022559a
pakxe <pigkill40@naver.com>
2024-03-21 13:34:29 +0900

    test: baseUrl 설정

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
+    baseUrl: 'http://localhost:8080',
   },
 });
