7c0469d9acf710a756f8280898a6657d9524af9e
rbgksqkr <rbgks1937@naver.com>
2024-03-21 20:49:15 +0900

    chore: 헤더 경로 수정
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -24,7 +24,7 @@ class Header {
   template() {
     return /*html*/ `
       <h1>
-        <a href='/'>
+        <a href=${process.env.NODE_ENV === 'production' ? process.env.PUBLIC_PATH :'/'}>
           <img src="./images/logo.png" alt="MovieList 로고" />
         </a>
       </h1>
