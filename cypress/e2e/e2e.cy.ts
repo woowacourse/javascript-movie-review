53885e87e7bfed533b2bdbb9a7b12c7bf051dd90
rbgksqkr <rbgks1937@naver.com>
2024-03-21 17:35:38 +0900

    test(e2e): 더보기 클릭 시 영화 목록 추가 렌더링 테스트 구현
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- cypress/e2e/e2e.cy.ts
+++ cypress/e2e/e2e.cy.ts
@@ -5,4 +5,9 @@ describe('영화 리뷰 e2e 테스트', () => {
   it('랜딩 페이지 접속 시 영화 목록 20개를 보여준다.', () => {
     cy.get('.item-list').find('li').should('have.length', 20);
   });
+
+  it('더보기 클릭 시 영화 목록 20개를 추가로 보여준다.', () => {
+    cy.get('#more-button').click();
+    cy.get('.item-list').find('li').should('have.length', 40);
+  });
 });
