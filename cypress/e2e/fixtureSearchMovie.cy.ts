75004f9809df5b2b2f7960a8f3941ed6546136b5
jayming66 <mjw8941@gmail.com>
2024-03-21 17:15:57 +0900

    test: Fixture를 이용한 영화 검색 결과 렌더링 테스트
    Co-Authored-By: 00kang <70834044+00kang@users.noreply.github.com>

---
+++ cypress/e2e/fixtureSearchMovie.cy.ts
@@ -0,0 +1,28 @@
+/* eslint-disable max-lines-per-function */
+describe('Fixture를 이용한 영화 검색 테스트', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api\.themoviedb\.org\/3\/search\/movie*/,
+      },
+      { fixture: 'movie-search.json' },
+    ).as('getSearchMovies');
+
+    cy.visit('http://localhost:8080');
+  });
+
+  it('영화 검색 API를 호출하면 검색한 영화에 대한 결과가 목록에 나열된다', () => {
+    cy.get('.search-box input[name=query]').type('해리');
+    cy.get('.search-box').submit();
+
+    cy.wait('@getSearchMovies').then((interception) => {
+      // fixture data 검증 - 10개의 fixture data
+      const searchMovies = interception.response?.body.results;
+      expect(searchMovies.length).to.equal(20);
+
+      // fixture data로 렌더링 검증
+      cy.get('.item-list > li').should('have.length', 20);
+    });
+  });
+});
