5f6072e4beefac896c31e5366bb58d78ded2001c
GimGayeon <kky1373@naver.com>
2024-03-21 17:21:43 +0900

    feat(index): 컴포넌트 연결
    Co-Authored-By: Largopie <jae990320@kakao.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import "./resource/index.js"
+import Header from './components/Header.ts';
+import MovieContainer from './components/MovieContainer.ts';
+import SearchBox from './components/SearchBox.ts';
+import Title from './components/Title.ts';
+import './resource/index.js';
+import { LOGO } from './resource/index.js';
+
+const app = document.getElementById('app');
+const main = document.createElement('main');
+
+const logo = document.createElement('img');
+logo.src = LOGO;
+logo.setAttribute('alt', 'MovieList 로고');
+
+const header = Header({ title: Title({ element: logo }), searchBox: SearchBox() });
+
+app.appendChild(header);
+app.appendChild(main);
+const movieContainer = new MovieContainer(main);
+
+app.addEventListener('search', (event) => {
+  movieContainer.render(event.detail);
+});
+
+app.addEventListener('home-click', () => {
+  movieContainer.render();
+});
