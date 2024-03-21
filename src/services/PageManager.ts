74dc17e4317349157a1089c3fe2045457106dca5
brgndy <brgndy96@gmail.com>
2024-03-21 11:23:17 +0900

    feat: 페이지 매니저 클래스 생성
    Co-authored-by: 0jenn0 <jenn0.6n@gmail.com>

---
+++ src/services/PageManager.ts
@@ -0,0 +1,25 @@
+class PageManager {
+  currentPage;
+
+  static DEFAULT_PAGE_NUMBER = 2;
+
+  constructor() {
+    this.currentPage = PageManager.DEFAULT_PAGE_NUMBER;
+  }
+
+  increasePage() {
+    return (this.currentPage += 1);
+  }
+
+  resetPage() {
+    this.currentPage = PageManager.DEFAULT_PAGE_NUMBER;
+  }
+
+  getCurrentPage() {
+    return this.currentPage;
+  }
+}
+
+const pageManager = new PageManager();
+
+export default pageManager;
