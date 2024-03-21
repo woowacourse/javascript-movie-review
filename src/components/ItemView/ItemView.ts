a37e3ebffc5052ed8c4994892352391f35572c3c
greetings1012 <ahnqwerty@gmail.com>
2024-03-21 10:49:00 +0900

    feat(ItemView): ItemView 클래스 구현

--- src/components/ItemView/ItemView.ts
+++ src/components/ItemView/ItemView.ts
@@ -1,18 +1,30 @@
+import { fetchPopularMovies, fetchSearchMovies } from '../../domain/DTO/Request/sendRequest';
 import { BUTTON } from '../../constants/INFORMATION';
 import Button from '../Button/Button';
 import MovieItems from '../MovieItems/MovieItems';
 
-const ItemView = {
-  create(containerTitle: string, fetchData: object) {
-    const itemView = document.createElement('section');
+class ItemView {
+  #page: number;
 
-    itemView.classList.add('item-view');
+  constructor(itemViewTitle: string = '지금 인기 있는 영화') {
+    this.#page = 0;
+    this.create(itemViewTitle);
+  }
 
-    itemView.appendChild(this.createTitle(containerTitle));
-    itemView.appendChild(MovieItems.create(fetchData));
-    itemView.appendChild(Button.create(BUTTON.showMore));
+  async create(itemViewTitle: string) {
+    const itemView = document.querySelector('.item-view');
+
+    if (itemView) {
+      const button = Button.create(BUTTON.showMore);
+
+      itemView.appendChild(this.createTitle(itemViewTitle));
+      itemView.appendChild(button);
+
+      this.mountItems(button);
+      button.addEventListener('click', () => this.mountItems(button));
+    }
     return itemView;
-  },
+  }
 
   createTitle(containerTitle: string) {
     const title = document.createElement('h2');
@@ -20,7 +32,24 @@ const ItemView = {
     title.textContent = `${containerTitle}`;
 
     return title;
-  },
-};
+  }
+
+  async mountItems(button: HTMLElement, search?: string) {
+    const skeleton = MovieItems.createSkeleton();
+    const movieListData = await this.getMovieListData(search);
+
+    button.insertAdjacentElement('beforebegin', skeleton);
+
+    MovieItems.replaceSkeletons(skeleton, movieListData);
+  }
+
+  async getMovieListData(search?: string) {
+    if (search) {
+      return await fetchSearchMovies(++this.#page, search);
+    } else {
+      return await fetchPopularMovies(++this.#page);
+    }
+  }
+}
 
 export default ItemView;
