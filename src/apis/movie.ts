bfa07e5a54fa988dd84aecf53f39c6c9b249ab5f
rbgksqkr <rbgks1937@naver.com>
2024-03-21 15:26:12 +0900

    feat(movie API): 응답 코드에 따라 Error 발생 로직 구현
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/apis/movie.ts
+++ src/apis/movie.ts
@@ -5,29 +5,39 @@ const BASE_URL = 'https://api.themoviedb.org';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 
 export const getPopularMovies = async (page: number): Promise<ISearchResult> => {
-  const response = await fetch(BASE_URL + `/3/movie/popular?language=ko-KR&page=${page}`, {
+  const fetchUrl = `/3/movie/popular?language=ko-KR&page=${page}`;
+  const response = await fetch(BASE_URL + fetchUrl, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${process.env.API_KEY}`,
       accept: 'application/json',
     },
   });
+
+  if (response.status === 400) throw new Error('[API ERROR] BAD_REQUEST');
+  if (response.status === 401) throw new Error('[API ERROR] NOT_AUTHORIZED');
+  if (response.status === 404) throw new Error('[API ERROR] NOT_FOUND');
+  if (response.status !== 200) throw new Error('[API ERROR] ERROR');
+
   const { results, total_pages } = await response.json();
 
   return { movies: results.map(parseMovieResponse), totalPages: total_pages };
 };
 
 export const searchMoviesByTitle = async (title: string, page: number): Promise<ISearchResult> => {
-  const response = await fetch(
-    BASE_URL + `/3/search/movie?query=${title}&include_adult=false&language=en-US&page=${page}`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${process.env.API_KEY}`,
-        accept: 'application/json',
-      },
+  const fetchUrl = `/3/search/movie?query=${title}&include_adult=false&language=en-US&page=${page}`;
+  const response = await fetch(BASE_URL + fetchUrl, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.API_KEY}`,
+      accept: 'application/json',
     },
-  );
+  });
+
+  if (response.status === 400) throw new Error('[API ERROR] BAD_REQUEST');
+  if (response.status === 401) throw new Error('[API ERROR] NOT_AUTHORIZED');
+  if (response.status === 404) throw new Error('[API ERROR] NOT_FOUND');
+  if (response.status !== 200) throw new Error('[API ERROR] ERROR');
 
   const { results, total_pages } = await response.json();
 
