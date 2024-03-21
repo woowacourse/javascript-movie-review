65ffdcc72f3dc50c9943ea92491ca1ba3b6b925e
GimGayeon <kky1373@naver.com>
2024-03-21 17:22:45 +0900

    chore: ESLint 적용
    Co-Authored-By: Largopie <jae990320@kakao.com>

--- cypress/e2e/movie-list-api.cy.js
+++ cypress/e2e/movie-list-api.cy.js
@@ -1,18 +1,18 @@
-import { POPULAR_MOVIES_URL } from "../../src/api";
+import { POPULAR_MOVIES_URL } from '../../src/api';
 
 describe('영화 목록 API 테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080');
-  })
+  });
   it('한 페이지의 인기 영화 목록을 불러왔을 때, 20개의 영화 목록이 나열된다.', () => {
     cy.request({
       method: 'GET',
       url: `${POPULAR_MOVIES_URL}?language=ko-KR&page=1`,
-      headers:{
-        Authorization: `Bearer ${Cypress.env('ACCESS_TOKEN')}`
+      headers: {
+        Authorization: `Bearer ${Cypress.env('ACCESS_TOKEN')}`,
       },
     }).as('popularMovies');
 
     cy.get('@popularMovies').its('body.results').should('have.length', 20);
-  })
-})
+  });
+});
