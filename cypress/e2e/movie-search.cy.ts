b7c7a8d65f83fe69b37a84e128da7186786b2efb
Jeongwoo Park <wahoo9040@naver.com>
2024-03-21 16:44:28 +0900

    text: cypress 검색 결과 테스트
    Co-Authored-By: 월하 <52562061+vi-wolhwa@users.noreply.github.com>

--- cypress/e2e/movie-search.cy.ts
+++ cypress/e2e/movie-search.cy.ts
@@ -1,5 +1,9 @@
 describe('검색 테스트', () => {
   beforeEach(() => {
+    cy.visit('http://localhost:8080');
+  });
+
+  it('검색 결과가 20개 보다 작으면 더보기 버튼은 사라진다.', () => {
     cy.intercept(
       {
         method: 'GET',
@@ -7,12 +11,16 @@ describe('검색 테스트', () => {
       },
       { fixture: 'movie-search.json' },
     );
-
-    cy.visit('http://localhost:8080');
-  });
-
-  it('검색 결과가 20개 보다 작으면 더보기 버튼은 사라진다.', () => {
     cy.get('.search-button').click();
     expect(cy.get('#see-more-button').should('have.class', 'hidden'));
+  });
+
+  it('검색 결과는 제목에 포함되어 있어야 한다.', () => {
+    cy.get('input[name="searchContent"]').type('해리포터');
+    cy.get('.search-button').click();
+    cy.get('.item-title').each(($title) => {
+      const text = $title.text();
+      expect(text).contain('해리 포터');
+    });
   });
 });
