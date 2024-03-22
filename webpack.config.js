0d1ed0a5c690d4f90db7f573b2e32c8749997609
rbgksqkr <rbgks1937@naver.com>
2024-03-22 18:46:01 +0900

    fix: webpack image import 오류 해결
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
-const webpack = require('webpack');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -39,10 +38,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
