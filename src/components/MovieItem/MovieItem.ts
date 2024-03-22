74e7d01ea2ec7f7c0044e9d242af45ae83b0ba40
김진호 <rlawlsgh1227@gmail.com>
2024-03-23 00:38:57 +0900

    refactor: MovieItem template로 사용해서 코드 가독성 증대

--- src/components/MovieItem/MovieItem.ts
+++ src/components/MovieItem/MovieItem.ts
@@ -9,71 +9,44 @@ interface MovieItemsProps {
   vote_average: number;
 }
 
+const template = /* html */ `
+  <a href="#">
+    <div class="item-card">
+      <img class="item-thumbnail" loading="lazy" />
+      <p class="item-title"></p>
+      <div class="item-score-container">
+        <p class="item-score"></p>
+        <img class="star-icon" alt="별점" />
+      </div>
+    </div>
+  </a>
+`;
+
 class MovieItem {
-  private template: HTMLElement;
+  private template: HTMLLIElement;
 
-  constructor() {
-    this.template = this.createSkeleton();
+  constructor(props: MovieItemsProps) {
+    this.template = this.createTemplate();
+    this.setMovieInfo(props);
   }
 
-  createSkeleton() {
-    const li = document.createElement('li');
-    li.classList.add('li-skeleton');
-
-    const a = document.createElement('a');
-    a.setAttribute('href', '#');
-
-    const div = document.createElement('div');
-    div.classList.add('item-card', 'skeleton');
-
-    const img = document.createElement('img');
-    img.classList.add('item-thumbnail', 'skeleton');
-    img.setAttribute('loading', 'lazy');
-
-    const p1 = document.createElement('p');
-    p1.classList.add('item-title', 'skeleton');
-
-    const scoreContainer = document.createElement('div');
-    scoreContainer.classList.add('item-score-container');
-
-    const p2 = document.createElement('p');
-    p2.classList.add('item-score', 'skeleton');
-
-    const starImg = document.createElement('img');
-    starImg.classList.add('star-icon');
-
-    scoreContainer.appendChild(p2);
-    scoreContainer.appendChild(starImg);
-    div.appendChild(img);
-    div.appendChild(p1);
-    div.appendChild(scoreContainer);
-    a.appendChild(div);
-    li.appendChild(a);
-
-    return li;
-  }
-
-  insertInfo({ poster_path, title, vote_average }: MovieItemsProps) {
-    this.template.classList.remove('li-skeleton');
-    const div = this.template.querySelector('.item-card') as HTMLElement;
-    div.classList.remove('skeleton');
-    const img = this.template.querySelector('.item-thumbnail') as HTMLImageElement;
-    img.setAttribute('src', IMAGE_URL_PREFIX + poster_path);
-    img.setAttribute('alt', title);
-    img.classList.remove('skeleton');
-    const p1 = this.template.querySelector('.item-title') as HTMLElement;
-    p1.classList.remove('skeleton');
-    p1.textContent = title;
-    const p2 = this.template.querySelector('.item-score') as HTMLElement;
-    p2.classList.remove('skeleton');
-    p2.innerHTML = `${vote_average}`;
-    const starImage = this.template.querySelector('.star-icon') as HTMLImageElement;
-    starImage.setAttribute('src', StarFilled);
-    starImage.setAttribute('alt', '별점');
-  }
-
-  getElement() {
+  get element() {
     return this.template;
+  }
+
+  createTemplate() {
+    const movieItem = document.createElement('li');
+    movieItem.innerHTML = template;
+    return movieItem;
+  }
+
+  setMovieInfo({ poster_path, title, vote_average }: MovieItemsProps) {
+    this.template
+      .querySelector('.item-thumbnail')
+      ?.setAttribute('src', IMAGE_URL_PREFIX + poster_path);
+    (this.template.querySelector('.item-title') as HTMLElement).textContent = title;
+    (this.template.querySelector('.item-score') as HTMLElement).textContent = `${vote_average}`;
+    this.template.querySelector('.star-icon')?.setAttribute('src', StarFilled);
   }
 }
 
