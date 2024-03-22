2c43e0528799342a7e8566e4ea12756afaea1bb1
brgndy <brgndy96@gmail.com>
2024-03-22 21:29:58 +0900

    refactor: 데이터 페칭 App 내부로 위치 수정

--- src/components/MovieContainer/MovieContainer.ts
+++ src/components/MovieContainer/MovieContainer.ts
@@ -1,9 +1,11 @@
-import { MovieData } from '../../types/movie';
 import { addShowMoreButtonEventListener } from '../ShowMoreButton/eventHandler';
 import { renderHandler } from './render';
+import loadingOrErrorStateUIManager from '../../services/LoadingOrErrorStateUIManager';
+import { API_ENDPOINT, API_OPTION } from '../../constants/api/api';
 
-function MovieContainer(movieData: MovieData) {
-  const { results } = movieData;
+async function MovieContainer() {
+  const data = await loadingOrErrorStateUIManager.fetchData(API_ENDPOINT.POPULAR(), { headers: API_OPTION.headers });
+  const { results } = data;
   renderHandler(results);
   addShowMoreButtonEventListener();
 }
