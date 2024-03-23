b36e75d882b31e7e8f83518b0ca54589261f1727
simhorim <hori5020@gmail.com>
2024-03-24 06:45:38 +0900

    refactor(index): 컴포넌트 조합 및 app에 추가

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import MovieHeader from './components/MovieHeader/MovieHeader';
 import ItemView from './components/ItemView/ItemView';
 
 const init = () => {
-  MovieHeader.create();
-  const item = new ItemView();
+  const itemView = new ItemView();
+  const header = MovieHeader.create(
+    () => itemView.showPopularMovies(),
+    () => itemView.showSearchMovies(),
+  );
+
+  const app = document.getElementById('app');
+  app.prepend(header);
+  app.appendChild(itemView.getItemView());
 };
 
 init();
