9e58abd81c9b6b070e755653f4c9f715cb485752
rbgksqkr <rbgks1937@naver.com>
2024-03-23 18:06:17 +0900

    test: cypress 환경변수 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'cypress';
+import config from './cypress.env.json';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
+    supportFile: false,
+    env: {
+      ACCESS_TOKEN: config.ACCESS_TOKEN
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
