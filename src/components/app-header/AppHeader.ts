494df6494724ae4b042d6a266ef4f91c6156bfa0
vi-wolhwa <cerezo00@naver.com>
2024-03-24 02:04:25 +0900

    refactor: AppHeader Wrapper 사용

--- src/components/app-header/AppHeader.ts
+++ src/components/app-header/AppHeader.ts
@@ -1,9 +1,10 @@
 import { appendChildren } from '../../utils/domUtils';
+import Wrapper from '../common/Wrapper';
 import Logo from './Logo';
 import SearchBox from './SearchBox';
 
 export default function AppHeader() {
-  const $header = document.createElement('header');
+  const $header = Wrapper({ type: 'header' });
   const $logo = Logo();
   const $searchBox = SearchBox();
 
