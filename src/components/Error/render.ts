44705795e5942f2b7731da0daf6084fe4c36f88e
river <jenn0.6n@gmail.com>
2024-03-23 22:37:53 +0900

    Refactor: Error 컴포넌트에서 메시지 컴포넌트 생성 부분 중복 제거

--- src/components/Error/render.ts
+++ src/components/Error/render.ts
@@ -1,31 +1,30 @@
 import createElement from '../../utils/createElement';
 
+const ERROR_MESSAGES = {
+  NOT_FOUND_MESSAGE: '죄송합니다. 현재 찾을 수 없는 페이지를 요청 하셨습니다.',
+  INSTRUCTION_MESSAGE: '존재하지 않는 주소를 입력하셨거나, 요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.',
+};
+
 const createErrorTitle = (errorStatus: string) => {
   const title = createElement('h1', { className: 'error-title', textContent: errorStatus });
 
   return title;
 };
 
-const createNotFoundMessage = () => {
-  return createElement('p', {
-    className: 'error-text',
-    textContent: '죄송합니다. 현재 찾을 수 없는 페이지를 요청 하셨습니다.',
-  });
-};
-
-const createInstructionMessage = () => {
-  return createElement('p', {
-    className: 'error-text',
-    textContent: '존재하지 않는 주소를 입력하셨거나, 요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.',
-  });
+const createErrorMessagesElements = (): HTMLElement[] => {
+  return Object.values(ERROR_MESSAGES).map((message) =>
+    createElement('p', {
+      className: 'error-text',
+      textContent: message,
+    }),
+  );
 };
 
 const createErrorDetail = () => {
   const box = createElement('div', { className: 'error-detail-container' });
-  const notFoundMessage = createNotFoundMessage();
-  const instructionMessage = createInstructionMessage();
-  [notFoundMessage, instructionMessage].forEach((element) => {
-    box.appendChild(element);
+
+  createErrorMessagesElements().forEach((htmlElement) => {
+    box.appendChild(htmlElement);
   });
 
   return box;
