6e6498b475998de47a9d2181f4dc862dd2fdf262
simhorim <hori5020@gmail.com>
2024-03-24 05:12:05 +0900

    refactor(Button): 이벤트 핸들러 등록

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -1,11 +1,13 @@
 import Button from '../../interfaces/Button';
 
 const Button = {
-  create(buttonInformation: Button): HTMLElement {
+  create(buttonInformation: Button, onButtonClick: () => void): HTMLElement {
     const button = document.createElement('button');
 
     button.textContent = `${buttonInformation.innerText}`;
     buttonInformation.classList.forEach((className) => button.classList.add(className));
+
+    button.addEventListener('click', onButtonClick);
 
     return button;
   },
