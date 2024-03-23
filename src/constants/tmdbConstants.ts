2bac5442a129a2ec772123b6eef1b8bb098dc1d4
vi-wolhwa <cerezo00@naver.com>
2024-03-24 00:24:14 +0900

    init

--- src/constants/tmdbConstants.ts
+++
@@ -1,3 +0,0 @@
-export const POPULAR_MOVIES_URL = 'https://api.themoviedb.org/3/movie/popular';
-
-export const SEARCH_MOVIES_URL = 'https://api.themoviedb.org/3/search/movie';
--- src/utils/domUtil.ts
+++
@@ -1,5 +0,0 @@
-export function appendChildren(targetElement: HTMLElement, children: HTMLElement[]) {
-  children.forEach((child) => {
-    targetElement.appendChild(child);
-  });
-}
