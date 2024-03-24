62b87c16a887885c288105540764e21fbc805f75
vi-wolhwa <cerezo00@naver.com>
2024-03-24 21:23:26 +0900

    docs: MovieItem 주석 위치 변경

--- src/components/movie-list-section/MovieItem.ts
+++ src/components/movie-list-section/MovieItem.ts
@@ -1,3 +1,6 @@
+/* 이 이상 분리가 어려우므로, 다음 규칙 비활성화 */
+/* eslint-disable max-lines-per-function */
+
 import IMG_SRC from '../../constants/imgSrc';
 import TmdbAPI from '../../services/TmdbAPI';
 import { appendChildren } from '../../utils/domUtils';
@@ -12,8 +15,6 @@ interface Props {
   voteAverage: number;
 }
 
-/* 이 이상 분리가 어려우므로, 다음 규칙 비활성화 */
-// eslint-disable-next-line max-lines-per-function
 export default function MovieItem({ posterPath, title, voteAverage }: Props) {
   const $listItem = ListItem();
   const $wrapper = Wrapper({ type: 'div', attrs: { class: 'item-card' } });
