39fbe5d5ffc35fde6057be69705637c1795a9fc6
hanyoung <phanyoung@naver.com>
2024-03-24 12:59:56 +0900

    style(generateNetworkNotWorkingScreen): html string formatting

--- src/components/templates/error/generateNetworkNotWorkingScreen.ts
+++ src/components/templates/error/generateNetworkNotWorkingScreen.ts
@@ -3,7 +3,8 @@ import { HTMLTemplate } from "../../abstract/BaseComponent";
 
 export const generateNetworkNotWorkingScreen = (): HTMLTemplate => {
   return `
-    <div class="unexpected-case-container"><img class="unexpected-case-image" src='${IMAGES.workers}' />
+    <div class="unexpected-case-container">
+      <img class="unexpected-case-image" src="${IMAGES.workers}" />
       <p class="unexpected-case-message">네트워크를 확인해 주세요.</p>
     </div>
   `;
