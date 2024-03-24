7b82ffb83eab27eb112bc0eeb133e84a64bd6435
vi-wolhwa <cerezo00@naver.com>
2024-03-24 20:17:18 +0900

    refactor: DomUtils 확장 및 jsdoc 수정

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -1,10 +1,10 @@
 /**
- * querySelector의 단축 함수입니다. 선택된 요소가 없을 경우 null을 반환하거나, 옵션에 따라 에러를 발생시킵니다.
- * @param selector - 선택할 요소의 CSS 선택자입니다.
- * @param scope - 검색 범위를 지정하는 요소입니다. 기본값은 document입니다.
- * @param canNull - 선택된 요소가 없을 경우 null을 반환할 수 있습니다. 기본값은 false입니다.
- * @returns 선택된 요소를 반환합니다. 요소가 없으면 null을 반환하거나, throwError가 true이면 에러를 발생시킵니다.
- * @throws Error - throwError가 true이고 선택된 요소가 없을 경우 에러를 발생시킵니다.
+ * querySelector의 단축 함수다. 선택된 요소가 없을 경우 null을 반환하거나, 옵션에 따라 에러를 발생시킨다.
+ * @param selector - 선택할 요소의 CSS 선택자
+ * @param scope - 검색 범위를 지정하는 요소 (Default: document)
+ * @param canNull - 선택된 요소가 없을 경우 null 반환 여부 (Default: false)
+ * @returns 선택된 요소를 반환한다. 요소가 없으면 null을 반환하거나, throwError가 true면 에러를 발생시킨다.
+ * @throws Error - throwError가 true이고 선택된 요소가 없을 경우 에러를 발생시킨다.
  */
 export const $ = (selector: string, scope: Document | Element = document, canNull = false) => {
   const element = scope.querySelector(selector);
@@ -17,12 +17,12 @@ export const $ = (selector: string, scope: Document | Element = document, canNul
 };
 
 /**
- * querySelectorAll의 단축 함수입니다. 선택된 요소가 없을 경우 빈 NodeList를 반환하거나, 옵션에 따라 에러를 발생시킵니다.
- * @param selector - 선택할 요소의 CSS 선택자입니다.
- * @param scope - 검색 범위를 지정하는 요소입니다. 기본값은 document입니다.
- * @param throwError - 선택된 요소가 없을 경우 에러를 발생시킬지 여부입니다.
- * @returns 선택된 요소의 NodeList를 반환합니다. 만약 선택된 요소가 없다면, 빈 NodeList를 반환하거나 throwError가 true이면 에러를 발생시킵니다.
- * @throws Error - throwError가 true이고 선택된 요소가 없을 경우 에러를 발생시킵니다.
+ * querySelectorAll의 단축 함수다. 선택된 요소가 없을 경우 빈 NodeList를 반환하거나, 옵션에 따라 에러를 발생시킨다.
+ * @param selector - 선택할 요소의 CSS 선택자
+ * @param scope - 검색 범위를 지정하는 요소 (Default: document)
+ * @param throwError - 선택된 요소가 없을 경우 에러를 발생시킬지 여부
+ * @returns 선택된 요소의 NodeList를 반환한다. 만약 선택된 요소가 없다면, 빈 NodeList를 반환하거나 throwError가 true면 에러를 발생시킨다.
+ * @throws Error - throwError가 true이고 선택된 요소가 없을 경우 에러를 발생시킨다.
  */
 export const $$ = (selector: string, scope: Document | Element = document, throwError = false) => {
   const elements = scope.querySelectorAll(selector);
@@ -35,14 +35,14 @@ export const $$ = (selector: string, scope: Document | Element = document, throw
 };
 
 /**
- * 주어진 속성 객체를 HTML 요소에 적용하는 함수입니다.
+ * 주어진 속성 객체를 HTML 요소에 적용하는 함수다.
  * @param element HTML 요소
  * @param attributes 요소에 적용할 속성들이 담긴 객체
  */
 export const setAttributes = (element: HTMLElement, attributes: { [key: string]: string }): void => {
   Object.keys(attributes).forEach((key) => {
     if (key === 'text') {
-      /* 요소의 속성을 변경하는 모듈이므로, 다음 규칙을 비활성화 한다. */
+      /* 요소의 속성을 변경하는 모듈이므로, 다음 규칙을 비활성화한다. */
       // eslint-disable-next-line no-param-reassign
       element.innerText = attributes[key];
     } else {
@@ -52,12 +52,17 @@ export const setAttributes = (element: HTMLElement, attributes: { [key: string]:
 };
 
 /**
- * 여러 자식 요소들을 부모 요소에 추가합니다.
- * @param parent - 자식 요소들을 추가할 부모 요소입니다.
- * @param children - 추가할 자식 요소들의 배열입니다.
+ * 여러 자식 요소들을 마지막 부모 요소에 추가하고, 부모 요소들을 순차적으로 서로의 자식으로 추가한다.
+ * 첫 번째 부모 요소가 루트이며, 각 부모 요소는 이전 요소의 자식 요소가 된다.
+ * 모든 자식 요소들은 마지막 부모 요소의 자식 요소로 추가된다.
+ * @param parents - 자식 요소들을 추가할 부모 요소들의 배열
+ * @param children - 추가할 자식 요소들의 배열
  */
-export const appendChildren = (parent: Element, children: Element[]): void => {
-  children.forEach((child) => {
-    parent.appendChild(child);
-  });
+export const appendChildren = (parents: Element[], children: Element[]): void => {
+  parents.slice(1).reduce((prevParent, currentParent) => {
+    prevParent.appendChild(currentParent);
+    return currentParent;
+  }, parents[0]);
+
+  children.forEach((child) => parents[parents.length - 1].appendChild(child));
 };
