eefa3aa0a156efad26aaf6f9704143671b34f453
river <jenn0.6n@gmail.com>
2024-03-24 21:23:11 +0900

    Test: 검색창, 검색 버튼이 클릭에 force 속성 추가

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -14,8 +14,8 @@ describe('비동기 호출에 대한 테스트 코드 작성', () => {
 
   it('키워드를 입력하면 그에 맞는 영화 리스트 목록을 보여준다.', () => {
     const searchKeyword = '어벤져스';
-    cy.get('.search-form input').type(searchKeyword);
-    cy.get('.search-button').click();
+    cy.get('.search-form input').type('어벤져스', { force: true });
+    cy.get('.search-button').click({ force: true });
     const apiKey = Cypress.env('api_key');
     const apiUrl = `${BASE_URL}/search/movie?api_key=${apiKey}&query=${searchKeyword}&language=ko-kr`;
 
@@ -23,7 +23,7 @@ describe('비동기 호출에 대한 테스트 코드 작성', () => {
   });
 
   it('빈값을 검색하면 alert창이 뜨고, 화면이 재렌더링되지 않는다.', () => {
-    cy.get('.search-button').click();
+    cy.get('.search-button').click({ force: true });
 
     cy.on('window:alert', (str) => {
       expect(str).to.equal('검색어는 1글자 이상이어야 합니다..');
@@ -34,8 +34,8 @@ describe('비동기 호출에 대한 테스트 코드 작성', () => {
 
   it('키워드를 검색하면 스켈레톤 UI가 렌더링됐다가 데이터가 다 받아지면 목록으로 보여준다.', () => {
     const searchKeyword = '어벤져스';
-    cy.get('.search-form input').type(searchKeyword);
-    cy.get('.search-button').click();
+    cy.get('.search-form input').type(searchKeyword, { force: true });
+    cy.get('.search-button').click({ force: true });
 
     cy.get('body').then(($body) => {
       if ($body.find('.skeleton').length) {
