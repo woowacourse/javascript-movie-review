83e738c38b30ddb131e96896f3008c4e24f964ee
vi-wolhwa <cerezo00@naver.com>
2024-03-24 17:47:17 +0900

    refactor: TmdbAPI 사용 방식 변경

--- src/services/TmdbAPI.ts
+++ src/services/TmdbAPI.ts
@@ -2,25 +2,27 @@ class TmdbAPI {
   private static domain = 'https://api.themoviedb.org/3';
   private static language = 'ko-KR';
 
-  private static createUrl(path: string, params: Record<string, string>): string {
+  private static createUrl(path: string, params: UrlParams): string {
     const queryString = new URLSearchParams({ ...params, language: this.language }).toString();
     return `${this.domain}/${path}?${queryString}`;
   }
 
   /* TMDB API 공식문서에 따라 그룹화 */
-  public static url = {
+  public static PATH = {
     movie: {
-      popular: (params: { page: string }) => this.createUrl('movie/popular', params)
+      popular: 'movie/popular'
     },
     search: {
-      movie: (params: { query: string; page: string }) => this.createUrl('search/movie', params)
+      movie: 'search/movie'
     }
   } as const;
 
-  public static fetch<T extends TmdbResponse<Movie>>(url: string): Promise<T> {
+  public static fetch({ path, params }: { path: string; params: UrlParams }): Promise<TmdbResponse> {
+    const url = this.createUrl(path, params);
+    // QUEST: 두 번째 then 필요성 검증
     return fetch(url)
       .then((response) => response.json())
-      .then((response) => response as T);
+      .then((response) => response);
   }
 }
 
