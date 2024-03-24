df7d4369ab0a92008ea2a4f0431dc21e57968edb
rbgksqkr <rbgks1937@naver.com>
2024-03-24 17:30:27 +0900

    refactor(MovieItem): 역할별로 메서드 분리

--- src/components/movieItem/MovieItem.ts
+++ src/components/movieItem/MovieItem.ts
@@ -13,32 +13,43 @@ class MovieItem {
   }
 
   paint(movie: Movie) {
-    const $thumbnail = dom.getElement<HTMLImageElement>(this.$target, '.item-thumbnail');
     const $title = dom.getElement<HTMLParagraphElement>(this.$target, '.item-title');
-    const $score = dom.getElement<HTMLParagraphElement>(this.$target, '.item-score');
-    const $starIcon = dom.getElement<HTMLImageElement>(this.$target, '.item-star-icon');
-    const $caption = dom.getElement<HTMLDivElement>(this.$target, '.item-caption');
-
-    const image = new Image();
-    image.src = movie.imageSrc;
-    image.onload = () => {
-      $thumbnail.src = image.src;
-      $thumbnail.classList.remove('skeleton');
-    };
-
-    $thumbnail.setAttribute('alt', movie.title);
+    this.#renderThumbnail(movie.imageSrc, movie.title);
+    this.#renderCaption(movie.score);
     $title.textContent = movie.title;
-    $score.textContent = movie.score.toString();
-    $starIcon.setAttribute('src', FILLED_STAR);
-    $starIcon.setAttribute('alt', '별점');
-
     $title.classList.remove('skeleton');
-    $caption.classList.remove('skeleton');
   }
 
   create(movie: Movie) {
     this.paint(movie);
     return this.$target;
+  }
+
+  #renderThumbnail(imageSrc: string, title: string) {
+    const $thumbnail = dom.getElement<HTMLImageElement>(this.$target, '.item-thumbnail');
+
+    const image = new Image();
+    image.src = imageSrc;
+    image.onload = () => {
+      $thumbnail.src = image.src;
+      $thumbnail.setAttribute('alt', title);
+      $thumbnail.classList.remove('skeleton');
+    };
+  }
+
+  #renderCaption(score: number) {
+    const $caption = dom.getElement<HTMLDivElement>(this.$target, '.item-caption');
+    const $starIcon = dom.getElement<HTMLImageElement>(this.$target, '.item-star-icon');
+    const $score = dom.getElement<HTMLParagraphElement>(this.$target, '.item-score');
+
+    const image = new Image();
+    image.src = FILLED_STAR;
+    image.onload = () => {
+      $starIcon.src = image.src;
+      $starIcon.setAttribute('alt', '별점');
+      $score.textContent = score.toString();
+      $caption.classList.remove('skeleton');
+    };
   }
 }
 
