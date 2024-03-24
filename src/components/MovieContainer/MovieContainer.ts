4d226910ac48d082dcd063dc24cfc47c479340b4
river <jenn0.6n@gmail.com>
2024-03-24 16:59:57 +0900

    Refactor: 인기영화 결과 fetch를 App이 아닌닌 MovieContainer에서 하도록 수정

--- src/components/MovieContainer/MovieContainer.ts
+++ src/components/MovieContainer/MovieContainer.ts
@@ -1,9 +1,11 @@
-import { MovieData } from '../../types/movie';
+import { API_ENDPOINT, API_OPTION } from '../../constants/api/api';
+import uiFeedBackManager from '../../services/UIFeedBackManager';
 import { addShowMoreButtonEventListener } from '../ShowMoreButton/eventHandler';
 import { renderHandler } from './render';
 
-function MovieContainer(movieData: MovieData) {
-  const { results } = movieData;
+async function MovieContainer() {
+  const data = await uiFeedBackManager.fetchData(API_ENDPOINT.POPULAR(), 'GET', null, API_OPTION.headers);
+  const { results } = data;
   renderHandler(results);
   addShowMoreButtonEventListener();
 }
