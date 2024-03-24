77eea3cb71da5c19e31647d57181834aeb60d80c
jaeml06 <jaeml0630@gmail.com>
2024-03-25 00:22:27 +0900

    refactor: buildData로 분리

--- src/apis/fetchData.js
+++ src/apis/fetchData.js
@@ -1,26 +1,12 @@
 import toast from "../components/toast/toast";
-import { MOVIE_SEARCH_URL, POPULAR_MOVIES_URL } from "../constants/constant";
+import { MOVIE_SEARCH_URL, NETWORK_ERROR_MESSAGE, POPULAR_MOVIES_URL } from "../constants/constant";
 import { mapDataToMovies } from "../domain/MovieService";
 
 const API_KEY = process.env.API_KEY;
 
 export async function fetchPopularMovieList(pageNumber) {
   try {
-    const popularMovieUrl =
-      POPULAR_MOVIES_URL +
-      '?' +
-      new URLSearchParams({
-        api_key: API_KEY,
-        language: 'ko-KR',
-        page: pageNumber.toString(),
-      });
-      
-    const response = await fetch(popularMovieUrl);
-    const popularMovies = await response.json();
-    
-    if(!response.ok){
-      throw new Error(popularMovies.status_message);
-    }
+    const popularMovies = await buildData({api_key: API_KEY, language: 'ko-KR', page:pageNumber}, POPULAR_MOVIES_URL);
 
     return [mapDataToMovies(popularMovies), popularMovies.total_pages];
   } catch (error) {
@@ -30,25 +16,23 @@ export async function fetchPopularMovieList(pageNumber) {
 
 export async function fetchSearchMovieList(inputValue, pageNumber) {
   try {
-    const searchMovieUrl =
-      MOVIE_SEARCH_URL +
-      '?' +
-      new URLSearchParams({
-        query: inputValue,
-        api_key: API_KEY,
-        language: 'ko-KR',
-        page: pageNumber.toString(),
-      });
-
-    const response = await fetch(searchMovieUrl);
-    const searchMovies = await response.json();
-
-    if(!response.ok){
-      throw new Error(searchMovies.status_message);
-    }
-    
-    return [mapDataToMovies(searchMovies), searchMovies.total_pages];
+    const searchMovies = await buildData({query: inputValue, api_key: API_KEY, language: 'ko-KR', page:pageNumber}, MOVIE_SEARCH_URL);
+    return [mapDataToMovies(searchMovies), searchMovies.total_pages, searchMovies.total_results];
   } catch (error) {
     toast(error.message);
   }
+}
+
+async function buildData(urlParams, baseURL) {
+  const targetMovieUrl =
+  baseURL +
+      '?' +
+      new URLSearchParams(urlParams);
+  const response = await fetch(targetMovieUrl);
+    if(!response.ok){
+      await response.json();
+      throw new Error(NETWORK_ERROR_MESSAGE);
+    }
+    const jsonData = await response.json();
+    return jsonData;
 }
