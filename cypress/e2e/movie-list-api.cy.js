c04d081d588c56a709082ce55054ccd654187920
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-25 00:24:32 +0900

    test(movie-list-api): 클라이언트 측 에러 처리 추가

--- cypress/e2e/movie-list-api.cy.js
+++ cypress/e2e/movie-list-api.cy.js
@@ -15,4 +15,34 @@ describe('영화 목록 API 테스트', () => {
 
     cy.get('@popularMovies').its('body.results').should('have.length', 20);
   });
+
+  it('잘못된 ACCESS_TOKEN을 보낼 시 401에러가 발생합니다.', () => {
+    const WRONG_TOKEN = 'ABCD123';
+
+    cy.request({
+      method: 'GET',
+      url: `${POPULAR_MOVIES_URL}?language=ko-KR&page=1`,
+      headers: {
+        Authorization: `Bearer ${WRONG_TOKEN}`,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+    });
+  });
+
+  it('잘못된 페이지를 보낼 시 400에러가 발생합니다.', () => {
+    const WRONG_PAGE = 0;
+
+    cy.request({
+      method: 'GET',
+      url: `${POPULAR_MOVIES_URL}?language=ko-KR&page=${WRONG_PAGE}`,
+      headers: {
+        Authorization: `Bearer ${Cypress.env('ACCESS_TOKEN')}`,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+    });
+  });
 });
