9bfa5186a49f8d332d5498df4bb2c11a605e4f18
badahertz52 <badahertz52@gmail.com>
2024-03-24 16:44:16 +0900

    docs:NoneMovieItme에 관련 이미지 추가

--- src/components/NoneMovieItem.ts
+++ src/components/NoneMovieItem.ts
@@ -1,3 +1,6 @@
+import NoMovie from '../images/no_movie.svg';
+import { createElementWithAttribute } from '../utils';
+
 class NoneMovieItem {
   #element: HTMLElement;
 
@@ -11,8 +14,12 @@ class NoneMovieItem {
 
   #makeNoneMovieItem = () => {
     const $noItem = document.createElement('li');
-    $noItem.textContent = '검색 결과가 없습니다.';
+    const $img = createElementWithAttribute('img', { src: NoMovie });
+    const $text = document.createElement('p');
+    $text.textContent = '검색 결과가 없습니다.';
 
+    $noItem.appendChild($img);
+    $noItem.appendChild($text);
     return $noItem;
   };
 }
