751d73459d1bb5ac947b662fcae3a152a5a37319
vi-wolhwa <cerezo00@naver.com>
2024-03-25 02:32:08 +0900

    refactor: TMDB API 상수

--- src/services/TmdbAPI.ts
+++ src/services/TmdbAPI.ts
@@ -3,7 +3,13 @@ if (!process.env.TMDB_API_KEY) {
 }
 
 class TmdbAPI {
-  public static imgUrl = 'https://image.tmdb.org/t/p/w500';
+  public static IMG_URL = 'https://image.tmdb.org/t/p/w500' as const;
+
+  /* TMDB API 공식문서에 따라 그룹화 */
+  public static PATH = {
+    popular: 'movie/popular',
+    search: 'search/movie'
+  } as const;
 
   private static apiKey = process.env.TMDB_API_KEY!;
   private static domain = 'https://api.themoviedb.org/3';
@@ -16,12 +22,6 @@ class TmdbAPI {
 
     return `${this.domain}/${path}?${params}`;
   }
-
-  /* TMDB API 공식문서에 따라 그룹화 */
-  public static PATH = {
-    popular: 'movie/popular',
-    search: 'search/movie'
-  } as const;
 
   public static fetch(params: TmdbUrlParams): Promise<TmdbResponse> {
     const url = this.createUrl(params);
