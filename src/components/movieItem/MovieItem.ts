2eaba993ec42b8935f09864aa6fe788ff46a17da
rbgksqkr <rbgks1937@naver.com>
2024-03-24 17:14:01 +0900

    refactor(MovieItem): 이미지가 모두 load되고 나서 DOM에 반영

--- src/components/movieItem/MovieItem.ts
+++ src/components/movieItem/MovieItem.ts
@@ -3,43 +3,42 @@ import './MovieItem.css';
 import FILLED_STAR from '../../assets/images/star_filled.png';
 import { Movie } from '../../types/movie';
 import { dom } from '../../utils/dom';
+import skeleton from '../Skeleton';
 
 class MovieItem {
-  $target: HTMLElement;
+  $target = document.createElement('li');
 
-  constructor(movie: Movie) {
-    this.$target = document.createElement('li');
-    this.$target.innerHTML = this.template();
-    this.paint(movie);
-  }
-
-  template() {
-    return `   
-        <a href="#">
-          <div class="item-card">
-          <img
-          class="item-thumbnail skeleton"
-          loading="lazy"
-        />
-        <p class="item-title"></p>
-        <div class="item-caption">
-          <p class="item-score"></p>
-          <img class="item-star-icon" src=${FILLED_STAR} alt="별점" />
-        </div>
-        </div>
-        </a>
-      `;
+  constructor() {
+    this.$target.appendChild(skeleton.create(1));
   }
 
   paint(movie: Movie) {
-    const $image = dom.getElement<HTMLImageElement>(this.$target, '.item-thumbnail');
+    const $thumbnail = dom.getElement<HTMLImageElement>(this.$target, '.item-thumbnail');
     const $title = dom.getElement<HTMLParagraphElement>(this.$target, '.item-title');
     const $score = dom.getElement<HTMLParagraphElement>(this.$target, '.item-score');
+    const $starIcon = dom.getElement<HTMLImageElement>(this.$target, '.item-star-icon');
+    const $caption = dom.getElement<HTMLDivElement>(this.$target, '.item-caption');
 
-    $image.setAttribute('src', movie.imageSrc);
-    $image.setAttribute('alt', movie.title);
+    const image = new Image();
+    image.src = movie.imageSrc;
+    image.onload = () => {
+      $thumbnail.src = image.src;
+      $thumbnail.classList.remove('skeleton');
+    };
+
+    $thumbnail.setAttribute('alt', movie.title);
     $title.textContent = movie.title;
     $score.textContent = movie.score.toString();
+    $starIcon.setAttribute('src', FILLED_STAR);
+    $starIcon.setAttribute('alt', '별점');
+
+    $title.classList.remove('skeleton');
+    $caption.classList.remove('skeleton');
+  }
+
+  create(movie: Movie) {
+    this.paint(movie);
+    return this.$target;
   }
 }
 
