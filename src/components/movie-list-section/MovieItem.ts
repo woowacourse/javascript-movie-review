b8dbd83619009c375d56837520200e2e2dbcf972
vi-wolhwa <cerezo00@naver.com>
2024-03-24 20:30:08 +0900

    feat: 영화 아이템의 별점 이미지 추가

--- src/components/movie-list-section/MovieItem.ts
+++ src/components/movie-list-section/MovieItem.ts
@@ -1,3 +1,4 @@
+import IMG_SRC from '../../constants/imgSrc';
 import TmdbAPI from '../../services/TmdbAPI';
 import { appendChildren } from '../../utils/domUtils';
 import Img from '../common/Img';
@@ -11,15 +12,18 @@ interface Props {
   voteAverage: number;
 }
 
+/* 이 이상 분리가 어려우므로, 다음 규칙 비활성화 */
+// eslint-disable-next-line max-lines-per-function
 export default function MovieItem({ posterPath, title, voteAverage }: Props) {
   const $listItem = ListItem();
   const $wrapper = Wrapper({ type: 'div', attrs: { class: 'item-card' } });
   const $poster = Img({ img: { src: `${TmdbAPI.imgUrl}${posterPath}`, alt: title } });
   const $title = TextBox({ type: 'p', attrs: { text: title, class: 'item-title' } });
   const $score = TextBox({ type: 'p', attrs: { text: voteAverage.toFixed(1), class: 'item-score' } });
+  const $starImg = Img({ img: { src: IMG_SRC.start_filled, alt: '별점' } });
 
-  appendChildren($wrapper, [$poster, $title, $score]);
-  appendChildren($listItem, [$wrapper]);
+  appendChildren($score, [$starImg]);
+  appendChildren([$listItem, $wrapper], [$poster, $title, $score]);
 
   return $listItem;
 }
