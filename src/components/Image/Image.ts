895498daf2cecff28d615aebbff0017d57b81c61
김진호 <rlawlsgh1227@gmail.com>
2024-03-24 15:10:50 +0900

    feat: 모달 닫는 버튼 생성

--- src/components/Image/Image.ts
+++ src/components/Image/Image.ts
@@ -1,16 +1,18 @@
 interface ImageProps {
   src: string;
   alt?: string;
+  classname?: string[];
   onImageClick?: () => void;
 }
 
 class Image {
   private template: HTMLImageElement;
 
-  constructor({ src, alt, onImageClick }: ImageProps) {
+  constructor({ src, alt, classname, onImageClick }: ImageProps) {
     const img = document.createElement('img');
     img.src = src;
     if (typeof alt !== 'undefined') img.alt = alt;
+    if (typeof classname !== 'undefined') img.classList.add(...classname);
     if (typeof onImageClick !== 'undefined') img.addEventListener('click', onImageClick);
     this.template = img;
   }
