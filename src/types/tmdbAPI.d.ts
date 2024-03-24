a609a95a96efe64f4465247ca177a6cc60d0f906
vi-wolhwa <cerezo00@naver.com>
2024-03-24 17:46:03 +0900

    refactor: TMDB API 타입 선언 - 확장 제거

--- src/types/tmdbAPI.d.ts
+++ src/types/tmdbAPI.d.ts
@@ -1,12 +1,3 @@
-/* 제너릭 타입 상속이 목적이므로, 다음 규칙을 비활성화 한다. */
-// eslint-disable-next-line no-use-before-define
-interface TmdbResponse<T extends Movie> {
-  page: number;
-  results: T[];
-  total_pages: number;
-  total_results: number;
-}
-
 interface Movie {
   adult: boolean;
   backdrop_path: string;
@@ -22,4 +13,16 @@ interface Movie {
   video: boolean;
   vote_average: number;
   vote_count: number;
+}
+
+interface TmdbResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface UrlParams {
+  page?: string;
+  query?: string;
 }
