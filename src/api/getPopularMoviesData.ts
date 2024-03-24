ca7a77bccc61a236ed853a008e2bfe0d37309ebd
ImxYJL <allensain14@gmail.com>
2024-03-24 23:47:14 +0900

    refactor: API_KEY를 string으로 단언

--- src/api/getPopularMoviesData.ts
+++ src/api/getPopularMoviesData.ts
@@ -1,39 +1,33 @@
 import { getMovieData } from "./getMovieData";
 import Movie from "../movie/Movie";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY as string;
 const BASE_URL = "https://api.themoviedb.org/3";
 const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
 
 export const getPopularMoviesData = async (currentPage: string) => {
-  if (typeof API_KEY === "string") {
-    const params = {
-      api_key: API_KEY,
-      language: "ko-KR",
-      page: currentPage,
-    };
+  const params = {
+    api_key: API_KEY,
+    language: "ko-KR",
+    page: currentPage,
+  };
 
-    const popularMovieUrl = `${POPULAR_MOVIES_URL}?${new URLSearchParams(
-      params
-    ).toString()}`;
+  const popularMovieUrl = `${POPULAR_MOVIES_URL}?${new URLSearchParams(
+    params
+  ).toString()}`;
 
-    try {
-      const popularMovies = await getMovieData(popularMovieUrl);
-      if (popularMovies && popularMovies.results) {
-        const movies = popularMovies.results.map(
-          (item: IMovieItemData) => new Movie(item)
-        );
-        return movies;
-      } else {
-        throw new Error("인기 영화를 불러오는데 실패했습니다.");
-      }
-    } catch (error) {
-      console.error("인기 영화 정보를 가져오는 중 오류 발생:", error);
+  try {
+    const popularMovies = await getMovieData(popularMovieUrl);
+    if (popularMovies && popularMovies.results) {
+      const movies = popularMovies.results.map(
+        (item: IMovieItemData) => new Movie(item)
+      );
+      return movies;
+    } else {
       throw new Error("인기 영화를 불러오는데 실패했습니다.");
     }
-  } else {
-    // NOTE: API_KEY가 문자열이 아닌 경우
-    console.error("API_KEY가 유효하지 않습니다.");
-    throw new Error("API_KEY가 유효하지 않습니다.");
+  } catch (error) {
+    console.error("인기 영화 정보를 가져오는 중 오류 발생:", error);
+    throw new Error("인기 영화를 불러오는데 실패했습니다.");
   }
 };
--- src/api/getSearchedMoviesData.ts
+++ src/api/getSearchedMoviesData.ts
@@ -1,7 +1,7 @@
 import { getMovieData } from "./getMovieData";
 import Movie from "../movie/Movie";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY as string;
 const BASE_URL = "https://api.themoviedb.org/3";
 const MOVIE_SEARCH_URL = `${BASE_URL}/search/movie`;
 
@@ -9,30 +9,28 @@ export const getSearchedMoviesData = async (
   currentPage: string,
   title: string
 ) => {
-  if (typeof API_KEY === "string") {
-    const params = {
-      api_key: API_KEY,
-      language: "ko-KR",
-      page: currentPage,
-      query: `${title}`,
-    };
+  const params = {
+    api_key: API_KEY,
+    language: "ko-KR",
+    page: currentPage,
+    query: `${title}`,
+  };
 
-    const searchMovieUrl = `${MOVIE_SEARCH_URL}?${new URLSearchParams(
-      params
-    ).toString()}`;
+  const searchMovieUrl = `${MOVIE_SEARCH_URL}?${new URLSearchParams(
+    params
+  ).toString()}`;
 
-    try {
-      const searchedMovies = await getMovieData(searchMovieUrl);
-      if (searchedMovies && searchedMovies.results) {
-        const movies = searchedMovies.results.map(
-          (item: IMovieItemData) => new Movie(item)
-        );
-        return movies;
-      } else {
-        throw new Error("서버에서 영화 데이터를 불러오는데 실패했습니다.");
-      }
-    } catch (error) {
-      throw new Error("네트워크 오류로 인해 영화 검색에 실패했습니다.");
+  try {
+    const searchedMovies = await getMovieData(searchMovieUrl);
+    if (searchedMovies && searchedMovies.results) {
+      const movies = searchedMovies.results.map(
+        (item: IMovieItemData) => new Movie(item)
+      );
+      return movies;
+    } else {
+      throw new Error("서버에서 영화 데이터를 불러오는데 실패했습니다.");
     }
+  } catch (error) {
+    throw new Error("네트워크 오류로 인해 영화 검색에 실패했습니다.");
   }
 };
