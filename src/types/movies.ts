32c46e0a4a492cbe7b05dcce591cc4fb7f0df277
hwinkr <dnddl8280@naver.com>
2024-03-25 11:13:34 +0900

    config(type): aapi 호출 시 전달되는 JSON 타입 추가

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -2,9 +2,26 @@ export interface FetchResponse<T> {
   results: T;
 }
 
+export interface MovieItemResponse {
+  id: number;
+  title: string;
+  original_title: string;
+  original_language: string;
+  backdrop_path: string;
+  poster_path: string;
+  genre_ids: number[];
+  overview: string;
+  popularity: number;
+  vote_average: number;
+  vote_count: number;
+  release_date: string;
+  video: boolean;
+  adult: boolean;
+}
+
 export interface MovieItem {
   id: number;
   title: string;
-  backdrop_path: string;
-  vote_average: number;
+  imagePath: string;
+  voteAverage: number;
 }
