0a09961a397f3f539e5c8597a2fd60980e2b5a82
hwinkr <dnddl8280@naver.com>
2024-03-25 11:17:27 +0900

    chore(index.ts):: App 컴포넌트 폴더 구조 변경 반영

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "../templates/reset.css";
 import "../templates/common.css";
 
 import { $ } from "./utils/dom";
-import App from "./App";
+import App from "./components/App";
 
 const root = $<HTMLDivElement>("#app");
 root && new App(root);
