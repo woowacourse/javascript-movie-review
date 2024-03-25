235855b1f60298d9c064a81a264d38a9f109700a
greetings1012 <ahnqwerty@gmail.com>
2024-03-26 01:34:31 +0900

    test: 더 보기 버튼을 눌렀을 때의 e2e 테스트 추가

--- cypress/e2e/FixtureTest.cy.ts
+++ cypress/e2e/FixtureTest.cy.ts
@@ -9,10 +9,18 @@ describe('Fixture를 이용한 테스트', () => {
     cy.intercept(
       {
         method: 'GET',
-        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular*/,
+        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular.+1$/,
       },
-      { fixture: 'movie-popular.json' },
-    ).as('getPopularMovies');
+      { fixture: 'movie-popular-page1.json' },
+    ).as('getPopularMoviesPage1');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular.+2$/,
+      },
+      { fixture: 'movie-popular-page2.json' },
+    ).as('getPopularMoviesPage2');
 
     cy.intercept(
       {
@@ -26,19 +34,28 @@ describe('Fixture를 이용한 테스트', () => {
   });
 
   it('초기 화면에는 지금 인기 있는 영화가 20개씩 목록에 나열되어야 한다', () => {
-    cy.wait('@getPopularMovies').then((interception) => {
-      // interception으로 fixture가 잘 불러와졌는지 확인하는 코드 샘플
+    cy.wait('@getPopularMoviesPage1').then((interception) => {
       const popularMovies = interception.response?.body.results;
       expect(popularMovies.length).to.equal(20);
 
-      // 제대로 렌더링이 되었는지 테스트하는 코드 샘플
       const popularMovieItems = cy.get('.item-list > li');
       expect(popularMovieItems.should('have.length', 20));
     });
   });
 
+  it('초기 화면에서 더 보기를 누르면 영화 20개가 추가로 나열되어야 한다.', () => {
+    cy.wait('@getPopularMoviesPage1').then((interception) => {
+      cy.scrollTo('bottom');
+      cy.get('.btn').click();
+    });
+    cy.wait('@getPopularMoviesPage2').then((interception) => {
+      const popularMovieItems = cy.get('.item-list > li');
+      expect(popularMovieItems.should('have.length', 40));
+    });
+  });
+
   it('검색 창에 "해리 포터"를 입력 시 해리 포터 관련 영화 목록이 나타나야 한다.', () => {
-    cy.wait('@getPopularMovies').then(() => {
+    cy.wait('@getPopularMoviesPage1').then(() => {
       cy.get('input').type('해리 포터');
       cy.get('.search-button').click();
     });
@@ -50,7 +67,7 @@ describe('Fixture를 이용한 테스트', () => {
   });
 
   it('마지막 페이지인 경우 더 보기 버튼이 사라져야 한다', () => {
-    cy.wait('@getPopularMovies').then(() => {
+    cy.wait('@getPopularMoviesPage1').then(() => {
       cy.get('input').type('해리 포터');
       cy.get('.search-button').click();
     });
@@ -61,14 +78,14 @@ describe('Fixture를 이용한 테스트', () => {
   });
 
   it('로고 이미지 클릭 시 지금 인기 있는 영화 목록 20개를 나열해야 한다.', () => {
-    cy.wait('@getPopularMovies').then(() => {
+    cy.wait('@getPopularMoviesPage1').then(() => {
       cy.get('input').type('해리 포터');
       cy.get('.search-button').click();
     });
     cy.wait('@getSearchMovies').then(() => {
       cy.get('h1').click();
     });
-    cy.wait('@getPopularMovies').then(() => {
+    cy.wait('@getPopularMoviesPage1').then(() => {
       const popularMovieItems = cy.get('.item-list > li');
       expect(popularMovieItems.should('have.length', 20));
     });
