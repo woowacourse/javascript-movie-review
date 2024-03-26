d282ed5c09b75987b81cd88cd594823c3a3f8bfa
Lee sang Yeop <mma7710@naver.com>
2024-03-26 18:17:31 +0900

    refactor(seeMoreButton): MoviePosterBoard 에서 관리하던 seeMoreButton 을 분리

--- src/components/MoviePosterBoard/MoviePosterBoard.ts
+++ src/components/MoviePosterBoard/MoviePosterBoard.ts
@@ -1,42 +1,47 @@
 import "./style.css";
 
 import createMoviePoster, { MovieInfo } from "../MoviePoster/createMoviePoster";
-import { fetchPopularMovie, fetchTargetMovie } from "../../apis/fetchMovie";
 
 import { $ } from "../../utils/selector";
-import createButton from "../Button/createButton";
 import createElement from "../../utils/createElement";
 import createNetworkFallback from "../NetworkErrorFallBack/createNetworkErrorFallback";
 import createSkeletonMoviePoster from "../MoviePoster/createSkeletonMoviePoster";
+import createSeeMoreButton from "../Button/createSeeMoreButton";
 
 export type MoviePosterType = "popular" | "search";
+const numberOfPosters = 20;
 
 class MoviePosterBoard {
   public element;
   private moviePosterUl: HTMLElement;
-  private seeMoreButton: HTMLElement;
-  private page: number;
+  private seeMoreButton;
 
   constructor(posterType: MoviePosterType, movieName?: string) {
-    this.page = 1;
-    const description = this.#createDescription(posterType, movieName);
-    this.element = this.#createSectionElement(description);
+    const description = this.createDescription(posterType, movieName);
+    this.element = this.createSectionElement(description);
     this.moviePosterUl = createElement({
       tagName: "ul",
       attrs: { class: "item-list" },
     });
-    this.seeMoreButton = this.#createSeeMoreButton(posterType, movieName);
-    this.element.append(this.moviePosterUl, this.seeMoreButton);
-    this.seeMoreButton.click();
+    this.seeMoreButton = new createSeeMoreButton();
+    this.addSeeMoreButtonEvent(posterType, movieName);
+    this.element.append(this.moviePosterUl, this.seeMoreButton.element);
+    this.seeMoreButton.element.click();
   }
 
-  addMoviePoster(movieInfos: MovieInfo[]) {
+  private addMoviePoster(movieInfos: MovieInfo[]) {
     const newMoviePosters = movieInfos.map(createMoviePoster);
 
     this.moviePosterUl.append(...newMoviePosters);
   }
 
-  #createSectionElement(description: string) {
+  private notFoundMovie(movieName?: string) {
+    this.element
+      .querySelector("h2")
+      ?.replaceChildren(movieName + " 그런 건 없어용!~ 🌞");
+  }
+
+  private createSectionElement(description: string) {
     const section = createElement({
       tagName: "section",
       attrs: { class: "item-view" },
@@ -47,32 +52,21 @@ class MoviePosterBoard {
     return section;
   }
 
-  #createSeeMoreButton(posterType: MoviePosterType, movieName?: string) {
-    const seeMoreButton = createButton("더보기");
-
-    seeMoreButton?.addEventListener(
-      "click",
-      this.#getSeeMoreButtonClickEvent(posterType, movieName).bind(this)
-    );
-
-    return seeMoreButton;
+  private addSkeletonPosters(count: number) {
+    this.moviePosterUl.append(...this.createSkeletons(count));
   }
 
-  #addSkeletonPosters() {
-    this.moviePosterUl.append(...this.#createSkeletons());
-  }
-
-  #deleteLast20Posters() {
-    Array.from({ length: 20 }).forEach(() =>
+  private deleteLastPosters(count: number) {
+    Array.from({ length: count }).forEach(() =>
       this.moviePosterUl.lastChild?.remove()
     );
   }
 
-  #createSkeletons() {
-    return Array.from({ length: 20 }).map(createSkeletonMoviePoster);
+  private createSkeletons(count: number) {
+    return Array.from({ length: count }).map(createSkeletonMoviePoster);
   }
 
-  #showNetworkFallbackComponent(
+  private showNetworkFallbackComponent(
     posterType: MoviePosterType,
     movieName?: string
   ) {
@@ -81,57 +75,40 @@ class MoviePosterBoard {
     $("body")?.append(networkErrorFallback);
   }
 
-  #getSeeMoreButtonClickEvent(posterType: MoviePosterType, movieName?: string) {
-    return async (event: Event) => {
-      this.#addSkeletonPosters();
-      const fetchFunc =
-        posterType === "popular" ? fetchPopularMovie : fetchTargetMovie;
-
-      let TMDBResponse;
-
-      try {
-        TMDBResponse = await fetchFunc(this.page, movieName as string);
-      } catch (error) {
-        this.#fetchErrorHandler(posterType, movieName);
-        return;
-      }
-      if (!TMDBResponse) {
-        return;
-      }
-      this.#deleteLast20Posters();
-
-      if (this.page === TMDBResponse?.total_pages)
-        this.seeMoreButton.classList.add("display-none");
-
-      this.page++;
-      const movieInfos = TMDBResponse.results.map((result) => {
-        return {
-          title: result.title,
-          imgSrc: `https://image.tmdb.org/t/p/w220_and_h330_face/${result.poster_path}`,
-          rating: result.vote_average,
-        };
-      });
-
-      this.addMoviePoster(movieInfos);
-      if (!this.moviePosterUl.children.length) {
-        this.element
-          .querySelector("h2")
-          ?.replaceChildren(movieName + " 그런 건 없어용!~ 🌞");
-      }
-    };
+  private fetchErrorHandler(posterType: MoviePosterType, movieName?: string) {
+    return this.showNetworkFallbackComponent(posterType, movieName);
   }
 
-  #fetchErrorHandler(posterType: MoviePosterType, movieName?: string) {
-    if (this.page === 1)
-      return this.#showNetworkFallbackComponent(posterType, movieName);
-
-    alert("잠시 인터넷이 끊긴거 같아요 나중에 다시 시도해주세요");
-    this.#deleteLast20Posters();
-  }
-
-  #createDescription(posterType: MoviePosterType, name?: string) {
+  private createDescription(posterType: MoviePosterType, name?: string) {
     if (posterType === "search" && name) return `"${name}" 검색 결과`;
     return "지금 인기있는 영화";
+  }
+
+  private addSeeMoreButtonEvent(
+    posterType: MoviePosterType,
+    movieName?: string
+  ) {
+    this.seeMoreButton.element.addEventListener("click", () =>
+      this.handleSeeMoreButton(posterType, movieName)
+    );
+  }
+
+  private async handleSeeMoreButton(
+    posterType: MoviePosterType,
+    movieName?: string
+  ) {
+    try {
+      this.addSkeletonPosters(numberOfPosters);
+      const fetchedMovieInfo = await this.seeMoreButton.getMoreMoviePoster(
+        posterType,
+        movieName
+      );
+      this.deleteLastPosters(numberOfPosters);
+      if (fetchedMovieInfo) this.addMoviePoster(fetchedMovieInfo);
+      else this.notFoundMovie(movieName);
+    } catch (error) {
+      this.fetchErrorHandler(posterType, movieName);
+    }
   }
 }
 
