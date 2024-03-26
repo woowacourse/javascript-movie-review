2b8b610811cd9b4f60de98fb1131304ce57d0cc2
rbgksqkr <rbgks1937@naver.com>
2024-03-26 20:04:58 +0900

    feat(movieDetailModal): 별점 주는 로직 구현

--- src/components/movieDetailModal/movieDetailModal.ts
+++ src/components/movieDetailModal/movieDetailModal.ts
@@ -1,18 +1,20 @@
 import './movieDetailModal.css';
 
 import movieDetail from '../../domain/movieDetail';
-import { MovieDetailResponse } from '../../types/movie';
+import { MovieDetailResponse, StarScore } from '../../types/movie';
 import { dom } from '../../utils/dom';
 import FILLED_STAR from '../../assets/images/star_filled.png';
 import EMPTY_STAR from '../../assets/images/star_empty.png';
+import { SCORE_TEXT } from '../../constants/movie';
 
 class MovieDetailModal {
   $target = document.createElement('div');
+  stars?: Element[];
 
   constructor() {
     this.$target.classList.add('modal');
     this.$target.innerHTML = this.template();
-
+    this.initStarRate();
     this.setEvent();
   }
 
@@ -40,13 +42,15 @@ class MovieDetailModal {
                     </div>
                     <div id='user-score' class='user-score-container'>
                         <p>내 별점</p>
-                        <img class='star-icon' src=${EMPTY_STAR} />
-                        <img class='star-icon' src=${EMPTY_STAR} />
-                        <img class='star-icon' src=${EMPTY_STAR} />
-                        <img class='star-icon' src=${EMPTY_STAR} />
-                        <img class='star-icon' src=${EMPTY_STAR} />
-                        <p id='score-number'>6</p>
-                        <p id='score-text'>보통이예요</p>
+                        <div id='star-container'>
+                          <img data-id='1' class='user-star-icon' src=${EMPTY_STAR} />
+                          <img data-id='2' class='user-star-icon' src=${EMPTY_STAR} />
+                          <img data-id='3' class='user-star-icon' src=${EMPTY_STAR} />
+                          <img data-id='4' class='user-star-icon' src=${EMPTY_STAR} />
+                          <img data-id='5' class='user-star-icon' src=${EMPTY_STAR} />
+                        </div>
+                        <p id='score-number'>0</p>
+                        <p id='score-text'>${SCORE_TEXT[0]}</p>
                     </div>
                 </div>
             </div>
@@ -55,7 +59,6 @@ class MovieDetailModal {
   }
 
   open(movieResponse: MovieDetailResponse) {
-    this.$target.classList.add('open');
     const { genres, imageSrc, description, title, score } = movieDetail.create(movieResponse);
 
     const $thumbnail = dom.getElement<HTMLImageElement>(this.$target, '#thumbnail');
@@ -69,15 +72,61 @@ class MovieDetailModal {
     $description.textContent = description;
     $title.textContent = title;
     $score.textContent = score.toString();
+    document.body.style.overflow = 'hidden';
+    this.$target.classList.add('open');
   }
 
   close() {
     this.$target.classList.remove('open');
+    document.body.style.overflow = 'unset';
   }
 
   setEvent() {
     const $backdrop = dom.getElement(this.$target, '.detail-modal-backdrop');
-    $backdrop.addEventListener('click', this.close.bind(this));
+    $backdrop.addEventListener('click', () => {
+      this.initStarRate();
+      this.close();
+    });
+
+    const $starContainer = dom.getElement(this.$target, '#star-container');
+    $starContainer.addEventListener('click', e => {
+      if (e.target === e.currentTarget) return;
+      const target = e.target as HTMLImageElement;
+      const id = Number(target.dataset.id);
+      if (typeof id === 'undefined') return;
+      const $scoreNumber = dom.getElement(this.$target, '#score-number');
+      const $scoreText = dom.getElement(this.$target, '#score-text');
+      const score = (id * 2) as StarScore;
+      $scoreNumber.textContent = score.toString();
+      $scoreText.textContent = SCORE_TEXT[score];
+
+      this.initStarRate(id);
+      this.fillRate(Number(target.dataset.id));
+    });
+  }
+
+  fillRate(count: number) {
+    const $starContainer = dom.getElement(this.$target, '#star-container');
+    const stars = [...$starContainer.children];
+    for (let i = 0; i <= count - 1; i++) {
+      stars[i].classList.add('filled');
+      stars[i].setAttribute('src', FILLED_STAR);
+    }
+  }
+
+  initStarRate(id = 0) {
+    const $starContainer = dom.getElement(this.$target, '#star-container');
+    const stars = [...$starContainer.children];
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].classList.remove('filled');
+      stars[i].setAttribute('src', EMPTY_STAR);
+    }
+
+    const $scoreNumber = dom.getElement(this.$target, '#score-number');
+    const $scoreText = dom.getElement(this.$target, '#score-text');
+    const score = (id * 2) as StarScore;
+    $scoreNumber.textContent = score.toString();
+    $scoreText.textContent = SCORE_TEXT[score];
   }
 }
 
