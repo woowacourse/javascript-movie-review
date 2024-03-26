3bedd7101b486c5bcf610b5b7deec59c531b20c3
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-26 18:37:27 +0900

    refactor(Movies, Fetcher): 에러 핸들링 메서드 Movies로 이동

--- src/api/Fetcher.ts
+++ src/api/Fetcher.ts
@@ -1,16 +1,8 @@
+import Movies from '../domain/Movies';
 import CustomError from '../utils/CustomError';
 
 export interface DataFetcher {
   getData<T>(url: string, headers: HeadersInit): Promise<T>;
-}
-
-export interface ErrorResponse {
-  status: number;
-  response: Response;
-}
-
-export interface ErrorData {
-  status_message: string;
 }
 
 class Fetcher {
@@ -20,20 +12,9 @@ class Fetcher {
       headers: new Headers(headers),
     });
 
-    if (!response.ok) await this.#handleErrorResponse({ status: response.status, response });
+    if (!response.ok) await Movies.handleErrorResponse(response);
 
     return response.json();
-  }
-
-  async #handleErrorResponse({ status, response }: ErrorResponse) {
-    const { status_message }: ErrorData = await response.json();
-
-    if (status === 400) throw new CustomError(status_message, status);
-    if (status === 401) throw new CustomError(status_message, status);
-    if (status === 403) throw new CustomError('Forbidden', status);
-    if (status === 404) throw new CustomError(status_message, status);
-    if (Math.floor(status / 100) === 5)
-      throw new CustomError('서버에 문제가 발생했습니다. 다시 시도해보시겠어요?', status);
   }
 }
 
--- src/domain/Movies.ts
+++ src/domain/Movies.ts
@@ -1,11 +1,16 @@
 import { MOVIE_SEARCH_URL, POPULAR_MOVIES_URL } from '../api';
 import Fetcher, { DataFetcher } from '../api/Fetcher';
+import CustomError from '../utils/CustomError';
 
 export interface ResponseMovieData {
   page: number;
   results: MovieInfo[];
   total_pages: number;
   total_results: number;
+}
+
+export interface ResponseErrorData {
+  status_message: string;
 }
 
 export interface MovieInfo {
@@ -27,44 +32,39 @@ class Movies {
   }
 
   async getPopularMovies(page = 1): Promise<MovieInfo[]> {
-    const urlParams = {
-      language: 'ko-KR',
-      page: String(page),
-    };
+    const urlParams = { language: 'ko-KR', page: String(page) };
     const url = `${POPULAR_MOVIES_URL}?${new URLSearchParams(urlParams)}`;
+    const { results }: ResponseMovieData = await this.#fetcher.getData(url, this.#headers);
 
-    try {
-      const { results }: ResponseMovieData = await this.#fetcher.getData(url, this.#headers);
-
-      return results.map((result) => ({
-        poster_path: result.poster_path,
-        title: result.title,
-        vote_average: result.vote_average,
-      }));
-    } catch (error) {
-      return [];
-    }
+    return results.map((result) => ({
+      poster_path: result.poster_path,
+      title: result.title,
+      vote_average: result.vote_average,
+    }));
   }
 
   async getSearchMovies(query: string, page = 1): Promise<MovieInfo[]> {
-    const urlParams = {
-      query,
-      language: 'ko-KR',
-      page: String(page),
-    };
+    const urlParams = { query, language: 'ko-KR', page: String(page) };
     const url = `${MOVIE_SEARCH_URL}?${new URLSearchParams(urlParams)}`;
 
-    try {
-      const { results }: ResponseMovieData = await this.#fetcher.getData(url, this.#headers);
+    const { results }: ResponseMovieData = await this.#fetcher.getData(url, this.#headers);
 
-      return results.map(({ poster_path, title, vote_average }) => ({
-        poster_path,
-        title,
-        vote_average,
-      }));
-    } catch (error) {
-      return [];
-    }
+    return results.map(({ poster_path, title, vote_average }) => ({
+      poster_path,
+      title,
+      vote_average,
+    }));
+  }
+
+  static async handleErrorResponse(response: Response) {
+    const { status_message }: ResponseErrorData = await response.json();
+
+    if (response.status === 400) throw new CustomError(status_message, response.status);
+    if (response.status === 401) throw new CustomError(status_message, response.status);
+    if (response.status === 403) throw new CustomError('Forbidden', response.status);
+    if (response.status === 404) throw new CustomError(status_message, response.status);
+    if (Math.floor(response.status / 100) === 5)
+      throw new CustomError('서버에 문제가 발생했습니다. 다시 시도해보시겠어요?', response.status);
   }
 }
 
