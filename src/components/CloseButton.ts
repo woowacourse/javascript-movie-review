577f330d6ceeb47abbf23ba9e3c381d133f60078
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-26 21:17:12 +0900

    feat: 모달 닫기 버튼 컴포넌트

---
+++ src/components/CloseButton.ts
@@ -0,0 +1,28 @@
+/* eslint-disable max-lines-per-function */
+import closeImg from "../../templates/exit.png";
+import { createElementWithAttribute } from "../utils";
+
+function closeModal() {
+  console.log("closeModal");
+  const $modal = document.querySelector(".detail-modal");
+  if (!$modal) return;
+  $modal.remove();
+}
+
+const CloseButton = () => {
+  const $button = createElementWithAttribute("button", {
+    class: "close-button",
+  });
+  const $img = createElementWithAttribute("img", {
+    src: closeImg,
+  });
+  $button.appendChild($img);
+
+  $button.addEventListener("click", () => {
+    closeModal();
+  });
+
+  return $button;
+};
+
+export default CloseButton;
