fb42f6ee9becdfb075a5cd79ddf923e4d69a8627
pakxe <pigkill40@naver.com>
2024-03-26 15:41:59 +0900

    feat: key에 해당하는 환경 변수가 없는 경우에 대해 조건문 내부의 비교하는 값을 수정

--- src/util/getEnvVariable.ts
+++ src/util/getEnvVariable.ts
@@ -6,7 +6,7 @@ const KEY_LIST: Record<string, string | undefined> = {
 function getEnvVariable(key: string): string {
   const value = KEY_LIST[key] ?? '';
 
-  if (value === undefined) throw new Error(`${key}에 대한 환경 변수를 찾을 수 없습니다.`);
+  if (value === '') throw new Error(`${key}에 대한 환경 변수를 찾을 수 없습니다.`);
 
   return value;
 }
