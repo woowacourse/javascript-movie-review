6547297eb8d8eca16aa6779e99650d9b09f9ea18
brgndy <brgndy96@gmail.com>
2024-03-27 06:36:35 +0900

    refactor: 별점 속성 담은 타입 추가

--- src/types/movie.d.ts
+++ src/types/movie.d.ts
@@ -20,4 +20,38 @@ export type MovieItemProps = {
   vote_count: number;
 };
 
+export type TotalMovieItemProps = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  star_rating: StarRate;
+};
+
+export type StarRate = 2 | 4 | 6 | 8 | 10;
+
 export type ShowMoreButtonOption = 'popular' | 'search';
+
+export type MovieGenre = {
+  id: number;
+  name: string;
+};
+
+export type MovieDetailProps = {
+  genres: MovieGenre[];
+  title: string;
+  overview: string;
+  vote_average: number;
+  poster_path: string;
+  star_rating: StarRate;
+};
