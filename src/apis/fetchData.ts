f06eeafea063e43d727320f870554f9b3945de10
jaeml06 <jaeml0630@gmail.com>
2024-03-26 16:22:05 +0900

    refactor: 제네릭 타입 사용

--- src/apis/fetchData.ts
+++ src/apis/fetchData.ts
@@ -1,17 +1,17 @@
 import toast from '../components/toast/toast';
 import { MOVIE_SEARCH_URL, NETWORK_ERROR_MESSAGE, POPULAR_MOVIES_URL } from '../constants/constant';
 import { mapDataToMovies } from '../domain/MovieService';
-import { MovieAPIResponse, UrlParams } from '../interface/Movie';
+import { MovieAPIResponse, MovieList, UrlParams } from '../interface/Movie';
 
 const API_KEY: string | undefined = process.env.API_KEY;
 
 export async function fetchPopularMovieList(pageNumber: number): Promise<MovieAPIResponse> {
   try {
-    const popularMovies = await buildData(
+    const popularMovies = await buildData<MovieList>(
       { api_key: API_KEY, language: 'ko-KR', page: pageNumber.toString() } as UrlParams,
       POPULAR_MOVIES_URL,
     );
-
+    console.log(popularMovies);
     return [mapDataToMovies(popularMovies), popularMovies.total_pages, popularMovies.total_results];
   } catch (error) {
     toast(NETWORK_ERROR_MESSAGE);
@@ -21,10 +21,11 @@ export async function fetchPopularMovieList(pageNumber: number): Promise<MovieAP
 
 export async function fetchSearchMovieList(inputValue: string, pageNumber: number): Promise<MovieAPIResponse> {
   try {
-    const searchMovies = await buildData(
+    const searchMovies = await buildData<MovieList>(
       { query: inputValue, api_key: API_KEY, language: 'ko-KR', page: pageNumber.toString() } as UrlParams,
       MOVIE_SEARCH_URL,
     );
+    console.log(searchMovies);
     return [mapDataToMovies(searchMovies), searchMovies.total_pages, searchMovies.total_results];
   } catch (error) {
     toast(NETWORK_ERROR_MESSAGE);
@@ -32,14 +33,14 @@ export async function fetchSearchMovieList(inputValue: string, pageNumber: numbe
   }
 }
 
-async function buildData(urlParams: UrlParams, baseURL: string) {
+async function buildData<T>(urlParams: UrlParams, baseURL: string): Promise<T> {
   const targetMovieUrl = baseURL + '?' + new URLSearchParams(urlParams);
   const response = await fetch(targetMovieUrl);
-  console.log(response);
   if (!response.ok) {
     await response.json();
     throw new Error(NETWORK_ERROR_MESSAGE);
   }
   const jsonData = await response.json();
+  console.log(jsonData);
   return jsonData;
 }
--- src/interface/Movie.ts
+++ src/interface/Movie.ts
@@ -13,3 +13,28 @@ export interface UrlParams extends URLSearchParams {
   page: string;
   query?: string;
 }
+
+export interface MovieList {
+  page: number;
+  results: Array<MovieList>;
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieData {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: Array<number>;
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
