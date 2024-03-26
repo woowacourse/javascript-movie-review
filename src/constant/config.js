ceb765e3c78bb9364f8c1404cb8b5b658c0c4c03
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-26 17:33:52 +0900

    refactor: API 엔드포인트를 요청 메서드 별로 관리 가능하도록 구조 변경

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,5 +1,9 @@
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w';
 
-export const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
-export const MOVIE_SEARCH_URL = `${BASE_URL}/search/movie`;
+export const ENDPOINT = {
+  GET: {
+    POPULAR_MOVIES: '/movie/popular',
+    MOVIE_SEARCH: '/search/movie',
+  },
+};
