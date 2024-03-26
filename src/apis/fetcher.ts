da8a2a5c7fb9c268ed5b4377b050623de6bc7a76
rbgksqkr <rbgks1937@naver.com>
2024-03-26 17:12:33 +0900

    refactor(fetcher): 에러 헨들링을 fetcher 내에서 처리

--- src/apis/fetcher.ts
+++ src/apis/fetcher.ts
@@ -1,27 +1,51 @@
 import { ERROR_MESSAGE } from '../constants/message';
-import { BadRequestError, InvalidRequestError, NotFoundError, ServerError, UnAuthorizedError } from '../errors/error';
+import {
+  BadRequestError,
+  InvalidRequestError,
+  PageNotFoundError,
+  ServerError,
+  UnAuthorizedError,
+} from '../errors/error';
+import { dom } from '../utils/dom';
 
 const fetcher = {
   async get<T>(url: string): Promise<T> {
-    const response = await fetch(url, {
+    const result = fetch(url, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
         accept: 'application/json',
       },
-    });
+    })
+      .then(res => {
+        this.handleError(res.status);
+        return res.json();
+      })
+      .catch(error => {
+        this.handleErrorToast(error);
+        return { results: [], total_pages: 0, total_results: 0 };
+      });
 
-    this.handleError(response.status);
-    return await response.json();
+    return result;
   },
 
   handleError(status: number) {
     if (status === 400) throw new BadRequestError(ERROR_MESSAGE.BAD_REQUEST);
     else if (status === 401) throw new UnAuthorizedError(ERROR_MESSAGE.UNAUTHORIZED);
-    else if (status === 404) throw new NotFoundError(ERROR_MESSAGE.NOT_FOUND);
+    else if (status === 404) throw new PageNotFoundError(ERROR_MESSAGE.NOT_FOUND);
     else if (status === 500 || status === 501 || status === 502 || status === 503)
       throw new ServerError(ERROR_MESSAGE.SERVER_ERROR);
     else if (status !== 200) throw new InvalidRequestError(ERROR_MESSAGE.INVALID_REQUEST);
+  },
+
+  handleErrorToast(errorMessage: string) {
+    const $toastButton = dom.getElement<HTMLButtonElement>(document.body, '#toast-button');
+    const clickEvent = new CustomEvent('onToast', {
+      detail: errorMessage,
+      bubbles: true,
+    });
+
+    $toastButton.dispatchEvent(clickEvent);
   },
 };
 
