4b0fbec79e50df3e796ac2aace4b303e447232de
pakxe <pigkill40@naver.com>
2024-03-26 15:42:15 +0900

    refactor: js로 작성되어있는 css를 분리

--- src/component/RetryButton.js
+++ src/component/RetryButton.js
@@ -14,15 +14,6 @@ function renderRetryButton() {
   button.className = 'retry-button';
 
   button.style.background = `no-repeat url(${retryIcon})`;
-  button.style.backgroundSize = 'cover';
-  button.style.backgroundColor = 'white';
-  button.style.backgroundPosition = '4px 3px';
-
-  button.style.borderRadius = '50%';
-  button.style.width = '50px';
-  button.style.height = '50px';
-  button.style.cursor = 'pointer';
-  button.style.border = 'none';
 
   return button;
 }
