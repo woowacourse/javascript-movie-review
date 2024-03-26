39ba651d9ee7e6504905dc467b3dcfabe17c1c89
pakxe <pigkill40@naver.com>
2024-03-26 15:38:43 +0900

    feat: 데이터를 fetch해오는 함수에 generic 적용

--- src/util/fetchDataFromUrl.ts
+++ src/util/fetchDataFromUrl.ts
@@ -2,7 +2,10 @@ import { ResponseMovieList } from '../interface/api';
 import { Params, convertObjectToURLSearchParams } from './convertObjectToURLSearchParams';
 
 // fetch 포장 함수
-export default async function fetchDataFromUrl(url: string, query: Record<string, Params>) {
+export default async function fetchDataFromUrl<T = ResponseMovieList>(
+  url: string,
+  query: Record<string, Params>,
+): Promise<T> {
   const paramsObj = convertObjectToURLSearchParams(query);
   const queryUrl = url + '?' + paramsObj;
   const response = await fetch(queryUrl);
@@ -13,6 +16,5 @@ export default async function fetchDataFromUrl(url: string, query: Record<string
     throw new Error(`${errorMessage} 다시 요청해주세요.`);
   }
 
-  const { total_pages, results }: ResponseMovieList = await response.json();
-  return { total_pages, results };
+  return await response.json();
 }
