ebbfb7d6be3fc2280bf4864939bfd5cd90e7ac13
rbgksqkr <rbgks1937@naver.com>
2024-03-26 16:46:49 +0900

    feat(movie API): 영화 상세보기 API 호출 함수 구현

--- src/apis/movie.ts
+++ src/apis/movie.ts
@@ -1,5 +1,5 @@
-import { POPULAR_MOVIES_URL, SEARCH_MOVIES_URL } from '../constants/url';
-import { MovieResponse, MovieSearchResult } from '../types/movie';
+import { MOVIE_API_URL, POPULAR_MOVIES_URL, SEARCH_MOVIES_URL } from '../constants/url';
+import { MovieDetailResponse, MovieResponse, MovieSearchResult } from '../types/movie';
 import fetcher from './fetcher';
 
 export const getPopularMovies = async (page: number): Promise<MovieSearchResult> => {
@@ -12,4 +12,9 @@ export const searchMoviesByTitle = async (title: string, page: number): Promise<
   const params = `?query=${title}&include_adult=false&language=en-US&page=${page}`;
   const { results, total_pages, total_results } = await fetcher.get<MovieResponse>(SEARCH_MOVIES_URL + params);
   return { movies: results, totalPages: total_pages, movieCount: total_results };
+};
+
+export const getDetailMovie = async (movieId: number) => {
+  const params = `/${movieId}?language=en-US`;
+  return await fetcher.get<MovieDetailResponse>(MOVIE_API_URL + params);
 };
