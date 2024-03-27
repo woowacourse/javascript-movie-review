6aba3ac4eea5e8b2f5f3b15c193ac4d27e5f5e19
simhorim <hori5020@gmail.com>
2024-03-27 18:34:19 +0900

    feat(index): 무한스크롤 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './resources.js';
 import MovieHeader from './components/MovieHeader/MovieHeader';
 import ItemView from './components/ItemView/ItemView';
+import Throttle from './Utils/Throttle.js';
 
 const init = () => {
   const itemView = new ItemView();
@@ -12,6 +13,15 @@ const init = () => {
   const app = document.getElementById('app');
   app.prepend(header);
   app.appendChild(itemView.getItemView());
+
+  window.addEventListener(
+    'scroll',
+    Throttle(() => {
+      if (window.innerHeight + window.scrollY + 10 >= document.body.offsetHeight) {
+        itemView.mountItems();
+      }
+    }, 1000),
+  );
 };
 
 init();
