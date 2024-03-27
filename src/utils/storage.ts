883d5863d93c40a1cb083aeeb0ac6c4e621b044a
rbgksqkr <rbgks1937@naver.com>
2024-03-27 11:23:35 +0900

    feat(storage): 로컬 스토리지 메서드에 타입 지정 가능한 유틸함수 구현

---
+++ src/utils/storage.ts
@@ -0,0 +1,17 @@
+const storage = {
+  get<T>(key: string): T {
+    const item = localStorage.getItem(key);
+    if (item === null) {
+      return '' as T;
+    }
+
+    return JSON.parse(item);
+  },
+
+  set<T>(key: string, value: T) {
+    const item = JSON.stringify(value);
+    localStorage.setItem(key, item);
+  },
+};
+
+export default storage;
