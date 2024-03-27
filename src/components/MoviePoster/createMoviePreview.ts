65cfe720e36ccd6de316d402084f6966194d2f3a
ss0526100 <ss0526100@gmail.com>
2024-03-27 21:48:41 +0900

    feat:영화 프리뷰를 보여주는 컴포넌트 구현(함수형)

---
+++ src/components/MoviePoster/createMoviePreview.ts
@@ -0,0 +1,43 @@
+import "./style.css";
+
+import MovieThumbnail from "./createMovieThumbnail";
+import createElement from "../../utils/createElement";
+import starFills from "./star_filled.png";
+
+export interface MovieInfo {
+  title: string;
+  imgSrc: string;
+  rating: number;
+}
+
+class MoviePreview {
+  element = createElement("li");
+  #thumbnail;
+
+  constructor({ title, imgSrc, rating }: MovieInfo) {
+    this.#thumbnail = new MovieThumbnail(imgSrc, title);
+
+    const anchor = createElement("a");
+    const div = createElement("div", { attrs: { class: "item-card" } });
+    const titleParagraph = createElement("p", {
+      attrs: { class: "item-title" },
+      content: title,
+    });
+    const scoreParagraph = createElement("p", {
+      attrs: { class: " item-score" },
+    });
+    const starImg = createElement("img", {
+      attrs: {
+        src: starFills,
+        alt: "별점",
+      },
+    });
+
+    scoreParagraph.append(starImg, rating.toFixed(1));
+    div.append(this.#thumbnail.element, titleParagraph, scoreParagraph);
+    anchor.append(div);
+    this.element.append(anchor);
+  }
+}
+
+export default MoviePreview;
