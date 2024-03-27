7efdd664228d9f97a590720b5572399d7842717d
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-27 17:02:05 +0900

    refactor: 이미지 확장자에 대한 모듈 선언 적용

--- src/util/convertToPosterPath.js
+++
@@ -1,6 +0,0 @@
-import { IMAGE_BASE_URL } from '../constant/config.js';
-import posterNotFoundImage from '../asset/poster_not_found.jpg';
-
-export default function convertToPosterPath({ relativePath, width }) {
-  return relativePath ? `${IMAGE_BASE_URL}${width}${relativePath}` : posterNotFoundImage;
-}
