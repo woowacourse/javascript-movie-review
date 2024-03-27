fe625d5f2a60f466cb3accbe154b689a236ab558
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-27 16:33:09 +0900

    refactor: fetch 실패시 재시도 횟수를 컨트롤러가 관리하도록 수정

--- src/util/retryLimiter.ts
+++
@@ -1,9 +0,0 @@
-export class RetryLimitError extends Error {
-  constructor() {
-    super('더 이상 요청할 수 없습니다.');
-  }
-}
-
-export function retryLimiter(tryCount: number) {
-  if (tryCount > 5) throw new RetryLimitError();
-}
