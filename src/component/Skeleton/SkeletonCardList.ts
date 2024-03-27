165f288ef5d02d5e74f3ee56a3a5d8c9f44726f0
pakxe <pigkill40@naver.com>
2024-03-27 21:57:24 +0900

    feat: 스켈레톤 목록을 구성하는 아이템에 대한 책임을 외부로 이동

--- src/component/Skeleton/SkeletonCardList.ts
+++ src/component/Skeleton/SkeletonCardList.ts
@@ -1,13 +1,29 @@
+import { handleElementVisibilityByElement } from '../../util/handleHideElement';
+
 const SKELETON_ROW_COUNT = 2;
 const SKELETON_CON_COUNT_LIST = [2, 3, 4];
 
-function createSkeletonCardList(createSkeletonItem: () => HTMLElement): Array<HTMLElement> {
-  const skeletonItemCount = SKELETON_ROW_COUNT * SKELETON_CON_COUNT_LIST[2];
-  const skeletonItemList = new Array(skeletonItemCount).fill(0).map(() => createSkeletonItem());
+class SkeletonCardList {
+  private skeletonCardList: Array<HTMLElement> | null;
 
-  skeletonItemList.forEach((item) => document.body.append(item));
+  constructor() {
+    this.skeletonCardList = null;
+  }
 
-  return skeletonItemList;
+  create(createSkeletonItem: () => HTMLElement): Array<HTMLElement> {
+    const skeletonItemCount = SKELETON_ROW_COUNT * SKELETON_CON_COUNT_LIST[2];
+    const skeletonItemList = new Array(skeletonItemCount).fill(0).map(() => createSkeletonItem());
+
+    skeletonItemList.forEach((item) => document.body.append(item));
+
+    this.skeletonCardList = skeletonItemList;
+
+    return skeletonItemList;
+  }
+
+  handleVisibility(type: boolean) {
+    this.skeletonCardList?.forEach((item) => handleElementVisibilityByElement(item, type));
+  }
 }
 
-export default createSkeletonCardList;
+export default SkeletonCardList;
