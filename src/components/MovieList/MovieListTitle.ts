49c327d8d427b844648f5edeb1f429ea37291285
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-03-27 11:41:35 +0900

    test: 테스트 파일 분리 (영화 목록 렌더링 테스트, 영화 검색 테스트)
    - tag 값 → id 값으로 접근

--- src/components/MovieList/MovieListTitle.ts
+++ src/components/MovieList/MovieListTitle.ts
@@ -19,8 +19,8 @@ export default class MovieListTitle extends BaseComponent {
     const query = this.queryState.get();
 
     return `
-      <h2 class="movie-list-title">
-        ${query ? `"${query}" 검색결과` : "지금 인기 있는 영화"}
+      <h2 id="movie-list-title" class="movie-list-title">
+        ${query ? `"${query}" 검색 결과` : "지금 인기 있는 영화"}
       </h2>
     `;
   }
