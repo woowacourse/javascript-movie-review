4610a77d39a58170c571d8749de43df7a0f2cffb
greetings1012 <ahnqwerty@gmail.com>
2024-03-27 15:32:33 +0900

    refactor: 클래스 멤버를 이용해 불필요한 함수 인자 삭제

--- src/components/MovieList/MovieList.ts
+++ src/components/MovieList/MovieList.ts
@@ -12,23 +12,23 @@ class MovieList {
   constructor(search?: string) {
     this.#page = 0;
     this.#search = search;
-    if (search) {
-      this.create(`"${search}"${CONTAINER_TITLE.searchResult}`, search);
+    if (this.#search) {
+      this.create(`"${this.#search}"${CONTAINER_TITLE.searchResult}`);
     } else {
-      this.create(CONTAINER_TITLE.popular, search);
+      this.create(CONTAINER_TITLE.popular);
     }
   }
 
-  async create(movieListTitle: string, search?: string) {
+  async create(movieListTitle: string) {
     const movieList = getDomElement('.item-view');
     const listEnd = document.createElement('div');
     listEnd.classList.add('list-end');
 
     movieList.appendChild(this.createTitle(movieListTitle));
     movieList.appendChild(listEnd);
-    await this.mountItems(listEnd, search);
+    await this.mountItems(listEnd);
 
-    await setupInfiniteScroll(listEnd, this.mountItems.bind(this), search);
+    await setupInfiniteScroll(listEnd, this.mountItems.bind(this), this.#search);
 
     getDomElement('#app').appendChild(movieList);
   }
@@ -41,20 +41,20 @@ class MovieList {
     return title;
   }
 
-  async mountItems(listEnd: HTMLElement, search?: string): Promise<void> {
+  async mountItems(listEnd: HTMLElement): Promise<void> {
     const skeleton = MovieItems.createSkeleton();
     listEnd.insertAdjacentElement('beforebegin', skeleton);
 
-    const movieListData: IRespondData = await this.getMovieListData(search);
+    const movieListData: IRespondData = await this.getMovieListData();
     MovieItems.replaceAllSkeletons(skeleton, movieListData);
     if (this.#page === movieListData.total_pages || this.#page === OPTIONS.maxPage) {
       listEnd.remove();
     }
   }
 
-  async getMovieListData(search?: string) {
-    if (search) {
-      return await fetchSearchMovies(++this.#page, search);
+  async getMovieListData() {
+    if (this.#search) {
+      return await fetchSearchMovies(++this.#page, this.#search);
     } else {
       return await fetchPopularMovies(++this.#page);
     }
