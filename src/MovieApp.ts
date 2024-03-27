ca22a2e4c667b131748cae5c7c71e09e0a4539c3
이태훈 <rhymint@gmail.com>
2024-03-27 13:36:03 +0900

    refactor: MovieApp.ts 생성을 통한 index.ts 로부터의 기능 분리

---
+++ src/MovieApp.ts
@@ -0,0 +1,82 @@
+import './reset.css';
+import './app.css';
+
+import Header from './components/Header/Header';
+import MovieItems from './components/MovieItems/MovieItems';
+import { MatchedMoviesService, PopularMoviesService } from './services/MovieService';
+import Fallback from './components/Fallback/Fallback';
+import MovieDetailModal from './components/MovieDetailModal/MovieDetailModal';
+import MovieGenres from './api/MovieGenres';
+import MovieGenresCollection from './domain/MovieGenresCollection';
+
+class MovieApp {
+  private body = document.querySelector('body') as HTMLBodyElement;
+  private header = new Header();
+  private movieItems = new MovieItems();
+  private movieDetailModal = new MovieDetailModal();
+
+  constructor() {
+    this.createElements();
+    this.setEventListener();
+    MovieGenresCollection.initialize();
+  }
+
+  createElements() {
+    this.body.appendChild(this.header.getElement());
+    this.body.appendChild(this.movieItems.getElement());
+    this.body.appendChild(this.movieDetailModal.getElement());
+  }
+
+  setEventListener() {
+    this.setGetPopularMoviesEventListener();
+    this.setGetMatchedMoviesEventListener();
+    this.setAPIErrorEventListener();
+    this.setToggleMovieDetailModalEventListener();
+  }
+
+  setGetPopularMoviesEventListener() {
+    document.addEventListener('GetPopularMovies', () => {
+      this.movieItems.moviesService = new PopularMoviesService();
+      this.reLoad();
+    });
+  }
+
+  setGetMatchedMoviesEventListener() {
+    document.addEventListener('GetMatchedMovies', (event) => {
+      if (!(event instanceof CustomEvent)) return;
+      const { query } = event.detail;
+
+      this.movieItems.moviesService = new MatchedMoviesService(query);
+      this.reLoad();
+    });
+  }
+
+  setAPIErrorEventListener() {
+    document.addEventListener('APIError', (event) => {
+      if (!(event instanceof CustomEvent)) return;
+      const main = this.body.querySelector('main') as HTMLElement;
+      main.innerHTML = '';
+      main.appendChild(new Fallback(event.detail.message).getElement());
+    });
+  }
+
+  setToggleMovieDetailModalEventListener() {
+    document.addEventListener('toggleMovieDetailModal', (event) => {
+      if (!(event instanceof CustomEvent)) return;
+      this.movieDetailModal.setMovieDetail(event.detail.value);
+      this.movieDetailModal.toggleModal();
+    });
+  }
+
+  reLoad() {
+    const fallback = this.body.querySelector('section');
+    if (fallback) {
+      fallback.remove();
+      this.movieItems.createElements();
+    }
+    this.movieItems.resetMovieItems();
+    this.movieItems.fetchMovieItems();
+  }
+}
+
+export default MovieApp;
