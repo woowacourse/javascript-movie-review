467a32c25ac134e90f3a45ef56e424c03b9d91a0
pakxe <pigkill40@naver.com>
2024-03-27 21:54:15 +0900

    chore: 불필요한 import 파일 제거

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import './asset/logo.png';
-import './asset/search_button.png';
-import './asset/star_empty.png';
-import './asset/star_filled.png';
-import './asset/retry.svg';
-import './asset/poster_not_found.jpg';
-
 import './component/toast/toast.css';
 import './component/reset.css';
 import './component/common.css';
 
-import { App } from './controller/controller.js';
+import { App } from './controller/controller';
 
 const app = new App();
 app.init();
