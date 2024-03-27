a6559b22a07a2d2e8219175d6777d56a0ed55bfa
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-27 16:33:09 +0900

    refactor: fetch 실패시 재시도 횟수를 컨트롤러가 관리하도록 수정

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -4,24 +4,25 @@ import createHeader from '../component/Header.js';
 import PageNumberManager from '../domain/pageNumberManager.ts';
 import { $ } from '../util/selector.js';
 import toast from '../component/toast/toast.js';
-import { RetryLimitError, retryLimiter } from '../util/retryLimiter.ts';
 import { MOVIE_LIST_TYPE } from '../constant/config';
 
 export class App {
   #searchKeyword;
+  #tryCount;
   #pageNumberManager;
   #movieService;
   #movieContainer;
 
   constructor() {
     this.#searchKeyword = '';
+    this.#tryCount = 1;
     this.#pageNumberManager = new PageNumberManager();
     this.#pageNumberManager.setPageType(MOVIE_LIST_TYPE.popular.type);
 
     this.#movieService = new MovieService();
     this.#movieContainer = new MovieContainer({
       title: MOVIE_LIST_TYPE.popular.title,
-      handleMoreButton: () => this.addMovieList(1),
+      handleMoreButton: () => this.addMovieList(),
     });
   }
 
@@ -35,34 +36,36 @@ export class App {
       this.#searchKeyword = '';
       $('form.search-box').reset();
 
-      this.addMovieList(1);
+      this.addMovieList();
     });
 
-    await this.addMovieList(1);
+    await this.addMovieList();
   }
 
-  async addMovieList(tryCount) {
+  async addMovieList() {
     try {
-      retryLimiter(tryCount);
       this.#movieContainer.pushMoreSkeletonList();
       this.#movieContainer.removeRetryButton();
+
       const moviePageData = await this.fetchMoviePageData();
       this.#movieContainer.replaceSkeletonListToData(moviePageData);
       this.#pageNumberManager.increase();
+      this.#tryCount = 1;
     } catch (error) {
-      this.handleRetryAddMovieList(error, tryCount);
+      this.handleRetryAddMovieList(error.message);
     }
   }
 
-  handleRetryAddMovieList(error, tryCount) {
-    if (error instanceof RetryLimitError) return toast(error);
-
+  handleRetryAddMovieList(errorMessage) {
+    toast(errorMessage);
     this.#movieContainer.removeSkeleton();
-    this.#movieContainer.createRetryButton(() => {
-      this.addMovieList(tryCount + 1);
-    });
+    this.#tryCount += 1;
 
-    toast(error);
+    if (this.#tryCount > 5) {
+      toast('재요청 가능한 횟수를 초과하였습니다.\n새로고침 후 다시 시도해 주세요.');
+    } else {
+      this.#movieContainer.createRetryButton(() => this.addMovieList());
+    }
   }
 
   async fetchMoviePageData() {
@@ -76,8 +79,8 @@ export class App {
 
   async fetchMovieData(mode, pageNumber, searchKeyword = '') {
     const fetchFunctions = {
-      search: this.#movieService.fetchSearchResult.bind(this.#movieService),
-      popular: this.#movieService.fetchPopularMovieList.bind(this.#movieService),
+      [MOVIE_LIST_TYPE.search.type]: this.#movieService.fetchSearchResult.bind(this.#movieService),
+      [MOVIE_LIST_TYPE.popular.type]: this.#movieService.fetchPopularMovieList.bind(this.#movieService),
     };
 
     const fetchFunction = fetchFunctions[mode];
@@ -87,11 +90,12 @@ export class App {
   }
 
   setSearchKeyword() {
-    const searchKeyword = $('form.search-box > input').value;
-    if (!searchKeyword || searchKeyword === '') {
-      return toast('검색어를 입력해주세요.');
+    const searchKeyword = $('form.search-box > input').value.trim();
+    if (!searchKeyword) {
+      toast('검색어를 입력해주세요.');
+      return;
     }
-    this.#searchKeyword = $('form.search-box > input').value;
+    this.#searchKeyword = searchKeyword;
   }
 
   makeSearchPage() {
@@ -99,8 +103,8 @@ export class App {
 
     this.setSearchKeyword();
     this.#movieContainer.clearMovieList();
-    this.#movieContainer.setTitle(`"${this.#searchKeyword}" 검색 결과`);
+    this.#movieContainer.setTitle(MOVIE_LIST_TYPE.search.title(this.#searchKeyword));
 
-    this.addMovieList(1);
+    this.addMovieList();
   }
 }
