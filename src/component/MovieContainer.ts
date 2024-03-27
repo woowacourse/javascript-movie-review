41219eaaa09b46531c8b0f93a87c6cc3c363e303
pakxe <pigkill40@naver.com>
2024-03-27 21:55:32 +0900

    feat: movieContainer에 물려있던 과도한 책임 분리

--- src/component/MovieContainer.ts
+++ src/component/MovieContainer.ts
@@ -5,8 +5,9 @@ import { handleElementVisibilityByElement } from '../util/handleHideElement';
 import { MovieData } from '../interface/MovieData';
 import wrapItemWithLi from '../util/wrapItemWithLi';
 import Movie from '../domain/Movie';
-import { EventHandler } from '../interface/view';
+import { EventHandler } from '../interface/event';
 import SkeletonCardList from './Skeleton/SkeletonCardList';
+import removeAllChild from '../util/removeAllChild';
 
 class MovieContainer {
   private movieContainer;
@@ -22,43 +23,60 @@ class MovieContainer {
     this.firstSkeletonItem = null;
     this.skeletonList = new SkeletonCardList();
 
-    this.createSkeletonMovieList();
+    // this.createSkeletonMovieList();
     this.addHandlerToMoreButton(handleMoreList);
   }
 
-  private createSkeletonMovieList() {
-    const skeletonMovieList = this.skeletonList.create(createSkeletonMovieItem);
-    const wrappedSkeletonMovieListWithLi = wrapItemWithLi(skeletonMovieList);
-    wrappedSkeletonMovieListWithLi.forEach((item) => this.movieListContainer.append(item));
+  // step2 에서 사용
+  // private createSkeletonMovieList() {
+  //   const skeletonMovieList = this.skeletonList.create(createSkeletonMovieItem);
+  //   const wrappedSkeletonMovieListWithLi = wrapItemWithLi(skeletonMovieList);
+  //   wrappedSkeletonMovieListWithLi.forEach((item) => this.movieListContainer.append(item));
 
-    this.firstSkeletonItem = wrappedSkeletonMovieListWithLi[0]; // 새로운 아이템 push를 위한 진입점 마킹
+  //   this.firstSkeletonItem = wrappedSkeletonMovieListWithLi[0]; // 새로운 아이템 push를 위한 진입점 마킹
+  // }
+
+  // 결과 없음 알림 요소 토글, ul 클리어, 버튼 보임 여부 토글
+  groundWorkForNewMovieList({ movieList, hasNextPage }: { movieList: Array<Movie>; hasNextPage: boolean }) {
+    try {
+      const noticeNoDataElement = $('.notice-no-data', this.movieContainer);
+      if (noticeNoDataElement && movieList.length !== 0) noticeNoDataElement.remove();
+    } catch (e) {
+      // 결과 없음 알림 요소가 없는 경우
+      if (movieList.length === 0) this.noticeNoData();
+    }
+
+    if (movieList.length === 0) this.clearMovieList();
+
+    handleElementVisibilityByElement(this.moreButton, hasNextPage);
   }
 
   pushNewMovieList({ movieList, hasNextPage }: { movieList: Array<Movie>; hasNextPage: boolean }) {
-    handleElementVisibilityByElement(this.moreButton, hasNextPage);
-
-    if (movieList.length === 0) {
-      this.clearList();
-      return;
-    }
+    this.groundWorkForNewMovieList({ movieList, hasNextPage });
 
     const movieItemList = movieList.map((movie) => createMovieItem(movie.data));
 
-    movieItemList.forEach((item) => this.firstSkeletonItem?.before(item));
+    if (this.firstSkeletonItem) movieItemList.forEach((item) => this.firstSkeletonItem!.before(item));
+    else movieItemList.forEach((item) => this.movieListContainer.append(item));
   }
 
   private addHandlerToMoreButton(handleMoreList: EventHandler) {
     this.moreButton.addEventListener('click', handleMoreList);
   }
 
-  private clearList() {
+  clearMovieList() {
+    this.skeletonList.handleVisibility(false);
+    if (this.movieListContainer.firstChild) removeAllChild(this.movieListContainer);
+    handleElementVisibilityByElement(this.moreButton, false);
+  }
+
+  noticeNoData() {
     const noticeNoDataElement = document.createElement('h3');
     noticeNoDataElement.textContent = '결과가 존재하지 않습니다.';
+    noticeNoDataElement.classList.add('notice-no-data');
 
     const mainTitle = $('.main-title');
     mainTitle.after(noticeNoDataElement);
-
-    this.skeletonList.handleVisibility(false);
   }
 }
 
