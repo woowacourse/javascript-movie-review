8b9357bd4ecb985a65473349d026cff338a2da6e
손진영 <sm099@naver.com>
2024-03-27 11:20:25 +0900

    refactor(MovieHeader): Header 내 Logo를 컴포넌트로 분리

---
+++ src/components/movie/MovieLogo/MovieLogo.ts
@@ -0,0 +1,27 @@
+import Component from '../../common/Component/Component';
+
+import { on } from '../../../utils/dom/eventListener/eventListener';
+
+import { Logo } from '../../../assets';
+
+class MovieLogo extends Component {
+  protected render() {
+    this.$element.insertAdjacentHTML('beforeend', this.createComponent());
+  }
+
+  protected createComponent() {
+    return /* html */ ` 
+      <img src=${Logo} alt="MovieList 로고" /> 
+    `;
+  }
+
+  protected setEvent() {
+    on({ target: this.$element, eventName: 'click', eventHandler: this.handleClickLogo });
+  }
+
+  private handleClickLogo() {
+    window.location.reload();
+  }
+}
+
+export default MovieLogo;
