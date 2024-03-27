e041add56a12f4b898f31a7f766757dfcbde9a15
greetings1012 <ahnqwerty@gmail.com>
2024-03-27 15:44:59 +0900

    refactor(ToastPopup): 떠있는 시간을 인수로 받도록 수정

--- src/components/ToastPopup/ToastPopup.ts
+++ src/components/ToastPopup/ToastPopup.ts
@@ -1,12 +1,12 @@
 import { getDomElement } from '../../util/DOM';
 
-const ToastPopup = (message: string) => {
+const ToastPopup = (message: string, activeTime: number) => {
   const toastMessage = getDomElement('#toast_message');
   toastMessage.textContent = message;
   toastMessage.classList.add('active');
   setTimeout(function () {
     toastMessage.classList.remove('active');
-  }, 2000);
+  }, activeTime);
 };
 
 export default ToastPopup;
