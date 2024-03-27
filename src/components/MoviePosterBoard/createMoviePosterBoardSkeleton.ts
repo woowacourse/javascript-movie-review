43ae82a53832567f457ca8105b21c4ca906d643a
ss0526100 <ss0526100@gmail.com>
2024-03-27 21:51:48 +0900

    feat: moviePosterBoard 스켈레톤 구현(함수형)

---
+++ src/components/MoviePosterBoard/createMoviePosterBoardSkeleton.ts
@@ -0,0 +1,18 @@
+import createElement from "../../utils/createElement";
+import { createMoviePreviewSkeleton } from "../MoviePoster/utils/createMovieSkeleton";
+
+const createMoviePosterBoardSkeleton = () => {
+  const posterBoard = createElement("ul", {
+    attrs: { class: "item-list item-list-skeleton" },
+  });
+
+  const skeletonPosters = Array.from({ length: 20 }).map(
+    createMoviePreviewSkeleton
+  );
+
+  posterBoard.append(...skeletonPosters);
+
+  return posterBoard;
+};
+
+export default createMoviePosterBoardSkeleton;
