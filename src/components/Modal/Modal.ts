c622724f190032f46269a69499162ecb5825b877
hwinkr <dnddl8280@naver.com>
2024-03-28 13:25:57 +0900

    feat(Modal): 재사용할 수 있는 모달 컴포넌트 구현
    - html dialog 태그 활용
    - 공개 메서드 showModal -> Element를 인자로 받아서 dialog에 추가한 후, dailog 태그가 기본적으로 제공하는 showModal 메서드 호출
    - 공개 메서드 hideModal -> 모달 뒷배경을 클릭하거나, 외부에서 호출할 경우 모달을 언마운트하고 내용을 비움

---
+++ src/components/Modal/Modal.ts
@@ -0,0 +1,47 @@
+import Component from "../common/Component";
+
+import { $ } from "../../utils/dom";
+
+import "./Modal.css";
+
+export default class Modal extends Component {
+  private $dialog: HTMLDialogElement | undefined;
+
+  protected getTemplate() {
+    return /*html*/ `
+      <dialog id="modal"></dialog>
+    `;
+  }
+
+  protected render(): void {
+    this.$target.insertAdjacentHTML("afterend", this.getTemplate());
+
+    const $dialog = $<HTMLDialogElement>("dialog#modal");
+
+    if ($dialog) {
+      this.$dialog = $dialog;
+    }
+  }
+
+  protected setEvent(): void {
+    this.$dialog?.addEventListener("click", (event: Event) => {
+      if (event.target === event.currentTarget) {
+        this.hideModal();
+      }
+    });
+  }
+
+  public hideModal() {
+    if (!this.$dialog) return;
+
+    this.$dialog.innerHTML = "";
+    this.$dialog?.close();
+  }
+
+  public showModal(children: Element) {
+    if (!this.$dialog) return;
+
+    this.$dialog.appendChild(children);
+    this.$dialog?.showModal();
+  }
+}
