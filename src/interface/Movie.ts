9eec33465d05b5ad7e1bbf5ddc1fe22d2e180b28
jaeml06 <jaeml0630@gmail.com>
2024-03-28 23:31:57 +0900

    feat: 영화 상세 보기를 위한 타입 추가

--- src/interface/Movie.ts
+++ src/interface/Movie.ts
@@ -2,7 +2,7 @@ export interface Movie {
   id: number;
   title: string;
   poster_path: string | null;
-  vote_average: number;
+  vote_average: string;
 }
 
 export type MovieAPIResponse = [Movie[], number, number];
@@ -10,7 +10,7 @@ export type MovieAPIResponse = [Movie[], number, number];
 export interface UrlParams extends URLSearchParams {
   api_key?: string;
   language: string;
-  page: string;
+  page?: string;
   query?: string;
 }
 
@@ -37,4 +37,67 @@ export interface MovieData {
   video: boolean;
   vote_average: number;
   vote_count: number;
+}
+
+export interface MovieDetailData {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: Collection;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Collection {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface MovieDetail extends Movie {
+  overview: string;
+  genres: string[];
 }
