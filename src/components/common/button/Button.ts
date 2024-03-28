8aed7c51a1503103b5a927b23eb409d86c084b7a
skiende74 <skiende74@gmail.com>
2024-03-28 14:09:37 +0900

    refactor(Button): 버튼 엘리먼트 단일라인 if를 중괄호 if로 변경

--- src/components/common/button/Button.ts
+++ src/components/common/button/Button.ts
@@ -9,10 +9,14 @@ class Button {
   $target: HTMLButtonElement = document.createElement('button');
 
   constructor(props: ButtonProps) {
-    if (props.id !== undefined) this.$target.id = props.id;
     this.$target.classList.add(...(props.classNames ?? []));
     this.$target.append(...(props.children ?? []));
-    if (props.onClick) this.$target.addEventListener('click', props.onClick);
+    if (props.id !== undefined) {
+      this.$target.id = props.id;
+    }
+    if (props.onClick) {
+      this.$target.addEventListener('click', props.onClick);
+    }
   }
 }
 
