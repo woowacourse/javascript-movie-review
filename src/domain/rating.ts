cf2ec3dbf92864006c02583ebdf4b90685eb0a2d
bmj0053 <bmj05378@gmail.com>
2024-03-28 23:16:42 +0900

    refactor: rating 도메인 타입 강화 및  리팩터링

--- src/domain/rating.ts
+++ src/domain/rating.ts
@@ -2,26 +2,25 @@ import { RatingListType, RatingType } from '../types/ratings';
 
 const rating = {
   getLocalData(): RatingListType {
-    const data = JSON.parse(localStorage.getItem('ratings') ?? '[]');
+    const data = JSON.parse(localStorage.getItem('ratings') ?? '[]') as RatingListType;
     return data;
   },
 
-  getLocalDataItem(id: number) {
+  getLocalDataItem(id: number): RatingType {
     const dataList = this.getLocalData();
-    return dataList.filter((data: RatingType) => data.id === id)[0] ?? { id, ratingValue: 0 };
+    return dataList.find((data) => data.id === id) ?? { id, ratingValue: 0 };
   },
 
   updateLocalData(id: number, ratingValue: number) {
     const dataList = this.getLocalData();
-    const isIncluded = dataList.filter((data: RatingType) => data.id === id).length > 0;
-    const newData = { id, ratingValue };
+    const newData: RatingType = { id, ratingValue };
+    const updatedDataList = dataList.map((data) => (data.id !== id ? data : newData));
 
-    const updatedData = dataList.map((data: RatingType) => {
-      if (data.id !== id) return data;
-      return newData;
-    });
-    if (!isIncluded) updatedData.push(newData);
-    localStorage.setItem('ratings', JSON.stringify(updatedData));
+    if (!dataList.some((data) => data.id === id)) {
+      updatedDataList.push(newData);
+    }
+
+    localStorage.setItem('ratings', JSON.stringify(updatedDataList));
   },
 };
 
