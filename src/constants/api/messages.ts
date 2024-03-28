2fd6718411b5c6f1b2b45d67bda375d47bdb4362
brgndy <brgndy96@gmail.com>
2024-03-29 08:52:07 +0900

    feat: 로컬스토리지 관련 에러 함수 추가

--- src/constants/api/messages.ts
+++ src/constants/api/messages.ts
@@ -4,6 +4,7 @@ const ERROR_MESSAGE = Object.freeze({
   FAIL_GET_ALL_GENRE: '영화 장르 정보를 받아오는 데에 실패했어요!',
   INVALID_CONDITION_SEARCH_KEYWORD: '검색어는 1글자 이상이어야 합니다.',
   INVALID_STAR_RATE: '유효하지 않은 별점이에요!',
+  FAIL_GET_DATA_FROM_LOCAL_STORAGE: '로컬 스토리지에서 데이터를 받아오지 못했어요!',
 });
 
 export default ERROR_MESSAGE;
--- src/services/LocalStorageService.ts
+++ src/services/LocalStorageService.ts
@@ -1,13 +1,16 @@
 import { TotalMovieItemProps } from '../types/movie';
 import LOCAL_STORAGE from '../constants/api/localStorage';
+import HttpError from '../error/HttpError';
+import ERROR_MESSAGE from '../constants/api/messages';
 
 const LocalStorageService = {
   getData(): TotalMovieItemProps[] {
     const data = localStorage.getItem(LOCAL_STORAGE.KEY);
-    if (data) {
-      return JSON.parse(data);
+    if (!data) {
+      const error = new HttpError(ERROR_MESSAGE.FAIL_GET_DATA_FROM_LOCAL_STORAGE, 503);
+      throw error;
     }
-    return [];
+    return JSON.parse(data);
   },
 
   setData(data: TotalMovieItemProps[]) {
