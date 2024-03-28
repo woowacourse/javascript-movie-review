112e2766858aae003111d46217acae5848db8d7d
hwinkr <dnddl8280@naver.com>
2024-03-28 13:48:17 +0900

    refactor(MovieFetcher): 영화 데이터 api를 제공하는 도메인에 상세 정보 엔드포인트 추가

--- src/http/MovieFetcher.ts
+++ src/http/MovieFetcher.ts
@@ -1,7 +1,14 @@
 import Fetcher from "./Fetcher";
-import { BASE_URL } from "../constants/movies";
+
 import { HttpFetcher } from "../types/http";
-import { FetchResponse, MovieItem, MovieItemResponse } from "../types/movies";
+import { FetchResponse, MovieDetailItem, MovieDetailResponse, MovieItem, MovieItemResponse } from "../types/movies";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+interface UrlOption {
+  page?: number;
+  query?: string;
+}
 
 class MovieFetcher {
   private movieFetcher: HttpFetcher;
@@ -9,27 +16,48 @@ class MovieFetcher {
   private MOVIE_API_END_POINT = {
     popular: "movie/popular?",
     search: "search/movie?",
+    detail: "movie/",
   } as const;
 
   constructor(baseUrl: string) {
     this.movieFetcher = new Fetcher(baseUrl);
   }
 
-  private createMovieFetchUrl(page: number, query?: string): string {
+  private createMovieFetchUrl(options: UrlOption = {}): string {
+    const { page, query } = options;
+
     if (!process.env.API_KEY) return "";
 
     const requestUrl = new URLSearchParams({
-      language: "ko-KR",
-      page: String(page),
       api_key: process.env.API_KEY,
+      language: "ko-KR",
     });
 
-    query && requestUrl.append("query", query);
+    if (page) {
+      requestUrl.append("page", String(page));
+    }
+
+    if (query) {
+      requestUrl.append("query", query);
+    }
 
     return requestUrl.toString();
   }
 
-  private formattingMovieResponse(movies: MovieItemResponse[]): MovieItem[] {
+  private formattingMovieDetailResponse(movie: MovieDetailResponse): MovieDetailItem {
+    const formattedGenres = movie.genres.map(({ name }) => name);
+
+    return {
+      id: movie.id,
+      title: movie.title,
+      imagePath: movie.poster_path,
+      voteAverage: movie.vote_average,
+      genres: formattedGenres,
+      overview: movie.overview,
+    };
+  }
+
+  private formattingMovieListResponse(movies: MovieItemResponse[]): MovieItem[] {
     return movies.map((movie) => {
       return {
         id: movie.id,
@@ -42,21 +70,31 @@ class MovieFetcher {
 
   private async getMovieData(url: string): Promise<MovieItem[] | undefined> {
     const response = await this.movieFetcher.get<FetchResponse<MovieItemResponse[]>>(url);
-    const popularMovies = this.formattingMovieResponse(response.results);
+    const popularMovies = this.formattingMovieListResponse(response.results);
 
     return popularMovies;
   }
 
   public async getPopularMovies(page: number): Promise<MovieItem[] | undefined> {
-    const url = this.MOVIE_API_END_POINT.popular + this.createMovieFetchUrl(page);
+    const url = this.MOVIE_API_END_POINT.popular + this.createMovieFetchUrl({ page });
 
     return await this.getMovieData(url);
   }
 
   public async getSearchMovies(page: number, query: string): Promise<MovieItem[] | undefined> {
-    const url = this.MOVIE_API_END_POINT.search + this.createMovieFetchUrl(page, query);
+    const url = this.MOVIE_API_END_POINT.search + this.createMovieFetchUrl({ page, query });
 
     return await this.getMovieData(url);
+  }
+
+  public async getMovieDetail(id: string): Promise<MovieDetailItem | undefined> {
+    const url = this.MOVIE_API_END_POINT.detail + id + "?" + this.createMovieFetchUrl();
+
+    const response = await this.movieFetcher.get<MovieDetailResponse>(url);
+
+    const formattedMovieDetail = this.formattingMovieDetailResponse(response);
+
+    return formattedMovieDetail;
   }
 }
 
