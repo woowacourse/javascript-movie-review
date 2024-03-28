7ff385df158279672ba4fbae89afe1be6bbd3880
skiende74 <skiende74@gmail.com>
2024-03-28 13:59:38 +0900

    feat(MovieItem): MovieItem에 시매틱태그 적용

--- src/components/movieItem/MovieItem.ts
+++ src/components/movieItem/MovieItem.ts
@@ -14,33 +14,34 @@ class MovieItem {
 
   template() {
     return `   
-        <a href="#">
-          <div class="item-card">
-          <img
-          class="item-thumbnail skeleton"
-          loading="lazy"
-          alt=""
-        />
-        <p class="item-title"></p>
-        <div class="item-caption">
-          <p class="item-score"></p>
-          <img class="item-star-icon" src="./images/star_filled.png" alt="별점" />
-        </div>
-        </div>
-        </a>
-      `;
+  <a href="#">
+    <article class="item-card">
+      <img
+      class="item-thumbnail skeleton"
+      loading="lazy"
+      alt="" />
+      <h3 class="item-title"></h3>
+      <div class="item-caption">
+        <p class="item-score"></p>
+        <img class="item-star-icon" src="./images/star_filled.png" alt="별점" />
+      </div>
+    </article>
+  </a>
+  `;
   }
 
   paint(movie: IMovie) {
     const $image = dom.getElement<HTMLImageElement>(this.$target, '.item-thumbnail');
-    const $title = dom.getElement<HTMLParagraphElement>(this.$target, '.item-title');
-    const $score = dom.getElement<HTMLParagraphElement>(this.$target, '.item-score');
+    const $title = dom.getElement(this.$target, '.item-title');
+    const $score = dom.getElement(this.$target, '.item-score');
 
     $image.setAttribute('src', movie.imageSrc);
     $image.setAttribute('alt', movie.title);
-    $title.textContent = movie.title;
+    $title.innerText = movie.title;
     $score.textContent = movie.score.toString();
   }
+
+  #formatScore(score: number) {}
 }
 
 export default MovieItem;
