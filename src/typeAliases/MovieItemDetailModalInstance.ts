9b2ad42f87df1d25e73ca8e91f567baa2eeb3ec2
simhorim <hori5020@gmail.com>
2024-03-28 11:42:07 +0900

    feat(MovieItemDetailModalInstance): 타입 별칭 생성

---
+++ src/typeAliases/MovieItemDetailModalInstance.ts
@@ -0,0 +1,5 @@
+import MovieItemDetailModal from '../components/Modal/MovieItemDetailModal';
+
+type MovieItemDetailModalInstance = InstanceType<typeof MovieItemDetailModal>;
+
+export default MovieItemDetailModalInstance;
