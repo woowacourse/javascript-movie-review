19b90e0f2a40d63c087d174ad33d0c51a9c439b2
hwinkr <dnddl8280@naver.com>
2024-03-28 13:47:28 +0900

    refactor(type): 영화 상세 정보 도메인 객체 추가 구현

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -2,26 +2,47 @@ export interface FetchResponse<T> {
   results: T;
 }
 
-export interface MovieItemResponse {
+interface MovieFiledCommonResponse {
   id: number;
   title: string;
-  original_title: string;
-  original_language: string;
+  adult: boolean;
   backdrop_path: string;
   poster_path: string;
-  genre_ids: number[];
+  original_title: string;
+  original_language: string;
   overview: string;
   popularity: number;
-  vote_average: number;
-  vote_count: number;
   release_date: string;
   video: boolean;
-  adult: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
-export interface MovieItem {
-  id: number;
-  title: string;
+export interface MovieItemResponse extends MovieFiledCommonResponse {
+  genre_ids: number[];
+}
+
+export interface MovieDetailResponse extends MovieFiledCommonResponse {
+  belongs_to_collection: any;
+  budget: number;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  imdb_id: string;
+  production_companies: { id: number; name: string }[];
+  production_countries: { iso_3166_1: string; name: string }[];
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: { iso_639_1: string; name: string }[];
+  status: string;
+  tagline: string;
+}
+
+export interface MovieItem extends Pick<MovieItemResponse, "id" | "title"> {
   imagePath: string;
   voteAverage: number;
+}
+
+export interface MovieDetailItem extends Pick<MovieItem, "id" | "title" | "imagePath" | "voteAverage"> {
+  genres: string[];
+  overview: string;
 }
