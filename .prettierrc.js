fa33d33d588471a638be63738ce3728a92978aa0
skiende74 <skiende74@gmail.com>
2024-03-28 13:30:20 +0900

    chore(prettier): prettierrc에서 proseWrap삭제

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,11 @@
 module.exports = {
-  printWidth: 120, 
-  tabWidth: 2, 
-  useTabs: false, 
-  semi: true, 
-  singleQuote: true, 
-  trailingComma: 'all', 
-  bracketSpacing: true, 
-  arrowParens: 'avoid', 
-  proseWrap: 'never',
-  endOfLine: 'auto'
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
 };
