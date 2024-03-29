eff7abcddb125782457bb0f123e1297494c7fa0f
Kim Da Eun <midekuna@gmail.com>
2024-03-29 16:40:58 +0900

    chore: Maint.ts의 사용하지 않는 코드 제거

--- src/components/Main/Main.ts
+++ src/components/Main/Main.ts
@@ -1,25 +1,5 @@
 import MovieList from '../MovieList/MovieList';
 
-// const clearItemViewForError = () => {
-//   const $ul = document.querySelector('.item-view ul') as HTMLElement;
-//   const $h2 = document.querySelector('.item-view h2') as HTMLElement;
-//   const $itemView = document.querySelector('.item-view');
-//   $itemView?.removeChild($ul);
-//   $itemView?.removeChild($h2);
-// };
-
-// const showSearchResultsNotFound = ($skeletonMovieList: HTMLElement) => {
-//   const $itemView = document.querySelector('.item-view');
-//   const $skeletonUl = $skeletonMovieList.querySelector('ul') as HTMLElement;
-//   $skeletonMovieList.removeChild($skeletonUl);
-
-//   const $searchResultsNotFound = document.createElement('p');
-//   $searchResultsNotFound.classList.add('search-results-not-found');
-//   $searchResultsNotFound.textContent = '검색 결과가 존재하지 않습니다.';
-
-//   $itemView?.appendChild($searchResultsNotFound);
-// };
-
 const Main = () => {
   const $main = document.createElement('main');
   const movieList = MovieList();
