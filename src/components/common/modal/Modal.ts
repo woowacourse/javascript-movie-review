77913598e2ceec093d8d00ed2ae809eb76a04b09
skiende74 <skiende74@gmail.com>
2024-03-29 18:02:11 +0900

    feat(Modal): 기본 모달 재사용 컴포넌트 작성

---
+++ src/components/common/modal/Modal.ts
@@ -0,0 +1,53 @@
+import { dom } from '../../../utils/dom';
+import './Modal.css';
+
+const OPEN_CSS = 'modal-open';
+
+class Modal {
+  $target: HTMLDivElement = document.createElement('div');
+  $container: HTMLDivElement;
+  $backdrop: HTMLDivElement;
+
+  constructor(children: HTMLElement[]) {
+    this.$target.innerHTML = this.#template();
+    this.$container = dom.getElement(this.$target, '.modal-container');
+    this.$backdrop = dom.getElement(this.$target, '.modal-backdrop');
+    this.$backdrop.addEventListener('click', () => {
+      this.close();
+    });
+
+    this.$target.classList.add('modal');
+    this.$container.append(...children);
+  }
+
+  #template() {
+    return `<div class="modal-container"></div>
+        <div class="modal-backdrop"></div>`;
+  }
+
+  close() {
+    this.$target.classList.remove(OPEN_CSS);
+  }
+
+  open() {
+    this.$target.classList.add(OPEN_CSS);
+  }
+
+  toggle() {
+    if (this.$target.classList.contains(OPEN_CSS)) {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+
+  appendAll(nodes: Node[]) {
+    this.$container.append(...nodes);
+  }
+
+  append(node: Node) {
+    this.$container.append(node);
+  }
+}
+
+export default Modal;
