8b91e8697322ff9b634ae10ee7eb41a4fcac218c
Jeongwoo Park <wahoo9040@naver.com>
2024-03-29 14:42:39 +0900

    feat: 영화 모달 내부 헤더

--- src/view/movieDetailModal.ts
+++ src/view/movieDetailModal.ts
@@ -1,12 +1,40 @@
+import XImage from '../assets/images/closeButton.svg';
+
 function closeModal() {
   const modal = document.getElementById('movie-detail-modal') as HTMLDialogElement;
   document.body.classList.remove('no-scroll-y');
   modal.close();
 }
 
+function createMovieTitle() {
+  const title = document.createElement('div');
+  title.innerText = '영화제목';
+  return title;
+}
+
+function createModalCloseButton() {
+  const button = document.createElement('button');
+  button.className = 'flex-center-aligned close-button transition-background';
+  const closeImage = document.createElement('img');
+  closeImage.src = XImage;
+  closeImage.alt = 'close-button';
+  closeImage.addEventListener('click', closeModal);
+  button.append(closeImage);
+  return button;
+}
+
+function createMovieDetailHeader() {
+  const div = document.createElement('div');
+  div.className = 'movie-detail-header flex-center-aligned';
+  div.append(createMovieTitle(), createModalCloseButton());
+  return div;
+}
+
 function createMovieDetailContainer() {
   const container = document.createElement('div');
   container.classList.add('movie-modal');
+  const header = createMovieDetailHeader();
+  container.append(header);
   return container;
 }
 
