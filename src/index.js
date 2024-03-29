dc94a0ece3439730aa8cef6e6175d4f364abcc59
simhorim <hori5020@gmail.com>
2024-03-29 14:01:26 +0900

    fix(index): 스크롤이 바닥에 닿기 전 영화 목록 업데이트 되는 오류 해결

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import './resources.js';
 import MovieHeader from './components/MovieHeader/MovieHeader';
 import ItemView from './components/ItemView/ItemView';
-import Throttle from './Utils/Throttle.ts';
+import Throttle from './Utils/Throttle';
+import MovieItemDetailModal from './components/Modal/MovieItemDetailModal';
 
 const init = () => {
-  const itemView = new ItemView();
+  const movieItemDetailModal = new MovieItemDetailModal();
+  const itemView = new ItemView(movieItemDetailModal);
   const header = MovieHeader.create(
     () => itemView.showPopularMovies(),
     () => itemView.showSearchMovies(),
   );
-
   const app = document.getElementById('app');
+
   app.prepend(header);
   app.appendChild(itemView.getItemView());
+  app.appendChild(movieItemDetailModal.getElement());
 
-  const isTouchedBottom = () => window.innerHeight + window.scrollY + 10 >= document.body.offsetHeight;
   window.addEventListener(
     'scroll',
     Throttle(() => {
-      if (isTouchedBottom) itemView.mountItems();
+      if (window.innerHeight + window.scrollY + 0.75 >= document.body.offsetHeight) itemView.mountItems();
     }, 1000),
   );
 };
