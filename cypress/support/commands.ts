ec06f70be24dec3faa72fa0fb0a49ce1335ec653
badahertz52 <badahertz52@gmail.com>
2024-03-29 16:13:59 +0900

    cypress: 영화 상세 정보 모달 관련 테스트 추가

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -8,9 +8,15 @@ Cypress.Commands.add('scrollBottom', () => {
   cy.get('.scroll-observer-target').scrollIntoView();
 });
 
+Cypress.Commands.add('clickFirstMovieCard', () => {
+  cy.get('.movie-list-container').within(() => {
+    cy.get('.movie-list').find('li').first().click();
+  });
+});
 declare namespace Cypress {
   interface Chainable {
     visitMainPage(): Cypress.Chainable<void>;
     scrollBottom(): Cypress.Chainable<void>;
+    clickFirstMovieCard(): Cypress.Chainable<void>;
   }
 }
--- cypress/utils/createURL.js
+++ cypress/utils/createURL.js
@@ -7,3 +7,8 @@ export const getSearchURL = (page) =>
   new RegExp(
     `^https:\\/\\/api\\.themoviedb\\.org\\/3\\/search\\/movie\\?query=.*&include_adult=false&language=ko&page=${page}`,
   );
+
+export const getMovieInfoURL = (id) =>
+  new RegExp(
+    `^https:\\/\\/api\\.themoviedb\\.org\\/3\\/movie\\/${id}\\?language=ko`,
+  );
