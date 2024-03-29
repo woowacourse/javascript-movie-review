715510e61a3d96e9cbff68567d0dcffaefb50296
ss0526100 <ss0526100@gmail.com>
2024-03-29 20:12:00 +0900

    refactor: 컴포넌트 숨김/보임 함수 분리

---
+++ src/domainComponent/utils/hideElement.ts
@@ -0,0 +1,5 @@
+const hideElement = (element: HTMLElement) => {
+  element.classList.add("visibility-hidden-no-position");
+};
+
+export default hideElement;
---
+++ src/domainComponent/utils/revealElement.ts
@@ -0,0 +1,5 @@
+const revealElement = (element: HTMLElement) => {
+  element.classList.remove("visibility-hidden-no-position");
+};
+
+export default revealElement;
