39f4c47ac959cf3f3f204f65e9f36ea513fc088e
Jeongwoo Park <wahoo9040@naver.com>
2024-03-29 19:06:09 +0900

    refactor: fetchErrorCheck 함수 모듈 분리

---
+++ src/api/fetchErrorCheck.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line max-lines-per-function
+function fetchErrorCheck(status: number) {
+  if (status === 200) return;
+
+  // eslint-disable-next-line default-case
+  switch (status) {
+    case 401:
+      throw new Error(`${status} 유효하지 않은 접근입니다.`);
+    case 403:
+      throw new Error(`${status} 접근 권한이 없습니다.`);
+    case 404:
+      throw new Error(`${status} 컨텐츠를 찾을 수 없습니다.`);
+    case 500:
+    case 502:
+    case 503:
+      throw new Error(`${status} 서버에서 문제가 발생했습니다.`);
+  }
+}
+
+export default fetchErrorCheck;
--- src/api/fetchMovies.ts
+++ src/api/fetchMovies.ts
@@ -1,28 +1,10 @@
 import movieStateMethod from '../store/movieStore';
+import fetchErrorCheck from './fetchErrorCheck';
 
 export interface IFetchParams {
   url: string;
   page: number;
   query?: string;
-}
-
-// eslint-disable-next-line max-lines-per-function
-function handleError(status: number) {
-  if (status === 200) return;
-
-  // eslint-disable-next-line default-case
-  switch (status) {
-    case 401:
-      throw new Error(`${status} 유효하지 않은 접근입니다.`);
-    case 403:
-      throw new Error(`${status} 접근 권한이 없습니다.`);
-    case 404:
-      throw new Error(`${status} 컨텐츠를 찾을 수 없습니다.`);
-    case 500:
-    case 502:
-    case 503:
-      throw new Error(`${status} 서버에서 문제가 발생했습니다.`);
-  }
 }
 
 // eslint-disable-next-line max-lines-per-function
@@ -46,7 +28,7 @@ async function fetchMovies() {
     page: movieStateMethod.getPage(),
     query: movieStateMethod.getQuery(),
   });
-  handleError(response.status);
+  fetchErrorCheck(response.status);
   return response.json();
 }
 
