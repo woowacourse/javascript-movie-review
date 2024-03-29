2debe74d060c7486f881dddc05f9e83877866533
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-29 16:12:46 +0900

    feat: 영화 요약 정보 미등록시 처리

--- src/components/MovieOverview.ts
+++ src/components/MovieOverview.ts
@@ -2,7 +2,12 @@ import { createElementWithAttribute } from "../utils";
 
 const MovieOverview = (overview: string, className: string) => {
   const $overview = createElementWithAttribute("p", { class: className });
-  $overview.textContent = overview;
+
+  if (overview.length !== 0) {
+    $overview.textContent = overview;
+  } else {
+    $overview.textContent = "영화 정보가 등록되어 있지 않습니다.";
+  }
 
   return $overview;
 };
