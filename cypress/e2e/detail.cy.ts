e6b8ce06e8fd08b433779549dfb02ae33997d200
rbgksqkr <rbgks1937@naver.com>
2024-03-29 20:01:48 +0900

    test(e2e): 영화 상세보기 모달 렌더링 테스트 구현

--- cypress/e2e/detail.cy.ts
+++ cypress/e2e/detail.cy.ts
@@ -2,10 +2,14 @@ import { MOVIE_API_URL } from '../../src/constants/url';
 
 describe('영화 상세보기 테스트', () => {
   beforeEach(() => {
+    cy.intercept({
+      method: 'GET',
+      url: /^https:\/\/api\.themoviedb\.org\/3\/movie*/,
+    }).as('getDetailMovie');
     cy.visit('/');
   });
 
-  it('영화 목록 API를 호출하면 한 번에 20개씩 목록에 나열되어야 한다', () => {
+  it('영화 상세보기 API를 호출하면 해당 영화의 poster_path를 반환해야한다.', () => {
     const movieId = 1011985;
     const params = `/${movieId}?language=ko-KR`;
     const movieDetailUrl = MOVIE_API_URL + params;
@@ -21,5 +25,14 @@ describe('영화 상세보기 테스트', () => {
 
     cy.get('@detailMovies').its('status').should('eq', 200);
     cy.get('@detailMovies').its('body').should('have.a.property', 'poster_path');
+  });
+
+  it('쿵푸팬더 4를 클릭하면 쿵푸팬더 4 영화의 상세 정보를 조회할 수 있다.', () => {
+    cy.get('.item-card').first().click();
+    cy.wait('@getDetailMovie').then(interception => {
+      if (interception.response === undefined) return;
+      const movieDetailTitle = cy.get('.item-card').first().get('#modal-title');
+      expect(movieDetailTitle.should('have.text', '쿵푸팬더 4'));
+    });
   });
 });
