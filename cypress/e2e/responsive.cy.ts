ac5a27c9d8ac4cef12c4fe1006bd301828e655ed
rbgksqkr <rbgks1937@naver.com>
2024-03-29 21:22:57 +0900

    test(e2e): 영화 상세보기 모달 반응형 UI 테스트 코드 작성

--- cypress/e2e/responsive.cy.ts
+++ cypress/e2e/responsive.cy.ts
@@ -18,4 +18,18 @@ describe('반응형 UI 테스트', () => {
     cy.viewport(740, 800);
     cy.get('.item-list').should('have.css', 'grid-template-columns', '140px 140px');
   });
+
+  it('영화 상세보기 모달은 viewport의 width가 750px, 400px 기준으로 크기가 변한다.', () => {
+    cy.get('.item-card').first().click();
+    cy.wait('@getPopularMovies').then(interception => {
+      if (interception.response === undefined) return;
+      cy.wait('@getDetailMovie').then(() => {
+        cy.get('#thumbnail').should('be.visible');
+        cy.viewport(750, 800);
+        cy.get('#thumbnail').should('not.be.visible');
+        cy.viewport(400, 800);
+        cy.get('.detail-modal-container').should('have.css', 'inset', '200px 0px 0px');
+      });
+    });
+  });
 });
