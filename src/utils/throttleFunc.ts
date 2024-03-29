208a184de4bed8d62f562f1f2483084a543cf1a2
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-30 04:22:57 +0900

    refactor: 불필요한 주석 삭제

--- src/utils/throttleFunc.ts
+++ src/utils/throttleFunc.ts
@@ -1,13 +1,5 @@
 let throttle: NodeJS.Timeout | null = null;
 
-// window.addEventListener("scroll", () => {
-//   if (!throttle) {
-//     throttle = setTimeout(() => {
-//       throttle = null;
-//     }, 300);
-//   }
-// });
-
 const throttleFunc = (func: () => void) => {
   if (!throttle) {
     throttle = setTimeout(() => {
