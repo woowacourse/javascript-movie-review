7914a3f20d12d53a5f5c593e0fb62bbc16308ae3
rbgksqkr <rbgks1937@naver.com>
2024-03-29 20:23:57 +0900

    test(e2e): 별점이 로컬 스토리지에 저장되었는지 확인하는 테스트 코드 작성

--- cypress/e2e/detail.cy.ts
+++ cypress/e2e/detail.cy.ts
@@ -28,23 +28,46 @@ describe('영화 상세보기 테스트', () => {
   });
 
   it('쿵푸팬더 4를 클릭하면 쿵푸팬더 4 영화의 상세 정보를 조회할 수 있다.', () => {
+    const movieTitle = '쿵푸팬더 4';
+
     cy.get('.item-card').first().click();
     cy.wait('@getDetailMovie').then(interception => {
       if (interception.response === undefined) return;
+
       const movieDetailTitle = cy.get('.detail-modal-container').first().get('#modal-title');
-      expect(movieDetailTitle.should('have.text', '쿵푸팬더 4'));
+      expect(movieDetailTitle.should('have.text', movieTitle));
     });
   });
 
-  it('상세보기 모달에서 해당 영화의 별점을 매길 수 있는데 첫번째 별을 클릭하면 2점을 매기고 최악이예요 텍스트를 출력한다.', () => {
+  it('상세보기 모달에서 해당 영화의 별점을 매길 수 있는데 첫번째 별을 클릭하면 "2점"을 매기고 "최악이예요" 텍스트를 출력한다.', () => {
+    const scoreNumber = 2;
+    const scoreText = '최악이예요';
+
     cy.get('.item-card').first().click();
     cy.wait('@getDetailMovie').then(interception => {
       if (interception.response === undefined) return;
+
       cy.get('#star-container > img').first().click();
       const score = cy.get('#score-number');
-      expect(score.should('have.text', '2'));
+      expect(score.should('have.text', scoreNumber));
       const text = cy.get('#score-text');
-      expect(text.should('have.text', '최악이예요'));
+      expect(text.should('have.text', scoreText));
+    });
+  });
+
+  it('별점을 매기면 새로고침해도 별점 데이터를 유지하기 위해 로컬 스토리지에 저장된다.', () => {
+    const movieId = 1011985;
+    const score = 2;
+
+    cy.get('.item-card').first().click();
+    cy.wait('@getDetailMovie').then(interception => {
+      if (interception.response === undefined) return;
+
+      cy.get('#star-container > img').first().click();
+      cy.window().then(win => {
+        const storageMovie = win.localStorage.getItem('movies');
+        expect(storageMovie).to.equal(`[{"id":${movieId},"score":${score}}]`);
+      });
     });
   });
 });
