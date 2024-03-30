f5741290a2e9e598b62d45f1552200cd72830709
chysis <chysiss@naver.com>
2024-03-31 00:30:51 +0900

    refactor: Modal 컴포넌트 삭제, MovieDetailModal을 독립적으로 관리

--- src/components/Modal/MovieDetailModal.ts
+++ src/components/Modal/MovieDetailModal.ts
@@ -1,36 +1,43 @@
 import Star from '../../assets/star_filled.png';
 import NoImage from '../../assets/no-image.png';
-import Modal from './Modal';
 import './MovieDetailModal.css';
 import { MovieDetailAPI } from '../../domain/services/API.type';
 import { POSTER_BASE_URL } from '../../consts/Api';
 
-const movieDetailModalLayout = document.createElement('div');
-movieDetailModalLayout.classList.add('movie-detail-container');
+class MovieDetailModal {
+  modal = document.createElement('div');
 
-class MovieDetailModal extends Modal {
   constructor() {
-    super({ child: movieDetailModalLayout });
+    this.render();
+    this.mount();
+    this.setEvents();
   }
 
-  rerender(movieData: MovieDetailAPI) {
-    movieDetailModalLayout.replaceChildren();
+  render() {
+    const backdrop = document.createElement('div');
+    const container = document.createElement('div');
+
+    this.modal.classList.add('modal');
+    backdrop.classList.add('modal-backdrop');
+    container.classList.add('movie-detail-container');
+
+    this.modal.appendChild(backdrop);
 
     const detailTitle = document.createElement('h2');
-    detailTitle.classList.add('detail-title');
-    detailTitle.textContent = movieData.title;
-    movieDetailModalLayout.append(detailTitle);
+    detailTitle.classList.add('detail-title', 'skeleton');
+    // detailTitle.textContent = movieData.title;
+    container.append(detailTitle);
 
     const detailBox = document.createElement('div');
     detailBox.classList.add('detail-box');
 
     const detailImage = document.createElement('img');
     detailImage.classList.add('detail-image');
-    if (movieData.poster_path) {
-      detailImage.setAttribute('src', POSTER_BASE_URL + movieData.poster_path);
-    } else {
-      detailImage.setAttribute('src', NoImage);
-    }
+    // if (movieData.poster_path) {
+    //   detailImage.setAttribute('src', POSTER_BASE_URL + movieData.poster_path);
+    // } else {
+    //   detailImage.setAttribute('src', NoImage);
+    // }
 
     detailBox.append(detailImage);
 
@@ -44,7 +51,8 @@ class MovieDetailModal extends Modal {
     detailGenreAndScore.classList.add('detail-genre-and-score');
 
     const detailGenre = document.createElement('p');
-    detailGenre.textContent = movieData.genres.map(genre => genre.name).join(', ');
+    detailGenre.classList.add('detail-genre');
+    // detailGenre.textContent = movieData.genres.map(genre => genre.name).join(', ');
     detailGenreAndScore.append(detailGenre);
 
     const detailScoreBox = document.createElement('div');
@@ -55,7 +63,8 @@ class MovieDetailModal extends Modal {
     detailScoreBox.append(detailScoreIcon);
 
     const detailScore = document.createElement('p');
-    detailScore.textContent = String(movieData.vote_average);
+    detailScore.classList.add('detail-score');
+    // detailScore.textContent = String(movieData.vote_average);
     detailScoreBox.append(detailScore);
 
     detailGenreAndScore.append(detailScoreBox);
@@ -63,7 +72,7 @@ class MovieDetailModal extends Modal {
 
     const detailDescription = document.createElement('p');
     detailDescription.classList.add('detail-description');
-    detailDescription.textContent = movieData.overview;
+    // detailDescription.textContent = movieData.overview;
 
     detailPropertyBox.append(detailGenreAndScore);
     detailPropertyBox.append(detailDescription);
@@ -79,11 +88,11 @@ class MovieDetailModal extends Modal {
     // const detailRateStarBox = document.createElement('div');
 
     const detailRateStar = document.createElement('p');
-    detailRateStar.textContent = '★★★★★';
+    // detailRateStar.textContent = '★★★★★';
     detailRateBox.append(detailRateStar);
 
     const detailRateScore = document.createElement('p');
-    detailRateScore.textContent = '10 명작이에요';
+    // detailRateScore.textContent = '10 명작이에요';
     detailRateBox.append(detailRateScore);
 
     detailInfo.append(detailPropertyBox);
@@ -91,8 +100,51 @@ class MovieDetailModal extends Modal {
 
     detailBox.append(detailInfo);
 
-    movieDetailModalLayout.append(detailTitle);
-    movieDetailModalLayout.append(detailBox);
+    container.append(detailTitle);
+    container.append(detailBox);
+
+    this.modal.appendChild(container);
+  }
+
+  mount() {
+    console.log('hi');
+    const target = document.querySelector('main');
+    if (!target) return;
+    target.append(this.modal);
+  }
+
+  rerender(movieData: MovieDetailAPI) {
+    const detailTitle = this.modal.querySelector('.detail-title');
+    const detailImage = this.modal.querySelector('.detail-image');
+    const detailGenre = this.modal.querySelector('.detail-genre');
+    const detailScore = this.modal.querySelector('.detail-score');
+    const detailDescription = this.modal.querySelector('.detail-description');
+
+    if (detailTitle) detailTitle.textContent = movieData.title;
+    if (detailImage) {
+      if (movieData.poster_path) {
+        detailImage.setAttribute('src', POSTER_BASE_URL + movieData.poster_path);
+      } else {
+        detailImage.setAttribute('src', NoImage);
+      }
+    }
+    if (detailGenre) detailGenre.textContent = movieData.genres.map(genre => genre.name).join(', ');
+    if (detailScore) detailScore.textContent = String(movieData.vote_average);
+    if (detailDescription) detailDescription.textContent = movieData.overview;
+  }
+
+  setEvents() {
+    const backdrop = this.modal.querySelector('.modal-backdrop');
+    if (!backdrop) return;
+    backdrop.addEventListener('click', () => this.toggle());
+  }
+
+  toggle() {
+    if (this.modal.classList.contains('modal--open')) {
+      this.modal.classList.remove('modal--open');
+    } else {
+      this.modal.classList.add('modal--open');
+    }
   }
 }
 
