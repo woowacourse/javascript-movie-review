d558e933a1c950fe0e1f3f14a9a6c69136a59a60
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-31 22:31:50 +0900

    feat: 별점 Rate[] 타입을 보장

--- src/model/RateDataStateStore.ts
+++ src/model/RateDataStateStore.ts
@@ -2,18 +2,21 @@ import LocalStorageService from "../service/localStorageService";
 import { Rate } from "../type/rate";
 
 class RateDataStateStore {
-  #rateList: Rate[] | undefined;
+  #rateList: Rate[];
 
   constructor() {
+    if (!LocalStorageService.getData("rateList")) {
+      LocalStorageService.setData("rateList", []);
+    }
     this.#rateList = LocalStorageService.getData("rateList");
   }
 
   setRateList() {
-    if (this.#rateList) LocalStorageService.setData("rateList", this.#rateList);
+    LocalStorageService.setData("rateList", this.#rateList);
   }
 
   setNewRate(newRate: Rate) {
-    const isExist = this.#rateList?.find(
+    const isExist = this.#rateList.find(
       (rate) => rate.movieId === newRate.movieId,
     );
 
@@ -25,22 +28,19 @@ class RateDataStateStore {
   }
 
   addNewRate(newRate: Rate) {
-    this.#rateList?.push(newRate);
+    this.#rateList.push(newRate);
     this.setRateList();
   }
 
   updateRateList(newRate: Rate) {
-    const rateIndex = this.#rateList?.findIndex(
+    const rateIndex = this.#rateList.findIndex(
       (rate) => rate.movieId === newRate.movieId,
     );
-
-    if (rateIndex === undefined) return;
-    if (this.#rateList) this.#rateList[rateIndex].rate = newRate.rate;
+    this.#rateList[rateIndex].rate = newRate.rate;
     this.setRateList();
   }
 
   getOneRate(movieId: number) {
-    if (!this.#rateList) return undefined;
     return this.#rateList.find((rate) => rate.movieId === movieId);
   }
 }
