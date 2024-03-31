e0cf641d0c4351d74c6babb75894128e5db77487
simhorim <hori5020@gmail.com>
2024-03-31 18:10:18 +0900

    feat(HandleStarOnClickProps): 인터페이스 생성

---
+++ src/interfaces/HandleStarOnClickProps.ts
@@ -0,0 +1,10 @@
+import DetailMovieData from './DetailMovieData';
+
+interface HandleStarOnClickProps {
+  detailMovieData: DetailMovieData;
+  index: number;
+  star: HTMLElement;
+  starsContainer: HTMLElement;
+}
+
+export default HandleStarOnClickProps;
