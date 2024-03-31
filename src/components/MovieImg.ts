fdc09ad344f88ceb9393e1e1475a393aeeb0a7b9
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-31 22:23:11 +0900

    chore: 함수명 변경

--- src/components/MovieImg.ts
+++ src/components/MovieImg.ts
@@ -4,7 +4,7 @@ import { createElementWithAttribute } from "../utils";
 
 const POSTER_SIZE = "w500";
 
-function SkeletonImg(className: string) {
+function createSkeletonImg(className: string) {
   const $skeletonImg = createElementWithAttribute("div", {
     class: `${className} skeleton`,
   });
@@ -14,7 +14,7 @@ function SkeletonImg(className: string) {
 const imgSrc = (path: string | null) =>
   path === null ? noImg : IMAGE_URL + POSTER_SIZE + path;
 
-function PosterImg(posterPath: string, title: string, className: string) {
+function createPosterImg(posterPath: string, title: string, className: string) {
   const $img = createElementWithAttribute("img", {
     class: className,
     src: imgSrc(posterPath),
@@ -26,9 +26,9 @@ function PosterImg(posterPath: string, title: string, className: string) {
 }
 
 const MovieImg = (posterPath: string, title: string, className: string) => {
-  const skeletonUI = SkeletonImg(className);
+  const skeletonUI = createSkeletonImg(className);
 
-  const imgElement = PosterImg(posterPath, title, className);
+  const imgElement = createPosterImg(posterPath, title, className);
 
   imgElement.addEventListener("load", () => {
     skeletonUI.style.display = "none";
