a6b518a113f7b0255739b058f4822e3cb8744d41
simhorim <hori5020@gmail.com>
2024-03-31 18:09:36 +0900

    feat(MyRatingContainer): 별점 동적 생성 기능 구현

--- src/components/MovieItem/MyRatingContainer.ts
+++ src/components/MovieItem/MyRatingContainer.ts
@@ -1,13 +1,17 @@
 import { MY_RATING } from '../../constants/INFORMATION';
+import UserMoviesStorage from '../../domain/UserMoviesStorage/UserMoviesStorage';
 import DetailMovieData from '../../interfaces/DetailMovieData';
 import { starEmpty, starFilled } from '../../resources';
+import UserVote from '../../typeAliases/UserVote';
 import StarImg from './StarImg';
+import HandleStarOnClickProps from '../../interfaces/HandleStarOnClickProps';
+import ChangeMyRatingContentProps from '../../interfaces/ChangeMyRatingContentProps';
 
 const MyRatingContainer = {
-  create() {
+  create(detailMovieData: DetailMovieData) {
     const myRatingContainer = document.createElement('div');
 
-    const myRatingContent = this.createMyRatingContent();
+    const myRatingContent = this.createMyRatingContent(detailMovieData);
 
     myRatingContainer.classList.add('movie-item-detail_my-rating-container');
 
@@ -16,13 +20,13 @@ const MyRatingContainer = {
     return myRatingContainer;
   },
 
-  createMyRatingContent() {
+  createMyRatingContent(detailMovieData: DetailMovieData) {
     const myRatingContent = document.createElement('div');
 
     const title = this.createTitle();
-    const starsContainer = this.createStarsContainer();
-    const score = this.createScore();
-    const ment = this.createMent();
+    const starsContainer = this.createStarsContainer(detailMovieData);
+    const score = this.createScore(detailMovieData);
+    const ment = this.createMent(detailMovieData);
 
     myRatingContent.classList.add('movie-item-detail_my-rating-content');
 
@@ -42,33 +46,98 @@ const MyRatingContainer = {
     return title;
   },
 
-  createStarsContainer() {
+  createStarsContainer(detailMovieData: DetailMovieData) {
     const starsContainer = document.createElement('div');
 
     starsContainer.classList.add('movie-item-detail_my-rating-stars');
 
-    Array.from({ length: MY_RATING.starCount }).forEach(() => starsContainer.appendChild(StarImg.create(starEmpty)));
-
-    const movieItemDetailMyRatingStars = starsContainer.querySelectorAll('img');
+    this.createStars(detailMovieData, starsContainer);
 
     return starsContainer;
   },
-  // 별들을 queryselector로 부르고
-  // foreach를 통해 해당 부분에 클릭 이벤트를 달고 인덱스를 넘겨준다
-  // 예를 들어 3번째를 클릭하면
 
-  createScore() {
+  createStars(detailMovieData: DetailMovieData, starsContainer: HTMLElement) {
+    starsContainer.replaceChildren();
+
+    const userMovie = UserMoviesStorage.getMatchedUserMovie(detailMovieData);
+
+    if (userMovie) {
+      const starFilledCount = Number(userMovie.user_vote) / 2;
+      const starEmptyCount = MY_RATING.starCount - starFilledCount;
+
+      Array.from({ length: starFilledCount }).forEach(() => starsContainer.appendChild(StarImg.create(starFilled)));
+      Array.from({ length: starEmptyCount }).forEach(() => starsContainer.appendChild(StarImg.create(starEmpty)));
+    } else {
+      Array.from({ length: MY_RATING.starCount }).forEach(() => starsContainer.appendChild(StarImg.create(starEmpty)));
+    }
+
+    this.setStarHandler(detailMovieData, starsContainer);
+  },
+
+  setStarHandler(detailMovieData: DetailMovieData, starsContainer: HTMLElement) {
+    const stars = starsContainer.querySelectorAll('img');
+
+    stars.forEach((star, index) => {
+      star.classList.add('movie-item-detail_my-rating-star');
+
+      star.addEventListener('click', () => {
+        this.handleStarOnClick({ detailMovieData, index, star, starsContainer });
+      });
+    });
+  },
+
+  handleStarOnClick({ detailMovieData, index, star, starsContainer }: HandleStarOnClickProps) {
+    this.addNewUserMovie(detailMovieData);
+    this.updateUserVote(detailMovieData, index);
+    this.changeMyRatingContent({ detailMovieData, star, starsContainer });
+  },
+
+  addNewUserMovie(detailMovieData: DetailMovieData) {
+    if (!UserMoviesStorage.getMatchedUserMovie(detailMovieData)) UserMoviesStorage.addUserMovie(detailMovieData);
+  },
+
+  updateUserVote(detailMovieData: DetailMovieData, index: number) {
+    const userMovie = UserMoviesStorage.getMatchedUserMovie(detailMovieData);
+
+    if (userMovie) {
+      userMovie.user_vote = String((index + 1) * 2) as UserVote;
+
+      UserMoviesStorage.setMatchedUserMovie(userMovie);
+    }
+  },
+
+  changeMyRatingContent({ detailMovieData, star, starsContainer }: ChangeMyRatingContentProps) {
+    const updatedUserMovie = UserMoviesStorage.getMatchedUserMovie(detailMovieData);
+
+    if (updatedUserMovie) {
+      const score = star.parentNode?.nextSibling;
+      if (score) score.textContent = updatedUserMovie.user_vote;
+
+      const ment = star.parentNode?.nextSibling?.nextSibling;
+      if (ment) ment.textContent = MY_RATING.ment[updatedUserMovie.user_vote];
+
+      this.createStars(detailMovieData, starsContainer);
+    }
+  },
+
+  createScore(detailMovieData: DetailMovieData) {
     const score = document.createElement('p');
 
-    score.textContent = MY_RATING.score[0];
+    const userMovie = UserMoviesStorage.getMatchedUserMovie(detailMovieData);
+
+    if (userMovie) score.textContent = userMovie.user_vote;
+    else score.textContent = MY_RATING.default;
 
     return score;
   },
 
-  createMent() {
+  createMent(detailMovieData: DetailMovieData) {
     const ment = document.createElement('p');
 
-    ment.textContent = MY_RATING.ment[0];
+    const userMovie = UserMoviesStorage.getMatchedUserMovie(detailMovieData);
+
+    if (userMovie) ment.textContent = MY_RATING.ment[userMovie.user_vote];
+    else ment.textContent = MY_RATING.ment[MY_RATING.default];
 
     return ment;
   },
