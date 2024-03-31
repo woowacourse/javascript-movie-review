c0c646b674b958a9297a35d66f9e4dfd4c7d9d5e
ss0526100 <ss0526100@gmail.com>
2024-03-31 21:54:34 +0900

    feat: 스토리지 인터페이스 구현

---
+++ src/storage/StorageInterface.ts
@@ -0,0 +1,27 @@
+interface MovieInfo {
+  userRating: number;
+}
+
+class StorageInterface {
+  #storage = localStorage;
+
+  getMovieInfo(movieId: string) {
+    const movieInfos = this.#getMovieInfos();
+
+    return movieInfos[movieId];
+  }
+
+  setMovieInfo(movieId: string, movieInfo: MovieInfo) {
+    const movieInfos = this.#getMovieInfos();
+    console.log(movieInfos);
+
+    movieInfos[movieId] = movieInfo;
+    this.#storage.setItem("movieInfos", JSON.stringify(movieInfos));
+  }
+
+  #getMovieInfos() {
+    return JSON.parse(this.#storage.getItem("movieInfos") ?? "{}");
+  }
+}
+
+export default StorageInterface;
