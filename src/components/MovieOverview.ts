dfb13aea3d77f45935dc9eaf1daf7ff1f9641b93
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-31 22:24:20 +0900

    feat: overview 없을 시 처리 조건 확장

--- src/components/MovieOverview.ts
+++ src/components/MovieOverview.ts
@@ -3,7 +3,7 @@ import { createElementWithAttribute } from "../utils";
 const MovieOverview = (overview: string, className: string) => {
   const $overview = createElementWithAttribute("p", { class: className });
 
-  if (overview.length !== 0) {
+  if (overview) {
     $overview.textContent = overview;
   } else {
     $overview.textContent = "영화 정보가 등록되어 있지 않습니다.";
