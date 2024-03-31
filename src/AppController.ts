7a37cb8ae1a24dc8f192ade57880b49c64f46540
jaeml06 <jaeml0630@gmail.com>
2024-03-31 22:42:39 +0900

    refactor: querySelectr을 Dom 유틸로 변경

--- src/AppController.ts
+++ src/AppController.ts
@@ -1,5 +1,6 @@
 import { createHeader } from './components/header/header';
 import { MovieListWrapper } from './components/movieListWrapper/MovieListWrapper';
+import { Dom } from './utils/Dom';
 
 class AppController {
   #currentView;
@@ -16,13 +17,12 @@ class AppController {
     };
 
     const inputSubmitHandler = async (inputValue: string) => {
-      const input: HTMLInputElement | null = document.querySelector('header .search-box > input');
-      if (!input) return;
+      const input: HTMLInputElement = Dom.getElement(document, 'header .search-box > input');
       if (input.offsetWidth === 0) {
-        document.querySelector('header h1')?.classList.add('clicked-logo');
-        document.querySelector('header > .search-box')?.classList.add('clicked-form');
-        document.querySelector('header .search-box > input')?.classList.add('clicked-input');
-        document.querySelector('header')?.classList.add('clicked-header');
+        Dom.getElement(document, 'header h1').classList.add('clicked-logo');
+        Dom.getElement(document, 'header > .search-box').classList.add('clicked-form');
+        Dom.getElement(document, 'header .search-box > input').classList.add('clicked-input');
+        Dom.getElement(document, 'header').classList.add('clicked-header');
         return;
       }
       if (inputValue === '') return;
