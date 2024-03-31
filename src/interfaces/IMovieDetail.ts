5306ebeb7746fbd332bd98aab338e3130a45fa7a
greetings1012 <ahnqwerty@gmail.com>
2024-03-31 20:59:53 +0900

    refactor(IMovieDetail): 영화 상세 정보 fetch를 위한 인터페이스 데이터 양식에 맞게 수정

--- src/interfaces/IMovieDetail.ts
+++ src/interfaces/IMovieDetail.ts
@@ -1,9 +1,9 @@
 interface IMovieDetail {
   adult: boolean;
-  backdrop_path: any;
-  belongs_to_collection: any;
+  backdrop_path: string;
+  belongs_to_collection: BelongsToCollection;
   budget: number;
-  genres: any[];
+  genres: Genre[];
   homepage: string;
   id: number;
   imdb_id: string;
@@ -11,9 +11,9 @@ interface IMovieDetail {
   original_title: string;
   overview: string;
   popularity: number;
-  poster_path: any;
-  production_companies: any[];
-  production_countries: any[];
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
   release_date: string;
   revenue: number;
   runtime: number;
@@ -26,10 +26,33 @@ interface IMovieDetail {
   vote_count: number;
 }
 
+interface BelongsToCollection {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
 interface SpokenLanguage {
   english_name: string;
   iso_639_1: string;
   name: string;
 }
-
-export { IMovieDetail };
+export { IMovieDetail, BelongsToCollection, SpokenLanguage, Genre, ProductionCompany, ProductionCountry };
