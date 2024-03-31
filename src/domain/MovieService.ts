3f4eba06299db4a3c00c4367ec617bd100adc958
pakxe <pigkill40@naver.com>
2024-04-01 04:00:27 +0900

    feat: api를 받아서 간접적으로 데이터를 불러오도록 수정

--- src/domain/MovieService.ts
+++ src/domain/MovieService.ts
@@ -1,55 +1,22 @@
-import { MOVIE_SEARCH_URL, POPULAR_MOVIES_URL } from '../constant/config';
-import fetchDataFromUrl from '../util/fetchDataFromUrl';
-import Movie from './Movie';
-import getEnvVariable from '../util/getEnvVariable';
+import { MovieServiceType } from './MovieServiceType';
 
-interface MovieListData {
-  total_pages: number;
-  results: { id: number; title: string; poster_path: string; vote_average: number; overview: string }[];
-}
+class MovieService implements MovieServiceType {
+  private readonly api;
 
-interface MoviePageData extends MovieListData {
-  pageNumber: number;
-}
-
-class MovieService {
-  async fetchPopularMovieList(pageNumber: number) {
-    const { total_pages, results } = await fetchDataFromUrl(POPULAR_MOVIES_URL, {
-      api_key: getEnvVariable('API_KEY'),
-      language: 'ko-KR',
-      page: pageNumber,
-    });
-
-    return this.createMoviePageData({ total_pages, results, pageNumber });
+  constructor(api: MovieServiceType) {
+    this.api = api;
   }
 
-  async fetchSearchResult({ query, pageNumber }: { query: string; pageNumber: number }) {
-    const { total_pages, results } = await fetchDataFromUrl(MOVIE_SEARCH_URL, {
-      api_key: getEnvVariable('API_KEY'),
-      language: 'ko-KR',
-      query,
-      page: pageNumber,
-    });
-
-    return this.createMoviePageData({ total_pages, results, pageNumber });
+  async fetchPopularMovieList(currentPage: number) {
+    return await this.api.fetchPopularMovieList(currentPage);
   }
 
-  // 중간 도메인 객체
-  createMoviePageData({ total_pages, results, pageNumber }: MoviePageData) {
-    const movieList: Movie[] = results.map(
-      (result) =>
-        new Movie({
-          id: result.id,
-          title: result.title,
-          posterPath: result.poster_path,
-          voteAverage: result.vote_average,
-        }),
-    );
+  async fetchSearchResult(props: { query: string; currentPage: number }) {
+    return await this.api.fetchSearchResult(props);
+  }
 
-    return {
-      hasNextPage: total_pages > pageNumber,
-      movieList,
-    };
+  async fetchMovieDetail(movieId: number) {
+    return await this.api.fetchMovieDetail(movieId);
   }
 }
 
