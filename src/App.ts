51bc430ec8768f279c33378639c361d90e57c4c7
anttiey <hanuuny@gmail.com>
2024-03-31 17:13:27 +0900

    feat(App): 위로 스크롤 기능 추가

--- src/App.ts
+++ src/App.ts
@@ -4,12 +4,14 @@ import MovieReviewBody from './components/movie/MovieReviewBody/MovieReviewBody'
 import ErrorModal from './components/movie/ErrorModal/ErrorModal';
 import { querySelector } from './utils/dom/selector';
 import { createElement } from './utils/dom/createElement/createElement';
+import { UpButton } from './assets';
 
 class App extends Component {
   errorModal: ErrorModal | undefined;
 
   protected render() {
     this.$element.append(this.createComponent());
+    this.$element.insertAdjacentHTML('afterend', this.createFloatingButton());
     this.errorModal = new ErrorModal(this.$element);
   }
 
@@ -26,6 +28,19 @@ class App extends Component {
     return $app;
   }
 
+  private createFloatingButton() {
+    return /* html */ `
+      <button id="floating-button" class="floating-button">
+        <img src=${UpButton} alt="위로 스크롤" />
+      </button>
+    `;
+  }
+
+  protected setEvent(): void {
+    const $floatingButton = querySelector<HTMLButtonElement>('#floating-button');
+    $floatingButton.addEventListener('click', this.scrollToTop);
+  }
+
   private updateMovieReviewBody(movieName: string) {
     const $main = querySelector<HTMLElement>('main', this.$element);
     $main.innerHTML = '';
@@ -37,6 +52,10 @@ class App extends Component {
     if (error instanceof Error) {
       this.errorModal?.openModal();
     }
+  }
+
+  private scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
 
