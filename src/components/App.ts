801b5f85b4289ae4e63da9d337dce889609ae664
Kim Da Eun <midekuna@gmail.com>
2024-03-31 21:07:20 +0900

    chore: App 컴포넌트에 Modal 컴포넌트 부착

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,3 +1,4 @@
+import Modal from './common/modal/Modal';
 import Header from './Header/Header';
 import Main from './Main/Main';
 
@@ -5,8 +6,11 @@ function App() {
   return {
     render: () => {
       const $app = document.querySelector('#app');
-      $app?.appendChild(Header().render());
-      $app?.appendChild(Main().render());
+      if ($app) {
+        $app.appendChild(Header().render());
+        $app.appendChild(Main().render());
+        $app.appendChild(Modal().render());
+      }
     },
   };
 }
