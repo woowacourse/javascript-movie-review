b62c0f31e8e914b5f5ec8128b6f4a3607b278703
jinsil <jinsil378900@gmail.com>
2024-03-31 20:06:35 +0900

    feat: 다시 0점도 될 수 있게 하기

--- src/components/movieDetailModal/MovieDetailModal.ts
+++ src/components/movieDetailModal/MovieDetailModal.ts
@@ -22,15 +22,15 @@ class MovieDetailModal extends Modal {
 
   async renderMovieDetail() {
     this.#movie = await fetchMovieDetail(this.#movieId);
-    this.#movie.userVote = this.setUserVote();
-    const detailContent = new MovieDetailContent(this.#movie);
-    const detailMovieContent = detailContent.render();
-    this.setContent(detailMovieContent);
+    this.#movie.userVote = this.getUserVote();
+    const movieDetailContent = new MovieDetailContent(this.#movie);
+    this.setContent(movieDetailContent.render());
+
     this.manageCloseModal();
     this.manageUserVote();
   }
 
-  setUserVote() {
+  getUserVote() {
     const existingMovie = this.#userMovies.find(item => item.id === this.#movie.id);
     return existingMovie ? existingMovie.userVote : 0;
   }
@@ -58,9 +58,9 @@ class MovieDetailModal extends Modal {
       if (target instanceof HTMLImageElement) {
         const clickedIndex = Array.from(starsContainer.children).indexOf(target);
         const userVote = (clickedIndex + 1) * 2;
-        this.#movie.userVote = userVote;
+        this.#movie.userVote = this.#movie.userVote === userVote ? 0 : userVote;
         this.renderUserVote(starsContainer, voteNumber, voteText);
-        this.addUserMovie(userVote);
+        this.updateUserMovie();
       }
     });
   }
@@ -75,12 +75,12 @@ class MovieDetailModal extends Modal {
     voteText.textContent = RATING_MESSAGES[userVote];
   }
 
-  addUserMovie(userVote: number) {
+  updateUserMovie() {
     const existingMovieIndex = this.#userMovies.findIndex(item => item.id === this.#movie.id);
     if (existingMovieIndex !== -1) {
-      this.#userMovies[existingMovieIndex].userVote = userVote;
+      this.#userMovies[existingMovieIndex].userVote = this.#movie.userVote;
     } else {
-      const newUserMovie: Movie = { ...this.#movie, userVote };
+      const newUserMovie: Movie = { ...this.#movie, userVote: this.#movie.userVote };
       this.#userMovies.push(newUserMovie);
     }
 
