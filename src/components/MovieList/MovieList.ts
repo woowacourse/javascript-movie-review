bd77bc903fac801385ced10ef12b85524571c6e5
chysis <chysiss@naver.com>
2024-04-01 17:40:48 +0900

    fix: 검색 결과 없는 경우 UI 출력

--- src/components/MovieList/MovieList.ts
+++ src/components/MovieList/MovieList.ts
@@ -1,15 +1,11 @@
 import MovieItem from '../MovieItem/MovieItem';
+import MovieDetailModal from '../Modal/MovieDetailModal';
+import Skeleton from '../Skeleton/Skeleton';
+import { NotFound } from '../NotFound/NotFound';
 import { Movie } from './../../types/movie';
 import { PAGE_SIZE } from '../../consts/common';
-import '../MovieList/MovieList.css';
-import MovieDetailModal from '../Modal/MovieDetailModal';
 import { MovieDetailAPI } from '../../domain/services/API.type';
-import Skeleton from '../Skeleton/Skeleton';
-
-interface Props {
-  movieList: Movie[];
-  isLoading: boolean;
-}
+import '../MovieList/MovieList.css';
 
 class MovieList {
   movieListBox = document.createElement('ul');
@@ -40,6 +36,10 @@ class MovieList {
   }
 
   renderMovieList(movieList: Movie[]) {
+    if (!movieList.length) {
+      return NotFound();
+    }
+
     const fragment = new DocumentFragment();
     movieList.forEach(movie => {
       const movieItem = new MovieItem();
