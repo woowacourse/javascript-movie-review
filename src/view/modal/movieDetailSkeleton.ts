ee5f654a3af73b00eb0ed77e7f6ac6c57851ff8a
Jeongwoo Park <wahoo9040@naver.com>
2024-04-01 09:45:09 +0900

    feat: 스켈레톤 화면 비율 수정

--- src/view/modal/movieDetailSkeleton.ts
+++ src/view/modal/movieDetailSkeleton.ts
@@ -1,27 +1,34 @@
-function createMovieDetailSkeleton() {
-  const skeleton = document.createElement('div');
-  skeleton.innerHTML = `<div class="movie-modal">
-    <div class="movie-detail-header flex-XY-aligned">
+function createImageSkeleton() {
+  const div = document.createElement('div');
+  div.className = 'movie-detail-header flex-XY-aligned';
+  div.innerHTML = `
       <div class="custom-skeleton h-20 w-100"></div>
       <button class="flex-XY-aligned close-button transition-background">
         <img src="/009bf62bd786c8837153.svg" alt="close-button" />
-      </button>
-    </div>
-    <div class="flex-XY-aligned padding-32 gap-16">
-      <div>
-        <img
-          class="h-433 w-292 custom-skeleton mobile-display-none"
-        />
-      </div>
-      <div class="movie-detail-info w-396">
-        <div class="custom-skeleton h-20 w-100"></div>
-        <div>
-          <div class="custom-skeleton h-20 w-396"></div>
-        </div>
-        <div class="flex-Y-center user-rate-box custom-skeleton"></div>
-      </div>
-    </div>
-  </div>`;
+      </button>`;
+  return div;
+}
+
+function createInfoSkeleton() {
+  const div = document.createElement('div');
+  div.className = 'flex-XY-aligned padding-32 gap-16';
+  div.innerHTML = ` 
+  <div>
+    <img class="h-433 w-292 custom-skeleton mobile-display-none"/>
+  </div>
+  <div class="movie-detail-info w-1-ratio h-433">
+    <div class="custom-skeleton h-20 w-1-ratio"></div>
+    <div class="custom-skeleton h-20 w-1-ratio"></div>
+    <div class="flex-Y-center user-rate-box custom-skeleton"></div>
+  </div>
+  `;
+  return div;
+}
+
+function createMovieDetailSkeleton() {
+  const skeleton = document.createElement('div');
+  skeleton.className = 'movie-modal';
+  skeleton.append(createImageSkeleton(), createInfoSkeleton());
   return skeleton;
 }
 
