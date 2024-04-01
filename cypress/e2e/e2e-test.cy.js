f2cb6aefc245a619d0001165dab41b9c39da2b59
pakxe <pigkill40@naver.com>
2024-04-01 17:12:04 +0900

    test: e2e 테스트 수정

--- cypress/e2e/e2e-test.cy.js
+++ cypress/e2e/e2e-test.cy.js
@@ -8,18 +8,18 @@ describe('영화 e2e 테스트', () => {
     cy.get('h2').contains('검색 결과').should('exist');
 
     cy.get('.logo').click();
-    cy.contains('지금 인기 있는 영화').should('exist');
+    cy.contains('지금 인기있는 영화').should('exist');
     cy.get('.search-box').get('input').should('not.have.value');
   });
 
-  it('더보기를 누르면 영화 리스트를 더 불러온다.', () => {
+  it('스크롤을 제일 아래로 내리면 새로운 영화 목록을 보여준다.', () => {
     cy.visit('/');
 
-    cy.contains('더 보기').click();
+    cy.window().scrollTo('bottom');
     cy.get('ul.item-list > li').should('have.length.greaterThan', 20);
   });
 
-  it('더 불러올 영화 목록이 없으면 더 보기 버튼을 띄우지 않는다.', () => {
+  it('더 불러올 영화 목록이 없으면 무한스크롤을 멈춘다.', () => {
     cy.intercept(
       {
         method: 'GET',
@@ -30,7 +30,7 @@ describe('영화 e2e 테스트', () => {
 
     cy.visit('/');
 
-    cy.contains('더 보기').should('have.css', 'visibility', 'hidden');
+    cy.get('.loader').should('not.exist');
   });
 
   it('키워드로 검색하면 검색 페이지로 전환된다.', () => {
@@ -64,7 +64,7 @@ describe('영화 e2e 테스트', () => {
     cy.get('.search-box').get('input').type('쿵푸');
     cy.get('.search-box').submit();
 
-    cy.get('.empty-search-result').contains('검색 결과가 없습니다.').should('exist');
+    cy.contains('결과가 존재하지 않습니다.').should('exist');
   });
 
   it('검색어를 입력하지 않으면 검색어가 없다고 안내한다.', () => {
