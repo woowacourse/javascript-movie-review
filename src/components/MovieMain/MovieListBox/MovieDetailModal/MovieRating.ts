c9380ee99631c4be21c09f22d41ad5e93b3603a9
hain-tain <hain7ce@gmail.com>
2024-04-01 17:07:20 +0900

    refactor(MovieRating): 별점 text 및 number 계산해주는 함수 constants > rating 파일로 분리

--- src/components/MovieMain/MovieListBox/MovieDetailModal/MovieRating.ts
+++ src/components/MovieMain/MovieListBox/MovieDetailModal/MovieRating.ts
@@ -1,3 +1,5 @@
+import { getRatingNumber, getRatingText } from "../../../../constants/rating";
+
 import createElement from "../../../utils/createElement";
 import generateStarRangeInput from "../../../common/generateStarRangeInput";
 
@@ -76,28 +78,11 @@ class MovieRating {
   }
 
   private ratingNumber(rating: number) {
-    return String(rating * 2);
+    return String(getRatingNumber(rating));
   }
 
   private ratingText(rating: number) {
-    switch (rating) {
-      case 1:
-        return "최악이예요";
-      case 2:
-        return "별로예요";
-
-      case 3:
-        return "보통이에요";
-
-      case 4:
-        return "재미있어요";
-
-      case 5:
-        return "명작이에요";
-
-      default:
-        return "별점을 매겨보세요";
-    }
+    return getRatingText(rating);
   }
 }
 
--- src/constants/rating.ts
+++ src/constants/rating.ts
@@ -1,2 +1,27 @@
 export const DEFAULT_RATING = 0;
 export const MAX_RATING = 5;
+
+export const getRatingNumber = (rating: number) => {
+  return rating * 2;
+};
+
+export const getRatingText = (rating: number) => {
+  switch (rating) {
+    case 1:
+      return "최악이예요";
+    case 2:
+      return "별로예요";
+
+    case 3:
+      return "보통이에요";
+
+    case 4:
+      return "재미있어요";
+
+    case 5:
+      return "명작이에요";
+
+    default:
+      return "별점을 매겨보세요";
+  }
+};
