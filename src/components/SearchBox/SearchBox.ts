fa1efdf848ad22c8fc80fe7ed81d548b14f12440
chysis <chysiss@naver.com>
2024-04-01 17:40:18 +0900

    style: 반응형 header UI 적용

--- src/components/SearchBox/SearchBox.ts
+++ src/components/SearchBox/SearchBox.ts
@@ -1,23 +1,33 @@
 import { URL } from '../../consts/common';
 import { setUrlParams } from '../../utils/queryString';
 import '../SearchBox/SearchBox.css';
+import Toast from '../Toast/Toast';
+
+interface Props {
+  headerImage: HTMLImageElement;
+  onSearch: () => Promise<void>;
+}
 
 class SearchBox {
   searchBox = document.createElement('form');
   searchInput = document.createElement('input');
   searchButton = document.createElement('button');
+
+  headerImage;
   onSearch;
 
-  constructor({ onSearch: onSearch }: { onSearch: () => Promise<void> }) {
+  constructor({ headerImage, onSearch }: Props) {
+    this.headerImage = headerImage;
     this.onSearch = onSearch;
     this.setEvents(onSearch);
+    this.setResponsiveEvents();
   }
 
   init() {
     this.searchBox.classList.add('search-box');
+    this.searchInput.classList.add('search-input');
     this.searchInput.setAttribute('type', 'text');
     this.searchInput.setAttribute('placeholder', '검색');
-    this.searchInput.required = true;
 
     this.searchButton.classList.add('search-button');
     this.searchButton.textContent = '검색';
@@ -31,12 +41,45 @@ class SearchBox {
   setEvents(onSearch: () => void) {
     this.searchBox.addEventListener('submit', (e: Event) => {
       e.preventDefault();
+      if (this.searchInput.classList.contains('hidden')) {
+        this.headerImage.classList.add('hidden');
+        this.searchBox.classList.add('expand');
+
+        this.searchInput.classList.remove('hidden');
+
+        return;
+      }
+
+      if (this.searchInput.value === '') {
+        new Toast('검색어가 없습니다.');
+        return;
+      }
 
       setUrlParams(URL.MODE, 'search');
       setUrlParams(URL.QUERY, this.searchInput.value);
       setUrlParams(URL.PAGES, '1');
 
       onSearch();
+    });
+  }
+
+  setResponsiveEvents() {
+    let debounce: NodeJS.Timeout;
+
+    window.addEventListener('resize', () => {
+      if (debounce) clearTimeout(debounce);
+
+      debounce = setTimeout(() => {
+        if (window.innerWidth < 410) {
+          this.searchInput.classList.add('hidden');
+          this.searchBox.classList.remove('expand');
+          this.searchButton.classList.add('alone');
+        } else {
+          this.searchInput.classList.remove('hidden');
+          this.headerImage.classList.remove('hidden');
+          this.searchBox.classList.remove('expand');
+        }
+      }, 100);
     });
   }
 }
