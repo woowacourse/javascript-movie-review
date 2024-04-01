929a9f2a767eef6c06678b2ac3b4ddcfa2abab8f
jinsil <jinsil378900@gmail.com>
2024-04-01 15:24:20 +0900

    fix: header 버튼 두 번 눌리는 오류 해결

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -6,75 +6,86 @@ interface Props {
 }
 
 const Header = ({ onLogoClick, inputSubmitHandle }: Props) => {
-  const header = render({ onLogoClick, inputSubmitHandle });
-  return header;
-};
+  const render = () => {
+    const header = document.createElement('header');
+    header.className = 'header';
 
-const render = ({ onLogoClick, inputSubmitHandle }: Props) => {
-  const header = document.createElement('header');
-  header.className = 'header';
+    const logo = document.createElement('h1');
+    logo.className = 'logo';
+    const logoImage = document.createElement('img');
+    logoImage.src = logoPng;
+    logoImage.alt = 'MovieList 로고';
+    logo.appendChild(logoImage);
 
-  const logo = document.createElement('h1');
-  logo.className = 'logo';
-  const logoImage = document.createElement('img');
-  logoImage.src = logoPng;
-  logoImage.alt = 'MovieList 로고';
-  logo.appendChild(logoImage);
+    const searchBox = document.createElement('form');
+    searchBox.className = 'search-box closed';
 
-  const searchBox = document.createElement('form');
-  searchBox.className = 'search-box closed';
+    const searchInput = document.createElement('input');
+    searchInput.className = 'search-input closed';
+    searchInput.placeholder = '검색';
 
-  const searchInput = document.createElement('input');
-  searchInput.className = 'search-input closed';
-  searchInput.placeholder = '검색';
+    const searchButton = document.createElement('button');
+    searchButton.type = 'submit';
+    searchButton.className = 'search-button';
+    searchButton.textContent = '검색';
 
-  const searchButton = document.createElement('button');
-  searchButton.type = 'submit';
-  searchButton.className = 'search-button';
-  searchButton.textContent = '검색';
+    searchBox.append(searchInput, searchButton);
 
-  searchBox.append(searchInput, searchButton);
+    header.append(logo, searchBox);
 
-  header.append(logo, searchBox);
+    header.onclick = event => {
+      if ((event.target as HTMLElement).tagName === 'HEADER') {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    };
 
-  header.onclick = event => {
-    if ((event.target as HTMLElement).tagName === 'HEADER') {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+    if (onLogoClick) {
+      logo.addEventListener('click', () => {
+        searchInput.value = '';
+        onLogoClick();
+      });
     }
-  };
 
-  if (onLogoClick) {
-    logo.addEventListener('click', () => {
-      searchInput.value = '';
-      onLogoClick();
-    });
-  }
+    window.addEventListener('resize', () => {
+      const screenWidth = window.innerWidth;
 
-  if (inputSubmitHandle) {
-    searchBox.addEventListener('submit', event => {
-      event.preventDefault();
+      if (inputSubmitHandle) {
+        searchBox.addEventListener('submit', event => {
+          event.preventDefault();
 
-      const isSearchInputClosed = searchInput.classList.contains('closed');
-      const searchInputValue = searchInput.value.trim();
+          const isSearchInputClosed = searchInput.classList.contains('closed');
+          const searchInputValue = searchInput.value.trim();
 
-      if (isSearchInputClosed) {
-        toggleElementsVisibility(false);
-      } else if (searchInputValue === '') {
-        toggleElementsVisibility(true);
-      } else {
-        inputSubmitHandle(searchInputValue);
-        toggleElementsVisibility(true);
+          if (screenWidth <= 650) {
+            if (isSearchInputClosed) {
+              toggleElementsVisibility(false);
+            } else if (searchInputValue === '') {
+              toggleElementsVisibility(true);
+            } else {
+              inputSubmitHandle(searchInputValue);
+              toggleElementsVisibility(true);
+            }
+          } else {
+            if (searchInputValue !== '') {
+              inputSubmitHandle(searchInputValue);
+            }
+          }
+        });
       }
     });
-  }
 
-  function toggleElementsVisibility(isSearchClosed: boolean) {
-    searchBox.classList.toggle('closed', isSearchClosed);
-    searchInput.classList.toggle('closed', isSearchClosed);
-    logo.classList.toggle('closed', !isSearchClosed);
-  }
+    const toggleElementsVisibility = (isSearchClosed: boolean) => {
+      searchBox.classList.toggle('closed', isSearchClosed);
+      searchInput.classList.toggle('closed', isSearchClosed);
+      logo.classList.toggle('closed', !isSearchClosed);
+    };
 
-  return header;
+    window.dispatchEvent(new Event('resize'));
+
+    return header;
+  };
+
+  return render();
 };
 
 export default Header;
