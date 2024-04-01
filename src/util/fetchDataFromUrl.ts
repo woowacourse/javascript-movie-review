969d3763ad88d8c7daf48604bca62444b4a3fc8d
pakxe <pigkill40@naver.com>
2024-04-01 14:55:47 +0900

    chore: 주석 제거

--- src/util/fetchDataFromUrl.ts
+++ src/util/fetchDataFromUrl.ts
@@ -13,8 +13,6 @@ export default async function fetchDataFromUrl<T = ResponseMovieList>(
   if (!response.ok) {
     if (response.status === 404) throw new Error('클라이언트 에러');
     else if (response.status === 500) throw new Error('서버 에러');
-    // const errorBody = await response.json(); // 오류 메시지가 JSON 형태일 경우
-    // const errorMessage = errorBody.message || '알 수 없는 에러 발생';
     else throw new Error('알 수 없는 오류 발생');
   }
 
