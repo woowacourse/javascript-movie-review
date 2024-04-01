645f07218989148c92aa3ff39d443432e7a4651d
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-04-01 15:55:25 +0900

    test(MovieList): 더보기 기능에 대한 테스트 삭제

--- cypress/e2e/MovieList.cy.ts
+++ cypress/e2e/MovieList.cy.ts
@@ -16,10 +16,4 @@ describe("영화 목록 렌더링 테스트", () => {
     cy.get("#movie-list-title").contains("인기 있는 영화").should("exist");
     cy.get("li").should("have.length", 20);
   });
-
-  it("더보기 버튼을 눌렀을 때 다음 20개의 영화 항목들을 보여준다.", () => {
-    cy.get("li").should("have.length", 20);
-    cy.get("#watch-more-button").contains("더 보기").click();
-    cy.get("li").should("have.length", 40);
-  });
 });
