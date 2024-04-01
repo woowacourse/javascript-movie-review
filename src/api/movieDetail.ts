cc06a5777535e3f60a8a0c48c1d639ad5e77aa3c
GimGayeon <kky1373@naver.com>
2024-04-01 15:11:24 +0900

    feat(api): 영화 디테일 정보 불러오기 api 기능 구현

---
+++ src/api/movieDetail.ts
@@ -0,0 +1,13 @@
+import { MovieDetailDataType } from './apiType';
+import { wrappingMovieDetailAPI } from './wrappingAPI';
+import { catchError } from './errorStatus';
+import { CONSTANT_URL, URL_LANGUAGE } from '../constant/api';
+
+export const getMovieDetail = async (movieId: number): Promise<MovieDetailDataType> => {
+  const url = `${CONSTANT_URL.movieDetail}${movieId}?${URL_LANGUAGE}`;
+  const movieData = await catchError(url);
+
+  const cleaningData = wrappingMovieDetailAPI(movieData);
+
+  return cleaningData;
+};
