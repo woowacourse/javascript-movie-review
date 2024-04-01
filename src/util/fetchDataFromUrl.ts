1d260b0a31805a4777b79939ed82df1402a3affa
pakxe <pigkill40@naver.com>
2024-04-01 14:55:37 +0900

    feat: 오류 처리를 위해 분기 추가

--- src/util/fetchDataFromUrl.ts
+++ src/util/fetchDataFromUrl.ts
@@ -1,19 +1,21 @@
-import { ResponseMovieList } from '../interface/api';
+import { ResponseMovieList } from '../api/DB/TMDBType';
 import { Params, convertObjectToURLSearchParams } from './convertObjectToURLSearchParams';
 
 // fetch 포장 함수
 export default async function fetchDataFromUrl<T = ResponseMovieList>(
   url: string,
-  query: Record<string, Params>,
+  query?: Record<string, Params>,
 ): Promise<T> {
-  const paramsObj = convertObjectToURLSearchParams(query);
+  const paramsObj = query ? convertObjectToURLSearchParams(query) : '';
   const queryUrl = url + '?' + paramsObj;
   const response = await fetch(queryUrl);
 
   if (!response.ok) {
-    const errorBody = await response.json(); // 오류 메시지가 JSON 형태일 경우
-    const errorMessage = errorBody.message || '알 수 없는 에러 발생';
-    throw new Error(`${errorMessage} 다시 요청해주세요.`);
+    if (response.status === 404) throw new Error('클라이언트 에러');
+    else if (response.status === 500) throw new Error('서버 에러');
+    // const errorBody = await response.json(); // 오류 메시지가 JSON 형태일 경우
+    // const errorMessage = errorBody.message || '알 수 없는 에러 발생';
+    else throw new Error('알 수 없는 오류 발생');
   }
 
   return await response.json();
