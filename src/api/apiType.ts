b8dcb1bd6e5592c52e18f5f352c32afc990178bb
GimGayeon <kky1373@naver.com>
2024-04-01 15:08:36 +0900

    feat(apiType): common 타입 생성

--- src/api/apiType.ts
+++ src/api/apiType.ts
@@ -1,9 +1,19 @@
-export interface MovieDataType {
+interface CommonType {
   movieId: number;
   movieTitle: string;
   voteAverage: number;
+  posterPath: string;
+}
+
+export interface MovieDataType extends CommonType {
   voteCount: number;
   popularity: number;
-  posterPath: string;
   releaseDate: string;
+}
+
+type GenresType = { id: number; name: string }[];
+
+export interface MovieDetailDataType extends CommonType {
+  genres: GenresType;
+  overview: string;
 }
