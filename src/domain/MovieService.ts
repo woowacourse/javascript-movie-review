a151f891f968dea1fbc5ddf7bb337106e5baffc1
pakxe <pigkill40@naver.com>
2024-04-01 14:56:36 +0900

    feat: 오류를 잡아 토스트를 띄우도록 구현

--- src/domain/MovieService.ts
+++ src/domain/MovieService.ts
@@ -1,22 +1,45 @@
+import TMDB from '../api/DB/TMDB';
+import renderToast from '../component/toast/toast';
 import { MovieServiceType } from './MovieServiceType';
 
+type validAPIName = 'TMDB';
+
+const API_FACTORY: Record<validAPIName, MovieServiceType> = {
+  TMDB: TMDB,
+};
 class MovieService implements MovieServiceType {
   private readonly api;
 
-  constructor(api: MovieServiceType) {
-    this.api = api;
+  constructor(apiName: validAPIName) {
+    this.api = API_FACTORY[apiName];
   }
 
   async fetchPopularMovieList(currentPage: number) {
-    return await this.api.fetchPopularMovieList(currentPage);
+    try {
+      return await this.api.fetchPopularMovieList(currentPage);
+    } catch (error) {
+      if (error instanceof Error) renderToast(error.message);
+      // TODO: null이 아닌 오류 객체를 반환
+      return null;
+    }
   }
 
   async fetchSearchResult(props: { query: string; currentPage: number }) {
-    return await this.api.fetchSearchResult(props);
+    try {
+      return await this.api.fetchSearchResult(props);
+    } catch (error) {
+      if (error instanceof Error) renderToast(error.message);
+      return null;
+    }
   }
 
   async fetchMovieDetail(movieId: number) {
-    return await this.api.fetchMovieDetail(movieId);
+    try {
+      return await this.api.fetchMovieDetail(movieId);
+    } catch (error) {
+      if (error instanceof Error) renderToast(error.message);
+      return null;
+    }
   }
 }
 
