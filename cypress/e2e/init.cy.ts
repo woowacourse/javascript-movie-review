dfd4ccae0c8bdfb5e8ff49cfaec81ab724e765db
김진호 <rlawlsgh1227@gmail.com>
2024-04-02 15:18:36 +0900

    refactor: 테스트코드 리팩터링

--- cypress/e2e/init.cy.ts
+++ cypress/e2e/init.cy.ts
@@ -14,16 +14,14 @@ describe('service init view test', () => {
   });
 
   context('Flow: 처음 페이지에 접근했을 때', () => {
-    it('처음 실행됐을 때, 모든 component가 정상적으로 생성되어야 한다.', () => {
+    it('모든 component가 정상적으로 생성되어야 한다.', () => {
       cy.get('header').should('exist');
       cy.get('header').find('.search-box').should('exist');
       cy.get('main').should('exist');
       cy.get('main').find('.btn').should('exist');
     });
-  });
 
-  context('Flow: 처음 페이지에 접근했을 때 영화 모드 검증', () => {
-    it('처음 실행됐을 때, 인기있는 영화 화면을 보여준다.', () => {
+    it('인기있는 영화 화면을 보여준다.', () => {
       cy.get('main').find('h2').should('have.text', '지금 인기있는 영화');
     });
   });
