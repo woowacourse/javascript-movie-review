eb5222609056eaca4b67660c5d4e65c5f8bf3d9f
river <jenn0.6n@gmail.com>
2024-04-02 13:41:09 +0900

    Feat: Intersection Observer API 사용해서 무한 스크롤 구현

--- src/services/loadMorePage.ts
+++ src/services/loadMorePage.ts
@@ -2,15 +2,13 @@ import { createMovieItems } from '../components/MovieContainer/render';
 import { API_ENDPOINT, API_OPTION } from '../constants/api/api';
 import { Movie } from '../domain/movie';
 import { ShowMoreButtonOption } from '../types/tmdb';
+import createElement from '../utils/createElement';
 import removeHTMLElements from '../utils/removeHTMLElements';
 import pageManager from './PageManager';
 import uiFeedBackManager from './UIFeedBackManager';
 
 export const checkDataLength = (currentPage: number, total_pages: number, movies: Movie[]) => {
   if (total_pages < currentPage) {
-    if (currentShowMoreEventHandler) {
-      window.removeEventListener('scroll', currentShowMoreEventHandler);
-    }
     displayNoMorePagesMessage();
     return;
   }
@@ -21,10 +19,14 @@ const displayNoMorePagesMessage = () => {
   const container = document.querySelector('.item-view');
   if (!container) return;
 
-  const message = document.createElement('p');
-  message.textContent = '더 이상 페이지가 없습니다.';
-  message.classList.add('no-more-pages');
-  container.appendChild(message);
+  const message = createElement('p', {
+    className: 'no-more-pages',
+    textContent: '더 이상 페이지가 없습니다.',
+  });
+
+  if (!document.querySelector('.no-more-pages')) {
+    container.appendChild(message);
+  }
 };
 
 const getTotalApiUrl = (option: ShowMoreButtonOption, keyword: string, pageNumber: number) => {
@@ -33,7 +35,7 @@ const getTotalApiUrl = (option: ShowMoreButtonOption, keyword: string, pageNumbe
     : API_ENDPOINT.POPULAR(pageNumber);
 };
 
-const fetchNextPage = async (event: Event, option: ShowMoreButtonOption, keyword: string) => {
+const fetchNextPage = async (option: ShowMoreButtonOption, keyword: string) => {
   const currentPage = pageManager.currentPage;
   const updatePageNumber = pageManager.increasePage();
   const totalUrl = getTotalApiUrl(option, keyword, updatePageNumber);
@@ -41,38 +43,59 @@ const fetchNextPage = async (event: Event, option: ShowMoreButtonOption, keyword
   if (moviePage) {
     checkDataLength(currentPage, moviePage.totalPages, moviePage.movies);
     pageManager.setTotalPages(moviePage.totalPages);
+    updateTargetElement();
   }
-  if (!event.target) return;
 };
 
-let currentShowMoreEventHandler: (event: Event) => void;
-
-const handleScroll = (option: ShowMoreButtonOption, inputValue: string) => {
-  return async (event: Event) => {
-    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-    if (scrollTop + clientHeight >= scrollHeight - 100) {
-      const currentPage = pageManager.currentPage;
-      const totalPages = pageManager.totalPages;
-      if (currentPage < totalPages) {
-        removeHTMLElements('.no-more-pages');
-        fetchNextPage(event, option, inputValue);
-      } else {
-        displayNoMorePagesMessage();
-        window.removeEventListener('scroll', currentShowMoreEventHandler);
+const createIntersectionObserver = (option: ShowMoreButtonOption, inputValue: string) => {
+  const handleIntersection = async (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {
+        const currentPage = pageManager.currentPage;
+        const totalPages = pageManager.totalPages;
+        if (currentPage < totalPages) {
+          removeHTMLElements('.no-more-pages');
+          await fetchNextPage(option, inputValue);
+        } else {
+          displayNoMorePagesMessage();
+          observer.disconnect();
+        }
       }
-    }
+    });
   };
+
+  const observer = new IntersectionObserver(handleIntersection, {
+    threshold: 0.5,
+  });
+
+  return observer;
 };
+
+const updateTargetElement = () => {
+  const currentTarget = document.querySelector('.item-view li:last-child');
+  if (currentTarget) {
+    observer.observe(currentTarget);
+  }
+};
+
+let observer: IntersectionObserver;
 
 const addInfiniteScrollEventListener = async (option: ShowMoreButtonOption = 'popular', inputValue: string = '') => {
-  if (currentShowMoreEventHandler) window.removeEventListener('scroll', currentShowMoreEventHandler);
+  if (observer) {
+    observer.disconnect();
+  }
+
+  observer = createIntersectionObserver(option, inputValue);
 
   const initialUrl = getTotalApiUrl(option, inputValue, 1);
   const initialMoviePage = await uiFeedBackManager.fetchData(initialUrl, 'GET', null, API_OPTION.headers);
   if (initialMoviePage) {
     pageManager.setTotalPages(initialMoviePage.totalPages);
-    currentShowMoreEventHandler = handleScroll(option, inputValue);
-    window.addEventListener('scroll', currentShowMoreEventHandler);
+
+    const target = document.querySelector('.item-list li:last-child');
+    if (target) {
+      observer.observe(target);
+    }
   }
 };
 
