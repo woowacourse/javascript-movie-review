a3b479b88b53153a1d15743e766a55c0dfb6fb78
bmj0053 <bmj05378@gmail.com>
2024-04-02 15:31:19 +0900

    fix: 더미데이터 삭제

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './reset.css';
 import './common.css';
 import MovieApp from './MovieApp';
-import DUMMY from './constants/dummy';
-
-localStorage.setItem('ratings', JSON.stringify(DUMMY));
 
 new MovieApp();
 
