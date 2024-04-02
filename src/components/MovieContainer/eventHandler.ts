c7ba0ff0be9eeb19e847bd5885c0d082df712e51
river <jenn0.6n@gmail.com>
2024-04-02 16:35:47 +0900

    Refactor: 함수 depth 줄이기

--- src/components/MovieContainer/eventHandler.ts
+++ src/components/MovieContainer/eventHandler.ts
@@ -7,7 +7,6 @@ const fetchMovieDetails = async (movieId: string): Promise<TMDBDetailProps | nul
   try {
     const response = await tmdbApi.sendRequest(API_ENDPOINT.DETAIL(Number(movieId)));
     const { id, title, poster_path, genres, vote_average, overview } = response;
-
     return { id, title, poster_path, genres, vote_average, overview };
   } catch (error) {
     console.error(error);
@@ -15,24 +14,24 @@ const fetchMovieDetails = async (movieId: string): Promise<TMDBDetailProps | nul
   }
 };
 
-async function movieContainerEventlistener() {
+const handleMovieItemClick = async (event: Event) => {
+  const target = event.target as HTMLElement;
+  const movieItem = target.closest('li[data-id]');
+  if (!movieItem) return;
+
+  const movieId = movieItem.getAttribute('data-id');
+  if (!movieId) return;
+
+  const movieDetails = await fetchMovieDetails(movieId);
+  if (!movieDetails) return;
+
+  const movieDetailModal = MovieDetailModal(movieDetails);
+  document.body.appendChild(movieDetailModal);
+};
+
+function movieContainerEventlistener() {
   const movieItemList = document.querySelector('.item-list');
-
-  movieItemList?.addEventListener('click', async (event) => {
-    const target = event.target as HTMLElement;
-    const movieItem = target.closest('li[data-id]');
-
-    if (movieItem) {
-      const movieId = movieItem.getAttribute('data-id');
-      if (movieId) {
-        const movieDetails = await fetchMovieDetails(movieId);
-        if (movieDetails) {
-          const movieDetailModal = MovieDetailModal(movieDetails);
-          document.body.appendChild(movieDetailModal);
-        }
-      }
-    }
-  });
+  movieItemList?.addEventListener('click', handleMovieItemClick);
 }
 
 export default movieContainerEventlistener;
