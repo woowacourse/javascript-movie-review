eb95332f5333928b3b866ed67b8e7bf18db187cc
rbgksqkr <rbgks1937@naver.com>
2024-04-02 21:52:24 +0900

    refactor(constants): resize delay & mobile width 상수화

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -3,6 +3,7 @@ import './Header.css';
 import SEARCH_BUTTON_IMG from '../../assets/images/search_button.png';
 import { dom } from '../../utils/dom';
 import Button from '../common/button/Button';
+import CONFIG from '../../constants/config';
 
 let debounce: NodeJS.Timeout | undefined;
 
@@ -71,13 +72,13 @@ class Header {
 
     debounce = setTimeout(() => {
       const width = window.innerWidth;
-      if (width > 400) {
+      if (width > CONFIG.MOBILE_DEVICE_WIDTH) {
         dom.getElement(this.$target, '#logo').classList.remove('clicked-logo');
         dom.getElement(this.$target, '.search-box').classList.remove('clicked-form');
         dom.getElement(this.$target, '.search-input').classList.remove('clicked-input');
         dom.getElement(document.body, 'header').classList.remove('clicked-header');
       }
-    }, 100);
+    }, CONFIG.WINDOW_RESIZE_DELAY);
   }
 }
 
