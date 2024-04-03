475e58ad5093f64d3759083541ba4da23a670307
rbgksqkr <rbgks1937@naver.com>
2024-04-03 20:48:25 +0900

    fix(e2e): 인기순이 바뀔 때 테스트케이스가 깨지는 오류 해결

--- cypress/e2e/detail.cy.ts
+++ cypress/e2e/detail.cy.ts
@@ -10,6 +10,10 @@ describe('영화 상세보기 모달 테스트', () => {
       method: 'GET',
       url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/\d/,
     }).as('getDetailMovie');
+    cy.intercept({
+      method: 'GET',
+      url: /^https:\/\/api\.themoviedb\.org\/3\/search\/movie*/,
+    }).as('getSearchMovies');
     cy.visit('/');
   });
 
@@ -31,27 +35,25 @@ describe('영화 상세보기 모달 테스트', () => {
     cy.get('@detailMovies').its('body').should('have.a.property', 'poster_path');
   });
 
-  it('쿵푸팬더 4를 클릭하면 쿵푸팬더 4 영화의 상세 정보를 조회할 수 있다.', () => {
-    const movieTitle = '쿵푸팬더 4';
+  it('해리를 검색하고 해리 포터와 비밀의 방을 클릭하면 해당 영화의 상세 정보를 조회할 수 있다.', () => {
+    const movieTitle = '해리 포터와 비밀의 방';
+    cy.get('#search-input').type('해리{enter}');
 
-    cy.wait('@getPopularMovies').then(popular => {
-      if (popular.response === undefined) return;
-      cy.get('.item-card').first().click();
-
-      cy.wait('@getDetailMovie').then(interception => {
-        if (interception.response === undefined) return;
-        const movieDetailTitle = cy.get('.detail-modal-container').first().get('#modal-title');
-        expect(movieDetailTitle.should('have.text', movieTitle));
-      });
+    cy.wait('@getSearchMovies').then(search => {
+      if (search.response === undefined) return;
+      cy.get('.item-card > .item-title').first().click();
+      const movieDetailTitle = cy.get('.detail-modal-container').first().get('#modal-title');
+      expect(movieDetailTitle.should('have.text', movieTitle));
     });
   });
 
   it('상세보기 모달에서 해당 영화의 별점을 매길 수 있는데 첫번째 별을 클릭하면 "2점"을 매기고 "최악이예요" 텍스트를 출력한다.', () => {
     const scoreNumber = 2;
     const scoreText = '최악이예요';
+    cy.get('#search-input').type('해리{enter}');
 
-    cy.wait('@getPopularMovies').then(popular => {
-      if (popular.response === undefined) return;
+    cy.wait('@getSearchMovies').then(search => {
+      if (search.response === undefined) return;
       cy.get('.item-card').first().click();
 
       cy.wait('@getDetailMovie').then(interception => {
@@ -66,7 +68,7 @@ describe('영화 상세보기 모달 테스트', () => {
   });
 
   it('별점을 매기면 새로고침해도 별점 데이터를 유지하기 위해 로컬 스토리지에 저장된다.', () => {
-    const movieId = 1011985;
+    const movieId = 823464;
     const score = 2;
 
     cy.wait('@getPopularMovies').then(popular => {
