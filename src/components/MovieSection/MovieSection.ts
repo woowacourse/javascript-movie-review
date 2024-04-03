65c78bee0ad97b729f73ce32dad1dfaf09456ed3
hwinkr <dnddl8280@naver.com>
2024-04-03 16:19:15 +0900

    chore(MovieSection): Optional 유틸리티 타입 사용, 빈 검색화면 ul 태그 사용으로 수정

--- src/components/MovieSection/MovieSection.ts
+++ src/components/MovieSection/MovieSection.ts
@@ -2,18 +2,19 @@ import Component from "../common/Component";
 import MovieListContainer from "../MovieListContainer/MovieListContainer";
 
 import { $ } from "../../utils/dom";
+import { Optional } from "../../types/utility";
 
 import "./MovieSection.css";
 
 export default class MovieSection extends Component {
-  private searchKeyword: string | undefined;
+  private searchKeyword: Optional<string>;
 
   protected getTemplate() {
     return /*html*/ `
       <div id="movie-list">
         <h2 id="main-title" class="font-bold select-none main-title"></h2>
       </div>
-      <div id="empty-result" class="flex flex-col text-center text-base hidden empty-result"></div>
+      <ul id="empty-result" class="flex flex-col text-center text-base hidden empty-result"></ul>
       <div id="scroll-trigger" class="h-5 full-width"></div>
     `;
   }
