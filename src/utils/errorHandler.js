4b332202bc7bb247569baf408e158ed6212277e8
jinsil <jinsil378900@gmail.com>
2024-04-03 23:58:37 +0900

    refactor: 에러메세지 추가

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -4,9 +4,9 @@ import view from "../view/view";
 const errorHandler = (code) => {
   view.hideSkeleton();
   if (code >= 400 && code < 500){
-    toast('클라이언트 오류');
+    toast('클라이언트 오류: 다시 접속해주세요.');
   } else if (code >= 500 && code < 600){
-    toast('서버 오류');
+    toast('서버 오류: 다시 접속해주세요.');
   } else if (code === 'Failed to fetch') {
     toast('이 서비스는 일시적으로 오프라인 상태입니다. 네트워크를 확인해주세요.');
   } else {
