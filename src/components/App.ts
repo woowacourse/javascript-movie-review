7f65f91ca43ba6f8b477a4bd1935f171c9782067
hain-tain <hain7ce@gmail.com>
2024-04-03 16:04:03 +0900

    feat(App): page 상태를 객체로 변경 및 renderNextPage, renderSearchNextPage 를 renderPage 함수로 추상화

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,15 +1,12 @@
 import MOVIE_LIST_BOX_TITLE from "../constants/movieListBoxTitle";
 import MovieHeader from "./MovieHeader/MovieHeader";
 import MovieMain from "./MovieMain/MovieMain";
-import createElement from "./utils/createElement";
 import generateScrollTopButton from "./common/generateScrollTopButton";
 import getMovieListByQuery from "../domain/getMovieListByQuery";
 import getPopularMovieList from "../domain/getPopularMovieList";
 
 class App {
-  private FIRST_PAGE = 1;
-
-  private currentPage = this.FIRST_PAGE;
+  private currentPage = new Page();
 
   private movieMain;
   private query = "";
@@ -17,7 +14,9 @@ class App {
   constructor($root: HTMLElement) {
     this.movieMain = new MovieMain({
       title: MOVIE_LIST_BOX_TITLE.popular,
-      getMoreMovies: this.renderNextPage.bind(this),
+      getMoreMovies: () => {
+        this.renderPage({ renderFn: this.renderPopularMovieList.bind(this) });
+      },
     });
 
     this.firstRender($root);
@@ -37,51 +36,62 @@ class App {
 
   private logoClickHandler() {
     this.query = "";
-    this.currentPage = this.FIRST_PAGE;
+    this.currentPage = new Page();
 
     this.movieMain.changeMovieListBox({
       title: MOVIE_LIST_BOX_TITLE.popular,
-      getMoreMovies: this.renderNextPage.bind(this),
+      getMoreMovies: () => {
+        this.renderPage({ renderFn: this.renderPopularMovieList.bind(this) });
+      },
     });
 
     this.renderPopularMovieList();
   }
 
   private searchBoxSubmitHandler(query: string) {
-    this.currentPage = this.FIRST_PAGE;
+    this.currentPage = new Page();
     this.query = query;
 
     this.movieMain.changeMovieListBox({
       title: MOVIE_LIST_BOX_TITLE.search(query),
-      getMoreMovies: this.renderSearchNextPage.bind(this),
+      getMoreMovies: () => {
+        this.renderPage({
+          renderFn: () => this.searchMovies.bind(this)(this.query),
+        });
+      },
     });
     this.searchMovies(query);
   }
 
-  private renderNextPage() {
-    this.currentPage += 1;
-    this.renderPopularMovieList();
-  }
-
-  private renderSearchNextPage() {
-    this.currentPage += 1;
-    this.searchMovies(this.query);
+  private renderPage({
+    renderFn,
+    page,
+  }: {
+    page?: number;
+    renderFn: () => void;
+  }) {
+    if (page) {
+      this.currentPage.setPage(page);
+    } else {
+      this.currentPage.nextPage();
+    }
+    renderFn();
   }
 
   private async renderPopularMovieList() {
     try {
       const { movieList, totalPage } = await getPopularMovieList({
-        page: this.currentPage,
+        page: this.currentPage.page,
       });
 
-      if (this.currentPage === totalPage) {
+      if (this.currentPage.equalPage(totalPage)) {
         this.movieMain.removeMovieMoreObserver();
       }
 
       this.movieMain.reRender(movieList);
     } catch (error) {
       if (error instanceof Error) {
-        this.currentPage -= 1;
+        this.currentPage.previousPage();
         this.movieMain.renderMessage(error.message);
         this.movieMain.reRender([]);
       }
@@ -91,7 +101,7 @@ class App {
   private async searchMovies(query: string) {
     try {
       const { movieList, totalPage } = await getMovieListByQuery({
-        page: this.currentPage,
+        page: this.currentPage.page,
         query,
       });
 
@@ -99,14 +109,14 @@ class App {
         this.renderNoResult("검색 결과가 없습니다.");
       }
 
-      if (this.currentPage === totalPage) {
+      if (this.currentPage.equalPage(totalPage)) {
         this.movieMain.removeMovieMoreObserver();
       }
 
       this.movieMain.reRender(movieList);
     } catch (error) {
       if (error instanceof Error) {
-        this.currentPage -= 1;
+        this.currentPage.previousPage();
         this.renderNoResult(error.message);
         this.movieMain.reRender([]);
       }
@@ -120,3 +130,31 @@ class App {
 }
 
 export default App;
+
+class Page {
+  private FIRST_PAGE = 1;
+  page: number;
+
+  constructor(page?: number) {
+    this.page = page || this.FIRST_PAGE;
+  }
+
+  nextPage() {
+    this.page++;
+  }
+
+  previousPage() {
+    this.page--;
+  }
+
+  setPage(page: number) {
+    this.page = page;
+  }
+
+  equalPage(page: number) {
+    if (this.page === page) {
+      return true;
+    }
+    return false;
+  }
+}
