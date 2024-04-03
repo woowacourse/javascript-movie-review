6b171659d31bab5e7f922f2c3cec07a5c6dea9d9
Jeongwoo Park <wahoo9040@naver.com>
2024-04-03 16:37:47 +0900

    refactor: 상수 분리

--- src/view/event/scrollEvent.ts
+++ src/view/event/scrollEvent.ts
@@ -1,9 +1,10 @@
 import { renderNewMovies } from '../main';
 
 let isLoading = false;
+const OBSERVE_HEIGHT = 200;
 
 const scrollEventHandler = async () => {
-  if (window.innerHeight + window.scrollY > document.body.offsetHeight - 200 && !isLoading) {
+  if (window.innerHeight + window.scrollY > document.body.offsetHeight - OBSERVE_HEIGHT && !isLoading) {
     isLoading = true;
     await renderNewMovies();
     isLoading = false;
