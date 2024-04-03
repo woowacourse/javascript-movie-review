0ad58b6936a171dfa2f7ac84f44ec9e8576d291a
hwinkr <dnddl8280@naver.com>
2024-04-03 16:01:38 +0900

    feat(type.ts): 특정 객체의 키들로 이루어진 배열인지 타입을 확인하는 함수 구현

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,3 +1,7 @@
-export const isValidElement = <K, T extends K>(options: K[], value: K): value is T => {
+export const isArrayElement = <K, T extends K>(options: K[], value: K): value is T => {
   return options.includes(value);
+};
+
+export const isArrayWithObjectKeys = <T extends string[]>(item: object, candidates: string[]): candidates is T => {
+  return Object.keys(item).every((key) => candidates.includes(key));
 };
