d9e876f29c4d66d6b2e30add1ddb2eaab10ccae4
Soyeon Choe <soy2302ten@gmail.com>
2024-04-03 13:56:10 +0900

    test: esc키를 누르면 모달이 닫히는지 확인하는 e2e테스트

--- cypress/e2e/modal.spec.ts
+++ cypress/e2e/modal.spec.ts
@@ -14,7 +14,15 @@ describe('Modal E2E 테스트', () => {
 
   it('닫기 버튼을 클릭하면 modal이 닫힌다.', () => {
     cy.get('.item-card').first().click();
+    cy.get('.modal').should('exist');
     cy.get('.modal-body__close-btn').click();
+    cy.get('.modal').should('not.exist');
+  });
+
+  it('esc 키보드를 누르면 modal이 닫힌다.', () => {
+    cy.get('.item-card').first().click();
+    cy.get('.modal').should('exist');
+    cy.get('body').type('{esc}');
     cy.get('.modal').should('not.exist');
   });
 });
