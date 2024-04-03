5cdec22bd9d20652eeca4cab5b42bbf0052018c4
00kang <kdb070970@gmail.com>
2024-04-03 19:54:43 +0900

    refactor: 메서드 분리

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -18,86 +18,104 @@ export default class Modal {
 
   constructor(movieId: number) {
     this.#movieId = movieId;
+
     const savedVotes = localStorage.getItem('myVoteResult');
     if (savedVotes) {
       this.#myVoteResult = JSON.parse(savedVotes);
     }
   }
 
-  // eslint-disable-next-line max-lines-per-function
   async generateModal() {
-    if (this.#modalElement) {
-      this.#modalElement.remove();
-      this.#modalElement = null;
-    }
+    this.#removeExistingModal();
 
-    const movieDetail = await fetchMovieDetail(this.#movieId);
+    const movieDetail = await this.#fetchMovieDetail();
 
-    const posterPath = movieDetail.poster_path ? `https://image.tmdb.org/t/p/w500${movieDetail.poster_path}` : NoImage;
-
-    const genres = movieDetail.genres.map((genre: any) => genre.name).join(', ');
-
-    const overview = movieDetail.overview ? movieDetail.overview : '해당 영화의 줄거리 정보가 없습니다.';
-
-    const modalElement = document.createElement('div');
-    modalElement.classList.add('modal', 'modal--open');
-
-    const modalHTML = /* html */ `
-    <div class="modal-backdrop"></div>
-    <div class="modal-container">
-      <div class="modal-header">
-        <h3 class="detail-title text-detail-title">${movieDetail.title}</h3>
-        <button class="modal-close-button"></button>
-      </div>
-      <div class="modal-body">
-        <img src=${posterPath} alt="포스터 이미지" class="detail-poster"/>
-        <div class="modal-contents">
-          <div class="detail-text-container">
-            <div class="detail-text-top">
-              <p class="detail-genres text-detail-contents">${genres}</p>
-              <p class="detail-vote_average text-detail-contents">
-                <img src=${StarFilled} alt="별점" class="star-start" />
-                ${movieDetail.vote_average.toFixed(2)}
-              </p>
-            </div>
-            <p class="detail-overview text-detail-contents">${overview}</p>
-          </div>
-          <div class="my-vote">
-            <p class="my-vote-title text-detail-vote">내 별점</p>
-            <div class="my-vote-body">
-              <button><img src=${StarEmpty}/></button>
-              <button><img src=${StarEmpty}/></button>
-              <button><img src=${StarEmpty}/></button>
-              <button><img src=${StarEmpty}/></button>
-              <button><img src=${StarEmpty}/></button>
-            </div>
-            <p class="my-vote-number text-detail-vote-contents">0</p>
-            <p class="my-vote-description text-detail-vote-contents">남겨주세요</p>
-          </div>
-        </div>
-      </div>
-    </div>
-   `;
-
-    modalElement.innerHTML = modalHTML;
+    const modalElement = this.#createModalElement(movieDetail);
     document.body.appendChild(modalElement);
     this.#modalElement = modalElement;
 
+    this.#setupModalEventListener(this.#modalElement);
+  }
+
+  get Element() {
+    return this.#modalElement;
+  }
+
+  #removeExistingModal() {
+    if (!this.#modalElement) return;
+
+    this.#modalElement.remove();
+    this.#modalElement = null;
+  }
+
+  async #fetchMovieDetail() {
+    const moviedetail = await fetchMovieDetail(this.#movieId);
+    const movieDetail = await moviedetail.json();
+
+    return movieDetail;
+  }
+
+  // eslint-disable-next-line max-lines-per-function
+  #createModalElement(movieDetail: any) {
+    const modalElement = document.createElement('div');
+    modalElement.classList.add('modal', 'modal--open');
+
+    const posterPath = movieDetail.poster_path ? `https://image.tmdb.org/t/p/w500${movieDetail.poster_path}` : NoImage;
+    const genres = movieDetail.genres.map((genre: any) => genre.name).join(', ');
+    const overview = movieDetail.overview ? movieDetail.overview : '해당 영화의 줄거리 정보가 없습니다.';
+
+    const modalHTML = /* html */ `
+      <div class="modal-backdrop"></div>
+      <div class="modal-container">
+        <div class="modal-header">
+          <h3 class="detail-title text-detail-title">${movieDetail.title}</h3>
+          <button class="modal-close-button"></button>
+        </div>
+        <div class="modal-body">
+          <img src=${posterPath} alt="포스터 이미지" class="detail-poster"/>
+          <div class="modal-contents">
+            <div class="detail-text-container">
+              <div class="detail-text-top">
+                <p class="detail-genres text-detail-contents">${genres}</p>
+                <p class="detail-vote_average text-detail-contents">
+                  <img src=${StarFilled} alt="별점" class="star-start" />
+                  ${movieDetail.vote_average.toFixed(2)}
+                </p>
+              </div>
+              <p class="detail-overview text-detail-contents">${overview}</p>
+            </div>
+            <div class="my-vote">
+              <p class="my-vote-title text-detail-vote">내 별점</p>
+              <div class="my-vote-body">
+                <button><img src=${StarEmpty}/></button>
+                <button><img src=${StarEmpty}/></button>
+                <button><img src=${StarEmpty}/></button>
+                <button><img src=${StarEmpty}/></button>
+                <button><img src=${StarEmpty}/></button>
+              </div>
+              <p class="my-vote-number text-detail-vote-contents">0</p>
+              <p class="my-vote-description text-detail-vote-contents">남겨주세요</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    modalElement.innerHTML = modalHTML;
+    return modalElement;
+  }
+
+  #setupModalEventListener(modalElement: HTMLElement) {
     modalElement.addEventListener('click', (event) => {
       this.handleModalClick(event);
 
       const button = event.target;
       const starButtons = modalElement.querySelectorAll('.my-vote-body button img');
-
       const starIndex = Array.from(starButtons).findIndex((btn) => btn === button);
       if (starIndex !== -1) {
         this.handleStarClick(starIndex);
       }
     });
-  }
-
-  get Element() {
-    return this.#modalElement;
   }
 
   // eslint-disable-next-line max-lines-per-function
@@ -107,6 +125,15 @@ export default class Modal {
     document.body.style.overflow = 'hidden';
     await this.generateModal();
 
+    this.#VoteHandler();
+
+    document.addEventListener('keydown', this.#handleKeyDown);
+
+    this.#isOpen = true;
+  }
+
+  // eslint-disable-next-line max-lines-per-function
+  #VoteHandler() {
     const savedVotes = localStorage.getItem('myVoteResult');
     if (savedVotes) {
       const savedVotesJSON = JSON.parse(savedVotes);
@@ -114,15 +141,10 @@ export default class Modal {
     }
 
     const voteForMovie = this.#myVoteResult[this.#movieId];
-
     if (voteForMovie) {
       this.#updateVoteStar(voteForMovie);
       this.#updateVoteText(voteForMovie);
     }
-
-    document.addEventListener('keydown', this.#handleKeyDown);
-
-    this.#isOpen = true;
   }
 
   // eslint-disable-next-line max-lines-per-function
@@ -166,11 +188,7 @@ export default class Modal {
     document.body.style.overflow = 'auto';
 
     this.removeKeydownEventListener();
-
-    if (this.#modalElement) {
-      this.#modalElement.remove();
-      this.#modalElement = null;
-    }
+    this.#removeExistingModal();
 
     this.#isOpen = false;
   }
