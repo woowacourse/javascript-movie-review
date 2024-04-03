b79111d3aeb6094dd58fb7c90a44cee08ce58e14
hwinkr <dnddl8280@naver.com>
2024-04-03 16:10:39 +0900

    chore(delete unused file): 사용하지 않는 파일 삭제

--- src/domains/MovieListManager/MovieListManager.type.ts
+++
@@ -1,6 +0,0 @@
-import { MovieItem } from "../../types/movies";
-
-export interface MovieListManagerInterface {
-  isEndPage: () => boolean;
-  fetchMovieList: () => Promise<MovieItem[] | undefined>;
-}
