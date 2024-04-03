d8dc01b6f18565197fb5b8172b9763308fe306b9
Jeongwoo Park <wahoo9040@naver.com>
2024-04-03 13:05:31 +0900

    refactor: 이미지 크기 상수 분리

--- src/constants/tmdbConstants.ts
+++ src/constants/tmdbConstants.ts
@@ -1,7 +1,8 @@
 const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
 
-// TODO: 사이즈를 나누면 어떨까?
-export const MOVIE_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const IMAGE_SIZE = '500';
+
+export const MOVIE_IMAGE_BASE_URL = `https://image.tmdb.org/t/p/w${IMAGE_SIZE}`;
 
 export const POPULAR_MOVIES_URL = `${TMDB_API_BASE_URL}/movie/popular`;
 
