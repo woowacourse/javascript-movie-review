be4e26e5938e9e979c79e2b4dca6283f61d4f6cf
jayming66 <mjw8941@gmail.com>
2024-04-04 13:03:52 +0900

    feat: 로컬스토리지 사용 - Map객체로 유저 별점 저장

--- src/store/UserRate.ts
+++ src/store/UserRate.ts
@@ -1,11 +1,22 @@
 export const getUserRateFromLocalStorage = (movieId: string) => {
-  const localData = localStorage.getItem(movieId);
-  const userRate = Number(localData) ?? 0;
+  const localData = localStorage.getItem('userRates');
+  const userRateData = localData ? JSON.parse(localData) : null;
+
+  if (!userRateData || typeof userRateData !== 'object') return 0;
+
+  const userRateMap = new Map(Object.entries(userRateData));
+  const userRate = userRateMap.has(movieId) ? Number(userRateMap.get(movieId)) : 0;
 
   return userRate;
 };
 
 export const updateUserRateToLocalStorage = (movieId: string, rate: string) => {
-  if (Number(rate) === getUserRateFromLocalStorage(movieId)) return;
-  localStorage.setItem(movieId, rate);
+  const localData = localStorage.getItem('userRates');
+  const userRateData = localData ? JSON.parse(localData) : {};
+
+  if (typeof userRateData !== 'object') return;
+
+  userRateData[movieId] = rate;
+
+  localStorage.setItem('userRates', JSON.stringify(userRateData));
 };
