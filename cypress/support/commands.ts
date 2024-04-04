f7723e70771527cadfdf063367657d0e1b13a27f
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-04 21:03:55 +0900

    test: 영화 상세 정보 모달 테스트 추가 및 기존 테스트 코드 수정

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -27,6 +27,19 @@ Cypress.Commands.add('testAPIWithFixture', (type: string, fixturePath: string) =
   );
 });
 
+Cypress.Commands.add('testAPIWithSingleMovie', (movieId: string, fixturePath: string) => {
+  const url = new RegExp(`https?://api.themoviedb.org/3/movie/${movieId}*`);
+  cy.intercept(
+    {
+      method: 'GET',
+      url: url,
+    },
+    {
+      fixture: fixturePath,
+    },
+  );
+});
+
 Cypress.Commands.add('verifyToastExists', (message: string) => {
   cy.get('div.toast', { timeout: 5000 }).should('be.visible').and('contain', message);
 });
@@ -35,13 +48,22 @@ Cypress.Commands.add('verifyToastNotExists', () => {
   cy.get('div.toast', { timeout: 5000 }).should('not.be.visible');
 });
 
+Cypress.Commands.add('clickFirstMovieItem', () => {
+  cy.wait(1000);
+  const item = cy.get('ul.item-list > li').first();
+  item.click();
+  cy.wait(1000);
+});
+
 declare namespace Cypress {
   interface Chainable {
     visitHome(): Chainable<void>;
     addSearchInput(input: string): Chainable<Element>;
     submitSearchInput(): Chainable<Element>;
     testAPIWithFixture(type: string, fixturePath: string): Chainable<void>;
+    testAPIWithSingleMovie(movieId: string, fixturePath: string): Chainable<void>;
     verifyToastExists(message: string): Chainable<void>;
     verifyToastNotExists(): Chainable<void>;
+    clickFirstMovieItem(): Chainable<void>;
   }
 }
