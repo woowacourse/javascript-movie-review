e637d8dded367fe9fa9b95c925f4556a4e79fe21
ImxYJL <allensain14@gmail.com>
2024-04-05 13:51:26 +0900

    test(e2e): 무한 스크롤 테스트를 인터셉터를 이용해 테스트

--- cypress/e2e/movieReview.cy.ts
+++ cypress/e2e/movieReview.cy.ts
@@ -1,7 +1,9 @@
-describe("실제 api를 호출하는 테스트", () => {
-  const BASE_URL = "https://api.themoviedb.org/3";
-  const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
+import { infiniteScroll } from "../../src/utility/infiniteScroll";
 
+const BASE_URL = "https://api.themoviedb.org/3";
+const POPULAR_MOVIES_URL = `${BASE_URL}/movie/popular`;
+
+describe("실제 api를 호출하는 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:8080");
   });
@@ -33,22 +35,23 @@ describe("실제 api를 호출하는 테스트", () => {
       "해당 키워드에 해당하는 영화가 없습니다."
     );
   });
-
-  it("페이지 하단으로 스크롤을 옮기면 다음 영화 리스트 20개를 렌더링한다.", () => {
-    // given
-    const movieItemCount = 20;
-    cy.get(".item-list li").should("have.length", movieItemCount);
-
-    // when
-    cy.window().scrollTo("bottom");
-
-    // then
-    cy.get(".item-list li").should("have.length", movieItemCount * 2);
-  });
 });
 
 describe("인터셉터를 사용하는 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:8080");
+  });
+
+  it("페이지 하단으로 스크롤을 옮기면 다음 영화 리스트 20개를 렌더링한다.", () => {
+    // given
+    cy.intercept(POPULAR_MOVIES_URL, {
+      fixture: "movie-popular-lists.json",
+    });
+
+    // when
+    cy.window().scrollTo("bottom");
+
+    // then
+    cy.get(".item-list li").should("have.length", 40);
   });
 });
