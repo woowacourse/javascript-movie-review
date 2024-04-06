656ed71474be2c9a54da1906a78005bef37654b8
vi-wolhwa <cerezo00@naver.com>
2024-04-06 13:21:24 +0900

    refactor: domUtils 분리

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -29,14 +29,6 @@ export const $$ = <T extends Element = Element>(
   return elements;
 };
 
-export const setAttributes = (element: HTMLElement, attributes: { [key: string]: string | EventListener }): void => {
-  Object.entries(attributes).forEach(([key, value]) => {
-    if (typeof value === 'function') element.addEventListener(key, value);
-    else if (key === 'text') element.innerText = value;
-    else element.setAttribute(key, value);
-  });
-};
-
 export const appendChildren = (parents: Element | Element[], children: Element[]): void => {
   const parentsArray = Array.isArray(parents) ? parents : [parents];
 
@@ -46,4 +38,28 @@ export const appendChildren = (parents: Element | Element[], children: Element[]
   });
 
   children.forEach((child) => parentsArray[parentsArray.length - 1].appendChild(child));
+};
+
+const addEventListenerToElement = (element: HTMLElement, eventType: string, listener: EventListener) => {
+  element.addEventListener(eventType, listener);
+};
+
+const setTextContentOfElement = (element: HTMLElement, text: string) => {
+  element.innerText = text;
+};
+
+const setAttributeOfElement = (element: HTMLElement, attributeName: string, attributeValue: string) => {
+  element.setAttribute(attributeName, attributeValue);
+};
+
+export const configureElement = (element: HTMLElement, attributes: { [key: string]: string | EventListener }): void => {
+  Object.entries(attributes).forEach(([key, value]) => {
+    if (typeof value === 'function') {
+      addEventListenerToElement(element, key, value as EventListener);
+    } else if (key === 'text') {
+      setTextContentOfElement(element, value as string);
+    } else {
+      setAttributeOfElement(element, key, value as string);
+    }
+  });
 };
