02e069222aada3cb8d5841c2cf9f207b9246734e
Cron <hojoong.choi@woowahan.com>
2025-03-17 17:55:48 +0900

    test: 비동기 API 테스트

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,30 @@
-describe("template spec", () => {
-  it("passes", () => {
+/// <reference types="cypress" />
+
+describe("비동기 API 테스트", () => {
+  it("영화 목록 API를 호출하면 한 번에 20개씩 목록에 나열되어야 한다.", () => {
     cy.visit("localhost:5173");
+
+    const MOVIE_URL = "https://api.themoviedb.org/3/movie/popular";
+    const params = new URLSearchParams({
+      language: "ko-KR",
+      page: "1",
+    });
+    const popularMovieUrl = MOVIE_URL + "?" + params.toString();
+
+    const options = {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${Cypress.env("API_KEY")}`,
+      },
+    };
+
+    cy.request({
+      method: "GET",
+      url: popularMovieUrl,
+      ...options,
+    }).as("popularMovies");
+
+    cy.get("@popularMovies").its("status").should("eq", 200);
+    cy.get("@popularMovies").its("body.results").should("have.length", 20);
   });
 });
