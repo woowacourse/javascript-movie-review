866828b2149e46dfddaa3758630215085afec0ea
wo-o29 <woogur29@gmail.com>
2025-03-18 19:07:54 +0900

    ​​feat: SearchBox 컴포넌트 추가
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,18 @@
-import image from "../templates/images/star_filled.png";
+import getPopularMovieList from "./apis/getPopularMovieList";
+import $SearchBox from "./components/SearchBox/SearchBox";
+import $MovieListSection from "./components/MovieListSection/MovieListSection";
 
-console.log("npm run dev 명령어를 통해 영화 리뷰 미션을 시작하세요");
+const app = document.querySelector("#wrap");
+const footer = document.querySelector("footer")!;
 
-console.log(
-  "%c" +
-    " _____ ______   ________  ___      ___ ___  _______                \n" +
-    "|\\   _ \\  _   \\|\\   __  \\|\\  \\    /  /|\\  \\|\\  ___ \\               \n" +
-    "\\ \\  \\\\\\__\\ \\  \\ \\  \\|\\  \\ \\  \\  /  / | \\  \\ \\   __/|              \n" +
-    " \\ \\  \\\\|__| \\  \\ \\  \\\\\\  \\ \\  \\/  / / \\ \\  \\ \\  \\_|/__            \n" +
-    "  \\ \\  \\    \\ \\  \\ \\  \\\\\\  \\ \\    / /   \\ \\  \\ \\  \\_|\\ \\           \n" +
-    "   \\ \\__\\    \\ \\__\\ \\_______\\ \\__/ /     \\ \\__\\ \\_______\\          \n" +
-    "    \\|__|     \\|__|\\|_______|\\|__|/       \\|__|\\|_______|          \n" +
-    "                                                                   \n" +
-    "                                                                   \n" +
-    "                                                                   \n" +
-    " ________  _______   ___      ___ ___  _______   ___       __      \n" +
-    "|\\   __  \\|\\  ___ \\ |\\  \\    /  /|\\  \\|\\  ___ \\ |\\  \\     |\\  \\    \n" +
-    "\\ \\  \\|\\  \\ \\   __/|\\ \\  \\  /  / | \\  \\ \\   __/|\\ \\  \\    \\ \\  \\   \n" +
-    " \\ \\   _  _\\ \\  \\_|/_\\ \\  \\/  / / \\ \\  \\ \\  \\_|/_\\ \\  \\  __\\ \\  \\  \n" +
-    "  \\ \\  \\\\  \\\\ \\  \\_|\\ \\ \\    / /   \\ \\  \\ \\  \\_|\\ \\ \\  \\|\\__\\_\\  \\ \n" +
-    "   \\ \\__\\\\ _\\\\ \\_______\\ \\__/ /     \\ \\__\\ \\_______\\ \\____________\\\n" +
-    "    \\|__|\\|__|\\|_______|\\|__|/       \\|__|\\|_______|\\|____________|",
-  "color: #d81b60; font-size: 14px; font-weight: bold;"
+const data = await getPopularMovieList(1);
+app?.insertBefore(
+  $MovieListSection({
+    title: "지금 인기 있는 영화",
+    movieList: data.results,
+  }),
+  footer
 );
 
-addEventListener("load", () => {
-  const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = image;
-
-  if (app) {
-    app.appendChild(buttonImage);
-  }
-});
+const topRatedContainer = document.querySelector(".top-rated-container")!;
+topRatedContainer.prepend($SearchBox());
