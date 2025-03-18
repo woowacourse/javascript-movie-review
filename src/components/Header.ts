23280bd426272b76cbd1736a010f33180a66d65d
youdame <112458620+youdame@users.noreply.github.com>
2025-03-18 17:33:36 +0900

    feat: 서치바 완성

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,13 +1,15 @@
 import Button from "./Button";
+import SearchBar from "./SearchBar";
 
 function Header() {
   return /*html*/ `<header>
     <div class="background-container">
       <div class="overlay" aria-hidden="true"></div>
       <div class="top-rated-container">
-        <h1 class="logo">
-          <img src="./images/logo.png" alt="MovieList" />
-        </h1>
+        <div class="logo">
+          <img class="logo-img" src="./images/logo.png" alt="MovieList" />
+          ${SearchBar()}
+        </div>
         <div class="top-rated-movie">
           <div class="rate">
             <img src="./images/star_empty.png" class="star" />
---
+++ src/components/SearchBar.ts
@@ -0,0 +1,11 @@
+function SearchBar() {
+  return /*html*/ `  
+    <form class ="search-form">
+      <input placeholder="검색어를 입력하세요"/>
+      <button>
+      <img src="./images/search.png" alt="검색 아이콘"/>
+      </button>
+    </form>`;
+}
+
+export default SearchBar;
