5cd89d845cbde34a5fce4e4e8ec16861d10723e8
keemsebeen <sebin0580@gmail.com>
2025-03-18 22:25:43 +0900

    feat: App 컴포넌트에 env 관련 로직 추가
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,14 @@
+import { ENV } from './api/env';
+import Fetcher, { QUERY_PARAMS } from './api/Fetcher';
 import { Footer } from './components/common/Footer';
 import { Header } from './components/common/Header';
 import { MovieList } from './components/feature/MovieList';
 import { MOVIE_RESPONSE } from './constants/movie.data';
 
-export const App = () => {
+export const App = async () => {
+  const fetcher = new Fetcher(ENV.VITE_API_URL);
+  const data = await fetcher.get(QUERY_PARAMS.MOVIE);
   Header();
   MovieList(MOVIE_RESPONSE);
-
   Footer();
 };
