b03172a0702b1c60bca527ea135935b7bc05b91e
SuHwa Lee <tnghk9611@naver.com>
2025-03-18 21:41:18 +0900

    feat: 영화 타입 정의
    Co-Authored-By: keemsebeen <sebin0580@gmail.com>

--- src/types/Movie.types.ts
+++ src/types/Movie.types.ts
@@ -15,7 +15,7 @@ export type MovieItem = {
   overview: string;
   popularity: number;
   poster_path: string;
-  release_date: Date;
+  release_date: string;
   title: string;
   video: boolean;
   vote_average: number;
