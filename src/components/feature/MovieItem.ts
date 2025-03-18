571532de783e36add062417ddd91cd0ce9ce05e6
keemsebeen <sebin0580@gmail.com>
2025-03-18 21:07:03 +0900

    feat: MovieItem 컴포넌트 구현
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

---
+++ src/components/feature/MovieItem.ts
@@ -0,0 +1,45 @@
+import { MovieItem as MovieItemType } from '../../../types/Movie';
+import { createElement } from '../../utils/createElement';
+import { Icon } from '../common/Icon';
+import { Text } from '../common/Text';
+
+export const MovieItem = ({ ...props }: MovieItemType) => {
+  const { title, vote_average, poster_path } = props;
+  const liElement = <HTMLLIElement>createElement('li');
+
+  const itemDiv = <HTMLDivElement>createElement('div');
+  itemDiv.classList.add('item');
+
+  const movieImg = <HTMLImageElement>createElement('img');
+  movieImg.src = poster_path;
+  movieImg.width = 200;
+  movieImg.height = 300;
+  movieImg.alt = title;
+
+  const descItemDiv = <HTMLDivElement>createElement('div');
+  descItemDiv.classList.add('item-desc');
+
+  const rateText = Text({
+    classList: ['rate'],
+    props: {
+      textContent: `${vote_average}`,
+    },
+  });
+
+  const starIcon = Icon({
+    size: 16,
+    src: './images/star_empty.png',
+    classList: ['star'],
+  });
+
+  const titleText = Text({
+    props: {
+      textContent: title,
+    },
+  });
+
+  liElement.append(movieImg, descItemDiv);
+  descItemDiv.append(rateText, starIcon, titleText);
+
+  return liElement;
+};
