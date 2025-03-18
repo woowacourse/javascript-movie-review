418de54ee022b3dbff4b23394367d54a5ae12aca
keemsebeen <sebin0580@gmail.com>
2025-03-18 17:26:40 +0900

    feat: Icon 컴포넌트 구현
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

---
+++ src/components/common/Icon.ts
@@ -0,0 +1,18 @@
+import { ComponentProps } from '../../types/Component.types';
+import { createElement } from '../../utils/createElement';
+
+type IconProps = {
+  size: number;
+  src: string;
+} & ComponentProps;
+
+export const Icon = ({ size, src, classList, props }: IconProps) => {
+  const iconElement = <HTMLImageElement>createElement('img', props);
+
+  iconElement.width = size;
+  iconElement.height = size;
+  iconElement.src = src;
+  iconElement.classList.add(classList.join(' '));
+
+  return iconElement;
+};
