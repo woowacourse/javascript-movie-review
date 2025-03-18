763d0381569e5de4796a92d91d162cd56d02b2b4
장정안 <wjddks96@naver.com>
2025-03-18 16:39:33 +0900

    chore: 이미지 폴더구조 변경

---
+++ src/assets/images.ts
@@ -0,0 +1,7 @@
+export const images = {
+  logo: "./logo.png",
+  starEmpty: "./star_empty.png",
+  starFull: "./star_filled.png",
+  woowacourse: "./woowacourse_logo.png",
+  modalButton: "./modal_button_close.png",
+} as const;
