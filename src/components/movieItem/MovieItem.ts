0ddc403f7023a1d1f0e0debbc4020cf534bc1391
장정안 <wjddks96@naver.com>
2025-03-18 17:53:54 +0900

    feat: MovieItem 컴포넌트 구현

---
+++ src/components/movieItem/MovieItem.ts
@@ -0,0 +1,29 @@
+interface MovieItemProps {
+  title: string;
+  rate: number;
+  src: string;
+}
+
+const MovieItem = (props: MovieItemProps) => {
+  const { title, rate, src } = props;
+  return `
+              <li>
+                <div class="item">
+                  <img
+                    class="thumbnail"
+                    src="${src}"
+                    alt="${title}"
+                  />
+                  <div class="item-desc">
+                    <p class="rate">
+                      <img src="./images/star_empty.png" class="star" />
+                      <span>${rate}</span>
+                    </p>
+                    <strong>${title}</strong>
+                  </div>
+                </div>
+              </li>
+  `;
+};
+
+export default MovieItem;
