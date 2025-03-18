42c438e04992a02d43b32155aae381c64d53a68a
wo-o29 <woogur29@gmail.com>
2025-03-18 16:10:20 +0900

    ​​feat: MovieItem 컴포넌트 추가
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

---
+++ src/components/MovieItem/MovieItem.ts
@@ -0,0 +1,37 @@
+import { MovieDetail } from "../../../types/type";
+
+const IMAGE_PATH_PREFIX = "https://image.tmdb.org/t/p/w440_and_h660_face";
+const $MovieItem = ({ title, poster_path, vote_average }: MovieDetail) => {
+  const rate = createElement("p", {
+    className: "rate",
+  });
+  const star = createElement("img", {
+    src: "./images/star_empty.png",
+    className: "star",
+  });
+  const rateValue = createElement("span", {
+    textContent: `${vote_average}`,
+  });
+  rate.append(star, rateValue);
+
+  const movieTitle = createElement("strong", {
+    textContent: title,
+  });
+  const description = createElement("div", {
+    className: "item-desc",
+  });
+  description.append(rate, movieTitle);
+
+  const item = createElement("li", {
+    className: "item",
+  });
+  const poster = createElement("img", {
+    className: "thumbnail",
+    src: IMAGE_PATH_PREFIX + poster_path,
+    alt: title,
+  });
+  item.append(poster, description);
+  return item;
+};
+
+export default $MovieItem;
