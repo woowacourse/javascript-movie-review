7a1370b96111997e23ff117124d99ffb76109788
bunju20 <ghkd2271@naver.com>
2025-03-18 19:21:36 +0900

    feat:  movie 컴포넌트 구현

---
+++ src/ui/components/Movie.js
@@ -0,0 +1,30 @@
+export default class Movie {
+  constructor({ id, title, posterPath, voteAverage }) {
+    this.id = id;
+    this.title = title;
+    this.posterPath = posterPath;
+    this.voteAverage = voteAverage;
+  }
+
+  render() {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <div class="item">
+        <img
+          class="thumbnail"
+          src="https://media.themoviedb.org/t/p/w440_and_h660_face${this.posterPath}"
+          alt="${this.title}"
+        />
+        <div class="item-desc">
+          <p class="rate">
+            <img src="./images/star_empty.png" class="star" /><span>${this.voteAverage}</span>
+          </p>
+          <strong>${this.title}</strong>
+        </div>
+      </div>
+    `;
+
+    return li;
+  }
+}
