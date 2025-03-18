888b1deeec4175d504bb9d84203e06b492039346
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-18 17:43:22 +0900

    ✨ feat: getPopularityMovie 함수 생성
    Co-authored-by: kimyou1102 <kimyou1102@users.noreply.github.com>

---
+++ src/Domain/getPopularityMovie.ts
@@ -0,0 +1,17 @@
+import { ApiResponse } from "../types/movie";
+import { getFetchData } from "../utils/getfetchData";
+
+export async function getPopularityMovie(
+  page: Number
+): Promise<ApiResponse | null> {
+  try {
+    const data = await getFetchData<ApiResponse>(
+      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=${page}&sort_by=popularity.desc`
+    );
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
