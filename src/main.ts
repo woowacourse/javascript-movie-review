38e26e09f06beb424a118020dae908cd1d45161d
hwanheejung <hwanheejung117@gmail.com>
2025-03-18 17:20:28 +0900

    refactor: App class로 마이그레이션

--- src/main.ts
+++ src/main.ts
@@ -11,83 +11,97 @@ export interface Movie {
 }
 
 export const BASE_URL = "https://api.themoviedb.org/3/movie";
-const MAX_PAGE = 2;
+const MAX_PAGE = 500;
 
-const App = async () => {
-  let page = 1;
+class App {
+  #page: number;
+  #movies: Movie[];
 
-  const { movies, topRatedMovie } = await fetchMovies(page);
+  constructor() {
+    this.#page = 1;
+    this.#movies = [];
+    this.#initialSetup();
+  }
 
-  renderHeader(topRatedMovie);
-  renderMoviesContainer();
-  renderMovies(movies);
-  renderMoreMoviesButton();
-  renderFooter();
+  async #initialSetup() {
+    const movies = await this.#fetchMovies(this.#page);
+    this.#movies = movies;
 
-  const moreButton = document.querySelector(".more-movies-button");
+    this.#renderHeader(this.#movies[0]);
+    this.#renderMoviesContainer();
+    this.#renderMovies(movies);
+    this.#renderMoreMoviesButton();
+    this.#renderFooter();
 
-  moreButton?.addEventListener("click", async () => {
-    page += 1;
-    const { movies: newMovies } = await fetchMovies(page);
-    renderMovies(newMovies);
+    this.#addEventListeners();
+  }
 
-    if (page >= MAX_PAGE) {
-      removeMoreMoviesButton();
+  #addEventListeners() {
+    const moreButton = document.querySelector(".more-movies-button");
+    moreButton?.addEventListener("click", () => this.#refetchMovies());
+  }
+
+  async #refetchMovies() {
+    this.#page += 1;
+    const newMovies = await this.#fetchMovies(this.#page);
+    this.#renderMovies(newMovies);
+
+    if (this.#page >= MAX_PAGE) {
+      this.#removeMoreMoviesButton();
       return;
     }
-  });
-};
+  }
 
-const fetchMovies = async (page: number) => {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
-    },
-  };
+  async #fetchMovies(page: number) {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+      },
+    };
 
-  const raw = await fetch(
-    `${BASE_URL}/popular?language=en-US&page=${page}`,
-    options
-  );
-  const data = await raw.json();
-  const movies: Movie[] = data.results;
-  const topRatedMovie: Movie = movies[0];
+    const raw = await fetch(
+      `${BASE_URL}/popular?language=en-US&page=${page}`,
+      options
+    );
+    const data = await raw.json();
+    const movies: Movie[] = data.results;
 
-  return { movies, topRatedMovie };
-};
+    return movies;
+  }
 
-const renderHeader = (topRatedMovie: Movie) => {
-  const root = document.querySelector("#wrap");
-  root?.insertAdjacentHTML("afterbegin", Header(topRatedMovie));
-};
+  #renderHeader(topRatedMovie: Movie) {
+    const root = document.querySelector("#wrap");
+    root?.insertAdjacentHTML("afterbegin", Header(topRatedMovie));
+  }
 
-const renderMoviesContainer = () => {
-  const section = document.querySelector("main section");
-  section?.insertAdjacentHTML(
-    "afterbegin",
-    "<h2>지금 인기 있는 영화</h2><ul class='thumbnail-list'></ul>"
-  );
-};
+  #renderMoviesContainer() {
+    const section = document.querySelector("main section");
+    section?.insertAdjacentHTML(
+      "afterbegin",
+      "<h2>지금 인기 있는 영화</h2><ul class='thumbnail-list'></ul>"
+    );
+  }
 
-const renderMovies = (movies: Movie[]) => {
-  const ul = document.querySelector(".thumbnail-list");
-  ul?.insertAdjacentHTML("beforeend", MovieList(movies));
-};
+  #renderMovies(movies: Movie[]) {
+    const ul = document.querySelector(".thumbnail-list");
+    ul?.insertAdjacentHTML("beforeend", MovieList(movies));
+  }
 
-const renderFooter = () => {
-  const root = document.querySelector("#wrap");
-  root?.insertAdjacentHTML("beforeend", Footer());
-};
+  #renderFooter() {
+    const root = document.querySelector("#wrap");
+    root?.insertAdjacentHTML("beforeend", Footer());
+  }
 
-const renderMoreMoviesButton = () => {
-  const container = document.querySelector(".container");
-  container?.insertAdjacentHTML("beforeend", MoreMoviesButton());
-};
+  #renderMoreMoviesButton() {
+    const container = document.querySelector(".container");
+    container?.insertAdjacentHTML("beforeend", MoreMoviesButton());
+  }
 
-const removeMoreMoviesButton = () => {
-  document.querySelector(".more-movies-button")?.remove();
-};
+  #removeMoreMoviesButton() {
+    document.querySelector(".more-movies-button")?.remove();
+  }
+}
 
-App();
+new App();
