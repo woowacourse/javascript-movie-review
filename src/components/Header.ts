d082a27ed1b81870fbd438c09e22e589be80f4db
ShinjungOh <tlswjd9260@gmail.com>
2025-03-18 21:00:35 +0900

    feat: Header 컴포넌트 구현
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,24 +1,31 @@
-const Header = () => {
+type HeaderProps = {
+  navigationBar: HTMLDivElement;
+};
+
+const Header = ({ navigationBar }: HeaderProps) => {
   const header = document.createElement("header");
 
   header.innerHTML = `
-        <div class="background-container">
-          <div class="overlay" aria-hidden="true"></div>
-          <div class="top-rated-container">
-            <h1 class="logo">
-              <img src="images/logo.png" alt="MovieList" />
-            </h1>
-            <div class="top-rated-movie">
-              <div class="rate">
-                <img src="images/star_empty.png" class="star" />
-                <span class="rate-value">9.5</span>
-              </div>
-              <div class="title">인사이드 아웃2</div>
-              <button class="primary detail">자세히 보기</button>
-            </div>
-          </div>
+  <div class="background-container">
+    <div class="overlay" aria-hidden="true"></div>
+    <div class="top-rated-container">
+      
+      <div class="top-rated-movie">
+        <div class="rate">
+          <img src="images/star_empty.png" class="star" />
+          <span class="rate-value">9.5</span>
         </div>
-    `;
+        <div class="title">인사이드 아웃2</div>
+        <button class="primary detail">자세히 보기</button>
+      </div>
+    </div>
+  </div>
+`;
+
+  const topRatedContainer = header.querySelector(".top-rated-container");
+  if (topRatedContainer) {
+    topRatedContainer.prepend(navigationBar);
+  }
 
   return header;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
 import Button from "./components/Button.ts";
+import Header from "./components/Header.ts";
+import Input from "./components/Input.ts";
+import NavigationBar from "./components/NavigationBar.ts";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const input = Input({
+    type: "text",
+    placeholder: "검색어를 입력하세요",
+    onClick: () => {
+      console.log("검색 아이콘 클릭");
+    },
+  });
+
+  const navigationBar = NavigationBar({ input });
+  const header = Header({ navigationBar });
+
+  const wrap = document.querySelector("#wrap");
+  wrap?.prepend(header);
+
   const button = Button({
-    text: "버튼",
+    text: "더 보기",
     onClick: () => {
       console.log("버튼 클릭");
     },
   });
 
-  const footer = document.querySelector(".footer");
-  footer?.appendChild(button);
+  // Input 컴포넌트 사용하는 코드
+  // const header = document.querySelector(".header");
+  // header?.appendChild(input);
+  const section = document.querySelector("section");
+  section?.appendChild(button);
 });
