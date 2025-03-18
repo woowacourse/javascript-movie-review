f43be4606c9427250f448a154d673eb341bc02da
yeji0214 <1225yj50@naver.com>
2025-03-18 17:59:34 +0900

    feat: 영화 리스트 가져오기

--- src/main.ts
+++ src/main.ts
@@ -14,25 +14,35 @@ async function fetchPopularMovieList() {
 
   const response = await fetch(url, options)
   .then(res => res.json())
-  .then(json => console.log(json))
+  .then(json => json)
   .catch(err => console.error(err));
-	
+
 	return response;
 }
 
-console.log(fetchPopularMovieList());
+// console.log(await fetchPopularMovieList().results);
 
-addEventListener("load", () => {
+addEventListener("load", async () => {
   const app = $("#app");
   
   const header = Header({title:"인사이드 아웃2"});
   if (!header) return;
   const footer = Footer();
-  const movieItem = MovieItem({
-    src:"https://media.themoviedb.org/t/p/w440_and_h660_face/pmemGuhr450DK8GiTT44mgwWCP7.jpg",
-    title:"인사이드 아웃2",
-    rate:7.7
+  const movies = await fetchPopularMovieList()
+  const movieItem = movies.results.map((movie)=>{
+    // console.log(movie)
+    return MovieItem({
+      src:movie.poster_path,
+      title:movie.title,
+      rate:movie.vote_average
+    })
   })
+  console.log(movieItem)
+  // MovieItem({
+  //   src:"https://media.themoviedb.org/t/p/w440_and_h660_face/pmemGuhr450DK8GiTT44mgwWCP7.jpg",
+  //   title:"인사이드 아웃2",
+  //   rate:7.7
+  // })
 
   if (app) {
     app.appendChild(header);
