cd5a4d9092801f9b8169d988e0c675e65a50746c
wo-o29 <woogur29@gmail.com>
2025-03-18 16:14:57 +0900

    ​​refactor: dom 변수 `$` prefix 추가
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/components/MovieItem/MovieItem.ts
+++ src/components/MovieItem/MovieItem.ts
@@ -2,36 +2,36 @@ import { MovieDetail } from "../../../types/type";
 
 const IMAGE_PATH_PREFIX = "https://image.tmdb.org/t/p/w440_and_h660_face";
 const $MovieItem = ({ title, poster_path, vote_average }: MovieDetail) => {
-  const rate = createElement("p", {
+  const $rate = createElement("p", {
     className: "rate",
   });
-  const star = createElement("img", {
+  const $star = createElement("img", {
     src: "./images/star_empty.png",
     className: "star",
   });
-  const rateValue = createElement("span", {
+  const $rateValue = createElement("span", {
     textContent: `${vote_average}`,
   });
-  rate.append(star, rateValue);
+  $rate.append($star, $rateValue);
 
-  const movieTitle = createElement("strong", {
+  const $movieTitle = createElement("strong", {
     textContent: title,
   });
-  const description = createElement("div", {
+  const $description = createElement("div", {
     className: "item-desc",
   });
-  description.append(rate, movieTitle);
+  $description.append($rate, $movieTitle);
 
-  const item = createElement("li", {
+  const $item = createElement("li", {
     className: "item",
   });
-  const poster = createElement("img", {
+  const $poster = createElement("img", {
     className: "thumbnail",
     src: IMAGE_PATH_PREFIX + poster_path,
     alt: title,
   });
-  item.append(poster, description);
-  return item;
+  $item.append($poster, $description);
+  return $item;
 };
 
 export default $MovieItem;
