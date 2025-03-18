142cc8d11eff29f7826672fdbd60012a94cba6bd
장정안 <wjddks96@naver.com>
2025-03-18 21:31:35 +0900

    feat: search 아이콘 Images에 추가

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -4,4 +4,5 @@ export const images = {
   starFull: "./star_filled.png",
   woowacourse: "./woowacourse_logo.png",
   modalButton: "./modal_button_close.png",
+  search: "./search.png",
 } as const;
