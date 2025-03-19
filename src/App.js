60ee2ca52ce55c1ccc32fa1db222f219e3bb81f8
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-19 13:50:15 +0900

    feat: Banner 컨텐츠 변수화 및 Header 스타일링
    Co-authored-by: 이상희 <sang6299@naver.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer.js";
 import Banner from "./components/Banner/index.js";
 import MovieList from "./components/MovieList/index.js";
 import Header from "./components/Header/index.js";
+import MovieItem from "./components/MovieList/MovieItem.js";
 
 class App {
   #$target;
@@ -16,6 +17,7 @@ class App {
   async #init() {
     this.#movies = await this.#fetchMovies();
     this.#$target.appendChild(this.#template());
+    this.#mount();
   }
 
   #template() {
@@ -23,7 +25,7 @@ class App {
     template.innerHTML = /* html */ `
       <div id="wrap">
         ${Header()}
-        ${Banner()}
+        ${Banner(this.#movies[0])}
 
         <div class="container">
           ${MovieList(this.#movies)}
@@ -35,9 +37,9 @@ class App {
     return template.content;
   }
 
-  async #fetchMovies() {
+  async #fetchMovies(page = 1) {
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+      `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${page}`,
       {
         method: "GET",
         headers: {
@@ -47,6 +49,22 @@ class App {
     );
     const data = await response.json();
     return data.results;
+  }
+
+  #mount() {
+    const $background = this.#$target.querySelector(".background-container");
+    $background.style.backgroundImage = `url(https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces${
+      this.#movies[0].backdrop_path
+    })`;
+
+    window.addEventListener("scroll", () => {
+      const $header = document.querySelector("header");
+      if (window.scrollY > 0) {
+        $header.classList.add("scrolled");
+        return;
+      }
+      $header.classList.remove("scrolled");
+    });
   }
 }
 
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,4 +1,4 @@
-const Banner = () => {
+const Banner = ({ vote_average, title }) => {
   return /* html */ `
     <div class="background-container">
       <div class="overlay" aria-hidden="true" ></div>
@@ -6,9 +6,9 @@ const Banner = () => {
         <div class="top-rated-movie">
           <div class="rate">
             <img src="./images/star_empty.png" class="star" />
-            <span class="rate-value">9.5</span>
+            <span class="rate-value">${vote_average}</span>
           </div>
-          <div class="title">인사이드 아웃2</div>
+          <div class="title">${title}</div>
           <button class="primary detail">자세히 보기</button>
         </div>
       </div>
