aeeafb4201ffe4a2959a236668e912aae741b9ed
deokbin <deokbin@naver.com>
2025-03-19 15:41:39 +0900

    feat: 헤더 및 푸터 렌더링 함수 추가

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import MovieList from "./components/MovieList";
+import { MovieInfo } from "../types/movieType.ts";
 
 const options = {
   method: "GET",
@@ -16,6 +19,10 @@ async function getPopularMovies() {
 
   if (response.status === 200) {
     const data = await response.json();
+    // 헤더 렌더링
+    renderHeader(data.results[0]);
+
+    // MovieList 렌더링
     const movieList = new MovieList(data.results);
     const $listContainer = movieList.renderMovieList();
     const $section = document.querySelector("section");
@@ -23,4 +30,19 @@ async function getPopularMovies() {
   }
 }
 
+function renderHeader({ title, poster_path, vote_average }: MovieInfo) {
+  const $container = document.querySelector("#wrap");
+  const $header = Header({ title, poster_path, vote_average });
+
+  $container?.prepend($header);
+}
+
+function renderFooter() {
+  const $container = document.querySelector("#wrap");
+  const $footer = Footer();
+  $container?.appendChild($footer);
+}
+
 getPopularMovies();
+
+renderFooter();
