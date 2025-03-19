c47ce1ecbddc1d93d9f00a3da26c9a4662f67dec
wo-o29 <woogur29@gmail.com>
2025-03-19 13:32:36 +0900

    ​​refactor: 안쓰는 태그 제거
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/components/Banner/Banner.ts
+++ src/components/Banner/Banner.ts
@@ -6,13 +6,11 @@ const $Banner = () => {
   $backgroundContainer.innerHTML = `
   <div class="overlay" aria-hidden="true"></div>
   <div class="top-rated-container">
-    <div class="top-rated-movie">
-      <div class="rate">
-        <img src="./images/star_empty.png" class="star" />
-        <span class="rate-value">9.5</span>
-      </div>
-      <div class="title">인사이드 아웃2</div>
+    <div class="rate">
+      <img src="./images/star_empty.png" class="star" />
+      <span class="rate-value">9.5</span>
     </div>
+    <div class="title">인사이드 아웃2</div>
   </div>`;
 
   return $backgroundContainer;
