2b0736bef46192d3a9a7478b9536ed422038cc79
장정안 <wjddks96@naver.com>
2025-03-19 11:46:29 +0900

    feat: domHelper 내 isTarget 유틸 함수 작성

--- src/utils/domHelper.ts
+++ src/utils/domHelper.ts
@@ -22,12 +22,22 @@ export const $$ = (
 };
 
 export const parseAttribute = (attribute: Attribute) => {
-  console.log(
-    Object.entries(attribute)
-      .map(([key, value]) => `${key} = ${value}`)
-      .join("")
-  );
   return Object.entries(attribute)
     .map(([key, value]) => `${key}="${value}"`)
     .join("");
+};
+
+export const isTarget = (
+  target: EventTarget | null,
+  {
+    targetSelector,
+    parentSelector,
+  }: { targetSelector: string; parentSelector: string }
+) => {
+  const children = $$(targetSelector, $(parentSelector));
+
+  if (target instanceof Element && children)
+    return [...children].includes(target) || target.closest(targetSelector);
+
+  return false;
 };
