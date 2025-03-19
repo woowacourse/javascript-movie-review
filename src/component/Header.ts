a0538957e0c9ceaa0d484265618be6e85e6f0b3c
youdame <112458620+youdame@users.noreply.github.com>
2025-03-19 14:16:55 +0900

    feat: 헤더 api 값 반영

--- src/component/Header.ts
+++ src/component/Header.ts
@@ -2,13 +2,12 @@ import { Movie } from "../type";
 import Button from "./Button";
 import SearchBar from "./SearchBar";
 
-const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w1920";
 function Header({ movie }: { movie: Movie }) {
-  console.log(movie);
   return /*html*/ `<header>
     <div class="background-container">
       <div class="overlay" aria-hidden="true">
-      <img src=${IMAGE_BASE_URL}${movie.poster_path} />
+      <img src=${IMAGE_BASE_URL}${movie.backdrop_path} />
       </div>
       <div class="top-rated-container">
         <div class="logo">
@@ -18,9 +17,9 @@ function Header({ movie }: { movie: Movie }) {
         <div class="top-rated-movie">
           <div class="rate">
             <img src="images/star_empty.png" class="star" />
-            <span class="rate-value">9.5</span>
+            <span class="rate-value">${movie.vote_average.toFixed(1)}</span>
           </div>
-          <div class="title">인사이드 아웃2</div>
+          <div class="title">${movie.title}</div>
          ${Button({ text: "자세히보기" })}
         </div>
       </div>
