20d7cdcef4957038aa98265c42b3d1157e074416
ohgus <anakin1108@naver.com>
2025-03-19 14:41:51 +0900

    feat: 검색 API 로직 구현

---
+++ src/features/search/api/getSearchedPost.ts
@@ -0,0 +1,19 @@
+const url = (query: string, page: number) =>
+  `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=ko-KR&page=${page}`;
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+  },
+};
+
+export const getSearchedPost = async (query: string, page: number) => {
+  const response = await fetch(url(query, page), options);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch searched post");
+  }
+
+  return response.json();
+};
