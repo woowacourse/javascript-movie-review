b1973f56b0c72fc7041707657609722f4cb904c6
keemsebeen <sebin0580@gmail.com>
2025-03-19 11:12:05 +0900

    refactor: App 컴포넌트 data 추가
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,16 @@
 import { ENV } from './api/env';
-import Fetcher, { QUERY_PARAMS } from './api/Fetcher';
+import Fetcher, { API_OPTION } from './api/Fetcher';
+import { QUERY_PARAMS } from './api/MovieFetcher';
 import { Footer } from './components/common/Footer';
 import { Header } from './components/common/Header';
 import { MovieList } from './components/feature/MovieList';
-import { MOVIE_RESPONSE } from './constants/movie.data';
+import { MovieList as MovieListType } from './types/Movie.types';
 
 export const App = async () => {
   const fetcher = new Fetcher(ENV.VITE_API_URL);
-  const data = await fetcher.get(QUERY_PARAMS.MOVIE);
+  const data = await fetcher.get(QUERY_PARAMS.MOVIE, API_OPTION.headers);
+  console.log('data', data);
   Header();
-  MovieList(MOVIE_RESPONSE);
+  MovieList(data as MovieListType);
   Footer();
 };
