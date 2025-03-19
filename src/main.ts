cbd5b75f2b4c5dc37db667c5d98ade6e573a000c
hoyyChoi <hoyeon8165@gmail.com>
2025-03-19 20:56:01 +0900

    feat: 검색어 기반 api 호출

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,15 @@ const ulElement = document.querySelector(".thumbnail-list") as HTMLUListElement;
 const movieListController = new MovieListController(ulElement);
 await movieListController.getPopularMovieList();
 
-getSearchMovieResult("하하", 1);
+const searchBarElement = document.querySelector(
+  ".search-bar",
+) as HTMLFormElement;
+
+searchBarElement.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const formElement = event.target as HTMLElement;
+  const target = formElement.querySelector("input") as HTMLInputElement;
+
+  const result = await getSearchMovieResult(target.value, 1);
+});
