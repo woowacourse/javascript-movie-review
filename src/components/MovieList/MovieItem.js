1e9807a8c3c1e8c510e51808712f93d551e5b35f
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-19 13:50:38 +0900

    feat: MovieItem 기본 포스터 이미지 지정
    Co-authored-by: 이상희 <sang6299@naver.com>

--- src/components/MovieList/MovieItem.js
+++ src/components/MovieList/MovieItem.js
@@ -1,8 +1,12 @@
 const MovieItem = ({ poster_path, title, vote_average }) => {
+  const imageUrl = poster_path
+    ? `https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}`
+    : "./images/logo.png";
+
   return /* html */ `
     <li>
       <div class="item">
-        <img class="thumbnail" src="https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}" alt="${title}" />
+        <img class="thumbnail" src="${imageUrl}" alt="${title}" />
         <div class="item-desc">
           <p class="rate">
             <img src="./images/star_empty.png" class="star" />
