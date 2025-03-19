dbddec30ce680be2d55bf73350949143c790cbb5
deokbin <deokbin@naver.com>
2025-03-19 14:51:04 +0900

    fix: thumbnailImage 컴포넌트 속성 수정

--- src/components/ThumbnailImage.js
+++ src/components/ThumbnailImage.js
@@ -2,8 +2,8 @@ function ThumbnailImage({ title, imagePath }) {
   const $thumbnailImage = document.createElement("img");
 
   $thumbnailImage.classList.add("thumbnail");
-  $thumbnailImage.classList.src = imagePath;
-  $thumbnailImage.classList.alt = `${title} Thumbnail 이미지`;
+  $thumbnailImage.src = `https://media.themoviedb.org/t/p/w440_and_h660_face${imagePath}`;
+  $thumbnailImage.alt = `${title} Thumbnail 이미지`;
 
   return $thumbnailImage;
 }
