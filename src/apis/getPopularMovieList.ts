48c8aefdd7d461285be7247223d93ee1ed56dc30
wo-o29 <woogur29@gmail.com>
2025-03-19 19:54:51 +0900

    feat: 인기 영화 및 검색 영화 목록 API 응답 오류 처리 추가
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/apis/getPopularMovieList.ts
+++ src/apis/getPopularMovieList.ts
@@ -2,7 +2,15 @@ import { MovieData } from "../../types/type";
 import { tmdbClient } from "./api";
 
 const getPopularMovieList = async (page: number): Promise<MovieData> => {
-  return tmdbClient.get(`/movie/popular?language=ko-KR&page=${page}`);
+  const response = await tmdbClient.get(
+    `/movie/popular?language=ko-KR&page=${page}`
+  );
+
+  if ("status_message" in response) {
+    throw new Error(response.status_message);
+  }
+
+  return response;
 };
 
 export default getPopularMovieList;
--- src/apis/getSearchedMovieList.ts
+++ src/apis/getSearchedMovieList.ts
@@ -5,9 +5,15 @@ const getSearchedMovieList = async (
   query: string,
   page: number
 ): Promise<MovieData> => {
-  return tmdbClient.get(
+  const response = await tmdbClient.get(
     `/search/movie?query=${query}&language=ko-KR&page=${page}`
   );
+
+  if ("status_message" in response) {
+    throw new Error(response.status_message);
+  }
+
+  return response;
 };
 
 export default getSearchedMovieList;
