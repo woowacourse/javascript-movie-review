0f1c9c2bbd14b4242b19590c1eb0862c7e71d96d
youdame <112458620+youdame@users.noreply.github.com>
2025-03-19 13:59:21 +0900

    feat: api 연결 및 헤더에 이미지 띄우기
    Co-authored-by: seo_dev <jin123457@users.noreply.github.com>

--- src/component/Header.ts
+++ src/component/Header.ts
@@ -1,18 +1,23 @@
+import { Movie } from "../type";
 import Button from "./Button";
 import SearchBar from "./SearchBar";
 
-function Header() {
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+function Header({ movie }: { movie: Movie }) {
+  console.log(movie);
   return /*html*/ `<header>
     <div class="background-container">
-      <div class="overlay" aria-hidden="true"></div>
+      <div class="overlay" aria-hidden="true">
+      <img src=${IMAGE_BASE_URL}${movie.poster_path} />
+      </div>
       <div class="top-rated-container">
         <div class="logo">
-          <img class="logo-img" src="./images/logo.png" alt="MovieList" />
+          <img class="logo-img" src="images/logo.png" alt="MovieList" />
           ${SearchBar()}
         </div>
         <div class="top-rated-movie">
           <div class="rate">
-            <img src="./images/star_empty.png" class="star" />
+            <img src="images/star_empty.png" class="star" />
             <span class="rate-value">9.5</span>
           </div>
           <div class="title">인사이드 아웃2</div>
