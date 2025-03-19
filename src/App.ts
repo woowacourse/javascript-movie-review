ff78df2ffa1077cb0c35acc022aa4b639b7223fa
장정안 <wjddks96@naver.com>
2025-03-19 11:42:37 +0900

    feat: fetchData로 GET하여 영화 리스트 출력
    Co-authored-by: thgml05 <thgml05@users.noreply.github.com>

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 import Button from "./components/@common/Button";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import MovieItem from "./components/movieItem/MovieItem";
 import { reRender } from "./utils/Core";
@@ -10,24 +11,25 @@ const App = () => {
   // 모든 컴포넌트의 부모
   //영화리스트 전체를 GET해와서 Header에 영화 하나 뿌려야 댐.
 
+  const fetchData = async () => {
+    const response = await fetch(import.meta.env.VITE_API_URL, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+      },
+      method: "GET",
+    });
+    const data = await response.json();
+    movies = data.results;
+    console.log("movies", movies);
+    reRender();
+  };
+
   if (movies.length === 0) {
-    const fetchData = async () => {
-      const response = await fetch(import.meta.env.VITE_API_URL, {
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
-        },
-        method: "GET",
-      });
-      const data = await response.json();
-      movies = data.results;
-      console.log("movies", movies);
-      reRender();
-    };
     fetchData();
   }
 
   return `
-    ${Header({ rate: 9.5, title: "인사이드 아웃2" })}
+    ${Header({ rate: movies[0].vote_count, title: movies[0].title })}
     <div class="app-layout">
     <h1 class="sub-title">지금 인기 있는 영화</h1>
     <ul class="thumbnail-list">
@@ -48,6 +50,7 @@ const App = () => {
       children: "더 보기",
     })}
     </div>
+    ${Footer()}
   `;
 };
 
