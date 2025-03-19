daa3ae77e8be50f105da6ad6290e22f6b55eb50d
장정안 <wjddks96@naver.com>
2025-03-19 11:46:53 +0900

    feat: Core 컴포넌트 내 useState 및 event 등록 설정

--- src/utils/Core.ts
+++ src/utils/Core.ts
@@ -1,35 +1,118 @@
-interface Options {
-  root: Element | null;
-  rootComponent: (() => string) | null; //App 컴포넌트
+import { UnPack } from "../../types";
+import { debounce } from "./debounce";
+import { $, isTarget } from "./domHelper";
+
+export interface EventCallback {
+  (e: HTMLElementEventMap[keyof HTMLElementEventMap]): void;
+}
+export interface Event {
+  parentSelector: string;
+  targetSelector: string;
+  event: keyof HTMLElementEventMap;
+  callback: EventCallback;
 }
 
-const Core = () => {
-  const options: Options = {
+export interface Effect {
+  callback(): unknown;
+  deps: unknown[];
+}
+
+interface Options<T = unknown> {
+  currentStateKey: number;
+  currentEffectsKey: number;
+  states: T[];
+  events: Event[];
+  effects: Effect[];
+  root: null | Element;
+  rootComponent: null | (() => string);
+}
+
+interface Dispatch<T> {
+  (value: T): void;
+}
+
+function Core() {
+  const options: Options<UnPack<Parameters<typeof useState>>> = {
+    currentStateKey: 0,
+    currentEffectsKey: 0,
+    states: [],
+    events: [],
+    effects: [],
     root: null,
     rootComponent: null,
   };
 
-  const _render = () => {
+  function useState<S = undefined>(initialState?: S): [S, Dispatch<S>];
+  function useState<S = undefined>(initialState?: S): [unknown, Dispatch<S>] {
+    const { currentStateKey: key, states } = options;
+    if (states.length === key) states.push(initialState);
+
+    const state = states[key];
+
+    const setState = (newState: S) => {
+      if (newState === state) return;
+
+      states[key] = newState;
+      _render();
+    };
+    options.currentStateKey += 1;
+
+    return [state, setState];
+  }
+
+  const _render = debounce(() => {
     const { root, rootComponent } = options;
     if (!root || !rootComponent) return;
-
     root.innerHTML = rootComponent();
-  };
+    options.currentStateKey = 0;
+    options.currentEffectsKey = 0;
 
-  const render = (
+    _addEvent();
+
+    options.events = [];
+  });
+
+  function render(
     rootComponent: Options["rootComponent"],
     root: Options["root"]
-  ) => {
+  ) {
     options.root = root;
     options.rootComponent = rootComponent;
-
     _render();
-  };
+  }
 
-  const reRender = () => {
+  function useEvents(parentSelector: string) {
+    function addEvent(
+      event: Event["event"],
+      targetSelector: Event["targetSelector"],
+      callback: Event["callback"]
+    ) {
+      const { events } = options;
+
+      events.push({ event, targetSelector, parentSelector, callback });
+    }
+
+    return [addEvent];
+  }
+
+  function _addEvent() {
+    options.events.forEach(
+      ({ parentSelector, targetSelector, event, callback }) => {
+        $(parentSelector)?.addEventListener(event, (e) => {
+          const $parent = $(parentSelector);
+
+          if (isTarget(e.target, { targetSelector, parentSelector }) && $parent)
+            callback(e);
+        });
+      }
+    );
+  }
+
+  function reRender() {
     _render();
-  };
-  return { render, reRender };
-};
+  }
 
-export const { render, reRender } = Core();
+  return { useState, useEvents, render, reRender };
+}
+
+export const { useState, useEvents, render, reRender } = Core();
