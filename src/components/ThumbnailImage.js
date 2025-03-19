7b74f57a8554296db69f85649cd608e6a92be0ed
deokbin <deokbin@naver.com>
2025-03-19 17:28:00 +0900

    feat: 이미지 경로가 null인 경우 기본 이미지로 대체

--- src/components/ThumbnailImage.js
+++ src/components/ThumbnailImage.js
@@ -2,7 +2,11 @@ function ThumbnailImage({ title, poster_path }) {
   const $thumbnailImage = document.createElement("img");
 
   $thumbnailImage.classList.add("thumbnail");
-  $thumbnailImage.src = `https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}`;
+  if (!poster_path) {
+    $thumbnailImage.src = "./default_poster_image.png";
+  } else {
+    $thumbnailImage.src = `https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}`;
+  }
   $thumbnailImage.alt = `${title} Thumbnail 이미지`;
 
   return $thumbnailImage;
