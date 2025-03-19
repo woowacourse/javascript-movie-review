aef9d6bd18aeb473758f26835edb895a5396c4aa
wo-o29 <woogur29@gmail.com>
2025-03-19 16:43:11 +0900

    ​​feat: Skeleton 컴포넌트 추가
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

---
+++ src/components/Skeleton/List/SkeletonList.ts
@@ -0,0 +1,28 @@
+import $SkeletonItem from "../Item/SkeletonItem";
+
+export const replaceSkeletonList = () => {
+  const $movieListSection = document.querySelector(
+    ".movie-list-section"
+  ) as HTMLElement;
+  $movieListSection.replaceChildren($SkeletonList());
+};
+
+export const addSkeletonList = () => {
+  const $movieListSection = document.querySelector(
+    ".thumbnail-list"
+  ) as HTMLElement;
+  $movieListSection.appendChild($SkeletonList());
+};
+
+export const removeSkeletonList = () => {
+  const $skeletonList = document.querySelector(".skeleton-list");
+  $skeletonList?.remove();
+};
+
+const $SkeletonList = () => {
+  const $skeletonList = createElement("ul", { className: "skeleton-list" });
+  $skeletonList.append(...Array.from({ length: 20 }, () => $SkeletonItem()));
+  return $skeletonList;
+};
+
+export default $SkeletonList;
