d11839608760007fa4adbb2c549de2004c8e676a
wo-o29 <woogur29@gmail.com>
2025-03-19 14:24:01 +0900

    refactor: SearchBox -> SearchForm 이름 변경​​
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

---
+++ src/components/SearchForm/SearchForm.ts
@@ -0,0 +1,48 @@
+import getSearchedMovieList from "../../apis/getSearchedMovieList";
+import { replaceMovieListBox } from "../../main";
+import { removeBanner } from "../Banner/Banner";
+
+const handleSearchFormSubmit = async (e: Event) => {
+  e.preventDefault();
+  const target = e.target as HTMLFormElement;
+  const searchValue = target["search-input"].value;
+
+  try {
+    const searchResult = await getSearchedMovieList(searchValue, 1);
+    removeBanner();
+    replaceMovieListBox({
+      title: `"${searchValue}" 검색 결과`,
+      movieList: searchResult.results,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message);
+    }
+  }
+};
+
+const $SearchForm = () => {
+  const $searchForm = createElement("form", {
+    className: "search-box",
+  });
+  const $searchInput = createElement("input", {
+    className: "search-input",
+    name: "search-input",
+    type: "text",
+    placeholder: "검색어를 입력하세요",
+  });
+  const $searchButton = createElement("button", {
+    type: "submit",
+  });
+  const $searchIcon = createElement("img", {
+    src: "/images/search.svg",
+    alt: "search",
+  });
+  $searchButton.appendChild($searchIcon);
+
+  $searchForm.append($searchInput, $searchButton);
+  $searchForm.addEventListener("submit", handleSearchFormSubmit);
+  return $searchForm;
+};
+
+export default $SearchForm;
