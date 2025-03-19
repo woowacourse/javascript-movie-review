3f5e29cae8c1447adf8f41932f1d8801ce18be6e
장정안 <wjddks96@naver.com>
2025-03-19 20:15:26 +0900

    feat: 공통 컴포넌트 Button에 PropsWithChildren 삽입

--- src/components/@common/Button.ts
+++ src/components/@common/Button.ts
@@ -1,16 +1,17 @@
+import { PropsWithChildren } from "../../../types";
 import { Attribute, parseAttribute } from "../../utils/domHelper";
 
 interface ButtonProps {
   attribute: Attribute;
-  children: string;
 }
 
-const Button = (props: ButtonProps) => {
+const Button = (props: PropsWithChildren<ButtonProps>) => {
   const { attribute, children } = props;
 
-  return `<button ${
-    attribute ? parseAttribute(attribute) : ""
-  }">${children}</button>`;
+  return `
+    <button ${
+      attribute ? parseAttribute(attribute) : ""
+    }" >${children}</button>`;
 };
 
 export default Button;
