f4687d2828bd20a220f6e74582a1d3364f8bfd8c
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-19 17:56:19 +0900

    feat: search API 불러오기

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { getSearchMovieResult } from "./api/getSearchMovieResult";
 import MovieListController from "./controller/MovieListController";
 
 const ulElement = document.querySelector(".thumbnail-list") as HTMLUListElement;
 const movieListController = new MovieListController(ulElement);
 await movieListController.getPopularMovieList();
+
+getSearchMovieResult("하하", 1);
