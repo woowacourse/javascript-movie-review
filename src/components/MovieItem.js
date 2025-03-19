ee7448e09ac961d54da8327ae20115459ec5f95f
deokbin <deokbin@naver.com>
2025-03-19 13:57:29 +0900

    feat: movieItem 함수에 인자 전달

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,18 +1,18 @@
 import MovieCaption from "./MovieCaption";
 import ThumbnailImage from "./ThumbnailImage";
 
-function MovieItem() {
+function MovieItem({ title, poster_path, vote_average }) {
   const $movieItem = document.createElement("li");
   const $movieItemContainer = document.createElement("div");
   $movieItemContainer.classList.add("item");
 
   const thumbnailImage = ThumbnailImage({
-    title: this.title,
-    imagePath: this.poster_path,
+    title,
+    imagePath: poster_path,
   });
   const movieCaption = MovieCaption({
-    title: this.title,
-    vote_average: this.vote_average,
+    title,
+    vote_average,
   });
 
   $movieItemContainer.appendChild(thumbnailImage);
