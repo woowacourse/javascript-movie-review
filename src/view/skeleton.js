26b38c00f688c08d4e8bcaaf3fba049a818eb578
spoyodevelop <kjy9505@gmail.com>
2025-03-19 20:59:06 +0900

    feat:  스켈레톤 요소 추가
    Co-authored-by: yeongipark <yeongipark@users.noreply.github.com>

--- src/view/skeleton.js
+++ src/view/skeleton.js
@@ -1,11 +1,9 @@
 export function showSkeleton() {
-  const skeleton = document.querySelector("#skeleton-list");
+  const skeleton = document.querySelector(".skeleton-list");
   if (skeleton) skeleton.classList.remove("hide");
-  if (skeleton) skeleton.classList.add("show");
 }
 
 export function hideSkeleton() {
-  const skeleton = document.querySelector("#skeleton-list");
+  const skeleton = document.querySelector(".skeleton-list");
   if (skeleton) skeleton.classList.add("hide");
-  if (skeleton) skeleton.classList.remove("show");
 }
