154ea897189de41c2145aeeb72efa38d10667b39
youdame <112458620+youdame@users.noreply.github.com>
2025-03-19 22:06:47 +0900

    feat: 검색 기능 구현
    Co-authored-by: seo_dev <jin123457@users.noreply.github.com>

--- src/component/MovieList.ts
+++ src/component/MovieList.ts
@@ -2,18 +2,40 @@ import { IMovie } from '../type';
 import createDOMElement from '../util/createDomElement';
 import Movie from './Movie';
 
-function MovieList({ movies }: { movies: IMovie[] }) {
+function MovieList({ title, movies }: { title: string; movies: IMovie[] }) {
   return createDOMElement({
     tag: 'div',
     children: [
       createDOMElement({
         tag: 'h2',
-        innerText: '지금 인기 있는 영화'
+        textContent: title
+      }),
+
+      movies.length !== 0
+        ? createDOMElement({
+            tag: 'ul',
+            className: 'thumbnail-list',
+            children: movies.map((movie) => Movie({ movie }))
+          })
+        : NoResult()
+    ]
+  });
+}
+
+function NoResult() {
+  return createDOMElement({
+    tag: 'div',
+    className: 'no-result',
+    children: [
+      createDOMElement({
+        tag: 'img',
+        attributes: {
+          src: 'images/으아아행성이.svg'
+        }
       }),
       createDOMElement({
-        tag: 'ul',
-        className: 'thumbnail-list',
-        children: movies.map((movie) => Movie({ movie }))
+        tag: 'p',
+        textContent: '검색 결과가 없습니다.'
       })
     ]
   });
