2ebca9c5a966a27b3d24b2e6b78dfbf762cc60c5
keemsebeen <sebin0580@gmail.com>
2025-03-19 13:49:41 +0900

    chore: 불필요한 import 제거
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,9 @@
-import { ENV } from './api/env';
-import Fetcher, { API_OPTION } from './api/Fetcher';
-import { QUERY_PARAMS } from './api/MovieFetcher';
 import { Footer } from './components/common/Footer';
 import { Header } from './components/common/Header';
 import { MovieList } from './components/feature/MovieList';
-import { MovieList as MovieListType } from './types/Movie.types';
 
 export const App = async () => {
-  const fetcher = new Fetcher(ENV.VITE_API_URL);
-  const data = await fetcher.get(QUERY_PARAMS.MOVIE, API_OPTION.headers);
-  console.log('data', data);
   Header();
-  MovieList(data as MovieListType);
+  MovieList();
   Footer();
 };
