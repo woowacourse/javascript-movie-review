ddcecbcd66fc9e6b9e6436cea515787b28fe0c86
wo-o29 <woogur29@gmail.com>
2025-03-19 15:11:46 +0900

    ​​feat: 영화 리스트에 영화를 추가하는 함수 구현
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/components/MovieList/MovieList.ts
+++ src/components/MovieList/MovieList.ts
@@ -2,6 +2,17 @@ import { MovieDetail } from "../../../types/type";
 import $EmptyList from "../EmptyList/EmptyList";
 import $MovieItem from "../MovieItem/MovieItem";
 
+export const addMovieItem = (movieList: MovieDetail[]) => {
+  const $movieList = document.querySelector(
+    ".thumbnail-list"
+  ) as HTMLUListElement;
+
+  const movieListFragment = wrapFragment(
+    movieList.map((movieItemData) => $MovieItem(movieItemData))
+  );
+  $movieList.appendChild(movieListFragment);
+};
+
 const $MovieList = (movieList: MovieDetail[]) => {
   if (!movieList.length) {
     return $EmptyList();
