8922096ae3a02e94fb799a84eeefc3d6674bcf48
yeji0214 <1225yj50@naver.com>
2025-03-19 10:41:50 +0900

    feat: 인기순 영화 목록 20개 가져오기
    Co-authored-by: 서민지 <minji2219@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -3,50 +3,64 @@ import Header from "./components/layout/Header.ts";
 import MovieItem from "./components/movie/MovieItem.ts";
 import { $ } from "./utils/dom.ts";
 
-async function fetchPopularMovieList() {
-  const url = "https://api.themoviedb.org/3/movie/popular";
+type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+async function fetchPopularMovieList(): Promise<{ results: Movie[] }> {
+  const url = "https://api.themoviedb.org/3/movie/popular?language=ko-KR";
   const options = {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-      accept: 'application/json'
-    }
+      accept: "application/json",
+    },
+  };
+
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
   }
 
-  const response = await fetch(url, options)
-  .then(res => res.json())
-  .then(json => json)
-  .catch(err => console.error(err));
-
-	return response;
+  return response.json();
 }
 
-// console.log(await fetchPopularMovieList().results);
+const movieList = document.createElement("ul");
+movieList.classList.add("thumbnail-list");
 
 addEventListener("load", async () => {
   const app = $("#app");
-  
-  const header = Header({title:"인사이드 아웃2"});
+
+  const header = Header({ title: "인사이드 아웃2" });
   if (!header) return;
   const footer = Footer();
-  const movies = await fetchPopularMovieList()
-  const movieItem = movies.results.map((movie)=>{
-    // console.log(movie)
-    return MovieItem({
-      src:movie.poster_path,
-      title:movie.title,
-      rate:movie.vote_average
-    })
-  })
-  console.log(movieItem)
-  // MovieItem({
-  //   src:"https://media.themoviedb.org/t/p/w440_and_h660_face/pmemGuhr450DK8GiTT44mgwWCP7.jpg",
-  //   title:"인사이드 아웃2",
-  //   rate:7.7
-  // })
+
+  const movies = await fetchPopularMovieList();
+  movies.results.forEach((movie: Movie) => {
+    const movieElement = MovieItem({
+      src: `https://image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path}`,
+      title: movie.title,
+      rate: movie.vote_average,
+    });
+
+    movieList.appendChild(movieElement);
+  });
 
   if (app) {
     app.appendChild(header);
-    app.appendChild(movieItem);
+    app.appendChild(movieList);
     app.appendChild(footer);
   }
 });
