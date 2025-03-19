293a51f55a7e058481dda0f114ad7986636ff792
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-19 14:13:15 +0900

    feat: 검색창 form 태그로 wrapping

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,10 +1,12 @@
 const SearchBar = () => {
   return /* html */ `
     <div class="search-bar-container">
-      <input type="text" class="search-bar" placeholder="검색어를 입력하세요" />
-      <button class="search-button">
-        <img src="./images/search.png" alt="search" width="16" height="16" />
-      </button>
+      <form id="search-form" class="search-form">
+        <input type="text" class="search-bar" placeholder="검색어를 입력하세요" />
+        <button type="submit" class="search-button">
+          <img src="./images/search.png" alt="search" width="16" height="16" />
+        </button>
+      </form>
     </div>
   `;
 };
