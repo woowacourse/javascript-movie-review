c47d770475661017e8a68b4ae7ebb530894d0299
youdame <112458620+youdame@users.noreply.github.com>
2025-03-19 14:56:10 +0900

    feat: 더보기 버튼 추가

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import getPopularMovies from './api/getPopularMovies';
+import Button from './component/Button';
 import Footer from './component/Footer';
 import Header from './component/Header';
 import MovieList from './component/MovieList';
@@ -22,7 +23,11 @@ const renderMovieList = async () => {
   const { results: movies } = await getPopularMovies({ page: 1 });
   const container = $('.container');
   const movieList = MovieList({ movies });
+
+  const moreButton = Button({ text: '더보기' });
+
   container?.insertAdjacentHTML('afterbegin', movieList);
+  container?.insertAdjacentHTML('beforeend', moreButton);
 };
 
 const renderFooter = () => {
