ce0f6d21aca28c4d205fc43ac6f84d5387a51014
ShinjungOh <tlswjd9260@gmail.com>
2025-03-19 15:34:49 +0900

    feat: MovieItem 컴포넌트 구현 및 Movie 타입 지정
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- src/components/MovieItem.ts
+++ src/components/MovieItem.ts
@@ -1,28 +1,22 @@
-type MovieItemProps = {
-  rate: number;
-  name: string;
-  imgSrc: string;
-};
+import { Movie } from "../../types/movie";
 
-const MovieItem = ({ rate, name, imgSrc }: MovieItemProps) => {
+const MovieItem = ({ rate, title, imgSrc }: Movie) => {
   const movieItem = document.createElement("li");
-
-  console.log(rate, name, imgSrc);
 
   movieItem.innerHTML = `
                 <div class="item">
                   <img
                     class="thumbnail"
-                    src="https://media.themoviedb.org/t/p/w440_and_h660_face/pmemGuhr450DK8GiTT44mgwWCP7.jpg"
-                    alt="인사이드 아웃 2"
+                    src=${imgSrc}
+                    alt=${title}
                   />
                   <div class="item-desc">
                     <p class="rate">
                       <img src="images/star_empty.png" class="star" /><span
-                        >7.7</span
+                        >${rate}</span
                       >
                     </p>
-                    <strong>인사이드 아웃 2</strong>
+                    <strong>${title}</strong>
                   </div>
                 </div>
               `;
---
+++ types/movie.ts
@@ -0,0 +1,6 @@
+export type Movie = {
+  id?: number;
+  title: string;
+  rate: number;
+  imgSrc: string;
+};
