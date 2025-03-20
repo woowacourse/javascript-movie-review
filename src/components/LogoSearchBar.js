e8a27e73b70514cbf1c069fba8ad4054b051f737
deokbin <deokbin@naver.com>
2025-03-20 15:27:06 +0900

    feat: LogoSearchBar 요소를 반환하도록 수정

--- src/components/LogoSearchBar.js
+++ src/components/LogoSearchBar.js
@@ -1,15 +1,19 @@
 function LogoSearchBar() {
-  return `
-  <h1 class="logo">
-  <img src="./logo.png" alt="MovieList" />
-  </h1>
+  const container = document.createElement("div");
+
+  container.innerHTML = `
+    <h1 class="logo">
+      <img src="./logo.png" alt="MovieList" />
+    </h1>
     <div class="search-container">
       <input placeholder="검색어를 입력하세요." class="search-input"/> 
       <button class="search-button">
         <img src="./search.svg" alt="검색"/>
       </button> 
-  </div>
+    </div>
   `;
+
+  return container;
 }
 
 export default LogoSearchBar;
