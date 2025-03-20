f2d4fb86e09877a534ed88742eb1d6f5aa7326b9
spoyodevelop <kjy9505@gmail.com>
2025-03-20 13:40:03 +0900

    refactor: skeleton 관련 function 통합

--- src/view/skeleton.js
+++
@@ -1,9 +0,0 @@
-export function showSkeleton() {
-  const skeleton = document.querySelector(".skeleton-list");
-  if (skeleton) skeleton.classList.remove("hide");
-}
-
-export function hideSkeleton() {
-  const skeleton = document.querySelector(".skeleton-list");
-  if (skeleton) skeleton.classList.add("hide");
-}
