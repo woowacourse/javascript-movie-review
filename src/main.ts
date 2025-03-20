f02f94ce69eeb616d3e400c3a450880ce759eb7f
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-20 16:44:10 +0900

    🐛 fix: 배포를 위한 즉시 실행 함수 사용
    Co-authored-by: kimyou1102 <kimyou1102@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import App from "./App.js";
 
 const app = new App();
-
-await app.init();
+(async function () {
+  await app.init();
+})();
 // https://api.themoviedb.org/3/search/movie?query=%EC%A7%B1%EA%B5%AC&include_adult=false&language=en-US&page=1
