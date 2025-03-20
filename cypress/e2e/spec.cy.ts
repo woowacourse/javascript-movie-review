3f3467194ea9ccf7c1b0b2b7932982ddd9ea9b69
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-20 12:24:46 +0900

    ✅ test: 더보기 버튼 클릭 시 새로운 영화 랜더링
    Co-authored-by: kimyou1102 <kimyou1102@users.noreply.github.com>

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,26 +2,45 @@
 
 describe("Fixture를 이용한 E2E 테스트", () => {
   beforeEach(() => {
+    // 첫 번째 요청 (초기 로딩)
     cy.intercept(
-      {
-        method: "GET",
-        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular*/,
-      },
+      "GET",
+      /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular(\?.*)?$/,
       { fixture: "movie-popular.json" }
     ).as("getPopularMovies");
+
+    cy.intercept(
+      "GET",
+      /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular\?language=ko-KR&page=2$/,
+      { fixture: "movie-popular-page2.json" }
+    ).as("getPopularMoviesPage2");
 
     cy.visit("http://localhost:5173");
   });
 
-  it("영화 목록 API를 호출하면 20개의 영화가 랜더링 되야한다.", () => {
+  it("영화 목록 API를 호출하면 20개의 영화가 랜더링 되어야 한다.", () => {
     cy.wait("@getPopularMovies").then((interception) => {
-      // interception으로 fixture가 잘 불러와졌는지 확인하는 코드 샘플
+      // Fixture가 정상적으로 불러와졌는지 확인
       const popularMovies = interception.response?.body.results;
       expect(popularMovies.length).to.equal(20);
-
-      // 20개의 영화가 랜더링 되었는지 확인
-      const popularMovieItems = cy.get(".thumbnail-list > li");
-      expect(popularMovieItems.should("have.length", 20));
     });
+
+    // 영화 목록이 정상적으로 20개 렌더링 되는지 확인
+    cy.get(".thumbnail-list > li").should("have.length", 20);
+  });
+
+  it("더보기 버튼 클릭 시 20개의 영화가 추가된다.", () => {
+    // 첫 번째 API 응답 기다리기
+    cy.wait("@getPopularMovies");
+    cy.get(".thumbnail-list > li").should("have.length", 20);
+
+    // 더보기 버튼 클릭
+    cy.get(".more-button").should("exist").click();
+
+    // 두 번째 API 응답 기다리기
+    cy.wait("@getPopularMoviesPage2");
+
+    // 영화 개수가 40개인지 확인
+    cy.get(".thumbnail-list > li").should("have.length", 40);
   });
 });
