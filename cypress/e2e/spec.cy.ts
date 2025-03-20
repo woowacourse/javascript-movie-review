1c29264b9205e83b09524fe35bfaeafa0f044713
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-20 11:23:24 +0900

    ✅ test: 페이지 방문 시 영화 20개 랜더링 테스트
    Co-authored-by: kimyou1102 <kimyou1102@users.noreply.github.com>

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,27 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("localhost:5173");
+/// <reference types="cypress" />
+
+describe("Fixture를 이용한 E2E 테스트", () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular*/,
+      },
+      { fixture: "movie-popular.json" }
+    ).as("getPopularMovies");
+
+    cy.visit("http://localhost:5173");
+  });
+
+  it("영화 목록 API를 호출하면 20개의 영화가 랜더링 되야한다.", () => {
+    cy.wait("@getPopularMovies").then((interception) => {
+      // interception으로 fixture가 잘 불러와졌는지 확인하는 코드 샘플
+      const popularMovies = interception.response?.body.results;
+      expect(popularMovies.length).to.equal(20);
+
+      // 20개의 영화가 랜더링 되었는지 확인
+      const popularMovieItems = cy.get(".thumbnail-list > li");
+      expect(popularMovieItems.should("have.length", 20));
+    });
   });
 });
