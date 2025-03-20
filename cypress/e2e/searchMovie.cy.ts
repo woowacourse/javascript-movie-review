032107a9d92e65ac2170fa0fcb7dae96fe5ec817
mlnwns <mlnwns@naver.com>
2025-03-20 17:11:38 +0900

    refactor: 영화 검색 결과에 대한 비동기 API 테스트 개선
    Co-authored-by: 신종욱 <dlsxjzld@users.noreply.github.com>

--- cypress/e2e/searchMovie.cy.ts
+++ cypress/e2e/searchMovie.cy.ts
@@ -8,61 +8,16 @@ describe("영화 검색 비동기 API 테스트", () => {
   it("미키 17 검색하면 1개만 나오고 더보기 버튼 안보여야 한다", () => {
     cy.get("#search-bar").type("미키 17");
 
-    const query = encodeURIComponent("미키 17");
-
-    const searchedMovieUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=ko-KR&page=1`;
-
-    const options = {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${Cypress.env("TMDB_TOKEN")}`,
-      },
-    };
-
-    cy.intercept({
-      method: "GET",
-      url: searchedMovieUrl,
-      ...options,
-    }).as("searchMovies");
-
     cy.get(".search-icon").click();
-    cy.wait("@searchMovies")
-      .its("response")
-      .should((response) => {
-        expect(response?.statusCode).to.eq(200);
-        expect(response?.body.results).to.have.length(1);
-      });
+    cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 1);
     cy.get(".see-more").should("not.exist");
   });
 
-  it("짱구 검색하면 20개 나오고 더보기 버튼이 보여야 한다.", () => {
+  it("짱구 검색하면 20개 나오고 '더보기' 버튼이 보여야 한다.", () => {
     cy.get("#search-bar").type("짱구");
-
-    const query = encodeURIComponent("짱구");
-
-    const searchedMovieUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=ko-KR&page=1`;
-
-    const options = {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${Cypress.env("TMDB_TOKEN")}`,
-      },
-    };
-
-    cy.intercept({
-      method: "GET",
-      url: searchedMovieUrl,
-      ...options,
-    }).as("searchMovies");
-
     cy.get(".search-icon").click();
 
-    cy.wait("@searchMovies")
-      .its("response")
-      .should((response) => {
-        expect(response?.statusCode).to.eq(200);
-        expect(response?.body.results).to.have.length(20);
-      });
+    cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
     cy.get(".see-more").should("exist");
   });
 });
