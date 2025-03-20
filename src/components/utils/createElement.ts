01bc931b7fd9bf230653c28c40b3a8fd602f4896
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-03-20 13:29:41 +0900

    refactor: 엘리먼트를 생성하는 유틸 함수 타입스크립트로 전환

--- src/components/utils/createElement.ts
+++ src/components/utils/createElement.ts
@@ -1,13 +1,22 @@
-const createElement = ({ tag, classNames = [], ...attributes }) => {
+type createElementProps = {
+  tag: string;
+  classNames: string[];
+};
+
+const createElement = ({
+  tag,
+  classNames = [],
+  ...attributes
+}: createElementProps) => {
   const $element = document.createElement(tag);
 
   classNames.forEach((className) => $element.classList.add(className));
 
   Object.entries(attributes).forEach(([key, value]) => {
-    if (key === 'required') {
+    if (key === "required" && $element instanceof HTMLInputElement) {
       $element.required = Boolean(value);
     } else {
-      $element.setAttribute(key, value);
+      $element.setAttribute(key, value as string);
     }
   });
 
