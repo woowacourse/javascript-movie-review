120fa647f9397ca0a6034c0e5981b1e7cfe01164
spoyodevelop <kjy9505@gmail.com>
2025-03-20 13:41:18 +0900

    refactor: 관심사에 따라 createMovieList 분리

---
+++ src/service/createMovieList.ts
@@ -0,0 +1,38 @@
+import { hideElement, showElement } from "../view/InputView";
+import MovieItem from "../components/moveItem/movieItem";
+import type { Result } from "../../types/TMDB";
+import { createElementsFragment } from "../util/dom";
+
+export default async function createMovieList(
+  loadMovies: () => Promise<{ results: Result[]; isLastPage: boolean }>,
+  reset?: boolean
+) {
+  const skeleton = document.querySelector(".skeleton-list");
+  showElement(skeleton);
+  const { results, isLastPage } = await loadMovies();
+  hideElement(skeleton);
+
+  if (isLastPage) {
+    document.getElementById("load-more")?.classList.add("hide");
+  }
+
+  addMovies(results, reset);
+}
+
+function addMovies(results: Result[], reset?: boolean) {
+  const $list = document.getElementById("thumbnail-list");
+
+  if (reset && $list) $list.innerHTML = "";
+
+  const movieItems = results.map((result: Result) => {
+    const { title, poster_path, vote_average } = result;
+    const movieItem = MovieItem({
+      title,
+      src: poster_path,
+      rate: vote_average,
+    });
+    return movieItem;
+  });
+
+  $list?.appendChild(createElementsFragment(movieItems));
+}
