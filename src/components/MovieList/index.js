124b4e87c0c4464099e77b2aef32358354054f56
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-20 14:21:36 +0900

    refactor: MovieList 리팩터링

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,15 +1,19 @@
-import ListTitle from "./ListTitle";
-import MovieItem from "./MovieItem";
+// components/MovieList/index.js
+import ListTitle from "./ListTitle.js";
+import MovieItem from "./MovieItem.js";
+import MoreButton from "../MoreButton.js";
 
-const MovieList = (movies) => {
+const MovieList = ({ movies, query, searchedMoviesLength }) => {
+  // 검색 결과인 경우, 현재 렌더링된 영화 수가 총 검색 결과 수보다 작아야 MoreButton을 표시
+  const showMoreButton = !query || movies.length < searchedMoviesLength;
   return /* html */ `
     <main>
       <section>
-        ${ListTitle()}
+        ${ListTitle({ query })}
         <ul id="movie-list" class="thumbnail-list" data-testid="movie-list">
           ${movies.map((movie) => MovieItem(movie)).join("")}
         </ul>
-        <button id="more-button" class="primary more" data-testid='more-button'>더 보기</button>
+        ${showMoreButton ? MoreButton() : ""}
       </section>
     </main>
   `;
