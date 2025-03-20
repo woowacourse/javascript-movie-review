d2d21c15fc940e6e4f2c15ff41ddaa0813890b25
spoyodevelop <kjy9505@gmail.com>
2025-03-20 16:18:52 +0900

    refactor: 에러 메세지 분리
    Co-authored-by: yeongipark <yeongipark@users.noreply.github.com>

--- src/setting/settings.js
+++ src/setting/settings.js
@@ -3,9 +3,6 @@ export const URLS = {
   searchMovieUrl: "https://api.themoviedb.org/3/search/movie",
 };
 
-export const ERROR_MESSAGE = {
-  FETCH_ERROR: "API 서버 상태가 좋지 않아 데이터를 가져오는데 실패했습니다.",
-};
 export const defaultOptions = {
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
