5c755655bbceaf9432a6961b2cbfdbd8a92e3b6d
keemsebeen <sebin0580@gmail.com>
2025-03-20 10:57:53 +0900

    refactor: Footer 컴포넌트 구조 수정
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/components/common/Footer.ts
+++ src/components/common/Footer.ts
@@ -3,29 +3,17 @@ import { Img } from './Img';
 import { Text } from './Text';
 
 export const Footer = () => {
-  const footerElement = createElement<HTMLButtonElement>('footer', {
+  return createElement<HTMLElement>('footer', {
     classList: 'footer',
+    children: [
+      Text({
+        props: { textContent: '© 우아한테크코스 All Rights Reserved.' },
+      }),
+      Img({
+        width: '180',
+        height: '30',
+        src: './images/woowacourse_logo.png',
+      }),
+    ],
   });
-
-  const text = Text({
-    props: {
-      textContent: '© 우아한테크코스 All Rights Reserved.',
-    },
-  });
-
-  const p = createElement<HTMLParagraphElement>('p');
-
-  const icon = Img({
-    width: '180',
-    height: '30',
-    src: './images/woowacourse_logo.png',
-  });
-
-  p.appendChild(icon);
-  footerElement.append(text, p);
-
-  const app = document.querySelector('#app');
-  app?.appendChild(footerElement);
-
-  return footerElement;
 };
