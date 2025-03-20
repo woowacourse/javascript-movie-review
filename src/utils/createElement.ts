cbfbdf1e817036be15d1d3e73f43dd6cfa273f5d
keemsebeen <sebin0580@gmail.com>
2025-03-20 10:59:40 +0900

    refactor: createElement에 children 추가
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -1,12 +1,12 @@
 type Element = keyof HTMLElementTagNameMap;
 
 export type ElementProps = {
-  [prop: string]: string;
+  [prop: string]: string | HTMLElement[];
 };
 
 export const createElement = <T extends HTMLElement>(
   tag: Element,
-  props: ElementProps = {},
+  { children, ...props }: ElementProps & { children?: HTMLElement[] } = {},
 ): T => {
   const element = document.createElement(tag) as T;
 
@@ -15,11 +15,14 @@ export const createElement = <T extends HTMLElement>(
       (element[key as keyof T] as unknown) = value;
       return;
     }
-
     if (!(key in element)) {
-      element.setAttribute(key, value);
+      element.setAttribute(key, value as string);
     }
   });
+
+  if (children) {
+    element.append(...children);
+  }
 
   return element;
 };
