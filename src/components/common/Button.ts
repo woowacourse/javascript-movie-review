3030a27c8d66fd92ac65b6640af650c217a349af
keemsebeen <sebin0580@gmail.com>
2025-03-20 10:57:05 +0900

    refactor: Button 컴포넌트 구조 수정
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/components/common/Button.ts
+++ src/components/common/Button.ts
@@ -2,22 +2,32 @@ import { ComponentProps } from '../../types/Component.types';
 import { createElement } from '../../utils/createElement';
 
 type ButtonProps = {
-  type: 'button' | 'submit' | 'reset';
-  onClick: VoidFunction;
+  type?: 'button' | 'submit' | 'reset';
+  height?: string;
+  onClick: (event: MouseEvent) => void;
 } & ComponentProps;
 
-export const Button = ({ type, onClick, classList, props }: ButtonProps) => {
-  const buttonElement = createElement<HTMLButtonElement>('button', props);
-
-  buttonElement.type = type;
+export const Button = ({
+  type = 'button',
+  height = '36',
+  onClick,
+  classList,
+  props,
+}: ButtonProps) => {
+  const buttonElement = createElement<HTMLButtonElement>('button', {
+    type,
+    ...props,
+  });
 
   if (classList && classList.length > 0) {
     buttonElement.classList.add(...classList);
   }
 
-  if (onClick) {
-    buttonElement.addEventListener('click', onClick);
+  if (height) {
+    buttonElement.style.height = `${height}px`;
   }
+
+  buttonElement.addEventListener('click', onClick);
 
   return buttonElement;
 };
