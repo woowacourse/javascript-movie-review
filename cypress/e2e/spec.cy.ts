25b77a5453336077502f4697d7640304e41c09a6
deokbin <deokbin@naver.com>
2025-03-20 15:39:45 +0900

    test: 영화 검색 API 테스트 추가

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
 
 describe("비동기 API 테스트 ", () => {
-  it("영화 목록 API 를 호출하면 한 번에 20개씩 목록에 나열되어야 한다.", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173/");
-
+  });
+  it("영화 목록 API 를 호출하면 한 번에 20개씩 목록에 나열되어야 한다.", () => {
     const popularMovieUrl =
       "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
 
@@ -22,6 +23,27 @@ describe("비동기 API 테스트 ", () => {
 
     cy.get("@popularMovies").its("status").should("eq", 200);
     cy.get("@popularMovies").its("body.results").should("have.length", 20);
+  });
+
+  it("영화를 검색하면 한 번에 20개씩 목록에 나열되어야 한다.", () => {
+    const searchWord = "사랑";
+
+    const searchMovieUrl = `https://api.themoviedb.org/3/search/movie?query=${searchWord}&include_adult=false?language=ko-KR&page=1`;
+    const options = {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${Cypress.env("TMDB_TOKEN")}`,
+      },
+    };
+
+    cy.request({
+      method: "GET",
+      url: searchMovieUrl,
+      ...options,
+    }).as("searchedMovies");
+
+    cy.get("@searchedMovies").its("status").should("eq", 200);
+    cy.get("@searchedMovies").its("body.results").should("have.length", 20);
   });
 });
 
