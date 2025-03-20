51da82583d4ff4c12ec4fd81d0d88fd52d79af08
keemsebeen <sebin0580@gmail.com>
2025-03-20 16:29:55 +0900

    refactor: HttpError 문구 변경

--- src/api/HttpError.ts
+++ src/api/HttpError.ts
@@ -5,7 +5,7 @@ export default class HttpError extends Error {
 
   constructor(status: number) {
     if (!Object.hasOwnProperty.call(STATUS_MESSAGE, status)) {
-      throw new Error(`Invalid status code: ${status}`);
+      throw new Error(`에러가 발생했어요. ${status}`);
     }
 
     super(STATUS_MESSAGE[status as keyof typeof STATUS_MESSAGE]);
