91eb2bd16d62b4f9f8857a8c7c448431e4ac7970
keemsebeen <sebin0580@gmail.com>
2025-03-20 14:58:24 +0900

    test: 에러 테스트 작성
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- cypress/e2e/error.cy.ts
+++ cypress/e2e/error.cy.ts
@@ -3,7 +3,20 @@
 describe('영화 리뷰 e2e 테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/movie/popular?*',
+      },
+      {
+        statusCode: 401,
+      },
+    ).as('getError');
   });
 
-  it('401 에러가 발생할 경우, 에러 메시지를 보여준다.', () => {});
+  it('401 에러가 발생했을 경우 에러 메시지를 보여준다.', () => {
+    cy.wait(3000);
+    cy.get('@getError').its('response.statusCode').should('eq', 401);
+  });
 });
