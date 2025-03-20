a6cc006eee55060d21635d5bd5bd47f9894fc07a
guesung <gueit214@naver.com>
2025-03-20 14:06:52 +0900

    refactor : movieApi errorHandling 부분 함수화

--- src/apis/MovieApi.ts
+++ src/apis/MovieApi.ts
@@ -1,7 +1,5 @@
 import { MoviesResponse } from "../../types/movieApiType";
-
-export async function getMovies({ page }: { page: number }) {
-  const url = `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${page}`;
+async function fetchWithErrorHandling(url: string) {
   const options = {
     method: "GET",
     headers: {
@@ -10,19 +8,15 @@ export async function getMovies({ page }: { page: number }) {
     },
   };
 
-  // try {
-  //   const response = await fetch(url, options);
-  //   return response.json() as unknown as MoviesResponse;
-  // } catch (error) {
-  //   throw Error(`${error.status}`);
-  // }
-
-  const response = fetch(url, options).then((res) => {
-    if (res.ok) return res.json();
+  return fetch(url, options).then((res) => {
+    if (res.ok) return res.json() as unknown as MoviesResponse;
     throw new Error(String(res.status));
   });
+}
 
-  return response as unknown as MoviesResponse;
+export async function getMovies({ page }: { page: number }) {
+  const url = `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${page}`;
+  return fetchWithErrorHandling(url);
 }
 
 export async function getMovieByName({
@@ -33,24 +27,5 @@ export async function getMovieByName({
   page: number;
 }) {
   const url = `https://api.themoviedb.org/3/search/movie?query=${name}&include_adult=false&language=ko-KR&page=${page}`;
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-    },
-  };
-
-  const response = fetch(url, options)
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json);
-      return json;
-    })
-    .catch((err) => {
-      throw Error(`${err.status}`);
-    });
-  return response as unknown as MoviesResponse;
-
-  // return (await fetch(url, options)).json() as unknown as MoviesResponse;
+  return fetchWithErrorHandling(url);
 }
