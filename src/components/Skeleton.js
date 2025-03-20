21bcafbd89e72728dd0de5829371e5cab81b6919
deokbin <deokbin@naver.com>
2025-03-20 14:01:59 +0900

    style: skeleton style 적용

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,17 +1,20 @@
-import MovieItem from "./MovieItem";
+import "./skeleton.css";
+
 function Skeleton() {
-  // const movieItem = MovieItem();
-  const $skeletonContainer = document.createElement("div");
+  const $skeletonContainer = document.createElement("li");
   $skeletonContainer.classList.add("skeletonContainer");
+
   const $skeletonItem = document.createElement("div");
   $skeletonItem.classList.add("skeletonItem");
 
-  const $image = document.querySelector(".thumbnail");
-  const $caption = document.querySelector(".item-desc");
-  console.log($image);
-  $skeletonItem.appendChild($image);
-  $skeletonItem.appendChild($caption);
+  const $imageSkeleton = document.createElement("div");
+  $imageSkeleton.classList.add("skeleton-image");
 
+  const $captionSkeleton = document.createElement("div");
+  $captionSkeleton.classList.add("skeleton-caption");
+
+  $skeletonItem.appendChild($imageSkeleton);
+  $skeletonItem.appendChild($captionSkeleton);
   $skeletonContainer.appendChild($skeletonItem);
   return $skeletonContainer;
 }
