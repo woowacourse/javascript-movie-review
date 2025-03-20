3026a31c0adcf1dbeb48499a5f5aace32e00728f
dev-dino22 <wlsskdud02@naver.com>
2025-03-20 15:41:40 +0900

    chore: img 소스 경로 public 제거

--- src/component/common/Banner.js
+++ src/component/common/Banner.js
@@ -1,12 +1,14 @@
-import roundRating from "../../util/roundRating.js"
+import roundRating from '../../util/roundRating.js'
 
-function Banner(data){
-    return `
-    <div class="background-container" style="background-image: url('https://media.themoviedb.org/t/p/w440_and_h660_face/${data.poster_path}');">
+function Banner(data) {
+  return `
+    <div class="background-container" style="background-image: url('https://media.themoviedb.org/t/p/w440_and_h660_face/${
+      data.poster_path
+    }');">
         <div class="overlay" aria-hidden="true"></div>
           <div class="top-rated-movie">
             <div class="rate">
-              <img src="./public/images/star_empty.png" class="star" />
+              <img src="./images/star_empty.png" class="star" />
               <span class="rate-value">${roundRating(data.vote_average)}</span>
             </div>
             <div class="title">${data.title}</div>
--- src/component/common/MovieLayout.js
+++ src/component/common/MovieLayout.js
@@ -1,56 +1,63 @@
-import MovieList from "./MovieList.js";
-import Button from "./Button.js";
-import { fetchPopularMovies } from "../../api/fetch.js";
-import hideskeleton from "../../util/hideskeleton.js";
+import MovieList from './MovieList.js'
+import Button from './Button.js'
+import { fetchPopularMovies } from '../../api/fetch.js'
+import hideskeleton from '../../util/hideskeleton.js'
 
 class MovieLayout {
-    #state
+  #state
 
-    constructor(movieData) {
-        this.#state = {
-            title: "지금 인기 있는 영화",
-            eventName: "readMoreMovieList",
-            isPossibleMore: movieData.length === 20,
-            movieData,
-        }
-        this.render();
+  constructor(movieData) {
+    this.#state = {
+      title: '지금 인기 있는 영화',
+      eventName: 'readMoreMovieList',
+      isPossibleMore: movieData.length === 20,
+      movieData,
     }
+    this.render()
+  }
 
-    setState(newState) {
-        this.#state = {...this.#state, ...newState};
-        this.render();
-    }
+  setState(newState) {
+    this.#state = { ...this.#state, ...newState }
+    this.render()
+  }
 
-    template() {
-        if(this.#state.movieData.length === 0){
-            return`
+  template() {
+    if (this.#state.movieData.length === 0) {
+      return `
             <div class="flex-center gap-16">
-                <img src="../public/images/hangsung.png" />
+                <img src="./images/hangsung.png" />
                 <div class="text-xl">검색 결과가 없습니다.</div>
             </div>
             `
-        }
+    }
 
-        return `
+    return `
             <h2 id="movieListTitle" class="text-xl">${this.#state.title}</h2>
             <div id="movieListContainer">
                 ${MovieList(this.#state.movieData).template().outerHTML}
             </div>
-            ${this.#state.isPossibleMore? Button({content: '더보기', eventName: this.#state.eventName, type: 't', width:'100%'}) : ""}
-        `}
-    
-    render(){
-        document.getElementById('MovieSection').innerHTML = this.template();
-        hideskeleton();
-    }
+            ${
+              this.#state.isPossibleMore
+                ? Button({
+                    content: '더보기',
+                    eventName: this.#state.eventName,
+                    type: 't',
+                    width: '100%',
+                  })
+                : ''
+            }
+        `
+  }
 
-    newMovieListRender(dataList) {
-        const ul = MovieList(dataList).template();
-        document.getElementById('movieListContainer').appendChild(ul);
-    }
+  render() {
+    document.getElementById('MovieSection').innerHTML = this.template()
+    hideskeleton()
+  }
 
+  newMovieListRender(dataList) {
+    const ul = MovieList(dataList).template()
+    document.getElementById('movieListContainer').appendChild(ul)
+  }
 }
 
-   
-
-export default MovieLayout;
+export default MovieLayout
