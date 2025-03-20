456ef1276f0f4f7666ac47d18b92f397dfc70068
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-03-20 15:54:52 +0900

    fix: 새로고침하면 기존 쿼리가 사라지게 함

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import fetchPopularMovies from "./fetch/fetchPopularMovies";
 import Main from "./components/Main";
 import movies from "./store/Movies";
+import deleteParams from "./utils/deleteParams";
+
+deleteParams();
 
 const PAGE = 1;
 const popularMovieData = await fetchPopularMovies(PAGE);
