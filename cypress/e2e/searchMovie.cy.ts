e2c4c33bbdf06aa7c3492001d22ed4aefe154579
mlnwns <mlnwns@naver.com>
2025-03-20 17:12:32 +0900

    test: 짱구 검색 후 더보기 버튼 클릭 시 영화 추가 표시 및 더보기 버튼 사라짐 테스트
    Co-authored-by: 신종욱 <dlsxjzld@users.noreply.github.com>

--- cypress/e2e/searchMovie.cy.ts
+++ cypress/e2e/searchMovie.cy.ts
@@ -20,4 +20,15 @@ describe("영화 검색 비동기 API 테스트", () => {
     cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
     cy.get(".see-more").should("exist");
   });
+
+  it("짱구를 검색한 후 '더보기' 버튼을 클릭하면 영화가 추가로 표시되며, 마지막 페이지(2)에 도달하면 '더보기' 버튼이 사라진다.", () => {
+    cy.get("#search-bar").type("짱구");
+    cy.get(".search-icon").click();
+    cy.get(".see-more").click();
+
+    cy.get(".thumbnail-list")
+      .find(".thumbnail")
+      .should("have.length.at.most", 40);
+    cy.get(".see-more").should("not.exist");
+  });
 });
