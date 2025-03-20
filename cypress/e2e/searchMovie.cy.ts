58ea3785878da0030eb3fc280cbb5f5dcdb4f2e1
keemsebeen <sebin0580@gmail.com>
2025-03-20 14:51:46 +0900

    test: searchMovie 검색 결과 test 추가
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- cypress/e2e/searchMovie.cy.ts
+++ cypress/e2e/searchMovie.cy.ts
@@ -7,13 +7,13 @@ describe('검색 e2e 테스트', () => {
     cy.intercept('GET', 'https://api.themoviedb.org/3/movie/popular?*', {
       fixture: 'popularMovie.json',
     }).as('getPopular');
-
-    cy.intercept('GET', 'https://api.themoviedb.org/3/search/movie?*', {
-      fixture: 'searchMovie.json',
-    }).as('getSearchMovie');
   });
 
   it('검색창에 검색어를 입력하면 스켈레톤을 보여준 후 검색 데이터를 보여준다.', () => {
+    cy.intercept('GET', 'https://api.themoviedb.org/3/search/movie?*', {
+      fixture: 'searchMovie.json',
+    }).as('getSearchMovie');
+
     cy.wait('@getPopular');
 
     cy.wait(3000);
@@ -28,5 +28,18 @@ describe('검색 e2e 테스트', () => {
     cy.get('.movie-item').should('have.length', 9);
   });
 
-  it('검색 결과가 없을 경우 검색 결과가 없다는 메시지를 보여준다.', () => {});
+  it('검색 결과가 없을 경우 검색 결과가 없다는 메시지를 보여준다.', () => {
+    cy.wait('@getPopular');
+
+    cy.wait(3000);
+
+    cy.get('#search-input').type('세라랑 메리랑 페어 ㅎ.ㅎ');
+    cy.get('.search-bar').click();
+    cy.get('.skeleton-item').should('have.length', 20);
+
+    cy.get('.skeleton-item').should('not.exist');
+    cy.get('.result-container')
+      .contains('검색 결과가 없습니다.')
+      .should('exist');
+  });
 });
