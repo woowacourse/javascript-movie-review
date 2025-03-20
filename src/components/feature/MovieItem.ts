56f9e77e29cf949eed98df74269c7f17c8b1f9d3
keemsebeen <sebin0580@gmail.com>
2025-03-20 11:03:51 +0900

    refactor: MovieItem 컴포넌트 구조 변경
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/components/feature/MovieItem.ts
+++ src/components/feature/MovieItem.ts
@@ -1,56 +1,70 @@
 import { MovieItem as MovieItemType } from '../../types/Movie.types';
 import { createElement } from '../../utils/createElement';
+import { Box } from '../common/Box';
 import { Img } from '../common/Img';
 import { Text } from '../common/Text';
 
-export const MovieItem = ({ ...props }: MovieItemType) => {
-  const { title, vote_average, poster_path } = props;
-  const liElement = <HTMLLIElement>createElement('li', {
-    classList: 'flex flex-col justify-start gap-10',
-  });
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w220_and_h330_face';
+const DEFAULT_IMAGE_URL = './images/no_image.png';
 
-  // TODO : URL 분리
-  const movieImg = Img({
-    src: poster_path
-      ? 'https://image.tmdb.org/t/p/w220_and_h330_face' + poster_path
-      : './images/no_image.png',
+const createRatingSection = (vote_average: number) => {
+  return Box({
+    classList: ['flex', 'gap-8'],
+    props: {
+      children: [
+        Img({
+          width: '16',
+          height: '16',
+          src: './images/star_empty.png',
+        }),
+        Text({
+          classList: ['text-lg', 'font-semibold', 'text-yellow'],
+          props: {
+            textContent: `${vote_average}`,
+          },
+        }),
+      ],
+    },
+  });
+};
+
+const createDescriptionSection = (title: string, vote_average: number) => {
+  return Box({
+    classList: ['flex', 'flex-col', 'gap-10'],
+    props: {
+      children: [
+        createRatingSection(vote_average),
+        Text({
+          classList: ['text-xl', 'font-bold'],
+          props: {
+            textContent: title,
+          },
+        }),
+      ],
+    },
+  });
+};
+
+const createMovieImage = (title: string, poster_path: string) => {
+  return Img({
+    src: poster_path ? `${IMAGE_BASE_URL}${poster_path}` : DEFAULT_IMAGE_URL,
     classList: ['thumbnail'],
     props: {
       alt: title,
     },
   });
+};
 
-  const descItemDiv = <HTMLDivElement>createElement('div', {
-    classList: 'flex flex-col gap-10',
+export const MovieItem = ({
+  title,
+  vote_average,
+  poster_path,
+}: MovieItemType) => {
+  return createElement<HTMLLIElement>('li', {
+    classList: 'flex flex-col justify-start gap-10',
+    children: [
+      createMovieImage(title, poster_path),
+      createDescriptionSection(title, vote_average),
+    ],
   });
-
-  const rateDiv = <HTMLDivElement>createElement('div', {
-    classList: 'flex gap-8',
-  });
-
-  const rateText = Text({
-    classList: ['text-lg', 'font-semibold', 'text-yellow'],
-    props: {
-      textContent: `${vote_average}`,
-    },
-  });
-
-  const starIcon = Img({
-    width: '16',
-    height: '16',
-    src: './images/star_empty.png',
-  });
-
-  const titleText = Text({
-    classList: ['text-xl', 'font-bold'],
-    props: {
-      textContent: title,
-    },
-  });
-
-  liElement.append(movieImg, descItemDiv);
-  descItemDiv.append(rateDiv, titleText);
-  rateDiv.append(starIcon, rateText);
-
-  return liElement;
 };
