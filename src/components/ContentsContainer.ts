89f2a8c1b7d27a991849708c42fe266f1dd29c30
deokbin <deokbin@naver.com>
2025-03-20 09:22:03 +0900

    feat: 컨텐츠 렌더링 시 제목 추가
    Co-authored-by: Seungsoo Choi <aydenote@users.noreply.github.com>

--- src/components/ContentsContainer.ts
+++ src/components/ContentsContainer.ts
@@ -3,12 +3,17 @@ import MovieService from "../services/MovieService";
 import Button from "./Button";
 import MovieList from "./MovieList";
 
-function ContentsContainer(results: MovieInfo[]) {
+function ContentsContainer(results: MovieInfo[], contentTitle: string) {
   // MovieList 렌더링
   // Todo: handler로 분리하기
   const movieList = new MovieList(results);
   const $listContainer = movieList.renderMovieList();
   const $section = document.querySelector("section");
+  const $h2 = document.createElement("h2");
+
+  $h2.innerText = contentTitle;
+
+  $section?.appendChild($h2);
   $section?.appendChild($listContainer);
 
   const movieService = new MovieService();
@@ -24,6 +29,14 @@ function ContentsContainer(results: MovieInfo[]) {
 
   // 더보기 버튼
   const $main = document.querySelector("main");
+
+  if ($main) {
+    // 기존 "더 보기" 버튼 제거 (중복 방지)
+    const existingButton = $main.querySelector("button.more");
+    if (existingButton) {
+      existingButton.remove();
+    }
+  }
   const $button = Button("더 보기", "more", clickMoreMovies);
 
   $main?.appendChild($button);
--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,41 @@ import { MovieInfo } from "../types/movieType.ts";
 import ContentsContainer from "./components/ContentsContainer.ts";
 import MovieService from "./services/MovieService.ts";
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-  },
-};
-
 async function getPopularMovies() {
   const movieService = new MovieService();
   const data = await movieService.getPopularMovies();
   // 헤더 렌더링
   renderHeader(data.results[0]);
 
+  // 이벤트 등록
+  const $input = document.querySelector(".search-input");
+  const $button = document.querySelector(".search-button");
+  const $section = document.querySelector("section") as HTMLDivElement;
+
+  $input?.addEventListener("keydown", async (event) => {
+    const keyboardEvent = event as KeyboardEvent;
+    if (keyboardEvent.key === "Enter") {
+      const inputValue = (event.target as HTMLInputElement).value;
+      const searchResult = await movieService.getSearchResult(inputValue);
+      if ($section) {
+        $section.innerHTML = "";
+      }
+      console.log(document.querySelector("main"));
+      ContentsContainer(searchResult.results, `"${inputValue}" 검색 결과`);
+    }
+  });
+
+  $button?.addEventListener("click", async (event) => {
+    const inputValue = $input?.value;
+    const searchResult = await movieService.getSearchResult(inputValue);
+    if ($section) {
+      $section.innerHTML = "";
+    }
+    ContentsContainer(searchResult.results, `"${inputValue}" 검색 결과`);
+  });
+
   // 컨텐츠 컨테이너
-  ContentsContainer(data.results);
+  ContentsContainer(data.results, "지금 인기 있는 영화");
 }
 
 function renderHeader({ title, poster_path, vote_average }: MovieInfo) {
