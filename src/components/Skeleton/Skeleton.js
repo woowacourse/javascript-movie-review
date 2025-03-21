89fb251344f8eb114f241fbd76aae85052ed31bb
deokbin <deokbin@naver.com>
2025-03-21 19:40:10 +0900

    refactor: skeletonContainer DOM 조작하는 방식에서 innerHTML 방식으로 변경

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -4,18 +4,12 @@ function Skeleton() {
   const $skeletonContainer = document.createElement("li");
   $skeletonContainer.classList.add("skeletonContainer");
 
-  const $skeletonItem = document.createElement("div");
-  $skeletonItem.classList.add("skeletonItem");
-
-  const $imageSkeleton = document.createElement("div");
-  $imageSkeleton.classList.add("skeleton-image");
-
-  const $captionSkeleton = document.createElement("div");
-  $captionSkeleton.classList.add("skeleton-caption");
-
-  $skeletonItem.appendChild($imageSkeleton);
-  $skeletonItem.appendChild($captionSkeleton);
-  $skeletonContainer.appendChild($skeletonItem);
+  $skeletonContainer.innerHTML = `
+    <div class="skeletonItem">
+      <div class="skeleton-image"></div>
+      <div class="skeleton-caption"></div>
+    </div>
+  `;
   return $skeletonContainer;
 }
 
