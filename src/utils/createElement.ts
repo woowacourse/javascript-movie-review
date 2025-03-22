a362078f259debaa5f82aa657e5de19ffd581b42
yeji0214 <1225yj50@naver.com>
2025-03-22 23:24:34 +0900

    style: 포맷팅 통일

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -1,7 +1,7 @@
-export const createElement = (htmlTemplate:string) => {
+export const createElement = (htmlTemplate: string) => {
   const $el = document.createElement("div");
   $el.innerHTML = htmlTemplate.trim();
-  
+
   const firstChild = $el.firstChild;
   if (firstChild instanceof Element) {
     return firstChild as HTMLElement;
