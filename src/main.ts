72d883d9cff3a788a3a1effbd7135947a03fc9e2
yeji0214 <1225yj50@naver.com>
2025-03-22 23:04:26 +0900

    refactor: main.ts 함수 및 상수 분리

--- src/main.ts
+++ src/main.ts
@@ -10,56 +10,88 @@ import { $ } from "./utils/dom.ts";
 import { loadMovies } from "./utils/loadMovies.ts";
 
 let currentPage = 1;
-const movieList = document.createElement("ul");
-movieList.classList.add("thumbnail-list");
 
-const wrapper = document.createElement("div");
-wrapper.setAttribute("id", "wrap");
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
+const FAIL_TO_LOAD_MOVIES = "영화 목록을 가져오지 못했습니다.";
+const CAPTION = "지금 인기 있는 영화";
 
 addEventListener("load", async () => {
   const app = $("#app");
+  if (!app) return;
 
-  const header = Header({ title: "로딩중 ...", imageUrl: "", voteAverage: 0 });
-  if (!header) return;
+  const movieList = document.createElement("ul");
+  movieList.classList.add("thumbnail-list");
+
+  const wrapper = document.createElement("div");
+  wrapper.setAttribute("id", "wrap");
+
+  const initialHeader = Header({
+    title: "로딩중 ...",
+    imageUrl: "",
+    voteAverage: 0,
+  });
+
+  if (!initialHeader) return;
+  wrapper.appendChild(initialHeader);
+
+  app.appendChild(wrapper);
+
+  renderMovieContainer(wrapper, movieList);
+  updateMovieContainer(wrapper, initialHeader);
+
   const footer = Footer();
-
-  if (app) {
-    app.appendChild(wrapper);
-    wrapper.appendChild(header);
-    wrapper.appendChild(Caption({ title: "지금 인기 있는 영화" }));
-    wrapper.appendChild(movieList);
-
-    for (let i = 0; i < 20; i++) {
-      const skeletonItem = SkeletonMovieItem();
-      movieList.appendChild(skeletonItem);
-    }
-    wrapper.appendChild(movieList);
-
-    try {
-      const movies: MovieResponse = await fetchPopularMovieList(currentPage);
-      const topMovie = movies.results[0];
-
-      if (topMovie) {
-        const updatedHeader = Header({
-          title: topMovie.title,
-          imageUrl: `https://image.tmdb.org/t/p/w500${topMovie.poster_path}`,
-          voteAverage: topMovie.vote_average,
-        });
-
-        if (updatedHeader) wrapper.replaceChild(updatedHeader, header);
-      }
-
-      loadMovies(movies);
-
-      wrapper.appendChild(
-        LoadMoreButton({
-          loadFn: fetchPopularMovieList,
-        })
-      );
-    } catch (error) {
-      wrapper.appendChild(NoSearchResults("영화 목록을 가져오지 못했습니다."));
-    }
-
-    app.appendChild(footer);
-  }
+  app.appendChild(footer);
 });
+
+const renderMovieContainer = (
+  wrapper: HTMLDivElement,
+  movieList: HTMLUListElement
+) => {
+  wrapper.appendChild(Caption({ title: CAPTION }));
+  wrapper.appendChild(movieList);
+
+  for (let i = 0; i < 20; i++) {
+    const skeletonItem = SkeletonMovieItem();
+    movieList.appendChild(skeletonItem);
+  }
+  wrapper.appendChild(movieList);
+};
+
+const updateMovieContainer = async (
+  wrapper: HTMLDivElement,
+  initialHeader: HTMLElement
+) => {
+  try {
+    const movies: MovieResponse = await fetchPopularMovieList(currentPage);
+
+    updateTopMovieInfo(movies, wrapper, initialHeader);
+    loadMovies(movies);
+
+    wrapper.appendChild(
+      LoadMoreButton({
+        loadFn: fetchPopularMovieList,
+      })
+    );
+  } catch (error) {
+    wrapper.appendChild(NoSearchResults(FAIL_TO_LOAD_MOVIES));
+  }
+};
+
+const updateTopMovieInfo = (
+  movies: MovieResponse,
+  wrapper: HTMLDivElement,
+  initialHeader: HTMLElement
+) => {
+  const topMovie = movies.results[0];
+
+  if (topMovie) {
+    const updatedHeader = Header({
+      title: topMovie.title,
+      imageUrl: `${IMG_BASE_URL}${topMovie.poster_path}`,
+      voteAverage: topMovie.vote_average,
+    });
+
+    if (updatedHeader && initialHeader)
+      wrapper.replaceChild(updatedHeader, initialHeader);
+  }
+};
