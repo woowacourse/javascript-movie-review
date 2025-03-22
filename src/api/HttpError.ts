dd14d01b9b45ed349087d16b323ce2ea4104496a
keemsebeen <sebin0580@gmail.com>
2025-03-22 15:04:53 +0900

    refactor: HttpError 조건문 변경
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/api/HttpError.ts
+++ src/api/HttpError.ts
@@ -4,7 +4,7 @@ export default class HttpError extends Error {
   public readonly status: number | undefined;
 
   constructor(status: number) {
-    if (!Object.hasOwnProperty.call(STATUS_MESSAGE, status)) {
+    if (!(status in STATUS_MESSAGE)) {
       throw new Error(`에러가 발생했어요. ${status}`);
     }
 
