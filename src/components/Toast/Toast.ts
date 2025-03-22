7f30cb36fb57570a49e6a0a4c8360cfbe26e003a
spoyodevelop <kjy9505@gmail.com>
2025-03-22 12:50:31 +0900

    refactor: 토스트 타입 구현

--- src/components/Toast/Toast.ts
+++ src/components/Toast/Toast.ts
@@ -1,7 +1,16 @@
-const Toast = {
+type ToastType = "error" | "info";
+
+interface IToast {
+  showToast(message: string, type?: ToastType, duration?: number): void;
+  resetToast(): void;
+}
+
+const Toast: IToast = {
   showToast(message, type = "error", duration = 5000) {
     if (type === "info") duration = 2000;
-    let toastContainer = document.querySelector(".toast-container");
+    let toastContainer = document.querySelector(
+      ".toast-container"
+    ) as HTMLDivElement | null;
     if (!toastContainer) {
       toastContainer = document.createElement("div");
       toastContainer.className = "toast-container";
@@ -9,10 +18,10 @@ const Toast = {
     }
     const toast = document.createElement("div");
     toast.className = `toast ${type}`;
-    if (type == "error") message = message.replace("[ERROR]", "");
-
+    if (type === "error") {
+      message = message.replace("[ERROR]", "");
+    }
     toast.innerHTML = message;
-
     toastContainer.appendChild(toast);
 
     setTimeout(() => {
@@ -29,8 +38,11 @@ const Toast = {
       setTimeout(() => toast.remove(), 300);
     });
   },
+
   resetToast() {
-    let toastContainer = document.querySelector(".toast-container");
+    const toastContainer = document.querySelector(
+      ".toast-container"
+    ) as HTMLDivElement | null;
     if (toastContainer) toastContainer.remove();
   },
 };
