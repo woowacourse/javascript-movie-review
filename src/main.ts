eea4ad924b5e8f9612daa383a88d86c304ed6db7
hwanheejung <hwanheejung117@gmail.com>
2025-03-22 19:03:13 +0900

    fix: 폴더 이름 수정

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import PopularMovieBoard from "./components/MoviesScreen/PopularMovieBoard";
-import SearchMovieBoard from "./components/MoviesScreen/SearchMovieBoard";
+import PopularMovieBoard from "./components/MovieBoards/PopularMovieBoard";
+import SearchMovieBoard from "./components/MovieBoards/SearchMovieBoard";
 import { isHTMLElement } from "./utils/typeGuards";
 
 interface AppContract {
