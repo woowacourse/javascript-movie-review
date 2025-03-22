c2addd3a58c8de9d8ab0ed55db5b271db6a1ee21
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-22 16:16:01 +0900

    refactor: 영화 검색 api 함수 구현

--- src/features/search/api/getSearchedPost.ts
+++ src/features/search/api/getSearchedPost.ts
@@ -1,26 +1,18 @@
 import ErrorPage from "../../../shared/ui/components/ErrorPage";
-
-const url = (query: string, page: number) =>
-  `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=ko-KR&page=${page}`;
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-  },
-};
+import { apiClient } from "../../../shared/utils/apiClient";
 
 export const getSearchedPost = async (query: string, page: number) => {
   try {
-    const response = await fetch(url(query, page), options);
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch searched post");
-    }
-
-    return response.json();
+    return await apiClient(
+      "GET",
+      `/search/movie?query=${query}&include_adult=true&language=ko-KR&page=${page}`
+    );
   } catch (error) {
-    const $container = document.querySelector(".container");
-    $container!.replaceChildren(ErrorPage());
+    if (error instanceof Error) {
+      const $container = document.querySelector(".container");
+      $container!.replaceChildren(
+        ErrorPage("검색 결과를 불러오는데 실패하였습니다.")
+      );
+    }
   }
 };
