64d9f7b583da298fc0c5e6347aacc5583e7de17b
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-22 16:47:10 +0900

    ♻️ refactor: 불필요한 주석 제거

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,3 @@ const app = new App();
 (async function () {
   await app.init();
 })();
-// https://api.themoviedb.org/3/search/movie?query=%EC%A7%B1%EA%B5%AC&include_adult=false&language=en-US&page=1
