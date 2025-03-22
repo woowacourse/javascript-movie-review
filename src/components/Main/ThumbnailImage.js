db496574512c7f52bac791d244f4dc4df18e5050
deokbin <deokbin@naver.com>
2025-03-22 23:20:36 +0900

    feat: 이미지가 없을 경우 title 만 보여주도록 변경

--- src/components/Main/ThumbnailImage.js
+++ src/components/Main/ThumbnailImage.js
@@ -1,13 +1,21 @@
 function ThumbnailImage({ title, poster_path }) {
-  const $thumbnailImage = document.createElement("img");
-
-  $thumbnailImage.classList.add("thumbnail");
   if (!poster_path) {
-    $thumbnailImage.src = "./default_poster_image.png";
-  } else {
-    $thumbnailImage.src = `https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}`;
+    const $titleElement = document.createElement("p");
+    $titleElement.classList.add("thumbnail-title");
+    $titleElement.textContent = title;
+    return $titleElement;
   }
+
+  const $thumbnailImage = document.createElement("img");
+  $thumbnailImage.classList.add("thumbnail");
+
+  const defaultImage = "./default_poster_image.png";
+  $thumbnailImage.src = `https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}`;
   $thumbnailImage.alt = `${title} Thumbnail 이미지`;
+
+  $thumbnailImage.onerror = () => {
+    $thumbnailImage.src = defaultImage;
+  };
 
   return $thumbnailImage;
 }
