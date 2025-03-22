5f89a933ea35f421820812cd1b5493d5d8152bba
keemsebeen <sebin0580@gmail.com>
2025-03-22 16:23:34 +0900

    refactor: isValidStatusCode에 따른 메시지 추가, 타입 가드 적용
    Co-Authored-By: SuHwa Lee <tnghk9611@naver.com>

--- src/api/HttpError.ts
+++ src/api/HttpError.ts
@@ -1,13 +1,17 @@
-import { STATUS_MESSAGE } from '../constants/errorStatus';
+import { STATUS_MESSAGE, StatusCode } from '../constants/errorStatus';
+
+export const isValidStatusCode = (status: number): status is StatusCode => {
+  return Object.keys(STATUS_MESSAGE).includes(status.toString());
+};
 
 export default class HttpError extends Error {
   public readonly status: number | undefined;
 
   constructor(status: number) {
-    if (!(status in STATUS_MESSAGE)) {
-      throw new Error(`에러가 발생했어요. ${status}`);
-    }
-
-    super(STATUS_MESSAGE[status as keyof typeof STATUS_MESSAGE]);
+    const message = isValidStatusCode(status)
+      ? STATUS_MESSAGE[status]
+      : '알 수 없는 오류가 발생했습니다.';
+    super(message);
+    this.status = status;
   }
 }
