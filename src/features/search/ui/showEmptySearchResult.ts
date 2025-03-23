47a96727aedbcf53f2010dae36f7b63205a08d8f
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-24 04:19:34 +0900

    refactor: toElement 유틸함수로 리팩토링

--- src/features/search/ui/showEmptySearchResult.ts
+++ src/features/search/ui/showEmptySearchResult.ts
@@ -1,15 +1,17 @@
 import MoreMoviesButton from "../../../shared/ui/components/MoreMoviesButton";
-import EmptySearchResult from "./components/EmptySearchResult";
+import { toElement } from "../../../shared/utils/toElement";
 
 export function showEmptySearchResult() {
   const $movieContainer = document.getElementById("movie-container");
-  const $emptySearchResult = document.querySelector(
-    ".empty-search-result-container"
-  );
 
-  if (!$emptySearchResult) {
-    $movieContainer?.appendChild(EmptySearchResult());
-  }
+  $movieContainer?.appendChild(
+    toElement(`
+  <div class="empty-search-result-container">
+    <img src="./images/으아아행성이.png" alt="검색 결과가 없습니다." class="empty-search-result-image"/>
+    <p class="empty-search-result-text">검색 결과가 없습니다.</p>
+  </div>
+  `)
+  );
 
   const $moreMoviesButton = MoreMoviesButton();
   $moreMoviesButton?.classList.add("disabled");
