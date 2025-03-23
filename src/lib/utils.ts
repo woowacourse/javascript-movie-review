43d51c97e66ff9fb941e1814fc1a13ad8e492255
guesung <gueit214@naver.com>
2025-03-23 14:17:54 +0900

    fix: fix the type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { concat, flat, map, pipe, reduce, zip } from '@fxts/core';
+import { concat, flat, pipe, reduce, zip } from '@fxts/core';
 import type { HTMLType } from './types';
 
 export const isElement = (target: EventTarget | null): target is Element => {
@@ -16,20 +16,14 @@ export const $ = (selector: string) => {
   return document.querySelector(selector) as HTMLElement;
 };
 
-export const createElement = (innerHTML: string) => {
-  // TODO: html의 내부에 요소가 2개 이상 있는 경우 에러 발생
-  const element = document.createElement('div');
-  element.innerHTML = innerHTML;
-  return element.firstElementChild as HTMLElement;
-};
+// const escape = (str: string) =>
+//   String(str)
+//     .replace(/&/g, '&amp;')
+//     .replace(/</g, '&lt;')
+//     .replace(/>/g, '&gt;')
+//     .replace(/"/g, '&quot;')
+//     .replace(/'/g, '&#39;');
 
-const escape = (str: string) =>
-  String(str)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
 export function html(strings: TemplateStringsArray, ...values: any[]) {
   return pipe(
     zip(
