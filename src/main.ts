05e756a8edb33d4cdc43d43974da0d6e4269cea6
guesung <gueit214@naver.com>
2025-03-23 16:45:50 +0900

    feat: 이벤트를 핸들링하는 모듈

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import App from './App';
+import eventHandlerInstance from './lib/modules/EventHandler';
 import { $ } from './lib/utils';
 
 addEventListener('load', async () => {
   $('#app').appendChild(new App().element);
+  eventHandlerInstance.attachEventListener();
 });
