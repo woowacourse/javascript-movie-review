907206585cfbba6e9b39c4f9d025563c396b7b09
guesung <gueit214@naver.com>
2025-03-23 14:35:24 +0900

    fix: viewheight 설정

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,8 @@ dotenv.config();
 
 export default defineConfig({
   e2e: {
+    viewportHeight: 1000,
     experimentalStudio: true,
-    baseUrl: process.env.VITE_BASE_URL ?? 'http://localhost:5173',
+    baseUrl: process.env.VITE_BASE_URL ?? 'http://localhost:4173',
   },
 });
