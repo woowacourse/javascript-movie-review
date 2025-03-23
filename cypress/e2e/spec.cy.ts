093d33e6e60ae81a55ef347f957050a571b2a90d
mun-kyeong <horse6953@naver.com>
2025-03-24 03:08:25 +0900

    [refactor] : e2e 모킹 테스트로 변경

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,27 +1,95 @@
-describe("E2E테스트", () => {
+/// <reference types="cypress" />
+
+describe("Fixture을 리용한 목록 목킹 E2E테스트", () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular*/,
+      },
+      { fixture: "movie-popular-page1.json" }
+    ).as("getPopularMoviesPage1");
+
+    cy.visit("http://localhost:5173/");
+
+    cy.intercept(
+      {
+        method: "GET",
+        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular.*page=2/,
+      },
+      { fixture: "movie-popular-page2.json" }
+    ).as("getPopularMoviesPage2");
+  });
+
+  it("영화 데이가 잘 가져와지는지 확인할 수 있다.", () => {
+    cy.wait("@getPopularMoviesPage1").then((interception) => {
+      const popularMovies = interception.response.body.results;
+      expect(popularMovies.length).to.equal(20);
+    });
+  });
+
+  it("사용자가 영화 목록 20개를 볼 수 있다.", () => {
+    cy.wait("@getPopularMoviesPage1").then((interception) => {
+      const popularMovies = interception.response.body.results;
+      expect(popularMovies.length).to.equal(20);
+
+      const popularMovieItems = cy.get(".thumbnail-list > li");
+      expect(popularMovieItems.should("have.length", 20));
+    });
+  });
+  it("사용자가 더 보기를 누르면 다음 목록을 보여준다.", () => {
+    cy.wait("@getPopularMoviesPage1");
+
+    cy.get(".show-more").should("exist");
+    cy.get(".show-more").click();
+
+    cy.wait("@getPopularMoviesPage2");
+
+    cy.get(".thumbnail-list > li").should("have.length", 40);
+  });
+});
+
+describe("Fixture을 리용한 검색 목킹 E2E테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/");
+    cy.wait(2000);
   });
 
-  describe("목록", () => {
-    it("사용자가 영화 목록 20개를 볼 수 있다.", () => {
-      cy.get(".thumbnail-list > li").should("have.length", 20);
-    });
-    it("사용자가 더 보기를 누르면 다음 목록을 보여준다.", () => {
-      cy.get(".show-more").should("exist");
-      cy.get(".show-more").click();
-      cy.get(".thumbnail-list > li").should("have.length", 40);
-    });
+  it("검색어 '미키' 입력 시 20개의 결과를 보여준다.", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        pathname: "/3/search/movie",
+        query: {
+          query: "미키",
+        },
+      },
+      { fixture: "movie-title-miki.json" }
+    ).as("getSearchMiki");
+
+    cy.search("미키");
+
+    cy.wait("@getSearchMiki");
+
+    cy.get(".thumbnail-list > li").should("have.length", 20);
   });
 
-  describe("검색", () => {
-    it("검색어를 입력했을 때 목록이 있다면 목록을 보여준다.", () => {
-      cy.search("짱구");
-      cy.get(".thumbnail-list > li").should("have.length", 20);
-    });
-    it("검색어를 입력했을 때 목록이 없다면 빈 화면을 보여준다.", () => {
-      cy.search("없는제목우아아아아아아");
-      cy.get(".error").contains("검색 결과가 없습니다.").should("exist");
-    });
+  it("검색어를 입력했을 때 목록이 없다면 빈 화면을 보여준다.", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        pathname: "/3/search/movie",
+        query: {
+          query: "없는제목우아아아아아아",
+        },
+      },
+      { fixture: "movie-title-none.json" }
+    ).as("getSearchMovieNone");
+
+    cy.search("없는제목우아아아아아아");
+
+    cy.wait("@getSearchMovieNone");
+
+    cy.get(".error").contains("검색 결과가 없습니다.").should("exist");
   });
 });
