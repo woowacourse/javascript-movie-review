1e53c5303f40102e26cc3a247b436f892c49c6f9
kimyou1102 <yoosk5485@naver.com>
2025-03-24 14:30:43 +0900

    feat: 썸네일 스켈레톤 UI 추가

--- src/UI/Header/Thumbnail/Thumbnail.js
+++ src/UI/Header/Thumbnail/Thumbnail.js
@@ -8,6 +8,25 @@ class Thumbnail {
     const $div = document.createElement("div");
     $div.classList.add("background-container");
 
+    if (!this.movie) {
+      $div.classList.add("thumbnail-skeleton-box");
+
+      $div.innerHTML = /*html*/ `
+        <div class="overlay" aria-hidden="true"></div>
+        <div class="top-rated-container">
+          <div class="top-rated-movie">
+            <div class="empty-rate">
+              <span class="rate-empty-value"> </span>
+            </div>
+            <div class="empty-title"> </div>
+            <button class="primary detail">자세히 보기</button>
+          </div>
+        </div>
+      </div>
+    `;
+
+      return $div;
+    }
     $div.style.backgroundImage = `url("${this.movie.backdrop_path}")`;
 
     $div.innerHTML = /*html*/ `
