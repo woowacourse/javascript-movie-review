666e293d233660baf64e648e4e2f1ad6c51c4921
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-24 15:17:30 +0900

    refactor: createSkeletons 함수에 createFragment함수 적용

--- src/shared/ui/createSkeletons.ts
+++ src/shared/ui/createSkeletons.ts
@@ -1,12 +1,13 @@
+import { createFragment } from "../utils/createFragment";
 import { MovieSkeleton } from "./components/MovieSkeleton";
 
 export const createSkeletons = (count = 10) => {
   const skeleton = document.createElement("div");
   skeleton.classList.add("skeleton");
 
-  for (let i = 0; i < count; i++) {
-    skeleton.appendChild(MovieSkeleton());
-  }
+  skeleton.append(
+    createFragment(Array.from({ length: count }, () => MovieSkeleton()))
+  );
 
   return skeleton;
 };
