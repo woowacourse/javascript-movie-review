bbff9800796ec749b6803ec45b5c2f4b0b0fa035
ahhyun <wndkgus0225@naver.com>
2025-03-24 20:11:19 +0900

    fix: skeleton UI 불러오는 개수를 배열로 변경

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,31 +1,30 @@
-import MovieItem from './MovieItem';
-import SkeletonMovieItem from './SkeletonMovieItem';
-import NothingMovieList from './NothingMovieList';
-import createElement from './utils/createElement';
+import MovieItem from "./MovieItem";
+import SkeletonMovieItem from "./SkeletonMovieItem";
+import NothingMovieList from "./NothingMovieList";
+import createElement from "./utils/createElement";
 
 const MovieList = ({ movies }) => {
   const $ul = createElement({
-    tag: 'ul',
-    classNames: ['thumbnail-list'],
+    tag: "ul",
+    classNames: ["thumbnail-list"],
   });
-  
-  if(movies.length === 0) {
+
+  if (movies.length === 0) {
     return NothingMovieList();
   }
 
-  if(movies === "loading") {
-    [1,1,1].forEach(() => {
-      $ul.appendChild(SkeletonMovieItem());
-    });
-    
-  }
-
-  else {
+  const SKELETON_ITEMS_COUNT = 20;
+  if (movies === "loading") {
+    Array(SKELETON_ITEMS_COUNT)
+      .fill(null)
+      .forEach(() => {
+        $ul.appendChild(SkeletonMovieItem());
+      });
+  } else {
     movies.forEach((movie) => {
       $ul.appendChild(MovieItem({ movie }));
     });
   }
-
 
   return $ul;
 };
