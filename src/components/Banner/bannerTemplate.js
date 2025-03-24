09526b05ed2108737001782ec809d71266812101
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-24 14:59:13 +0900

    refactor: Banner 컴포넌트 구조 변경

---
+++ src/components/Banner/bannerTemplate.js
@@ -0,0 +1,19 @@
+const bannerTemplate = ({ vote_average, title }) => {
+  return /* html */ `
+    <div id="banner" class="background-container">
+      <div class="overlay" aria-hidden="true" ></div>
+      <div class="top-rated-container">
+        <div class="top-rated-movie">
+          <div class="rate">
+            <img src="./images/star_empty.png" class="star" />
+            <span class="rate-value">${vote_average}</span>
+          </div>
+          <div class="title">${title}</div>
+          <button class="primary detail">자세히 보기</button>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+export default bannerTemplate;
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,19 +1,32 @@
-const Banner = ({ vote_average, title }) => {
-  return /* html */ `
-    <div id="banner" class="background-container">
-      <div class="overlay" aria-hidden="true" ></div>
-      <div class="top-rated-container">
-        <div class="top-rated-movie">
-          <div class="rate">
-            <img src="./images/star_empty.png" class="star" />
-            <span class="rate-value">${vote_average}</span>
-          </div>
-          <div class="title">${title}</div>
-          <button class="primary detail">자세히 보기</button>
-        </div>
-      </div>
-    </div>
-  `;
-};
+// components/BannerComponent.js
+import store from "../../store/store.ts";
+import bannerTemplate from "./bannerTemplate.js";
+import SkeletonBanner from "../Skeleton/SkeletonBanner.js";
+
+class Banner {
+  constructor($container) {
+    this.$container = $container;
+    store.subscribe(this.render.bind(this));
+    this.render(store.getState());
+  }
+
+  render(state) {
+    if (!state.query) {
+      if (state.movies.length) {
+        this.$container.innerHTML = bannerTemplate(state.movies[0]);
+        const $banner = this.$container.querySelector("#banner");
+        if ($banner) {
+          $banner.style.backgroundImage = `url(${
+            import.meta.env.VITE_TMDB_API_BANNER_URL
+          }${state.movies[0].backdrop_path})`;
+        }
+      } else {
+        this.$container.innerHTML = SkeletonBanner();
+      }
+    } else {
+      this.$container.innerHTML = "";
+    }
+  }
+}
 
 export default Banner;
