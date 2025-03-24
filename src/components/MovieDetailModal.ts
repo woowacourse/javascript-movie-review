450685fd8d4647be2f74e61e8ec8bb786a28fb17
guesung <gueit214@naver.com>
2025-03-24 10:47:19 +0900

    feat: 실제 데이터

--- src/components/MovieDetailModal.ts
+++ src/components/MovieDetailModal.ts
@@ -1,6 +1,8 @@
 import { AppState } from '@/App';
 import { Component } from './core';
 import { html } from '@/lib/utils';
+import { DEFAULT_BACK_DROP_URL } from '@/lib/constants';
+import { join, map, pipe } from '@fxts/core';
 
 interface MovieDetailModalProps {
   movieDetailResponse: AppState['movieDetailResponse'];
@@ -8,6 +10,13 @@ interface MovieDetailModalProps {
 
 export default class MovieDetailModal extends Component<MovieDetailModalProps> {
   template() {
+    if (!this.props.movieDetailResponse) return html``;
+
+    const { backdrop_path, title, release_date, genres, overview, vote_average } = this.props.movieDetailResponse;
+
+    const backgroundImage = backdrop_path
+      ? `${DEFAULT_BACK_DROP_URL}${backdrop_path}`
+      : './images/default_thumbnail.jpeg';
     return html`
       <div class="modal-background active" id="modalBackground" data-action="close-movie-detail-modal">
         <div class="modal" data-action="not-close-movie-detail-modal">
@@ -16,19 +25,20 @@ export default class MovieDetailModal extends Component<MovieDetailModalProps> {
           </button>
           <div class="modal-container">
             <div class="modal-image">
-              <img src="https://image.tmdb.org/t/p/original//pmemGuhr450DK8GiTT44mgwWCP7.jpg" />
+              <img src="${backgroundImage}" />
             </div>
             <div class="modal-description">
-              <h2>인사이드 아웃 2</h2>
-              <p class="category">2024 · 모험, 애니메이션, 코미디, 드라마, 가족</p>
-              <p class="rate"><img src="./images/star_filled.png" class="star" /><span>7.7</span></p>
-              <hr />
-              <p class="detail">
-                13살이 된 라일리의 행복을 위해 매일 바쁘게 머릿속 감정 컨트롤 본부를 운영하는 ‘기쁨’, ‘슬픔’, ‘버럭’,
-                ‘까칠’, ‘소심’. 그러던 어느 날, 낯선 감정인 ‘불안’, ‘당황’, ‘따분’, ‘부럽’이가 본부에 등장하고, 언제나
-                최악의 상황을 대비하며 제멋대로인 ‘불안’이와 기존 감정들은 계속 충돌한다. 결국 새로운 감정들에 의해
-                본부에서 쫓겨나게 된 기존 감정들은 다시 본부로 돌아가기 위해 위험천만한 모험을 시작하는데…
+              <h2>${title}</h2>
+              <p class="category">
+                ${release_date} ·
+                ${pipe(
+                  map((genre) => genre.name, genres),
+                  join(', '),
+                )}
               </p>
+              <p class="rate"><img src="./images/star_filled.png" class="star" /><span>${vote_average}</span></p>
+              <hr />
+              <p class="detail">${overview}</p>
             </div>
           </div>
         </div>
