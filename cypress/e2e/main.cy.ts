0f25bff213bd0cb52a71ca989555e583ccc529a3
hanheel <hanhee630630@gmail.com>
2025-03-24 15:37:47 +0900

    feat : 검색 결과 테스트에서 결과 유무만 검증하던 로직을 실제 결과 검증으로 개선

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -2,7 +2,6 @@
 
 describe('Fixture를 이용한 테스트', () => {
   beforeEach(() => {
-    // https://docs.cypress.io/api/commands/intercept
     cy.intercept(
       {
         method: 'GET',
@@ -18,9 +17,15 @@ describe('Fixture를 이용한 테스트', () => {
     });
   });
 
-  it('인기 영화 목록이 20개 렌더링이 되어야 한다.', () => {
+  it('초기화면에서 인기 영화 목록이 20개 렌더링이 되어야 한다.', () => {
     cy.get('.item').should('have.length', 20);
   });
 
-  it('검색 결과가 없을 경우 에러메세지 출력', () => {});
+  it('더보기 버튼을 누를 경우, 20개의 작품이 더 렌더링된다', () => {
+    it('검색어를 입력하고, 더보기 버튼을 누르면 아이템을 띄워줘야 한다.', () => {
+      cy.get('.button--medium').click();
+
+      cy.get('.item').should('have.length', 40);
+    });
+  });
 });
