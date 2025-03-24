1f8d940cf31131d2d1137342cde1c4226e8b2de2
ShinjungOh <tlswjd9260@gmail.com>
2025-03-24 15:06:18 +0900

    refactor: 공통 API 도메인 주소 분리

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,4 @@
+export const baseApiUrl = "https://api.themoviedb.org/3";
 export const popularApiUrl =
-  "https://api.themoviedb.org/3/movie/popular?language=ko-KR&region=ko-KR";
-export const searchApiUrl = "https://api.themoviedb.org/3/search/movie?";
+  `${baseApiUrl}/movie/popular?language=ko-KR&region=ko-KR`;
+export const searchApiUrl = `${baseApiUrl}/search/movie?`;
