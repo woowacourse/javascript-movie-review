a50a0f8fdf25815ec29a9870bdf1fbb55e0d56a1
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-24 14:58:23 +0900

    refactor: App 컴포넌트 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,74 +1,42 @@
+// App.js
 import Header from "./components/Header/index.js";
 import Footer from "./components/Footer/index.js";
 import Banner from "./components/Banner/index.js";
-import SkeletonBanner from "./components/Skeleton/SkeletonBanner.js";
 import MovieList from "./components/MovieList/index.js";
 import { fetchPopularMovies } from "./APIs/movieAPI.ts";
 import store from "./store/store.ts";
-import { attachMoreButtonEvent } from "./components/MoreButton/MoreButton.js";
 
 class App {
   constructor($target) {
     this.$target = $target;
 
-    store.subscribe(() => this.render());
+    const $headerTemplate = document.createElement("template");
+    $headerTemplate.innerHTML = Header();
+    this.$target.appendChild($headerTemplate.content);
+
+    this.bannerContainer = document.createElement("section");
+    this.bannerContainer.id = "banner-container";
+    this.$target.appendChild(this.bannerContainer);
+
+    this.mainContainer = document.createElement("div");
+    this.mainContainer.classList.add("container");
+    this.$target.appendChild(this.mainContainer);
+
+    const $footerTemplate = document.createElement("template");
+    $footerTemplate.innerHTML = Footer();
+    this.$target.appendChild($footerTemplate.content);
+
+    this.bannerComponent = new Banner(this.bannerContainer);
+    this.movieListComponent = new MovieList(this.mainContainer);
+
     if (store.getState().movies.length === 0) {
       this.loadPopularMovies();
     }
-    this.render();
   }
 
   async loadPopularMovies() {
     const movies = await fetchPopularMovies((error) => alert(error.message));
     store.setState({ movies });
-  }
-
-  render() {
-    const state = store.getState();
-    this.$target.innerHTML = /* html */ `
-      <div id="wrap">
-        ${Header()}
-        ${
-          !state.query
-            ? state.movies.length
-              ? Banner(state.movies[0])
-              : SkeletonBanner()
-            : ""
-        }
-        <div class="container">
-          ${MovieList({
-            movies: state.movies,
-            query: state.query,
-            searchedMoviesLength: state.searchedMoviesLength,
-          })}
-        </div>
-        ${Footer()}
-      </div>
-    `;
-
-    this.mount(state);
-  }
-
-  mount(state) {
-    attachMoreButtonEvent();
-
-    const $banner = document.querySelector("#banner");
-    if (state.movies.length && $banner) {
-      $banner.style.backgroundImage = `url(${
-        import.meta.env.VITE_TMDB_API_BANNER_URL
-      }${state.movies[0].backdrop_path})`;
-    }
-
-    window.addEventListener("scroll", () => {
-      const $header = document.querySelector("#header");
-      if ($header) {
-        if (window.scrollY > 0) {
-          $header.classList.add("scrolled");
-          return;
-        }
-        $header.classList.remove("scrolled");
-      }
-    });
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import App from "./App";
 
-const $app = document.querySelector("#app");
+const $app = document.querySelector("#wrap");
 new App($app);
