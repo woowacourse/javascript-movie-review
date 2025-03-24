98997102f69db8dbb1b29031436c2f23f99fd816
mlnwns <mlnwns@naver.com>
2025-03-24 16:19:13 +0900

    test: 인기 영화 API 테스트를 위한 인터셉트 작성 및 사용

--- cypress/e2e/popularMovie.cy.ts
+++ cypress/e2e/popularMovie.cy.ts
@@ -2,32 +2,33 @@
 
 describe("인기 있는 영화 비동기 API 테스트", () => {
   beforeEach(() => {
-    cy.visit("localhost:5173");
-    const popularMovieUrl =
-      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
-    const options = {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${Cypress.env("TMDB_TOKEN")}`,
-      },
-    };
+    cy.intercept(
+      "GET",
+      "https://api.themoviedb.org/3/movie/popular?*",
+      (req) => {
+        req.continue((res) => {
+          if (req.url.includes("page=1")) {
+            res.body.results = Array(20).fill({ title: "Page 1" });
+          } else if (req.url.includes("page=2")) {
+            res.body.results = Array(20).fill({ title: "Page 2" });
+          }
+        });
+      }
+    ).as("fetchMovies");
 
-    cy.request({
-      method: "GET",
-      url: popularMovieUrl,
-      ...options,
-    }).as("popularMovies");
+    cy.visit("localhost:5173");
+    cy.wait("@fetchMovies");
   });
 
   it("초기 페이지 로딩 시 인기 있는 영화 목록 API를 호출하면서 한 번에 20개씩 목록에 나열되어야 한다.", () => {
-    cy.get("@popularMovies").its("status").should("eq", 200);
-    cy.get("@popularMovies").its("body.results").should("have.length", 20);
+    cy.get("@fetchMovies").its("response.statusCode").should("eq", 200);
+    cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
   });
 
   it("인기 있는 영화에서 더보기 버튼을 누르면 영화 목록 API를 호출하며 20개가 목록에 추가된다.", () => {
+    cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
     cy.get(".see-more").click();
-    cy.get("@popularMovies").its("status").should("eq", 200);
-    cy.get("@popularMovies").its("body.results").should("have.length", 20);
+    cy.wait("@fetchMovies").its("request.url").should("include", "page=2");
     cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 40);
   });
 });
