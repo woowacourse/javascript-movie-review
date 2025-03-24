218a59791a9f7713b4259cf5b8277db0acc68da9
mlnwns <mlnwns@naver.com>
2025-03-24 19:45:10 +0900

    test: 하드코딩 테스트 intercept로 대체 및 스켈레톤 검사

--- cypress/e2e/popularMovie.cy.ts
+++ cypress/e2e/popularMovie.cy.ts
@@ -3,21 +3,23 @@
 describe("인기 있는 영화 비동기 API 테스트", () => {
   beforeEach(() => {
     cy.intercept(
-      "GET",
-      "https://api.themoviedb.org/3/movie/popular?*",
+      {
+        method: "GET",
+        url: "https://api.themoviedb.org/3/movie/popular?*",
+      },
       (req) => {
         req.continue((res) => {
-          if (req.url.includes("page=1")) {
-            res.body.results = Array(20).fill({ title: "Page 1" });
-          } else if (req.url.includes("page=2")) {
-            res.body.results = Array(20).fill({ title: "Page 2" });
-          }
+          res.setDelay(1000);
+          res.send({ fixture: "popularMovieData.json" });
         });
       }
     ).as("fetchMovies");
 
-    cy.visit("localhost:5173");
+    cy.visit("http://localhost:5173");
+    cy.get(".skeleton-container").should("exist");
+
     cy.wait("@fetchMovies");
+    cy.get(".skeleton-container").should("not.exist");
   });
 
   it("초기 페이지 로딩 시 인기 있는 영화 목록 API를 호출하면서 한 번에 20개씩 목록에 나열되어야 한다.", () => {
@@ -25,10 +27,14 @@ describe("인기 있는 영화 비동기 API 테스트", () => {
     cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
   });
 
-  it("인기 있는 영화에서 더보기 버튼을 누르면 영화 목록 API를 호출하며 20개가 목록에 추가된다.", () => {
+  it("인기 있는 영화에서 더보기 버튼을 누르면 스켈레톤이 표시되고, 영화 목록 API를 호출하며 20개가 목록에 추가된다.", () => {
     cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
     cy.get(".see-more").click();
+
+    cy.get(".skeleton-container").should("exist");
     cy.wait("@fetchMovies").its("request.url").should("include", "page=2");
+
+    cy.get(".skeleton-container").should("not.exist");
     cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 40);
   });
 });
--- cypress/e2e/searchMovie.cy.ts
+++ cypress/e2e/searchMovie.cy.ts
@@ -2,33 +2,39 @@
 
 describe("영화 검색 비동기 API 테스트", () => {
   beforeEach(() => {
-    cy.visit("localhost:5173");
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://api.themoviedb.org/3/search/movie*",
+      },
+      (req) => {
+        req.continue((res) => {
+          res.setDelay(1000);
+          res.send({ fixture: "searchedMovieData.json" });
+        });
+      }
+    ).as("searchMovies");
+
+    cy.visit("http://localhost:5173");
   });
 
-  it("미키 17 검색하면 1개만 나오고 더보기 버튼 안보여야 한다", () => {
-    cy.get("#search-bar").type("미키 17");
-
-    cy.get(".search-icon").click();
-    cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 1);
-    cy.get(".see-more").should("not.exist");
-  });
-
-  it("짱구 검색하면 20개 나오고 '더보기' 버튼이 보여야 한다.", () => {
+  it("검색 및 더보기 버튼 클릭 시 스켈레톤이 표시되고, 데이터를 받아오면 사라진다.", () => {
     cy.get("#search-bar").type("짱구");
     cy.get(".search-icon").click();
+    cy.get(".skeleton-container").should("exist");
 
+    cy.wait("@searchMovies");
     cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
-    cy.get(".see-more").should("exist");
-  });
+    cy.get(".skeleton-container").should("not.exist");
 
-  it("짱구를 검색한 후 '더보기' 버튼을 클릭하면 영화가 추가로 표시되며, 마지막 페이지(2)에 도달하면 '더보기' 버튼이 사라진다.", () => {
-    cy.get("#search-bar").type("짱구");
-    cy.get(".search-icon").click();
     cy.get(".see-more").click();
+    cy.get(".skeleton-container").should("exist");
 
-    cy.get(".thumbnail-list")
-      .find(".thumbnail")
-      .should("have.length.at.most", 40);
+    cy.wait("@searchMovies");
+    cy.get(".skeleton-container").should("not.exist");
+
+    cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 40);
+
     cy.get(".see-more").should("not.exist");
   });
 });
