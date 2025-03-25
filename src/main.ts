f192807bf74c4f2d6aad6610c7d0bbb124649603
jin123457 <jin_ox@naver.com>
2025-03-26 02:43:36 +0900

    refactor: getPopularMovies 호출을 initMovies 함수로 이동하여 비즈니스 로직 분리

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
-import getPopularMovies from './api/getPopularMovies';
+import { initMovies } from './domain/initMovies';
 import { renderHeader } from './view/render/renderHeader';
 import { renderBanner } from './view/render/renderBanner';
 import { renderFooter } from './view/render/renderFooter';
 import { renderMovieList } from './view/render/renderMovieList';
 
 addEventListener('DOMContentLoaded', async () => {
+  const movies = await initMovies();
+
   renderHeader();
-
-  const params = {
-    page: '1',
-    language: 'ko-KR'
-  };
-
-  const movies = await getPopularMovies('/movie/popular', params);
 
   if (movies?.results.length > 0) {
     renderBanner(movies.results);
