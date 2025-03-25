eba107cc3968030131a365078acf847eb926d2a3
ShinjungOh <tlswjd9260@gmail.com>
2025-03-25 16:08:20 +0900

    feat: 별점이 소숫점 1자리까지만 출력되도록 처리

--- src/utils/mapper.ts
+++ src/utils/mapper.ts
@@ -3,6 +3,6 @@ import { Movie } from "../../types/movie";
 export const mapToMovie = (apiData: any): Movie => ({
   id: apiData.id,
   title: apiData.title,
-  voteAverage: apiData.vote_average,
+  voteAverage: Number(apiData.vote_average.toFixed(1)),
   posterPath: apiData.poster_path,
 });
