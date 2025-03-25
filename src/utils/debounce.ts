1a2f2fda23f45c0927e5fbf4225aaa6e411cf703
thgml05 <aisohii@naver.com>
2025-03-25 14:30:13 +0900

    refactor : debounce 함수 settimeout 활용으로 변경

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,9 +1,25 @@
-export const debounce = (callback: () => void) => {
-  let id = -1;
+// export const debounce = (callback: () => void) => {
+//   let id = -1;
 
-  return () => {
-    cancelAnimationFrame(id);
+//   return () => {
+//     cancelAnimationFrame(id);
 
-    id = requestAnimationFrame(callback);
+//     id = requestAnimationFrame(callback);
+//   };
+// };
+
+export const timeoutDebounce = <T extends (...args: unknown[]) => void>(
+  fn: T,
+  delay: number
+) => {
+  let timeout: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>): Promise<unknown> => {
+    return new Promise((resolve) => {
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(async () => {
+        const result = await fn(...args);
+        resolve(result);
+      }, delay);
+    });
   };
 };
