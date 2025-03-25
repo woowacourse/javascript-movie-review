a4b0ce0f9c2cc9787fbea1d010a48741aa07af7d
장정안 <wjddks96@naver.com>
2025-03-25 13:24:09 +0900

    feat: 더보기 영화 리스트 코드 내  반환타입 설정

--- src/apis/movies/useGetMoreMovieList.ts
+++ src/apis/movies/useGetMoreMovieList.ts
@@ -11,7 +11,7 @@ import { options, url } from "../config/config";
 const useGetMoreMovieList = () => {
   const fetchMoreMovies = async (
     callback: (page: number) => Promise<Movie[] | null>
-  ) => {
+  ): Promise<Movie[] | null> => {
     const nextPage = currentPage + 1;
 
     if (searchInputValue.trim()) {
@@ -20,16 +20,20 @@ const useGetMoreMovieList = () => {
         const response = await fetch(url.more(nextPage), options);
         const data = await response.json();
         appendSearchResults(data.results);
+
+        return data.results;
       } catch (error) {
         console.error("Error fetching search results:", error);
         setIsMoreError(true);
       }
+      return null;
     } else {
       // 첫 화면에서 더보기 버튼 누를 때
       const newMovies = await callback(nextPage);
       if (newMovies) {
         appendMovies(newMovies);
       }
+      return newMovies;
     }
   };
   return { fetchMoreMovies };
