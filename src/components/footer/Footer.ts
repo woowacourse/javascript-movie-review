171451a6204e5ce88ba6c6f66a7450e21f7ea6de
장정안 <wjddks96@naver.com>
2025-03-25 15:44:56 +0900

    feat: footer 이미지 내 alt 추가

--- src/components/footer/Footer.ts
+++ src/components/footer/Footer.ts
@@ -4,7 +4,7 @@ const Footer = () => {
   return `
     <footer class="footer">
         <p>&copy; 우아한테크코스 All Rights Reserved.</p>
-        <p><img src="${images.woowacourse}" width="180" /></p>
+        <p><img src="${images.woowacourse}" width="180" alt="우아한테크코스"/></p>
     </footer>
   `;
 };
