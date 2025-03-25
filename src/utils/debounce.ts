cb443956b81c94064602a8843a99fd9c94f68f01
thgml05 <aisohii@naver.com>
2025-03-26 00:26:47 +0900

    refactor : debounce 함수 -> timeoutDebounce 함수로 수정

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,13 +1,3 @@
-// export const debounce = (callback: () => void) => {
-//   let id = -1;
-
-//   return () => {
-//     cancelAnimationFrame(id);
-
-//     id = requestAnimationFrame(callback);
-//   };
-// };
-
 export const timeoutDebounce = <T extends (...args: unknown[]) => void>(
   fn: T,
   delay: number
