4c449f9d532ee0116665b081fc6ae497c0e3e261
spoyodevelop <kjy9505@gmail.com>
2025-03-25 14:06:39 +0900

    chore: 뷰포트 설정

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    viewportWidth: 1440,
+    viewportHeight: 860,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
