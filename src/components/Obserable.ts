ba6c7d0102ed5bfac05fcd76bfc4e7ebe7db6599
guesung <gueit214@naver.com>
2025-03-25 15:34:52 +0900

    feat: Obserable 객체

---
+++ src/components/Obserable.ts
@@ -0,0 +1,16 @@
+import Component from './core/Component';
+
+interface ObserableProps {
+  callback: (entries: IntersectionObserverEntry[]) => void;
+}
+
+export default class Obserable extends Component<ObserableProps> {
+  onRender() {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio <= 0) return;
+
+      this.props.callback(entries);
+    });
+    intersectionObserver.observe(this.element);
+  }
+}
