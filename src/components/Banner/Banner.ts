4052a328f4636a024ca83bf3cc709781e4ce7de7
Oliy <eunsoa0310@gmail.com>
2025-03-25 14:22:42 +0900

    refactor: 배너 컴포넌트에서 innerHTML 템플릿을 createElement 함수로 변환

--- src/components/Banner/Banner.ts
+++ src/components/Banner/Banner.ts
@@ -1,3 +1,5 @@
+import { ICON_PATH } from "../../constants/imagePaths";
+
 export const removeBanner = () => {
   const $header = document.querySelector("header") as HTMLElement;
   $header.classList.remove("banner-open");
@@ -17,15 +19,35 @@ const $Banner = () => {
     className: "background-container",
   });
 
-  $backgroundContainer.innerHTML = `
-  <div class="overlay" aria-hidden="true"></div>
-  <div class="top-rated-container">
-    <div class="rate">
-      <img src="./star_empty.svg" class="star" />
-      <span class="rate-value">9.5</span>
-    </div>
-    <div class="title">인사이드 아웃2</div>
-  </div>`;
+  const $overlay = createElement("div", {
+    className: "overlay",
+    ariaHidden: "true",
+  });
+
+  const $star = createElement("img", {
+    src: ICON_PATH.STAR_EMPTY,
+    className: "star",
+  });
+  const $rateValue = createElement("span", {
+    className: "rate-value",
+    textContent: "9.5",
+  });
+  const $rate = createElement("div", {
+    className: "rate",
+  });
+  $rate.append($star, $rateValue);
+
+  const $title = createElement("div", {
+    className: "title",
+    textContent: "인사이드 아웃2",
+  });
+
+  const $topRatedContainer = createElement("div", {
+    className: "top-rated-container",
+  });
+  $topRatedContainer.append($rate, $title);
+
+  $backgroundContainer.append($overlay, $topRatedContainer);
 
   return $backgroundContainer;
 };
