caf92bb6416785f2f4009b8b9415e5a5f5559c3b
Ko Daeun <drechsu@gmail.com>
2025-03-26 13:25:53 +0900

    refactor : ApiClient클래스의 try catch 삭제 및 에러처리 추가

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,13 +1,14 @@
 class ApiClient {
   static async fetch({ url, options }: { url: string; options: RequestInit }) {
-    try {
-      const raw = await fetch(url, options);
-      const data = await raw.json();
+    const raw = await fetch(url, options);
 
-      return data;
-    } catch (e) {
-      throw new Error((e as Error).message);
+    if (!raw.ok) {
+      throw new Error(`HTTP error: ${raw.status}`);
     }
+
+    const data = await raw.json();
+
+    return data;
   }
 }
 
