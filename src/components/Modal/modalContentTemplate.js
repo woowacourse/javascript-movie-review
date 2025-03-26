d057a91e30be18a5fceec8b6968983efab15f10c
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-26 17:47:38 +0900

    feat: Modal 반응형 구현

--- src/components/Modal/modalContentTemplate.js
+++ src/components/Modal/modalContentTemplate.js
@@ -12,17 +12,19 @@ const modalContentTemplate = async (id) => {
       }" alt="${movie.title}" />
     </div>
     <div class="modal-description">
-      <h2>${movie.title}</h2>
-      <p class="category">${movie.release_date.slice(
-        0,
-        4
-      )} · ${movie.genres.join(", ")}</p>
-      <p class="rate">
-        <span class="label">평균</span>
-        <img src="./images/star_filled.png" class="star" /><span>${
-          movie.vote_average
-        }</span>
-      </p>
+      <div class="description-information">
+        <h2>${movie.title}</h2>
+        <p class="category">${movie.release_date.slice(
+          0,
+          4
+        )} · ${movie.genres.join(", ")}</p>
+        <p class="rate">
+          <span class="label">평균</span>
+          <img src="./images/star_filled.png" class="star" /><span>${
+            movie.vote_average
+          }</span>
+        </p>
+      </div>
       <hr />
       <p class="subtitle">내 별점</p>
       <hr />
