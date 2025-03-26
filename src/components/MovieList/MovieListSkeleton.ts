c5b536139f07b803fbd75a7aa2340a536b7e6dd4
sooyeoniya <tndus1306403@naver.com>
2025-03-26 14:31:46 +0900

    refactor: skeleton UI innerHTML로 변경
    - 가독성 향상

--- src/components/MovieList/MovieListSkeleton.ts
+++ src/components/MovieList/MovieListSkeleton.ts
@@ -1,25 +1,27 @@
 import { Skeleton } from "../index";
 
+const SKELETON_COUNT = 20;
+
 export default function MovieListSkeleton() {
   const $ul = document.querySelector(".thumbnail-list");
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < SKELETON_COUNT; i++) {
     const $li = document.createElement("li");
-    const $item = document.createElement("div");
-    $item.className = "item";
 
-    const $imageSkeleton = Skeleton({ width: 200, height: 300 });
-    const $voteAverageSkeleton = Skeleton({ width: 60, height: 15 });
-    const $titleSkeleton = Skeleton({ width: 150, height: 20 });
+    const $imageSkeleton = Skeleton({ width: 200, height: 300 }).outerHTML;
+    const $voteAverageSkeleton = Skeleton({ width: 60, height: 15 }).outerHTML;
+    const $titleSkeleton = Skeleton({ width: 150, height: 20 }).outerHTML;
 
-    const $itemDesc = document.createElement("div");
-    $itemDesc.className = "item-desc";
+    $li.innerHTML = /* html */ `
+      <div class="item">
+        ${$imageSkeleton}
+        <div class="item-desc">
+          ${$voteAverageSkeleton}
+          ${$titleSkeleton}
+        </div> 
+      </div> 
+    `;
 
-    $itemDesc.append($voteAverageSkeleton, $titleSkeleton);
-    $item.append($imageSkeleton);
-    $item.append($itemDesc);
-
-    $li.append($item);
     $ul?.append($li);
   }
   return $ul;
