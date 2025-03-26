f6ff3a11b13c7074a80c894e697ce068f04f052d
keemsebeen <sebin0580@gmail.com>
2025-03-26 22:10:51 +0900

    feat: App 컴포넌트에 Toast 추가

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,9 @@
 import { Footer } from './components/common/Footer';
 import { Header } from './components/common/Header';
+import { Toast } from './components/common/Toast';
 import { MovieList } from './components/feature/MovieList';
 
 export const App = async () => {
+  Toast();
   document.querySelector('#app')?.append(Header(), await MovieList(), Footer());
 };
