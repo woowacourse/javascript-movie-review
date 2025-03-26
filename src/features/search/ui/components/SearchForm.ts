4ebd21a863cca3084d5bad406fe1e2af01bb1fe4
ohgus <anakin1108@naver.com>
2025-03-26 21:51:45 +0900

    refactor: SearchForm을 별도의 컴포넌트로 분리

---
+++ src/features/search/ui/components/SearchForm.ts
@@ -0,0 +1,22 @@
+import { searchFormSubmitHandler } from "../searchFormSubmitHandler";
+
+const SearchForm = () => {
+  const searchForm = document.createElement("form");
+  searchForm.classList.add("search-form");
+
+  searchForm.innerHTML = /*html*/ `
+    <input
+      id="search-input"
+      name="search-input"
+      type="text"
+      placeholder="검색어를 입력하세요"
+    />
+    <button type="submit" class="search-button">
+      <img src="images/search.png" alt="Search" />
+    </button>
+`;
+
+  return searchForm;
+};
+
+export default SearchForm;
