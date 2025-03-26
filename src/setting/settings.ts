bb61191f178d9cc112c863e906d4ed80dcd852e4
spoyodevelop <kjy9505@gmail.com>
2025-03-26 17:41:41 +0900

    feat: ratingNumbers, ratingMessages 작성

--- src/setting/settings.ts
+++ src/setting/settings.ts
@@ -21,3 +21,17 @@ export const paths = {
   search: "./images/Search.png",
   star_empty: "./images/star_empty.png",
 } as const;
+export const ratingMessages = {
+  1: "최악이예요",
+  2: "별로예요",
+  3: "보통이에요",
+  4: "재미있어요",
+  5: "명작이에요",
+} as const;
+export const ratingNumbers = {
+  1: "(2/10)",
+  2: "(4/10)",
+  3: "(6/10)",
+  4: "(8/10)",
+  5: "(10/10)",
+} as const;
