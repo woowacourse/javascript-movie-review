d1eb574752d002786734edfc3984f744fefb1c2f
ohgus <anakin1108@naver.com>
2025-03-26 22:02:40 +0900

    refactor: main.ts 내부 초기 렌더링 로직 별도의 함수로 분리

--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,36 @@ import { addMoviePost } from "./shared/ui/addMoviePost";
 import { addMoreMovies } from "./shared/domain/addMoreMovies";
 
 addEventListener("DOMContentLoaded", async () => {
+  const $movieList = document.querySelector(".thumbnail-list") as HTMLElement;
+
+  checkUrl();
+  await initMovieList($movieList);
+  await initAddMoreMoviesButton($movieList);
+});
+
+function checkUrl() {
   const url = new URL(window.location.href);
   if (url.searchParams.has("page")) {
     url.searchParams.delete("page");
     window.history.replaceState({}, document.title, url.pathname);
     window.location.reload();
   }
+}
 
-  const $movieList = document.querySelector(".thumbnail-list") as HTMLElement;
-
-  if ($movieList) showSkeletons($movieList);
+async function initMovieList(movieList: HTMLElement) {
+  if (movieList) showSkeletons(movieList);
 
   const movies = await getMovieList({ page: 1 });
 
-  if (movies && $movieList) {
+  if (movies && movieList) {
     Header(movies.results[0]);
 
-    $movieList.innerHTML = "";
-    addMoviePost(movies.results, $movieList);
+    movieList.innerHTML = "";
+    addMoviePost(movies.results, movieList);
   }
+}
 
+async function initAddMoreMoviesButton(movieList: HTMLElement) {
   const $movieContainer = document.getElementById("movie-container");
   const addMoreMoviesButton = CustomButton({
     title: "더보기",
@@ -36,7 +46,7 @@ addEventListener("DOMContentLoaded", async () => {
 
   const $moreMoviesButton = document.getElementById("more-movies-button");
   $moreMoviesButton?.addEventListener("click", async () => {
-    if (!$movieList) return;
-    await addMoreMovies($movieList);
+    if (!movieList) return;
+    await addMoreMovies(movieList);
   });
-});
+}
