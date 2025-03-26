4e33c71bdbf3ee72b2d0e4bef458211bfaf8d758
ohgus <anakin1108@naver.com>
2025-03-26 23:30:12 +0900

    refactor: 새로고침을 눌렀을 때 무조건 홈으로 돌아가지 않고 쿼리를 확인해 검색중인 영화가 있으면 해당 영화 검색 결과 페이지 1로 이동

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,38 @@
 import { getMovieList } from "./features/movie/api/getMovieList";
+import { getSearchedPost } from "./features/search/api/getSearchedPost";
 import Header from "./shared/ui/components/Header";
 import { CustomButton } from "./shared/ui/components/CustomButton";
 import { showSkeletons } from "./shared/ui/showSkeletons";
 import { addMoviePost } from "./shared/ui/addMoviePost";
 import { addMoreMovies } from "./shared/domain/addMoreMovies";
+import { updateSearchPageUI } from "./features/search/ui/searchFormSubmitHandler";
 
 addEventListener("DOMContentLoaded", async () => {
   const $movieList = document.querySelector(".thumbnail-list") as HTMLElement;
 
-  checkUrl();
   await initMovieList($movieList);
   await initAddMoreMoviesButton($movieList);
 });
 
-function checkUrl() {
-  const url = new URL(window.location.href);
-  if (url.searchParams.has("page")) {
-    url.searchParams.delete("page");
-    window.history.replaceState({}, document.title, url.pathname);
-    window.location.reload();
-  }
-}
-
 async function initMovieList(movieList: HTMLElement) {
-  if (movieList) showSkeletons(movieList);
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  const query = params.get("query");
+  const movies = query
+    ? await getSearchedPost(query, 1)
+    : await getMovieList({ page: 1 });
 
-  const movies = await getMovieList({ page: 1 });
+  if (!movies || !movieList) return;
 
-  if (movies && movieList) {
-    Header(movies.results[0]);
+  showSkeletons(movieList);
 
+  Header(movies.results[0]);
+
+  if (query) {
+    url.searchParams.set("page", "1");
+    window.history.replaceState({}, document.title, url.toString());
+    updateSearchPageUI(movies.results, movies.total_pages, url.searchParams);
+  } else {
     movieList.innerHTML = "";
     addMoviePost(movies.results, movieList);
   }
@@ -37,15 +40,20 @@ async function initMovieList(movieList: HTMLElement) {
 
 async function initAddMoreMoviesButton(movieList: HTMLElement) {
   const $movieContainer = document.getElementById("movie-container");
+  if (!$movieContainer) return;
+
   const addMoreMoviesButton = CustomButton({
     title: "더보기",
     className: "add-more-button",
     id: "more-movies-button",
   });
-  $movieContainer?.appendChild(addMoreMoviesButton);
+
+  $movieContainer.appendChild(addMoreMoviesButton);
 
   const $moreMoviesButton = document.getElementById("more-movies-button");
-  $moreMoviesButton?.addEventListener("click", async () => {
+  if (!$moreMoviesButton) return;
+
+  $moreMoviesButton.addEventListener("click", async () => {
     if (!movieList) return;
     await addMoreMovies(movieList);
   });
