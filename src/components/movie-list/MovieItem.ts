1da54ed71ac09f88bd129bfafefc33e881bf6bc1
ha-kuku <hakukudh@gmail.com>
2025-03-26 17:26:23 +0900

    feat: skeleton UI 구현

--- src/components/movie-list/MovieItem.ts
+++ src/components/movie-list/MovieItem.ts
@@ -2,22 +2,37 @@ import { Movie } from "../../../types/movie";
 
 const MovieItem = ({ title, voteAverage, posterPath }: Movie) => {
   const movieItem = document.createElement("li");
-
-  const mappedImage = posterPath
-    ? `https://image.tmdb.org/t/p/w500${posterPath}`
-    : "images/nullImage.png";
+  movieItem.classList.add("movie-item");
 
   movieItem.innerHTML = `
     <div class="item">
-      <img class="thumbnail" src="${mappedImage}" alt="${title}" />
+      <div class="thumbnail skeleton skeleton-img"></div>
       <div class="item-desc">
-        <p class="rate">
-          <img src="images/star_empty.png" class="star" /><span>${voteAverage}</span>
-        </p>
-        <strong>${title}</strong>
+        <div class="skeleton skeleton-text" style="width: 40%;"></div>
+        <div class="skeleton skeleton-text" style="width: 60%;"></div>
       </div>
     </div>
   `;
+
+  const img = new Image();
+  img.src = posterPath
+    ? `https://image.tmdb.org/t/p/w500${posterPath}`
+    : "images/nullImage.png";
+  img.alt = title;
+  img.classList.add("thumbnail");
+  img.onload = () => {
+    movieItem.innerHTML = `
+      <div class="item">
+        <img class="thumbnail" src="${img.src}" alt="${title}" />
+        <div class="item-desc">
+          <p class="rate">
+            <img src="images/star_empty.png" class="star" /><span>${voteAverage}</span>
+          </p>
+          <strong>${title}</strong>
+        </div>
+      </div>
+    `;
+  };
 
   return movieItem;
 };
