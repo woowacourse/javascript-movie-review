e6f7f72660b62b9dd30f80ff39ca3d92f4799f9f
ohgus <anakin1108@naver.com>
2025-03-26 23:28:41 +0900

    refactor: ui 업데이트 로직을 별도의 함수로 분리

--- src/features/search/ui/searchFormSubmitHandler.ts
+++ src/features/search/ui/searchFormSubmitHandler.ts
@@ -2,38 +2,14 @@ import { addMoviePost } from "../../../shared/ui/addMoviePost";
 import { showSkeletons } from "../../../shared/ui/showSkeletons";
 import { getSearchedPost } from "../api/getSearchedPost";
 import { disableMoreButton } from "../../../shared/ui/disabledMoreButton";
+import { IMovie } from "../../../shared/types/movies";
 
 export const searchFormSubmitHandler = async (e: Event) => {
-  const $thumbnailList = document.querySelector(
-    ".thumbnail-list"
-  ) as HTMLElement | null;
-
-  if ($thumbnailList) {
-    $thumbnailList.innerHTML = "";
-    showSkeletons($thumbnailList);
-  }
-
-  const $overlay = document.querySelector(".overlay");
-  $overlay?.classList.add("disabled");
-
-  const $topRatedMovie = document.querySelector(".top-rated-movie");
-  $topRatedMovie?.classList.add("disabled");
-
-  const $backgroundContainer = document.querySelector(".background-container");
-  $backgroundContainer?.classList.add("background-container-disabled");
-
-  const $movieListTitle = document.querySelector(".movie-list-title");
-
   const formData = new FormData(e.target as HTMLFormElement);
   let searchQuery = formData.get("search-input");
 
-  if ($movieListTitle) {
-    $movieListTitle.textContent = `"${searchQuery}" 검색 결과`;
-  }
-
   const params = new URLSearchParams(window.location.search);
   const page = params.get("page");
-
   if (!page) {
     params.append("page", "1");
     params.append("query", searchQuery as string);
@@ -47,17 +23,51 @@ export const searchFormSubmitHandler = async (e: Event) => {
     parseInt(params.get("page")!)
   );
 
-  disableMoreButton(
+  updateSearchPageUI(
+    searchedMovies.results,
     searchedMovies.total_pages,
-    parseInt(params.get("page")!),
-    searchedMovies.results
+    params
   );
-
-  if ($thumbnailList) {
-    $thumbnailList.innerHTML = "";
-    addMoviePost(searchedMovies.results, $thumbnailList);
-  }
 
   const newUrl = `${window.location.pathname}?${params.toString()}`;
   history.pushState(null, "", newUrl);
 };
+
+export function updateSearchPageUI(
+  searchedMovies: IMovie[],
+  searchedMoviesTotalPages: number,
+  params: URLSearchParams
+) {
+  const $thumbnailList = document.querySelector(
+    ".thumbnail-list"
+  ) as HTMLElement;
+  const $movieListTitle = document.querySelector(".movie-list-title");
+
+  if (!$thumbnailList || !$movieListTitle) return;
+
+  $thumbnailList.innerHTML = "";
+  showSkeletons($thumbnailList);
+
+  $movieListTitle.textContent = `"${params.get("query")}" 검색 결과`;
+
+  $thumbnailList.innerHTML = "";
+  addMoviePost(searchedMovies, $thumbnailList);
+
+  disableHeaderImage();
+  disableMoreButton(
+    searchedMoviesTotalPages,
+    parseInt(params.get("page")!),
+    searchedMovies
+  );
+}
+
+function disableHeaderImage() {
+  const $overlay = document.querySelector(".overlay");
+  $overlay?.classList.add("disabled");
+
+  const $topRatedMovie = document.querySelector(".top-rated-movie");
+  $topRatedMovie?.classList.add("disabled");
+
+  const $backgroundContainer = document.querySelector(".background-container");
+  $backgroundContainer?.classList.add("background-container-disabled");
+}
