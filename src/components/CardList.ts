1a7824195b2f6eeda14303b3fe68519629a5c992
ShinjungOh <tlswjd9260@gmail.com>
2025-03-26 16:55:04 +0900

    refactor: CardList Props 타입 범용적으로 변경

--- src/components/CardList.ts
+++ src/components/CardList.ts
@@ -1,42 +1,39 @@
 import CardItem from "./CardItem.ts";
-import { Movie } from "../../types/movie";
 
 type CardListProps = {
-  movieItems?: Movie[];
+  items?: {
+    title: string;
+    voteAverage: number;
+    posterPath: string | null;
+    description?: string;
+  }[];
 };
 
-const CardList = ({ movieItems = [] }: CardListProps) => {
+const CardList = ({ items = [] }: CardListProps) => {
   const movieContainer = document.createElement("section");
   movieContainer.classList.add("movie-container");
 
-  if (movieItems === null) {
+  if (items === null) {
     return;
   }
 
-  if (movieItems.length !== 0) {
+  if (items.length !== 0) {
     const ul = document.createElement("ul");
     ul.classList.add("thumbnail-list");
 
     const fragment = document.createDocumentFragment();
 
-    const cardItems = movieItems.map((movie) =>
+    const cardItems = items.map((item) =>
       CardItem({
-        title: movie.title,
-        rating: movie.voteAverage,
-        imageSrc: movie.posterPath,
+        title: item.title,
+        rating: item.voteAverage,
+        imageSrc: item.posterPath,
       })
     );
 
     fragment.append(...cardItems);
     ul.appendChild(fragment);
     movieContainer.appendChild(ul);
-  }
-
-  if (movieItems.length === 0) {
-    movieContainer.innerHTML = `
-      <img src="images/empty_logo.png" alt="우아한테크코스 로고" />
-      <h2 class="empty-content">검색 결과가 없습니다.</h2>
-  `;
   }
 
   return movieContainer;
