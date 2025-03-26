1fff2f5a158b682cea755073cb6c632112e9f74c
ohgus <anakin1108@naver.com>
2025-03-26 20:50:04 +0900

    refactor: movie.name 속성 제거 모두 title로 통일

--- src/features/movie/ui/components/MoviePost.ts
+++ src/features/movie/ui/components/MoviePost.ts
@@ -3,7 +3,6 @@ import { IMovie } from "../../../../shared/types/movies";
 const MoviePost = (movie: IMovie) => {
   const moviePost = document.createElement("li");
 
-  const movieTitle = movie.name ? movie.name : movie.title;
   const movieImgPath = movie.poster_path
     ? `https://media.themoviedb.org/t/p/w440_and_h660_face${movie.poster_path}`
     : "images/nullImage.png";
@@ -13,7 +12,7 @@ const MoviePost = (movie: IMovie) => {
       <img
         class="thumbnail"
         src=${movieImgPath}
-        alt=${movieTitle}
+        alt=${movie.title}
       />
       <div class="item-desc">
         <p class="rate">
@@ -21,7 +20,7 @@ const MoviePost = (movie: IMovie) => {
             >${movie.vote_average.toFixed(1)}</span
           >
         </p>
-        <strong>${movieTitle}</strong>
+        <strong>${movie.title}</strong>
       </div>
     </div>
   `;
