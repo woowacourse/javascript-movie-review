09fb97f3e15fbbeb6adc51f943c87e7e6299772f
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-26 11:46:37 +0900

    feat: movie detail modal 핸들러 구현

---
+++ src/shared/ui/movieDetailModalHandler.ts
@@ -0,0 +1,38 @@
+import { getMovieDetails } from "../../features/movie/api/getMovieDetails";
+import ErrorPage from "./components/ErrorPage";
+import MovieDetailModal from "./components/MovieDetailModal";
+
+export const movieDetailModalHandler = () => {
+  const $movieCardButton = document.querySelectorAll(".movie-card-button");
+  $movieCardButton.forEach((button) => {
+    button.addEventListener("click", async (e) => {
+      const target = e.target as HTMLElement;
+      const movieId = target.closest(".movie-card-button")?.id;
+
+      try {
+        const {
+          title,
+          backdrop_path,
+          release_date,
+          genres,
+          vote_average,
+          overview,
+        } = await getMovieDetails(Number(movieId));
+        const movieDetails = {
+          title,
+          backdrop_path,
+          release_date,
+          genres,
+          vote_average,
+          overview,
+        };
+
+        MovieDetailModal(movieDetails);
+      } catch (error) {
+        if (error instanceof Error) {
+          ErrorPage("영화 상세 정보를 불러오는데 실패하였습니다.");
+        }
+      }
+    });
+  });
+};
