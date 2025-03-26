97d1bb8d6ce94447244fa0d6cd25293edce06e5a
Jeongeun Lee <leeju1009@gmail.com>
2025-03-27 02:19:49 +0900

    refactor: 통일성을 고려하여 함수선언문을 화살표 함수로 수정

--- src/domain/calculatePageNumber.ts
+++ src/domain/calculatePageNumber.ts
@@ -1,7 +1,7 @@
 import { ITEMS } from "../constants/movie";
 
-function calculatePageNumber(totalMovies: number) {
+const calculatePageNumber = (totalMovies: number) => {
   return totalMovies / ITEMS.perPage + 1;
-}
+};
 
 export default calculatePageNumber;
