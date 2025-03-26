020eab3814e14c511223d777158af9afe1d662d6
spoyodevelop <kjy9505@gmail.com>
2025-03-26 15:22:40 +0900

    refactor: checkApiAvailability 관심사 분리

--- src/service/errorService.ts
+++ src/service/errorService.ts
@@ -11,3 +11,23 @@ export function handleConnectionError(): void {
   $fallbackDetails.innerText =
     "뭔가 잘못되었어요. 인터넷 상태를 체크하신뒤 세로 고침을 해주세요!";
 }
+export function checkApiAvailability(infiniteScrollInstance, delay = 3000) {
+  setTimeout(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular")
+      .then((response) => {
+        if (response.ok) {
+          infiniteScrollInstance.resumeInfiniteScroll();
+        } else {
+          checkApiAvailability(delay * 2);
+        }
+      })
+      .catch((error) => {
+        Toast.showToast(
+          "서버의 상황이 좋지 않아 접속을 다시 시도를 하고 있어요.... 잠깐 기다려 보세요...",
+          "info",
+          2000
+        );
+        checkApiAvailability(delay * 2);
+      });
+  }, delay);
+}
