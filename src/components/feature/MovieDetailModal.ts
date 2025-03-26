44e759d8cb83014baf649920e6c681f88d11539e
keemsebeen <sebin0580@gmail.com>
2025-03-26 19:51:12 +0900

    refactor: MovieDetailModal 내부 overview 컴포넌트 분리

--- src/components/feature/MovieDetailModal.ts
+++ src/components/feature/MovieDetailModal.ts
@@ -1,14 +1,13 @@
+import MovieRating from '../../domain/MovieRating';
 import { MovieDetailResponse } from '../../types/MovieDetail.types';
 import { Box } from '../common/Box';
 import { Horizon } from '../common/Horizon';
 import { IconButton } from '../common/IconButton';
 import { Img } from '../common/Img';
 import { Text } from '../common/Text';
+import { DEFAULT_IMAGE_URL, IMAGE_BASE_URL } from './MovieItem';
 
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w220_and_h330_face';
-const DEFAULT_IMAGE_URL = './images/no_image.png';
-
-const scoreTexts: Record<number, string> = {
+const SCORE_TEXT: Record<number, string> = {
   2: '최악이에요',
   4: '별로예요',
   6: '보통이에요',
@@ -25,30 +24,85 @@ const handleCloseBtnClick = () => {
 };
 
 const scoreTextElement = Text({
-  classList: ['text-xl', 'font-semibold', 'text-yellow'],
+  classList: ['text-xl', 'font-semibold', 'text-white'],
   props: {
     textContent: '0점',
   },
 });
 
 const scoreElement = Text({
-  classList: ['text-lg', 'font-semibold', 'text-white'],
+  classList: ['text-lg', 'font-semibold', 'text-opacity-blue'],
   props: {
     textContent: '(0/10)',
   },
 });
 
-export const MovieDetailModal = (movieDetailData: MovieDetailResponse) => {
-  const { title, release_date, genres, overview, vote_average, poster_path } =
-    movieDetailData;
+const movieInfo = (
+  title: string,
+  formattedReleaseDate: number,
+  genreNames: string,
+  vote_average: number,
+) => {
+  return Box({
+    classList: ['modal-info'],
+    props: {
+      children: [
+        Text({
+          classList: ['text-xl', 'font-bold'],
+          props: {
+            textContent: title,
+          },
+        }),
 
+        Text({
+          classList: ['text-lg', 'font-semibold', 'text-white'],
+          props: {
+            textContent: `${formattedReleaseDate} | ${genreNames}`,
+          },
+        }),
+        Box({
+          classList: ['movie-rate'],
+          props: {
+            children: [
+              Text({
+                classList: ['text-lg', 'font-semibold', 'text-white'],
+                props: {
+                  textContent: '평점',
+                },
+              }),
+              Box({
+                classList: ['flex-row'],
+                props: {
+                  children: [
+                    Img({
+                      width: '20',
+                      height: '20',
+                      src: './images/star_filled.png',
+                    }),
+                    Text({
+                      classList: ['text-lg', 'font-semibold', 'text-yellow'],
+                      props: {
+                        textContent: ` ${vote_average}`,
+                      },
+                    }),
+                  ],
+                },
+              }),
+            ],
+          },
+        }),
+      ],
+    },
+  });
+};
+
+const myRatingSection = (movieId: number, title: string) => {
+  const movieRate = new MovieRating(movieId, title);
   let selectedScore = 0;
-
-  const formattedReleaseDate = new Date(release_date).getFullYear();
-  const genreNames = genres.map((genre) => genre.name).join(', ');
 
   const updateStars = (score: number) => {
     selectedScore = score;
+    movieRate.setRating(score);
 
     starElements.forEach((star, index) => {
       const starScore = (index + 1) * 2;
@@ -62,7 +116,7 @@ export const MovieDetailModal = (movieDetailData: MovieDetailResponse) => {
       }
     });
 
-    scoreTextElement.textContent = scoreTexts[score] || '0점';
+    scoreTextElement.textContent = SCORE_TEXT[score] || '0점';
     scoreElement.textContent = `(${score}/10)`;
   };
 
@@ -79,125 +133,128 @@ export const MovieDetailModal = (movieDetailData: MovieDetailResponse) => {
     });
   });
 
+  const storedRating = movieRate.getRating();
+  if (storedRating !== null) {
+    selectedScore = storedRating;
+    updateStars(storedRating);
+  } else {
+    updateStars(0);
+  }
+
   return Box({
-    classList: ['modal-container'],
+    classList: ['modal-my-rate'],
     props: {
       children: [
-        Img({
-          src: poster_path
-            ? `${IMAGE_BASE_URL}${poster_path}`
-            : DEFAULT_IMAGE_URL,
-          classList: ['modal-image'],
+        Text({
+          classList: ['text-xl', 'font-semibold'],
           props: {
-            alt: title,
+            textContent: '내 별점',
           },
         }),
-
         Box({
-          classList: ['modal-description'],
+          classList: ['modal-rate'],
           props: {
             children: [
               Box({
-                classList: ['modal-between'],
+                classList: ['flex-row'],
                 props: {
-                  children: [
-                    Text({
-                      classList: ['text-xl', 'font-bold'],
-                      props: {
-                        textContent: title,
-                      },
-                    }),
-                    IconButton({
-                      width: '15',
-                      height: '15',
-                      src: './images/modal_button_close.png',
-                      onClick: () => {
-                        handleCloseBtnClick();
-                      },
-                    }),
-                  ],
+                  children: starElements,
+                },
+              }),
+              Box({
+                classList: ['flex-row'],
+                props: {
+                  children: [scoreTextElement, scoreElement],
+                },
+              }),
+            ],
+          },
+        }),
+      ],
+    },
+  });
+};
+
+const overView = (overview: string) => {
+  return Box({
+    props: {
+      children: [
+        Text({
+          classList: ['text-xl', 'font-semibold'],
+          props: {
+            textContent: '줄거리',
+          },
+        }),
+        Box({
+          classList: ['detail'],
+          props: {
+            children: [
+              Text({
+                props: {
+                  textContent: overview ? overview : '줄거리 정보가 없습니다.',
+                },
+              }),
+            ],
+          },
+        }),
+      ],
+    },
+  });
+};
+
+export const MovieDetailModal = (movieDetailData: MovieDetailResponse) => {
+  const {
+    id,
+    title,
+    release_date,
+    genres,
+    overview,
+    vote_average,
+    poster_path,
+  } = movieDetailData;
+
+  const formattedReleaseDate = new Date(release_date).getFullYear();
+  const genreNames = genres.map((genre) => genre.name).join(', ');
+
+  return Box({
+    classList: ['modal-col'],
+    props: {
+      children: [
+        IconButton({
+          width: '15',
+          height: '15',
+          src: './images/modal_button_close.png',
+          classList: ['modal-close-btn'],
+          onClick: () => handleCloseBtnClick(),
+        }),
+        Box({
+          classList: ['modal-container'],
+          props: {
+            children: [
+              Img({
+                src: poster_path
+                  ? `${IMAGE_BASE_URL}${poster_path}`
+                  : DEFAULT_IMAGE_URL,
+                classList: ['modal-image'],
+                props: {
+                  alt: title,
                 },
               }),
 
-              Text({
-                classList: ['text-lg', 'font-semibold', 'text-yellow'],
-                props: {
-                  textContent: `${formattedReleaseDate} | ${genreNames}`,
-                },
-              }),
               Box({
-                classList: ['rate'],
+                classList: ['modal-description'],
                 props: {
                   children: [
-                    Text({
-                      classList: ['text-lg', 'font-semibold', 'text-white'],
-                      props: {
-                        textContent: '평점',
-                      },
-                    }),
-                    Box({
-                      classList: ['flex-row'],
-                      props: {
-                        children: [
-                          Img({
-                            width: '16',
-                            height: '16',
-                            src: './images/star_filled.png',
-                          }),
-                          Text({
-                            classList: [
-                              'text-lg',
-                              'font-semibold',
-                              'text-yellow',
-                            ],
-                            props: {
-                              textContent: ` ${vote_average}`,
-                            },
-                          }),
-                        ],
-                      },
-                    }),
-                  ],
-                },
-              }),
-              Horizon({ classList: ['custom-hr'] }),
-              Text({
-                classList: ['text-lg', 'font-semibold'],
-                props: {
-                  textContent: '내 별점',
-                },
-              }),
-              Box({
-                classList: ['rate'],
-                props: {
-                  children: [
-                    Box({
-                      classList: ['flex-row'],
-                      props: {
-                        children: starElements,
-                      },
-                    }),
-                    scoreTextElement,
-                    scoreElement,
-                  ],
-                },
-              }),
-              Horizon({ classList: ['custom-hr'] }),
-              Text({
-                classList: ['text-lg', 'font-semibold'],
-                props: {
-                  textContent: '줄거리',
-                },
-              }),
-              Box({
-                classList: ['detail'],
-                props: {
-                  children: [
-                    Text({
-                      props: {
-                        textContent: overview,
-                      },
-                    }),
+                    movieInfo(
+                      title,
+                      formattedReleaseDate,
+                      genreNames,
+                      vote_average,
+                    ),
+                    Horizon({ classList: ['custom-hr'] }),
+                    myRatingSection(id, title),
+                    Horizon({ classList: ['custom-hr'] }),
+                    overView(overview),
                   ],
                 },
               }),
