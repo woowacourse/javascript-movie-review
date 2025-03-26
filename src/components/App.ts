9a11ca3cee757575bf172220134ec886b31db037
sooyeoniya <tndus1306403@naver.com>
2025-03-27 01:36:42 +0900

    chore(Tab): 구현하지 않는 Tab 기능 제거

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,9 +1,8 @@
 import Button from "./Common/Button";
-import Modal from "./Layout/Modal";
 import Header from "./Layout/Header";
-import Tab from "./Layout/Tab";
-import Main from "./Layout/Main";
+import Modal from "./Layout/Modal";
 import Footer from "./Layout/Footer";
+import Main from "./Layout/Main";
 
 export default function App() {
   const $wrap = document.createElement("div");
@@ -13,13 +12,12 @@ export default function App() {
   $container.className = "container";
 
   const $showMore = Button({ className: "show-more", textContent: "더 보기" });
-  const $tab = Tab();
   const $main = Main.getInstance().getElement();
   const $header = Header.getInstance().getElement();
   const $modal = Modal();
   const $footer = Footer.getInstance().getElement();
 
-  $container.append($tab, $main, $showMore);
+  $container.append($main, $showMore);
   $wrap.append($header, $container, $footer, $modal);
 
   return $wrap;
