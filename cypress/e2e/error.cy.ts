3c37f154064f7a33b6e8d47e7d88c6e85484fc5b
keemsebeen <sebin0580@gmail.com>
2025-03-27 00:05:11 +0900

    test: error 테스트 구체화

--- cypress/e2e/error.cy.ts
+++ cypress/e2e/error.cy.ts
@@ -16,7 +16,12 @@ describe('영화 리뷰 e2e 테스트', () => {
   });
 
   it('401 에러가 발생했을 경우 에러 메시지를 보여준다.', () => {
-    cy.wait(3000);
+    cy.wait(1000);
     cy.get('@getError').its('response.statusCode').should('eq', 401);
+    cy.get('.toast').should('exist');
+    cy.get('.toast p').should(
+      'have.text',
+      '🔒 인증되지 않은 요청입니다. 로그인 후 이용해주세요. 🔒',
+    );
   });
 });
