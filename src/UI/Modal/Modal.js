f1e3d78b783ff35b7bca938c20d5d7dac25db7cf
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-27 11:47:13 +0900

    ✨ feat: 모달 기본 UI 구현 (수정 필요)

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -27,13 +27,29 @@ class Modal {
                 this.movieDetail.release_date.split("-")[0]
               } · ${this.movieDetail.genres.map((data) => data.name)}
             </p>
+            <div class="average_star_container">
+            <p class=>평균 </p>
             <p class="rate">
               <img src="./images/star_filled.png" class="star" /><span
                 >${parseFloat(this.movieDetail.vote_average).toFixed(1)}</span
               >
             </p>
+            </div>
             <hr />
-            <p class="subtitle">줄거리</p>
+            <p class="middle_font">내 별점</p>
+            <div class="my_star_container">
+              <p class="rate">
+                <img src="./images/star_empty.png" class="star1" />
+                <img src="./images/star_empty.png" class="star2" />
+                <img src="./images/star_empty.png" class="star3" />
+                <img src="./images/star_empty.png" class="star4" />
+                <img src="./images/star_empty.png" class="star5" />
+              </p>
+              <p class="middle_font"> 명작이에요</p>
+              <p class="star_count">(8/10)</p>
+            </div>
+            <hr/>
+            <p class="middle_font">줄거리</p>
             <p class="detail">
               ${this.movieDetail.overview}
             </p>
