37e8aea92f8069446a4526c2ba3433e5eab72c5f
dlsxjzld <sinjuk1@naver.com>
2025-03-27 21:59:01 +0900

    feat: 영화 상세 정보 컴포넌트 추가

---
+++ src/components/movie/movieDetailInfo.ts
@@ -0,0 +1,56 @@
+import { createElementWithAttributes } from "../utils/createElementWithAttributes";
+import noImage from "/images/no_image.png";
+
+const movieDetailInfo = (movie) => {
+  const $movieDetailInfo = createElementWithAttributes({
+    tag: "div",
+    className: "modal-container",
+    children: [
+      {
+        tag: "div",
+        className: "modal-image",
+        children: [
+          {
+            tag: "img",
+            attributes: {
+              src:
+                movie.poster_path === null
+                  ? noImage
+                  : `https://image.tmdb.org/t/p/w440_and_h660_face${movie.poster_path}`,
+            },
+          },
+        ],
+      },
+      {
+        tag: "div",
+        className: "modal-description",
+        children: [
+          { tag: "h2", textContent: movie.title },
+          {
+            tag: "p",
+            className: "category",
+            textContent: movie.genres.map((genre) => genre.name).join(", "),
+          },
+          {
+            tag: "p",
+            className: "rate",
+            children: [
+              {
+                tag: "img",
+                className: "star",
+                attributes: { src: "./images/star_filled.png" },
+                children: [{ tag: "span", textContent: movie.vote_average }],
+              },
+            ],
+          },
+          { tag: "hr" },
+          { tag: "p", className: "detail", textContent: movie.overview },
+        ],
+      },
+    ],
+  });
+
+  return $movieDetailInfo;
+};
+
+export default movieDetailInfo;
