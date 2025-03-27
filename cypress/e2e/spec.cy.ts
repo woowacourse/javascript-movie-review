b8c937878da1bf27c881ebd23e9f3ab8e711a911
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-27 14:59:14 +0900

    test: 상세 모달 e2e테스트코드 추가

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe("영화 리뷰 페이지 테스트", () => {
   beforeEach(() => {
     cy.visit("localhost:5173");
@@ -5,11 +7,6 @@ describe("영화 리뷰 페이지 테스트", () => {
   describe("인기 있는 영화가 정상적으로 보여진다.", async () => {
     it("처음 페이지에 접속하면 20개의 영화 목록이 보인다.", () => {
       cy.get(".item").should("have.length", 20);
-    });
-
-    it("더보기 버튼을 누르면 20개의 영화가 추가된다.", () => {
-      cy.get(".add-more-button").click();
-      cy.get(".item").should("have.length", 40);
     });
   });
   describe("특정 키워드로 영화 검색을 하면 해당 영화들이 보여진다.", () => {
@@ -32,7 +29,7 @@ describe("영화 리뷰 페이지 테스트", () => {
     });
   });
 
-  describe("영화 API 테스트", () => {
+  describe("인기 영화 API 테스트", () => {
     beforeEach(() => {
       cy.intercept(
         {
@@ -49,9 +46,46 @@ describe("영화 리뷰 페이지 테스트", () => {
 
     it("영화 API를 통해 영화 목록을 받아온다.", () => {
       cy.wait("@getPopularMovies").then((interception) => {
-        const popularMovies = interception.response.body.results;
+        const popularMovies = interception.response?.body.results;
         expect(popularMovies.length).to.equal(20);
       });
+    });
+  });
+
+  // describe("영화 무한 스크롤 테스트", () => {
+  //   it("지금 인기 있는 영화 페이지에서 스크롤이 가장 하단에 위치할 시 추가 영화 목록 20개를 받아온다.", () => {
+  //     cy.get(".item").should("have.length", 20);
+
+  //     cy.get("#target").should("exist").scrollIntoView();
+
+  //     cy.wait(1000);
+
+  //     cy.get(".item").should("have.length", 40);
+  //   });
+  // });
+
+  describe("영화 상세 모달 테스트", () => {
+    it("지금 인기 있는 영화 페이지에서 특정 Movie Card 클릭 시 영화 상세 모달이 띄어지고 삭제 버튼 클릭 시 모달이 삭제된다.", () => {
+      cy.get(".movie-card-button").first().click();
+      cy.get(".modal").should("exist");
+      cy.get("#closeModal").click();
+      cy.get(".modal").should("not.exist");
+    });
+  });
+
+  describe("영화 내 별점 업데이트 테스트", () => {
+    it("영화 상세 모달에서 내 별점 2점 클릭 시 내 별점이 2점으로 업데이트 된다.", () => {
+      cy.get(".movie-card-button").first().click();
+      cy.get(".star-button").first().click();
+      cy.get(".star-rating")
+        .first()
+        .find('img[src="./images/star_filled.png"]')
+        .should("have.length", 1);
+
+      cy.get(".rating-out-of-ten")
+        .invoke("text")
+        .should("include", "최악이에요")
+        .and("include", "(2/10)");
     });
   });
 });
