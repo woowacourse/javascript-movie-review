27f38c2b304e29901851b38302b32badb78723ec
dlsxjzld <sinjuk1@naver.com>
2025-03-27 15:29:12 +0900

    refactor: 사용자 친화적인 에러 메시지를 출력한다는 주석 추가

--- src/domain/apis/tmdbApi.ts
+++ src/domain/apis/tmdbApi.ts
@@ -39,9 +39,10 @@ const tmdbApi = async <T>(
     return response;
   } catch (error) {
     if (error instanceof Error) {
-      const message =
-        ERROR_MESSAGE[Number(error.message)] ?? "네트워크 에러입니다.";
-      throw new Error(message);
+      // TMDB API에서 전달하는 error에 관련된 status_code를 통해 사용자 친화적인 에러 메시지를 출력합니다.
+      throw new Error(
+        ERROR_MESSAGE[Number(error.message)] ?? "네트워크 에러입니다."
+      );
     } else {
       throw new Error("알 수 없는 에러입니다.");
     }
