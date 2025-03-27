c60fa5925909617b08474feacf73b65463105f1e
Jeongeun Lee <leeju1009@gmail.com>
2025-03-27 18:02:28 +0900

    refactor: searchBar의 로직 분리

--- src/components/SearchBar.ts
+++ src/components/SearchBar.ts
@@ -1,16 +1,15 @@
-import { TotalMovies } from "../../types/domain.ts";
-import calculatePageNumber from "../domain/calculatePageNumber.ts";
-import movieService from "../service/movieService.ts";
+import { Movie } from "../../types/domain.ts";
 import { selectElement } from "../utils/dom.ts";
-import { toggleElementVisibility } from "../utils/Render.ts";
 import MovieItem from "./MovieItem";
 import MovieList from "./MovieList.ts";
 
 class SearchBar {
   #element;
+  #query;
 
   constructor() {
     this.#element = document.createElement("div");
+    this.#query = "";
   }
 
   create() {
@@ -21,22 +20,81 @@ class SearchBar {
     return this.#element;
   }
 
-  async onSearchClick() {
-    const searchBar = selectElement<HTMLInputElement>(".search-bar");
-    const query = searchBar.value;
-    const seeMoreButton = selectElement<HTMLButtonElement>("#seeMore");
+  async #onSearch(
+    movieList: MovieList,
+    getSearchResults: (
+      query: string,
+      currentItemCount: number
+    ) => Promise<Movie[]>
+  ) {
+    const currentItemCount = movieList.getTotalItems();
+    const newMovieData = await getSearchResults(this.#query, currentItemCount);
 
-    this.#changeTitleStyle(query);
-    toggleElementVisibility(".no-thumbnail", "hidden");
-    toggleElementVisibility(".skeleton-list", "show");
+    this.#renderMovieList(newMovieData, movieList);
+  }
+
+  async #onSearchTriggerBar(
+    getSearchResults: (
+      query: string,
+      currentItemCount: number
+    ) => Promise<Movie[]>
+  ) {
+    this.#changeTitleStyle();
 
     const movieList = new MovieList([]);
     movieList.clearList();
 
-    await this.#renderSearchResult(movieList, query);
+    const searchBar = selectElement<HTMLInputElement>(".search-bar");
+    this.#query = searchBar.value;
 
+    await this.#onSearch(movieList, getSearchResults);
+
+    this.#onSeeMoreButtonClick(movieList, getSearchResults);
+  }
+
+  #onSeeMoreButtonClick(
+    movieList: MovieList,
+    getSearchResults: (
+      query: string,
+      currentItemCount: number
+    ) => Promise<Movie[]>
+  ) {
+    const seeMoreButton = selectElement<HTMLButtonElement>("#seeMore");
     seeMoreButton.onclick = async () => {
-      await this.#renderSearchResult(movieList, query);
+      await this.#onSearch(movieList, getSearchResults);
+    };
+  }
+
+  async setEvent(
+    getSearchResults: (
+      query: string,
+      currentItemCount: number
+    ) => Promise<Movie[]>
+  ) {
+    const searchBar = selectElement<HTMLInputElement>(".search-bar");
+    const searchButton = selectElement<HTMLImageElement>("#search");
+
+    searchButton.onclick = () => {
+      this.#onSearchTriggerBar(getSearchResults);
+    };
+
+    const handleEnterKeyDown = async (event: KeyboardEvent) => {
+      if (event.isComposing) {
+        return;
+      }
+
+      if (event.key === "Enter") {
+        this.#onSearchTriggerBar(getSearchResults);
+        searchBar.blur();
+      }
+    };
+
+    searchBar.onfocus = () => {
+      window.addEventListener("keydown", handleEnterKeyDown);
+    };
+
+    searchBar.onblur = () => {
+      window.removeEventListener("keydown", handleEnterKeyDown);
     };
   }
 
@@ -46,25 +104,6 @@ class SearchBar {
     searchInput.classList.add("search-bar");
     searchInput.placeholder = "검색어를 입력하세요";
 
-    const handleEnterKeyDown = (event: KeyboardEvent) => {
-      if (event.isComposing) {
-        return;
-      }
-
-      if (event.key === "Enter") {
-        this.onSearchClick();
-        searchInput.blur();
-      }
-    };
-
-    searchInput.onfocus = () => {
-      window.addEventListener("keydown", handleEnterKeyDown);
-    };
-
-    searchInput.onblur = () => {
-      window.removeEventListener("keydown", handleEnterKeyDown);
-    };
-
     return searchInput;
   }
 
@@ -73,14 +112,11 @@ class SearchBar {
     imgButton.id = "search";
     imgButton.src = "./images/search_button.png";
     imgButton.alt = "SearchButton";
-    imgButton.onclick = () => {
-      this.onSearchClick();
-    };
 
     return imgButton;
   }
 
-  #changeTitleStyle(query: string) {
+  #changeTitleStyle() {
     const overlay = selectElement<HTMLDivElement>(".overlay");
     const topRatedContainer = selectElement<HTMLDivElement>(".top-rated-movie");
     const backgroundContainer = selectElement<HTMLDivElement>(
@@ -88,38 +124,19 @@ class SearchBar {
     );
     const subTitle = selectElement<HTMLHeadingElement>(".subTitle");
 
-    subTitle.textContent = `"${query}" 검색 결과`;
+    subTitle.textContent = `"${this.#query}" 검색 결과`;
     overlay.style.display = "none";
     topRatedContainer.style.display = "none";
     backgroundContainer.style.height = "auto";
   }
 
-  async #renderSearchResult(movieListInstance: MovieList, query: string) {
-    const currentItemCount = movieListInstance.getTotalItems();
-    const pageNumber = calculatePageNumber(currentItemCount);
+  #renderMovieList(movies: Movie[], movieList: MovieList) {
+    const movieItems = movies.map(({ title, posterPath, voteAverage }) => {
+      const movieItem = new MovieItem({ title, voteAverage, posterPath });
+      return movieItem.create();
+    });
 
-    toggleElementVisibility(".skeleton-list", "show");
-    toggleElementVisibility("#seeMore", "hidden");
-
-    const searchResult = await this.#getSearchResult(pageNumber, query);
-    if (pageNumber < searchResult.totalPages)
-      toggleElementVisibility("#seeMore", "show");
-    if (searchResult.totalResults === 0)
-      toggleElementVisibility(".no-thumbnail", "show");
-    toggleElementVisibility(".skeleton-list", "hidden");
-
-    const movieItems = searchResult.results.map(
-      ({ title, posterPath, voteAverage }) => {
-        const movieItem = new MovieItem({ title, voteAverage, posterPath });
-        return movieItem.create();
-      }
-    );
-
-    movieListInstance.updateList(movieItems);
-  }
-
-  async #getSearchResult(pageNumber: number, query: string) {
-    return (await movieService.searchMovies(pageNumber, query)) as TotalMovies;
+    movieList.updateList(movieItems);
   }
 }
 
