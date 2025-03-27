12c49abde1cea645ee7419b7a1440f26d6b5357f
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-27 15:53:10 +0900

    feat: 별점 세부 항목 상수화

--- src/constants/config.js
+++ src/constants/config.js
@@ -9,4 +9,12 @@ const ERROR_MESSAGES = Object.freeze({
     "영화 정보를 불러오는 데 실패했습니다. 새로고침 해 주세요.",
 });
 
-export { MOVIE_COUNT, ERROR_MESSAGES };
+const SCORE_MESSAGES = Object.freeze({
+  2: "최악이예요",
+  4: "별로예요",
+  6: "보통이에요",
+  8: "재미있어요",
+  10: "명작이에요",
+});
+
+export { MOVIE_COUNT, ERROR_MESSAGES, SCORE_MESSAGES };
