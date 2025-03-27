fb6931c728d183c7d471093e4c2fce4281862bde
deokbin <deokbin@naver.com>
2025-03-27 15:59:07 +0900

    feat: 장르 관련 타입 추가

--- types/movieType.ts
+++ types/movieType.ts
@@ -1,8 +1,14 @@
 export interface MovieInfo {
   id?: number;
   title: string;
+  genres: Genre[];
   poster_path: string;
   backdrop_path?: string;
   overview: string;
   vote_average: number;
 }
+
+type Genre = {
+  id: number;
+  name: string;
+};
