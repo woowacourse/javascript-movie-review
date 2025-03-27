78f2a5dcb542a1ef5af1b587ec7cc528eeeaa517
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-03-27 10:50:40 +0900

    refactor: getPage 메서드의 실제 역할에 맞게 getNextPage로 네이밍 변경

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,10 +1,10 @@
-import MovieList from './MovieList';
-import fetchPopularMovies from '../fetch/fetchPopularMovies';
-import fetchSearchMovies from '../fetch/fetchSearchMovies';
-import movies from '../store/Movies';
-import page from '../store/page';
-import createElement from './utils/createElement';
-import { ButtonType } from '../types/ButtonType';
+import MovieList from "./MovieList";
+import fetchPopularMovies from "../fetch/fetchPopularMovies";
+import fetchSearchMovies from "../fetch/fetchSearchMovies";
+import movies from "../store/Movies";
+import page from "../store/page";
+import createElement from "./utils/createElement";
+import { ButtonType } from "../types/ButtonType";
 
 interface ButtonProps {
   text: string;
@@ -12,40 +12,42 @@ interface ButtonProps {
 }
 const Button = ({ text, type }: ButtonProps) => {
   const $button = createElement({
-    tag: 'button',
-    classNames: ['primary', `${type}`],
+    tag: "button",
+    classNames: ["primary", `${type}`],
   });
 
   $button.textContent = text;
 
-  $button.addEventListener("click", async () => {    
+  $button.addEventListener("click", async () => {
     const params = new URLSearchParams(window.location.search);
-    
+
     let fetchedMovies;
-    const currentPage = page.getPage();
-    if(params.has("query")) {
-      fetchedMovies = await fetchSearchMovies(params.get("query") || '', currentPage);
-    }
-    else {
+    const currentPage = page.getNextPage();
+    if (params.has("query")) {
+      fetchedMovies = await fetchSearchMovies(
+        params.get("query") || "",
+        currentPage
+      );
+    } else {
       fetchedMovies = await fetchPopularMovies(currentPage);
     }
 
     movies.addMovies(fetchedMovies.results);
 
-    if (fetchedMovies.totalPages  === currentPage) {
-      $button.classList.toggle('disappear');
+    if (fetchedMovies.totalPages === currentPage) {
+      $button.classList.toggle("disappear");
     }
 
-    const thumbnailList = document.querySelector('.thumbnail-list');
+    const thumbnailList = document.querySelector(".thumbnail-list");
     if (thumbnailList) {
       thumbnailList.remove();
     }
 
-    const section = document.querySelector('section');
+    const section = document.querySelector("section");
     if (section) {
       section.appendChild(
         MovieList({
-            movies: movies.movieList,
+          movies: movies.movieList,
         })
       );
     }
