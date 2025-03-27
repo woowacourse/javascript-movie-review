b4a059301f2d960f222d748d89d9eb22d0bfe521
deokbin <deokbin@naver.com>
2025-03-27 15:58:33 +0900

    feat: 썸네일에 고유 영화 id 데이터셋 부여

--- src/components/Main/MovieItem.js
+++ src/components/Main/MovieItem.js
@@ -1,12 +1,13 @@
 import MovieCaption from "./MovieCaption";
 import ThumbnailImage from "./ThumbnailImage";
 
-function MovieItem({ title, poster_path, vote_average }) {
+function MovieItem({ id, title, poster_path, vote_average }) {
   const $movieItem = document.createElement("li");
   const $movieItemContainer = document.createElement("div");
   $movieItemContainer.classList.add("item");
 
   const thumbnailImage = ThumbnailImage({
+    id,
     title,
     poster_path,
   });
