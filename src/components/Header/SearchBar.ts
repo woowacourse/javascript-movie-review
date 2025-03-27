701c001d3c6ab9d634b0a36cde4a6301661c705a
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-27 18:15:36 +0900

    refactor: SearchBar 컴포넌트 TS 마이그레이션

--- src/components/Header/SearchBar.ts
+++ src/components/Header/SearchBar.ts
@@ -1,14 +1,16 @@
-import { fetchSearchedMovies } from "../../APIs/movieAPI.ts";
+// SearchBar.ts
+import { fetchSearchedMovies } from "../../APIs/movieAPI";
+import Store from "../../store/store";
 
 const SEARCH_FORM = "search-form";
 
-const SearchBar = (store) => {
+const SearchBar = (store: Store): string => {
   setTimeout(() => attachSearchEvent(store), 0);
 
   return /* html */ `
     <div class="search-bar-container">
-      <form id="${SEARCH_FORM}" class="${SEARCH_FORM}" data-testid='${SEARCH_FORM}'>
-        <input type="text" name="query" data-testid='search-input' class="search-bar" placeholder="검색어를 입력하세요" autocomplete="off" />
+      <form id="${SEARCH_FORM}" class="${SEARCH_FORM}" data-testid="${SEARCH_FORM}">
+        <input type="text" name="query" data-testid="search-input" class="search-bar" placeholder="검색어를 입력하세요" autocomplete="off" />
         <button type="submit" class="search-button">
           <img src="./images/search.png" alt="search" width="16" height="16" />
         </button>
@@ -17,29 +19,28 @@ const SearchBar = (store) => {
   `;
 };
 
-function attachSearchEvent(store) {
-  const $searchForm = document.querySelector("#search-form");
-
+function attachSearchEvent(store: Store): void {
+  const $searchForm = document.querySelector(
+    "#search-form"
+  ) as HTMLFormElement | null;
   if ($searchForm) {
-    $searchForm.addEventListener("submit", async (event) => {
+    $searchForm.addEventListener("submit", async (event: Event) => {
       event.preventDefault();
       const formData = new FormData($searchForm);
-      const query = formData.get("query");
+      const query = formData.get("query") as string;
       if (!query) return;
 
       store.setState({ loading: true });
-
-      const searchedMovies = await fetchSearchedMovies(query, (error) =>
+      const searchedMovies = await fetchSearchedMovies(query, (error: Error) =>
         alert(error.message)
       );
       if (searchedMovies) {
         store.setState({
           movies: searchedMovies.results,
-          query: query,
+          query: query as string,
           searchedMoviesLength: searchedMovies.total_results,
         });
       }
-
       store.setState({ loading: false });
       $searchForm.reset();
       window.scrollTo(0, 0);
