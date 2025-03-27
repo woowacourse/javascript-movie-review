092ec695ff88e8824a10aa408d558d483eec5e85
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-27 13:06:39 +0900

    feat: 모달 밖 또는 esc 키를 눌렀을 때 모달 꺼지는 기능 구현

--- src/shared/ui/detailModal/removeDetailModal.ts
+++ src/shared/ui/detailModal/removeDetailModal.ts
@@ -2,7 +2,19 @@ export const removeDetailModal = () => {
   const $modalBackground = document.getElementById("modalBackground");
   const $closeModal = document.getElementById("closeModal");
 
+  $modalBackground?.addEventListener("click", (e) => {
+    if (e.target === $modalBackground) {
+      $modalBackground.remove();
+    }
+  });
+
   $closeModal?.addEventListener("click", () => {
     $modalBackground?.remove();
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (e.key === "Escape") {
+      $modalBackground?.remove();
+    }
   });
 };
