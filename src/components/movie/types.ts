aaa2e4bb81eda4eca080076e34db29a97bb05335
mlnwns <mlnwns@naver.com>
2025-03-27 17:40:55 +0900

    feat: 영화 상세 정보 타입 정의 추가

--- src/components/movie/types.ts
+++ src/components/movie/types.ts
@@ -21,3 +21,33 @@ export interface MovieData {
   total_pages: number;
   total_results: number;
 }
+
+export interface DetailMovieData {
+  adult: boolean;
+  backdrop_path: string;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: Date;
+  revenue: number;
+  runtime: number;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
