4452db3f946c4813f630dccae378e40b60cf411d
dev-dino22 <wlsskdud02@naver.com>
2025-03-27 13:52:19 +0900

    refactor: logo 클릭시 reload->a herf"/" 로 변경

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -3,14 +3,12 @@ import SearchForm from './SearchForm.js'
 function Header() {
   function template() {
     return `
-    
-     <h1 class="logo" data-action="reload">
-            <img data-action="reload" src="./images/logo.png" alt="MovieList" />
-        </h1>
-    <div class="header-container">
-
+        <a href="/" class="logo">
+            <img src="./images/logo.png" alt="MovieList" />
+        </a>
+        <div class="header-container">
             ${SearchForm()}
-    </div>
+        </div>
     `
   }
 
