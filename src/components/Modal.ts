10d62eb613e41d527a377375908965032802ee64
Jeongeun Lee <leeju1009@gmail.com>
2025-03-28 02:16:14 +0900

    feat: 모달 컴포넌트 생성

---
+++ src/components/Modal.ts
@@ -0,0 +1,28 @@
+import { selectElement } from "../utils/dom.ts";
+
+class Modal {
+  #element;
+
+  constructor() {
+    this.#element = this.create();
+  }
+
+  create() {
+    const template = /*html*/ `
+    <div class="modal-background active" id="modalBackground">
+      <div class="modal" id="modal">
+        <button class="close-modal" id="closeModal">
+          <img src="./images/modal_button_close.png" />
+        </button>
+      </div>
+    </div>
+    `;
+
+    const body = selectElement<HTMLBodyElement>("body");
+    body.insertAdjacentHTML("beforeend", template);
+
+    return selectElement<HTMLDivElement>("#modal");
+  }
+}
+
+export default Modal;
