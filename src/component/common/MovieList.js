cd4eb90b17d473907b39df3846cae5dfe1649704
dev-dino22 <wlsskdud02@naver.com>
2025-03-27 10:58:38 +0900

    refactor: MovieList의 반환값 엘리먼트->템플릿문자열 로 변경

--- src/component/common/MovieList.js
+++ src/component/common/MovieList.js
@@ -1,24 +1,24 @@
 import { createElement } from '../../util/utils.js'
 import MovieItem from './MovieItem.js'
-import roundRating from '../../util/roundRating.js';
+import roundRating from '../../util/roundRating.js'
 
 function MovieList(dataList) {
-  function template() {
-    const ul = createElement('ul');
-    ul.classList.add('thumbnail-list');
-    ul.id = 'thumbnailList';
-    const docfrag = document.createDocumentFragment();
-    dataList.forEach((data) => {
-      const li = createElement('li');
-      li.innerHTML = MovieItem({ img: data.poster_path, rating: roundRating(data.vote_average), title: data.title });
-      docfrag.appendChild(li);
-    })
-    ul.appendChild(docfrag);
-    return ul;
-  }
-  return {template};
+  return `
+    <ul id="thumbnailList" class="thumbnail-list">
+    ${dataList
+      .map((data) => {
+        return `<li>
+        ${MovieItem({
+          img: data.poster_path,
+          rating: roundRating(data.vote_average),
+          title: data.title,
+        })}
+      </li>
+      `
+      })
+      .join(' ')}
+    </ul>
+    `
 }
 
-
-
-export default MovieList;
+export default MovieList
