f1d12317e4dea80f4a47f6f2d83c4b9700d71054
guesung <gueit214@naver.com>
2025-03-27 10:57:17 +0900

    refactor: 유틸 함수 유형에 맞게 분리

---
+++ src/lib/utils/common.ts
@@ -0,0 +1,6 @@
+export const isError = (error: Error | unknown): error is Error => error instanceof Error;
+export const isString = (str: string | unknown): str is string => typeof str === 'string';
+
+export const variant = (object: Record<string, boolean>): string => {
+  return Object.entries(object).reduce((prev, [str, condition]) => (condition ? str + prev : prev), '');
+};
--- src/lib/utils/dom.ts
+++ src/lib/utils/dom.ts
@@ -1,29 +1,20 @@
 import { concat, flat, map, pipe, reduce, zip } from '@fxts/core';
-import type { HTMLType } from './types';
+import type { HTMLType } from '../types';
 
 export const isElement = (target: EventTarget | null): target is Element => target instanceof Element;
 export const isHTMLElement = (target: EventTarget | null): target is HTMLElement => target instanceof HTMLElement;
-
 export const isWindow = (target: Element | Window): target is Window => target instanceof Window;
-
 export const isHTMLFormElement = (target: EventTarget | null): target is HTMLFormElement =>
   target instanceof HTMLFormElement;
 
-export const isError = (error: Error | unknown): error is Error => error instanceof Error;
-
-export const isString = (str: string | unknown): str is string => typeof str === 'string';
-
 export const $ = (selector: string) => document.querySelector(selector) as HTMLElement;
-
-const escape = (str: string) => (Array.isArray(str) ? str.join('') : str);
 
 export function html(strings: TemplateStringsArray, ...values: any[]) {
   return pipe(
     zip(
       strings,
       concat(
-        // TODO: escape
-        map((value) => escape(value), values),
+        map((str) => (Array.isArray(str) ? str.join('') : str), values),
         [''],
       ),
     ),
@@ -31,7 +22,3 @@ export function html(strings: TemplateStringsArray, ...values: any[]) {
     reduce((a, b) => a + b),
   ) as HTMLType;
 }
-
-export const variant = (object: Record<string, boolean>): string => {
-  return Object.entries(object).reduce((prev, [str, condition]) => (condition ? str + prev : prev), '');
-};
