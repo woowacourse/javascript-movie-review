66d2074ba5d6c4e3beba76328c07dab1c8e0ed94
hoyyChoi <hoyeon8165@gmail.com>
2025-03-27 16:54:28 +0900

    feat: detailModal 컴포넌트 내부값 동적 변경

--- src/component/DetailModal.ts
+++ src/component/DetailModal.ts
@@ -1,6 +1,8 @@
+import { MovieItemType } from "../types/movieResultType";
 import createDOMElement from "../util/createDomElement";
+import defaultImage from "../../public/images/default_poster_image.png";
 
-const DetailModal = () => {
+const DetailModal = (movieItem: MovieItemType) => {
   return createDOMElement({
     tag: "dialog",
     className: "detail-modal-container",
@@ -10,13 +12,19 @@ const DetailModal = () => {
         className: "modal",
         children: [
           createDOMElement({
-            tag: "button",
-            className: "close-modal",
-            id: "closeModal",
+            tag: "form",
+            method: "dialog",
             children: [
               createDOMElement({
-                tag: "img",
-                src: "./images/modal_button_close.png",
+                tag: "button",
+                className: "close-modal",
+                id: "closeModal",
+                children: [
+                  createDOMElement({
+                    tag: "img",
+                    src: "./images/modal_button_close.png",
+                  }),
+                ],
               }),
             ],
           }),
@@ -27,11 +35,17 @@ const DetailModal = () => {
               // Left Image
               createDOMElement({
                 tag: "div",
-                className: "modal-image",
+                className: "modal-image loading",
                 children: [
                   createDOMElement({
                     tag: "img",
-                    src: "https://image.tmdb.org/t/p/original//pmemGuhr450DK8GiTT44mgwWCP7.jpg",
+                    src: movieItem.poster_path
+                      ? `https://media.themoviedb.org/t/p/w440_and_h660_face${movieItem.poster_path}`
+                      : defaultImage,
+                    alt: movieItem.title,
+                    onload: function () {
+                      this.parentElement?.classList.remove("loading");
+                    },
                   }),
                 ],
               }),
@@ -48,7 +62,7 @@ const DetailModal = () => {
                       createDOMElement({
                         tag: "h2",
                         className: "movie-title",
-                        textContent: "인사이드 아웃 2",
+                        textContent: movieItem.title,
                       }),
                       createDOMElement({
                         tag: "p",
@@ -76,7 +90,7 @@ const DetailModal = () => {
                               createDOMElement({
                                 tag: "span",
                                 className: "star-description",
-                                textContent: "7.7",
+                                textContent: movieItem.vote_average,
                               }),
                             ],
                           }),
@@ -140,9 +154,7 @@ const DetailModal = () => {
                       createDOMElement({
                         tag: "p",
                         className: "detail",
-                        textContent: `13살이 된 라일리의 행복을 위해 매일 바쁘게 머릿속 감정 컨트롤 본부를 운영하는 ‘기쁨’, ‘슬픔’, ‘버럭’, ‘까칠’, ‘소심’. 
-그러던 어느 날, 낯선 감정인 ‘불안’, ‘당황’, ‘따분’, ‘부럽’이가 본부에 등장하고, 언제나 최악의 상황을 대비하며 제멋대로인 ‘불안’이와 기존 감정들은 계속 충돌한다. 
-결국 새로운 감정들에 의해 본부에서 쫓겨나게 된 기존 감정들은 다시 본부로 돌아가기 위해 위험천만한 모험을 시작하는데…`,
+                        textContent: movieItem.overview ? movieItem.overview : "줄거리가 없습니다.",
                       }),
                     ],
                   }),
