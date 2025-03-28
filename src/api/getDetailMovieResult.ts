52d8de0129bf8185b0c5ebd3784c00d808da32b1
hoyyChoi <hoyeon8165@gmail.com>
2025-03-28 16:47:06 +0900

    fix: getDetailMovieResult 반환타입 지정

--- src/api/getDetailMovieResult.ts
+++ src/api/getDetailMovieResult.ts
@@ -1,6 +1,7 @@
+import { MovieItemType } from "../types/movieResultType";
 import baseApi from "./baseApi";
 
-export const getDetailMovieResult = async (id: number) => {
+export const getDetailMovieResult = async (id: number): Promise<MovieItemType> => {
   const movieResult = await baseApi(`/movie/${id}`);
 
   return movieResult;
