f70b11116217a00e521573c4bc4256b234f713ff
hoyyChoi <hoyeon8165@gmail.com>
2025-03-29 00:10:14 +0900

    feat: spinner 컴포넌트 변경

--- src/component/Spinner.ts
+++ src/component/Spinner.ts
@@ -3,27 +3,31 @@ import createDOMElement from "../util/createDomElement";
 const Spinner = () => {
   return createDOMElement({
     tag: "div",
-    className: "orbit-spinner",
-    children: [
-      createDOMElement({
-        tag: "div",
-        className: "planet",
-      }),
-      createDOMElement({
-        tag: "div",
-        className: "orbit",
-        children: [
-          createDOMElement({
-            tag: "div",
-            className: "satellite satellite-1",
-          }),
-          createDOMElement({
-            tag: "div",
-            className: "satellite satellite-2",
-          }),
-        ],
-      }),
-    ],
+    className: "spinner-wrapper",
+    children: createDOMElement({
+      tag: "div",
+      className: "orbit-spinner",
+      children: [
+        createDOMElement({
+          tag: "div",
+          className: "planet",
+        }),
+        createDOMElement({
+          tag: "div",
+          className: "orbit",
+          children: [
+            createDOMElement({
+              tag: "div",
+              className: "satellite satellite-1",
+            }),
+            createDOMElement({
+              tag: "div",
+              className: "satellite satellite-2",
+            }),
+          ],
+        }),
+      ],
+    }),
   });
 };
 
