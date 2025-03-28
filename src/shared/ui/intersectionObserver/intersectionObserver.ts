ada756ce60482544284936719b5f9c03891d7f2d
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-28 14:19:34 +0900

    refactor: error 발생 시 intersectionOberserver 연결 해제 로직 구현

--- src/shared/ui/intersectionObserver/intersectionObserver.ts
+++ src/shared/ui/intersectionObserver/intersectionObserver.ts
@@ -8,11 +8,16 @@ export const intersectionObserver = (movieList: HTMLElement) => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        try {
-          withSkeleton(movieList, addMoreMovies(movieList));
-        } catch (error) {
-          ErrorModal("영화 리스트를 불러오는데 실패하였습니다.");
-        }
+        const handleFetch = async () => {
+          try {
+            await withSkeleton(movieList, addMoreMovies(movieList));
+          } catch (error) {
+            ErrorModal("영화 리스트를 불러오는데 실패하였습니다.");
+            observer.disconnect();
+          }
+        };
+
+        handleFetch();
       }
     });
   });
