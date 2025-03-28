f78fb094d3b5b3fb52fee6591f3dc786e6c1001c
hoyyChoi <hoyeon8165@gmail.com>
2025-03-29 01:07:38 +0900

    feat: 메인페이지 무한 스크롤 구현

--- src/main/MainController.ts
+++ src/main/MainController.ts
@@ -82,6 +82,7 @@ class MainController {
       this.messageModalController.messageModalElement.showModal();
     }
     this.spinnerElement.classList.remove("active");
+    this.bindObserver();
   }
 
   async fetchAndStoreMovies(page: number = 1) {
@@ -109,6 +110,26 @@ class MainController {
       movieList,
       hasMore: newPage !== totalPage,
     });
+  }
+
+  bindObserver() {
+    const seeMore = this.mainElement.querySelector(".see-more") as Element;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          // 스크롤이 빠르게 내려갈 경우 → 감지 → 데이터 로딩 → 아직도 보이는 중 → 또 감지 (방지하고자, 감지 해제후, 비동기처리가 끝나면 다시 감지)
+          observer.unobserve(seeMore);
+          this.handleSeeMore().finally(() => {
+            observer.observe(seeMore);
+          });
+        }
+      },
+      {
+        root: null,
+        threshold: 0.1,
+      },
+    );
+    observer.observe(seeMore);
   }
 }
 
