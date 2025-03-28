142e2292b08ad884ebd7cc9cf5fb4ebae858b1e3
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-03-28 22:33:54 +0900

    feat: 영화 목록에서 더보기 버튼을 눌렀을 때 페이징하는 방식에서 무한 스크롤 방식으로 변경

--- src/components/MovieContainer.ts
+++ src/components/MovieContainer.ts
@@ -2,6 +2,11 @@ import MovieList from "./MovieList";
 import createElement from "./utils/createElement";
 import Button from "./Button";
 import MovieType from "../types/MovieType";
+import { setupInfiniteScroll } from "./utils/infiniteScroll";
+import renderMovieList from "./renderMovieList";
+import page from "../store/page";
+import fetchSearchMovies from "../fetch/fetchSearchMovies";
+import fetchPopularMovies from "../fetch/fetchPopularMovies";
 
 const BUTTON_MORE = "더보기";
 
@@ -26,7 +31,29 @@ const MovieContainer = ({ movies, status }: MovieContainerProps) => {
   $main.appendChild(Button({ text: BUTTON_MORE, type: "more" }));
   $container.appendChild($main);
 
+  init();
+
   return $container;
 };
 
 export default MovieContainer;
+
+const init = () => {
+  setupInfiniteScroll({
+    onLoad: async () => {
+      const params = new URLSearchParams(window.location.search);
+      const currentPage = page.getNextPage();
+      const res = params.has("query")
+        ? await fetchSearchMovies(params.get("query") || "", currentPage)
+        : await fetchPopularMovies(currentPage);
+      page.setTotalPages(res.totalPages);
+      renderMovieList(() =>
+        Promise.resolve({
+          results: res.results,
+          totalPages: res.totalPages,
+        })
+      );
+    },
+    offset: 150,
+  });
+};
