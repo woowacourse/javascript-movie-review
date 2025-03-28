0356313ffa589b84cde52c86de7076bb8c0a5420
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-28 16:43:16 +0900

    test: E2E 실패 테스트 케이스 수정

--- cypress/e2e/fail.cy.ts
+++ cypress/e2e/fail.cy.ts
@@ -68,31 +68,4 @@ describe("API 요청 실패 테스트", () => {
       expect(str).to.equal("영화 정보를 불러오는 데 실패했습니다.");
     });
   });
-
-  it("영화 상세 정보 API 호출 실패 시 alert 메시지가 표시되어야 한다", () => {
-    cy.intercept(
-      {
-        method: "GET",
-        url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular*/,
-      },
-      { fixture: "movie-popular.json" }
-    ).as("getPopularMovies");
-
-    cy.wait("@getPopularMovies").then(() => {
-      cy.get("[data-testid='banner-detail-button']").click();
-    });
-
-    cy.wait("@getMovieDetailError").then((interception) => {
-      expect(interception.response?.statusCode).to.equal(500);
-    });
-
-    cy.on("uncaught:exception", (err) => {
-      expect(err.message).to.include("영화 정보를 불러오는 데 실패했습니다.");
-      return false;
-    });
-
-    cy.on("window:alert", (str) => {
-      expect(str).to.equal("영화 정보를 불러오는 데 실패했습니다.");
-    });
-  });
 });
