ab46d6bab2afb5afa8f717a892d397c460372a27
guesung <gueit214@naver.com>
2025-03-28 16:36:38 +0900

    feat: 깊은 비교 `isEqual`

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,3 +4,5 @@ export const isString = (str: string | unknown): str is string => typeof str ===
 export const variant = (object: Record<string, boolean>): string => {
   return Object.entries(object).reduce((prev, [str, condition]) => (condition ? str + prev : prev), '');
 };
+
+export const isEqual = (a: unknown, b: unknown): boolean => JSON.stringify(a) === JSON.stringify(b);
