064024450ee2c32c10d2cfa28d6f34e546e92f30
ohgus <anakin1108@naver.com>
2025-03-28 14:04:04 +0900

    feat: movie post 생성할 때 모달 이벤트 추가

--- src/shared/ui/renderers/addMoviePost.ts
+++ src/shared/ui/renderers/addMoviePost.ts
@@ -1,6 +1,8 @@
+import getMovieDetail from "../../../features/movie/api/getMovieDetail";
 import MoviePost from "../../../features/movie/ui/components/MoviePost";
 import { showEmptySearchResult } from "../../../features/search/ui/renderers/showEmptySearchResult";
 import { IMovie } from "../../types/movies";
+import Modal from "../components/Modal";
 
 export function addMoviePost(movieList: IMovie[], $movieList: HTMLElement) {
   if (movieList.length === 0) {
@@ -22,7 +24,12 @@ export function addMoviePost(movieList: IMovie[], $movieList: HTMLElement) {
   const fragment = document.createDocumentFragment();
 
   movieList.forEach((movie: IMovie) => {
-    fragment.appendChild(MoviePost(movie));
+    const moviePost = MoviePost(movie);
+    moviePost.addEventListener("click", async () => {
+      const movieDetail = await getMovieDetail(movie.id);
+      document.body.appendChild(Modal(movieDetail));
+    });
+    fragment.appendChild(moviePost);
   });
 
   $movieList.appendChild(fragment);
