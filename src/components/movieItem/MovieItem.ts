087e3a7b26a73619d899d3e9a7e0312864bd2e16
thgml05 <aisohii@naver.com>
2025-03-28 21:59:34 +0900

    feat : 영화 상세 정보 모달창 열고 닫기 & esc 이벤트 추가

--- src/components/movieItem/MovieItem.ts
+++ src/components/movieItem/MovieItem.ts
@@ -1,16 +1,39 @@
+import useGetMovieDetail from "../../apis/movies/useGetMovieDetail";
 import { images } from "../../assets/images";
-
+import { isModalOpen, setIsModalOpen, setMovieDetail } from "../../store/store";
+import { useEvents } from "../../utils/Core";
 interface MovieItemProps {
+  id: number;
   title: string;
   rate: number;
   src: string;
 }
 
 const MovieItem = (props: MovieItemProps) => {
-  const { title, rate, src } = props;
+  const { id, title, rate, src } = props;
+
+  const { fetchMovieDetail } = useGetMovieDetail();
+
+  const [addEvent] = useEvents(`.thumbnail-list`);
+  addEvent("click", `#_${id}`, async () => {
+    if (!isModalOpen) {
+      setIsModalOpen(true);
+
+      const handleEscapeKey = (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+          setIsModalOpen(false);
+          document.removeEventListener("keydown", handleEscapeKey);
+        }
+      };
+
+      document.addEventListener("keydown", handleEscapeKey);
+    }
+    const detail = await fetchMovieDetail(id);
+    setMovieDetail(detail);
+  });
 
   return `
-              <li>
+              <li id="_${id}">
                 <div class="item">
                   <img
                     class="thumbnail"
