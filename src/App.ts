412fd8dede02be3898ee7f2c16d6e19a829ee08e
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-28 16:33:03 +0900

    refactor: App 컴포넌트 리팩터링

--- src/App.ts
+++ src/App.ts
@@ -6,18 +6,20 @@ import MovieList from "./components/MovieList/index";
 import { fetchPopularMovies, fetchSearchedMovies } from "./APIs/movieAPI";
 import Store from "./store/store";
 import { MOVIE_COUNT } from "./constants/config";
+import { getCurrentPage, isScrolledToBottom, withLoading } from "./utils/utils";
+import { appendHTML } from "./utils/templateUtils";
 
 class App {
-  #$target: HTMLElement;
-  #store: Store;
-  bannerContainer: HTMLElement;
-  mainContainer: HTMLElement;
-  bannerComponent: any;
-  movieListComponent: any;
+  private $target: HTMLElement;
+  private store: Store;
+  private $bannerContainer: HTMLElement;
+  private $mainContainer: HTMLElement;
+  $bannerComponent: Banner;
+  $movieListComponent: MovieList;
 
   constructor($target: HTMLElement) {
-    this.#$target = $target;
-    this.#store = new Store({
+    this.$target = $target;
+    this.store = new Store({
       movies: [],
       query: "",
       searchedMoviesLength: 0,
@@ -27,74 +29,72 @@ class App {
         : [],
     });
 
-    const $headerTemplate = document.createElement("template");
-    $headerTemplate.innerHTML = Header(this.#store);
-    this.#$target.appendChild($headerTemplate.content);
+    appendHTML(this.$target, Header(this.store));
 
-    this.bannerContainer = document.createElement("section");
-    this.bannerContainer.id = "banner-container";
-    this.#$target.appendChild(this.bannerContainer);
+    this.$bannerContainer = document.createElement("section");
+    this.$bannerContainer.id = "banner-container";
+    this.$target.appendChild(this.$bannerContainer);
 
-    this.mainContainer = document.createElement("div");
-    this.mainContainer.classList.add("container");
-    this.#$target.appendChild(this.mainContainer);
+    this.$mainContainer = document.createElement("section");
+    this.$mainContainer.classList.add("container");
+    this.$target.appendChild(this.$mainContainer);
 
-    const $footerTemplate = document.createElement("template");
-    $footerTemplate.innerHTML = Footer();
-    this.#$target.appendChild($footerTemplate.content);
+    appendHTML(this.$target, Footer());
 
-    this.bannerComponent = new Banner(this.bannerContainer, this.#store);
-    this.movieListComponent = new MovieList(this.mainContainer, this.#store);
+    this.$bannerComponent = new Banner(this.$bannerContainer, this.store);
+    this.$movieListComponent = new MovieList(this.$mainContainer, this.store);
 
-    if (this.#store.getState().movies.length === 0) {
-      this.#loadPopularMovies();
+    if (this.store.getState().movies.length === 0) {
+      this.loadPopularMovies();
     }
 
+    this.attachScrollEvent(this.store);
+  }
+
+  private async loadPopularMovies(): Promise<void> {
+    this.store.setState({ loading: true });
+    const movies = await fetchPopularMovies((error: Error) =>
+      alert(error.message)
+    );
+    this.store.setState({ movies, loading: false });
+  }
+
+  private attachScrollEvent(store: Store): void {
     window.addEventListener("scroll", async () => {
-      if (
-        window.innerHeight + window.scrollY >=
-        document.documentElement.scrollHeight - 180
-      ) {
-        const state = this.#store.getState();
-        const currentPage =
-          Math.floor(state.movies.length / MOVIE_COUNT.UNIT) + 1;
-
-        this.#store.setState({ loading: true });
-
+      if (isScrolledToBottom()) {
+        const state = store.getState();
+        const currentPage = getCurrentPage(
+          state.movies.length,
+          MOVIE_COUNT.UNIT
+        );
         if (
           !state.query &&
           state.movies.length < MOVIE_COUNT.MAX_PAGE * MOVIE_COUNT.UNIT
         ) {
-          const newMovies = await fetchPopularMovies(
-            (error: Error) => alert(error.message),
-            currentPage
+          const newMovies = await withLoading(store, () =>
+            fetchPopularMovies(
+              (error: Error) => alert(error.message),
+              currentPage
+            )
           );
-          this.#store.setState({ movies: [...state.movies, ...newMovies] });
-          return;
+          store.setState({ movies: [...state.movies, ...newMovies] });
+        } else if (
+          state.query &&
+          state.movies.length < state.searchedMoviesLength
+        ) {
+          const newMoviesData = await withLoading(store, () =>
+            fetchSearchedMovies(
+              state.query,
+              (error: Error) => alert(error.message),
+              currentPage
+            )
+          );
+          store.setState({
+            movies: [...state.movies, ...newMoviesData.results],
+          });
         }
-
-        if (state.movies.length >= state.searchedMoviesLength) return;
-
-        const newMoviesData = await fetchSearchedMovies(
-          state.query,
-          (error: Error) => alert(error.message),
-          currentPage
-        );
-
-        this.#store.setState({
-          movies: [...state.movies, ...newMoviesData.results],
-          loading: false,
-        });
       }
     });
-  }
-
-  async #loadPopularMovies(): Promise<void> {
-    this.#store.setState({ loading: true });
-    const movies = await fetchPopularMovies((error: Error) =>
-      alert(error.message)
-    );
-    this.#store.setState({ movies, loading: false });
   }
 }
 
