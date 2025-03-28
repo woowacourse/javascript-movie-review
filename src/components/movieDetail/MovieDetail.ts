eb1a0f4eafd90ab264617dbc91af41d8fd8b6f37
thgml05 <aisohii@naver.com>
2025-03-29 00:07:43 +0900

    feat : 별점 이벤트 등록 추가

--- src/components/movieDetail/MovieDetail.ts
+++ src/components/movieDetail/MovieDetail.ts
@@ -1,11 +1,40 @@
 import { movieDetail } from "../../store/store";
 import { useEvents } from "../../utils/Core";
-import { $ } from "../../utils/domHelper";
+import { $, $$ } from "../../utils/domHelper";
+
+const VOTE_TEXT = [
+  "최악이에요(2/10)",
+  "별로예요(4/10)",
+  "보통이예요(6/10)",
+  "재밌었어요(8/10)",
+  "명작이예요(10/10)",
+];
 
 const MovieDetail = () => {
   const [addEvent] = useEvents(".modal");
+
   addEvent("click", ".close-modal", () => {
     $(".modal-background").classList.remove("active");
+  });
+
+  addEvent("mouseover", ".my-vote-star", (event) => {
+    const index = parseInt(event.target.dataset.index);
+
+    const starElements = $$(".my-vote-star");
+    const starTextElement = $(".my-vote-text");
+
+    starElements.forEach((element, i) => {
+      if (i <= index) {
+        element.src = "./star_filled.png";
+        starTextElement.textContent = VOTE_TEXT[i];
+      } else element.src = "./star_empty.png";
+    });
+  });
+
+  addEvent("mouseout", ".my-vote-star", (event) => {
+    const starElements = $$(".my-vote-star");
+    starElements.forEach((element) => (element.src = "./star_empty.png"));
+    $(".my-vote-text").textContent = "";
   });
 
   if (!movieDetail) return;
@@ -35,6 +64,17 @@ const MovieDetail = () => {
               >
             </p>
             <hr />
+            <h4>내 별점</h4>
+            <div class="my-vote-container">
+              ${Array.from(
+                { length: 5 },
+                (_, index) =>
+                  `<img src="./star_empty.png" class="star my-vote-star" data-index="${index}" />`
+              ).join("")}
+              <span class="my-vote-text"></span>
+            </div>
+            <hr />
+            <h4>줄거리</h4>
             <p class="detail">
               ${movieDetail.overview}
             </p>
