2a48187d923647b2607da6cc035cda98a5250697
spoyodevelop <kjy9505@gmail.com>
2025-03-28 14:13:19 +0900

    chores: 주석 제거

--- src/service/loaderService.ts
+++ src/service/loaderService.ts
@@ -14,7 +14,6 @@ export default function createMovieLoader(
   let page = 1;
   let errorOccurred = false;
 
-  // 별도의 에러 핸들러를 정의하여 에러 발생 시 플래그를 설정하고 onError를 호출합니다.
   const handleError = (error: Error) => {
     errorOccurred = true;
     onError(error);
@@ -26,7 +25,6 @@ export default function createMovieLoader(
     const response = await fetchMovies(url, queryObject, options, handleError);
 
     if (errorOccurred) {
-      // 에러가 발생한 경우, 플래그를 초기화한 후 페이지 번호를 증가시키지 않고 그대로 반환합니다.
       errorOccurred = false;
       return { results: response.results, isLastPage: true };
     }
