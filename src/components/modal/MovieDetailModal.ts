1f7663cef6eb8a957ff1af637736290499aab9bd
장정안 <wjddks96@naver.com>
2025-03-28 17:12:08 +0900

    feat: MovieDetailModal 내에 StarRating 컴포넌트 삽입

--- src/components/modal/MovieDetailModal.ts
+++ src/components/modal/MovieDetailModal.ts
@@ -1,4 +1,6 @@
+import StarRating from "../starRating/StarRating";
 import Modal from "../@common/Modal";
+import { images } from "../../assets/images";
 
 interface MovieDetailModalProps {
   title: string;
@@ -7,15 +9,16 @@ interface MovieDetailModalProps {
   description: string;
   genres: string;
   releaseDate: string;
+  id: number;
 }
 
 const MovieDetailModal = (props: MovieDetailModalProps) => {
-  const { title, rate, src, description, genres, releaseDate } = props;
+  const { title, rate, src, description, genres, releaseDate, id } = props;
 
   return `
   ${Modal({
     children: `
-    <button class="close-modal" id="closeModal">
+        <button class="close-modal" id="closeModal">
           <img src="./modal_button_close.png" />
         </button>
         <div class="modal-container">
@@ -30,9 +33,17 @@ const MovieDetailModal = (props: MovieDetailModalProps) => {
               ${releaseDate} · ${genres}
             </p>
             <p class="rate">
-              <img src="./images/star_filled.png" class="star" /><span
+              <img src="${images.starFilled}" class="star-filled" /><span
                 >${rate}</span>
             </p>
+            <hr />
+            <div class="user-rating">
+              <h3>내 별점</h3>
+              ${StarRating({
+                movieId: id,
+                containerClass: `rating-container-${id}`,
+              })}
+            </div>
             <hr />
             <p class="detail">
               ${description}
