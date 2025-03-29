bfa59f97d457f2459358e4eff939594a8f6b7a9a
jin123457 <jin_ox@naver.com>
2025-03-30 05:20:09 +0900

    feat: 배너 스켈레톤 추가 및 관련 렌더링 로직 수정

--- src/domain/initMovies.ts
+++ src/domain/initMovies.ts
@@ -1,7 +1,9 @@
 import getPopularMovies from '../api/getPopularMovies';
+import { bannerSkeletons } from '../view/render/skeleton/bannerSkeleton';
 import { movieListSkeletons } from '../view/render/skeleton/movieListSkeletons';
 
 export const initMovies = async () => {
+  bannerSkeletons();
   movieListSkeletons();
 
   const params = {
--- src/view/render/renderBanner.ts
+++ src/view/render/renderBanner.ts
@@ -1,18 +1,14 @@
 import Banner from '../Banner';
-import Skeleton from '../../component/Skeleton';
 import { MoveType } from '../../type';
 import { $ } from '../../util/selector';
 
 export const renderBanner = async (movies: MoveType[]) => {
   const wrap = $('#wrap');
 
-  const bannerSkeleton = Skeleton({ height: 500 });
-  wrap?.prepend(bannerSkeleton);
-
   const hasBackdropMovies = movies.filter((movie) => movie.backdropPath !== null);
   const bannerMovie = hasBackdropMovies.length ? hasBackdropMovies[0] : movies[0];
 
-  bannerSkeleton?.replaceWith(Banner({ movie: bannerMovie }));
+  wrap?.prepend(Banner({ movie: bannerMovie }));
 };
 
 export const removeBanner = () => {
