f9eeba2afa8d71bdafca65c6f106ed76e818af37
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-03-29 22:14:51 +0900

    chore: 불필요한 import 제거

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,9 +1,5 @@
-import fetchPopularMovies from "../fetch/fetchPopularMovies";
-import fetchSearchMovies from "../fetch/fetchSearchMovies";
-import page from "../store/page";
 import createElement from "./utils/createElement";
 import { ButtonType } from "../types/ButtonType";
-import renderMovieList from "./renderMovieList";
 
 interface ButtonProps {
   text: string;
