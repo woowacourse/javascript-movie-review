2469e17b9f6239c05d134400786acd81c9268560
guesung <gueit214@naver.com>
2025-03-29 23:03:02 +0900

    feat: 만약 데이터 없다면 초기값 저장

--- src/decorators/persisted.ts
+++ src/decorators/persisted.ts
@@ -2,6 +2,9 @@ import { LocalStorage, LocalStorageKeyType } from '@/modules';
 import { Store } from '@/store';
 
 export const persisted = (key: LocalStorageKeyType, store: Store<any>) => {
+  const localData = LocalStorage.get(key);
+  if (!localData) LocalStorage.set(key, store.getState());
+
   store.setState(LocalStorage.get(key));
 
   store.subscribe((state) => LocalStorage.set(key, state));
