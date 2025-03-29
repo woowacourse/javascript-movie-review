ec645e08bc139affb79add7fbb5b3e9b07236ad9
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-29 16:42:31 +0900

    style: header UI 변경 및 header scroll 시 백그라운드 블러 처리

--- src/controller/HeaderController.ts
+++ src/controller/HeaderController.ts
@@ -14,25 +14,36 @@ class HeaderController {
     this.searchBarElement = $<HTMLFormElement>(".search-bar")!;
     this.headerLogoElement = $<HTMLDivElement>(".header-wrapper .logo")!;
 
+    this.bindScrollEvent();
     this.bindSearchEvent(onSearchKeywordSubmit);
     this.bindHomeLogoEvent(onHomeLogoClick);
   }
 
+  bindScrollEvent() {
+    const header = $("header")!;
+    const SCROLL_THRESHOLD = 300; // 몇 픽셀 내려갔을 때 배경 줄지 설정
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > SCROLL_THRESHOLD) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
+  }
+
   bindSearchEvent(onSearchKeywordSubmit: (searchValue: string) => void) {
-    this.searchBarElement?.addEventListener(
-      "submit",
-      async (event: SubmitEvent) => {
-        event.preventDefault();
+    this.searchBarElement?.addEventListener("submit", async (event: SubmitEvent) => {
+      event.preventDefault();
 
-        const formElement = event.target as HTMLElement;
-        const target = $<HTMLInputElement>("input", formElement);
-        const searchValue = target?.value;
+      const formElement = event.target as HTMLElement;
+      const target = $<HTMLInputElement>("input", formElement);
+      const searchValue = target?.value;
 
-        if (searchValue) {
-          onSearchKeywordSubmit(searchValue);
-        }
-      },
-    );
+      if (searchValue) {
+        onSearchKeywordSubmit(searchValue);
+      }
+    });
   }
 
   bindHomeLogoEvent(onHomeLogoClick: () => void) {
