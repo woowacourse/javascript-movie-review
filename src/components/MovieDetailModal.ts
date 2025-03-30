6c48bdc21ef0945e55e19623772e4e7aef72a06d
guesung <gueit214@naver.com>
2025-03-30 15:31:05 +0900

    fix: 스크롤 disabled 이슈

--- src/components/MovieDetailModal.ts
+++ src/components/MovieDetailModal.ts
@@ -143,6 +143,7 @@ export default class MovieDetailModal extends Modal {
       eventType: 'popstate',
       callbackWindow: () => {
         movieDetailResponseStore.reset();
+        this.enableScrollOutside();
       },
     });
   }
