1a3eea88cdcbd81075f6a000dc03fb2ca794a5c6
SuHwa Lee <tnghk9611@naver.com>
2025-03-30 22:04:21 +0900

    feat: 상세 정보에 필요한 타입 추가

--- src/types/Movie.types.ts
+++ src/types/Movie.types.ts
@@ -21,3 +21,33 @@ export type MovieItem = {
   vote_average: number;
   vote_count: number;
 };
+
+export type Genre = {
+  id: number;
+  name: string;
+};
+
+export type MovieDetail = {
+  adult: boolean;
+  backdrop_path: string | null;
+  budget: number;
+  genres: Genre[];
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  revenue: number;
+  runtime: number | null;
+  status: string;
+  tagline: string | null;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
