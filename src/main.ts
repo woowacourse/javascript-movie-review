48d37a0a0f0733fd74aa30902af726e3f63da552
dev-dino22 <wlsskdud02@naver.com>
2025-03-31 00:37:02 +0900

    fix: git Pages 오류로인해 main.ts 비동기 함수로 변경 및 코드 정리

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import MovieLayout from './component/feature/MovieLayout'
 import submitEvent from './component-event/submitEvent.js'
 import { fetchPopularMovies } from './api/fetch.js'
 import Banner from './component/common/Banner.js'
+;(async () => {
+  history.scrollRestoration = 'manual'
+  const movieData = await fetchPopularMovies(1)
+  const movieLayout = MovieLayout(movieData.results)
 
-history.scrollRestoration = 'manual'
-submitEvent
-//MovieLayout.skeletonRender()
-const movieData = await fetchPopularMovies(1)
-const movieLayout = MovieLayout(movieData.results)
-const bannerElement = document.getElementById('bannerSection')
-if (bannerElement) bannerElement.innerHTML = Banner(movieData.results[0])
-await submitEvent(movieLayout)
-Header()
+  const bannerElement = document.getElementById('bannerSection')
+  if (bannerElement) bannerElement.innerHTML = Banner(movieData.results[0])
+
+  await submitEvent(movieLayout)
+  Header()
+})()
