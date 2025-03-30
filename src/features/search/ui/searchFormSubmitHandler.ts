e3b6a053bff72879aa3a319345b2e82d18b307be
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-30 23:00:48 +0900

    refactor: 비동기 함수 await 추가

--- src/features/search/ui/searchFormSubmitHandler.ts
+++ src/features/search/ui/searchFormSubmitHandler.ts
@@ -16,7 +16,7 @@ export const searchFormSubmitHandler = async (e: Event) => {
 
   const params = getUrlParams();
   updateUrlParams(params, String(searchQuery));
-  updateSearchedMovieUI($thumbnailList, String(searchQuery));
+  await updateSearchedMovieUI($thumbnailList, String(searchQuery));
   updateUrl(params);
 };
 
