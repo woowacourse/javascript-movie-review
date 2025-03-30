913587c02ff26527d5c783e13825d1686394c90f
Jeongeun Lee <leeju1009@gmail.com>
2025-03-30 19:08:58 +0900

    fix: 페이지가 잘못 계산되는 문제 수정

--- src/domain/calculatePageNumber.ts
+++ src/domain/calculatePageNumber.ts
@@ -1,7 +1,7 @@
 import { ITEMS } from "../constants/movie";
 
 const calculatePageNumber = (totalMovies: number) => {
-  return totalMovies / ITEMS.perPage + 1;
+  return Math.ceil(totalMovies / ITEMS.perPage) + 1;
 };
 
 export default calculatePageNumber;
