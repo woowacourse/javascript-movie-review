45a16bc782fb2d127cfa1b5d8c41fb702b98d8e4
aydenote <dslgpgh@gmail.com>
2025-03-30 23:59:46 +0900

    feat: movieModal 컨트롤러 생성

---
+++ src/controllers/MovieModal.js
@@ -0,0 +1,14 @@
+import MovieDetailModal from "../components/MovieDetailModal";
+import registerModalEventHandlers from "../controllers/MovieDetailModalHandlers";
+
+export const ratingDescriptions = {
+  2: "최악이예요",
+  4: "별로예요",
+  6: "보통이에요",
+  8: "재미있어요",
+  10: "명작이에요",
+};
+
+function MovieModal() {}
+
+export default MovieModal;
