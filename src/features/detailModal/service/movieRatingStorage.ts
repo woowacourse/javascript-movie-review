959b9acfedcf8534aeefe27f036e61078bdda42d
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-31 23:10:48 +0900

    chore: 주석 제거

--- src/features/detailModal/service/movieRatingStorage.ts
+++ src/features/detailModal/service/movieRatingStorage.ts
@@ -1,5 +1,4 @@
 export const movieRatingStorage = (id: number, rating?: number) => {
-  console.log("alkfjslf", id, rating);
   const storedRatings = JSON.parse(
     localStorage.getItem("my-movie-rating") || "[]"
   );
@@ -8,16 +7,12 @@ export const movieRatingStorage = (id: number, rating?: number) => {
     return storedRatings.find((item: any) => item.id === id)?.rating || 0;
   }
 
-  console.log("???");
-
   const index = storedRatings.findIndex((item: any) => item.id === id);
   if (index !== -1) {
     storedRatings[index].rating = rating;
   } else {
     storedRatings.push({ id, rating });
   }
-
-  console.log("으음", storedRatings);
 
   localStorage.setItem("my-movie-rating", JSON.stringify(storedRatings));
 };
