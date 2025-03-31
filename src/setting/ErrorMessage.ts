04218ad1df15f0a76226099a41d878fdea868267
spoyodevelop <kjy9505@gmail.com>
2025-04-01 02:18:16 +0900

    fix: fallback error 메세지 수정

--- src/setting/ErrorMessage.ts
+++ src/setting/ErrorMessage.ts
@@ -4,7 +4,7 @@ export const ERROR_MESSAGE = {
   SERVER_ERROR: "서버에서 오류가 발생했습니다. 관리자에게 문의하세요.",
   NETWORK_DISCONNECTED: "인터넷 연결이 끊어졌습니다. 연결을 확인해 주세요.",
   FALLBACK_ERROR:
-    "뭔가 잘못되었어요. 인터넷 상태를 체크하신뒤 새로 고침을 해주세요!",
+    "통신 상황이 좋지 않으니, 잠시후 새로고침하고 다시 시도해주세요.",
   RETRY_ERROR:
     "최대 대기 시간(1분)을 초과했습니다. 인터넷 상태를 체크하신뒤에 새로 고침을 해주세요.",
 } as const;
