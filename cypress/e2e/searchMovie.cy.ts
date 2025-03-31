4d29f02b46f45ff4c3c6382bd512d4e583b53539
dlsxjzld <sinjuk1@naver.com>
2025-03-31 12:06:00 +0900

    refactor: 인기 영화 및 검색 영화 테스트에서 '더보기' 버튼 클릭 대신 스크롤로 변경

--- cypress/e2e/searchMovie.cy.ts
+++ cypress/e2e/searchMovie.cy.ts
@@ -5,7 +5,7 @@ describe("영화 검색 비동기 API 테스트", () => {
     cy.visit("localhost:5173");
   });
 
-  it("미키 17 검색하면 1개만 나오고 더보기 버튼 안보여야 한다", () => {
+  it("미키 17 검색하면 1개만 나오고 '더보기' 버튼이 존재하지 않아야 한다", () => {
     cy.get("#search-bar").type("미키 17");
 
     cy.get(".search-icon").click();
@@ -13,7 +13,7 @@ describe("영화 검색 비동기 API 테스트", () => {
     cy.get(".see-more").should("not.exist");
   });
 
-  it("짱구 검색하면 20개 나오고 '더보기' 버튼이 보여야 한다.", () => {
+  it("짱구 검색하면 20개 나오고 '더보기' 버튼이 존재해야 한다.", () => {
     cy.get("#search-bar").type("짱구");
     cy.get(".search-icon").click();
 
@@ -21,10 +21,10 @@ describe("영화 검색 비동기 API 테스트", () => {
     cy.get(".see-more").should("exist");
   });
 
-  it("짱구를 검색한 후 '더보기' 버튼을 클릭하면 영화가 추가로 표시되며, 마지막 페이지(2)에 도달하면 '더보기' 버튼이 사라진다.", () => {
+  it("짱구를 검색한 후 페이지 하단으로 스크롤하면 영화가 추가로 표시되고 '더보기' 버튼이 존재하지 않아야 한다", () => {
     cy.get("#search-bar").type("짱구");
     cy.get(".search-icon").click();
-    cy.get(".see-more").click();
+    cy.scrollTo("bottom");
 
     cy.get(".thumbnail-list")
       .find(".thumbnail")
