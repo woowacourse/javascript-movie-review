9ef3bb68a89f13c4e40d443442fa5beccabed34a
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-31 12:21:54 +0900

    refactor: DetailModalController 리팩토링

--- src/controller/DetailModalController.ts
+++ src/controller/DetailModalController.ts
@@ -1,89 +1,37 @@
-import DetailLoadingModal from "../component/detailModal/DetailLoadingModal";
-import DetailModal from "../component/detailModal/DetailModal";
-import { SCORE_RATING_TEXT } from "../constant/scoreRatingText";
 import MovieDetailModel from "../domain/MovieDetailModel";
-import { calculateScore } from "../domain/util/calculateScore";
-import { $, $all } from "../util/selector";
+import DetailModalView from "../view/DetailModalView";
 
 class DetailModalController {
-  movieDetailModel;
-  wrapElement;
-  detailModalElement: HTMLElement | null = null;
+  #model;
+  #view;
 
-  onErrorModalOpen;
+  #onErrorModalOpen;
 
   constructor({ onErrorModalOpen }: { onErrorModalOpen: (error: Error) => void }) {
-    this.movieDetailModel = MovieDetailModel();
-    this.wrapElement = $("#wrap");
+    this.#model = MovieDetailModel();
+    this.#view = new DetailModalView();
 
-    this.onErrorModalOpen = onErrorModalOpen;
-  }
-
-  bindEvents(movieId: number) {
-    if (this.detailModalElement) {
-      this.detailModalElement.addEventListener("click", (e) => {
-        if (e.target === e.currentTarget) this.closeModal();
-      });
-
-      const closeButton = $("#closeModal", this.detailModalElement);
-      closeButton?.addEventListener("click", () => this.closeModal());
-
-      window.addEventListener("keydown", (e: KeyboardEvent) => {
-        if (e.key === "Escape") {
-          this.closeModal();
-        }
-      });
-
-      const starButtons = $all(".modal-star-button", this.detailModalElement);
-      const ratingText = $(".modal-star-text", this.detailModalElement);
-
-      starButtons.forEach((button, index) => {
-        button.addEventListener("click", () => {
-          const score = calculateScore(index);
-          this.movieDetailModel.updateStarScore(movieId, score);
-
-          // 별 이미지 업데이트
-          starButtons.forEach((btn, i) => {
-            const img = btn as HTMLImageElement;
-            img.src = i <= index ? "./images/star_filled.png" : "./images/star_empty.png";
-          });
-
-          // 텍스트 업데이트
-          if (ratingText) {
-            const text = SCORE_RATING_TEXT[score];
-
-            ratingText.textContent = text;
-            const span = document.createElement("span");
-            span.className = "modal-star-score";
-            span.textContent = ` (${score}/10)`;
-            ratingText.appendChild(span);
-          }
-        });
-      });
-    }
+    this.#onErrorModalOpen = onErrorModalOpen;
   }
 
   async showModal(movieId: number) {
     try {
-      this.detailModalElement = DetailLoadingModal();
-      this.wrapElement?.insertAdjacentElement("afterend", this.detailModalElement);
+      this.#view.renderLoading();
 
-      const movieDetail = await this.movieDetailModel.getMovieDetailById(movieId);
+      const movieDetail = await this.#model.getMovieDetailById(movieId);
+      console.log(movieDetail);
 
-      const detailModal = DetailModal(movieDetail, () => this.#handleImageLoad(detailModal, movieId));
+      this.#view.renderDetailModalWhenReady(movieDetail, () => {
+        this.#view.bindCloseEvents(() => this.closeModal());
+        this.#view.bindStarEvents(movieId, this.#model.updateStarScore);
+      });
     } catch (error) {
-      this.onErrorModalOpen(error as Error);
+      this.#onErrorModalOpen(error as Error);
     }
   }
 
   closeModal() {
-    if (this.detailModalElement) this.detailModalElement.remove();
-  }
-
-  #handleImageLoad(detailModal: HTMLElement, movieId: number) {
-    this.detailModalElement?.replaceWith(detailModal);
-    this.detailModalElement = detailModal;
-    this.bindEvents(movieId);
+    this.#view.remove();
   }
 }
 export default DetailModalController;
