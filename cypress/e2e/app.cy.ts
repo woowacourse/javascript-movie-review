b8a281066e3cecd34097f8c6d54c6ddea440a8ec
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-31 12:30:07 +0900

    test: 기존 테스트코드 무한스크롤로 코드 수정

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,8 +3,10 @@ describe("전체 플로우 테스트", () => {
     cy.visit("localhost:5173");
   });
 
-  it("더 보기 버튼을 누른 후 검색을 진행하고 다시 홈 화면으로 돌아왔을 때, 영화 목록이 유지된다.", () => {
-    cy.get(".see-more").click();
+  it("스크롤을 하단까지 내린 후 검색을 진행하고 다시 홈 화면으로 돌아왔을 때, 영화 목록이 유지된다.", () => {
+    cy.get(".thumbnail-list > li").should("have.length", 20);
+
+    cy.scrollTo("bottom");
     cy.wait(500);
 
     cy.get(".search-bar input").type("짱구");
