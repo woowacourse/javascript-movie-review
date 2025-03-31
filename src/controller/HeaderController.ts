a070a749817a6436d88b3584fe911c2ea96c96b9
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-31 11:40:31 +0900

    refactor: HeaderController 리팩토링

--- src/controller/HeaderController.ts
+++ src/controller/HeaderController.ts
@@ -1,8 +1,9 @@
-import { $ } from "../util/selector";
+import HeaderScrollManager from "../lib/scroll/headerScrollManager";
+import HeaderView from "../view/HeaderView";
 
 class HeaderController {
-  searchBarElement;
-  headerLogoElement;
+  #view;
+  #scrollManager;
 
   constructor({
     onSearchKeywordSubmit,
@@ -11,50 +12,12 @@ class HeaderController {
     onSearchKeywordSubmit: (searchValue: string) => void;
     onHomeLogoClick: () => void;
   }) {
-    this.searchBarElement = $<HTMLFormElement>(".search-bar")!;
-    this.headerLogoElement = $<HTMLDivElement>(".header-wrapper .logo")!;
+    this.#view = new HeaderView();
+    this.#scrollManager = new HeaderScrollManager();
 
-    this.bindScrollEvent();
-    this.bindSearchEvent(onSearchKeywordSubmit);
-    this.bindHomeLogoEvent(onHomeLogoClick);
-  }
-
-  bindScrollEvent() {
-    const header = $("header")!;
-    const SCROLL_THRESHOLD = 300; // 몇 픽셀 내려갔을 때 배경 줄지 설정
-
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > SCROLL_THRESHOLD) {
-        header.classList.add("scrolled");
-      } else {
-        header.classList.remove("scrolled");
-      }
-    });
-  }
-
-  bindSearchEvent(onSearchKeywordSubmit: (searchValue: string) => void) {
-    this.searchBarElement?.addEventListener("submit", async (event: SubmitEvent) => {
-      event.preventDefault();
-
-      const formElement = event.target as HTMLElement;
-      const target = $<HTMLInputElement>("input", formElement);
-      const searchValue = target?.value;
-
-      if (searchValue) {
-        onSearchKeywordSubmit(searchValue);
-      }
-    });
-  }
-
-  bindHomeLogoEvent(onHomeLogoClick: () => void) {
-    this.headerLogoElement?.addEventListener("click", () => {
-      onHomeLogoClick();
-
-      const inputElement = $<HTMLInputElement>("input", this.searchBarElement);
-      if (inputElement) {
-        inputElement.value = "";
-      }
-    });
+    this.#view.bindSearchEvent(onSearchKeywordSubmit);
+    this.#view.bindHomeLogoClick(onHomeLogoClick);
+    this.#scrollManager.bind();
   }
 }
 
---
+++ src/view/HeaderView.ts
@@ -0,0 +1,34 @@
+import { $ } from "../util/selector";
+
+export default class HeaderView {
+  #searchBarElement: HTMLFormElement;
+  #headerLogoElement: HTMLDivElement;
+
+  constructor() {
+    this.#searchBarElement = $<HTMLFormElement>(".search-bar")!;
+    this.#headerLogoElement = $<HTMLDivElement>(".header-wrapper .logo")!;
+  }
+
+  bindSearchEvent(onSearch: (searchValue: string) => void) {
+    this.#searchBarElement.addEventListener("submit", (event: SubmitEvent) => {
+      event.preventDefault();
+
+      const formElement = event.target as HTMLElement;
+      const input = $<HTMLInputElement>("input", formElement);
+      const searchValue = input?.value;
+
+      if (searchValue) {
+        onSearch(searchValue);
+      }
+    });
+  }
+
+  bindHomeLogoClick(onClick: () => void) {
+    this.#headerLogoElement.addEventListener("click", () => {
+      onClick();
+
+      const input = $<HTMLInputElement>("input", this.#searchBarElement);
+      if (input) input.value = "";
+    });
+  }
+}
