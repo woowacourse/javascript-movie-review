5b3bd5fcbd33e05c3049088e7172a1fd89e31835
yeongipark <yeongi0111@naver.com>
2025-04-01 14:07:22 +0900

    refactor: fallback 텍스트 값 받는 걸로 변경
    - showFallback도 함수로 분리

--- src/components/fallback/fallback.js
+++ src/components/fallback/fallback.js
@@ -1,12 +1,12 @@
 import { createElement } from "../../util/dom";
-export default function Fallback() {
+export default function Fallback(text) {
   const $div = createElement("div", {
     className: "fallback-div",
     id: "fallback-div",
   });
   $div.innerHTML = `
       <img src="./images/fallback_no_movies.png" alt="머리 아픈 행성이" />
-      <h1 class="title">검색 결과가 없습니다.</h1>
+      <h1 class="title">${text}</h1>
   `;
 
   return $div;
