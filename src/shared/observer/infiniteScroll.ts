918ed7b1d1e13bdd121f2260c130e47f3c45a72a
ohgus <anakin1108@naver.com>
2025-04-01 17:49:46 +0900

    refactor: 무한 스크롤 로직 분리
    타겟 업데이트, 감지, 감지 후 동작, 에러

--- src/shared/observer/infiniteScroll.ts
+++ src/shared/observer/infiniteScroll.ts
@@ -2,6 +2,11 @@ import { addMoreMovies } from "../domain/addMoreMovies";
 import { pageManager } from "../domain/pageManager";
 import { showErrorPage } from "../ui/renderers/showErrorPage";
 
+interface InfiniteScrollOptions {
+  onIntersect: () => Promise<boolean>;
+  onError?: () => void;
+}
+
 const options = {
   root: null,
   rootMargin: "0px 0px 0px 0px",
@@ -10,39 +15,41 @@ const options = {
 
 let isLoading = false;
 
-const onIntersect = async (
-  entries: IntersectionObserverEntry[],
-  observer: IntersectionObserver
-) => {
-  const entry = entries[0];
+const createIntersectionObserver = ({
+  onIntersect,
+  onError,
+}: InfiniteScrollOptions) => {
+  const onIntersectHandler = async (
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) => {
+    const entry = entries[0];
 
-  if (entry.isIntersecting && !isLoading) {
-    if (pageManager.isLastPage()) {
+    if (entry.isIntersecting && !isLoading) {
+      if (pageManager.isLastPage()) {
+        observer.unobserve(entry.target);
+        return;
+      }
+
+      isLoading = true;
+
+      const success = await onIntersect();
+
+      if (!success) {
+        onError?.();
+        observer.unobserve(entry.target);
+        isLoading = false;
+        return;
+      }
+
       observer.unobserve(entry.target);
-      return;
-    }
-
-    const $movieList = document.querySelector(".thumbnail-list") as HTMLElement;
-    if (!$movieList) return;
-
-    isLoading = true;
-
-    const result = await addMoreMovies($movieList);
-
-    if (!result.success) {
-      showErrorPage();
-      observer.unobserve(entry.target);
+      updateObserverTarget(observer);
       isLoading = false;
-      return;
     }
+  };
 
-    observer.unobserve(entry.target);
-    updateObserverTarget(observer);
-    isLoading = false;
-  }
+  return new IntersectionObserver(onIntersectHandler, options);
 };
-
-const observer = new IntersectionObserver(onIntersect, options);
 
 function updateObserverTarget(observer: IntersectionObserver) {
   observer.disconnect();
@@ -60,5 +67,18 @@ export function initInfiniteScroll() {
 
   if (pageManager.isLastPage()) return;
 
+  const observer = createIntersectionObserver({
+    onIntersect: handleIntersect,
+    onError: showErrorPage,
+  });
+
   updateObserverTarget(observer);
+}
+
+async function handleIntersect() {
+  const $movieList = document.querySelector(".thumbnail-list") as HTMLElement;
+  if (!$movieList) return false;
+
+  const result = await addMoreMovies($movieList);
+  return result.success;
 }
