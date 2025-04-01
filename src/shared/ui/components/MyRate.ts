b44b43c8b2a1253b16c0d43e7e3b689c7ad012f5
ohgus <anakin1108@naver.com>
2025-04-01 15:18:14 +0900

    refactor: 별점 중복 로직 간소화

--- src/shared/ui/components/MyRate.ts
+++ src/shared/ui/components/MyRate.ts
@@ -1,43 +1,25 @@
 import { getRateScore, getRateText } from "../../utils/getRateText";
 import { localStorageHandler } from "../../stores/localStorage";
 
+const RATE_STARS = [2, 4, 6, 8, 10];
+
 const MyRate = (id: string) => {
   let userRate = Number(localStorageHandler.get(id)) ?? 0;
 
   return /*html*/ `
   <h3 class="my-rate-title">내 별점</h3>
   <div class="rate-display">
-    <span class="rate-stars">
-      <input type="radio" name="rate" id="rate1" value="2" class="star-icon-radio" />
-      <label for="rate1">
+  <span class="rate-stars">
+    ${RATE_STARS.map((rate) => {
+      return /*html*/ `
+      <input type="radio" name="rate" id="rate${rate}" value="${rate}" class="star-icon-radio" />
+      <label for="rate${rate}">
         <img class="star-icon" src="images/star_${
-          userRate >= 2 ? "filled" : "empty"
+          userRate >= rate ? "filled" : "empty"
         }.png" />
       </label>
-      <input type="radio" name="rate" id="rate2" value="4" class="star-icon-radio" />
-      <label for="rate2">
-        <img class="star-icon" src="images/star_${
-          userRate >= 4 ? "filled" : "empty"
-        }.png" />
-      </label>
-      <input type="radio" name="rate" id="rate3" value="6" class="star-icon-radio" />
-      <label for="rate3">
-        <img class="star-icon" src="images/star_${
-          userRate >= 6 ? "filled" : "empty"
-        }.png" />
-      </label>
-      <input type="radio" name="rate" id="rate4" value="8" class="star-icon-radio" />
-      <label for="rate4">
-        <img class="star-icon" src="images/star_${
-          userRate >= 8 ? "filled" : "empty"
-        }.png" />
-      </label>
-      <input type="radio" name="rate" id="rate5" value="10" class="star-icon-radio" />
-      <label for="rate5">
-        <img class="star-icon" src="images/star_${
-          userRate >= 10 ? "filled" : "empty"
-        }.png" />
-      </label>
+    `;
+    }).join("")}
     </span>
     <p class="rate-text">
       <span class="rate-text-description">${getRateText(userRate)}</span>
