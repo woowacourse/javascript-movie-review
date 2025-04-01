f1e07f780bd3ea6e72d3d86fc0dc32f013b26e23
Minje Sung <minje813@dgu.ac.kr>
2025-04-01 18:49:11 +0900

    refactor: Object.freeze -> readonly로 대체

--- src/constants/MovieRate.ts
+++ src/constants/MovieRate.ts
@@ -1,13 +1,13 @@
-type MOVIE_RATE_COMMENT = { [key: number]: string };
+type MOVIE_RATE_COMMENT = { readonly [key: number]: string };
 
 export const MOVIE_RATE_LIST_KEY = "movieRateList";
-export const MOVIE_RATE_COMMENT: MOVIE_RATE_COMMENT = Object.freeze({
+export const MOVIE_RATE_COMMENT: MOVIE_RATE_COMMENT = {
   2: "최악이예요",
   4: "별로예요",
   6: "보통이에요",
   8: "재미있어요",
   10: "명작이에요",
-});
+};
 export const MOVIE_NO_RATE_COMMENT = "";
 export const MOVIE_NO_DESCRIPTION =
   "이런! 아직 영화의 상세정보가 영화 정보 사이트에 등록되지 않았습니다. 🥲";
