cc8ed9e8ea005083a6b58a28acbc09a152e8583a
Minje Sung <minje813@dgu.ac.kr>
2025-04-01 18:57:17 +0900

    refactor: TMDBMovieDetail 변수명 수정

--- src/api/fetchMovieDetail.ts
+++ src/api/fetchMovieDetail.ts
@@ -9,17 +9,17 @@ async function fetchMovieDetail(id: string) {
   });
 
   try {
-    const TMDB_movieDetail = await ApiClient.get<TMDB_MovieDetail>(
+    const TMDBMovieDetail = await ApiClient.get<TMDB_MovieDetail>(
       `/movie/${id}?` + params.toString()
     );
     const movieDetail: MovieDetailProps = {
-      id: TMDB_movieDetail.id,
-      posterPath: TMDB_movieDetail.poster_path,
-      title: TMDB_movieDetail.title,
-      releaseYear: TMDB_movieDetail.release_date.split("-")[0],
-      category: TMDB_movieDetail.genres.map((genre) => genre.name),
-      rate: TMDB_movieDetail.vote_average,
-      detail: TMDB_movieDetail.overview,
+      id: TMDBMovieDetail.id,
+      posterPath: TMDBMovieDetail.poster_path,
+      title: TMDBMovieDetail.title,
+      releaseYear: TMDBMovieDetail.release_date.split("-")[0],
+      category: TMDBMovieDetail.genres.map((genre) => genre.name),
+      rate: TMDBMovieDetail.vote_average,
+      detail: TMDBMovieDetail.overview,
     };
 
     return movieDetail;
