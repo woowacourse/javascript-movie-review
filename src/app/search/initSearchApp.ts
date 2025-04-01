230db97d35eea2157b8351536e452837c771dcf8
yeongipark <yeongi0111@naver.com>
2025-04-01 14:57:19 +0900

    feat: 검색 결과 없을 시 fallback 추가

--- src/app/search/initSearchApp.ts
+++ src/app/search/initSearchApp.ts
@@ -1,7 +1,7 @@
 import mountSearchTitle from "../mount/mountSearchTitle";
 import mountMovieItemList from "../mount/mountMovieItemList";
 import MovieItemList from "../../components/movieItemList/movieItemList";
-import { defaultQueryObject, URLS } from "../../setting/settings";
+import { URLS } from "../../setting/settings";
 import type { MovieItemListInstance } from "../../../types/components";
 import { showSkeleton, hideSkeleton } from "../../service/skeleton";
 import getSearchParams from "../../util/getSearchParams";
@@ -9,6 +9,7 @@ import { registerObserver, releaseObserver } from "../../util/observer";
 import fetchWithErrorHandling from "../../util/fetchWithErrorHandling";
 import { TMDBResponse } from "../../../types/TMDB";
 import createInfiniteQuery from "../../service/createInfiniteQuery";
+import showFallback from "../../components/fallback/shwoFallback";
 
 const SENTINEL_SELECTOR = "#sentinel";
 
@@ -38,6 +39,11 @@ function mountSearchPageUI() {
 
 async function loadAndDisplayMovies() {
   const { results, page: currentPage, total_pages } = await loadSearchMovies();
+
+  if (results.length === 0) {
+    showFallback("검색 결과가 없습니다.");
+    return;
+  }
 
   if (total_pages <= currentPage) {
     releaseObserver({ sentinel: SENTINEL_SELECTOR });
