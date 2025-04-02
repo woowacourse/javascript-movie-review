99c1cc9f88b3b38ffdb1b5892f11cd7403606a0a
hoyyChoi <hoyeon8165@gmail.com>
2025-04-02 17:04:43 +0900

    refactor: starLength 5 상수 처리

--- src/components/DetailModal/MyStarRatingComponent.ts
+++ src/components/DetailModal/MyStarRatingComponent.ts
@@ -1,6 +1,8 @@
 import { RATING_MESSAGE } from "../../constant/ratingMessage";
 import createDOMElement from "../../util/createDomElement";
 
+const STAR_LENGTH = 5;
+
 const MyStarRatingComponent = (starScore: number) => {
   return createDOMElement({
     tag: "div",
@@ -35,14 +37,14 @@ export default MyStarRatingComponent;
 
 const starScoreComponent = (starScore: number) => {
   if (starScore) {
-    return Array.from({ length: 5 }, (_, index) =>
+    return Array.from({ length: STAR_LENGTH }, (_, index) =>
       createDOMElement({
         tag: "img",
         src: starScore > 0 && index < starScore / 2 ? "./images/star_filled.png" : "./images/star_empty.png",
       }),
     );
   }
-  return Array.from({ length: 5 }, () =>
+  return Array.from({ length: STAR_LENGTH }, () =>
     createDOMElement({
       tag: "img",
       src: "./images/star-empty.png",
