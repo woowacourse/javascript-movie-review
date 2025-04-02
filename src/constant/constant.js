a8733c3cfa1acd781f9bf582ac3f41e62ce16de9
마 <126178440+shuyeon@users.noreply.github.com>
2025-04-02 21:04:21 +0900

    refactor : rating string=>number

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -3,10 +3,10 @@ export const MOVIE_COUNT_PER_PAGE = 20;
 export const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
   export const RATING_MESSAGES = {
-    "null" : {comment:"별점 미등록", rating:"0"},
-    "0": {comment:"최악이에요", rating:"2"},
-    "1": {comment:"별로예요", rating:"4"},
-    "2": {comment:"보통이에요", rating:"6"},
-    "3": {comment:"재밌어요", rating:"8"},
-    "4": {comment:"명작이에요", rating:"10"},
+    "null" : {comment:"별점 미등록", rating:0},
+    "0": {comment:"최악이에요", rating:2},
+    "1": {comment:"별로예요", rating:4},
+    "2": {comment:"보통이에요", rating:6},
+    "3": {comment:"재밌어요", rating:8},
+    "4": {comment:"명작이에요", rating:10},
   };
