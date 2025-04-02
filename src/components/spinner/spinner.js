a554adc855eb1a8ceaaa9006a2f5bd0e99bd7565
yeongipark <yeongi0111@naver.com>
2025-04-02 23:40:29 +0900

    feat: 스피너 모달 중간에 띄우도록 수정

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -2,18 +2,17 @@ import { createElement } from "../../util/dom";
 
 const Spinner = (scale) => {
   const $spinner = createElement("div", {
-    className: "orbit-spinner",
-    style: `scale: ${scale}`,
+    className: "spinner-container",
   });
 
   $spinner.innerHTML = /*html*/ `
-  <div class="orbit-spinner" style="scale: ${scale}">
-      <div class="planet"></div>
-      <div class="orbit">
-        <div class="satellite satellite-1"></div>
-        <div class="satellite satellite-2"></div>
+    <div class="orbit-spinner" style="scale: ${scale}">
+        <div class="planet"></div>
+        <div class="orbit">
+          <div class="satellite satellite-1"></div>
+          <div class="satellite satellite-2"></div>
+        </div>
       </div>
-    </div>
   `;
 
   return $spinner;
