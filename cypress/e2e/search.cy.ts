2d71eb0f01b1b36f5345201a1b5dbb05422d5e24
hanheel <hanhee630630@gmail.com>
2025-04-02 22:57:17 +0900

    test : 테스트 설명을 단언형으로 변경

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -7,11 +7,11 @@ describe('검색결과가 없을 경우 테스트', () => {
     cy.get('.searchbar__icon').click();
   });
 
-  it('검색 결과가 없는 검색어를 입력하면, 검색 결과 없음 메시지를 띄워줘야 한다.', () => {
+  it('검색 결과가 없는 검색어를 입력하면, "검색 결과 없음" 메세지가 띄워진다', () => {
     cy.get('body').contains(ERROR_MESSAGE.NO_RESULT);
   });
 
-  it('검색 결과가 없는 검색어를 입력하면, 작품이 렌더링 되지 않는다.', () => {
+  it('검색 결과가 없는 검색어를 입력하면, 작품이 렌더링 되지 않아야 한다', () => {
     cy.get('.item').should('have.length', 0);
   });
 });
@@ -36,27 +36,27 @@ describe('검색 결과가 있는 경우 테스트', () => {
     cy.scrollToLoadMovies();
   });
 
-  it('[상세모달] 메인화면에서 Movie Item을 클릭할 경우 상세 모달이 띄워진다', () => {
+  it('[상세모달] 메인화면에서 Movie Item을 클릭할 경우 상세 모달이 열려야 한다', () => {
     cy.viewport(1280, 3000);
 
     cy.openDetailModal();
   });
 
-  it('[상세모달] esc를 누를 경우 모달이 꺼진다', () => {
+  it('[상세모달] esc를 누를 경우 모달이 닫혀야 한다', () => {
     cy.closeDetailModalByESC();
   });
 
-  it('[상세모달] X 아이콘을 누를 경우 모달이 꺼진다', () => {
+  it('[상세모달] X 아이콘을 누를 경우 모달이 닫혀야 한다', () => {
     cy.closeDetailModalByIcon();
   });
 
-  it('[상세모달] 모달 배경을 누를 경우 모달이 꺼진다', () => {
+  it('[상세모달] 모달 배경을 누를 경우 모달이 닫혀야 한다', () => {
     cy.viewport(1280, 3000);
 
     cy.closeModalByBackdrop();
   });
 
-  it('[별점 기록] 상세 모달창에서 별점을 기록할 경우, 해당 점수와 점수에 따른 멘트가 렌더링된다', () => {
+  it('[별점 기록] 상세 모달창에서 별점을 기록할 경우, 해당 점수와 점수에 따른 멘트가 렌더링 돼야 한다', () => {
     cy.openDetailModal();
     cy.rateMovie();
   });
