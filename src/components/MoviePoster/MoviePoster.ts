7912769707d3d03e0a3890f4e6aa6db611d21cd7
wo-o29 <woogur29@gmail.com>
2025-04-02 12:40:05 +0900

    ​​fix: placeholder 일때만 이미지 src 변경
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/components/MoviePoster/MoviePoster.ts
+++ src/components/MoviePoster/MoviePoster.ts
@@ -1,7 +1,8 @@
 import { MovieDetail } from "../../../types/type";
 
-const defaultPosterPath = "./default-poster.svg";
+const defaultPosterPath = "/default-poster.svg";
 const imagePathPreFix = "https://image.tmdb.org/t/p/w440_and_h660_face";
+const placeholderPosterPath = "/placeholder-poster.svg";
 
 interface MoviePosterProps extends Pick<MovieDetail, "title" | "poster_path"> {
   className: string;
@@ -10,14 +11,16 @@ interface MoviePosterProps extends Pick<MovieDetail, "title" | "poster_path"> {
 const $MoviePoster = ({ className, title, poster_path }: MoviePosterProps) => {
   const $poster = createElement("img", {
     className,
-    src: "./placeholder-poster.svg",
+    src: placeholderPosterPath,
     alt: title,
     loading: "lazy",
     onerror: "this.src='./error-poster.svg'",
     onload: function () {
-      this.src = poster_path
-        ? imagePathPreFix + poster_path
-        : defaultPosterPath;
+      if (this.src.includes(placeholderPosterPath)) {
+        this.src = poster_path
+          ? imagePathPreFix + poster_path
+          : defaultPosterPath;
+      }
     },
   });
 
