8c6497e9249bf2083c0772c3afa9e5baf8836536
dlsxjzld <sinjuk1@naver.com>
2025-04-02 16:12:59 +0900

    feat: 영화 상세 정보를 열 때 에러 처리 추가

--- src/components/movie/movieDetailModal/openMovieDetailModal.ts
+++ src/components/movie/movieDetailModal/openMovieDetailModal.ts
@@ -1,13 +1,18 @@
+import showErrorContainer from "../../errorContainer/showErrorContainer";
 import showMovieDetailModal from "./internal/showMovieDetailModal";
 import updateMovieDetailModal from "./internal/updateMovieDetailModal";
 
 const openMovieDetailInfo = async (event: MouseEvent) => {
-  const isUpdateCompleted = await updateMovieDetailModal(event);
-  if (!isUpdateCompleted) {
-    return;
-  }
+  try {
+    const isUpdateCompleted = await updateMovieDetailModal(event);
+    if (!isUpdateCompleted) {
+      return;
+    }
 
-  showMovieDetailModal();
+    showMovieDetailModal();
+  } catch (error) {
+    showErrorContainer(error);
+  }
 };
 
 export default openMovieDetailInfo;
