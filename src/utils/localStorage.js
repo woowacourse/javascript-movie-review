995924d8e52be029761618fe13e7afa62781078e
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-03 01:33:44 +0900

    refactor: localStorage key를 상수로 분리하여 휴먼 에러 방지

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,10 +1,12 @@
+import { STORAGE_KEYS } from "./constants/storageKeys.js";
+
 export function setRating(movieId, score) {
-  const ratings = JSON.parse(localStorage.getItem("ratings") || "{}");
+  const ratings = JSON.parse(localStorage.getItem(STORAGE_KEYS.RATINGS) || "{}");
   ratings[movieId] = score;
-  localStorage.setItem("ratings", JSON.stringify(ratings))
+  localStorage.setItem(STORAGE_KEYS.RATINGS, JSON.stringify(ratings))
 }
 
 export function getRating(movieId) {
-  const ratings = JSON.parse(localStorage.getItem("ratings") || "{}");
+  const ratings = JSON.parse(localStorage.getItem(STORAGE_KEYS.RATINGS) || "{}");
   return ratings[movieId] || 0;
 }
