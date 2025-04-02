1f57328e87126c4802f6bde39db709d4a0e83849
장정안 <wjddks96@naver.com>
2025-04-02 15:44:53 +0900

    refactor: localStorage 유틸 함수 중 불필요한 연산 제거를 위한 함수 분리

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -7,26 +7,36 @@ export interface MovieRating {
 
 const MOVIE_RATINGS_KEY = "movie_ratings" as const;
 
+export const withMovieRatings = <T>(
+  callback: (ratings: MovieRating[]) => T
+): T => {
+  const ratings = localStorage.getItem(MOVIE_RATINGS_KEY);
+  const parsedRatings = ratings ? JSON.parse(ratings) : [];
+  return callback(parsedRatings);
+};
+
 export const getMovieRatings = (): MovieRating[] => {
   const ratings = localStorage.getItem(MOVIE_RATINGS_KEY);
   return ratings ? JSON.parse(ratings) : [];
 };
 
 export const getMovieRating = (movieId: number): MovieRate | null => {
-  const ratings = getMovieRatings();
-  const movieRating = ratings.find((rating) => rating.movieId === movieId);
-  return movieRating ? movieRating.rating : null;
+  return withMovieRatings((ratings) => {
+    const movieRating = ratings.find((rating) => rating.movieId === movieId);
+    return movieRating ? movieRating.rating : null;
+  });
 };
 
 export const saveMovieRating = (movieId: number, rating: MovieRate): void => {
-  const ratings = getMovieRatings();
-  const existingRatingIndex = ratings.findIndex((r) => r.movieId === movieId);
+  withMovieRatings((ratings) => {
+    const existingRatingIndex = ratings.findIndex((r) => r.movieId === movieId);
 
-  if (existingRatingIndex !== -1) {
-    ratings[existingRatingIndex].rating = rating;
-  } else {
-    ratings.push({ movieId, rating });
-  }
+    if (existingRatingIndex !== -1) {
+      ratings[existingRatingIndex].rating = rating;
+    } else {
+      ratings.push({ movieId, rating });
+    }
 
-  localStorage.setItem(MOVIE_RATINGS_KEY, JSON.stringify(ratings));
+    localStorage.setItem(MOVIE_RATINGS_KEY, JSON.stringify(ratings));
+  });
 };
