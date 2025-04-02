058ac3c386252bc9194c8558e28b57c07faa5d5b
mun-kyeong <horse6953@naver.com>
2025-04-03 08:00:58 +0900

    refactor : setupUI 구조 간결화

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import "./event/movieEventHandler";
 import { renderMoviesList } from "./features/movies/movieListRenderer";
 import { createObserver } from "./apis/intersectionApi";
-import { toElement } from "./utils/domUtils";
 
 setupUI();
 
 function setupUI() {
-  const $container = document.querySelector(".container");
-
-  $container?.appendChild(toElement(`<div id="sentinel"></div>`));
+  document.querySelector(".container")!.innerHTML = `<div id="sentinel"></div>`;
 
   renderMoviesList();
   createObserver();
