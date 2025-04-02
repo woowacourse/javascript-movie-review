39d869149e51c3e59321518c2b41afdb3e6608f6
dlsxjzld <sinjuk1@naver.com>
2025-04-02 15:12:20 +0900

    test: 네트워크 연결 끊김에 대한 에러 메시지 테스트 추가

--- cypress/e2e/fetchError.cy.ts
+++ cypress/e2e/fetchError.cy.ts
@@ -3,7 +3,7 @@ import { ERROR_MESSAGE } from "../../src/domain/constants/errorMessage";
 import { TmdbApiFetchFailResponse } from "../../src/domain/apis/tmdbApi";
 
 describe("Fetch 에러 테스트", () => {
-  it("페이지 로드 시 fetch 요청 실패 (401 유효하지 않은 API 키)", () => {
+  it("페이지 로드 시 fetch 요청 실패 (404 찾을 수 없음)", () => {
     const tmdbErrorCode = 7;
     const errorMessage = ERROR_MESSAGE[tmdbErrorCode];
 
@@ -72,6 +72,29 @@ describe("Fetch 에러 테스트", () => {
 
     cy.wait("@getPopularMoviesServerError").then((interception) => {
       cy.get(".error-container").should("contain", errorMessage);
+    });
+  });
+});
+
+describe("네트워크 연결 끊김 테스트", () => {
+  beforeEach(() => {
+    cy.visit("localhost:5173");
+    cy.get(".thumbnail-list").find(".thumbnail").should("have.length", 20);
+  });
+
+  it("사용자가 페이지 하단으로 스크롤해서 영화 목록 API를 호출하는 도중에 네트워크 연결이 끊기면 오류 메시지를 표시한다", () => {
+    cy.intercept(
+      "GET",
+      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=2",
+      {
+        forceNetworkError: true,
+      }
+    ).as("loadMoreMoviesError");
+
+    cy.scrollTo("bottom");
+
+    cy.wait("@loadMoreMoviesError").then(() => {
+      cy.get(".error-container").should("contain", "네트워크 에러입니다.");
     });
   });
 });
