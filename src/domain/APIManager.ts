ce3f90a5e8db2e59f99b22231a6b921d583c00b8
H0ngJu <hongju4385@gmail.com>
2025-04-02 13:13:20 +0900

    refactor(APIManager): 타입 변환 함수 분리 및 api 응답 타입 선언

--- src/domain/APIManager.ts
+++ src/domain/APIManager.ts
@@ -1,19 +1,12 @@
 import { SYSTEM_CONSTANTS } from '../constants/systemConstants';
 import { redirectToPage } from '../route/router';
 import APIClient from './APIClient';
-import { Movie } from '../../types/apiMovie';
+import { Movie, MovieDetail } from '../../types/apiMovie';
 
 export async function extractedData(url: string) {
   try {
     const movieList = await APIClient.get(url);
-    const movieListData = movieList.results.map((movieItem: Movie) => ({
-      id: movieItem.id,
-      title: movieItem.title,
-      imgUrl: `${SYSTEM_CONSTANTS.BASE_IMG_URL}${movieItem.poster_path}`,
-      score: Number(movieItem.vote_average.toFixed(1)),
-      overview: movieItem.overview,
-    }));
-
+    const movieListData = movieList.results.map((movie: Movie) => convertToMovieData(movie));
     return { movieListData, totalPage: movieList.total_pages };
   } catch (error) {
     redirectToPage('/error');
@@ -21,20 +14,34 @@ export async function extractedData(url: string) {
   }
 }
 
+function convertToMovieData(movieitem: Movie) {
+  return {
+    id: movieitem.id,
+    title: movieitem.title,
+    imgUrl: `${SYSTEM_CONSTANTS.BASE_IMG_URL}${movieitem.poster_path}`,
+    score: Number(movieitem.vote_average.toFixed(1)),
+    overview: movieitem.overview,
+  };
+}
+
 export async function extractedMovieDetails(id: number) {
   try {
     const details = await APIClient.get(SYSTEM_CONSTANTS.DETAIL_URL(id));
-    return {
-      id: details.id,
-      title: details.title,
-      imgUrl: `${SYSTEM_CONSTANTS.BASE_IMG_URL}${details.poster_path}`,
-      score: Number(details.vote_average.toFixed(1)),
-      overview: details.overview,
-      genres: details.genres.map((genre: { name: string }) => genre.name).join(', '),
-      release_date: details.release_date.split('-')[0],
-    };
+    return convertToMovieDetailData(details);
   } catch (error) {
     redirectToPage('/error');
     throw error;
   }
+}
+
+export function convertToMovieDetailData(details: MovieDetail) {
+  return {
+    id: details.id,
+    title: details.title,
+    imgUrl: `${SYSTEM_CONSTANTS.BASE_IMG_URL}${details.poster_path}`,
+    score: Number(details.vote_average.toFixed(1)),
+    overview: details.overview,
+    genres: details.genres.map((genre: { name: string }) => genre.name).join(', '),
+    release_date: details.release_date.split('-')[0],
+  };
 }
