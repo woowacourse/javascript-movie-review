c3e364733254ae8b45e778dee363f8e00a6f78ec
Oliy <eunsoa0310@gmail.com>
2025-04-03 02:48:36 +0900

    refactor: 상수 객체에서 Object.freeze 제거 및 as const 추가

--- src/constants/imagePaths.ts
+++ src/constants/imagePaths.ts
@@ -1,21 +1,21 @@
-export const POSTER_PATH = Object.freeze({
+export const POSTER_PATH = {
   DEFAULT: "./default-poster.svg",
   LOADING: "./loading-poster.svg",
   ERROR: "./error-poster.svg",
-});
+} as const;
 
-export const ICON_PATH = Object.freeze({
+export const ICON_PATH = {
   STAR_EMPTY: "./star_empty.svg",
   STAR_FILLED: "./star_filled.svg",
   SEARCH: "./search.svg",
   MODAL_CLOSE: "./modal_button_close.svg",
-});
+} as const;
 
-export const LOGO_PATH = Object.freeze({
+export const LOGO_PATH = {
   LOGO: "./logo.svg",
   WOOWACOURSE_LOGO: "./woowacourse_logo.png",
-});
+} as const;
 
-export const IMAGE_PATH = Object.freeze({
+export const IMAGE_PATH = {
   EMPTY_PLANET: "./empty-planet.svg",
-});
+} as const;
--- src/constants/ratingTexts.ts
+++ src/constants/ratingTexts.ts
@@ -1,10 +1,10 @@
-const RATING_TEXTS = Object.freeze({
+const RATING_TEXTS = {
   "0": "평점 남기기",
   "1": "최악이예요",
   "2": "별로예요",
   "3": "보통이에요",
   "4": "재미있어요",
   "5": "명작이예요",
-});
+} as const;
 
 export default RATING_TEXTS;
