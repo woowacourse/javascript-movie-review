2c20925eb8bd562a27b302154ff9c005ee462168
mun-kyeong <horse6953@naver.com>
2025-04-03 07:31:05 +0900

    refactor : ratingMessages를 배열로 변환하여 타입 추론 문제 해결

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -12,14 +12,14 @@ interface ModalType {
   rate_number: number;
 }
 
-export const ratingMessages: { [key: number]: string } = {
-  0: "평가해주세요",
-  1: "최악이예요",
-  2: "별로예요",
-  3: "보통이에요",
-  4: "재미있어요",
-  5: "명작이에요",
-};
+export const ratingMessages = [
+  "평가해주세요",
+  "최악이예요",
+  "별로예요",
+  "보통이에요",
+  "재미있어요",
+  "명작이에요",
+] as const;
 
 export default function Modal({
   id,
