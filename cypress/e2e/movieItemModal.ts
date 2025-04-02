974a6049e142a301f4f57e4fcf57b366a9282286
H0ngJu <hongju4385@gmail.com>
2025-04-02 12:45:56 +0900

    refactor: movieItemModal 테스트 수정

--- cypress/e2e/movieItemModal.ts
+++ cypress/e2e/movieItemModal.ts
@@ -1,12 +1,14 @@
-describe('모달 테스트.', () => {
+describe('모달 시나리오 테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173');
   });
 
-  it('item을 클릭하면 모달창이 뜨고, 별점을 부여할 수 있다.', () => {
+  it('무비 아이템을 클릭하면 모달창이 뜨고, 별점을 부여한 후 창을 닫을 수 있다.', () => {
     cy.get('.item').eq(0).click();
     cy.get('.modal-star').eq(4).click();
     cy.get('.modal').should('contain', '명작이에요');
     cy.get('.modal').should('contain', '10/10');
+    cy.get('.close-modal').click();
+    cy.get('.active').should('not.exist');
   });
 });
