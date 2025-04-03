7e163e3316915858c07bd8501849c76e83deee13
Jeongeun Lee <leeju1009@gmail.com>
2025-04-03 22:56:28 +0900

    refactor: api 계층의 불필요한 catch 제거

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,20 +9,14 @@ const options = {
 
 const api = {
   async GETWithAuth(endpoint: string) {
-    try {
-      const url = `${BASE_URL}${endpoint}`;
-      const response = await fetch(url, options);
+    const url = `${BASE_URL}${endpoint}`;
+    const response = await fetch(url, options);
 
-      if (!response.ok) {
-        throw new Error(response.status.toString());
-      }
-
-      return await response.json();
-    } catch (error) {
-      if (error instanceof Error) {
-        throw error;
-      }
+    if (!response.ok) {
+      throw new Error(response.status.toString());
     }
+
+    return await response.json();
   },
 };
 
