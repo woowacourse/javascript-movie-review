38a151cd2ded1a78f0c646e34324846880ce004d
Jeongeun Lee <leeju1009@gmail.com>
2025-04-03 22:11:01 +0900

    refactor: 불필요한 에러 catch 로직 제거

--- src/api/movieApi.ts
+++ src/api/movieApi.ts
@@ -8,51 +8,33 @@ const defaultParams = {
 
 const movieApi = {
   async getMovieData(pageNumber: number) {
-    try {
-      const params = new URLSearchParams({
-        ...defaultParams,
-        page: pageNumber.toString(),
-      }).toString();
+    const params = new URLSearchParams({
+      ...defaultParams,
+      page: pageNumber.toString(),
+    }).toString();
 
-      const endpoint = `/movie/popular?${params}`;
-      return await api.GETWithAuth(endpoint);
-    } catch (error) {
-      if (error instanceof Error) {
-        throw error;
-      }
-    }
+    const endpoint = `/movie/popular?${params}`;
+    return await api.GETWithAuth(endpoint);
   },
 
   async getSearchData(pageNumber: number, query: string) {
-    try {
-      const params = new URLSearchParams({
-        ...defaultParams,
-        page: pageNumber.toString(),
-        query,
-      }).toString();
+    const params = new URLSearchParams({
+      ...defaultParams,
+      page: pageNumber.toString(),
+      query,
+    }).toString();
 
-      const endpoint = `/search/movie?${params}`;
-      return await api.GETWithAuth(endpoint);
-    } catch (error) {
-      if (error instanceof Error) {
-        throw error;
-      }
-    }
+    const endpoint = `/search/movie?${params}`;
+    return await api.GETWithAuth(endpoint);
   },
 
   async getMovieDetailsData(id: number) {
-    try {
-      const params = new URLSearchParams({
-        ...defaultParams,
-      }).toString();
+    const params = new URLSearchParams({
+      ...defaultParams,
+    }).toString();
 
-      const endpoint = `/movie/${id}?${params}`;
-      return await api.GETWithAuth(endpoint);
-    } catch (error) {
-      if (error instanceof Error) {
-        throw error;
-      }
-    }
+    const endpoint = `/movie/${id}?${params}`;
+    return await api.GETWithAuth(endpoint);
   },
 };
 
