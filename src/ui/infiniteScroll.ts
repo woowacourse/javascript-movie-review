e2329272379b50ebcc415aab7ed53d1ff69d591b
ShinjungOh <tlswjd9260@gmail.com>
2025-04-04 00:42:35 +0900

    refactor: 무한 스크롤 코드 배열 메소드로 개선

--- src/ui/infiniteScroll.ts
+++ src/ui/infiniteScroll.ts
@@ -9,21 +9,22 @@ export const scrollObserver = new IntersectionObserver(
   async (entries, observer) => {
     if (!$main) return;
 
-    for (const entry of entries) {
-      if (entry.isIntersecting) {
-        const { currentPage, totalPages, query, isLoading } = getState();
-        if (isLoading || currentPage === totalPages) {
-          if (currentPage === totalPages) observer.disconnect();
-          return;
-        }
-
-        updateState({ isLoading: true });
-        loadMoreMovies($main);
-
-        await fetchMovies(currentPage + 1, query);
-        loadMoreMovies($main);
-      }
+    if (!entries.some(entry => entry.isIntersecting)) {
+      return;
     }
+
+    const { currentPage, totalPages, query, isLoading } = getState();
+
+    if (isLoading || currentPage === totalPages) {
+      if (currentPage === totalPages) observer.disconnect();
+      return;
+    }
+
+    updateState({ isLoading: true });
+    loadMoreMovies($main);
+
+    await fetchMovies(currentPage + 1, query);
+    loadMoreMovies($main);
   },
   { threshold: 0.1 }
 );
