aac376c2e649dc0662388e0f3c11c48d1bc1383a
mun-kyeong <horse6953@naver.com>
2025-04-03 16:34:39 +0900

    refactor : queryParams를 인자로 받아 URL 처리 책임 분리

--- src/apis/MovieApi.ts
+++ src/apis/MovieApi.ts
@@ -2,8 +2,16 @@ import { MovieDetail, MoviesResponse } from "./apiTypes";
 
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 
+type FetchParams = {
+  language: "ko-KR";
+  page?: string;
+  query?: string;
+  include_adult?: "false" | "true";
+};
+
 async function fetchWithErrorHandling(
-  url: string
+  url: string,
+  params: FetchParams
 ): Promise<MoviesResponse | MovieDetail> {
   const options = {
     method: "GET",
@@ -12,21 +20,20 @@ async function fetchWithErrorHandling(
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
   };
+  const queryParams = new URLSearchParams(params);
 
-  return fetch(url, options).then((res) => {
+  return fetch(`${url}${queryParams.toString()}`, options).then((res) => {
     if (res.ok) return res.json();
     throw new Error(String(res.status));
   });
 }
 
 export async function fetchMovies({ page }: { page: number }) {
-  const params = new URLSearchParams({
+  const url = `${TMDB_BASE_URL}/movie/popular?`;
+  return fetchWithErrorHandling(url, {
     language: "ko-KR",
     page: String(page),
   });
-
-  const url = `${TMDB_BASE_URL}/movie/popular?${params.toString()}`;
-  return fetchWithErrorHandling(url);
 }
 
 export async function fetchMovieByName({
@@ -36,22 +43,18 @@ export async function fetchMovieByName({
   name: string;
   page: number;
 }) {
-  const params = new URLSearchParams({
+  const url = `${TMDB_BASE_URL}/search/movie?`;
+  return fetchWithErrorHandling(url, {
     query: name,
     include_adult: "false",
     language: "ko-KR",
     page: String(page),
   });
-
-  const url = `${TMDB_BASE_URL}/search/movie?${params.toString()}`;
-  return fetchWithErrorHandling(url);
 }
 
 export async function fetchMovieDetail({ id }: { id: number }) {
-  const params = new URLSearchParams({
+  const url = `${TMDB_BASE_URL}/movie/${id}?`;
+  return fetchWithErrorHandling(url, {
     language: "ko-KR",
   });
-
-  const url = `${TMDB_BASE_URL}/movie/${id}?${params.toString()}`;
-  return fetchWithErrorHandling(url);
 }
