2fd53f01bdcc50513dea532b73790327238f679c
mlnwns <mlnwns@naver.com>
2025-04-03 16:30:38 +0900

    test: 영화 상세 모달 테스트에서 중복 코드 제거 및 함수화

--- cypress/e2e/detailModal.cy.ts
+++ cypress/e2e/detailModal.cy.ts
@@ -14,32 +14,31 @@ describe("영화 상세 모달 테스트", () => {
     cy.wait("@fetchMovies");
   });
 
+  const openModal = () => {
+    cy.get(".thumbnail-list .item").first().click();
+    cy.get(".modal").should("be.visible");
+  };
+
   it("영화 썸네일을 클릭하면 모달이 나타나고, 영화 정보가 포함되어 있어야 한다.", () => {
     cy.get(".thumbnail-list .item")
       .first()
       .find("strong")
       .invoke("text")
       .then((expectedTitle) => {
-        cy.get(".thumbnail-list .item").first().click();
-
-        cy.get(".modal").should("be.visible");
+        openModal();
         cy.get(".modal h2").should("have.text", expectedTitle);
       });
   });
 
   it("모달 닫기 버튼을 클릭하면 모달이 닫혀야 한다.", () => {
-    cy.get(".thumbnail-list .item").first().click();
-    cy.get(".modal").should("be.visible");
-
+    openModal();
     cy.get(".close-modal").click();
     cy.get(".modal").should("not.visible");
   });
 
   it("모달 외부를 클릭하면 모달이 닫혀야 한다.", () => {
-    cy.get(".thumbnail-list .item").first().click();
-    cy.get(".modal").should("be.visible");
-
-    cy.get(".modal").click("topLeft");
-    cy.get(".modal").should("not.visible");
+    openModal();
+    cy.get("body").click(0, 0);
+    cy.get(".modal").should("not.be.visible");
   });
 });
