049cffbb4b01e512cc00dad3cc2561987be49113
H0ngJu <hongju4385@gmail.com>
2025-04-03 11:27:34 +0900

    test: 테스트 describe 수정

--- cypress/e2e/movieItemModal.cy.ts
+++ cypress/e2e/movieItemModal.cy.ts
@@ -1,9 +1,9 @@
-describe('모달 시나리오 테스트', () => {
+describe('영화 상세 정보 모달 시나리오 테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173');
   });
 
-  it('무비 아이템을 클릭하면 모달창이 뜨고, 별점을 부여한 후 창을 닫을 수 있다.', () => {
+  it('영화 아이템을 클릭하면 상세 정보 모달창이 뜨고, 별점을 부여한 후 창을 닫을 수 있다.', () => {
     cy.get('.item').eq(0).click();
     cy.get('.modal-star').eq(4).click();
     cy.get('.modal').should('contain', '명작이에요');
