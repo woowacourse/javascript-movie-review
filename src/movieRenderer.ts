84c4ce320eca863aef4b78d591ace529a58a2da0
hjkim0905 <eax9952@gmail.com>
2026-03-31 19:58:22 +0900

    feat: 더 보기 버튼 클릭 핸들러 완성
    Co-authored-by: th-97 <hood3392@gmail.com>

--- src/movieRenderer.ts
+++ src/movieRenderer.ts
@@ -1,28 +1,10 @@
 import { fetchMovies } from "./movieAPIResponse.ts";
 
-export const renderMovies = async () => {
-  const movies = await fetchMovies();
+export const renderMovies = async (moviePageCount) => {
+  const movies = await fetchMovies(moviePageCount);
 
   const list = document.querySelector(".thumbnail-list");
-  const banner = document.querySelector(".top-rated-movie");
-  const backgroundContainer = document.querySelector(".background-container");
   const posterBaseURL = "https://image.tmdb.org/t/p/original/";
-  const bannerBaseURL = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces";
-  //top-rated-movie ${bannerBaseURL} + ${mostPopularMovie.poster_path}
-  const mostPopularMovie = movies[0];
-
-  backgroundContainer.style.backgroundImage = `url("${bannerBaseURL + mostPopularMovie.backdrop_path}")`;
-
-  const mostPopularMovieBanner = `
-            <div class="rate">
-              <img src="/images/star_empty.png" class="star" />
-              <span class="rate-value">${mostPopularMovie.vote_average}</span>
-            </div>
-            <div class="title">${mostPopularMovie.title}</div>
-            <button class="primary detail">자세히 보기</button>
-            `;
-
-  banner.insertAdjacentHTML("beforeend", mostPopularMovieBanner);
 
   movies.forEach((movie: Movie) => {
     const li = `<li>
@@ -40,4 +22,27 @@ export const renderMovies = async () => {
             </li>`;
     list.insertAdjacentHTML("beforeend", li);
   });
+};
+
+export const renderBanner = async () => {
+  const movies = await fetchMovies(1);
+
+  const banner = document.querySelector(".top-rated-movie");
+  const backgroundContainer = document.querySelector(".background-container");
+  const bannerBaseURL = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces";
+
+  const mostPopularMovie = movies[0];
+
+  backgroundContainer.style.backgroundImage = `url("${bannerBaseURL + mostPopularMovie.backdrop_path}")`;
+
+  const mostPopularMovieBanner = `
+            <div class="rate">
+              <img src="/images/star_empty.png" class="star" />
+              <span class="rate-value">${mostPopularMovie.vote_average}</span>
+            </div>
+            <div class="title">${mostPopularMovie.title}</div>
+            <button class="primary detail">자세히 보기</button>
+            `;
+
+  banner.insertAdjacentHTML("beforeend", mostPopularMovieBanner);
 };
