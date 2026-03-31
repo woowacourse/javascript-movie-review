b553662e7b7252c443cbe7f39633d05aaafd028c
Antoliny0919 <antoliny0919@gmail.com>
2026-04-01 00:45:34 +0900

    refactor: 영화 데이터를 더 가져오는 기능 분리
    Co-authored-by: geongyu09 <geongyu09@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,23 @@ import Component from "./component.ts";
 
 let nextPageNum = 0;
 
+async function loadMoreMovies() {
+  await getPopularMovies({
+    pageNum: nextPageNum,
+    onSuccess: ({ page, results: movies }) => {
+      const ul = document.querySelector(".thumbnail-list");
+      nextPageNum = page + 1;
+      if (ul) renderMovies(ul, movies);
+    },
+    onError: function (error: Error): void {
+      throw new Error("Function not implemented.");
+    },
+    onLoading: function (): void {
+      throw new Error("Function not implemented.");
+    },
+  });
+}
+
 addEventListener("load", async () => {
   const app = document.querySelector("#app");
   if (app) {
@@ -14,21 +31,7 @@ addEventListener("load", async () => {
         const ul = document.querySelector(".thumbnail-list");
         const loadMoreButton = document.querySelector(".load-more-button");
         if (ul) renderMovies(ul, movies);
-        loadMoreButton?.addEventListener("click", async () => {
-          await getPopularMovies({
-            pageNum: nextPageNum,
-            onSuccess: ({ page, results: movies }) => {
-              nextPageNum = page + 1;
-              if (ul) renderMovies(ul, movies);
-            },
-            onError: function (error: Error): void {
-              throw new Error("Function not implemented.");
-            },
-            onLoading: function (): void {
-              throw new Error("Function not implemented.");
-            },
-          });
-        });
+        loadMoreButton?.addEventListener("click", loadMoreMovies);
       },
       onLoading: () => {
         // 로딩 중일 때 ui 보여주기
