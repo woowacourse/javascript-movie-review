c70908917e392d96b8f76182840b57ede42689a0
eunnn <ehddms0517@naver.com>
2026-03-31 21:40:42 +0900

    feat: index.html 및 main.ts에 영화 목록 렌더링을 위한 구조 추가
    Co-authored-by: janghw0126 <janghw232625@gmail.com>
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import image from "../templates/images/star_filled.png";
+import { fetchMovies } from "./features/fetchMovies";
+import MovieList from "./features/UI/MovieCard";
 
-addEventListener("load", () => {
+addEventListener("load", async () => {
   const app = document.querySelector("#app");
   const buttonImage = document.createElement("img");
   buttonImage.src = image;
@@ -8,4 +10,10 @@ addEventListener("load", () => {
   if (app) {
     app.appendChild(buttonImage);
   }
+
+  // fetchAPI를 불러와서 MovieList 생성자를 만들고, 불러오면 끝
+  const MovieListInstance = new MovieList();
+  const fetchMovie = await fetchMovies(1);
+
+  MovieListInstance.renderMovieList(fetchMovie);
 });
