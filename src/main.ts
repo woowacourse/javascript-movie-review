41923462477a077890a9f8d0a3ef2891f24bc478
binggwa <qudrhks317@gmail.com>
2026-03-31 15:08:19 +0900

    chore : 미션 진행을 위한 환경설정
    Co-authored-by: iftype <iftype@naver.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import image from "../templates/images/star_filled.png";
+import image from '../templates/images/star_filled.png';
 
-addEventListener("load", () => {
-  const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
+addEventListener('load', () => {
+  const app = document.querySelector('#app');
+  const buttonImage = document.createElement('img');
   buttonImage.src = image;
 
   if (app) {
