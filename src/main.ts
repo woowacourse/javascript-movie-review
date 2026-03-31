1abceb5b7793843f4a8e14fabc73beb152166b08
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-03-31 18:02:35 +0900

    feat: 더 보기 기능 구현
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -13,20 +13,14 @@ addEventListener("load", async () => {
   if (app) {
     app.appendChild(buttonImage);
     app.innerHTML = modal;
+    let pageNum = 1;
 
-    const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=ko-KR`;
+    const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=ko-KR&page=${pageNum}`;
     const response = await fetch(URL);
     const data = await response.json();
+    // console.log(data);
 
-    // 영화 하나 <div class="item">
-    /*
-      thumbnail, rate, title
-    */
     const movieList = data.results;
-
-    // document.querySelectorAll(".star").forEach((star) => {
-    //   (star as HTMLImageElement).src = star_empty;
-    // });
 
     const movieDisplay = document.querySelector(
       ".thumbnail-list",
@@ -52,14 +46,43 @@ addEventListener("load", async () => {
       </div>`;
 
       movieDisplay.appendChild(li);
+    });
 
-      // const thumnail = document.querySelector(".thumbnail") as HTMLImageElement;
-      // thumnail.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      // const title = document.querySelector("strong") as HTMLElement;
-      // title.textContent = movie.title;
+    const displayMoreBtn = document.querySelector(".display-more-btn");
+    displayMoreBtn?.addEventListener("click", async () => {
+      pageNum++;
 
-      // const rate = document.querySelector(".vote-average") as HTMLElement;
-      // rate.textContent = movie.vote_average;
+      const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=ko-KR&page=${pageNum}`;
+      const response = await fetch(URL);
+      const data = await response.json();
+
+      const movieList = data.results;
+
+      const movieDisplay = document.querySelector(
+        ".thumbnail-list",
+      ) as HTMLUListElement;
+
+      // 영화 20개
+      movieList.forEach((movie: any) => {
+        const li = document.createElement("li");
+
+        li.innerHTML = html` <div class="item">
+          <img
+            class="thumbnail"
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            alt="인사이드 아웃 2"
+          />
+          <div class="item-desc">
+            <p class="rate">
+              <img class="star" src="${star_empty}" />
+              <span class="vote-average">${movie.vote_average.toFixed(1)}</span>
+            </p>
+            <strong>${movie.title}</strong>
+          </div>
+        </div>`;
+
+        movieDisplay.appendChild(li);
+      });
     });
   }
 });
