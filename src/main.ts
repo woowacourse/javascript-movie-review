105e3859b934815a3e2a386e29c6c330f942f149
Antoliny0919 <antoliny0919@gmail.com>
2026-03-31 21:09:06 +0900

    feat: 영화목록을 API 데이터 기반으로 렌더링하는 기능 추가
    Co-authored-by: geongyu09 <geongyu09@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
-import image from "../templates/images/star_filled.png";
+import { getPopularMovies } from "./api.ts";
+import type { Movie } from "./api.ts";
 
-addEventListener("load", () => {
+addEventListener("load", async () => {
   const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = image;
-
   if (app) {
-    app.appendChild(buttonImage);
+    const { results: movies } = await getPopularMovies()
+    const ul = document.querySelector(".thumbnail-list");
+    if (ul) {
+      movies.forEach(movie => addMovies(ul, movie));
+    }
   }
 });
+
+function addMovies(parent: Element, movie: Movie) {
+  const { title, poster_path, vote_average } = movie
+  parent.innerHTML += /*html*/`
+  <li>
+    <div class="item">
+    <img
+    class="thumbnail"
+    src="https://image.tmdb.org/t/p/original/${poster_path}"
+    alt="${title}"
+    />
+      <div class="item-desc">
+        <p class="rate">
+          <img src="src/images/star_empty.png" class="star" /><span>${vote_average}</span>
+          </p>
+          <strong>${title}</strong>
+      </div>
+    </div>
+  </li>
+`
+}
