5987f07a2db5d28d56d8ceb7873b3138aee96ee9
binggwa <qudrhks317@gmail.com>
2026-03-31 18:44:19 +0900

    feat: api 타입 추가

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
+
+
 const options = {
   method: 'GET',
   headers: {
@@ -8,13 +10,24 @@ const options = {
   },
 };
 
-export const fetchPopularMovies = async () => {
+type resultData = {
+  title: string,
+  poster_path: string,
+  vote_average: number,
+}
+
+interface PreviewData {
+    page: number,
+    resultData: resultData[],
+}
+
+export const fetchPopularMovies = async (): Promise<PreviewData> => {
   const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options);
 
   if (!response.ok) {
     throw new Error('영화 데이터를 불러오는 데 실패했습니다.');
   }
 
-  const data = await response.json();
+  const data = await response.json() as PreviewData;
   return data;
 };
