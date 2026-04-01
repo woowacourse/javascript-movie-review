14311d538b9014cb19164ab4c6e434170f1b0da1
eunnn <ehddms0517@naver.com>
2026-04-01 14:41:45 +0900

    feat: fetchMovies 함수의 인자 추가 및 페이지네이션 기능 구현
    Co-authored-by: less <lejin0906@gmail.com>
    Co-authored-by: Henny <janghw232625@gmail.com>

--- src/features/fetchMovies.ts
+++ src/features/fetchMovies.ts
@@ -1,16 +1,39 @@
 import { Movie } from "../../types/types";
 
-export async function fetchMovies(page: number) {
-  const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
-  const BASE_URL: string = "https://api.themoviedb.org/3";
+// export async function fetchMovies(page: number) {
+//   const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
+//   const BASE_URL: string = "https://api.themoviedb.org/3";
+
+//   const response = await fetch(
+//     `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`,
+//   );
+//   if (!response.ok) {
+//     throw new Error(`API 요청 실패: ${response.status}`);
+//   }
+//   const data: { results: Movie[] } = await response.json();
+
+//   return data;
+// }
+
+// https://api.themoviedb.org/3/search/movie?query=abc
+
+const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL: string = "https://api.themoviedb.org/3";
+
+export async function fetchMovies(
+  path: string,
+  page: number,
+  params: string = "",
+) {
+  const queryUrl: string = params === "" ? "" : `&query=${params}`;
 
   const response = await fetch(
-    `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`,
+    `${BASE_URL}/${path}?api_key=${API_KEY}&language=ko-KR&page=${page}${queryUrl}`,
   );
   if (!response.ok) {
     throw new Error(`API 요청 실패: ${response.status}`);
   }
-  const data: { results: Movie[] } = await response.json();
+  const data: { results: Movie[]; total_pages: number } = await response.json();
 
   return data;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import image from "../templates/images/star_filled.png";
 import { fetchMovies } from "./features/fetchMovies";
 import MovieList from "./features/UI/MovieCard";
 
+let page: number = 1;
+
+// 렌더링 시 더보기 버튼
+const moreButton = document.querySelector(".btn-more") as HTMLButtonElement;
+
 addEventListener("load", async () => {
   const app = document.querySelector("#app");
   const buttonImage = document.createElement("img");
@@ -12,8 +17,24 @@ addEventListener("load", async () => {
   }
 
   // fetchAPI를 불러와서 MovieList 생성자를 만들고, 불러오면 끝
-  const MovieListInstance = new MovieList();
-  const fetchMovie = await fetchMovies(1);
-
-  MovieListInstance.renderMovieList(fetchMovie);
+  fetchApi();
 });
+
+// 더보기 버튼
+moreButton.addEventListener("click", async () => {
+  page += 1;
+  fetchApi();
+  // const fetchMovie = await fetchMovies("movie/popular", page);
+});
+
+async function fetchApi() {
+  const MovieListInstance = new MovieList();
+  const data = await fetchMovies("movie/popular", page);
+  MovieListInstance.renderMovieList(data);
+
+  if (data.total_pages === page) {
+    moreButton.style.display = "none";
+  } else moreButton.style.display = "block";
+}
+
+// 검색
