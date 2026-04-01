bf6f92347d4a5f576a69920f839d06f06805d090
eunnn <ehddms0517@naver.com>
2026-04-01 20:39:03 +0900

    refactor: TMDB API 관련 상수 추가
    Co-authored-by: janghw0126 <janghw232625@gmail.com>
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

---
+++ src/constants/constant.ts
@@ -0,0 +1,9 @@
+export const BACKDROP_IMAGE_URL: string =
+  "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/";
+
+export const THUMB_NAIL_URL: string =
+  "https://media.themoviedb.org/t/p/w440_and_h660_face/";
+
+export const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
+
+export const BASE_URL: string = "https://api.themoviedb.org/3";
