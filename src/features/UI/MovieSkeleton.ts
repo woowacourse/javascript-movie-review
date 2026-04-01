1c96936e71b9a2742930303cbe8a0535382bed89
eunnn <ehddms0517@naver.com>
2026-04-01 19:26:12 +0900

    feat: MovieSkeleton 컴포넌트 추가 및 렌더링 기능 구현
    Co-authored-by: janghw0126 <janghw232625@gmail.com>
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

---
+++ src/features/UI/MovieSkeleton.ts
@@ -0,0 +1,12 @@
+export class MovieSkeleton {
+  render(): string {
+    return `<div class="movie-skeleton">
+      <div class="movie-skeleton__poster"></div>
+      <div class="movie-skeleton__details">
+       <div class="movie-skeleton__title"></div>
+       <div class="movie-skeleton__info"></div>
+      </div>
+    </div>
+    `;
+  }
+}
