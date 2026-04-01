6a16aa5e777e1f2c3c752ac09a14fcd423e0ceb2
boyeon <boyeon6331@gmail.com>
2026-04-01 16:54:11 +0900

    test: 영화 목록 조회 기능 테스트
    - 20개 렌더링 확인
    - 더보기 클릭시 영화 목록 추가
    - 마지막 페이지에서 더보기 버튼 사라짐
    
    Co-authored-by: stardusty-lab stardusty-lab@users.noreply.github.com

---
+++ cypress/e2e/movie-list-rendering.cy.ts
@@ -0,0 +1,46 @@
+import { moviesFixture } from "../../test/fixtures";
+
+describe("template spec", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/movie/popular?page=1", {
+      statusCode: 200,
+      body: {
+        page: 1,
+        results: [...moviesFixture.results],
+        total_pages: 2,
+        total_results: 40,
+      },
+    }).as("getPopularPage1");
+
+    cy.intercept("GET", "**/movie/popular?page=2", {
+      statusCode: 200,
+      body: {
+        page: 2,
+        results: [...moviesFixture.results],
+        total_pages: 2,
+        total_results: 40,
+      },
+    }).as("getPopularPage2");
+
+    cy.visit("localhost:5173");
+    cy.wait("@getPopularPage1");
+  });
+
+  it("프로그램을 시작하면 20개의 영화 목록이 렌더링 된다.", () => {
+    cy.get("#movie-list li").should("have.length", 20);
+  });
+
+  it("더보기 버튼을 누르면 영화 목록이 추가로 생성되어 렌더링 된다.", () => {
+    cy.get("#more-button").click();
+    cy.wait("@getPopularPage2");
+
+    cy.get("#movie-list li").should("have.length.greaterThan", 20);
+  });
+
+  it("마지막 페이지까지 렌더링 됬을때 더보기 버튼을 출력하지 않는다.", () => {
+    cy.get("#more-button").click();
+    cy.wait("@getPopularPage2");
+
+    cy.get("#more-button").should("not.be.visible");
+  });
+});
