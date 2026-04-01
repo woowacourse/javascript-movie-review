7bf042b5ebfb5d54804167380e6af3328c7cf7f6
eunnn <ehddms0517@naver.com>
2026-04-01 16:09:38 +0900

    feat: 검색 기능 추가 및 검색 버튼 구현
    Co-authored-by: janghw0126 <janghw232625@gmail.com>
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- src/features/fetchMovies.ts
+++ src/features/fetchMovies.ts
@@ -1,22 +1,5 @@
 import { Movie } from "../../types/types";
 
-// export async function fetchMovies(page: number) {
-//   const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
-//   const BASE_URL: string = "https://api.themoviedb.org/3";
-
-//   const response = await fetch(
-//     `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`,
-//   );
-//   if (!response.ok) {
-//     throw new Error(`API 요청 실패: ${response.status}`);
-//   }
-//   const data: { results: Movie[] } = await response.json();
-
-//   return data;
-// }
-
-// https://api.themoviedb.org/3/search/movie?query=abc
-
 const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL: string = "https://api.themoviedb.org/3";
 
@@ -28,7 +11,7 @@ export async function fetchMovies(
   const queryUrl: string = params === "" ? "" : `&query=${params}`;
 
   const response = await fetch(
-    `${BASE_URL}/${path}?api_key=${API_KEY}&language=ko-KR&page=${page}${queryUrl}`,
+    `${BASE_URL}/${path}?api_key=${API_KEY}${queryUrl}&language=ko-KR&page=${page}`,
   );
   if (!response.ok) {
     throw new Error(`API 요청 실패: ${response.status}`);
--- src/main.ts
+++ src/main.ts
@@ -38,3 +38,46 @@ async function fetchApi() {
 }
 
 // 검색
+
+const btnSubmit = document.querySelector(".btn-submit") as HTMLButtonElement;
+
+btnSubmit.addEventListener("click", async (e) => {
+  e.preventDefault();
+  page = 1;
+  const searchInput = document.querySelector(
+    ".search-input",
+  ) as HTMLInputElement;
+  const searchMovie = searchInput.value.trim();
+
+  if (searchMovie === "") {
+    const MovieListInstance = new MovieList();
+    MovieListInstance.movieList!.innerHTML = "";
+    const data = await fetchMovies("movie/popular", page);
+    MovieListInstance.renderMovieList(data);
+
+    const mainTitle = document.querySelector(".main-title") as HTMLElement;
+    mainTitle.textContent = "지금 인기 있는 영화";
+
+    if (data.total_pages === page) {
+      moreButton.style.display = "none";
+    } else moreButton.style.display = "block";
+    return;
+  }
+
+  const MovieListInstance = new MovieList();
+
+  const data = await fetchMovies("search/movie", page, searchMovie);
+  MovieListInstance.movieList!.innerHTML = "";
+  MovieListInstance.renderMovieList(data);
+
+  const mainTitle = document.querySelector(".main-title") as HTMLElement;
+  mainTitle.textContent = `"${searchMovie}" 검색 결과`;
+
+  if (data.results.length === 0) {
+    MovieListInstance.movieList!.innerHTML = "<p>검색 결과가 없습니다.</p>";
+  }
+
+  if (data.total_pages === page) {
+    moreButton.style.display = "none";
+  } else moreButton.style.display = "block";
+});
