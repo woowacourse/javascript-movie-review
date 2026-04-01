063162a836a5c19b89af4852d6e8d3877a1ba638
kojesung <js95112345@gmail.com>
2026-04-01 14:40:44 +0900

    refactor: fetcher defaultOption Authorization 제거
    Co-authored-by: 송아영 <gustn99@users.noreply.github.com>

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,15 +1,15 @@
-export const fetcher = async <T, U>(
-  endpoint: string,
-  options: RequestInit = {
+export const fetcher = async <T, U>(endpoint: string, options: RequestInit) => {
+  const defaultOptions = {
     method: "GET",
+    ...options,
     headers: {
       accept: "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+      ...options.headers,
     },
-  },
-) => {
+  };
+
   try {
-    const response = await fetch(endpoint, options);
+    const response = await fetch(endpoint, defaultOptions);
     if (response.ok) {
       const data: T = await response.json();
       return data;
@@ -20,5 +20,3 @@ export const fetcher = async <T, U>(
     throw error;
   }
 };
-
-// TODO fetcher를 범용적인 api 호출 인스턴스로 갈건지, TMDB에서 쓰기 쉽게 하는 용도로 할건지 기준을 정하고 의도가 드러나게 수정
