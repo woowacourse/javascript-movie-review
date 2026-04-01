a13e62f13f9d72b7eeb4e1a94bcfc416171ad776
eunnn <ehddms0517@naver.com>
2026-04-01 16:08:43 +0900

    test: 검색 API 성공 시 데이터 반환 테스트 추가
    Co-authored-by: janghw0126 <janghw232625@gmail.com>
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -27,4 +27,30 @@ describe("TMDB API에서 인기 영화 목록을 가져온다.", () => {
     expect(data.results[0].title).toBe("Test Movie");
     expect(data.total_pages).toBe(11);
   });
+
+  test("검색 API 성공 시 데이터 반환", async () => {
+    global.fetch = vi.fn(async () => ({
+      ok: true,
+      json: () => ({
+        results: [
+          {
+            id: 1,
+            title: "Test Movie",
+            poster_path: "/test.jpg",
+            vote_average: 7.5,
+          },
+        ],
+        total_pages: 11,
+      }),
+    })) as any;
+
+    const data: { results: Movie[]; total_pages: number } = await fetchMovies(
+      "search/movie",
+      1,
+      "아바타",
+    );
+
+    expect(data.results[0].title).toBe("Test Movie");
+    expect(data.total_pages).toBe(11);
+  });
 });
