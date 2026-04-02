bfde8e40f889c325a63d5451e10f04bd8ab962fa
kojesung <js95112345@gmail.com>
2026-04-03 07:28:49 +0900

    refactor: image 폴더 public 내부로 이동

--- src/dom/render/renderThumbnailList.ts
+++ src/dom/render/renderThumbnailList.ts
@@ -20,7 +20,7 @@ export const renderThumbnailList = ({
                       />
                       <div class="item-desc">
                         <p class="rate">
-                          <img src="./images/star_empty.png" class="star" /><span
+                          <img src="./public/images/star_empty.png" class="star" /><span
                             >${movie.vote_average}</span
                           >
                         </p>
