208c6e716e6870baabc31a827dd5099d0955385a
eunnn <ehddms0517@naver.com>
2026-04-02 14:07:11 +0900

    fix: THUMB_NAIL_URL 이미지 URL 교체
    Co-authored-by: janghw0126 <janghw232625@gmail.com>
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,8 +1,7 @@
 export const BACKDROP_IMAGE_URL: string =
   "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/";
 
-export const THUMB_NAIL_URL: string =
-  "https://media.themoviedb.org/t/p/w440_and_h660_face/";
+export const THUMB_NAIL_URL: string = "https://media.themoviedb.org/t/p/w200";
 
 export const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
 
