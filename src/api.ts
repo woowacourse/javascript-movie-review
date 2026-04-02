d68295deb0ee6fb59ce1f6f3caeb7869dbae4567
binggwa <qudrhks317@gmail.com>
2026-04-02 13:54:55 +0900

    refactor: 페치 및 렌더함수 리팩토링

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,4 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-
 const options = {
   method: 'GET',
   headers: {
@@ -20,8 +19,31 @@ export interface PreviewData {
   total_pages: number;
 }
 
-export const fetchPopularMovies = async (page: number = 1): Promise<PreviewData> => {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, options);
+export interface Params {
+  page: number;
+  query?: string | undefined;
+  language?: string;
+  region?: string;
+}
+
+export interface Request {
+  path: string;
+  params: Params;
+}
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const fetchAPI = async (req: Request): Promise<PreviewData> => {
+  const url = BASE_URL + req.path;
+
+  const { query, page } = req.params;
+  const queryStr = query ? 'query=' + query : '';
+  const pageStr = '&' + 'page=' + page;
+  const regionStr = '&region=' + 'ko-kR';
+  const languageStr = '&language=' + 'ko';
+
+  const resultUrl = url + '?' + queryStr + pageStr + regionStr + languageStr;
+
+  const response = await fetch(resultUrl, options);
 
   if (!response.ok) {
     throw new Error('영화 데이터를 불러오는 데 실패했습니다.');
@@ -31,62 +53,16 @@ export const fetchPopularMovies = async (page: number = 1): Promise<PreviewData>
   return data;
 };
 
-export async function renderFetchMovieItem($target: HTMLElement | Element, page: number): Promise<number> {
-  try {
-    // 스켈레톤 나오고
-    $target.insertAdjacentHTML('beforeend', renderSkellMovieItem());
-    const data = await fetchPopularMovies(page);
-    // 다 지우기
-    removeSkeleton($target);
-    data.results.forEach((result) => {
-      $target.insertAdjacentHTML('beforeend', renderMovieItem(result));
-    });
-    toggleButton(data.total_pages, page); // 임시
-    return data.total_pages;
-  } catch (error) {
-    alert('영화 목록을 불러오지 못했습니다!');
-    return 0; // 임시
-  }
-}
-
-export const toggleButton = (totalPage: number, currentPage: number) => {
-  const $button = document.querySelector('#more-page-button');
-  if (currentPage < totalPage) {
-    $button?.classList.remove('hidden');
-  }
+export const fetchSearchMovies = (query: string, page: number = 1): Promise<PreviewData> => {
+  return fetchAPI({
+    path: '/search/movie',
+    params: { query, page },
+  });
 };
 
-function renderMovieItem(data: resultData): string {
-  return /* html */ `
-      <li>
-        <div class="item">
-          <img class="thumbnail" src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${data.title}" />
-          <div class="item-desc">
-            <p class="rate">
-              <img src="./images/star_empty.png" class="star" />
-              <span>${data.vote_average}</span>
-            </p>
-            <strong>${data.title}</strong>
-          </div>
-        </div>
-      </li>
-    `;
-}
-
-function renderSkellMovieItem(): string {
-  const skelHTML = /* html */ `
-      <li class= "skeleton-container">
-        <div class="item">
-          <div class="thumbnail skeleton"></div>
-        </div>
-      </li>
-    `;
-
-  return skelHTML.repeat(20);
-}
-
-export const removeSkeleton = ($target: Element) => {
-  $target.querySelectorAll('.skeleton-container').forEach((node) => {
-    node.remove();
+export const fetchPopularMovies = (page: number = 1): Promise<PreviewData> => {
+  return fetchAPI({
+    path: '/movie/popular',
+    params: { page },
   });
 };
---
+++ src/render.ts
@@ -0,0 +1,91 @@
+import { fetchPopularMovies, fetchSearchMovies, resultData } from './api.ts';
+
+export const renderFetchMovieItem = async (
+  $target: HTMLElement | Element,
+  page: number,
+  query?: string,
+): Promise<number|Error> => {
+  try {
+    // 스켈레톤 나오고
+    $target.insertAdjacentHTML('beforeend', renderSkellMovieItem());
+
+    // 데이터 페치받아오기
+    let data;
+    if (query) {
+      data = await fetchSearchMovies(query, page);
+    } else {
+      data = await fetchPopularMovies(page);
+    }
+
+    // 검색 결과 없으면 없음 ui 표시
+    if (query && data.results.length === 0) {
+      // 검색결과 없음 창 표시
+      $target.innerHTML = renderErrorpage(query);
+    }
+
+    // 스켈레톤 다 지우기
+    removeSkeleton($target);
+
+    // 페치해온 데이터 렌더링
+    const dataHTML = data.results.map(renderMovieItem).join('');
+    $target.insertAdjacentHTML('beforeend', dataHTML);
+
+    // 더보기 버튼 토글
+    toggleButton(data.total_pages, page);
+    
+    return data.total_pages;
+  } catch (error) {
+    alert('영화 목록을 불러오지 못했습니다!');
+    return new Error('영화 목록을 불러오지 못했습니다!')
+  }
+};
+
+export const renderErrorpage = (query: string) => {
+  return /* html */ `
+      <div class = "empty-result">
+        <img src="./images/empty.png" alt="검색 결과가 없습니다." class="empty-image" />
+        <p>"${query}" 검색 결과가 없습니다.</p>
+      </div>`;
+};
+
+export const toggleButton = (totalPage: number, currentPage: number) => {
+  const $button = document.querySelector('#more-page-button');
+  if (currentPage < totalPage) {
+    $button?.classList.remove('hidden');
+  }
+};
+
+export function renderMovieItem(data: resultData): string {
+  return /* html */ `
+      <li>
+        <div class="item">
+          <img class="thumbnail" src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${data.title}" />
+          <div class="item-desc">
+            <p class="rate">
+              <img src="./images/star_empty.png" class="star" />
+              <span>${data.vote_average}</span>
+            </p>
+            <strong>${data.title}</strong>
+          </div>
+        </div>
+      </li>
+    `;
+}
+
+export function renderSkellMovieItem(): string {
+  const skelHTML = /* html */ `
+      <li class= "skeleton-container">
+        <div class="item">
+          <div class="thumbnail skeleton"></div>
+        </div>
+      </li>
+    `;
+
+  return skelHTML.repeat(20);
+}
+
+export const removeSkeleton = ($target: Element) => {
+  $target.querySelectorAll('.skeleton-container').forEach((node) => {
+    node.remove();
+  });
+};
