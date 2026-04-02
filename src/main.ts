2c2cc78678c1ae95b3d869471c662125288c4be0
Antoliny0919 <antoliny0919@gmail.com>
2026-04-02 13:23:50 +0900

    refactor: observe 로직 분리
    Co-authored-by: geongyu09 <geongyu09@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { getPopularMovies, getSearchMovies } from "./api.ts";
 import Renderer from "./render.ts";
+import { observeHeaderScroll } from "./observer.ts";
 
 let nextPageNum = 0;
 let nextSearchPageNum = 0;
@@ -136,18 +137,3 @@ searchForm?.addEventListener("submit", (event) => {
 });
 
 addEventListener("load", loadInitialMovie);
-
-function observeHeaderScroll() {
-  // 배너영역을 기준으로 헤더의 background 색상이 변경된다.
-  const topRatedMovie = document.querySelector(".top-rated-movie");
-  const header = document.querySelector(".background-container");
-  if (topRatedMovie && header) {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        header.classList.toggle("scrolled", !entry.isIntersecting);
-      },
-      { threshold: 0 },
-    );
-    observer.observe(topRatedMovie);
-  }
-}
