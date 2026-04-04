e8e21d1a966e660d59de1d982f13f008b89163e8
kojesung <js95112345@gmail.com>
2026-04-05 00:48:49 +0900

    feat: 더보기 api 에러 헨들링 추가

--- src/dom/eventHandler/handleSeeMore.ts
+++ src/dom/eventHandler/handleSeeMore.ts
@@ -1,9 +1,12 @@
 import { getPopularMovies } from "../../apis/movie/api";
 import { getSearchedMovies } from "../../apis/search/api";
+import { getErrorMessage } from "../../utils/getErrorMessage";
+import { renderError } from "../render/renderError";
 import { renderThumbnailList } from "../render/renderThumbnailList";
 
 export const handleMainSeeMore = async () => {
   const mainThumbnailList = document.getElementById("main-thumbnail-list");
+  const mainErrorContainer = document.getElementById("main-error-container");
   const url = new URL(window.location.href);
   const params = url.searchParams;
   const prevPage = Number(params.get("page") || 1);
@@ -12,19 +15,27 @@ export const handleMainSeeMore = async () => {
   url.search = params.toString();
   window.history.pushState({}, "", url.toString());
 
-  const movies = await getPopularMovies({
-    page: prevPage + 1,
-    language: "ko-KR",
-  });
-
-  renderThumbnailList({
-    movies: movies.results,
-    thumbnailListElement: mainThumbnailList,
-  });
+  try {
+    const movies = await getPopularMovies({
+      page: prevPage + 1,
+      language: "ko-KR",
+    });
+    renderThumbnailList({
+      movies: movies.results,
+      thumbnailListElement: mainThumbnailList,
+    });
+  } catch (error) {
+    renderError({
+      container: mainErrorContainer,
+      message: getErrorMessage(error),
+    });
+  }
 };
 
 export const handleSearchSeeMore = async (keyword: string) => {
-  const mainThumbnailList = document.getElementById("search-thumbnail-list");
+  const searchThumbnailList = document.getElementById("search-thumbnail-list");
+  const mainErrorContainer = document.getElementById("main-error-container");
+
   const url = new URL(window.location.href);
   const params = url.searchParams;
   const prevPage = Number(params.get("page") || 1);
@@ -33,14 +44,20 @@ export const handleSearchSeeMore = async (keyword: string) => {
   url.search = params.toString();
   window.history.pushState({}, "", url.toString());
 
-  const movies = await getSearchedMovies({
-    query: keyword,
-    page: prevPage + 1,
-    language: "ko-KR",
-  });
-
-  renderThumbnailList({
-    movies: movies.results,
-    thumbnailListElement: mainThumbnailList,
-  });
+  try {
+    const movies = await getSearchedMovies({
+      query: keyword,
+      page: prevPage + 1,
+      language: "ko-KR",
+    });
+    renderThumbnailList({
+      movies: movies.results,
+      thumbnailListElement: searchThumbnailList,
+    });
+  } catch (error) {
+    renderError({
+      container: mainErrorContainer,
+      message: getErrorMessage(error),
+    });
+  }
 };
