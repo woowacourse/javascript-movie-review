001d4dac4c69361ada2296b39fe3a495fe662a6a
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-04 23:31:34 +0900

    feat: cy 명령어 추상화

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,34 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+Cypress.Commands.add('mockPopularMovies', (page: number) => {
+  cy.intercept(
+    { 
+      method: "GET", 
+      url: "**/movie/popular*", 
+      query: {
+        language: 'ko-KR', 
+        page: String(page) 
+      } 
+    },
+    { fixture: `popularMoviePage${page}.json` }
+  ).as(`getPopularMoviesPage${page}`);
+});
+
+Cypress.Commands.add("mockSearchMovies", (searchQuery: string, jsonFile: string) => {
+  cy.intercept(
+    {
+      method: "GET", 
+      url: "**/search/movie*",
+      query: {
+        query: String(searchQuery),
+        include_adult: String(false),
+        language: 'ko-KR',
+        page: String(1)
+      }
+    }, {
+    fixture: String(jsonFile)
+  }).as("searchMovies");
+});
+
+Cypress.Commands.add("performSearch", (searchQuery: string) => {
+  cy.get(".search-input").clear().type(searchQuery);
+  cy.get(".search-button").click();
+});
