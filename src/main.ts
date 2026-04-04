aa6a116a6088ac49c1a8c580a2d772307e7254dc
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-04-04 16:39:08 +0900

    refactor: 영화 렌더링 로직을 하나의 메서드로 분리

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {
   removeMovieSkeletonUIList,
   showBackgroundMovieInfo,
 } from "./view/movieListView.ts";
-import { fetchDefaultMovieList } from "./service/movieApi.ts";
+import { fetchMovieList } from "./service/movieApi.ts";
 import { getUListElement } from "./view/getElementView.ts";
 
 import {
@@ -20,20 +20,40 @@ export type State = {
   searchBarText: string;
 };
 
-addEventListener("load", async () => {
-  const state: State = {
-    pageNum: 1,
-    searchBarText: "",
-  };
+const state: State = {
+  pageNum: 1,
+  searchBarText: "",
+};
 
+export const loadMovies = async ({ reset = false }: { reset: boolean }) => {
   const movieDisplay = getUListElement(".thumbnail-list");
 
-  addMovieSkeletonUIList(movieDisplay, 20);
-  const movieList = await fetchDefaultMovieList(state.pageNum);
-  removeMovieSkeletonUIList(movieDisplay);
-  addMovieList(movieDisplay, movieList);
+  if (reset) {
+    movieDisplay.replaceChildren();
+  }
 
-  showBackgroundMovieInfo(movieList[0]);
+  addMovieSkeletonUIList(movieDisplay);
+
+  try {
+    const path =
+      state.searchBarText === "" ? "/movie/popular" : "/search/movie";
+    const movieList = await fetchMovieList(
+      path,
+      state.pageNum,
+      state.searchBarText,
+    );
+
+    addMovieList(movieDisplay, movieList);
+  } catch (error) {
+  } finally {
+    removeMovieSkeletonUIList(movieDisplay);
+  }
+};
+
+addEventListener("load", async () => {
+  loadMovies({ reset: false });
+
+  // showBackgroundMovieInfo(movieList[0]);
 
   bindSearchEvents(state);
   bindMoreMovieEvents(state);
