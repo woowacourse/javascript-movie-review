cb89e074bc6752f0b70b2645992dc8e508170651
박건규 <geongyu09@gmail.com>
2026-04-04 14:19:13 +0900

    feat: fetcher 유틸 함수가 promise를 반환하도록 수정

--- src/utils.ts
+++ src/utils.ts
@@ -5,13 +5,16 @@ interface FetcherArgs<T> {
   onLoading: () => void;
 }
 
-export function fetcher<T>(arg: FetcherArgs<T>) {
+export async function fetcher<T>(arg: FetcherArgs<T>): Promise<T> {
   const { fn, onSuccess, onError, onLoading } = arg;
   onLoading();
-  fn()
-    .then(onSuccess)
-    .catch((e: unknown) => {
-      console.error(e);
-      onError(e instanceof Error ? e : new Error(String(e)));
-    });
+  try {
+    const response = await fn();
+    onSuccess(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+    onError(error instanceof Error ? error : new Error(String(error)));
+    throw error;
+  }
 }
