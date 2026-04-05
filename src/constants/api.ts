d0fe8fc2fac940b2a40f0cb5e66685dd681af839
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-06 01:17:37 +0900

    refactor: api 관련 url 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -6,3 +6,5 @@ export const OPTIONS = {
     Authorization: `Bearer ${token}`,
   },
 };
+export const THUMBNAIL_IMAGE = "https://media.themoviedb.org/t/p/w200";
+export const BANNER_IMAGE_URL = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces";
