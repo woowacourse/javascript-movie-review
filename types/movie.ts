c1973fbd8c1988ccd9db70c5e09afb6d5a9b8019
jebiyeon02 <jhje5595@naver.com>
2026-04-05 17:53:53 +0900

    fix: genre_idx -> genre_ids 타입 불일치 수정

--- types/movie.ts
+++ types/movie.ts
@@ -1,7 +1,7 @@
 export interface Movie {
   adult: boolean;
   backdrop_path: string;
-  genre_idx: number[];
+  genre_ids: number[];
   id: number;
   original_language: string;
   original_title: string;
