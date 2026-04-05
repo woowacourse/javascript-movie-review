c20661497344737d06466bacc168bc0b39ea11dc
hjkim0905 <eax9952@gmail.com>
2026-04-05 22:46:05 +0900

    test: add tests for rejects of promises

--- __test__/ApiRequest.test.ts
+++ __test__/ApiRequest.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import { fetchMovies } from "../src/movieAPIResponse.ts";
+import { fetchMovies, fetchSearchedMovies } from "../src/movieAPIResponse.ts";
 import mockMovies from "../cypress/fixtures/movies.json";
 
 beforeEach(() => {
@@ -16,5 +16,25 @@ describe("Api Requests", () => {
   it("영화를 20개 가져온다", async () => {
     const result = await fetchMovies(1);
     expect(result.results).toHaveLength(20);
+  });
+
+  it("response.ok가 false이면 fetchMovies는 에러를 throw한다", async () => {
+    vi.stubGlobal(
+      "fetch",
+      vi.fn().mockResolvedValue({
+        ok: false,
+      }),
+    );
+    await expect(fetchMovies(1)).rejects.toThrow("FAILED TO FETCH POPULAR MOVIES");
+  });
+
+  it("response.ok가 false이면 fetchSearchedMovies는 에러를 throw한다", async () => {
+    vi.stubGlobal(
+      "fetch",
+      vi.fn().mockResolvedValue({
+        ok: false,
+      }),
+    );
+    await expect(fetchSearchedMovies("아이언맨", 1)).rejects.toThrow("FAILED TO FETCH SEARCHED MOVIES");
   });
 });
