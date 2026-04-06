95b0b9ed217b112c0808a120fea0129f689c822b
gustn99 <azerosong@gmail.com>
2026-04-06 20:35:40 +0900

    refactor: 중복 배너 css 제거

--- src/dom/render/renderBanner.ts
+++ src/dom/render/renderBanner.ts
@@ -17,8 +17,6 @@ export const renderBanner = ({ movie }: RenderBannerProps) => {
 
   if (banner && bannerTitle && bannerRate) {
     banner.style.backgroundImage = `url(${import.meta.env.VITE_TMDB_IMAGE_BASE_URL}/w1280${movie.backdrop_path})`;
-    banner.style.backgroundSize = "cover";
-    banner.style.backgroundPosition = "center";
     bannerTitle.textContent = movie.title;
     bannerRate.textContent = String(movie.vote_average);
   }
