34d576a79b773310066eb383e96bd30d0d35d35e
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-06 13:40:05 +0900

    refactor: 테스트 수정

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -7,24 +7,24 @@ describe("getMovies", () => {
     const moviesData = await getMovies(1);
 
     expect(moviesData).toBeDefined();
-    if (moviesData) {
-      expect(moviesData.results.length).toBeGreaterThan(0);
+    if (moviesData.success) {
+      expect(moviesData.data.results.length).toBeGreaterThan(0);
     }
   });
 });
 
 describe("searchMovies", () => {
   test("검색한 영화 목록을 반환한다", async () => {
-    const moviesData = await searchMovies("해리포터", 1);
+    const moviesData = await searchMovies(1, "해리포터");
 
     expect(moviesData).toBeDefined();
-    if (moviesData) {
-      expect(moviesData.results.length).toBeGreaterThan(0);
+    if (moviesData.success) {
+      expect(moviesData.data.results.length).toBeGreaterThan(0);
     }
   });
 
   test("검색 결과가 없다면 빈 목록을 반환한다", async () => {
-    const moviesData = await searchMovies("", 1);
-    expect(moviesData?.results).toEqual([]);
+    const moviesData = await searchMovies(1, "");
+    if (moviesData.success) expect(moviesData.data.results).toEqual([]);
   });
 });
