e0c676efa168ed56bee653b9a8e81d4ebb8d59d8
eunnn <ehddms0517@naver.com>
2026-04-06 14:16:43 +0900

    feat: 에러 테스트 케이스 추가

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -2,7 +2,7 @@ import { vi, expect, test, describe } from "vitest";
 import { Movie } from "../types/types";
 import { fetchMoviesApi } from "../src/features/api/fetchMoviesApi";
 
-describe("TMDB API에서 인기 영화 목록을 가져온다.", () => {
+describe("TMDB API에서 영화 목록을 가져온다.", () => {
   test("API 성공 시 데이터 반환", async () => {
     global.fetch = vi.fn(async () => ({
       ok: true,
@@ -47,5 +47,26 @@ describe("TMDB API에서 인기 영화 목록을 가져온다.", () => {
 
     expect(data.results[0].title).toBe("Test Movie");
     expect(data.total_pages).toBe(11);
+  });
+
+  test("API 응답이 실패하면 에러를 던진다", async () => {
+    global.fetch = vi.fn(async () => ({
+      ok: false,
+      status: 500,
+    })) as any;
+
+    await expect(fetchMoviesApi("movie/popular", 1)).rejects.toThrow(
+      "500 영화 정보를 불러오지 못했습니다.",
+    );
+  });
+
+  test("네트워크 에러가 발생하면 안내 메시지를 던진다", async () => {
+    global.fetch = vi.fn(async () => {
+      throw new TypeError("Fail");
+    }) as any;
+
+    await expect(fetchMoviesApi("movie/popular", 1)).rejects.toThrow(
+      "네트워크 연결을 확인해주세요.",
+    );
   });
 });
