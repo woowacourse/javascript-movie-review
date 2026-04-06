bb3ae626276a8de8b74d749a8ad7f54b39e50958
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-04-06 14:29:10 +0900

    fix: index.html에서 주석 제거, package.json 유저네임 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/javascript-movie-review/",
-});
+// export default defineConfig({
+//   base: "/javascript-movie-review/",
+// });
