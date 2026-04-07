314d094fd59977814c5793a2321c4500b5254e89
장혜원 <janghw232625@gmail.com>
2026-04-07 17:19:36 +0900

    refactor: 인기 영화 및 검색 쿼리를 위한 API 호출 함수와 핸들러 수정
    - API 호출 로직을 fetchMoviesApi.ts로 통일하여 응집도 향상
    - fetchPopularMovies, fetchSearchMovies로 네이밍을 구체화하여 가독성 개선
    - API와 Handler의 관심사 분리

--- src/features/api/fetchMoviesApi.ts
+++ src/features/api/fetchMoviesApi.ts
@@ -1,11 +1,25 @@
 import { Movie } from "../../../types/types";
 import { API_KEY, BASE_URL } from "../../constants/constant";
+import { POPULAR_PATH, SEARCH_PATH } from "../../constants/constant";
 
-export async function fetchMoviesApi(
-  path: string,
+export async function fetchPopularMovies(
   page: number,
-  params: string = "",
-) {
+): Promise<{ results: Movie[]; total_pages: number }> {
+  return await fetchApi(POPULAR_PATH, page);
+}
+
+export async function fetchSearchMovies(
+  page: number,
+  searchMovie?: string,
+): Promise<{ results: Movie[]; total_pages: number }> {
+  if (searchMovie) {
+    return await fetchApi(SEARCH_PATH, page, searchMovie);
+  }
+
+  return await fetchApi(POPULAR_PATH, page, searchMovie);
+}
+
+async function fetchApi(path: string, page: number, params: string = "") {
   const queryUrl: string =
     params === "" ? "" : `&query=${encodeURIComponent(params)}`;
 
--- src/features/handler/dataHandlers.ts
+++ src/features/handler/dataHandlers.ts
@@ -1,33 +1,14 @@
-import { fetchMoviesApi } from "../api/fetchMoviesApi";
+import { fetchPopularMovies, fetchSearchMovies } from "../api/fetchMoviesApi";
 import MovieList from "../UI/MovieList";
-import { POPULAR_PATH, SEARCH_PATH } from "../../constants/constant";
 import { Movie } from "../../../types/types";
 
 const movieListInstance = new MovieList();
 
-async function fetchApi(
-  page: number,
-  searchMovie?: string,
-): Promise<{ results: Movie[]; total_pages: number }> {
-  if (searchMovie) {
-    return await fetchMoviesApi(SEARCH_PATH, page, searchMovie);
-  }
-
-  return await fetchMoviesApi(POPULAR_PATH, page, searchMovie);
-}
-
-export async function handleMoreMovie(
-  page: number,
-  searchMovie: string,
-): Promise<{
-  results: Movie[];
-  total_pages: number;
-}> {
+export async function handleMoreMovie(page: number, searchMovie: string) {
   try {
-    const data: { results: Movie[]; total_pages: number } = await fetchApi(
-      page,
-      searchMovie,
-    );
+    const data: { results: Movie[]; total_pages: number } = searchMovie
+      ? await fetchSearchMovies(page, searchMovie)
+      : await fetchPopularMovies(page);
     return data;
   } catch (error) {
     throw new Error("영화 데이터를 불러오는 중 오류가 발생했습니다.");
@@ -40,9 +21,8 @@ export async function handleMovie(page: number): Promise<{
 }> {
   try {
     movieListInstance.renderSkeleton();
-    const data: { results: Movie[]; total_pages: number } = await fetchApi(
-      page,
-    );
+    const data: { results: Movie[]; total_pages: number } =
+      await fetchPopularMovies(page);
     return data;
   } catch (error) {
     throw new Error("영화 데이터를 불러오는 중 오류가 발생했습니다.");
@@ -58,10 +38,8 @@ export async function handleSearchMovie(
 }> {
   try {
     movieListInstance.renderSkeleton();
-    const data: { results: Movie[]; total_pages: number } = await fetchApi(
-      page,
-      searchMovie,
-    );
+    const data: { results: Movie[]; total_pages: number } =
+      await fetchSearchMovies(page, searchMovie);
     return data;
   } catch (error) {
     throw new Error("영화 데이터를 불러오는 중 오류가 발생했습니다.");
