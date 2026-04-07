a8faa77d80a9d393d9c759521cec88376a979b8f
lee-eojin <lejin0906@snu.ac.kr>
2026-04-07 14:57:43 +0900

    refactor: moreButton 로직 분리

---
+++ src/utils/dom.ts
@@ -0,0 +1,7 @@
+export function updateMoreButton(
+  moreButton: HTMLButtonElement,
+  totalPages: number,
+  currentPage: number,
+): void {
+  moreButton.style.display = totalPages === currentPage ? "none" : "block";
+}
