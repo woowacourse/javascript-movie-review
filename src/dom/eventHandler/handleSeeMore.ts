70d2f52d4409b683d47d6388d9e4d15d0422186f
gustn99 <azerosong@gmail.com>
2026-04-07 19:47:45 +0900

    feat: 더보기 버튼 에러 핸들링 추가

--- src/dom/eventHandler/handleSeeMore.ts
+++ src/dom/eventHandler/handleSeeMore.ts
@@ -1,5 +1,7 @@
 import { getPopularMovies } from "../../apis/movie/api";
 import { getSearchedMovies } from "../../apis/search/api";
+import TMDBError from "../../TMDBError";
+import { renderResultSectionContent } from "../render/renderResultSectionContent";
 import { renderThumbnailList } from "../render/renderThumbnailList";
 
 export const handleMainSeeMore = async () => {
@@ -12,15 +14,32 @@ export const handleMainSeeMore = async () => {
   url.search = params.toString();
   window.history.pushState({}, "", url.toString());
 
-  const movies = await getPopularMovies({
-    page: prevPage + 1,
-    language: "ko-KR",
-  });
+  try {
+    const popularMovies = await getPopularMovies({
+      page: prevPage + 1,
+      language: "ko-KR",
+    });
+    const isLastPage = popularMovies.page === popularMovies.total_pages;
+    const movies = popularMovies.results;
 
-  renderThumbnailList({
-    movies: movies.results,
-    thumbnailListElement: mainThumbnailList,
-  });
+    renderThumbnailList({
+      movies,
+      thumbnailListElement: mainThumbnailList,
+    });
+
+    renderResultSectionContent({
+      isLoading: false,
+      isError: false,
+      isLastPage,
+      movies,
+    });
+  } catch (error) {
+    let errorMessage = "알 수 없는 에러가 발생했습니다.";
+    if (error instanceof TMDBError) {
+      errorMessage = "TMDB에서 데이터를 불러오는 중 에러가 발생했습니다";
+    }
+    window.alert(errorMessage);
+  }
 };
 
 export const handleSearchSeeMore = async (keyword: string) => {
@@ -33,14 +52,31 @@ export const handleSearchSeeMore = async (keyword: string) => {
   url.search = params.toString();
   window.history.pushState({}, "", url.toString());
 
-  const movies = await getSearchedMovies({
-    query: keyword,
-    page: prevPage + 1,
-    language: "ko-KR",
-  });
+  try {
+    const searchResult = await getSearchedMovies({
+      query: keyword,
+      page: prevPage + 1,
+      language: "ko-KR",
+    });
+    const isLastPage = searchResult.page === searchResult.total_pages;
+    const movies = searchResult.results;
 
-  renderThumbnailList({
-    movies: movies.results,
-    thumbnailListElement: mainThumbnailList,
-  });
+    renderThumbnailList({
+      movies,
+      thumbnailListElement: mainThumbnailList,
+    });
+
+    renderResultSectionContent({
+      isLoading: false,
+      isError: false,
+      isLastPage,
+      movies,
+    });
+  } catch (error) {
+    let errorMessage = "알 수 없는 에러가 발생했습니다.";
+    if (error instanceof TMDBError) {
+      errorMessage = "TMDB에서 데이터를 불러오는 중 에러가 발생했습니다";
+    }
+    window.alert(errorMessage);
+  }
 };
