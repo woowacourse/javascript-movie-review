5f762554e700a882ca2d3a8ac784cd445570e0e8
inaemin <inaemin@gmail.com>
2026-04-07 16:45:13 +0900

    refactor: search-form 컴포넌트 함수 분리 및 개선
    - createForm, createInput, createButton, createSearchIcon 함수 분리
    - ROUTES.SEARCH 상수 사용
    - input required 추가

--- src/components/search-form.ts
+++ src/components/search-form.ts
@@ -1,31 +1,46 @@
 import searchIconSrc from "../../templates/images/search.png";
+import { ROUTES } from "../route/constants";
 
 export function createSearchForm(): HTMLDivElement {
   const wrapper = document.createElement("div");
   wrapper.className = "search-form-wrapper";
 
+  const form = createForm();
+  form.append(createInput(), createButton());
+  wrapper.appendChild(form);
+
+  return wrapper;
+}
+
+function createForm(): HTMLFormElement {
   const form = document.createElement("form");
   form.className = "search-form";
-  form.action = "/search";
+  form.action = ROUTES.SEARCH;
   form.method = "get";
+  return form;
+}
 
+function createInput(): HTMLInputElement {
   const input = document.createElement("input");
   input.type = "text";
   input.name = "query";
   input.placeholder = "검색어를 입력하세요";
   input.className = "search-input";
+  input.required = true;
+  return input;
+}
 
+function createButton(): HTMLButtonElement {
   const button = document.createElement("button");
   button.type = "submit";
   button.className = "search-button";
+  button.appendChild(createSearchIcon());
+  return button;
+}
 
+function createSearchIcon(): HTMLImageElement {
   const icon = document.createElement("img");
   icon.src = searchIconSrc;
   icon.alt = "검색";
-
-  button.appendChild(icon);
-  form.append(input, button);
-  wrapper.appendChild(form);
-
-  return wrapper;
+  return icon;
 }
