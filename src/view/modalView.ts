7f658aa586fdcd324552456b37988eeca5838b44
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-08 23:36:13 +0900

    feat: 영화 카드 클릭 시 모달 열기

--- src/view/modalView.ts
+++ src/view/modalView.ts
@@ -1,19 +1,27 @@
 class ModalView {
   #movieListContainer;
+  #modalContainer;
 
   constructor() {
     this.#movieListContainer = document.querySelector<HTMLElement>(".thumbnail-list");
+    this.#modalContainer = document.querySelector<HTMLElement>(".modal-background");
   };
 
   bindMovieClick(handler: (clickedMovieId: string) => void) {
     this.#movieListContainer?.addEventListener("click", (e) => {
       if (e.target instanceof Element) {
         const movieCard = e.target.closest(".movie");
-        if (movieCard) handler(movieCard.id);
+        if (movieCard) {
+          this.openModal();
+          handler(movieCard.id);
+        }
       };
     });
   };
 
+  openModal() {
+    this.#modalContainer?.classList.add("active");
+  };
 }
 
 export const modalView = new ModalView();
