6b1be4be1e937830b485acaac4a158a167745437
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-09 00:16:25 +0900

    feat: 영화 상세정보 api 타입 설정

--- types/api.d.ts
+++ types/api.d.ts
@@ -12,6 +12,21 @@ interface Movies {
   vote_average: number;
 }
 
+interface MovieModalData {
+  title: string;
+  release_date: string;
+  genres: Genre[];
+  vote_average: number;
+  poster_path: string;
+  overview: string;
+  vote_average: number;
+};
+
+interface Genre {
+  id: number;
+  name: string;
+};
+
 type ApiResult<T> = 
   | { success: true, data: T }
   | { success: false, error: string };
