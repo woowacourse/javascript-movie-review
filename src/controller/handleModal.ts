92e447d38ec46ef49d1a13662295be0932cef312
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-09 21:55:52 +0900

    feat: 영화 상세 모달 렌더링

--- src/controller/handleModal.ts
+++ src/controller/handleModal.ts
@@ -1,14 +1,14 @@
 import { getMovieDetails } from "../api/getMovieDetails";
 import { errorMovieDetail } from "../services/errorMovieDetail";
+import { modalView } from "../view/modalView";
 
 export async function handleModal(clickedMovieId: string) {
-  // 영화 상세정보 api
   const movieDetailsData = await getMovieDetails(Number(clickedMovieId));
-  // 에러 확인
+
   if (!movieDetailsData.success) {
     errorMovieDetail(movieDetailsData.error);
     return;
   };
-  // 상세보기 렌더링
-  
-}
+
+  modalView.renderMovieDetail(movieDetailsData.data);
+};
--- src/view/modalView.ts
+++ src/view/modalView.ts
@@ -1,12 +1,27 @@
+import { ORIGINAL_IMAGE } from "../constants/api";
+import NO_IMAGE from "../../public/images/no-image.png";
+
 class ModalView {
   #movieListContainer;
   #modalContainer;
   #closeModal;
+  #modalImage;
+  #modalDescriptionTitle;
+  #modalDescriptionYear ;
+  #modalDescriptionGenre;
+  #modalRateNumber;
+  #modalDetailDescription;
 
   constructor() {
     this.#movieListContainer = document.querySelector<HTMLElement>(".thumbnail-list");
     this.#modalContainer = document.querySelector<HTMLElement>(".modal-background");
     this.#closeModal = document.querySelector<HTMLButtonElement>(".close-modal");
+    this.#modalImage = document.querySelector<HTMLImageElement>("#modal-image");
+    this.#modalDescriptionTitle = document.querySelector<HTMLTitleElement>(".modal-description-title");
+    this.#modalDescriptionYear = document.querySelector<HTMLTitleElement>("#modal-description-year");
+    this.#modalDescriptionGenre = document.querySelector<HTMLTitleElement>("#modal-description-genre");
+    this.#modalRateNumber = document.querySelector<HTMLTitleElement>("#modal-rate-number");
+    this.#modalDetailDescription = document.querySelector<HTMLTitleElement>("#modal-detail-description");
   };
 
   bindMovieClick(handler: (clickedMovieId: string) => void) {
@@ -29,6 +44,34 @@ class ModalView {
     document.addEventListener("keydown", (e) => {
       if (e.key === "Escape") this.closeModal();
     });
+  };
+
+  renderMovieDetail(data: MovieModalData) {
+    if (this.#modalImage) {
+      this.#modalImage.src = data.poster_path ? `${ORIGINAL_IMAGE}${data.poster_path}` : `${NO_IMAGE}`;
+    };
+  
+    if (this.#modalDescriptionTitle) {
+      this.#modalDescriptionTitle.innerText = data.title ? `${data.title}` : "알 수 없음";
+    };
+
+    if (this.#modalDescriptionYear) {
+      this.#modalDescriptionYear.innerText = data.release_date ? `${data.release_date.split("-")[0]}` : "알 수 없음";
+    };
+
+    if (this.#modalDescriptionGenre) {
+      this.#modalDescriptionGenre.innerText = data.genres
+        ? data.genres.map((item) => item.name).join(", ")
+        : "알 수 없음";
+    };
+
+    if (this.#modalRateNumber) {
+      this.#modalRateNumber.innerText = data.vote_average ? `${data.vote_average.toFixed(1)}` : "알 수 없음";
+    }
+
+    if (this.#modalDetailDescription) {
+      this.#modalDetailDescription.innerText = data.overview ? `${data.overview}` : "알 수 없음";
+    };
   };
 
   openModal() {
