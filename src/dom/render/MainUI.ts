19c853873181b1152b9c3dcd94a84d15bd3eac11
kojesung <js95112345@gmail.com>
2026-04-09 17:40:06 +0900

    feat: MainUI 상태 관리하는 MainUI 클래스 생성

---
+++ src/dom/render/MainUI.ts
@@ -0,0 +1,87 @@
+import { getPopularMovies, Movie } from "../../apis/movie/api";
+import { getErrorMessage } from "../../utils/getErrorMessage";
+import { renderBanner } from "./renderBanner";
+import { renderThumbnailList } from "./renderThumbnailList";
+
+type MainState =
+  | { type: "loading" }
+  | { type: "data"; movies: Movie[]; isLastPage: boolean }
+  | { type: "error"; message: string }
+  | { type: "empty" };
+
+class MainUI {
+  mainState: MainState = { type: "loading" };
+  mainThumbnailList = document.getElementById("main-thumbnail-list");
+  mainSeeMoreButton = document.getElementById("main-see-more-button");
+  skeletonList = document.getElementById("skeleton-list");
+  emptyContainer = document.getElementById("empty-container");
+  errorContainer = document.getElementById("error-container");
+  bannerContainer = document.getElementById("background-container");
+
+  constructor() {
+    this.#render();
+  }
+
+  setMainState(mainState: MainState) {
+    this.mainState = mainState;
+    this.#render();
+  }
+
+  hide() {
+    this.mainThumbnailList?.classList.add("hidden");
+    this.mainSeeMoreButton?.classList.add("hidden");
+    this.skeletonList?.classList.add("hidden");
+    this.emptyContainer?.classList.add("hidden");
+    this.errorContainer?.classList.add("hidden");
+    this.bannerContainer?.classList.add("hidden");
+  }
+
+  async load() {
+    this.setMainState({ type: "loading" });
+    try {
+      const popularMovies = await getPopularMovies({ language: "ko-KR" });
+      const isLastPage = popularMovies.page === popularMovies.total_pages;
+      this.setMainState({
+        type: "data",
+        movies: popularMovies.results,
+        isLastPage,
+      });
+    } catch (error) {
+      this.setMainState({ type: "error", message: getErrorMessage(error) });
+    }
+  }
+
+  #render() {
+    this.hide();
+    if (this.mainState.type === "data") {
+      this.mainThumbnailList?.classList.remove("hidden");
+      renderThumbnailList({
+        movies: this.mainState.movies,
+        thumbnailListElement: this.mainThumbnailList,
+      });
+
+      const bannerMovieInfo = this.mainState.movies[0];
+      if (bannerMovieInfo) renderBanner({ movie: bannerMovieInfo });
+
+      if (!this.mainState.isLastPage)
+        this.mainSeeMoreButton?.classList.remove("hidden");
+      else {
+        this.mainSeeMoreButton?.classList.add("hidden");
+      }
+    }
+
+    if (this.mainState.type === "empty") {
+      this.emptyContainer?.classList.remove("hidden");
+    }
+
+    if (this.mainState.type === "error") {
+      this.errorContainer?.classList.remove("hidden");
+    }
+
+    if (this.mainState.type === "loading") {
+      this.skeletonList?.classList.remove("hidden");
+    }
+  }
+}
+
+export default MainUI;
