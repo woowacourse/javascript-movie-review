25cc0ca8748645b3671b020409653cfc50332286
binggwa <qudrhks317@gmail.com>
2026-04-09 17:25:50 +0900

    feat: 무한 스크롤 기능 추가

--- src/movieController.ts
+++ src/movieController.ts
@@ -2,10 +2,11 @@ import { renderFetchMovieItem, hideMoreButton } from './render.ts';
 
 export const initMovieList = (query?: string) => {
   let currentPage: number = 1;
+  let isFetching: boolean = false;
   const $thumbnailList = document.querySelector('.thumbnail-list');
-  const $button = document.querySelector('#more-page-button');
+  const $button = document.querySelector('#more-page-button') as HTMLElement | null;
 
-  if (!$thumbnailList) return;
+  if (!$thumbnailList || !$button) return;
 
   const loadMovies = async () => {
     try {
@@ -20,8 +21,36 @@ export const initMovieList = (query?: string) => {
     }
   };
 
-  // 초기 렌더링
-  loadMovies();
+  const handleIntersect = async (entries: IntersectionObserverEntry[]): Promise<void> => {
+    const entry = entries[0];
+
+    if (entry.isIntersecting && !isFetching) {
+      isFetching = true;
+      await loadMovies();
+      isFetching = false;
+    }
+  };
+
+  const observerOptions: IntersectionObserverInit = {
+    root: null,
+    rootMargin: '0px 0px 200px 0px', // 더보기 버튼 200px 위부터 관찰하기
+    threshold: 0,
+  };
+
+    const observer = new IntersectionObserver(handleIntersect, observerOptions);
+
+  const start = async () => {
+    // 초기 렌더링
+    isFetching = true;
+    await loadMovies();
+    isFetching = false;
+
+    // 초기 렌더링이 끝난 뒤, 더보기 버튼 관찰 시작
+    observer.observe($button);
+  }
+
+  // 실행
+  start();
 
   // 더보기 버튼 클릭 시 렌더링
   $button?.addEventListener('click', async () => {
