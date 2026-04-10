b6c3a56acb817e60f430984fdb26686a7b3f94b1
stardusty-lab <startdusty.lab@gmail.com>
2026-04-10 19:13:15 +0900

    fix: 상수변수 처리 못한 에러 메시지 변경

--- src/main.ts
+++ src/main.ts
@@ -146,10 +146,10 @@ addEventListener("load", async () => {
         async () => await getMoviePopular({ page }),
         async (e: ApiError) => {
           if (e.status_code == 22) {
-            displayErrorMessage("잘못된 페이지 요청입니다.");
+            displayErrorMessage(errorMessages.INVALID_PAGE);
             return;
           }
-          displayErrorMessage("영화 정보를 불러오지 못했습니다. 잠시 후 다시 시도해 주세요.");
+          displayErrorMessage(errorMessages.UNKNOWN);
         },
       );
 
