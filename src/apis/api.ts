42ef4f423809949303da56f419b97fac19916fcb
박건규 <geongyu09@gmail.com>
2026-04-10 22:54:01 +0900

    feat: 영화 상세 정보 조회 API 로직 구현

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,11 +1,17 @@
 import { fetcher } from "./utils";
-import { MoviesResponse, MoviesResponseDTO } from "./dtos";
+import {
+  MovieDetail,
+  MovieDetailDTO,
+  MoviesResponse,
+  MoviesResponseDTO,
+} from "./dtos";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const API_PATH = {
   POPULAR_MOVIE: "https://api.themoviedb.org/3/movie/popular",
   SEARCH_MOVIE: "https://api.themoviedb.org/3/search/movie",
+  MOVIE_DETAIL: "https://api.themoviedb.org/3/movie",
 };
 
 export async function getPopularMovies(arg: {
@@ -68,6 +74,38 @@ export async function getSearchMovies(arg: {
         throw new Error(`HTTP error! status: ${response.status}`);
       const data = await response.json();
       return MoviesResponseDTO.from(data);
+    },
+    onSuccess,
+    onError,
+    onLoading,
+  });
+}
+
+export async function getMovieDetails(arg: {
+  movieId: string;
+  onSuccess?: (data: MovieDetail) => void;
+  onError: (error: Error) => void;
+  onLoading: () => void;
+}) {
+  const { movieId, onSuccess, onError, onLoading } = arg;
+  return fetcher<any>({
+    //TODO: API 명세 보고 타입 정의하기
+    fn: async () => {
+      const url = new URL(`${API_PATH.MOVIE_DETAIL}/${movieId}`);
+      url.searchParams.set("language", "ko-KR");
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      };
+
+      const response = await fetch(url, options);
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+      const data = await response.json();
+      return MovieDetailDTO.from(data);
     },
     onSuccess,
     onError,
--- src/apis/dtos.ts
+++ src/apis/dtos.ts
@@ -18,6 +18,52 @@ interface MovieApiResponse {
   vote_count: number;
 }
 
+interface MovieDetailApiResponse {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  } | null;
+  budget: number;
+  genres: { id: number; name: string }[];
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 interface MoviesApiResponse {
   page: number;
   results: MovieApiResponse[];
@@ -27,6 +73,7 @@ interface MoviesApiResponse {
 
 // 내부 도메인 모델
 export interface Movie {
+  id: number;
   title: string;
   rating: number;
   voteAverage: number;
@@ -39,11 +86,22 @@ export interface MoviesResponse {
   totalPages: number;
 }
 
+export interface MovieDetail {
+  id: number;
+  title: string;
+  voteAverage: number;
+  posterPath: string;
+  genres: string[];
+  releaseYear: string;
+  tagline: string;
+}
+
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
 export class MovieDTO {
   static from(data: MovieApiResponse): Movie {
     return {
+      id: data.id,
       title: data.title,
       rating: data.vote_average,
       posterPath: data.poster_path
@@ -60,6 +118,22 @@ export class MoviesResponseDTO {
       page: data.page,
       movies: data.results.map(MovieDTO.from),
       totalPages: data.total_pages,
+    };
+  }
+}
+
+export class MovieDetailDTO {
+  static from(data: MovieDetailApiResponse): MovieDetail {
+    return {
+      id: data.id,
+      title: data.title,
+      posterPath: data.poster_path
+        ? `${IMAGE_BASE_URL}/${data.poster_path}`
+        : noImagePlanetImg,
+      voteAverage: data.vote_average,
+      genres: data.genres.map((g) => g.name),
+      releaseYear: data.release_date.slice(0, 4),
+      tagline: data.tagline,
     };
   }
 }
