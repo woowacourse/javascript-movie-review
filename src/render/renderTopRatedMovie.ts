4b01e75de13f69b1febea32c1c428dc30514cd61
bel1c10ud <mail@bellclou.de>
2026-04-11 22:26:55 +0900

    feat: 최상단 영화 클릭시 모달이 띄워지는 기능 추가

--- src/render/renderTopRatedMovie.ts
+++ src/render/renderTopRatedMovie.ts
@@ -1,14 +1,20 @@
+import bindClickTopRatedMovieEvent from "../event/bindClickTopRatedMovieEvent";
 import { Movie } from "../type";
 
 export default function renderTopRatedMovie(movie: Movie) {
-  const rateEl = document.querySelector(".top-rated-movie .rate-value");
-  const titleEl = document.querySelector(".top-rated-movie .title");
+  const containerEl = document.querySelector<HTMLDivElement>('.top-rated-movie');
+  const rateEl = document.querySelector<HTMLDivElement>(".top-rated-movie .rate-value");
+  const titleEl = document.querySelector<HTMLDivElement>(".top-rated-movie .title");
   const detailButtonEl = document.querySelector<HTMLButtonElement>(
     ".top-rated-movie .detail",
   );
   const backgroundContainerEl = document.querySelector<HTMLDivElement>(
     ".background-container",
   );
+
+  if (containerEl) {
+    containerEl.dataset.movieId = movie.id.toString();
+  }
 
   if (titleEl) {
     titleEl.textContent = movie.title;
@@ -26,4 +32,6 @@ export default function renderTopRatedMovie(movie: Movie) {
   if (backgroundContainerEl) {
     backgroundContainerEl.style.backgroundImage = `url(${import.meta.env.VITE_IMAGE_BASE_URL}/w1920_and_h800_multi_faces${movie.backdrop_path})`;
   }
+
+  bindClickTopRatedMovieEvent();
 }
