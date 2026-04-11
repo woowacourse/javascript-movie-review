6372874b1baff43b3f6b0720f2d3597820ec73c3
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-11 14:15:05 +0900

    refactor: 별점 사진 배열 인덱스 수정

--- src/view/myStarRatingView.ts
+++ src/view/myStarRatingView.ts
@@ -5,44 +5,54 @@ class MyStarRatingView {
   #myStarImageArray: (HTMLImageElement | null)[] = [];
   #myStarEvaluation;
   #myStarScore;
+  #evaluation: Record<number, string> = {
+    0: "나의 별점을 눌러보세요.",
+    2: "최악이예요",
+    4: "별로예요",
+    6: "보통이에요",
+    8: "재미있어요",
+    10: "명작이에요",
+  };
 
   constructor() {
-    for (let i = 0; i < 5; i++) {
-      this.#myStarImageArray[i] = document.querySelector<HTMLImageElement>(`#my-star-image-${i + 1}`);
+    for (let i = 1; i <= 5; i++) {
+      this.#myStarImageArray[i] = document.querySelector<HTMLImageElement>(`#my-star-image-${i}`);
     };
     this.#myStarEvaluation = document.querySelector<HTMLElement>("#my-star-evaluation");
     this.#myStarScore = document.querySelector<HTMLElement>("#my-star-score");
   };
 
-  bindHandleMyStarClick(handler: (score: string | undefined) => void) {
-    this.#myStarImageArray.forEach((item, index) => {
+  bindHandleMyStarClick(handler: (score: number) => void) {
+    this.#myStarImageArray.forEach((item) => {
       item?.addEventListener("click", () => {
-        const score = item.dataset.score;
-
-        for (let i = 0; i <= index; i++) {
-          const star = this.#myStarImageArray[i];
-          if (star) star.src = `${FILLED_STAR}`;
-        };
-
-        for (let i = index + 1; i < 5; i++) {
-          const star = this.#myStarImageArray[i];
-          if (star) star.src = `${EMPTY_STAR}`;
-        };
-        
-        if (this.#myStarEvaluation) {
-          this.#myStarEvaluation.innerText = `${item.dataset.comment}`;
-        };
-
-        if (this.#myStarScore) {
-          this.#myStarScore.innerText = `${score}`;
-        };
-
+        const score = Number(item.dataset.score);
+        this.renderRating( score);
         handler(score);
       });
     });
   };
 
-  
+  renderRating(score: number) {
+    const index = score / 2;
+
+    for (let i = 1; i <= index; i++) {
+      const star = this.#myStarImageArray[i];
+      if (star) star.src = `${FILLED_STAR}`;
+    };
+
+    for (let i = index + 1; i <= 5; i++) {
+      const star = this.#myStarImageArray[i];
+      if (star) star.src = `${EMPTY_STAR}`;
+    };
+        
+    if (this.#myStarEvaluation) {
+      this.#myStarEvaluation.innerText = `${this.#evaluation[score]}`;
+    };
+
+    if (this.#myStarScore) {
+      this.#myStarScore.innerText = `${score}`;
+    };
+  };
 };
 
 export const myStarRatingView = new MyStarRatingView();
