140a004bee3d27219247477c1dae28b33b72d829
kojesung <js95112345@gmail.com>
2026-04-11 16:31:12 +0900

    feat: 영화 상세 조회 api

--- src/apis/movie/api.ts
+++ src/apis/movie/api.ts
@@ -18,6 +18,55 @@ export interface Movie {
   vote_count: number;
 }
 
+export interface MovieDetail {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 export interface PopularMoviesParameter {
   language: string;
   page: number;
@@ -30,4 +79,8 @@ export const getPopularMovies = async (
   return await tmdbFetcher<TmdbPagination<Movie[]>>(
     `/movie/popular?${searchParams.toString()}`,
   );
+};
+
+export const getMovieDetail = async (movieId: number) => {
+  return await tmdbFetcher<MovieDetail>(`/movie/${movieId}?language=ko-KR`);
 };
