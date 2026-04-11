1af32c79917e81d2326d8b8b7e0669d04a5ab37b
lee-eojin <lejin0906@snu.ac.kr>
2026-04-11 16:29:08 +0900

    chore: init.ts에서 subscriptions로 이름 변경

--- src/subscriptions.ts
+++ src/subscriptions.ts
@@ -2,56 +2,52 @@ import { eventBus } from "./pubsub/EventBus";
 import { APP_EVENTS } from "./pubsub/AppEvents";
 import { Header } from "./features/ui/Header";
 import { movieListView } from "./features/ui/MovieList";
-import { state } from "./state";
-import { handleLogo } from "./features/handler/controllerHandlers";
+import { ScrollObserver } from "./features/ui/ScrollObserver";
+import { mainTitle, sentinel } from "./dom";
 
-function attachLogoListener() {
-  const logo = document.querySelector(".logo") as HTMLElement | null;
-  if (!logo) return;
-  logo.addEventListener("click", () => {
-    eventBus.publish(APP_EVENTS.LOGO_CLICK, undefined);
-  }, { once: true });
-}
-
-export function setupSubscriptions(
-  mainTitle: HTMLElement,
-): void {
-  eventBus.subscribe(APP_EVENTS.LOGO_CLICK, handleLogo);
-  
+export function registerSubscriptions(): void {
+  const scrollObserver = new ScrollObserver(sentinel);
   eventBus.subscribe(APP_EVENTS.TITLE_CHANGED, (title : string) => {
     mainTitle.textContent = title;
   });
 
   eventBus.subscribe(APP_EVENTS.LOAD_START, () => {
+    scrollObserver.disconnect();
     movieListView.renderSkeleton();
   });
 
   eventBus.subscribe(APP_EVENTS.MOVIES_LOADED, (data ) => {
     Header.clearHeader();
     Header.render(data.results[0] ?? null);
-    attachLogoListener();
     movieListView.clearList();
     movieListView.renderMovieList(data);
+    scrollObserver.observe();
   });
 
   eventBus.subscribe(APP_EVENTS.SEARCH_LOADED, (data) => {
     Header.clearHeader();
     Header.renderSearch();
-    attachLogoListener();
     if (data.results.length === 0) {
       movieListView.clearList();
       movieListView.showEmpty();
     } else {
       movieListView.clearList();
       movieListView.renderMovieList(data);
+      scrollObserver.observe();
     }
   });
 
   eventBus.subscribe(APP_EVENTS.MORE_LOADED, (data) => {
     movieListView.renderMovieList(data);
+    scrollObserver.observe();
+  });
+
+  eventBus.subscribe(APP_EVENTS.LAST_PAGE_REACHED, () => {
+    scrollObserver.disconnect();
   });
 
   eventBus.subscribe(APP_EVENTS.ERROR, (message) => {
+    scrollObserver.disconnect();
     movieListView.clearList();
     movieListView.showError(message);
   });
