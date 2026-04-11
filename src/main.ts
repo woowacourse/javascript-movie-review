56052e4dac8da8ea0d9a9b6935a87d33dcd95216
lee-eojin <lejin0906@snu.ac.kr>
2026-04-11 16:29:36 +0900

    refactor: 중복 로직 제거

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,6 @@
-import { handleInitial, handleSearch } from "./features/handler/controllerHandlers";
-import { setupSubscriptions } from "./init";
+import { registerSubscriptions } from "./subscriptions";
+import { setupRoutes, bindDomEvents } from "./router";
 
-const mainTitle = document.querySelector(".main-title") as HTMLElement;
-
-setupSubscriptions(mainTitle);
-
-addEventListener("load", async () => {
-  await handleInitial();
-});
-
-const submitContainer = document.querySelector(".background-container") as HTMLFormElement;
-
-submitContainer.addEventListener("submit", async (e: SubmitEvent) => {
-  e.preventDefault();
-  const searchInput = document.querySelector(".search-input") as HTMLInputElement;
-  await handleSearch(searchInput.value.trim());
-});
-
+registerSubscriptions();
+setupRoutes();
+bindDomEvents();
