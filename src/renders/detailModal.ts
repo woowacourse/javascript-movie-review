041cc2bc3db99fa7f15d456692289c683133d368
stardusty-lab <startdusty.lab@gmail.com>
2026-04-11 19:21:10 +0900

    fix: localStorage 가 비었을때 오류 처리 수정

--- src/renders/detailModal.ts
+++ src/renders/detailModal.ts
@@ -43,7 +43,7 @@ export const renderDetailModal = (movieInfo: MovieInfo) => {
   if(!detailModalRate) return;
   detailModalRate.textContent = mathRound(movieInfo.vote_average).toString();
 
-  const rates = JSON.parse(localStorage.getItem('rates')|| "");
+  const rates = JSON.parse(localStorage.getItem('rates') || '{}');
   const rate = rates[movieInfo.id];
   renderRateStart(rate, rootNode);
 
@@ -63,7 +63,7 @@ export const renderDetailModal = (movieInfo: MovieInfo) => {
     star.addEventListener('click', () => {
       const rate = (index + 1) * 2;
 
-      const prevRates = localStorage.getItem('rates') ? JSON.parse(localStorage.getItem('rates') || ""): {};
+      const prevRates = localStorage.getItem('rates') ? JSON.parse(localStorage.getItem('rates') || "{}"): {};
 
       const { id } = movieInfo;
       const rates = { ...prevRates, [id]: rate };
