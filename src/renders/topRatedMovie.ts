58a0b2c37ab8983191fe04e3ad79a8f95df81604
boyeon <boyeon6331@gmail.com>
2026-04-11 23:28:55 +0900

    refactor: 요소 못 찾으면 종료 대신 찾은 요소 업데이트로 변경

--- src/renders/topRatedMovie.ts
+++ src/renders/topRatedMovie.ts
@@ -2,36 +2,42 @@ import { Movie } from "../services/dto";
 
 export const renderTopRatedMovie = (movie: Movie) => {
   const topRatedContainer = document.querySelector(".top-rated-container");
-  if (!topRatedContainer) return null;
+  if (!topRatedContainer) return;
 
   const overlay = document.querySelector<HTMLDivElement>(".overlay");
-  if (!overlay) return null;
-  overlay.style.background = `url(${`https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces` + movie.backdrop_path}) center center no-repeat`;
+  if (overlay) {
+    overlay.style.background = `url(${`https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces` + movie.backdrop_path}) center center no-repeat`;
+  }
 
   const rateValue = topRatedContainer.querySelector(".rate-value");
-  if (!rateValue) return null;
-  rateValue.textContent = movie.vote_average.toString();
+  if (rateValue) {
+    rateValue.textContent = movie.vote_average.toString();
+  }
 
   const title = topRatedContainer.querySelector(".title");
-  if (!title) return null;
-  title.textContent = movie.title;
+  if (title) {
+    title.textContent = movie.title;
+  }
 };
 
 export const removeTopRatedMovie = () => {
   const topRatedMovie =
     document.querySelector<HTMLDivElement>(".top-rated-movie");
-  if (!topRatedMovie) return null;
-  topRatedMovie.style.display = "none";
+  if (topRatedMovie) {
+    topRatedMovie.style.display = "none";
+  }
 
   const background = document.querySelector<HTMLDivElement>(
     ".background-container",
   );
-  if (!background) return null;
-  background.style.backgroundColor = "transparent";
-  background.style.height = "auto";
+  if (background) {
+    background.style.backgroundColor = "transparent";
+    background.style.height = "auto";
+  }
 
   const overlay = document.querySelector<HTMLDivElement>(".overlay");
-  if (!overlay) return null;
-  overlay.style.background = "";
-  overlay.style.display = "none";
+  if (overlay) {
+    overlay.style.background = "";
+    overlay.style.display = "none";
+  }
 };
