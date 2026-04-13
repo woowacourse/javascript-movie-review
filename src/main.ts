9e055cfd79829def67a4d0be29f557538bee4e3d
stardusty-lab <startdusty.lab@gmail.com>
2026-04-13 16:10:38 +0900

    feat: errorMessages 상수변수 사용, 메시지 보여주는 처리 함수 사용 미반영분 추가 처리

--- src/main.ts
+++ src/main.ts
@@ -162,10 +162,10 @@ const handleMoreMovie = () => {
       async () => await getMoviePopular({ page }),
       async (e: RequestFetchResponse) => {
         if (e.data.status_code == 22) {
-          alert("잘못된 페이지 요청입니다.");
+          displayErrorMessage(errorMessages.INVALID_PAGE);
           return;
         }
-        alert("영화 정보를 불러오지 못했습니다. 잠시 후 다시 시도해 주세요.");
+        displayErrorMessage(errorMessages.UNKNOWN);
       },
     );
 
