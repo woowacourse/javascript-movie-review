9551cf7a19e2852e725eaa5ef0a8261a60e2fd1f
inaemin <inaemin@gmail.com>
2026-04-13 17:17:48 +0900

    feat: toMovieDetail 변환 함수 추가 및 평점 반올림 처리
    - TMDBMovieDetail을 MovieDetail로 변환하는 toMovieDetail 추가
    - toMovieItem, toMovieDetail 평점 소수점 첫째자리 반올림 처리

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,4 +1,4 @@
-import { TMDBMovie } from "../api/types";
+import { TMDBMovie, TMDBMovieDetail, MovieDetail } from "../api/types";
 import { TMDB_IMAGE_BASE_URL } from "../api/constants";
 import { MovieItem } from "../domains/movie/MovieList";
 
@@ -8,5 +8,17 @@ export const toMovieItem = (movie: TMDBMovie): MovieItem => ({
   id: movie.id,
   title: movie.title,
   posterSrc: `${TMDB_IMAGE_BASE_URL}/${TMDB_IMAGE_SIZE}${movie.poster_path}`,
-  rating: movie.vote_average,
+  rating: Math.round(movie.vote_average * 10) / 10,
+});
+
+export const toMovieDetail = (movie: TMDBMovieDetail): MovieDetail => ({
+  id: movie.id,
+  title: movie.title,
+  overview: movie.overview,
+  posterSrc: `${TMDB_IMAGE_BASE_URL}/${TMDB_IMAGE_SIZE}${movie.poster_path}`,
+  releaseYear: movie.release_date.slice(0, 4),
+  rating: Math.round(movie.vote_average * 10) / 10,
+  genres: movie.genres.map((g) => g.name).join(", "),
+  runtime: movie.runtime,
+  tagline: movie.tagline,
 });
