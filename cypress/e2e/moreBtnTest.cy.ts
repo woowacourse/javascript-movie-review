97d46db7e615e2204c34e202c1e832a54e549d83
김상윤 <ksy151020@gmail.com>
2026-04-13 17:52:05 +0900

    test: 무한스크롤 테스트를 fixture 기반 동적 검증으로 개선   - 페이지 크기를 fixture에서 읽어 하드코딩 제거   - 스크롤 횟수를 루프로 관리   - 절대 개수 대신 "이전보다 증가했는지"로 검증해 API 응답 크기 변동에 유연하게 대응

--- cypress/e2e/moreBtnTest.cy.ts
+++ cypress/e2e/moreBtnTest.cy.ts
@@ -2,48 +2,67 @@ describe('무한스크롤 테스트', () => {
     beforeEach(() => {
         cy.intercept('GET', '**/movie/popular**', { fixture: 'popularMovies.json' });
     });
-
     it('스크롤을 내리면 추가 영화 목록을 불러온다.', () => {
-        cy.visit('http://localhost:5173');
+        cy.fixture('popularMovies.json').then((data) => {
+            const pageSize = data.results.length;
+            const scrollCount = 20;
 
-        cy.get('.thumbnail-list li').should('have.length', 20);
+            cy.visit('http://localhost:5173');
 
-        cy.get('.thumbnail-list li').last().scrollIntoView();
+            cy.get('.thumbnail-list li').should('have.length', pageSize);
 
-        cy.get('.thumbnail-list li').should('have.length', 40);
+            for (let i = 0; i < scrollCount; i++) {
+                cy.get('.thumbnail-list li').then(($items) => {
+                    const beforeCount = $items.length;
 
-        cy.get('.thumbnail-list li').last().scrollIntoView();
+                    cy.get('.thumbnail-list li').last().scrollIntoView();
 
-        cy.get('.thumbnail-list li').should('have.length', 60);
-
-        cy.get('.thumbnail-list li').last().scrollIntoView();
-
-        cy.get('.thumbnail-list li').should('have.length', 80);
-
-        cy.get('.thumbnail-list li').last().scrollIntoView();
-
-        cy.get('.thumbnail-list li').should('have.length', 100);
-
-        cy.get('.thumbnail-list li').last().scrollIntoView();
-
-        cy.get('.thumbnail-list li').should('have.length', 120);
-
-        cy.get('.thumbnail-list li').last().scrollIntoView();
-
-        cy.get('.thumbnail-list li').should('have.length', 140);
-
-        cy.get('.thumbnail-list li').last().scrollIntoView();
-
-        cy.get('.thumbnail-list li').should('have.length', 160);
-
-        cy.get('.thumbnail-list li').last().scrollIntoView();
-
-        cy.get('.thumbnail-list li').should('have.length', 180);
-
-        cy.get('.thumbnail-list li').last().scrollIntoView();
-
-        cy.get('.thumbnail-list li').should('have.length', 200);
+                    cy.get('.thumbnail-list li').should('have.length.greaterThan', beforeCount);
+                });
+            }
+        });
     });
+    // it('스크롤을 내리면 추가 영화 목록을 불러온다.', () => {
+    //     cy.visit('http://localhost:5173');
+
+    //     cy.get('.thumbnail-list li').should('have.length', 20);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 40);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 60);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 80);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 100);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 120);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 140);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 160);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 180);
+
+    //     cy.get('.thumbnail-list li').last().scrollIntoView();
+
+    //     cy.get('.thumbnail-list li').should('have.length', 200);
+    // });
 });
 
 // describe('more btn test', () => {
