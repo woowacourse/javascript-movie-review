3ed864649fbcaceec48e10a4dbc4c519e304517e
boyeon <boyeon6331@gmail.com>
2026-04-14 23:47:07 +0900

    refactor: GET API 요청 함수 공통 로직 추상화

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,13 +11,8 @@ export class ApiError extends Error {
   }
 }
 
-export const getPopularMovies = async ({
-  page,
-}: {
-  page: number;
-}): Promise<Movies> => {
-  const url = `${apiUrl}/movie/popular?page=${page}&language=ko-KR`;
-  const res = await fetch(url, {
+const requestGet = async <T>(path: string): Promise<T> => {
+  const res = await fetch(`${apiUrl}${path}`, {
     method: "get",
     headers: {
       Authorization: `Bearer ${apiKey}`,
@@ -30,19 +25,16 @@ export const getPopularMovies = async ({
   throw new ApiError(errorBody.status_message, errorBody.status_code);
 };
 
+export const getPopularMovies = async ({
+  page,
+}: {
+  page: number;
+}): Promise<Movies> => {
+  return requestGet<Movies>(`/movie/popular?page=${page}&language=ko-KR`);
+};
+
 export const getTopRatedMovies = async () => {
-  const url = `${apiUrl}/movie/top_rated?language=ko-KR`;
-  const res = await fetch(url, {
-    method: "get",
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-    },
-  });
-
-  if (res.ok) return await res.json();
-
-  const errorBody = await res.json();
-  throw new ApiError(errorBody.status_message, errorBody.status_code);
+  return requestGet<Movies>(`/movie/top_rated?language=ko-KR`);
 };
 
 export const getSearchMovies = async ({
@@ -52,31 +44,11 @@ export const getSearchMovies = async ({
   page: number;
   query: string;
 }): Promise<Movies> => {
-  const url = `${apiUrl}/search/movie?page=${page}&query=${query}&language=ko-KR`;
-  const res = await fetch(url, {
-    method: "get",
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-    },
-  });
-
-  if (res.ok) return await res.json();
-
-  const errorBody = await res.json();
-  throw new ApiError(errorBody.status_message, errorBody.status_code);
+  return requestGet<Movies>(
+    `/search/movie?page=${page}&query=${query}&language=ko-KR`,
+  );
 };
 
 export const getMovieDetail = async (movieId: string): Promise<MovieDetail> => {
-  const url = `${apiUrl}/movie/${movieId}?language=ko-KR`;
-  const res = await fetch(url, {
-    method: "get",
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-    },
-  });
-
-  if (res.ok) return await res.json();
-
-  const errorBody = await res.json();
-  throw new ApiError(errorBody.status_message, errorBody.status_code);
+  return requestGet<MovieDetail>(`/movie/${movieId}?language=ko-KR`);
 };
