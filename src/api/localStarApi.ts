ee3513bb20d768913007b77877f573e6900baa20
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-15 16:19:54 +0900

    refactor: localstorage key 값 수정

--- src/api/localStarApi.ts
+++ src/api/localStarApi.ts
@@ -1,10 +1,10 @@
 export const localStarApi = {
   async saveRating(movieId: number, score: number) {
-    window.localStorage.setItem(`${movieId}`, `${score}`);
+    window.localStorage.setItem(`star_${movieId}`, `${score}`);
   },
 
   async getRating(movieId: number) {
-    const movieRatingScore = await window.localStorage.getItem(`${movieId}`);
+    const movieRatingScore = await window.localStorage.getItem(`star_${movieId}`);
     return movieRatingScore ? Number(movieRatingScore) : 0;
   },
 };
