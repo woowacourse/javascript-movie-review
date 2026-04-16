9c4a9dddfb1b703398d3ba79818ebd692987d2d9
박건규 <geongyu09@gmail.com>
2026-04-16 13:02:21 +0900

    fix: getItemFromLocalStorage에서 JSON.parse에러 처리 로직 추가

--- src/storage/utils/localstorage.ts
+++ src/storage/utils/localstorage.ts
@@ -1,6 +1,12 @@
 export const getItemFromLocalStorage = (key: string) => {
   const item = localStorage.getItem(key);
-  return item ? (JSON.parse(item) as string) : null;
+  if (!item) return null;
+
+  try {
+    return JSON.parse(item) as string;
+  } catch {
+    return null;
+  }
 };
 
 export const setItemInLocalStorage = (key: string, value: any) => {
