540f772e837e5e6fb87d8bc5dd7d0dedbd3aafda
박건규 <geongyu09@gmail.com>
2026-04-16 11:26:34 +0900

    fix: createStore에서 객체형 상태 사용히 깊은 복사를 하도록 수정

--- src/state/utils/state.ts
+++ src/state/utils/state.ts
@@ -1,24 +1,21 @@
+import { deepCopy } from "../../utils/deepCopy";
+
 export const createStore = <StateType>(initialState: StateType) => {
   if (typeof initialState === "function" || typeof initialState === "symbol")
     throw new Error("초기값은 함수가 아닌 객체 혹은 원시값이어야 합니다.");
 
-  let state: StateType;
-  if (typeof initialState === "object") {
-    state = { ...initialState };
-  } else {
-    state = initialState;
-  }
+  let state = deepCopy(initialState);
 
-  const getter = () => state;
+  const getter = () => deepCopy(state);
 
   const modify = (
     update: StateType | ((prevState: StateType) => StateType),
   ) => {
     if (typeof update === "function") {
-      state = (update as (prevState: StateType) => StateType)(state);
+      state = deepCopy((update as (prevState: StateType) => StateType)(state));
       return;
     }
-    state = update;
+    state = deepCopy(update);
   };
 
   return [getter, modify] as const;
