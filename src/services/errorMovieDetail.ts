bf3007eda59b93426da344bedbd192747e739c6f
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-16 13:04:00 +0900

    refactor: console.error()로 변경

--- src/services/errorMovieDetail.ts
+++ src/services/errorMovieDetail.ts
@@ -1,6 +1,6 @@
 import { modalView } from "../view/modalView";
 
 export function errorMovieDetail(errorMessage: string) {
-  console.log("에러 원인:", errorMessage);
+  console.error("에러 원인:", errorMessage);
   modalView.renderError();
 }
--- src/services/errorMovieList.ts
+++ src/services/errorMovieList.ts
@@ -3,18 +3,18 @@ import { movieListView } from "../view/movieListView";
 
 export const errorMovieList = {
   handleHomeError: (errorMessage: string) => {
-    console.log("에러 원인:", errorMessage);
+    console.error("에러 원인:", errorMessage);
     bannerView.renderErrorBanner();
     movieListView.renderErrorList();
   },
 
   handleLoadMoreError: (errorMessage: string) => {
-    console.log("에러 원인:", errorMessage);
+    console.error("에러 원인:", errorMessage);
     movieListView.renderErrorList();
   },
 
   handleSearchError: (errorMessage: string) => {
-    console.log("에러 원인:", errorMessage);
+    console.error("에러 원인:", errorMessage);
     movieListView.renderErrorList();
   },
 }
